288aae6a0cea21b44ea6fae8118a5d80b46cdf47
Milan Andric <mandric@gmail.com>
2012-01-16 21:14:09 -0600

    added MSBM form test.

---
+++ tests/msbm_form.js
@@ -0,0 +1,44 @@
+var smsparser = require('views/lib/smsparser'),
+    smsforms = require('views/lib/smsforms');
+
+
+exports.msbm_example_data = function (test) {
+    var def = smsforms['MSBM'];
+    var doc = {
+        sent_timestamp: '1-16-12 19:35',
+        from: '+15551212',
+        message: '1!MSBM!2012#1#16#12345678901#123#456#789#123#456#789#123#456#123#456'
+    };
+
+    test.expect(2);
+
+    var obj = smsparser.parse(def.fields, doc);
+    var expectedObj = {
+        med_year: '2012',
+        med_month: '1',
+        med_day: 16,
+        med_rc: 12345678901,
+        med_cta_a: 123,
+        med_cta_c: 456,
+        med_tdr_a: 789,
+        med_tdr_c: 123,
+        med_ctm_a: 456,
+        med_ctm_c: 789,
+        med_sro_a: 123,
+        med_sro_c: 456,
+        med_para_a: 123,
+        med_para_c: 456 
+    };
+
+    //console.log(obj);
+    //console.log(expectedObj);
+
+    test.same(obj, expectedObj);
+
+    var arr = smsparser.parseArray(def.fields, doc);
+    var expectedArr = ['1-16-12 19:35', '+15551212', '2012', '1', 16, 12345678901, 123, 456, 789, 123, 456, 789, 123, 456, 123, 456];
+
+    test.same(arr, expectedArr);
+
+    test.done();
+};
