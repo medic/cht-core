41850575c968a28b4d5c111f822bc705e7948306
Milan Andric <mandric@gmail.com>
2012-01-24 18:07:53 -0600

    cleaned things up a bit, towards #18.

--- scripts/tests/referral.js
+++ scripts/tests/referral.js
@@ -4,45 +4,50 @@ var fs = require('fs'),
     http = require('http'),
     querystring = require('querystring');
 
+var log = function(o) {
+    console.log('');
+    console.log(o);
+};
+
 // http request that does a callback request
 var req = function(options) {
     var r = http.request(options, function(res) {
+        log('http.request options:');
+        log(options);
         var resBody = '';
         res.setEncoding('utf8');
         res.on('data', function (chunk) {
             resBody += chunk;
         });
         res.on('end', function() {
-            console.log('response body');
-            console.log(resBody);
+            log('response body');
+            log(resBody);
             var resJSON = JSON.parse(resBody);
             var callback = resJSON.callback;
             if(callback && callback.options) {
-                console.log('callback request options');
-                console.log(callback.options);
                 var req2 = req(callback.options);
                 if (callback.data) {
-                    console.log('callback request data');
-                    console.log(JSON.stringify(callback.data));
+                    log('callback request data');
+                    log(JSON.stringify(callback.data));
                     req2.write(JSON.stringify(callback.data));
                 }
                 req2.end();
             } else {
-                console.log('done with task creation. do tasks...');
+                log('done with task creation. do tasks...');
                 tasksCreated === false ? doTasks() : allDone();
             }
         });
         res.on('error', function(err) {
             if (err) {
-                console.log('response error.')
-                console.log(err);
+                log('response error.')
+                log(err);
             }
         });
     });
     r.on('error', function(err) {
         if (err) {
-            console.log('request error.');
-            console.log(err);
+            log('request error.');
+            log(err);
         }
     });
     return r;
@@ -51,7 +56,7 @@ var req = function(options) {
 var tasksCreated = false;
 
 var allDone = function() {
-    console.log('Done!');
+    log('Done!');
 };
 
 var doTasks = function() {
@@ -71,13 +76,12 @@ var run = function(options, data) {
     options.headers['Content-Length'] = qs.length;
     // smsform message is received
     var r = req(options);
-    console.log('request data');
-    console.log(qs);
+    log('request data');
+    log(qs);
     r.write(qs);
     // TODO request new tasks/referrals to be sent out
 };
 
-console.log('http req options');
 var options = {
     host: 'localhost',
     port: 5984,
@@ -87,9 +91,8 @@ var options = {
         'Content-Type': 'application/x-www-form-urlencoded',
     }
 };
-console.log(options);
 
-console.log('data: smssync format example for referral form submission');
+log('smssync format example referral form submission data:');
 var data = {
     from: '+13128131320',
     message: '1!MSBR!2012#1#24#12345678901#1111#bbbbbbbbbbbbbbbbbbbb#22#8#cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc',
@@ -98,6 +101,6 @@ var data = {
     message_id: '13579',
     foo: 'bar' // extra is ok
 };
-console.log(data);
+log(data);
 
 run(options, data);
