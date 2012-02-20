e26f8d16293ba6ba7b58a322805a561f5ba6134c
Milan Andric <mandric@gmail.com>
2012-02-20 16:00:24 -0600

    added documentation to explain a bit more in msbr referral test. #25

--- packages/kujua-sms/tests/kujua-sms/referral_msbr.js
+++ packages/kujua-sms/tests/kujua-sms/referral_msbr.js
@@ -16,7 +16,7 @@ exports.referral_msbr = function (test) {
         return Math.floor(Math.random() * (to - from + 1) + from);
     };
 
-    // random ref_rc for better test data
+    // random ref_rc (Referral ID) for better test data
     var ref_rc = String(rand());
 
     var data = {
@@ -28,6 +28,7 @@ exports.referral_msbr = function (test) {
         foo: 'bar' // extra is ok
     };
 
+    // sms_message document
     var sms_message = {
        "from": "+13125551212",
        "message": "1!MSBR!2012#1#24#" + ref_rc +
@@ -39,6 +40,8 @@ exports.referral_msbr = function (test) {
        "locale": "en",
        "form": "MSBR"
     };
+
+    // parsed sms message data that includes labels
     var form_data = {
        "ref_year": [
           "2012",
@@ -78,6 +81,7 @@ exports.referral_msbr = function (test) {
        ]
     };
 
+    // run add_sms update
     var result = updates.add_sms(null, {
         method: "POST",
         query:{},
@@ -90,11 +94,13 @@ exports.referral_msbr = function (test) {
         form: data,
     });
 
+    // check results of add_sms update run
     var doc = result[0];
     var resp = JSON.parse(result[1]);
 
     test.same(doc, sms_message);
 
+    // Assert proper outgoing messages for the gateway to process
     test.same(resp.payload, {
         "success": true,
         "task": "send",
@@ -106,6 +112,10 @@ exports.referral_msbr = function (test) {
         ]
     });
 
+    /*
+     * Assert proper callback options for the gateway to process the next
+     * request.
+     */
     test.same(resp.callback.options, {
         "host": window.location.hostname,
         "port": window.location.port,
@@ -116,6 +126,10 @@ exports.referral_msbr = function (test) {
         }
      });
 
+    /*
+     * Assert we have formed a phase 1 tasks_referral document, not 'to' and
+     * 'clinic' fields are null. These get added in the next callback request.
+     */
     test.same(resp.callback.data, {
         "type": "tasks_referral",
         "state": "",
@@ -130,6 +144,9 @@ exports.referral_msbr = function (test) {
         "errors": []
     });
 
+    /*
+     * Our clinic mockup for this test. MSBR is Clinic -> Health Center
+     */
     var clinic1 = {
         "_id": "4a6399c98ff78ac7da33b639ed60f458",
         "_rev": "1-0b8990a46b81aa4c5d08c4518add3786",
@@ -148,7 +165,10 @@ exports.referral_msbr = function (test) {
         }
     };
 
-    // redefine global getRow to mock list function
+    /*
+     * Mockup out view data for lists.tasks_referral
+     * Redefines global getRow function, to pass into fakerequest
+     */
     var viewdata = {rows: [
         {
             "key": ["+13125551212"],
@@ -156,8 +176,10 @@ exports.referral_msbr = function (test) {
         }
     ]};
 
-    // step 2 call task_referral list function to add clinic data and construct
-    // final callback to save the document.
+    /*
+     * Execute step 2 by calling task_referral list function.  This adds clinic
+     * data and constructs task document ready to be saved.
+     */
     var result2 = fakerequest.list(lists.tasks_referral, viewdata, {
         method: "POST",
         query:{form: 'MSBR'},
@@ -173,6 +195,9 @@ exports.referral_msbr = function (test) {
         "Content-Type": "application/json; charset=utf-8"
     });
 
+    /*
+     * Assert callback options for creating a document.
+     */
     var doc2 = JSON.parse(result2.body);
     test.same(doc2.callback.options, {
         "host": window.location.hostname,
@@ -184,10 +209,13 @@ exports.referral_msbr = function (test) {
         }
      });
 
-
     // delete volatile properties for test
     delete doc2.callback.data.created;
 
+    /*
+     * The task that gets created after step2, includes clinic data and health
+     * center phone number is in the to field. Define and assert the task doc.
+     */
     var task = {
         "type": "tasks_referral",
         "state": "pending",
@@ -210,12 +238,15 @@ exports.referral_msbr = function (test) {
 
     test.same(doc2.callback.data, task);
 
-    // Step 3 tasks_referral doc (callback data) is saved
-    // HTTP POST to CouchDB API (No test)
+    // Step 3 tasks_referral doc is saved
+    // HTTP POST to CouchDB API (No test needed?)
 
-    // Step 4 Another request polls the db for tasks_referral documents that
-    // are pending. Messages get formatted into the payload for the gateway.
-
+    /*
+     * Step 4 Another request polls the db for tasks_referral documents that
+     * are pending. Messages get formatted into the payload for the gateway.
+     *
+     * Mockup the view data first.
+     */
     var viewdata = {rows: [
         {
             "key": [null, ref_rc],
@@ -224,6 +255,9 @@ exports.referral_msbr = function (test) {
         }
     ]};
 
+    /*
+     * Call the tasks_pending view.
+     */
     var result3 = fakerequest.list(lists.tasks_pending, viewdata, {
         method: "GET",
         headers:{
@@ -232,6 +266,10 @@ exports.referral_msbr = function (test) {
         }
     });
 
+    /*
+     * Assert the payload includes the message data for the gateway to send
+     * out, message includes all the data and goes to the health center.
+     */
     var doc3 = JSON.parse(result3.body);
     test.same(doc3.payload, {
         "success": true,
@@ -246,6 +284,10 @@ exports.referral_msbr = function (test) {
         ]
     });
 
+    /*
+     * Assert the callback to save the document after being processed, a POST
+     * to _all_docs is formed to support multiple tasks being processed.
+     */
     test.same(doc3.callback.options, {
        "host": window.location.hostname,
        "port": window.location.port,
@@ -256,6 +298,10 @@ exports.referral_msbr = function (test) {
        }
     });
 
+    /*
+     * Assert that docs is an array, and the document includes all the fields.
+     * The only real change is the state field is set to 'sent'.
+     */
     test.same(doc3.callback.data.docs, [{
         "type": "tasks_referral",
         "state": "sent",
