3f13d7b28df01858a7df78fee959d1fef55d3eee
Milan Andric <mandric@gmail.com>
2012-02-20 12:11:31 -0600

    moved all views into kujua-sms, since it needs them. #154

--- packages/kujua-sms/kujua-sms/views.js
+++ packages/kujua-sms/kujua-sms/views.js
@@ -21,3 +21,34 @@ exports.sms_messages = {
         }
     }
 };
+
+/*
+ * Get clinic based on phone number
+ */
+exports.clinic_by_phone = {
+    map: function (doc) {
+        if (doc.type === 'clinic') {
+            emit([doc.contact.phone], doc);
+        }
+    }
+};
+
+/*
+ * Get clinic based on referral id in a tasks_referral doc.
+ */
+exports.clinic_by_refid = {
+    map: function (doc) {
+        if (doc.type === 'tasks_referral' && doc.refid) {
+            // need String because rewriter wraps everything in quotes
+            emit([String(doc.refid), doc.created], doc.clinic);
+        }
+    }
+};
+
+exports.tasks_pending = {
+    map: function (doc) {
+        if (doc.type.substr(0,6) === 'tasks_' && doc.state === 'pending') {
+            emit([doc.created, doc.refid]);
+        }
+    }
+};
