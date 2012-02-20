4a91e4f448c882b73f824290126f7159c74a7b70
Milan Andric <mandric@gmail.com>
2012-02-20 15:19:23 -0600

    completed msbr referrals test. #25

--- packages/kujua-sms/tests/kujua-sms/referral_msbr.js
+++ packages/kujua-sms/tests/kujua-sms/referral_msbr.js
@@ -8,7 +8,7 @@ var updates = require('kujua-sms/updates'),
 
 exports.referral_msbr = function (test) {
 
-    test.expect(7);
+    test.expect(10);
 
     var rand = function(from, to) {
         from = from || 10000000000;
@@ -167,7 +167,7 @@ exports.referral_msbr = function (test) {
     ]};
 
     // step 2 call task_referral list function to add clinic data and construct
-    // last callback.
+    // final callback to save the document.
     var result2 = fakerequest.list(lists.tasks_referral, viewdata, {
         method: "POST",
         query:{form: 'MSBR'},
@@ -182,6 +182,7 @@ exports.referral_msbr = function (test) {
     test.same(result2.headers, {
         "Content-Type": "application/json; charset=utf-8"
     });
+
     var doc2 = JSON.parse(result2.body);
     test.same(doc2.callback.options, {
         "host": window.location.hostname,
@@ -197,7 +198,7 @@ exports.referral_msbr = function (test) {
     // delete volatile properties for test
     delete doc2.callback.data.created;
 
-    test.same(doc2.callback.data, {
+    var task = {
         "type": "tasks_referral",
         "state": "pending",
         "from": "+13125551212",
@@ -261,15 +262,139 @@ exports.referral_msbr = function (test) {
         },
         "clinic": clinic1,
         "errors": [],
+    };
+
+    test.same(doc2.callback.data, task);
+
+    // Step 3 tasks_referral doc (callback data) is saved
+    // HTTP POST to CouchDB API (No test)
+
+    // Step 4 Another request polls the db for tasks_referral documents that
+    // are pending. Messages get formatted into the payload for the gateway.
+
+    var viewdata = {rows: [
+        {
+            "key": [null, ref_rc],
+            "value": null,
+            "doc": task
+        }
+    ]};
+
+    var result3 = fakerequest.list(lists.tasks_pending, viewdata, {
+        method: "GET",
+        headers:{
+            "Content-Type":"application/json; charset=utf-8",
+            "Host": window.location.host
+        }
     });
 
-    // TODO Step 2 assertions
+    var doc3 = JSON.parse(result3.body);
+    test.same(doc3.payload, {
+        "success": true,
+        "task": "send",
+        "secret": "",
+        "messages": [
+            {
+                "to": "+17085551212",
+                "message": "Année: 2012, Mois: 1, Jour: 24, Code du RC: " + ref_rc 
+                           + ", Heure de départ: 1111, Nom: bbbbbbbbbbbbbbbbbbbb, Age: 22, Motif référence: TB dans le rouge, Si 'autre', précisez motif référence: cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
+            }
+        ]
+    });
 
-    // Step 3 The finalized doc is POSTed/saved
-    // Step 4 Another request polls the db for tasks_referral documents that
-    // are pending. messages get sent
-    // Step 5 Bulk db update to update the 'state' field to 'complete'
-    // http post to _bulk_docs
+    test.same(doc3.callback.options, {
+       "host": window.location.hostname,
+       "port": window.location.port,
+       "path": appdb + "/_bulk_docs",
+       "method": "POST",
+       "headers": {
+          "Content-Type": "application/json; charset=utf-8"
+       }
+    });
+
+    test.same(doc3.callback.data.docs, [{
+        "type": "tasks_referral",
+        "state": "sent",
+        "from": "+13125551212",
+        "to": "+17085551212",
+        "refid": ref_rc,
+        "sms_message": {
+           "from": "+13125551212",
+           "message": "1!MSBR!2012#1#24#" + ref_rc + "#1111#bbbbbbbbbbbbbbbbbbbb#22#8#cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+           "sent_timestamp": "1-19-12 18:45",
+           "sent_to": "+15551212",
+           "foo": "bar",
+           "type": "sms_message",
+           "locale": "en",
+           "form": "MSBR"
+        },
+        "messages": [
+           {
+              "to": "+17085551212",
+              "message": "Année: 2012, Mois: 1, Jour: 24, Code du RC: " + ref_rc + ", Heure de départ: 1111, Nom: bbbbbbbbbbbbbbbbbbbb, Age: 22, Motif référence: TB dans le rouge, Si 'autre', précisez motif référence: cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
+           }
+        ],
+        "form": "MSBR",
+        "form_data": {
+           "ref_year": [
+              "2012",
+              "Année"
+           ],
+           "ref_month": [
+              "1",
+              "Mois"
+           ],
+           "ref_day": [
+              24,
+              "Jour"
+           ],
+           "ref_rc": [
+              ref_rc,
+              "Code du RC"
+           ],
+           "ref_hour": [
+              1111,
+              "Heure de départ"
+           ],
+           "ref_name": [
+              "bbbbbbbbbbbbbbbbbbbb",
+              "Nom"
+           ],
+           "ref_age": [
+              22,
+              "Age"
+           ],
+           "ref_reason": [
+              "TB dans le rouge",
+              "Motif référence"
+           ],
+           "ref_reason_other": [
+              "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+              "Si 'autre', précisez motif référence"
+           ]
+        },
+        "clinic": {
+           "_id": "4a6399c98ff78ac7da33b639ed60f458",
+           "_rev": "1-0b8990a46b81aa4c5d08c4518add3786",
+           "type": "clinic",
+           "name": "Example clinic 1",
+           "contact": {
+              "name": "Sam Jones",
+              "phone": "+13125551212"
+           },
+           "parent": {
+              "type": "health_center",
+              "contact": {
+                 "name": "Neal Young",
+                 "phone": "+17085551212"
+              }
+           }
+        },
+        "errors": []
+    }]);
+
+    // Step 5 Bulk db update to update the 'state' field to 'sent'
+    // HTTP POST to _bulk_docs CouchDB API (No test)
 
     test.done();
 };
