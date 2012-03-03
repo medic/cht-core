97c42c6be05f71e115ee6bd95e9437dacf62816d
Milan Andric <mandric@gmail.com>
2012-03-03 03:11:17 -0600

    added kujua-utils and duality loader packages. some other minor stuff.

--- packages/kujua-utils/kujua-utils.js
+++ packages/kujua-utils/kujua-utils.js
@@ -1,7 +1,38 @@
 var jsDump = require('jsDump'),
+    _ = require('underscore')._,
     settings = require('settings/root');
 
-var logger = exports.logger = {
+exports.prettyMonth = function (month, full) {
+    var months_short = new Array(
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
+        'Sep', 'Oct', 'Nov', 'Dec');
+    var months_full = _.map(exports.months(), function(month) { return month[1]; });
+
+    if (full) {
+        return months_full[month];
+    }
+    
+    return months_short[month];
+};
+
+exports.months = function () {
+    return [
+        [0, 'January'],
+        [1, 'February'],
+        [2, 'March'],
+        [3, 'April'],
+        [4, 'May'],
+        [5, 'June'],
+        [6, 'July'],
+        [7, 'August'],
+        [8, 'September'],
+        [9, 'October'],
+        [10, 'November'],
+        [11, 'December']
+    ];
+};
+
+exports.logger = {
     levels: {silent:0, error:1, info:2, debug:3},
     log: function(obj) {
         if (typeof(console) !== 'undefined') {
@@ -28,7 +59,7 @@ var logger = exports.logger = {
     }
 };
 
-var dumper = exports.dumper = {
+exports.dumper = {
     levels: {silent:0, error:1, warn:2, info:3, debug:4},
     dump: function(obj) {
         if (typeof(console) !== 'undefined') {
