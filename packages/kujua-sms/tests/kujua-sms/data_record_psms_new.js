3176e8cfe1e5c9aa1a5cdf96c5203b14e352509f
Milan Andric <mandric@gmail.com>
2012-03-06 11:47:25 -0600

    saving progress on psms data record test refactor. #42

--- packages/kujua-sms/tests/kujua-sms/data_record_psms_new.js
+++ packages/kujua-sms/tests/kujua-sms/data_record_psms_new.js
@@ -7,79 +7,105 @@ var updates = require('kujua-sms/updates'),
     fakerequest = require('couch-fakerequest'),
     helpers = require('../../test-helpers/helpers');
 
+/*
+ * https://github.com/caolan/async#waterfall
+ *
+ * async.waterfall([
+ *  function(callback){
+ *      callback(null, 'one', 'two');
+ *  },
+ *  function(arg1, arg2, callback){
+ *      callback(null, 'three');
+ *  },
+ *  function(arg1, callback){
+ *      // arg1 now equals 'three'
+ *      callback(null, 'done');
+ *  }
+ *  ], function (err, result) {
+ *      // result now equals 'done'
+ *  });
+ */
 
-var add_sms = function (data, sms_message) {
+var fake = {
 
-    var result = updates.add_sms(null, {
-        uuid: '14dc3a5aa6',
-        method: "POST",
-        query: {},
-        headers: helpers.headers("url", querystring.stringify(data)),
-        body: querystring.stringify(data),
-        form: data
-    });
+    updates: {
+        add_sms: function (data, req) {
 
-    var doc = result[0];
-    var resp_body = JSON.parse(result[1]);
+            var uuid = req ? req.uuid : 'uuid-undefined';
 
-    return [doc, resp_body];
+            var result = updates.add_sms(null, {
+                uuid: uuid,
+                method: "POST",
+                query: {},
+                headers: helpers.headers("url", querystring.stringify(data)),
+                body: querystring.stringify(data),
+                form: data
+            });
 
-};
-var clinic = {
-    "_id": "4a6399c98ff78ac7da33b639ed60f458",
-    "_rev": "1-0b8990a46b81aa4c5d08c4518add3786",
-    "type": "clinic",
-    "name": "Example clinic 1",
-    "contact": {
-        "name": "Sam Jones",
-        "phone": "+13125551212"
+            var doc = result[0];
+            var resp_body = JSON.parse(result[1]);
+
+            return [doc, resp_body];
+         }
     },
-    "parent": {
-        "type": "health_center",
-        "contact": {
-            "name": "Neal Young",
-            "phone": "+17085551212"
+
+    lists: {
+        data_record: function(viewdata, req) {
+
+            var form = req ? req.query.form : 'PSMS';
+
+            var resp = fakerequest.list(lists.data_record, viewdata, {
+                method: "POST",
+                query: {form: form},
+                headers: helpers.headers('json', JSON.stringify(req.body)),
+                body: JSON.stringify(req.body),
+                form: {}
+            });
+            return resp;
         },
-        "parent": {
-            "type": "district_hospital",
-            "contact": {
-                "name": "Bernie Mac",
-                "phone": "+14155551212"
-            }
+        data_record_merge: function (viewdata, req) {
+
+            var form = req ? req.query.form : 'PSMS';
+
+            var resp = fakerequest.list(lists.data_record_merge, viewdata, {
+                method: "POST",
+                query: {form: form},
+                headers: helpers.headers('json', JSON.stringify(req.body)),
+                body: JSON.stringify(req.body),
+                form: {}
+            });
+
+            return resp;
         }
     }
 };
 
-
-/*
- * STEP 1:
- *
- * Run add_sms and expect a callback to add a clinic to a data record which
- * contains all the information from the SMS.
- **/
-exports.start_psms = function (test) {
-
-    text.expect(16);
-
-    var sms_message = {
-       _id: '14dc3a5aa6',
-       from: "+13125551212",
-       message: '1!PSMS!facility#2011#11#1#2#3#4#5#6#9#8#7#6#5#4',
-       sent_timestamp: "1-19-12 18:45",
-       sent_to: "+15551212",
-       type: "sms_message",
-       locale: "en",
-       form: "PSMS"
-    };
-
-    var data = {
-        from: '+13125551212',
-        message: '1!PSMS!facility#2011#11#1#2#3#4#5#6#9#8#7#6#5#4',
-        sent_timestamp: '1-19-12 18:45',
-        sent_to: '+15551212'
-    };
-
-    var form_data = {
+var example = {
+    clinic: {
+        "_id": "4a6399c98ff78ac7da33b639ed60f458",
+        "_rev": "1-0b8990a46b81aa4c5d08c4518add3786",
+        "type": "clinic",
+        "name": "Example clinic 1",
+        "contact": {
+            "name": "Sam Jones",
+            "phone": "+13125551212"
+        },
+        "parent": {
+            "type": "health_center",
+            "contact": {
+                "name": "Neal Young",
+                "phone": "+17085551212"
+            },
+            "parent": {
+                "type": "district_hospital",
+                "contact": {
+                    "name": "Bernie Mac",
+                    "phone": "+14155551212"
+                }
+            }
+        }
+    },
+    form_data: {
         days_stocked_out: {
             cotrimoxazole: [7, "Cotrimoxazole: Days stocked out"],
             eye_ointment: [4, "Eye Ointment: Days stocked out"],
@@ -99,30 +125,56 @@ exports.start_psms = function (test) {
             zinc: [4, "Zinc: Dispensed total"]
         },
         year: ['2011', 'Report Year']
-    };
-
-    var days_stocked_out = {
+    },
+    days_stocked_out: {
         cotrimoxazole: 7,
         eye_ointment: 4,
         la_6x1: 9,
         la_6x2: 8,
         ors: 5,
         zinc: 6
-    };
-
-    var quantity_dispensed = {
+    },
+    quantity_dispensed: {
         cotrimoxazole: 3,
         eye_ointment: 6,
         la_6x1: 1,
         la_6x2: 2,
         ors: 5,
         zinc: 4
+    }
+};
+
+
+/*
+ * STEP 1:
+ *
+ * Run add_sms and expect a callback to add a clinic to a data record which
+ * contains all the information from the SMS.
+ **/
+exports.step1 = function (test) {
+
+    text.expect(16);
+
+    var data = {
+        from: '+13125551212',
+        message: '1!PSMS!facility#2011#11#1#2#3#4#5#6#9#8#7#6#5#4',
+        sent_timestamp: '1-19-12 18:45',
+        sent_to: '+15551212'
+    };
+
+    var sms_message = {
+       _id: '14dc3a5aa6',
+       from: "+13125551212",
+       message: '1!PSMS!facility#2011#11#1#2#3#4#5#6#9#8#7#6#5#4',
+       sent_timestamp: "1-19-12 18:45",
+       sent_to: "+15551212",
+       type: "sms_message",
+       locale: "en",
+       form: "PSMS"
     };
 
     var expected = {
-        payload: {
-
-        },
+        payload: {},
         callback: {
             options: {
                 path: baseURL + "/PSMS/data_record/add/clinic/%2B13125551212"
@@ -130,7 +182,7 @@ exports.start_psms = function (test) {
             data: {
                 type: "data_record",
                 form: "PSMS",
-                form_data: form_data,
+                form_data: example.form_data,
                 related_entities: {
                     clinic: null
                 },
@@ -138,15 +190,16 @@ exports.start_psms = function (test) {
                 from: "+13125551212",
                 errors: [],
                 tasks: [],
-                days_stocked_out: days_stocked_out,
-                quantity_dispensed: quantity_dispensed,
+                days_stocked_out: example.days_stocked_out,
+                quantity_dispensed: example.quantity_dispensed,
                 month: '11',
                 year: '2011'
             }
         }
     };
 
-    var resp = add_sms(data, sms_message);
+
+    var resp = fake.updates.add_sms(data, {uuid: '14dc3a5aa6'});
 
     test.same(resp[0], sms_message);
 
@@ -158,7 +211,7 @@ exports.start_psms = function (test) {
         resp.callback.data,
         expected.callback.data);
 
-    step2_lists_data_record(test, resp.body);
+    step2(test, {body: resp.body});
 
 };
 
@@ -168,7 +221,7 @@ exports.start_psms = function (test) {
 // Run data_record/add/clinic and expect a callback to
 // check if the same data record already exists.
 //
-var step2_lists_data_record = function(test, req_body) {
+var step2 = function(test, req) {
 
     var viewdata = {rows: [
         {
@@ -177,23 +230,20 @@ var step2_lists_data_record = function(test, req_body) {
         }
     ]};
 
-    var resp = fakerequest.list(lists.data_record, viewdata, {
-        method: "POST",
-        query: {form: req_body.form},
-        headers: helpers.headers('json', JSON.stringify(req_body)),
-        body: JSON.stringify(req_body),
-        form: {}
-    });
+    var resp = fake.lists.data_record(viewdata, req);
+
+    var resp_body = JSON.parse(resp.body);
 
     test.same(
-        resp.callback.options.path,
+        resp_body.callback.options.path,
         baseURL + "/PSMS/data_record/merge/2011/11/" + clinic._id);
 
     test.same(
-        resp.callback.data.related_entities,
-        {clinic: clinic});
+        resp_body.callback.data.related_entities,
+        {clinic: example.clinic});
 
-    lists_data_record_merge(test, resp.body);
+    step3_1(test, {body: resp.body});
+    step3_2(test, {body: resp.body});
 
 };
 
@@ -208,7 +258,7 @@ var step2_lists_data_record = function(test, req_body) {
  * @param {Object} callback - Callback object used to form the next request
  * @api private
  */
-var lists_data_record_merge = function(test, callback) {
+var step3_1 = function(test, req) {
 
     var viewdata = {rows: [
         {
@@ -220,26 +270,21 @@ var lists_data_record_merge = function(test, callback) {
         }
     ]};
 
-    var resp = fakerequest.list(lists.data_record_merge, viewdata, {
-        method: "POST",
-        query: {form: "PSMS"},
-        headers: helpers.headers('json', JSON.stringify(callback.data)),
-        body: JSON.stringify(callback.data),
-        form: {}
-    });
+    var resp = fake.lists.data_record_merge(viewdata, req);
 
     var resp_body = JSON.parse(resp.body);
 
-    test.same(resp_body.callback.options,
-              expected_response.callback.options);
+    test.same(
+        resp_body.callback.options,
+        expected_response.callback.options);
 
-    test.same(resp_body.callback.data,
-              expected_response.callback.data);
+    test.same(
+        resp_body.callback.data,
+        expected_response.callback.data);
 
-    var expected_changes = function(resp_body) {
-
+    test.same(
         resp_body.callback.options.path =
-            appdb + "/777399c98ff78ac7da33b639ed60f422";
+        appdb + "/777399c98ff78ac7da33b639ed60f422";
 
         resp_body.callback.options.method = "POST";
 
