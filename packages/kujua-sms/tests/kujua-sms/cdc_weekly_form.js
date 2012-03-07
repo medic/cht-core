2558ad293f81ef4bc2df79f32aab88592172fc52
endor <fp@notjusthosting.com>
2012-03-07 16:50:04 +0100

    add test for parsing cdc weekly reports; towards #46

---
+++ packages/kujua-sms/tests/kujua-sms/cdc_weekly_form.js
@@ -0,0 +1,33 @@
+var smsparser = require('views/lib/smsparser'),
+    smsforms = require('views/lib/smsforms');
+
+exports.cdc_weekly_example_data = function (test) {
+
+    var def = smsforms['cdc_weekly'];
+    var doc = {
+        sent_timestamp: '2-1-12 15:35',
+        from: '+13125551212',
+        message: 'SUR WKN2# WKS 3# AFP 99# NNT 0# MSL 5# AES01'
+    };
+
+    test.expect(2);
+
+    var obj = smsparser.parse(def, doc);
+    var expectedObj = {
+        wkn: 2,
+        wks: 3,
+        afp: 99,
+        nnt: 0,
+        msl: 5,
+        aes: 1
+    };
+
+    test.same(obj, expectedObj);
+
+    var arr = smsparser.parseArray(def, doc);
+    var expectedArr = [2, 3, 99, 0, 5, 1]
+
+    test.same(arr, expectedArr);
+
+    test.done();
+};
