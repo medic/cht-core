6ae80fb370eb2491dece8ab61c8ebdb40672ffa5
Milan Andric <mandric@gmail.com>
2012-03-29 14:10:20 -0500

    beginnings of updatePhone field in records display.

--- packages/kujua-sms/kujua-sms/views.js
+++ packages/kujua-sms/kujua-sms/views.js
@@ -76,11 +76,33 @@ exports.facility_by_phone = {
 };
 
 /*
+ * Get phone numbers for health centers based on clinic docs
+ */
+exports.phones_by_district_and_health_center = {
+    map: function (doc) {
+        if (doc.type === 'clinic') {
+            if(doc.parent && doc.parent.parent && doc.parent.parent.contact) {
+                if(doc.parent && doc.parent.contact.phone) {
+                    emit([
+                        doc.parent.parent.name,
+                        doc.parent.name,
+                        doc.parent.contact.phone],
+                        null);
+                }
+            }
+        }
+    },
+    reduce: function(key, doc) {
+        return true;
+    }
+};
+
+/*
  * Get clinic based on phone number
  */
 exports.clinic_by_phone = {
     map: function (doc) {
-        if (doc.type === 'clinic') {
+        if (doc.type === 'clinic' && doc.contact) {
             emit([doc.contact.phone], doc);
         }
     }
