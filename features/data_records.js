42fc254af1b0026bb95dff88d78e2f76af3ef2b6
endor <fp@notjusthosting.com>
2012-04-26 12:44:41 +0200

    use a more readable/bdd-style syntax for the functional tests and make them exit correctly

--- features/data_records.js
+++ features/data_records.js
@@ -1,33 +1,47 @@
 var utils = require("./utils")
-  , username = "root"
-  , password = "password"
   , assert = require("assert")
-  , Browser = require("zombie");
+  , Browser = require("zombie")
+  , vows = require("vows")
+  , Feature = require("vows-bdd/lib").Feature
+  , username = "root"
+  , password = "password";
 
-process.on('exit', function () {
-    utils.removeTestDB(username, password);
-});
-  
-utils.createTestDB(username, password, function() {
-    utils.createDataRecord(username, password, function() {
+Feature("Data Records", module)
+    .scenario("Displaying data records")
+    .given("a test DB", function() {
+        utils.createTestDB(username, password, this.callback);
+    })
+    .and("a data record in the test DB", function() {
+        utils.createDataRecord(username, password, this.callback);
+    })
+    .when("I go to the data records page", function() {
         var browser = new Browser({ site: "http://localhost:5984" });
-        
-        browser.visit("/kujua-export-test/_design/kujua-export/_rewrite/data_records", function () {
-            utils.login(browser, username, password, function() {
-                browser.clickLink(".records a", function() {
-                    browser.clickLink(".docs a", function() {
-                        browser.clickLink(".records a", function() {
-                            browser.clickLink(".extend", function() {
-                                assert.equal(
-                                    undefined, browser.query(".extended.hide"),
-                                    "SMS info should be extended"
-                                );
-                                process.exit(0);
-                            });
-                        });
-                    });                
-                });
-            });
+        browser.visit("/kujua-export-test/_design/kujua-export/_rewrite/data_records", this.callback);
+    })
+    .and("I log in", function(browser) {
+        utils.login(browser, username, password, this.callback);
+    })
+    .and("I go to the data records page", function(browser) {
+        browser.clickLink(".records a", this.callback);
+    })
+    .and("I go to the docs page", function(browser) {
+        browser.clickLink(".docs a", this.callback);
+    })
+    .and("I go to the data records page", function(browser) {
+        browser.clickLink(".records a", this.callback);
+    })
+    .and("I extend the SMS information of the data record", function(browser) {
+        browser.clickLink(".extend", this.callback);
+    })
+    .then("the SMS information should be extended", function(err, browser) {
+        assert.equal(
+            undefined, browser.query(".extended.hide"),
+            "SMS info should be extended"
+        );
+    })
+    .complete(function() {
+        utils.removeTestDB(username, password, function() {
+            process.exit();            
         });
-    });
-});
+    })
+    .finish(module);
