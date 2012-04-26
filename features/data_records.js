abb3e921d1caef7dd10ad8dd5b0edbff1db495c5
endor <fp@notjusthosting.com>
2012-04-26 16:30:32 +0200

    add second test; wii

--- features/data_records.js
+++ features/data_records.js
@@ -1,6 +1,6 @@
 var utils = require("./utils")
+  , callback = null
   , assert = require("assert")
-  , Browser = require("zombie")
   , vows = require("vows")
   , Feature = require("vows-bdd/lib").Feature
   , username = "root"
@@ -8,20 +8,13 @@ var utils = require("./utils")
 
 Feature("Data Records", module)
     .scenario("Displaying data records")
-    .given("a test DB", function() {
-        utils.createTestDB(username, password, this.callback);
+    .given("I am logged in", function() {
+        utils.loggedIn(username, password, this.callback);
     })
-    .and("a data record in the test DB", function() {
-        utils.createDataRecord(username, password, this.callback);
+    .and("a data record in the test DB", function(browser) {
+        utils.createDataRecords(browser, username, password, this.callback);
     })
-    .when("I go to the data records page", function() {
-        var browser = new Browser({ site: "http://localhost:5984" });
-        browser.visit("/kujua-export-test/_design/kujua-export/_rewrite/data_records", this.callback);
-    })
-    .and("I log in", function(browser) {
-        utils.login(browser, username, password, this.callback);
-    })
-    .and("I go to the data records page", function(browser) {
+    .when("I go to the data records page", function(browser) {
         browser.clickLink(".records a", this.callback);
     })
     .and("I go to the docs page", function(browser) {
@@ -31,12 +24,49 @@ Feature("Data Records", module)
         browser.clickLink(".records a", this.callback);
     })
     .and("I extend the SMS information of the data record", function(browser) {
-        browser.clickLink(".extend", this.callback);
+        browser.clickLink(".extend:first", this.callback);
     })
     .then("the SMS information should be extended", function(err, browser) {
-        assert.equal(
-            undefined, browser.query(".extended.hide"),
+        var $ = browser.evaluate("window.jQuery");
+        assert.ok(
+            !$(".extended:first").hasClass('hide'),
             "SMS info should be extended"
+        );
+    })
+    .complete()
+    
+    .scenario("Filter data records")
+    .given("I am logged in", function() {
+        utils.loggedIn(username, password, this.callback);
+    })
+    .and("a PSMS and a MSBR data record the test DB", function(browser) {
+        utils.createDataRecords(browser, username, password, this.callback);
+    })
+    .when("I go to the data records page", function(browser) {
+        browser.clickLink(".records a", this.callback);
+    })
+    .and("I click on the form selector", function(browser) {
+        console.log('1')
+        browser.clickLink("Form", this.callback);
+    })
+    .and("I wait", function(browser) {
+        console.log('2')
+        browser.wait(3000, this.callback);
+    })
+    .and("I select MSBR as form", function(browser) {
+        console.log('3')
+        browser.clickLink("MSBR", this.callback);
+    })
+    .and("I wait", function(browser) {
+        console.log('4')
+        browser.wait(3000, this.callback);
+    })
+    .then("I should just see the MSBR data record but not the PSMS data record", function(err, browser) {
+        console.log('5')
+        var $ = browser.evaluate("window.jQuery");
+        assert.equal(
+            "Neal Young", $.trim($("td.contact-name").text()),
+            "PSMS data record with Neal Young as contact should be visible"
         );
     })
     .complete(function() {
--- features/utils.js
+++ features/utils.js
@@ -1,5 +1,7 @@
-var util = require('util');
-var exec = require('child_process').exec;
+var util = require('util')
+  , exec = require('child_process').exec
+  , browser = null
+  , Browser = require("zombie");
 
 exports.createTestDB = function(user, password, callback) {
     var url = "http://" + user + ":" + password + "@localhost:5984/kujua-export-test";
@@ -17,19 +19,37 @@ exports.removeTestDB = function(user, password, callback) {
     });
 };
 
-exports.createDataRecord = function(user, password, callback) {
+exports.createDataRecords = function(browser, user, password, callback) {
     var url = "http://" + user + ":" + password + "@localhost:5984/kujua-export-test";
-    exec("kanso upload features/valid_data_record.json " + url, function(error, stdout, stderr) {
+    exec("kanso upload features/valid_data_records.json " + url, function(error, stdout, stderr) {
         if (error) { util.puts(stderr); }
-        if (callback) { callback(); }    
+        if (callback) { callback(null, browser); }
     });
 };
 
-exports.login = function(browser, username, password, callback) {
-    browser.clickLink(".login", function() {
-        browser.
+exports.login = function(_browser, username, password, callback) {
+    _browser.clickLink(".login", function() {
+        _browser.
             fill("username", username).
             fill("password", password).
             clickLink(".modal-footer .btn.btn-primary", callback);
     });
+};
+
+exports.loggedIn = function(username, password, callback) {
+    if(browser) {
+        var _browser = browser.fork();
+        _browser.visit("/kujua-export-test/_design/kujua-export/_rewrite/data_records", callback);
+    } else {
+        exports.createTestDB(username, password, function() {
+            var _browser = new Browser({ site: "http://localhost:5984" });
+            _browser.visit("/kujua-export-test/_design/kujua-export/_rewrite/data_records", function(err, _browser) {
+                exports.login(_browser, username, password, function(err, _browser) {
+                    browser = _browser;
+                    _browser = browser.fork();
+                    _browser.visit("/kujua-export-test/_design/kujua-export/_rewrite/data_records", callback);
+                });
+            });
+        });        
+    }
 };
