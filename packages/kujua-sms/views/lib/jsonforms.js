aec4459473c22616bbf28c041b1d700aa9fd8bc1
Milan Andric <mandric@gmail.com>
2012-07-10 19:32:28 -0500

    added tiny labels (textforms support) to TEST form.

--- packages/kujua-sms/views/lib/jsonforms.js
+++ packages/kujua-sms/views/lib/jsonforms.js
@@ -6,18 +6,27 @@ exports['TEST'] = {
     meta: {code: "TEST", label: 'Test Monthly Report'},
     fields: {
         facility_id: {
-            labels: 'Health Facility Identifier',
+            labels: {
+                short: 'Health Facility Identifier',
+                tiny: 'HFI'
+            },
             type: 'string',
             required: true
         },
          year: {
-             labels: 'Report Year',
+             labels: {
+                 short: 'Report Year',
+                 tiny: 'RPY'
+             },
              type: 'integer',
              validate: {is_numeric_year: true},
              required: true
          },
          month: {
-             labels: 'Report Month',
+             labels: {
+                 short: 'Report Month',
+                 tiny: 'RPM'
+             },
              type: 'integer',
              validate: {is_numeric_month: true},
              list: [
@@ -36,46 +45,105 @@ exports['TEST'] = {
              ]
          },
          misoprostol_administered: {
-            type: "boolean",
+            type: 'boolean',
             labels: {
                short: {
-                  en: "Misoprostol?"
+                  en: 'Misoprostol?'
+               },
+               tiny: {
+                  en: 'MSP'
                },
                description: {
-                  en: "Was misoprostol administered?"
+                  en: 'Was misoprostol administered?'
                }
             }
          },
          'quantity_dispensed.la_6x1': {
-             labels: 'LA 6x1: Dispensed total', type: 'integer'},
+             labels: {
+                 short: 'LA 6x1: Dispensed total',
+                 tiny: 'L1T'
+             },
+             type: 'integer'
+         },
          'quantity_dispensed.la_6x2': {
-             labels: 'LA 6x2: Dispensed total', type: 'integer'},
+             labels: {
+                 short: 'LA 6x2: Dispensed total',
+                 tiny: 'L2T'
+             },
+             type: 'integer'
+         },
          'quantity_dispensed.cotrimoxazole': {
-             labels: 'Cotrimoxazole: Dispensed total', type: 'integer'},
+             labels: {
+                 short: 'Cotrimoxazole: Dispensed total',
+                 tiny: 'CDT'
+             },
+             type: 'integer'
+         },
          'quantity_dispensed.zinc': {
-             labels: 'Zinc: Dispensed total', type: 'integer'},
+             labels: {
+                 short: 'Zinc: Dispensed total',
+                 tiny: 'ZDT'
+             },
+             type: 'integer'
+         },
          'quantity_dispensed.ors': {
-             labels: 'ORS: Dispensed total', type: 'integer'},
+             labels: {
+                 short: 'ORS: Dispensed total',
+                 tiny: 'ODT'
+             },
+             type: 'integer'
+         },
          'quantity_dispensed.eye_ointment': {
-             labels: 'Eye Ointment: Dispensed total', type: 'integer'},
+             labels: {
+                 short: 'Eye Ointment: Dispensed total',
+                 tiny: 'EOT'
+             },
+             type: 'integer'
+         },
          'days_stocked_out.la_6x1': {
-             labels: 'LA 6x1: Days stocked out', type: 'integer'},
+             labels: {
+                 short: 'LA 6x1: Days stocked out',
+                 tiny: 'L1O'
+             },
+             type: 'integer'
+         },
          'days_stocked_out.la_6x2': {
-             labels: 'LA 6x2: Days stocked out', type: 'integer'},
+             labels: {
+                 short: 'LA 6x2: Days stocked out',
+                 tiny: 'L2O'
+             },
+             type: 'integer'},
          'days_stocked_out.cotrimoxazole': {
-             labels: 'Cotrimoxazole: Days stocked out', type: 'integer'},
+             labels: {
+                 short: 'Cotrimoxazole: Days stocked out',
+                 tiny: 'CDO'
+             },
+             type: 'integer'},
          'days_stocked_out.zinc': {
-             labels: 'Zinc: Days stocked out', type: 'integer'},
+             labels: {
+                 short: 'Zinc: Days stocked out',
+                 tiny: 'ZDO'
+             },
+             type: 'integer'},
          'days_stocked_out.ors': {
-             labels: 'ORS: Days stocked out', type: 'integer'},
+             labels: {
+                 short: 'ORS: Days stocked out',
+                 tiny: 'ODO'
+             },
+             type: 'integer'},
          'days_stocked_out.eye_ointment': {
-             labels: 'Eye Ointment: Days stocked out', type: 'integer'}
+             labels: {
+                 short: 'Eye Ointment: Days stocked out',
+                 tiny: 'EDO'
+             },
+             type: 'integer'}
     },
     autoreply: "Zikomo!",
     data_record_merge: "/:form/data_record/merge/:year/:month/:clinic_id",
     examples: {
          messages: {
-             muvuku: '1!TEST!facility#2012#4#222#333#444#555#666#777#888#999#111#222#333#444'
+             muvuku: '1!TEST!facility#2012#4#1#222#333#444#555#666#777#888#999#111#222#333#444',
+             textforms: 'TEST HFI facility#RPY 2012#RPM 4#MSP 1#L1T 222# L2T 333#CDT 444#ZDT 555#ODT 666#EOT 777#L10 888#L20 999#CDO 111#ZDO 222#ODO 333#EDO 444'
          }
      }
 };
