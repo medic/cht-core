b34d683f83216dea891bf0a454cd7f7b12de032c
Milan Andric <mandric@gmail.com>
2012-08-27 13:02:40 -0500

    update config.js with cdc-nepal string vals.

--- config.js
+++ config.js
@@ -2,8 +2,12 @@
   "_id": "config.js",
   "values": [
     { "key": "District", "value": "Field Office" },
+    { "key": "Districts", "value": "Field Offices" },
     { "key": "District Hospital", "value": "Field Office" },
+    { "key": "District Hospitals", "value": "Field Offices" },
     { "key": "Health Center", "value": "District" },
-    { "key": "Clinic", "value": "Reporting Unit" }
+    { "key": "Health Centers", "value": "Districts" },
+    { "key": "Clinic", "value": "Reporting Unit" },
+    { "key": "Clinics", "value": "Reporting Units" }
   ]
 }
