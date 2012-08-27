a0c5858900998f13a29814b8d1940c2d6266b6f8
Milan Andric <mandric@gmail.com>
2012-08-27 17:37:05 -0500

    added config.js example and updated readme.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Clone the repo as usual then do `git submodule init && git submodule update` to
 get a copy of `json-forms`.
 
+
+Then optionally configure the app with:
+
+```
+curl -X POST http://localhost:5984/yourdb -d @config.js -H "Content-Type: application/json"
+```
---
+++ config.js
@@ -0,0 +1,11 @@
+{
+  "_id": "config.js",
+  "values": [
+    { "key": "District Hospital", "value": "District" },
+    { "key": "District Hospitals", "value": "Districts" },
+    { "key": "Health Center", "value": "VDC" },
+    { "key": "Health Centers", "value": "VDCs" },
+    { "key": "Clinic", "value": "CHW" },
+    { "key": "Clinics", "value": "CHWs" }
+  ]
+}
