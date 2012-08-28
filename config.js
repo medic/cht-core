f65bfc932e996e5b59043def7a5637e5c3499c4d
Milan Andric <mandric@gmail.com>
2012-08-28 10:07:51 -0500

    minor config.js change.

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 {
   "_id": "config.js",
+  "_rev":"3-815a7c727d2834c304b6eb39f2baf848",
   "values": [
     { "key": "District Hospital", "value": "District" },
     { "key": "District Hospitals", "value": "Districts" },
-    { "key": "Health Center", "value": "VDC" },
-    { "key": "Health Centers", "value": "VDCs" },
-    { "key": "Clinic", "value": "CHW" },
-    { "key": "Clinics", "value": "CHWs" }
+    { "key": "Health Center", "value": "CHWL" },
+    { "key": "Health Centers", "value": "CHWLs" },
+    { "key": "Clinic", "value": "FCHV" },
+    { "key": "Clinics", "value": "FCHVs" }
   ]
 }
