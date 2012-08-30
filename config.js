9d7fd4760b08850c352144ddb0607f62392d0c15
Milan Andric <mandric@gmail.com>
2012-08-30 06:34:12 -0500

    added value for Clinic Name to config.js.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 {
   "_id": "config.js",
-  "_rev":"2-572c4070f0ab998cda9578cb34e25c70",
+  "_rev":"8-15defa20983e1affaaaa5ac0bd99223a",
   "values": [
     { "key": "District Hospital", "value": "District" },
     { "key": "District Hospitals", "value": "Districts" },
@@ -12,6 +12,7 @@
     { "key": "Health Center Contact Phone", "value": "Contact Phone" },
     { "key": "Clinic", "value": "CHW" },
     { "key": "Clinics", "value": "CHWs" },
+    { "key": "Clinic Name", "value": "Village Name" },
     { "key": "Clinic Contact Name", "value": "CHW Name" },
     { "key": "Clinic Contact Phone", "value": "Contact Phone" },
     { "key": "RC Code", "value": "Ward #" }
