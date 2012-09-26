e0c5142291082f3f36b1543e01f98847380ec85a
Milan Andric <mandric@gmail.com>
2012-09-26 15:55:10 -0500

    fixed district filtering on analytics dropdown.

--- packages/kujua-reporting/kujua-reporting/events.js
+++ packages/kujua-reporting/kujua-reporting/events.js
@@ -1,52 +1,71 @@
 var duality_events = require('duality/events'),
     templates = require('duality/templates'),
-    dutils = require('duality/utils'),
+    session = require('session'),
+    utils = require('kujua-utils'),
+    users = require('users'),
     settings = require('settings/root'),
     ddoc = settings.name;
 
 duality_events.on('init', function (ev) {
 
     var db = require('db').current(),
-        setup = $.kansoconfig('kujua-reporting', true),
-        view = 'facilities_by_type',
+        charts = require('./ui/charts'),
         q = {
             startkey: ['district_hospital'],
             endkey: ['district_hospital', {}],
             group: true
         };
 
-    // we have no forms in the config, nothing to do.
-    if (!setup || !setup.forms || setup.forms.length === 0)
-        return;
+    db.getView(ddoc, 'facilities_by_type', q, renderNav);
+    charts.initPieChart();
 
-    //
-    // render analtyics top nav, relies on kansoconfig `kujua-reporting.forms`
-    // config.
-    //
-    db.getView(ddoc, view, q, function(err, data) {
+});
 
-        var req = {},
-            charts = require('./ui/charts'),
-            forms = [];
+/**
+ * render analtyics top nav, relies on kansoconfig `kujua-reporting.forms`
+ * definition.
+ */
+var renderNav = function(err, data) {
+
+    session.info(function (err, info) {
 
         if (err) {
             return alert('Create facility data to use analytics.');
         }
 
-        // associate each form from config.js with a list of facilities
-        setup.forms.forEach(function(form) {
-            var f = {code: form.code, districts: []};
-            data.rows.forEach(function(row) {
-                f.districts.push({id:row.key[1], name:row.key[2]});
-            });
-            forms.push(f);
-        });
+        var isAdmin = utils.hasPerm(info.userCtx, 'can_edit_any_facility'),
+            isDistrictAdmin = utils.hasPerm(info.userCtx, 'can_edit_facility'),
+            setup = $.kansoconfig('kujua-reporting', true);
 
-        $('#topnav .nav .records').after(
-            templates.render('kujua-reporting/top_nav.html', req, {forms:forms})
-        );
-        charts.initPieChart();
+        // we have no forms in the config, nothing to do.
+        if (!setup || !setup.forms || setup.forms.length === 0)
+            return;
+
+        // only admin or district admin should have access
+        if (!isAdmin && !isDistrictAdmin)
+            return;
+
+        users.get(info.userCtx.name, function(err, user) {
+            var district = user.kujua_facility,
+                forms = [];
+
+            // associate each form from config.js with a list of facilities
+            setup.forms.forEach(function(form) {
+                var f = {code: form.code, districts: []};
+                data.rows.forEach(function(row) {
+                    if (isAdmin)
+                        f.districts.push({id:row.key[1], name:row.key[2]});
+                    else if (isDistrictAdmin && row.key[1] === district)
+                        f.districts.push({id:row.key[1], name:row.key[2]});
+                });
+                forms.push(f);
+            });
+
+            $('#topnav .nav .records').after(
+                templates.render('kujua-reporting/top_nav.html', {}, {forms:forms})
+            );
+        });
 
     });
 
-});
+};
