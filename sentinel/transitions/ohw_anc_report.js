a63057598eca856fb7eaa7980f9be2c52ff7272d
Rowan Crawford <wombleton@gmail.com>
2012-12-05 15:28:22 +1300

    Convert ANC/PNC reports

---
+++ sentinel/transitions/ohw_anc_report.js
@@ -0,0 +1,51 @@
+var config = require('../config'),
+    i18n = require('../i18n'),
+    moment = require('moment'),
+    utils = require('../lib/utils'),
+    db;
+
+module.exports = {
+    form: 'OANC',
+    requiredFields: 'related_entities.clinic',
+    onMatch: function(change, callback) {
+        var clinicName,
+            clinicPhone,
+            doc = change.doc;
+
+        db = db || require('../db');
+
+        clinicPhone = utils.getClinicPhone(doc);
+        clinicName = utils.getClinicName(doc);
+        utils.getOHWRegistration(doc.patient_id, function(err, registration) {
+            var changed,
+                horizon;
+
+            if (err) {
+                callback(err);
+            } else {
+                if (registration) {
+                    utils.addMessage(doc, clinicPhone, i18n("Thank you, {{clinic_name}}. ANC counseling visit for {{patient_name}} has been recorded.", {
+                        clinic_name: clinicName,
+                        patient_name: patientName
+                    }));
+                    horizon = moment(date.getDate()).add('days', config.get('ohw_obsolete_anc_reminders_days'));
+                    changed = utils.obsoleteScheduledMessages(registration, 'anc_visit', horizon.valueOf());
+                    if (changed) {
+                        db.saveDoc(registration, function(err) {
+                            callback(err, true);
+                        });
+                    } else {
+                        callback(null, true);
+                    }
+                } else if (clinicPhone) {
+                    utils.addMessage(doc, clinicPhone, i18n("No patient with id '{{patient_id}}' found.", {
+                        patient_id: doc.patient_id
+                    }));
+                    callback(null, true);
+                } else {
+                    callback(null, false);
+                }
+            }
+        });
+    }
+};
---
+++ sentinel/transitions/ohw_pnc_report.js
@@ -0,0 +1,53 @@
+var _ = require('underscore'),
+    utils = require('../lib/utils'),
+    db;
+
+module.exports = {
+    form: 'OPNC',
+    requiredFields: 'related_entities.clinic',
+    onMatch: function(change, callback) {
+        var clinicName,
+            clinicPhone,
+            doc = change.doc;
+
+        db = db || require('../db');
+
+        clinicPhone = utils.getClinicPhone(doc);
+        clinicName = utils.getClinicName(doc);
+        utils.getOHWRegistration(doc.patient_id, function(err, registration) {
+            var previousWeight;
+
+            if (err) {
+                callback(err);
+            } else {
+                if (registration) {
+                    previousWeight = registration.childWeight || registration.child_birth_weight;
+                    if (previousWeight === 'Normal' && doc.child_weight !== 'Normal') {
+                        utils.addMessage(doc, clinicPhone, i18n("Thank you, {{clinic_name}}. This child is low birth weight. " +
+                           "provide extra thermal protection for baby, feed the baby every two hours, visit the family " +
+                           "every day to check the baby for the first week, watch for signs of breathing difficulty. " +
+                           "Refer danger signs immediately to health facility.", {
+                                clinic_name: clinicName
+                        }));
+                    } else {
+                        utils.addMessage(doc, clinicPhone, i18n("Thank you, {{clinic_name}}. PNC counseling visit has been recorded for {{patient_name}}.", {
+                            clinic_name: clinicName,
+                            patient_name: doc.patient_name
+                        }));
+                    }
+                    registration.child_weight = doc.child_weight;
+                    db.saveDoc(registration, function(err) {
+                        callback(err, true);
+                    });
+                } else if (clinicPhone) {
+                    utils.addMessage(doc, clinicPhone, i18n("No patient with id '{{patient_id}}' found.", {
+                        patient_id: doc.patient_id
+                    }));
+                    callback(null, true);
+                } else {
+                    callback(null, false);
+                }
+            }
+        });
+    }
+};
