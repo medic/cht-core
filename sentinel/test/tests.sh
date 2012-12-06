300dac636fd6ac30918bb4470249e25976a45d6c
Rowan Crawford <wombleton@gmail.com>
2012-12-07 12:53:54 +1300

    nodeunit infrastructure, test design script

---
+++ sentinel/test/tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export SENTINEL_TEST=true
+
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+#NODEUNIT="node $DIR/../node_modules/nodeunit/bin/nodeunit --reporter=minimal "
+NODEUNIT="node --debug-brk $DIR/../node_modules/nodeunit/bin/nodeunit --reporter=minimal "
+
+cd $DIR
+$NODEUNIT unit
