55717199378bc971baa893cf6cd70875ab3eb1d1
Rowan Crawford <wombleton@gmail.com>
2012-12-07 12:52:04 +1300

    Update design to have all the right filters

--- sentinel/designs/lib/filters.js
+++ sentinel/designs/lib/filters.js
@@ -1,5 +1,52 @@
 exports.update_clinics = function(doc) {
-    var entities = doc.related_entities;
+    return doc.form &&
+        doc.related_entities &&
+        !doc.related_entities.clinic;
+};
 
-    return doc.form && entities && !entities.clinic;
+exports.ohw_anc_report = function(doc) {
+    return doc.form === 'OANC' &&
+        doc.related_entities &&
+        doc.related_entities.clinic;
+};
+
+exports.ohw_pnc_report = function(doc) {
+    return doc.form === 'OPNC' &&
+        doc.related_entities &&
+        doc.related_entities.clinic;
+};
+
+exports.ohw_birth_report = function(doc) {
+    return doc.form === 'OBIR' &&
+        doc.patient_id &&
+        doc.related_entities &&
+        doc.related_entities.clinic;
+};
+
+exports.ohw_danger_sign = function(doc) {
+    return doc.form === 'ODGR' &&
+        doc.patient_id &&
+        doc.related_entities &&
+        doc.related_entities.clinic;
+};
+
+exports.ohw_labor_report = function(doc) {
+    return doc.form === 'OLAB' &&
+        doc.patient_id &&
+        doc.related_entities &&
+        doc.related_entities.clinic;
+};
+
+exports.ohw_notifications = function(doc) {
+    return doc.form === 'ONOT' &&
+        doc.patient_id &&
+        doc.related_entities &&
+        doc.related_entities.clinic;
+};
+
+exports.ohw_registration = function(doc) {
+    return doc.form === 'ORPT' &&
+        doc.related_entities &&
+        doc.related_entities.clinic &&
+        (!doc.patient_identifiers || doc.patient_identifiers.length === 0);
 };
