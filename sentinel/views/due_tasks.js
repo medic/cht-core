9113bfcb59aa3f9186a28b74081a7c8cde4c3a62
Rowan Crawford <wombleton@gmail.com>
2012-12-07 10:31:35 +1300

    Remove redundant "views" dir

--- sentinel/views/due_tasks.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  map: function(doc) {
-    var tasks = doc.scheduled_tasks || [];
-    tasks.forEach(function(task, index) {
-      if (task.due)
-        emit(task.due, index);
-    });
-  }
-};
