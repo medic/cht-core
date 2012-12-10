a04e0c844e0e558d122ff3e58e1c86f184566fc8
Rowan Crawford <wombleton@gmail.com>
2012-12-10 16:22:22 +1300

    Another cut at transition checking

--- sentinel/designs/lib/filters.js
+++ sentinel/designs/lib/filters.js
@@ -5,68 +5,47 @@ exports.update_clinics = function(doc) {
 };
 
 exports.ohw_anc_report = function(doc) {
-    var transitions = require('filters/lib/transition');
-
-    return !transitions.completed(doc, 'ohw_anc_report') &&
-        doc.form === 'OANC' &&
+    return doc.form === 'OANC' &&
         doc.related_entities &&
         doc.related_entities.clinic;
 };
 
 exports.ohw_pnc_report = function(doc) {
-    var transitions = require('filters/lib/transition');
-
-    return !transitions.completed(doc, 'ohw_pnc_report') &&
-        doc.form === 'OPNC' &&
+    return doc.form === 'OPNC' &&
         doc.related_entities &&
         doc.related_entities.clinic;
 };
 
 exports.ohw_birth_report = function(doc) {
-    var transitions = require('filters/lib/transition');
-
-    return !transitions.completed(doc, 'ohw_birth_report') &&
-        doc.form === 'OBIR' &&
+    return doc.form === 'OBIR' &&
         doc.patient_id &&
         doc.related_entities &&
         doc.related_entities.clinic;
 };
 
 exports.ohw_danger_sign = function(doc) {
-    var transitions = require('filters/lib/transition');
-
-    return !transitions.completed(doc, 'ohw_danger_sign') &&
-        doc.form === 'ODGR' &&
+    return doc.form === 'ODGR' &&
         doc.patient_id &&
         doc.related_entities &&
         doc.related_entities.clinic;
 };
 
 exports.ohw_labor_report = function(doc) {
-    var transitions = require('filters/lib/transition');
-
-    return !transitions.completed(doc, 'ohw_labor_report') &&
-        doc.form === 'OLAB' &&
+    return doc.form === 'OLAB' &&
         doc.patient_id &&
         doc.related_entities &&
         doc.related_entities.clinic;
 };
 
 exports.ohw_notifications = function(doc) {
-    var transitions = require('filters/lib/transition');
-
-    return !transitions.completed(doc, 'ohw_notifications') &&
-        doc.form === 'ONOT' &&
+    return doc.form === 'ONOT' &&
         doc.patient_id &&
         doc.related_entities &&
         doc.related_entities.clinic;
 };
 
 exports.ohw_registration = function(doc) {
-    var transitions = require('filters/lib/transition');
-
-    return !transitions.completed(doc, 'ohw_registration') &&
-        doc.form === 'ORPT' &&
+    return doc.form === 'ORPT' &&
         doc.related_entities &&
         doc.related_entities.clinic &&
         (!doc.patient_identifiers || doc.patient_identifiers.length === 0);
