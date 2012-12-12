943dd2e4f27b7d1723a86d34beff192b9f37872c
Rowan Crawford <wombleton@gmail.com>
2012-12-12 14:36:38 +1300

    Convert OHW Birth Report to new workflow; test

---
+++ sentinel/test/unit/ohw_birth_report.js
@@ -0,0 +1,233 @@
+var _ = require('underscore'),
+    moment = require('moment'),
+    transition = require('../../transitions/ohw_birth_report'),
+    fakedb = require('../fake-db'),
+    utils = require('../../lib/utils'),
+    registration,
+    _getOHWRegistration;
+
+exports.setUp = function(callback) {
+    transition.db = fakedb;
+    _getOHWRegistration = utils.getOHWRegistration;
+    utils.getOHWRegistration = function(id, callback) {
+        if (id === 'fake') {
+            registration = false;
+        } else {
+            registration = {
+                scheduled_tasks: [
+                    {
+                        messages: [ { message: 'x' } ],
+                        type: 'upcoming_delivery'
+                    }
+                ]
+            };
+        }
+        callback(null, registration);
+    };
+    callback();
+};
+exports.tearDown = function(callback) {
+    utils.getOHWRegistration = _getOHWRegistration;
+    callback();
+};
+
+exports['responds to invalid patient'] = function(test) {
+    var doc = {
+        patient_id: 'fake',
+        related_entities: {
+            clinic: {
+                contact: {
+                    phone: 'clinic'
+                }
+            }
+        }
+    };
+    transition.onMatch({
+        doc: doc
+    }, function(err, complete) {
+        var task = _.first(doc.tasks),
+            message;
+
+        test.ok(complete);
+
+        test.ok(task);
+        message = _.first(task.messages).message;
+        test.ok(message.indexOf('fake') > 0);
+
+        test.done();
+    });
+};
+
+exports['responds to low weight report with advice'] = function(test) {
+    var doc = {
+        outcome_child: 'Alive and well',
+        birth_weight: 'Red',
+        days_since_delivery: 1,
+        patient_id: 'good',
+        related_entities: {
+            clinic: {
+                contact: {
+                    phone: 'clinic'
+                },
+                name: 'qq',
+                parent: {
+                    contact: {
+                        phone: 'parent'
+                    }
+                }
+            }
+        }
+    };
+    transition.onMatch({
+        doc: doc
+    }, function(err, complete) {
+        var message;
+
+        test.ok(complete);
+
+        test.equal(doc.tasks.length, 1);
+        message = _.first(_.first(doc.tasks).messages).message;
+
+        test.ok(message.indexOf('Thank you, qq.') >= 0);
+        test.ok(message.indexOf('low birth weight') >= 0);
+
+        test.done();
+    });
+};
+
+exports['responds to normal weight report with thanks'] = function(test) {
+    var doc = {
+        outcome_child: 'Alive and well',
+        birth_weight: 'Green',
+        days_since_delivery: 1,
+        patient_id: 'good',
+        related_entities: {
+            clinic: {
+                contact: {
+                    phone: 'clinic'
+                },
+                name: 'qq',
+                parent: {
+                    contact: {
+                        phone: 'parent'
+                    }
+                }
+            }
+        }
+    };
+    transition.onMatch({
+        doc: doc
+    }, function(err, complete) {
+        var message;
+
+        test.ok(complete);
+
+        test.equal(doc.tasks.length, 1);
+        message = _.first(_.first(doc.tasks).messages).message;
+
+        test.equals(message, 'Thank you, qq. Birth report has been recorded.');
+
+        test.done();
+    });
+};
+
+exports['updates registration with weight, birth date'] = function(test) {
+    var doc = {
+        outcome_child: 'Alive and well',
+        birth_weight: 'Green',
+        days_since_delivery: 1,
+        patient_id: 'good',
+        related_entities: {
+            clinic: {
+                contact: {
+                    phone: 'clinic'
+                },
+                name: 'qq',
+                parent: {
+                    contact: {
+                        phone: 'parent'
+                    }
+                }
+            }
+        }
+    };
+    transition.onMatch({
+        doc: doc
+    }, function(err, complete) {
+        var today = moment();
+
+        test.ok(complete);
+
+        test.equal(registration.child_outcome, 'Alive and well');
+        test.equal(registration.child_birth_weight, 'Green');
+        test.equal(registration.child_birth_date, today.startOf('day').subtract('days', 1).valueOf());
+
+        test.done();
+    });
+};
+
+exports['sets two pnc reminders with normal weight'] = function(test) {
+    var doc = {
+        outcome_child: 'Alive and well',
+        birth_weight: 'Green',
+        days_since_delivery: 1,
+        patient_id: 'good',
+        related_entities: {
+            clinic: {
+                contact: {
+                    phone: 'clinic'
+                },
+                name: 'qq',
+                parent: {
+                    contact: {
+                        phone: 'parent'
+                    }
+                }
+            }
+        }
+    };
+    transition.onMatch({
+        doc: doc
+    }, function(err, complete) {
+        debugger;
+        var reminders = utils.filterScheduledMessages(registration, 'pnc_visit');
+
+        test.equals(reminders.length, 2);
+
+        test.done();
+    });
+};
+
+exports['sets four pnc reminders with yellow weight'] = function(test) {
+    var doc = {
+        outcome_child: 'Alive and well',
+        birth_weight: 'Yellow',
+        days_since_delivery: 3,
+        patient_id: 'good',
+        related_entities: {
+            clinic: {
+                contact: {
+                    phone: 'clinic'
+                },
+                name: 'qq',
+                parent: {
+                    contact: {
+                        phone: 'parent'
+                    }
+                }
+            }
+        }
+    };
+    transition.onMatch({
+        doc: doc
+    }, function(err, complete) {
+        debugger;
+        var reminders = utils.filterScheduledMessages(registration, 'pnc_visit');
+
+        test.equals(reminders.length, 4);
+
+        test.done();
+    });
+};
+
+
--- sentinel/transitions/ohw_birth_report.js
+++ sentinel/transitions/ohw_birth_report.js
@@ -41,35 +41,36 @@ module.exports = {
                 });
                 utils.clearScheduledMessages(registration, 'anc_visit', 'miso_reminder', 'upcoming_delivery', 'pnc_visit', 'outcome_request');
 
-                if (registration.child_outcome === 'Deceased') {
-                    utils.addMessage(doc, clinicPhone, i18n("Thank you for your report."));
-                    utils.addMessage(doc, parentPhone, i18n("{{clinic_name}} has reported the child of {{patient_name}} as deceased.", {
-                        clinic_name: clinicName,
-                        patient_name: doc.patient_name
-                    }));
-                } else if (registration.child_birth_weight === 'Normal') {
-                    utils.addMessage(doc, clinicPhone, i18n("Thank you, {{clinic_name}}.", {
-                        clinic_name: clinicName
-                    }));
+                if (registration.child_birth_weight === 'Green') {
+                    utils.addMessage(doc, {
+                        phone: clinicPhone,
+                        message: i18n("Thank you, {{clinicName}}. Birth report has been recorded.", {
+                            clinicName: clinicName
+                        })
+                    });
                     self.scheduleReminders(registration, clinicName, clinicPhone, config.get('ohw_pnc_schedule_days'));
                 } else {
-                    utils.addMessage(doc, clinicPhone, i18n("Thank you, {{clinic_name}}. This child is low birth weight. Provide extra thermal protection for baby, feed the baby every two hours, visit the family every day to check the baby for the first week, watch for signs of breathing difficulty. Refer danger signs immediately to health facility.", {
-                        clinic_name: clinicName
-                    }));
-                    utils.addMessage(doc, clinicPhone, i18n("{{clinic_name}} has reported the child of {{patient_name}} as {{birth_weight}} birth weight.", {
-                        clinic_name: clinicName,
-                        patient_name: doc.patient_name,
-                        birth_weight: registration.child_birth_weight
-                    }));
+                    utils.addMessage(doc, {
+                        phone: clinicPhone,
+                        message: i18n("Thank you, {{clinicName}}. This child is low birth weight. " +
+                                      "Provide extra thermal protection for baby, feed the baby every two hours, " +
+                                      "visit the family every day to check the baby for the first week, watch for " +
+                                      "signs of breathing difficulty. Refer danger signs immediately to health facility.", {
+                            clinicName: clinicName
+                        })
+                    });
                     self.scheduleReminders(registration, clinicName, clinicPhone, config.get('ohw_low_weight_pnc_schedule_days'));
                 }
                 self.db.saveDoc(registration, function(err) {
-                    callback(err);
+                    callback(err, true);
                 });
             } else if (clinicPhone) {
-                utils.addMessage(doc, clinicPhone, i18n("No patient with id '{{patient_id}}' found.", {
-                    patient_id: doc.patient_id
-                }));
+                utils.addMessage(doc, {
+                    phone: clinicPhone,
+                    message: i18n("No patient with id '{{patient_id}}' found.", {
+                        patient_id: doc.patient_id
+                    })
+                });
                 callback(null, true);
             } else {
                 callback(null, false);
@@ -86,9 +87,9 @@ module.exports = {
             if (marker > now) {
                 utils.addScheduledMessage(doc, {
                     due: marker.valueOf(),
-                    message: i18n("Greetings, {{clinic_name}}. {{patient_name}} is due for a PNC visit today.", {
-                        clinic_name: clinicName,
-                        patient_name: doc.patient_name
+                    message: i18n("Greetings, {{clinicName}}. {{patient_id}} is due for a PNC visit today.", {
+                        clinicName: clinicName,
+                        patient_id: doc.patient_id
                     }),
                     phone: clinicPhone,
                     type: 'pnc_visit'
