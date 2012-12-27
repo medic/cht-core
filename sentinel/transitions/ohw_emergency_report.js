5cdeed775b00e5307a0bd897556f7c35c86a5572
Milan Andric <mandric@gmail.com>
2012-12-27 15:58:38 -0600

    bit of refactor in ohw_emergency_report transition and tests.

--- sentinel/transitions/ohw_emergency_report.js
+++ sentinel/transitions/ohw_emergency_report.js
@@ -2,62 +2,84 @@ var _ = require('underscore'),
     i18n = require('../i18n'),
     utils = require('../lib/utils');
 
-module.exports = {
-    db: require('../db'),
-    onMatch: function(change, callback) {
-        var doc = change.doc,
-            clinicName,
-            clinicPhone,
-            parentPhone = utils.getParentPhone(doc);
+var handleOnMatch = function(change, callback) {
+    var doc = change.doc,
+        clinicName,
+        clinicPhone,
+        parentPhone = utils.getParentPhone(doc);
 
-        clinicPhone = utils.getClinicPhone(doc);
-        clinicName = utils.getClinicName(doc);
+    clinicPhone = utils.getClinicPhone(doc);
+    clinicName = utils.getClinicName(doc);
 
-        utils.getOHWRegistration(doc.patient_id, function(err, registration) {
-            if (err) {
-                callback(err);
-            } else if (registration) {
-                utils.addMessage(doc, {
-                    phone: clinicPhone,
-                    message: i18n("Thank you. Danger sign {{danger_sign}} has been recorded for {{serial_number}}.", {
-                        danger_sign: doc.danger_sign,
-                        serial_number: registration.serial_number
-                    })
-                });
+    utils.getOHWRegistration(doc.patient_id, function(err, registration) {
 
-                registration.danger_signs = _.uniq(_.union(registration.danger_signs || [], [doc.danger_sign]));
+        if (err) {
+            return callback(err);
+        }
 
-                utils.updateScheduledMessage(registration, {
-                    message: i18n("Greetings, {{clinicName}}. {{patient_id}} is due to deliver soon. This pregnancy has been flagged as high-risk.", {
-                        clinicName: clinicName
-                    }),
-                    type: 'upcoming_delivery'
-                });
-                if (parentPhone) {
-                    utils.addMessage(doc, {
-                        phone: parentPhone,
-                        message: i18n("{{clinicName}} has reported danger sign {{danger_sign}} is present in {{serial_number}}. Please follow up.", {
-                            clinicName: clinicName,
-                            danger_sign: doc.danger_sign,
-                            serial_number: registration.serial_number
-                        })
-                    });
-                }
-                module.exports.db.saveDoc(registration, function(err) {
-                    callback(err, true);
-                });
-
-            } else if (clinicPhone) {
+        if (!registration) {
+            if (clinicPhone) {
                 utils.addMessage(doc, {
                     phone: clinicPhone,
                     message: i18n("No patient with id '{{patient_id}}' found.", {
                         patient_id: doc.patient_id
                     })
                 });
-                callback(null, true);
-            } else {
-                callback(null, false);
             }
+            return callback(null, true);
+        }
+
+        if (doc.labor_danger === 'no') {
+            //todo/update messaging
+            utils.addMessage(doc, {
+                phone: clinicPhone,
+                message: i18n("Thank you, {{clinicName}}. No danger sign for {{serial_number}} has been recorded.", {
+                    clinicName: clinicName,
+                    danger_sign: doc.danger_sign,
+                    serial_number: registration.serial_number
+                })
+            });
+            return callback(null, true);
+        }
+
+        utils.addMessage(doc, {
+            phone: clinicPhone,
+            message: i18n("Thank you, {{clinicName}}. Danger sign for {{serial_number}} has been recorded.", {
+                clinicName: clinicName,
+                danger_sign: doc.danger_sign,
+                serial_number: registration.serial_number
+            })
         });
-    }
+
+        /* deprecated
+         * utils.updateScheduledMessage(registration, {
+            message: i18n("Greetings, {{clinicName}}. {{patient_id}} is due to deliver soon. This pregnancy has been flagged as high-risk.", {
+                clinicName: clinicName
+            }),
+            type: 'upcoming_delivery'
+        });
+        module.exports.db.saveDoc(registration, function(err) {
+            callback(err, true);
+        });
+        */
+
+        if (doc.advice_received === 'no') {
+            utils.addMessage(doc, {
+                phone: parentPhone || '',
+                message: i18n(
+                    "{{clinicName}} has reported a danger sign for {{patient_id}}. Please follow up with her and provide necessary assistance immediately.", {
+                    clinicName: clinicName,
+                    patient_id: doc.patient_id
+                })
+            });
+        }
+
+        callback(null, true);
+
+    });
+};
+
+module.exports = {
+    db: require('../db'),
+    onMatch: handleOnMatch
 };
