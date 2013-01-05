8e8c1b9217ceb26b7193a29c032ad15c5e4d1741
Milan Andric <mandric@gmail.com>
2013-01-04 21:59:56 -0600

    redundant

--- sentinel/settings-test-example.js
+++
@@ -1,9 +0,0 @@
-// copy this file to settings-test.js and update its contents to the right values
-
-module.exports = {
-  host: 'localhost',
-  port: 5984,
-  db: 'kujua-test',
-  username: 'admin',
-  password: 'admin'
-}
