ab6b0d99e9bf14ada1b0f99272c931ff84b675c6
Milan Andric <mandric@gmail.com>
2013-01-06 21:48:31 -0600

    always use arrays in config.

--- sentinel/config.js
+++ sentinel/config.js
@@ -6,12 +6,12 @@ var config,
 config = {
     ohw_anc_reminder_schedule_weeks: [16, 24, 32, 36],
     ohw_anc_reminder_schedule_days: [81, 83, 91, 105],
-    ohw_miso_reminder_weeks: 32,
-    ohw_upcoming_delivery_weeks: 37,
-    ohw_outcome_request_weeks: 41,
+    ohw_miso_reminder_weeks: [32],
+    ohw_upcoming_delivery_weeks: [37],
+    ohw_outcome_request_weeks: [41],
     ohw_pnc_schedule_days: [1, 3, 7],
     ohw_low_weight_pnc_schedule_days: [1, 2, 3, 4, 5, 6, 7],
-    ohw_obsolete_anc_reminders_days: 21,
+    ohw_obsolete_anc_reminders_days: [21],
     send_weekly_reminders: {
         VPD: {
             3: "Last day to submit a timely VPD report for the previous week.",
