e3b23035a5bdad2e9f130d0383c50e92afa272f0
Ryan Ramage <ryan.ramage@gmail.com>
2013-01-14 14:33:20 -0700

    Adding kanso-gardener, and check for COUCH_URL env and use if available.

---
+++ packages/kanso-gardener/build/compile.js
@@ -0,0 +1,156 @@
+var path = require('path'),
+    exec = require('child_process').exec,
+    fs = require('fs.extra'),
+    _ = require('underscore'),
+    npm_cmd = "npm pack ";
+
+
+module.exports = {
+    run : function(root, path_loc, kanso_json, doc, callback) {
+        var folder_name = 'node_module';
+        if (kanso_json.node_module_folder) {
+            folder_name = kanso_json.node_module_folder;
+        }
+        var working_folder_name = folder_name + '_working';
+
+        var src_folder = path.join(root, folder_name),
+            package_folder = path.join(root, working_folder_name),
+            generated_package_json,
+            expected_tgz_name;
+        async.waterfall([
+            function(callback) {
+                copy_node_dir(src_folder, package_folder, callback);
+            },
+            function(callback) {
+                read_package_json(package_folder, kanso_json, callback);
+            },
+            function(package_json, callback) {
+                generated_package_json = package_json;
+                write_package_json(package_json, package_folder, callback);
+            },
+            function(callback) {
+                generate_tgz(package_folder, callback);
+            },
+            function(callback) {
+                expected_tgz_name = generate_tgz_name(generated_package_json);
+                attach_tgz(expected_tgz_name, doc, callback);
+            },
+            function(doc, callback) {
+                add_node_info(doc, expected_tgz_name, callback);
+            }
+        ], function(err, doc) {
+            clean_up(package_folder, expected_tgz_name,  function(err2) {
+                callback(err, doc);
+            })
+        });
+    }
+}
+
+function generate_full_command(package_folder) {
+    return npm_cmd + ' ' + package_folder;
+}
+
+
+function copy_node_dir(from, to, callback) {
+    fs.copyRecursive(from, to, callback);
+}
+
+function clean_up(dir, tgz_file, callback) {
+    async.parallel([
+        function(cb) {
+            fs.rmrf(dir, cb);
+        },
+        function(cb) {
+            fs.unlink(tgz_file, cb);
+        }
+    ], callback);
+}
+
+function sane_package_json(kanso_json) {
+    var package_json = _.extend({}, kanso_json);
+    _.each(['_id',
+            'minify',
+            'dependencies', 
+            'load', 
+            'modules', 
+            'attachments', 
+            'dust', 
+            'duality', 
+            'coffee-script', 
+            'less', 
+            'bundledDependencies'], 
+    function(field) {
+        if (package_json[field]) delete package_json[field];
+    })
+    return package_json;
+}
+
+function read_package_json(package_folder, kanso_json, callback) {
+    var package_json = path.join(package_folder, 'package.json');
+    var sane_defaults = sane_package_json(kanso_json);
+    console.log(package_json);
+    fs.readFile(package_json, function(err, content) {
+        if (err && err.code !== 'ENOENT') return  callback(err);
+        var json = {};
+        if (content) {
+            json = JSON.parse(content);
+        }
+        var p_json = _.defaults(json, sane_defaults);
+        callback(null, p_json);
+    });
+}
+
+function write_package_json(package_json, package_folder, callback) {
+    var package_json_file = path.join(package_folder, 'package.json');
+    fs.writeFile(package_json_file, JSON.stringify(package_json), function(err){
+        if (err) return callback(err);
+
+        // the following allows a package.tgz to have the same hash if nothing has changed
+        var time = new Date(0);
+        fs.utimes(package_json_file, time, time, callback);
+
+    });
+}
+
+
+function generate_tgz_name(package_json) {
+    return package_json.name + '-' + package_json.version + '.tgz';
+}
+
+function generate_tgz(package_folder, callback) {
+    var cmd = generate_full_command(package_folder);
+    console.log('running: ' + cmd);
+
+    exec(cmd, function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        callback(err);
+    });
+}
+
+
+function attach_tgz(tgz_name, doc, callback) {
+    fs.readFile(tgz_name, function (err, content) {
+        if (err) return callback(err);
+        
+        if (!doc._attachments) {
+            doc._attachments = {};
+        }
+        doc._attachments[tgz_name] = {
+            'content_type': 'application/octet-stream',
+            'data': content.toString('base64')
+        };
+        callback(null, doc);
+    });    
+}
+
+
+function add_node_info(doc, expected_tgz_name, callback) {
+
+    doc.node_module = expected_tgz_name;
+
+    if (!doc.kanso) doc.kanso = {};
+    if (!doc.kanso.config) doc.kanso.config = {};
+    doc.kanso.config.node_module = expected_tgz_name;
+    callback(null, doc);
+}
--- sentinel/db.js
+++ sentinel/db.js
@@ -1,7 +1,20 @@
 var couchdb = require('felix-couchdb'),
+	url = require('url'),
     settings;
 
-if (process.env.SENTINEL_TEST) {
+if (process.env.COUCH_URL) {
+	var couch_url = url.parse(process.env.COUCH_URL);
+	settings = {
+		port: couch_url.port,
+		host: couch_url.hostname,
+		db : couch_url.path
+	};
+	if (couch_url.auth) {
+		var unamepass = couch_url.auth.split(':');
+		settings.username = unamepass[0];
+		settings.password = unamepass[1];
+	}
+} else if (process.env.SENTINEL_TEST) {
     settings = require('./settings-test');
 } else {
     settings = require('./settings');
