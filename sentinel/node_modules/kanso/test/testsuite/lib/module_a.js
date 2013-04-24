41a069193e456a673a9e0d031069e27ddbf3da77
Ryan Ramage <ryan.ramage@gmail.com>
2013-04-24 16:30:28 -0600

    Removed a lot of cruft

--- sentinel/node_modules/kanso/test/testsuite/lib/module_a.js
+++
@@ -1,7 +0,0 @@
-var b = require('./module_b');
-
-exports.name = 'Module A';
-
-exports.b_name = function () {
-    return b.name;
-};
--- sentinel/node_modules/kanso/test/testsuite/lib/module_b.js
+++
@@ -1,7 +0,0 @@
-var a = require('./module_a');
-
-exports.name = 'Module B';
-
-exports.a_name = function () {
-    return a.name;
-};
