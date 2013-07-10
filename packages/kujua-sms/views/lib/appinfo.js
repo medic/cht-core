69fcf4bdfcfce2e3f41096a8f30cee755dbe4f94
Rowan Crawford <wombleton@gmail.com>
2013-07-10 15:31:30 +1200

    Apply all app_settings to info, and unroll translations

--- packages/kujua-sms/views/lib/appinfo.js
+++ packages/kujua-sms/views/lib/appinfo.js
@@ -13,6 +13,8 @@ exports.getAppInfo = function() {
         muvuku_webapp_url: '/json-forms/_design/json-forms/_rewrite/?_embed_mode=2'
     };
 
+    _.extend(info, this.app_settings || {});
+
     if (this.app_settings && this.app_settings.muvuku_webapp_url) {
         info.muvuku_webapp_url = this.app_settings.muvuku_webapp_url;
     }
@@ -21,10 +23,14 @@ exports.getAppInfo = function() {
     muvuku.query.sync_url = require('duality/core').getBaseURL() + '/add';
     info.muvuku_webapp_url = url.format(muvuku);
 
-    _.extend(info, {
-        gateway_number: this.app_settings && this.app_settings.gateway_number,
-        sha: this.kanso && this.kanso.git && this.kanso.git.commit
-    });
+    info.sha = this.kanso && this.kanso.git && this.kanso.git.commit;
 
+    if (info.translations) {
+        _.each(info.translations, function(t) {
+            if (t.key && !info[t.key]) {
+                info[t.key] = t.value;
+            }
+        })
+    }
     return info;
 };
