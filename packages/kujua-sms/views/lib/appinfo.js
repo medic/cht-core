d885d7dd8cc6f8b6c0f9df77c042cd462f23b13b
Rowan Crawford <wombleton@gmail.com>
2013-07-12 13:14:43 +1200

    Add gateway number to muvuku query

--- packages/kujua-sms/views/lib/appinfo.js
+++ packages/kujua-sms/views/lib/appinfo.js
@@ -5,24 +5,34 @@ var _ = require('underscore'),
  * This has to run in the shows/list/update context for 'this' to work
  * Specifically, needs patched duality/core.js to have correct context
  */
-exports.getAppInfo = function() {
-    var info,
+exports.getAppInfo = function(user) {
+    var gateway,
+        info,
         muvuku;
 
     info = {
         muvuku_webapp_url: '/json-forms/_design/json-forms/_rewrite/?_embed_mode=2'
     };
 
-    if (this.app_settings && this.app_settings.muvuku_webapp_url) {
-        info.muvuku_webapp_url = this.app_settings.muvuku_webapp_url;
+
+    if (this.app_settings) {
+        gateway = this.app_settings.gateway_number;
+
+        info.muvuku_webapp_url = info.muvuku_webapp_url || this.app_settings.muvuku_webapp_url;
     }
+
     muvuku = url.parse(info.muvuku_webapp_url, true);
     muvuku.search = null;
     muvuku.query._sync_url = require('duality/core').getBaseURL() + '/add';
+
+    if (gateway) {
+        muvuku.query._gateway_num = gateway;
+    }
+
     info.muvuku_webapp_url = url.format(muvuku);
 
     _.extend(info, {
-        gateway_number: this.app_settings && this.app_settings.gateway_number,
+        gateway_number: gateway,
         sha: this.kanso && this.kanso.git && this.kanso.git.commit
     });
 
