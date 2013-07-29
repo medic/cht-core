025d9a96eeb2c6adaed427ec2a197864ca53fa64
Rowan Crawford <wombleton@gmail.com>
2013-07-29 14:34:20 +1200

    Only exit if process.env.TEST_ENV if falsey

--- db.js
+++ db.js
@@ -1,21 +1,26 @@
-var couchdb = require('felix-couchdb'),
+var _ = require('underscore'),
+    couchdb = require('felix-couchdb'),
 	url = require('url'),
-    settings;
+    settings = {};
 
 if (process.env.COUCH_URL) {
 	var couch_url = url.parse(process.env.COUCH_URL);
 
-	settings = {
+    _.extend(settings, {
 		port: couch_url.port,
 		host: couch_url.hostname,
-		db : couch_url.path
-	};
+		db: couch_url.path
+	});
+
 	if (couch_url.auth) {
 		var index = couch_url.auth.indexOf(':');
-		settings.username = couch_url.substring(0, index);
-		settings.password = couch_url.substring(index + 1);
+
+        _.extend(settings, {
+            username: couch_url.substring(0, index),
+            password: couch_url.substring(index + 1)
+        });
 	}
-} else {
+} else if (!process.env.TEST_ENV) {
     console.log(
         "Please define a COUCH_URL in your environment e.g. \n" +
         "export COUCH_URL='http://admin:123qwe@localhost:5984/kujua-lite'"
