0b4788ff3b24df53f6dd12f9ef04d16d5136a686
Rowan Crawford <wombleton@gmail.com>
2013-08-23 12:09:37 +1200

    Flesh out getOffset, addRegime functionality

--- test/unit/add_regime.js
+++ test/unit/add_regime.js
@@ -1,4 +1,6 @@
 var _ = require('underscore'),
+    sinon = require('sinon'),
+    moment = require('moment'),
     transition = require('../../transitions/add_regimes');
 
 exports['signature'] = function(test) {
@@ -38,3 +40,50 @@ exports['filter fails if regime exists and matching scheduled_tasks'] = function
     }), false);
     test.done();
 };
+
+exports['getOffset returns false for bad syntax'] = function(test) {
+    test.equals(transition.getOffset('x'), false);
+    test.equals(transition.getOffset('2 muppets'), false);
+    test.equals(transition.getOffset('one week'), false);
+    test.done();
+};
+
+exports['getOffset returns durations for good syntax'] = function(test) {
+    test.equals(transition.getOffset('2 weeks').asDays(), 14);
+    test.equals(transition.getOffset('81 days').asDays(), 81);
+    test.done();
+};
+
+exports['regime generates two scheduled messages'] = function(test) {
+    var doc = {
+        serial_number: 'abc',
+        lmp_date: moment().valueOf()
+    };
+
+    sinon.stub(transition, 'getRegime').returns({
+        key: 'duckland',
+        messages: [
+            {
+                group: 1,
+                offset: '1 week',
+                message: "This is for serial number {{serial_number}}."
+            },
+            {
+                group: 4,
+                offset: '81 days',
+                message: "This is for serial number {{serial_number}}."
+            }
+        ],
+        start_from: 'lmp_date'
+    });
+
+    transition.addRegime(doc, 'duckland');
+
+    test.ok(doc.scheduled_tasks);
+    test.equals(doc.scheduled_tasks.length, 2);
+    test.equals(moment(doc.scheduled_tasks[1].due).diff(doc.lmp_date, 'days'), 81);
+
+    transition.getRegime.restore();
+
+    test.done();
+}
--- transitions/add_regimes.js
+++ transitions/add_regimes.js
@@ -1,5 +1,7 @@
 var _ = require('underscore'),
+    config = require('../config'),
     date = require('../date'),
+    i18n = require('../i18n'),
     moment = require('moment'),
     utils = require('../lib/utils');
 
@@ -16,16 +18,36 @@ module.exports = {
     onMatch: function(change, db, callback) {
         callback(null, true);
     },
+    getOffset: function(offset) {
+        var tokens = (offset || '').split(' '),
+            value = tokens[0],
+            unit = tokens[1];
+
+        if (/\d+/.test(value) && /(second|minute|hour|day|week|month|year)s?/.test(unit)) {
+            return moment.duration(Number(value), unit);
+        } else {
+            return false;
+        }
+    },
+    getRegime: function(key) {
+        var regimes = config.get('scheduled_reminder_regimes');
+
+        return _.find(regimes, function(regime) {
+            return regime.key === key;
+        });
+    },
     addRegime: function(doc, key) {
-        var regime,
+        var regime = module.exports.getRegime(key),
             docStart,
             start,
             clinicContactName = utils.getClinicContactName(doc),
-            clinicName = utils.getClinicName(doc);
-            now = moment(date.getDate()),
-            regimes = config.get('scheduled_reminder_regimes');
+            clinicName = utils.getClinicName(doc),
+            now = moment(date.getDate());
 
-
+            // if we  can't find the regime in config, we're done
+        if (!_.isObject(regime)) {
+            return false;
+        }
 
         docStart = doc[regime.start_from];
 
@@ -38,17 +60,27 @@ module.exports = {
         start = moment(docStart);
 
         _.each(regime.messages, function(msg) {
-            utils.addScheduledMessage(doc, {
-                message: i18n(msg.message, {
-                    clinic_name: clinicName,
-                    contact_name: clinicContactName,
-                    patient_id: doc.patient_id,
-                    serial_number: doc.serial_number
-                }),
-                group: msg.group,
-                phone: doc.from,
-                type: key
-            });
+            var due,
+                offset = module.exports.getOffset(msg.offset);
+
+            if (offset) {
+                due = start.clone().add(offset).valueOf();
+                utils.addScheduledMessage(doc, {
+                    due: due,
+                    message: i18n(msg.message, {
+                        clinic_name: clinicName,
+                        contact_name: clinicContactName,
+                        patient_id: doc.patient_id,
+                        serial_number: doc.serial_number
+                    }),
+                    group: msg.group,
+                    phone: doc.from,
+                    type: key
+                });
+            } else {
+                // bad offset, skip this msg
+                console.log("%s cannot be parsed as a valid offset. Skipping this msg of %s regime.", msg.offset, key);
+            }
         });
     }
 };
