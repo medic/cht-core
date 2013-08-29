46f386943a2c2643d745138ab2043fb057d0e8ff
Rowan Crawford <wombleton@gmail.com>
2013-08-29 13:26:23 +1200

    Add update notifications transition

---
+++ test/unit/update_notifications.js
@@ -0,0 +1,70 @@
+var _ = require('underscore'),
+    sinon = require('sinon'),
+    transition = require('../../transitions/update_notifications'),
+    utils = require('../../lib/utils');
+
+exports['onMatch signature'] = function(test) {
+    test.ok(_.isFunction(transition.onMatch));
+    test.equals(transition.onMatch.length, 3);
+    test.done();
+};
+
+exports['filter signature'] = function(test) {
+    test.ok(_.isFunction(transition.filter));
+    test.equals(transition.filter.length, 1);
+    test.done();
+};
+
+exports['filter tests'] = function(test) {
+    test.equals(transition.filter({}), false);
+    test.equals(transition.filter({
+        patient_id: 'x',
+    }), false);
+    test.equals(transition.filter({
+        form: 'x',
+        patient_id: 'x',
+    }), true);
+    test.done();
+};
+
+exports['returns false if not on or off form'] = function(test) {
+    sinon.stub(transition, 'getConfig').returns({
+        on_form: 'x',
+        off_form: 'y'
+    });
+
+    transition.onMatch({
+        doc: {
+            form: 'z'
+        }
+    }, {}, function(err, complete) {
+        test.equals(complete, false);
+        transition.getConfig.restore();
+        test.done();
+    });
+}
+exports['registration not found adds error'] = function(test) {
+    var doc = {
+        patient_id: 'x'
+    };
+
+    sinon.stub(transition, 'getConfig').returns({
+        patient_not_found: 'not found {{patient_id}}'
+    });
+    sinon.stub(utils, 'getRegistrations').callsArgWithAsync(1, null, []);
+
+    transition.onMatch({
+        doc: doc
+    }, {}, function(err, complete) {
+        test.equals(err, null);
+        test.equals(complete, true);
+
+        test.equals(doc.errors.length, 1);
+        test.equals(doc.errors[0].message, 'not found x');
+
+        transition.getConfig.restore();
+        utils.getRegistrations.restore();
+
+        test.done();
+    });
+}
