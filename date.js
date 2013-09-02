3b6d592e1dc212aafb5d2a0b383249f8c751fcde
Rowan Crawford <wombleton@gmail.com>
2013-09-02 14:49:56 +1200

    Add getDuration to date module

--- date.js
+++ date.js
@@ -44,6 +44,17 @@ function getDate() {
 module.exports = {
     getDate: getDate,
     getTimestamp: getTimestamp,
-    isSynthetic: isSynthetic
+    isSynthetic: isSynthetic,
+    getDuration: function(s) {
+        var tokens = (s || '').split(' '),
+            value = tokens[0],
+            unit = tokens[1];
+
+        if (/\d+/.test(value) && /(second|minute|hour|day|week|month|year)s?/.test(unit)) {
+            return moment.duration(Number(value), unit);
+        } else {
+            return null;
+        }
+    }
 };
 load();
