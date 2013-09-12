479dbdac1f7bfc87a505662034362367ac3dd383
Milan Andric <milan@medicmobile.org>
2013-09-12 18:59:49 -0500

    moved validatePatientId to utils

--- transitions/accept_patient_reports.js
+++ transitions/accept_patient_reports.js
@@ -95,20 +95,6 @@ module.exports = {
             callback(null);
         }
     },
-    validatePatientId: function(report, doc) {
-
-        report = report || {};
-
-        _.defaults(report, {
-            invalid_patient_id: "Patient ID '{{patient_id}}' is invalid. Please correct this and try again."
-        });
-
-        if (report.patient_id_validation_regexp) {
-            return new RegExp(report.patient_id_validation_regexp).test(doc.patient_id);
-        } else {
-            return true;
-        }
-    },
     handleReport: function(options, callback) {
         var doc = options.doc,
             report = options.report;
@@ -137,7 +123,7 @@ module.exports = {
             form: doc.form
         });
 
-        if (!module.exports.validatePatientId(report, doc)) {
+        if (!utils.validatePatientId(report, doc)) {
             messages.addError(doc, report.invalid_patient_id);
             return callback(null, true);
         }
