f9c191d35acf017c272ce6ba35d52a0bf9807bd6
Milan Andric <milan@medicmobile.org>
2013-11-16 15:42:20 +0300

    fixed bug on reporting rates when a clinic name is not defined.
    If a clinic name was not defined then the row on reporting rates would
    not have a label.  Added some sane defaults for that case.

--- packages/kujua-reporting/kujua-reporting/views.js
+++ packages/kujua-reporting/kujua-reporting/views.js
@@ -29,18 +29,34 @@ exports.facilities_by_parent = {
 exports.total_clinics_by_facility = {
     map: function (doc) {
         if (doc.type === 'clinic') {
-            var dh = doc.parent ? doc.parent.parent : undefined,
-                dh_id = dh ? dh._id : undefined,
-                dh_name = dh ? dh.name : undefined,
-                hc_id = doc.parent ? doc.parent._id : undefined,
-                hc_name = doc.parent ? doc.parent.name : undefined,
+            var dh = doc.parent && doc.parent.parent,
+                dh_id = dh && dh._id,
+                dh_name = dh && dh.name,
+                hc_id = doc.parent && doc.parent._id,
+                hc_name = doc.parent && doc.parent.name,
                 cl_name = doc.name,
                 phone = doc.contact && doc.contact.phone;
 
-            if (!cl_name) {
-                cl_name = doc.contact ? doc.contact.rc_code : undefined;
+            if (!cl_name && doc.contact) {
+                if (doc.contact.name) {
+                    cl_name = doc.contact.name;
+                    if (doc.contact.rc_code) {
+                        cl_name += ' ' + doc.contact.rc_code;
+                    }
+                } else {
+                    cl_name = doc.contact.rc_code;
+                }
             }
-            emit([dh_id, hc_id, doc._id, dh_name, hc_name, cl_name, phone], 1);
+
+            emit([
+                dh_id,
+                hc_id,
+                doc._id,
+                dh_name,
+                hc_name,
+                cl_name,
+                phone
+            ], 1);
         }
     },
     reduce: function (keys, values, rereduce) {
