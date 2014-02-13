ee61cb4a6965409f3ea3297af160a9ef5faa685e
garethbowen <gareth@bowenwebdesign.co.nz>
2014-02-14 09:03:55 +1300

    Minor test formatting

--- tests/send-message.js
+++ tests/send-message.js
@@ -1,12 +1,12 @@
 var data_record = require('lib/data_records');
 
 exports.countCharUpdatesNote = function(test) {
-    var modal = $('<div class="modal"><div class="modal-footer"><div class="note"/></div></div>'),
-        // value 110 chars long
-        message = $('<input value="abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij"/>');
+  var modal = $('<div class="modal"><div class="modal-footer"><div class="note"/></div></div>'),
+    // value 110 chars long
+    message = $('<input value="abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij"/>');
 
-    data_record.countChars(message, modal);
-    test.equals(modal.find('.note').html(), '110 characters');
-    test.done();
+  data_record.countChars(message, modal);
+  test.equals(modal.find('.note').html(), '110 characters');
+  test.done();
 };
 
