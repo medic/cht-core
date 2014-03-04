bf13e70673dc6e6f575141299797d66865d25466
Milan Andric <milan@medicmobile.org>
2014-03-03 22:12:06 -0300

    specified bash for CI script.

--- scripts/ci/after_success.sh
+++ scripts/ci/after_success.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
-
-npm install -g jsontool
+#!/bin/bash
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
+    npm install -g jsontool && \
     cat kanso.json | json -o json-4 -e "dependencies[\"kanso-gardener\"] = null; version += \"-alpha.$TRAVIS_BUILD_NUMBER\";" > tmp.json && \
     mv tmp.json kanso.json && \
     kanso push http://service:a5nghmongP\!@staging.dev.medicmobile.org/markets/alpha/upload
 fi;
 
 if [ "$TRAVIS_BRANCH" == "testing" ]; then
+    npm install -g jsontool && \
     cat kanso.json | json -o json-4 -e "dependencies[\"kanso-gardener\"] = null; version += \"-beta.$TRAVIS_BUILD_NUMBER\";" > tmp.json && \
     mv tmp.json kanso.json && \
     kanso push http://service:a5nghmongP\!@staging.dev.medicmobile.org/markets/beta/upload
