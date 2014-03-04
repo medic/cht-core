dedcda25708a00c941166c66d37c0ee6c7393b10
Milan Andric <milan@medicmobile.org>
2014-03-04 14:52:30 -0300

    temp hack for #447; make 3 attempts to push in after success script.

--- scripts/ci/after_success.sh
+++ scripts/ci/after_success.sh
@@ -3,15 +3,21 @@
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
     npm install -g jsontool && \
     cat kanso.json | json -o json-4 -e "dependencies[\"kanso-gardener\"] = null; version += \"-alpha.$TRAVIS_BUILD_NUMBER\";" > tmp.json && \
-    mv tmp.json kanso.json && \
-    kanso push http://service:a5nghmongP\!@staging.dev.medicmobile.org/markets/alpha/upload
+    mv tmp.json kanso.json && (
+        kanso push http://service:a5nghmongP\!@staging.dev.medicmobile.org/markets/alpha/upload ||
+        kanso push http://service:a5nghmongP\!@staging.dev.medicmobile.org/markets/alpha/upload ||
+        kanso push http://service:a5nghmongP\!@staging.dev.medicmobile.org/markets/alpha/upload
+    )
 fi;
 
 if [ "$TRAVIS_BRANCH" == "testing" ]; then
     npm install -g jsontool && \
     cat kanso.json | json -o json-4 -e "dependencies[\"kanso-gardener\"] = null; version += \"-beta.$TRAVIS_BUILD_NUMBER\";" > tmp.json && \
-    mv tmp.json kanso.json && \
-    kanso push http://service:a5nghmongP\!@staging.dev.medicmobile.org/markets/beta/upload
+    mv tmp.json kanso.json && (
+        kanso push http://service:a5nghmongP\!@staging.dev.medicmobile.org/markets/beta/upload || 
+        kanso push http://service:a5nghmongP\!@staging.dev.medicmobile.org/markets/beta/upload ||
+        kanso push http://service:a5nghmongP\!@staging.dev.medicmobile.org/markets/beta/upload 
+    )
 fi;
 
 exit 0;
