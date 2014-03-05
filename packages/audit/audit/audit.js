5890ef22da4b11d261630298d8e93f1d2e16fdb1
garethbowen <gareth@bowenwebdesign.co.nz>
2014-03-06 10:35:46 +1300

    Split audit.log in to 'create' and 'update' as we need to pass the generated docid to create. Issue: #415

--- packages/audit/audit/audit.js
+++ packages/audit/audit/audit.js
@@ -5,7 +5,15 @@ var db = require('db'),
   _ = require('underscore');
 
 module.exports = {
-  log: function(doc, callback) {
+  create: function(doc, docid, callback) {
+    var appdb = db.use(duality.getDBURL());
+    getUsername(function(user) {
+      var record = createRecord(docid);
+      record.history.push(createItem(doc, user, 'create'));
+      appdb.saveDoc(record, callback);
+    });
+  },
+  update: function(doc, callback) {
     var appdb = db.use(duality.getDBURL());
     appdb.getView(appname, 'audit_records_by_doc', {
       include_docs: false,
@@ -15,17 +23,10 @@ module.exports = {
       if (err) {
         return callback(err);
       }
-      session.info(function (err, info) {
-        var user = err ? undefined : info.userCtx.name;
-        var record;
-        var action;
-        if (result.rows.length === 0) {
-          record = createRecord(doc);
-          action = 'create';
-        } else {
-          record = result.rows[0];
-          action = doc._deleted ? 'delete' : 'update';
-        }
+      getUsername(function(user) {
+        var record = result.rows.length === 0 ? 
+          createRecord(doc._id) : result.rows[0];
+        var action = doc._deleted ? 'delete' : 'update';
         record.history.push(createItem(doc, user, action));
         appdb.saveDoc(record, callback);
       });
@@ -33,10 +34,16 @@ module.exports = {
   }
 };
 
-function createRecord(doc) {
+function getUsername(callback) {
+  session.info(function (err, info) {
+    callback.call(this, err ? undefined : info.userCtx.name);
+  });
+}
+
+function createRecord(docid) {
   return {
     type: 'audit_record',
-    record_id: doc._id,
+    record_id: docid,
     history: []
   };
 }
@@ -45,7 +52,7 @@ function createItem(doc, user, action) {
   return {
     user: user,
     action: action,
-    timestamp: JSON.stringify(new Date()),
+    reported_date: new Date().toISOString(),
     doc: doc
   };
 }
--- packages/audit/tests/audit/audit.js
+++ packages/audit/tests/audit/audit.js
@@ -13,19 +13,14 @@ exports.tearDown = function (callback) {
   callback();
 }
 
-exports['when no existing audit record, create one'] = function(test) {
-  test.expect(12);
+exports['calling create saves new audit record'] = function(test) {
+  test.expect(10);
 
   var docid = 123;
   var user = 'hacker';
-  var doc1 = {_id: docid};
+  var doc1 = {_id: undefined};
  
   sinon.stub(db, 'use').returns({
-    getView: function(appname, viewname, query, callback) {
-      test.equal(query.startkey[0], docid, 'correct startkey query param');
-      test.equal(query.endkey[0], docid, 'correct endkey query param');
-      callback(null, {"rows":[]});
-    },
     saveDoc: function(doc, callback) {
       test.equal(doc.type, 'audit_record', 'doc has correct type');
       test.equal(doc._id, undefined, 'doc has no id');
@@ -34,15 +29,50 @@ exports['when no existing audit record, create one'] = function(test) {
       test.equal(doc.history[0].user, user, 'user recorded correctly');
       test.equal(doc.history[0].action, 'create', 'action is set correctly');
       test.equal(doc.history[0].doc, doc1, 'current doc is saved');
-      test.ok(doc.history[0].timestamp, 'timestamp is set');
+      test.ok(doc.history[0].reported_date, 'reported_date is set');
       callback(null, {record_id: docid});
     }
   });
   sinon.stub(session, 'info')
     .callsArgWith(0, null, {userCtx: {name: user}});
 
-  audit.log(
+  audit.create(
     doc1,
+    docid,
+    function(err, response) {
+      test.equal(err, null, 'no error');
+      test.equal(response.record_id, docid, 'response is created doc');
+    }
+  );
+
+  test.done();
+};
+
+exports['when getting user fails, log audit record anyway'] = function(test) {
+  test.expect(10);
+
+  var docid = 123;
+  var doc1 = {_id: undefined};
+ 
+  sinon.stub(db, 'use').returns({
+    saveDoc: function(doc, callback) {
+      test.equal(doc.type, 'audit_record', 'doc has correct type');
+      test.equal(doc._id, undefined, 'doc has no id');
+      test.equal(doc.record_id, docid, 'doc refers to original doc');
+      test.equal(doc.history.length, 1, 'history has one entry');
+      test.equal(doc.history[0].user, undefined, 'user recorded correctly');
+      test.equal(doc.history[0].action, 'create', 'action is set correctly');
+      test.equal(doc.history[0].doc, doc1, 'current doc is saved');
+      test.ok(doc.history[0].reported_date, 'reported_date is set');
+      callback(null, {record_id: docid});
+    }
+  });
+  sinon.stub(session, 'info')
+    .callsArgWith(0, 'no user logged in');
+
+  audit.create(
+    doc1,
+    docid,
     function(err, response) {
       test.equal(err, null, 'no error');
       test.equal(response.record_id, docid, 'response is created doc');
@@ -64,27 +94,27 @@ exports['when existing audit record, append update to history'] = function(test)
   sinon.stub(db, 'use').returns({
     getView: function(appname, viewname, query, callback) {
       callback(null, {"rows":[{
-        _id: docid,
+        record_id: docid,
         history: [{
           user: user1,
           action: 'create',
-          timestamp: '1Z',
+          reported_date: '1Z',
           doc: doc1
         }]
       }]});
     },
     saveDoc: function(doc, callback) {
-      test.equal(doc._id, docid, 'doc has correct id');
+      test.equal(doc.record_id, docid, 'doc refers to original doc');
       test.equal(doc.history.length, 2, 'history has two entries');
 
       test.equal(doc.history[0].user, user1, 'user recorded correctly');
       test.equal(doc.history[0].action, 'create', 'action is set correctly');
-      test.equal(doc.history[0].timestamp, '1Z', 'timestamp is set');
+      test.equal(doc.history[0].reported_date, '1Z', 'reported_date is set');
       test.equal(doc.history[0].doc, doc1, 'doc is set');
 
       test.equal(doc.history[1].user, user2, 'user recorded correctly');
       test.equal(doc.history[1].action, 'update', 'action is set correctly');
-      test.ok(doc.history[1].timestamp, 'timestamp is set');
+      test.ok(doc.history[1].reported_date, 'reported_date is set');
       test.equal(doc.history[1].doc, doc2, 'new doc is set');
 
       callback(null, {record_id: docid});
@@ -93,7 +123,7 @@ exports['when existing audit record, append update to history'] = function(test)
   sinon.stub(session, 'info')
     .callsArgWith(0, null, {userCtx: {name: user2}});
 
-  audit.log(
+  audit.update(
     doc2,
     function(err, response) {
       test.equal(err, null, 'no error');
@@ -116,27 +146,27 @@ exports['when existing audit record is deleted, append delete to history'] = fun
   sinon.stub(db, 'use').returns({
     getView: function(appname, viewname, query, callback) {
       callback(null, {"rows":[{
-        _id: docid,
+        record_id: docid,
         history: [{
           user: user1,
           action: 'create',
-          timestamp: '1Z',
+          reported_date: '1Z',
           doc: doc1
         }]
       }]});
     },
     saveDoc: function(doc, callback) {
-      test.equal(doc._id, docid, 'doc has correct id');
+      test.equal(doc.record_id, docid, 'doc refers to original doc');
       test.equal(doc.history.length, 2, 'history has two entries');
 
       test.equal(doc.history[0].user, user1, 'user recorded correctly');
       test.equal(doc.history[0].action, 'create', 'action is set correctly');
-      test.equal(doc.history[0].timestamp, '1Z', 'timestamp is set');
+      test.equal(doc.history[0].reported_date, '1Z', 'reported_date is set');
       test.equal(doc.history[0].doc, doc1, 'doc is set');
 
       test.equal(doc.history[1].user, user2, 'user recorded correctly');
       test.equal(doc.history[1].action, 'delete', 'action is set correctly');
-      test.ok(doc.history[1].timestamp, 'timestamp is set');
+      test.ok(doc.history[1].reported_date, 'reported_date is set');
       test.equal(doc.history[1].doc, doc2, 'new doc is set');
 
       callback(null, {record_id: docid});
@@ -145,8 +175,45 @@ exports['when existing audit record is deleted, append delete to history'] = fun
   sinon.stub(session, 'info')
     .callsArgWith(0, null, {userCtx: {name: user2}});
 
-  audit.log(
+  audit.update(
     doc2,
+    function(err, response) {
+      test.equal(err, null, 'no error');
+      test.equal(response.record_id, docid, 'response is created doc');
+    }
+  );
+
+  test.done();
+};
+
+exports['when update but no existing audit'] = function(test) {
+  test.expect(8);
+
+  var docid = 123;
+  var user1 = 'admin';
+  var doc1 = {_id: docid, x: 'foo'};
+
+  sinon.stub(db, 'use').returns({
+    getView: function(appname, viewname, query, callback) {
+      callback(null, {"rows":[]});
+    },
+    saveDoc: function(doc, callback) {
+      test.equal(doc.record_id, docid, 'doc refers to original doc');
+      test.equal(doc.history.length, 1, 'history has one entry');
+
+      test.equal(doc.history[0].user, user1, 'user recorded correctly');
+      test.equal(doc.history[0].action, 'update', 'action is set correctly');
+      test.ok(doc.history[0].reported_date, 'reported_date is set');
+      test.equal(doc.history[0].doc, doc1, 'doc is set');
+
+      callback(null, {record_id: docid});
+    }
+  });
+  sinon.stub(session, 'info')
+    .callsArgWith(0, null, {userCtx: {name: user1}});
+
+  audit.update(
+    doc1,
     function(err, response) {
       test.equal(err, null, 'no error');
       test.equal(response.record_id, docid, 'response is created doc');
