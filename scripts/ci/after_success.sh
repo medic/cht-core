cdcc9088dbfed3940157b1b82edf702258e2ae3d
Milan Andric <milan@medicmobile.org>
2014-03-13 19:50:57 -0300

    updated path and auth for staging URL.

--- scripts/ci/after_success.sh
+++ scripts/ci/after_success.sh
@@ -4,14 +4,14 @@ if [ "$TRAVIS_BRANCH" == "develop" ]; then
     npm install -g jsontool && \
     cat kanso.json | json -o json-4 -e "dependencies[\"kanso-gardener\"] = null; version += \"-alpha.$TRAVIS_BUILD_NUMBER\";" > tmp.json && \
     mv tmp.json kanso.json && \
-    kanso push http://service:a5nghmongP\!@staging.dev.medicmobile.org/markets/alpha/upload
+    kanso push http://travis-ci:a5nghmongP\!@staging.dev.medicmobile.org/markets-alpha/upload
 fi;
 
 if [ "$TRAVIS_BRANCH" == "testing" ]; then
     npm install -g jsontool && \
     cat kanso.json | json -o json-4 -e "dependencies[\"kanso-gardener\"] = null; version += \"-beta.$TRAVIS_BUILD_NUMBER\";" > tmp.json && \
     mv tmp.json kanso.json && \
-    kanso push http://service:a5nghmongP\!@staging.dev.medicmobile.org/markets/beta/upload
+    kanso push http://travis-ci:a5nghmongP\!@staging.dev.medicmobile.org/markets-beta/upload
 fi;
 
 exit 0;
