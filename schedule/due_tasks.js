7c6b8eb960e4f0294fa7e910b6e48fb53bddfe97
garethbowen <gareth@bowenwebdesign.co.nz>
2014-03-20 09:33:42 +1300

    Refactored and created tests for schedule/due_tasks

--- schedule/due_tasks.js
+++ schedule/due_tasks.js
@@ -12,38 +12,30 @@ module.exports = function(db, audit, callback) {
         endkey: now.toISOString(),
         startkey: overdue.toISOString()
     }, function(err, result) {
-        var objs;
 
         if (err) {
-            callback(err);
-        } else {
-            objs = _.reduce(result.rows, function(memo, row) {
-                memo[row.id] = memo[row.id] || {
-                    due: row.key,
-                    doc: row.doc,
-                    tasks: []
-                };
-                memo[row.id].tasks.push(row.value);
-                return memo;
-            }, {});
-
-            async.forEachSeries(_.keys(objs), function(id, cb) {
-                var obj = objs[id],
-                    doc = obj.doc,
-                    due = obj.due,
-                    tasks = obj.tasks;
-
-                // set task to pending for gateway to pick up
-                _.each(doc.scheduled_tasks, function(task) {
-                    if (task.due === due) {
-                        task.state = 'pending';
-                    }
-                });
-
-                audit.saveDoc(doc, cb);
-            }, function(err) {
-                callback(err);
-            });
+            return callback(err);
         }
+
+        var objs = _.unique(result.rows, false, function(row) {
+            return row.id;
+        });
+
+        async.forEachSeries(objs, function(obj, cb) {
+            var doc = obj.doc,
+                due = obj.key;
+
+            // set task to pending for gateway to pick up
+            _.each(doc.scheduled_tasks, function(task) {
+                if (task.due === due) {
+                    task.state = 'pending';
+                }
+            });
+
+            audit.saveDoc(doc, cb);
+        }, function(err) {
+            callback(err);
+        });
+        
     });
 };
---
+++ test/unit/due_tasks.js
@@ -0,0 +1,220 @@
+var _ = require('underscore'),
+  sinon = require('sinon'),
+  moment = require('moment'),
+  schedule = require('../../schedule/due_tasks');
+
+exports['due_tasks handles view returning no rows'] = function(test) {
+  test.expect(3);
+
+  var db = {
+    view: function() {}
+  };
+
+  var audit = {
+    saveDoc: function() {}
+  };
+
+  var view = sinon.stub(db, 'view').callsArgWith(3, null, {
+    rows: []
+  });
+  var saveDoc = sinon.stub(audit, 'saveDoc').callsArgWith(1, null);
+
+  schedule(db, {}, function(err) {
+    test.equals(err, undefined);
+  });
+
+  test.equals(view.callCount, 1);
+  test.equals(saveDoc.callCount, 0);
+  test.done();
+}
+
+exports['set all due scheduled tasks to pending'] = function(test) {
+  test.expect(8);
+
+  var due = moment().toISOString();
+  var notDue = moment().add('days', 7).toISOString();
+  var id = 'xyz';
+
+  var db = {
+    view: function() {}
+  };
+  var view = sinon.stub(db, 'view').callsArgWith(3, null, {
+    rows: [
+      {
+        id: id,
+        key: due,
+        doc: {
+          scheduled_tasks: [
+            {
+              due: due,
+              state: 'scheduled'
+            },
+            {
+              due: notDue,
+              state: 'scheduled'
+            }
+          ]
+        }
+      }
+    ]
+  });
+
+  var audit = {
+    saveDoc: function(doc, callback) {
+      callback();
+    }
+  };
+  var saveDoc = sinon.spy(audit, 'saveDoc');
+
+  schedule(db, audit, function(err) {
+    test.equals(err, undefined);
+  });
+
+  test.equals(view.callCount, 1);
+  test.equals(saveDoc.callCount, 1);
+  var saved = saveDoc.firstCall.args[0];
+  test.equals(saved.scheduled_tasks.length, 2);
+  test.equals(saved.scheduled_tasks[0].due, due);
+  test.equals(saved.scheduled_tasks[0].state, 'pending');
+  test.equals(saved.scheduled_tasks[1].due, notDue);
+  test.equals(saved.scheduled_tasks[1].state, 'scheduled');
+  test.done();
+}
+
+
+exports['set all due scheduled tasks to pending and handles repeated rows'] = function(test) {
+  test.expect(8);
+
+  var due = moment().toISOString();
+  var notDue = moment().add('days', 7).toISOString();
+  var id = 'xyz';
+  var doc = {
+    scheduled_tasks: [
+      {
+        due: due,
+        state: 'scheduled'
+      },
+      {
+        due: notDue,
+        state: 'scheduled'
+      }
+    ]
+  };
+
+  var db = {
+    view: function() {}
+  };
+  var view = sinon.stub(db, 'view').callsArgWith(3, null, {
+    rows: [
+      {
+        id: id,
+        key: due,
+        doc: doc
+      },
+      {
+        id: id,
+        key: due,
+        doc: doc
+      }
+    ]
+  });
+
+  var audit = {
+    saveDoc: function(doc, callback) {
+      callback();
+    }
+  };
+  var saveDoc = sinon.spy(audit, 'saveDoc');
+
+  schedule(db, audit, function(err) {
+    test.equals(err, undefined);
+  });
+
+  test.equals(view.callCount, 1);
+  test.equals(saveDoc.callCount, 1);
+  var saved = saveDoc.firstCall.args[0];
+  test.equals(saved.scheduled_tasks.length, 2);
+  test.equals(saved.scheduled_tasks[0].due, due);
+  test.equals(saved.scheduled_tasks[0].state, 'pending');
+  test.equals(saved.scheduled_tasks[1].due, notDue);
+  test.equals(saved.scheduled_tasks[1].state, 'scheduled');
+  test.done();
+}
+
+
+exports['set all due scheduled tasks to pending and handles nonrepeated rows'] = function(test) {
+  test.expect(9);
+
+  var due = moment().toISOString();
+  var notDue = moment().add('days', 7).toISOString();
+  var id1 = 'xyz';
+  var id2 = 'abc';
+  var doc = {
+    scheduled_tasks: [
+      {
+        due: due,
+        state: 'scheduled'
+      },
+      {
+        due: notDue,
+        state: 'scheduled'
+      }
+    ]
+  };
+
+  var db = {
+    view: function() {}
+  };
+  var view = sinon.stub(db, 'view').callsArgWith(3, null, {
+    rows: [
+      {
+        id: id1,
+        key: due,
+        doc: {
+          scheduled_tasks: [
+            {
+              due: due,
+              state: 'scheduled'
+            }
+          ]
+        }
+      },
+      {
+        id: id2,
+        key: due,
+        doc: {
+          scheduled_tasks: [
+            {
+              due: due,
+              state: 'scheduled'
+            }
+          ]
+        }
+      }
+    ]
+  });
+
+  var audit = {
+    saveDoc: function(doc, callback) {
+      callback();
+    }
+  };
+  var saveDoc = sinon.spy(audit, 'saveDoc');
+
+  schedule(db, audit, function(err) {
+    test.equals(err, undefined);
+  });
+
+  test.equals(view.callCount, 1);
+  test.equals(saveDoc.callCount, 2);
+  var saved1 = saveDoc.firstCall.args[0];
+  test.equals(saved1.scheduled_tasks.length, 1);
+  test.equals(saved1.scheduled_tasks[0].due, due);
+  test.equals(saved1.scheduled_tasks[0].state, 'pending');
+  var saved2 = saveDoc.secondCall.args[0];
+  test.equals(saved2.scheduled_tasks.length, 1);
+  test.equals(saved2.scheduled_tasks[0].due, due);
+  test.equals(saved2.scheduled_tasks[0].state, 'pending');
+  test.done();
+}
+
