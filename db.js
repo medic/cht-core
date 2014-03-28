fe36c4313bad797f354fe867970037345d087d9e
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2014-03-28 16:28:37 +1300

    Initial commit

---
+++ db.js
@@ -0,0 +1,39 @@
+var _ = require('underscore'),
+    couchdb = require('felix-couchdb'),
+    url = require('url'),
+    settings = {};
+
+if (process.env.COUCH_URL) {
+    var couch_url = url.parse(process.env.COUCH_URL);
+
+    _.extend(settings, {
+        port: couch_url.port,
+        host: couch_url.hostname,
+        db: couch_url.path
+    });
+
+    if (couch_url.auth) {
+        var index = couch_url.auth.indexOf(':');
+
+        _.extend(settings, {
+            username: couch_url.auth.substring(0, index),
+            password: couch_url.auth.substring(index + 1)
+        });
+    }
+} else if (!process.env.TEST_ENV) {
+    console.log(
+        "Please define a COUCH_URL in your environment e.g. \n" +
+        "export COUCH_URL='http://admin:123qwe@localhost:5984/kujua-lite'\n\n" +
+        "If you are running tests use TEST_ENV=1 in your environment.\n"
+    );
+    process.exit(1);
+}
+
+var client = couchdb.createClient(
+    settings.port,
+    settings.host,
+    settings.username,
+    settings.password
+);
+module.exports = client.db(settings.db);
+module.exports.user = settings.username;
