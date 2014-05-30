a64e61979c164f0b4c105782b48dd7e2e680d50d
Milan Andric <milan@medicmobile.org>
2014-05-30 15:15:15 +0300

    fixed tests on update_notifications

--- test/unit/update_notifications.js
+++ test/unit/update_notifications.js
@@ -23,6 +23,11 @@ exports['filter tests'] = function(test) {
     test.equals(transition.filter({
         form: 'x',
         patient_id: 'x',
+    }), false);
+    test.equals(transition.filter({
+        form: 'x',
+        patient_id: 'x',
+        related_entities: {clinic: {contact: {phone: 'x'}}}
     }), true);
     test.done();
 };
@@ -62,11 +67,15 @@ exports['no configured on or off form returns false'] = function(test) {
 exports['registration not found adds error'] = function(test) {
     var doc = {
         form: 'on',
-        patient_id: 'x'
+        patient_id: 'x',
+        related_entities: {clinic: {contact: {phone: 'x'}}}
     };
 
     sinon.stub(transition, 'getConfig').returns({
-        patient_not_found: 'not found {{patient_id}}',
+        messages: [{
+            event_type: 'patient_not_found',
+            message: 'not found {{patient_id}}'
+        }],
         on_form: 'on'
     });
     sinon.stub(utils, 'getRegistrations').callsArgWithAsync(1, null, []);
