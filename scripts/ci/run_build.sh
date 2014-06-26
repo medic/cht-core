b9c0fcebdb8cc14a71dc3cc4e2aaaf1d09a847f2
Milan Andric <milan@medicmobile.org>
2014-06-26 15:11:36 +0200

    use --production on npm install to decrease ddoc size.

--- scripts/ci/run_build.sh
+++ scripts/ci/run_build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-cd kujua-api &&
-npm install &&
-cd .. &&
+# install deps for kujua-api 
+cd kujua-api && \
+npm install --production && \
+cd .. && \
 ./scripts/phantom_test.sh http://localhost:5984/kujua-lite
