d4b2ab40449228ce133602b9ba0efcb99c2db1e9
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2014-07-03 10:14:51 +1200

    Be more careful when determining if a form is compact or not. Now non-compact submissions must have a field label followed by whitespace. Issue: #552

---
+++ packages/kujua-sms/tests/kujua-sms/textforms_parser.js
@@ -0,0 +1,57 @@
+var smsparser = require('views/lib/textforms_parser');
+
+var def = {
+    meta: {
+        code: 'ANCR'
+    },
+    fields: {
+        name: {
+            labels: {
+                short: 'Name',
+                tiny: 'N'
+            },
+            type: 'string'
+        },
+        lmp: {
+            labels: {
+                short: 'LMP',
+                tiny: 'LMP'
+            },
+            type: 'integer'
+        },
+        somedate: {
+            labels: {
+                short: 'Date',
+                tiny: 'D'
+            },
+            type: 'date'
+        }
+    }
+};
+
+exports['is compact true'] = function(test) {
+    var doc = {
+        message: 'sarah 24 2012-03-12'
+    };
+
+    test.ok(smsparser.isCompact(def, doc));
+    test.done();
+};
+
+exports['is compact true when field value starts with field name'] = function(test) {
+    var doc = {
+        message: 'Norah 24 2012-03-12'
+    };
+
+    test.ok(smsparser.isCompact(def, doc));
+    test.done();
+};
+
+exports['is compact false'] = function(test) {
+    var doc = {
+        message: 'n sarah lmp 24 d 2012-03-12'
+    };
+
+    test.ok(!smsparser.isCompact(def, doc));
+    test.done();
+};
--- packages/kujua-sms/views/lib/textforms_parser.js
+++ packages/kujua-sms/views/lib/textforms_parser.js
@@ -33,7 +33,7 @@ var lower = function(str) {
 };
 
 var startsWith = function(lhs, rhs) {
-    return lhs && rhs && lhs.substring(0, rhs.length) === rhs;
+    return lhs && rhs && new RegExp('^' + rhs + '\\s', 'i').test(lhs);
 };
 
 /**
@@ -256,7 +256,7 @@ exports.isCompact = function(def, msg) {
         ];
     }));
     return !_.some(labels, function(label) {
-        return startsWith(lower(fields[0]), lower(label));
+        return startsWith(fields[0], label);
     });
 };
 
