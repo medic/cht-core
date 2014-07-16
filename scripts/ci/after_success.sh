d4dc7c0b6a7ffec73aa99a8eed53342b4d580359
Milan Andric <milan@medicmobile.org>
2014-07-15 23:08:12 -0300

    modified after_success build script to include bundled node module deps.

--- scripts/ci/after_success.sh
+++ scripts/ci/after_success.sh
@@ -4,53 +4,61 @@ MAX=50
 COUNT=0
 DONE=0
 
+# Never push to market on pull requests
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    exit 0;
+fi
+
 # Try pushing up to $MAX times.
 function push {
     ((COUNT++))
     local URL="$1"
     if [ $COUNT -le $MAX ]; then
-        kanso push --minify "$URL" && DONE=1
-        if [ $DONE == 1 ]; then 
-            exit 0
-        else 
-            push $URL
-        fi
+        kanso push --minify "$URL" && exit 0
+        push $URL
     else
         echo 'Failed to push to market.'
         exit 1
     fi
 }
 
-# Never push to market on pull requests
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    exit 0;
-fi
+function prep {
+    local args=''
+    local meta="$1"
+    args+='this.dependencies["kanso-gardener"] = null;'
+    args+='this.dependencies_included = true;'
+
+    # Process meta portion of version string if provided
+    if [ -n "$meta" ]; then
+        args+="this.version += \"-${meta}\";"
+    fi
+
+    # Install npm deps in module directories and tweak kanso gardener related
+    # configs so it knows.
+    npm install -g json && \
+    cd sentinel && npm install && cd .. && \
+    cd api && npm install && cd .. && \
+    cat kanso.json | json -o json-4 -e "$args" > tmp.json && \
+    mv tmp.json kanso.json
+}
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    npm install -g json && \
-    cat kanso.json | json -o json-4 -e "this.dependencies[\"kanso-gardener\"] = null;" > tmp.json && \
-    mv tmp.json kanso.json && \
+    prep && \
     push 'http://travis-ci:a5nghmongP!@staging.dev.medicmobile.org/markets-release/upload'
 fi;
 
 #if [ "$TRAVIS_BRANCH" == "develop" ]; then
-#    npm install -g json && \
-#    cat kanso.json | json -o json-4 -e "this.dependencies[\"kanso-gardener\"] = null; this.version += \"-alpha.$TRAVIS_BUILD_NUMBER\";" > tmp.json && \
-#    mv tmp.json kanso.json && \
+#    prep "-alpha.$TRAVIS_BUILD_NUMBER" && \
 #    push 'http://travis-ci:a5nghmongP!@staging.dev.medicmobile.org/markets-alpha/upload'
 #fi;
 
 if [ "$TRAVIS_BRANCH" == "testing" ]; then
-    npm install -g json && \
-    cat kanso.json | json -o json-4 -e "this.dependencies[\"kanso-gardener\"] = null; this.version += \"-beta.$TRAVIS_BUILD_NUMBER\";" > tmp.json && \
-    mv tmp.json kanso.json && \
+    prep "beta.$TRAVIS_BUILD_NUMBER" && \
     push 'http://travis-ci:a5nghmongP!@staging.dev.medicmobile.org/markets-beta/upload'
 fi;
 
 if [ "$TRAVIS_BRANCH" == "inbox-design" ]; then
-    npm install -g json && \
-    cat kanso.json | json -o json-4 -e "this.dependencies[\"kanso-gardener\"] = null; this.version += \"-alpha.$TRAVIS_BUILD_NUMBER\";" > tmp.json && \
-    mv tmp.json kanso.json && \
+    prep "alpha.$TRAVIS_BUILD_NUMBER" && \
     push 'http://travis-ci:a5nghmongP!@staging.dev.medicmobile.org/markets-alpha/upload'
 fi;
 
