d64d6c329969daff33c635f8b05d77ec2258ca3f
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2014-07-23 13:28:10 +1200

    Fixed jquery path. Issue: #370

--- tests_ui/karma-unit.conf.js
+++ tests_ui/karma-unit.conf.js
@@ -19,7 +19,7 @@ module.exports = function(config) {
     
     files: [
       // 3rd Party Code
-      'bower_components/jquery/jquery.js',
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-resource/angular-resource.js',
