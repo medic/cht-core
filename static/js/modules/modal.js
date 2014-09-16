3f5bca101d1cdd40955d3ad85320d0c58e1c2eee
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2014-09-17 08:34:49 +1200

    Replace original button label when finished submitting. Issue: #619

--- static/js/modules/modal.js
+++ static/js/modules/modal.js
@@ -3,6 +3,7 @@
   'use strict';
 
   exports.start = function(modal) {
+    var label = modal.find('.submit').text();
     modal.find('.submit').text('Updating...');
     modal.find('.btn, [name]').attr('disabled', true);
     return {
@@ -13,10 +14,10 @@
           message = description + ': ' + err;
         } else {
           modal.modal('hide');
-          modal.find('[name=message]').val('');
+          modal.find('input[type=text], textarea').val('');
         }
         modal.find('.modal-footer .note').text(message);
-        modal.find('.submit').text('Submit');
+        modal.find('.submit').text(label);
         modal.find('.btn, [name]').attr('disabled', false);
       }
     };
