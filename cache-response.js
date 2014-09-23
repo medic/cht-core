575bf9c87fb627cd6933a43e993187bfb79cbf38
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2014-09-23 17:22:15 +1200

    Fix caching for binary elements. Issue: #4

--- cache-response.js
+++ cache-response.js
@@ -1,4 +1,4 @@
-var http = require('http'),
+var request = require('request'),
     fs = require('fs'),
     path = require('path'),
     send = require('send'),
@@ -6,21 +6,12 @@ var http = require('http'),
     mkdirp = require('mkdirp'),
     cacheDir = '.cache';
 
-var store = function(cachePath, data, callback) {
-  mkdirp(path.dirname(cachePath), function(err) {
-    if (err) {
-      return callback(err);
-    }
-    fs.writeFile(cachePath, data, callback);
-  });
-};
-
 var retrieve = function(req, res, next, cachePath, options) {
-  var stream = send(req, cachePath, options);
-  stream.on('error', function(err) {
-    next(err.status === 404 ? null : err);
-  });
-  stream.pipe(res);
+  send(req, cachePath, options)
+    .on('error', function(err) {
+      next(err.status === 404 ? null : err);
+    })
+    .pipe(res);
 };
 
 exports = module.exports = function cacheResponse(target, options) {
@@ -38,36 +29,19 @@ exports = module.exports = function cacheResponse(target, options) {
     if (fs.existsSync(cachePath)) {
       return retrieve(req, res, next, cachePath, options);
     }
-    
-    http.get(target + req.originalUrl, function(cres) {
 
-      var data = [];
-
-      cres.on('data', function(chunk) {
-        data.push(chunk);
-      });
-
-      cres.on('close', function() {
-        res.end();
-      });
-
-      cres.on('end', function() {
-        var status = cres.statusCode;
-        if (status === 304 || (status >= 200 && status < 300)) {
-          store(cachePath, data.join(''), function(err) {
-            if (err) {
-              return next(err);
-            }
-            return retrieve(req, res, next, cachePath, options);
-          });
-        } else {
-          // let the request fall through
-          return next();
-        }
-      });
-
-    }).on('error', function(err) {
-      next(err.message);
+    mkdirp(path.dirname(cachePath), function(err) {
+      if (err) {
+        return next(err);
+      }
+      request(target + req.originalUrl)
+        .pipe(fs.createWriteStream(cachePath))
+        .on('close', function() {
+          return retrieve(req, res, next, cachePath, options);
+        })
+        .on('error', function(err) {
+          return next(err);
+        });
     });
 
   };
