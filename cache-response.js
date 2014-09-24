c80c02a359d1a79b7d77d181334ba66e733bed4d
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2014-09-24 15:26:37 +1200

    Proxy the request so auth settings are passed on to couch. Store meta data so we know if the content is gzipped or not. Issue: #4

--- cache-response.js
+++ cache-response.js
@@ -4,14 +4,51 @@ var request = require('request'),
     send = require('send'),
     rimraf = require('rimraf'),
     mkdirp = require('mkdirp'),
-    cacheDir = '.cache';
+    cacheDir = '.cache',
+    storedHeaders = [ 'content-encoding' ];
 
 var retrieve = function(req, res, next, cachePath, options) {
-  send(req, cachePath, options)
-    .on('error', function(err) {
-      next(err.status === 404 ? null : err);
+  getMeta(cachePath, function(err, meta) {
+    if (err) {
+      return next(err);
+    }
+    send(req, cachePath, options)
+      .on('headers', function(res) {
+        if (meta && meta.headers) {
+          meta.headers.forEach(function(header) {
+            if (header.value) {
+              res.setHeader(header.key, header.value);
+            }
+          });
+        }
+      })
+      .on('error', function(err) {
+        next(err.status === 404 ? null : err);
+      })
+      .pipe(res);
+  });
+};
+
+var getMetaPath = function(cachePath) {
+  return cachePath + '.json';
+};
+
+var getMeta = function(cachePath, callback) {
+  fs.readFile(getMetaPath(cachePath), function(err, data) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, JSON.parse(data));
+  });
+};
+
+var setMeta = function(cachePath, res, callback) {
+  var meta = {
+    headers: storedHeaders.map(function(header) {
+      return { key: header, value: res.headers[header] }
     })
-    .pipe(res);
+  };
+  fs.writeFile(getMetaPath(cachePath), JSON.stringify(meta), callback);
 };
 
 exports = module.exports = function cacheResponse(target, options) {
@@ -34,14 +71,24 @@ exports = module.exports = function cacheResponse(target, options) {
       if (err) {
         return next(err);
       }
-      request(target + req.originalUrl)
-        .pipe(fs.createWriteStream(cachePath))
-        .on('close', function() {
-          return retrieve(req, res, next, cachePath, options);
+      req
+        .pipe(request(target + req.originalUrl))
+        .on('response', function (resp) {
+          if (resp.statusCode >= 200 && resp.statusCode < 300) {
+            var stream = fs.createWriteStream(cachePath);
+            stream.on('finish', function() {
+              setMeta(cachePath, resp, function(err) {
+                if (err) {
+                  return next(err);
+                }
+                retrieve(req, res, next, cachePath, options);
+              });
+            });
+            resp.pipe(stream);
+          } else {
+            next();
+          }
         })
-        .on('error', function(err) {
-          return next(err);
-        });
     });
 
   };
