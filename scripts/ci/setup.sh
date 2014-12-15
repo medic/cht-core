b484fcbeb8443d1418c47c8e3945000caf3457a8
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2014-12-15 13:29:39 +1300

    Fixed bugs in setup script. Issue: #743

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -1,21 +1,20 @@
 #!/bin/bash -x
 
-local meta="$1"
-local args='this.dependencies["kanso-gardener"] = null;'
-args+='this.dependencies_included = true;'
+ARGS='this.dependencies["kanso-gardener"] = null;'
+ARGS+='this.dependencies_included = true;'
 
-# Process meta portion of version string if provided
+# Process append to the version string if pre-release
 if [ "$TRAVIS_BRANCH" == "testing" ]; then
-    args+="this.version += \"-beta.$TRAVIS_BUILD_NUMBER\";"
+    ARGS+="this.version += \"-beta.$TRAVIS_BUILD_NUMBER\";"
 fi
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
-    args+="this.version += \"-alpha.$TRAVIS_BUILD_NUMBER\";"
+    ARGS+="this.version += \"-alpha.$TRAVIS_BUILD_NUMBER\";"
 fi
 
 # Install npm deps in module directories and tweak kanso gardener related
 # configs so it knows.
 npm install -g json && \
-cat kanso.json | json -o json-4 -e "$args" > tmp.json && \
+cat kanso.json | json -o json-4 -e "$ARGS" > tmp.json && \
 mv tmp.json kanso.json
 
 exit 0;
