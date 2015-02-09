c5134d314d350157e0377f5c4809a714ceaafafd
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2015-02-09 14:31:09 +1300

    Removed unused packages

--- packages/couchdb-audit/couchdb-audit/views.js
+++
@@ -1,8 +0,0 @@
-
-exports.audit_records_by_doc = {
-  map: function (doc) {
-    if (doc.type === 'audit_record') {
-      emit([doc.record_id], 1);
-    }
-  }
-};
--- packages/moment-timezone/README.md
+++
@@ -1,2 +0,0 @@
-To package this for kanso manually include the moment-timezone.json file in
-moment.js as an argument to the moment.tz.add() call.
