b4b0dbe875c446e9d285528836de5758544cc89b
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2015-03-16 17:24:24 +1300

    Reformatting only.

--- db.js
+++ db.js
@@ -4,74 +4,74 @@ var path = require('path'),
 
 var couchUrl = process.env.COUCH_URL;
 if (couchUrl) {
-    var parsedUrl = url.parse(couchUrl);
+  var parsedUrl = url.parse(couchUrl);
 
-    module.exports = nano(couchUrl.substring(0, couchUrl.indexOf('/', 10)));
-    module.exports.medic = nano(couchUrl);
+  module.exports = nano(couchUrl.substring(0, couchUrl.indexOf('/', 10)));
+  module.exports.medic = nano(couchUrl);
 
-    module.exports.settings = {
-        protocol: parsedUrl.protocol,
-        port: parsedUrl.port,
-        host: parsedUrl.hostname,
-        db: parsedUrl.path.replace('/',''),
-        ddoc: 'medic'
-    };
+  module.exports.settings = {
+    protocol: parsedUrl.protocol,
+    port: parsedUrl.port,
+    host: parsedUrl.hostname,
+    db: parsedUrl.path.replace('/',''),
+    ddoc: 'medic'
+  };
 
-    if (parsedUrl.auth) {
-        var index = parsedUrl.auth.indexOf(':');
-        module.exports.settings.username = parsedUrl.auth.substring(0, index);
-        module.exports.settings.password = parsedUrl.auth.substring(index + 1);
+  if (parsedUrl.auth) {
+    var index = parsedUrl.auth.indexOf(':');
+    module.exports.settings.username = parsedUrl.auth.substring(0, index);
+    module.exports.settings.password = parsedUrl.auth.substring(index + 1);
+  }
+
+  module.exports.fti = function(index, data, cb) {
+    var url = path.join('_fti/local', module.exports.settings.db,
+                        '_design', module.exports.settings.ddoc, index);
+    if (data.q && !data.limit) {
+      data.limit = 1000;
     }
-
-    module.exports.fti = function(index, data, cb) {
-        var url = path.join('_fti/local', module.exports.settings.db,
-                            '_design', module.exports.settings.ddoc, index);
-        if (data.q && !data.limit) {
-            data.limit = 1000;
-        }
-        console.log('URL', url);
-        module.exports.request({
-            method: data.q ? 'post' : 'get',
-            path: url,
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            qs: data
-        }, function(err, result) {
-            if (err) {
-                // the request itself failed
-                return cb(err);
-            }
-            if (data.q && !result.rows) {
-                // the query failed for some reason
-                return cb(result);
-            }
-            cb(null, result);
-        });
-    };
-    module.exports.getPath = function() {
-        return path.join(module.exports.settings.db, '_design',
-                         module.exports.settings.ddoc, '_rewrite');
-    };
-    module.exports.getSettings = function(cb) {
-        var uri = path.join(module.exports.getPath(), 'app_settings',
-                            module.exports.settings.ddoc);
-        module.exports.request({ path: uri }, cb);
-    };
+    console.log('URL', url);
+    module.exports.request({
+      method: data.q ? 'post' : 'get',
+      path: url,
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      qs: data
+    }, function(err, result) {
+      if (err) {
+        // the request itself failed
+        return cb(err);
+      }
+      if (data.q && !result.rows) {
+        // the query failed for some reason
+        return cb(result);
+      }
+      cb(null, result);
+    });
+  };
+  module.exports.getPath = function() {
+    return path.join(module.exports.settings.db, '_design',
+                     module.exports.settings.ddoc, '_rewrite');
+  };
+  module.exports.getSettings = function(cb) {
+    var uri = path.join(module.exports.getPath(), 'app_settings',
+                        module.exports.settings.ddoc);
+    module.exports.request({ path: uri }, cb);
+  };
 } else if (process.env.TEST_ENV) {
-    // Running tests only
-    module.exports = {
-        fti: function() {},
-        settings: {},
-        medic: {
-            view: function() {},
-            get: function() {},
-            insert: function() {}
-        }
-    };
+  // Running tests only
+  module.exports = {
+    fti: function() {},
+    settings: {},
+    medic: {
+      view: function() {},
+      get: function() {},
+      insert: function() {}
+    }
+  };
 } else {
-    console.log(
-        "Please define a COUCH_URL in your environment e.g. \n" +
-        "export COUCH_URL='http://admin:123qwe@localhost:5984/medic'\n\n" +
-        "If you are running tests use TEST_ENV=1 in your environment.\n"
-    );
-    process.exit(1);
+  console.log(
+    "Please define a COUCH_URL in your environment e.g. \n" +
+    "export COUCH_URL='http://admin:123qwe@localhost:5984/medic'\n\n" +
+    "If you are running tests use TEST_ENV=1 in your environment.\n"
+  );
+  process.exit(1);
 }
