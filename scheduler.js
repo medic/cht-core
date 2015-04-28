79bfeda9c7d5299a5f4476844f3bcf2167115130
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2015-04-29 10:46:14 +1200

    Check for stats to submit every hour not every day.

--- scheduler.js
+++ scheduler.js
@@ -17,11 +17,11 @@ var wrapAsync = function(fn, name) {
 module.exports = {
   init: function() {
 
-    // collect usage stats at 3am each day
+    // collect usage stats at 3am every day
     new CronJob('0 0 3 * * *', wrapAsync(usageStats.go, 'usageStats')).start();
 
-    // submit usage stats at 4am each day
-    new CronJob('0 0 4 * * *', wrapAsync(statsSubmission.go, 'statsSubmission')).start();
+    // submit usage stats at 30 minutes past the hour every hour
+    new CronJob('0 30 * * * *', wrapAsync(statsSubmission.go, 'statsSubmission')).start();
 
   }
 };
