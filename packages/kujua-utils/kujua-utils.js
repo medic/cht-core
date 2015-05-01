8dbf83a4cf70c223baf96b2fbe7bef9e3faa9c96
Milan Andric <milan@medicmobile.org>
2015-04-30 22:43:12 -0300

    removed cruft in kujua-utils package.

--- packages/kujua-utils/kujua-utils.js
+++ packages/kujua-utils/kujua-utils.js
@@ -34,24 +34,6 @@ exports.prettyMonth = function (month, full) {
     return months_short[month];
 };
 
-//unused?
-var months = function () {
-    return [
-        [0, 'January'],
-        [1, 'February'],
-        [2, 'March'],
-        [3, 'April'],
-        [4, 'May'],
-        [5, 'June'],
-        [6, 'July'],
-        [7, 'August'],
-        [8, 'September'],
-        [9, 'October'],
-        [10, 'November'],
-        [11, 'December']
-    ];
-};
-
 var logger = exports.logger = {
     levels: {silent:0, error:1, warn:2, info:3, debug:4},
     log: function(obj) {
