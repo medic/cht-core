c07c1c1c96a9b03d3c56bc9f9e0da57e21b3627f
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2015-05-11 10:53:52 +1200

    Fixed bug with appointments not including correct registrations.

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -16,7 +16,7 @@ var getAppointmentDate = function(options, registration) {
 };
 
 var getAppointments = function(options, callback) {
-  utils.getAllRegistrations(options, function(err, registrations) {
+  utils.getAllRegistrations({}, function(err, registrations) {
     if (err) {
       return callback(err);
     }
