medic/cht-core

commit 3b0a5966ab5751207d05f7223a9694103dabc1db

Renamed `options.messages` parameter in `setMessages()`
For legacy reasons, this was previously called `options.contacts`.

static/js/controllers/inbox.js
@@ -133,8 +133,8 @@ require('moment/locales');
       $scope.setMessages = function(options) {
         options = options || {};
         if (options.changes) {
-          removeDeletedMessages(options.contacts);
-          mergeUpdatedMessages(options.contacts);
+          removeDeletedMessages(options.messages);
+          mergeUpdatedMessages(options.messages);
         } else {
           $scope.items = options.contacts || [];
         }

static/js/controllers/messages.js
@@ -22,7 +22,7 @@
             return console.log('Error fetching contact', err);
           }
           $scope.loading = false;
-          options.contacts = data;
+          options.messages = data;
           $scope.setMessages(options);
           if (callback) {
             callback();

commit 53f5793f6a67f6fd01de01112bc0c2b9f3a1faf6

Fix for #951 - messages tab is empty
Fixes regression caused in 3b0a5966ab5751207d05f7223a9694103dabc1db
which was causing `$scope.items` to be set to an empty list, except when
deleting or udpating a message.

static/js/controllers/inbox.js
@@ -136,7 +136,7 @@ require('moment/locales');
           removeDeletedMessages(options.messages);
           mergeUpdatedMessages(options.messages);
         } else {
-          $scope.items = options.contacts || [];
+          $scope.items = options.messages || [];
         }
       };
 