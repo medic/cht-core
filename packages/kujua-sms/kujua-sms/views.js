3badc1c73173d851df8a5ac9b0905406133e0d85
Milan Andric <milan@medicmobile.org>
2015-05-20 00:32:45 -0300

    support for medic-api messages endpoint.
    Issue: medic/medic-webapp/issues/817

--- packages/kujua-sms/kujua-sms/views.js
+++ packages/kujua-sms/kujua-sms/views.js
@@ -350,6 +350,42 @@ exports.clinic_by_refid = {
     }
 };
 
+exports.tasks_messages = {
+    map: function (doc) {
+        var _emit = function(tasks) {
+            tasks.forEach(function(task) {
+                task.messages.forEach(function(msg) {
+                    /*
+                     * uuid, to and message properties are required for message
+                     * to be processed/valid.
+                     */
+                    var when = task.due || task.timestamp || doc.reported_date;
+                    if (msg.uuid && msg.to && msg.message) {
+                        var val = {
+                            message: msg.message,
+                            to: msg.to,
+                            id: msg.uuid,
+                            state: task.state,
+                            state_details: task.state_details,
+                            state_history: task.state_history,
+                            due: task.due,
+                            timestamp: task.timestamp,
+                            _record_id: doc._id,
+                            _record_reported_date: doc.reported_date
+                        };
+                        // used for fetching a specific message based on uuid
+                        emit(msg.uuid, val);
+                        // used for querying latest tasks in a specific state
+                        emit([task.state, when], val);
+                    }
+                });
+            });
+        };
+        _emit(doc.tasks || []);
+        _emit(doc.scheduled_tasks || []);
+    }
+};
+
 exports.tasks_pending = {
     map: function (doc) {
         var has_pending,
