5beb5741c9c89e5167ddaee59809e44a3ce9781c
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2015-05-27 09:27:12 +1200

    Refactored DownloadUrl service to be a bit more readable.

--- static/js/services/download-url.js
+++ static/js/services/download-url.js
@@ -4,41 +4,49 @@
 
   var inboxServices = angular.module('inboxServices');
 
-  var luceneTypes = ['reports', 'contacts'];
-  var simpleTypes = ['messages', 'audit', 'feedback', 'logs'];
-
-  var buildUrl = function(type, params) {
-    return '/api/v1/export/' + type + '?' + $.param(params);
+  var types = {
+    reports:  { name: 'forms', format: 'xml', lucene: true },
+    contacts: { name: 'contacts', format: 'json', lucene: true },
+    messages: { name: 'messages', format: 'xml' },
+    audit:    { name: 'audit', format: 'xml' },
+    feedback: { name: 'feedback', format: 'xml' },
+    logs:     { name: 'logs', format: 'xml' }
   };
 
   inboxServices.factory('DownloadUrl', ['GenerateSearchQuery', 'Language',
     function(GenerateSearchQuery, Language) {
-      return function($scope, type, callback) {
+
+      var buildUrl = function(type, params) {
+        return '/api/v1/export/' + type.name + '?' + $.param(params);
+      };
+
+      var getParams = function(type, language, $scope, callback) {
+        var params = { format: type.format, locale: language };
+        if (!type.lucene) {
+          return callback(null, params);
+        }
+        GenerateSearchQuery($scope, function(err, response) {
+          if (err) {
+            return callback(err);
+          }
+          params.query = JSON.stringify(response.query);
+          params.schema = JSON.stringify(response.schema);
+          return callback(null, params);
+        });
+      };
+
+      return function($scope, typeName, callback) {
+        var type = types[typeName];
+        if (!type) {
+          return callback(new Error('Unknown download type'));
+        }
         Language(function(err, language) {
           if (err) {
             return callback(err);
           }
-          var params = { format: 'xml', locale: language };
-          if (simpleTypes.indexOf(type) !== -1) {
-            return callback(null, buildUrl(type, params));
-          } else if (luceneTypes.indexOf(type) !== -1) {
-            if (type === 'reports') {
-              type = 'forms';
-            }
-            if (type === 'contacts') {
-              params.format = 'json';
-            }
-            GenerateSearchQuery($scope, function(err, response) {
-              if (err) {
-                return callback(err);
-              }
-              params.query = JSON.stringify(response.query);
-              params.schema = JSON.stringify(response.schema);
-              return callback(null, buildUrl(type, params));
-            });
-          } else {
-            return callback(new Error('Unknown download type'));
-          }
+          getParams(type, language, $scope, function(err, params) {
+            callback(null, buildUrl(type, params));
+          });
         });
       };
     }
