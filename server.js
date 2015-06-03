04c4409c596ff8efa80fea2b29b24c028044b937
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2015-06-04 09:33:20 +1200

    Handle authentication errors better. Issue: #983

--- server.js
+++ server.js
@@ -313,7 +313,7 @@ proxy.on('proxyReq', function(proxyReq, req, res) {
 app.get('/api/v1/messages', function(req, res) {
   auth.check(req, 'can_view_data_records', null, function(err, ctx) {
     if (err) {
-      return serverError(err, res);
+      return error(err, res);
     }
     var opts = _.pick(req.query, 'limit', 'start', 'descending', 'state');
     messages.getMessages(opts, ctx && ctx.district, function(err, result) {
@@ -328,7 +328,7 @@ app.get('/api/v1/messages', function(req, res) {
 app.get('/api/v1/messages/:id', function(req, res) {
   auth.check(req, 'can_view_data_records', null, function(err, ctx) {
     if (err) {
-      return serverError(err, res);
+      return error(err, res);
     }
     messages.getMessage(req.params.id, ctx && ctx.district, function(err, result) {
       if (err) {
@@ -342,7 +342,7 @@ app.get('/api/v1/messages/:id', function(req, res) {
 app.put('/api/v1/messages/state/:id', jsonParser, function(req, res) {
   auth.check(req, 'can_update_messages', null, function(err, ctx) {
     if (err) {
-      return serverError(err, res);
+      return error(err, res);
     }
     messages.updateMessage(req.params.id, req.body, ctx && ctx.district, function(err, result) {
       if (err) {
@@ -357,8 +357,7 @@ app.post('/api/v1/records', [jsonParser, formParser], function(req, res) {
   auth.check(req, 'can_create_records', null, function(err, ctx) {
     var create;
     if (err) {
-      console.log('checkURL err', err);
-      return serverError(err, res);
+      return error(err, res);
     }
     if (req.headers['content-type'].toLowerCase() === 'application/x-www-form-urlencoded') {
       create = records.createRecord;
