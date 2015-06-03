1e320b318a7cebd10fff8cbb54a5a32cfbf150b9
Milan Andric <milan@medicmobile.org>
2015-06-03 18:39:41 -0300

    fixed bad merge on May 26 from testing.

--- server.js
+++ server.js
@@ -285,7 +285,6 @@ app.get('/api/v1/messages', function(req, res) {
       var opts = _.pick(req.query, 'limit', 'start', 'descending', 'state');
       messages.getMessages(opts, ctx && ctx.district, function(err, result) {
         if (err) {
-          console.error(err);
           return error(err, res);
         }
         res.json(result);
@@ -386,72 +385,6 @@ proxy.on('proxyReq', function(proxyReq, req, res) {
   }
 });
 
-app.get('/api/v1/messages', function(req, res) {
-  auth.check(req, 'can_view_data_records', null, function(err, ctx) {
-    if (err) {
-      return serverError(err, res);
-    }
-    var opts = _.pick(req.query, 'limit', 'start', 'descending', 'state');
-    messages.getMessages(opts, ctx && ctx.district, function(err, result) {
-      if (err) {
-        return serverError(err, res);
-      }
-      res.json(result);
-    });
-  });
-});
-
-app.get('/api/v1/messages/:id', function(req, res) {
-  auth.check(req, 'can_view_data_records', null, function(err, ctx) {
-    if (err) {
-      return serverError(err, res);
-    }
-    messages.getMessage(req.params.id, ctx && ctx.district, function(err, result) {
-      if (err) {
-        return serverError(err, res);
-      }
-      res.json(result);
-    });
-  });
-});
-
-app.put('/api/v1/messages/state/:id', jsonParser, function(req, res) {
-  auth.check(req, 'can_update_messages', null, function(err, ctx) {
-    if (err) {
-      return serverError(err, res);
-    }
-    messages.updateMessage(req.params.id, req.body, ctx && ctx.district, function(err, result) {
-      if (err) {
-        return serverError(err, res);
-      }
-      res.json(result);
-    });
-  });
-});
-
-app.post('/api/v1/records', [jsonParser, formParser], function(req, res) {
-  auth.check(req, 'can_create_records', null, function(err, ctx) {
-    var create;
-    if (err) {
-      console.log('checkURL err', err);
-      return serverError(err, res);
-    }
-    if (req.headers['content-type'].toLowerCase() === 'application/x-www-form-urlencoded') {
-      create = records.createRecord;
-    } else if (req.headers['content-type'].toLowerCase() === 'application/json') {
-      create = records.createRecordJSON;
-    } else {
-      return serverError('Content type not supported.', res);
-    }
-    create(req.body, ctx && ctx.district, function(err, result) {
-      if (err) {
-        return serverError(err, res);
-      }
-      res.json(result);
-    });
-  });
-});
-
 app.get('/api/v1/forms', function(req, res) {
   forms.listForms(req.headers, function(err, body, headers) {
       if (err) {
@@ -469,7 +402,7 @@ app.get('/api/v1/forms/:form', function(req, res) {
       form = parts.slice(0, -1).join('.'),
       format = parts.slice(-1)[0];
   if (!form || !format) {
-    return serverError(new Error('Invalid form parameter.'));
+    return serverError(new Error('Invalid form parameter.'), res);
   }
   forms.getForm(form, format, function(err, body, headers) {
     if (err) {
