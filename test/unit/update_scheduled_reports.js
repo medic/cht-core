487f788b68cebe008fac2420a4d0d7ca48a137f1
Milan Andric <milan@medicmobile.org>
2015-06-27 16:21:20 -0500

    improved tests for duplicate scheduled reports.

--- test/unit/update_scheduled_reports.js
+++ test/unit/update_scheduled_reports.js
@@ -22,7 +22,7 @@ exports['filter signature'] = function(test) {
     test.done();
 };
 
-exports['filter tests'] = function(test) {
+exports['docs fail filter'] = function(test) {
     var related_entities = {
         clinic: {
             contact: {
@@ -47,6 +47,17 @@ exports['filter tests'] = function(test) {
         month: 'x',
         related_entities: related_entities
     }), false);
+    test.done();
+};
+
+exports['doc pass filter'] = function(test) {
+    var related_entities = {
+        clinic: {
+            contact: {
+                phone: "x"
+            }
+        }
+    };
     // month, year property
     test.equals(transition.filter({
         form: 'x',
@@ -86,72 +97,140 @@ exports['filter tests'] = function(test) {
     test.done();
 };
 
+exports['use week view when doc has week property'] = function(test) {
+  var db = {
+    view: function(ddoc, view, q , callback) {
+      test.same(
+          view,
+          'data_records_by_form_year_week_clinic_id_and_reported_date'
+      );
+      test.done();
+    }
+  };
+  transition._getDuplicates(db, {week: 9});
+};
+
+exports['use month view when doc has month property'] = function(test) {
+  var db = {
+      view: function(ddoc, view, q , callback) {
+          test.same(
+              view,
+              'data_records_by_form_year_month_clinic_id_and_reported_date'
+          );
+          test.done();
+      }
+  };
+  transition._getDuplicates(db, {month: 9});
+};
+
 exports['calls audit.bulkSave with correct arguments'] = function(test) {
   test.expect(7);
 
   var db = {
-    view: function() {}
+      view: function() {}
   };
 
   var view = sinon.stub(db, 'view').callsArgWith(3, null, {rows: []});
 
   var audit = {
-    bulkSave: function(docs, options, callback) {
-      test.ok(docs);
-      test.ok(docs.length === 0);
-      test.ok(options);
-      test.ok(typeof(callback) === 'function');
-      callback();
-    }
+      bulkSave: function(docs, options, callback) {
+          test.ok(docs);
+          test.ok(docs.length === 0);
+          test.ok(options);
+          test.ok(typeof(callback) === 'function');
+          callback();
+      }
   };
 
   var bulkSave = sinon.spy(audit, 'bulkSave');
 
   transition.onMatch({
-    doc: {
-        _id: 'abc',
-        form: 'z',
-        year: 2013,
-        month: 4
-    }
+      doc: {
+          _id: 'abc',
+          form: 'z',
+          year: 2013,
+          month: 4
+      }
   }, db, audit, function(err, complete) {
-    test.equals(complete, true);
+      test.equals(complete, true);
   });
 
   test.equals(view.callCount, 1);
   test.equals(bulkSave.callCount, 1);
   test.done();
-}
+};
 
-exports['merge duplicates'] = function(test) {
-  // TODO
+exports['only one record in duplicates, mark transition complete'] = function(test) {
+  // todo
+  test.done();
+};
+
+exports['remove duplicates and replace with latest doc'] = function(test) {
+  test.expect(7);
   var db = {
-    view: function() {},
-    bulkDocs: function(options, callback) {
-      callback();
-    }
+    view: function() {}
   };
-  var view = sinon.stub(db, 'view').callsArgWith(3, null, {
+  var audit = {
+      bulkSave: function(docs, options, cb) {
+          test.same(docs.length, 2);
+          test.ok(options.all_or_nothing);
+          // new doc inherits id/rev from previous record and is deleted
+          docs.forEach(function(doc) {
+            if (doc._id === 'abc') {
+              test.same(doc._rev, '1-dddd');
+              test.same(doc.pills, 22);
+            }
+            if (doc._id === 'xyz') {
+              test.same(doc._deleted, true);
+            }
+          });
+          cb();
+      }
+  };
+  sinon.stub(db, 'view').callsArgWith(3, null, {
+    // ascending records
     rows: [
       {
-        _id: 'abc',
         key: [2013,4],
         doc: {
+          _id: 'abc',
+          _rev: '1-dddd',
           form: 'z',
           month: 4,
-          year: 2013
-        },
+          year: 2013,
+          pills: 12,
+          reported_date: 100
+        }
       },
       {
-        _id: 'xyz',
         key: [2013,4],
         doc: {
+          _id: 'xyz',
+          _rev: '1-kkkk',
           form: 'z',
           month: 4,
-          year: 2013
+          year: 2013,
+          pills: 22,
+          reported_date: 200
         }
       }
     ]
   });
-  test.done();
+  var bulkSave = sinon.spy(audit, 'bulkSave');
+  var change = {
+    doc: {
+        _id: 'xyz',
+        _rev: '1-kkkk',
+        form: 'z',
+        month: 4,
+        year: 2013,
+        pills: 22,
+        reported_date: 200
+    }
+  };
+  transition.onMatch(change, db, audit, function(err, complete) {
+    test.equals(complete, true);
+    test.equals(bulkSave.callCount, 1);
+    test.done();
+  });
 }
--- transitions/update_scheduled_reports.js
+++ transitions/update_scheduled_reports.js
@@ -1,92 +1,6 @@
 var _ = require('underscore'),
     utils = require('../lib/utils'),
-    new_doc;
-
-/**
- * If a record has a month/week/week_number, year and clinic then look for
- * duplicates and update those instead.
- *
- * POST from smssync creates doc
- * GET  /scheduled_reports/:form/:year/:week|month/:clinic_id (look for dups)
- *
- */
-var handleMatch = function(change, db, audit, callback) {
-    var self = module.exports;
-
-    new_doc = change.doc;
-
-    getDuplicates(db, function(err, rows) {
-
-        // only one record in duplicates, mark transition complete
-        if (rows && rows.length === 1) {
-            return callback(null, true);
-        }
-
-        // remove duplicates and replace latest with new_doc
-        var found_target,
-            docs = [];
-
-        _.each(rows, function(row) {
-            var doc = row.doc;
-            if (doc._id === new_doc._id) {
-                doc._deleted = true;
-            } else if (!found_target) {
-                var id = doc._id,
-                    rev = doc._rev;
-                found_target = true;
-                // overwrite data except _rev and _id fields
-                doc = new_doc;
-                doc._id = id;
-                doc._rev = rev;
-            } else {
-                doc._deleted = true;
-            }
-            docs.push(doc);
-        });
-
-        audit.bulkSave(docs, {
-            all_or_nothing: true,
-            docs: docs
-        }, function(err) {
-            // cancels transition and marks as incomplete
-            if (err) {
-                console.error('error doing bulk save', err);
-                return callback(null, false);
-            }
-            callback(null, true);
-        });
-    });
-
-};
-
-//
-// look for duplicate from same year, month/week and reporting unit
-// also includes new_doc
-//
-var getDuplicates = function(db, callback) {
-
-    var doc = new_doc,
-        q = { include_docs: true },
-        view,
-        clinic_id = utils.getClinicID(doc),
-        self = module.exports;
-
-    if (doc.week || doc.week_number) {
-        q.startkey = [doc.form, doc.year, doc.week || doc.week_number, clinic_id];
-        q.endkey = [doc.form, doc.year, doc.week || doc.week_number, clinic_id, {}];
-        view = 'data_records_by_form_year_week_clinic_id_and_reported_date';
-    } else if (doc.month) {
-        q.startkey = [doc.form, doc.year, doc.month, clinic_id];
-        q.endkey = [doc.form, doc.year, doc.month, clinic_id, {}];
-        view = 'data_records_by_form_year_month_clinic_id_and_reported_date';
-    } else {
-        return callback();
-    }
-
-    db.view('kujua-sentinel', view, q, function(err, data) {
-        callback(err, data && data.rows);
-    });
-};
+    logger = require('../lib/logger');
 
 module.exports = {
     filter: function(doc) {
@@ -99,5 +13,82 @@ module.exports = {
             doc.year
         );
     },
-    onMatch: handleMatch
+    /**
+     * If a record has a month/week/week_number, year and clinic then look for
+     * duplicates and update those instead.
+     *
+     * POST from smssync creates doc
+     * GET  /scheduled_reports/:form/:year/:week|month/:clinic_id (look for dups)
+     *
+     */
+    onMatch: function(change, db, audit, callback) {
+        var self = module.exports;
+        self._getDuplicates(db, change.doc, function(err, rows) {
+
+            // only one record in duplicates, mark transition complete
+            if (rows && rows.length === 1) {
+                return callback(null, true);
+            }
+
+            // remove duplicates and replace with latest doc
+            var found_target,
+                docs = [];
+
+            _.each(rows, function(row) {
+                var doc = row.doc;
+                if (doc._id === change.doc._id) {
+                    doc._deleted = true;
+                } else if (!found_target) {
+                    var id = doc._id,
+                        rev = doc._rev;
+                    found_target = true;
+                    // overwrite data except _rev and _id fields
+                    doc = change.doc;
+                    doc._id = id;
+                    doc._rev = rev;
+                } else {
+                    doc._deleted = true;
+                }
+                docs.push(doc);
+            });
+
+            audit.bulkSave(docs, {
+                all_or_nothing: true,
+                docs: docs
+            }, function(err) {
+                // cancels transition and marks as incomplete
+                if (err) {
+                    logger.error('error doing bulk save', err);
+                    return callback(null, false);
+                }
+                callback(null, true);
+            });
+        });
+
+    },
+    //
+    // look for duplicate from same year, month/week and reporting unit
+    // also includes changed doc
+    //
+    _getDuplicates: function(db, doc, callback) {
+        var q = { include_docs: true },
+            view,
+            clinic_id = utils.getClinicID(doc);
+
+        if (doc.week || doc.week_number) {
+            q.startkey = [doc.form, doc.year, doc.week || doc.week_number, clinic_id];
+            q.endkey = [doc.form, doc.year, doc.week || doc.week_number, clinic_id, {}];
+            view = 'data_records_by_form_year_week_clinic_id_and_reported_date';
+        } else if (doc.month || doc.month_num) {
+            q.startkey = [doc.form, doc.year, doc.month || doc.month_num, clinic_id];
+            q.endkey = [doc.form, doc.year, doc.month || doc.month_num, clinic_id, {}];
+            view = 'data_records_by_form_year_month_clinic_id_and_reported_date';
+        } else {
+            return callback();
+        }
+
+        db.view('kujua-sentinel', view, q, function(err, data) {
+            callback(err, data && data.rows);
+        });
+    }
 };
