5ced89525fa3d7e97941b38cd659b1e3609a2546
Alex Anderson <anderson@medicmobile.org>
2015-08-11 19:28:41 +0200

    Minify enketo.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,6 +68,10 @@ module.exports = function(grunt) {
         ],
         dest: 'static/dist/dependencies.js',
       },
+      enketo_js: {
+        src: ['static/enketo/js/*.js'],
+        dest: 'static/dist/enketo.js',
+      },
     },
     uglify: {
       options: {
@@ -78,6 +82,7 @@ module.exports = function(grunt) {
           'static/dist/templates.js': ['static/dist/templates.js'],
           'static/dist/dependencies.js': ['static/dist/dependencies.js'],
           'static/dist/inbox.js': ['static/dist/inbox.js'],
+          'static/dist/enketo.js': ['static/dist/enketo.js'],
         }
       }
     },
@@ -157,7 +162,7 @@ module.exports = function(grunt) {
       enketo: {
         expand:true,
         cwd: 'static/',
-        src: 'enketo/**/*',
+        src: ['enketo/**/*', '!enketo/js/**'],
         dest: 'static/dist/',
       },
     },
@@ -269,7 +274,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-,glify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-karma');
@@ -284,7 +289,8 @@ module.exports = function(grunt) {
   grunt.registerTask('mmjs', 'Build the JS resources', [
     'browserify:dist',
     'ngtemplates',
-    'concat:js'
+    'concat:js',
+    'concat:enketo_js',
   ]);
 
   grunt.registerTask('mmcss', 'Build the CSS resources', [
