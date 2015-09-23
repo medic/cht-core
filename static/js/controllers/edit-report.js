6cdc8b76fa8ce478f416ccd16efed938e528824c
Alex Anderson <anderson@medicmobile.org>
2015-09-23 14:58:54 +0200

    Remove unused code
    Issue: #1260

--- static/js/controllers/edit-report.js
+++ static/js/controllers/edit-report.js
@@ -13,44 +13,6 @@ var modal = require('../modules/modal');
         delete $scope.enketo_report;
       });
 
-      $scope.$root.loadComposer = function() {
-        $('#edit-report [name=facility]').select2('val', null);
-      };
-
-      $scope.$root.loadFormFor = function(doc) {
-        loadForm(doc.form, doc.content, doc._id);
-      };
-
-      $scope.$root.loadXmlFrom = function(formInternalId, content) {
-        $('#create-report').modal('hide');
-        loadForm(formInternalId, content);
-        $('#edit-report').modal('show');
-      };
-
-      var loadForm = function(formInternalId, formInstanceData, docId) {
-        var formWrapper = $('.edit-report-dialog .form-wrapper');
-
-        Enketo.render(formWrapper, formInternalId, formInstanceData)
-          .then(function(form) {
-            if(!formInstanceData) {
-              if($state.includes('contacts') && $state.params.id) {
-                var content = $('<' + form.getModel().getXML().children[0].children[0].children[0].nodeName + '>');
-                $('<g_patient_id>', { text: $state.params.id }).appendTo(content);
-                form.getModel().mergeXml(content[0].outerHTML);
-              }
-            }
-
-            $scope.enketo_report = {
-              formInternalId: formInternalId,
-              docId: docId,
-              formInstance: form
-            };
-          })
-          .catch(function(err) {
-            return console.error('Error loading form.', err);
-          });
-      };
-
       $scope.saveReport = function() {
         var $modal = $('#edit-report');
         var docId = $modal.find('[name=id]').val();
