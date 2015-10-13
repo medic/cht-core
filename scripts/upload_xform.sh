a7ed749ece163f5aad18625a9ec3da7ed7479ce4
Marc Abbyad <marc@medicmobile.org>
2015-10-13 12:41:50 -0400

    Protect against filenames with spaces
    Issue #1433

--- scripts/upload_xform.sh
+++ scripts/upload_xform.sh
@@ -89,8 +89,8 @@ rev=$(jq -r .rev <<< "$revResponse")
 check_rev
 
 # Upload a temp file with the title stripped
-cp $XFORM_PATH $XFORM_PATH.tmp
-sed -i '/<h:title>/d' $XFORM_PATH.tmp
+cp "$XFORM_PATH" "$XFORM_PATH.tmp"
+sed -i '/<h:title>/d' "$XFORM_PATH.tmp"
 
 echo "[$SELF] Uploading form: $ID..."
 revResponse=$(curl -# -f -X PUT -H "Content-Type: text/xml" \
@@ -98,7 +98,7 @@ revResponse=$(curl -# -f -X PUT -H "Content-Type: text/xml" \
     "${docUrl}/xml?rev=${rev}")
 rev=$(jq -r .rev <<< "$revResponse")
 
-rm $XFORM_PATH.tmp
+rm "$XFORM_PATH.tmp"
 
 while [ $# -gt 0 ]; do
     attachment="$1"
