medic/cht-core

commit e91185a45bd5e37ecc34c0f5511f455286d6be7e

Don't overwrite params. Include null_district. Issue: #1031

packages/kujua-sms/kujua-sms/rewrites.js
@@ -55,14 +55,7 @@ exports.rules = [
     },
     {
         from: '/export/forms/:form',
-        to: '_list/export_data_records/data_records',
-        query: {
-            include_docs: 'true',
-            descending: 'true',
-            startkey: [true, ':form', {}],
-            endkey: [true, ':form'],
-            form: ':form'
-        }
+        to: '_list/export_data_records/data_records'
     },
     {
         from: '/:form/data_record/add/clinic/:phone',

lib/shows.js
@@ -66,8 +66,6 @@ function onExportFormsSubmit(ev) {
     if (startVal !== '') {
         // update endkey for startdate as view is descending
         endkey.push(startDate.valueOf());
-    } else {
-        endkey.push(0);
     }
     if (endVal !== '') {
         // update startkey for enddate as view is descending
@@ -519,7 +517,7 @@ function renderDownloadForms() {
         keys = _.keys(titles).sort();
 
         var forms = _.reduce(data.rows, function(memo, row) {
-            var dh_id = row.key[0],
+            var dh_id = row.key[0] || 'null_district',
                 form = row.key[1],
                 dh_name = row.key[2],
                 title = '',
@@ -531,6 +529,8 @@ function renderDownloadForms() {
                 startkey: [true, dh_id, form],
                 endkey: [true, dh_id, form],
                 dh_name: dh_name,
+                include_docs: true,
+                descending: true,
                 tz: tz
             });
 

packages/kujua-sms/kujua-sms/views.js
@@ -10,7 +10,7 @@ exports.data_records = {
         if (doc.type === 'data_record') {
             clinicId = objectpath.get(doc, 'related_entities.clinic._id');
             centerId = objectpath.get(doc, 'related_entities.clinic.parent._id');
-            districtId = objectpath.get(doc, 'related_entities.clinic.parent.parent._id');
+            districtId = objectpath.get(doc, 'related_entities.clinic.parent.parent._id') || 'null_district';
             valid = !doc.errors || doc.errors.length === 0;
 
             emit([doc.reported_date], 1);

commit f6f47c3a9d3d845483716e78697d12a6d69e444f

fixed bug in exporting form data. Issue #1273
This is a regression committed in
https://github.com/medic/medic-webapp/commit/e91185a45bd5e37ecc34c0f5511f455286d6be7e

Still looking to add a test...

packages/kujua-sms/kujua-sms/rewrites.js
@@ -55,7 +55,11 @@ exports.rules = [
     },
     {
         from: '/export/forms/:form',
-        to: '_list/export_data_records/data_records'
+        to: '_list/export_data_records/data_records',
+        query: {
+            include_docs: 'true',
+            descending: 'true'
+        }
     },
     {
         from: '/:form/data_record/add/clinic/:phone',