f934a878844522a9955c84e29ce6f402449edf08
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2015-11-18 11:04:17 +1300

    Include URL in about page. Issue: #1596

--- static/js/controllers/help.js
+++ static/js/controllers/help.js
@@ -8,7 +8,7 @@
     ['$scope',
     function ($scope) {
       $scope.filterModel.type = 'help';
-
+      $scope.url = window.location.hostname;
       $scope.reload = function() {
         window.location.reload(false);
       };
