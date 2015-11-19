9b2f4930cc8bf3a7e7a7f5c5cc6495d7580380e8
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2015-11-20 00:21:13 +1300

    Show the current username in the help page. Issue: #1612

--- static/js/controllers/help.js
+++ static/js/controllers/help.js
@@ -5,10 +5,11 @@
   var inboxControllers = angular.module('inboxControllers');
 
   inboxControllers.controller('HelpCtrl',
-    ['$scope',
-    function ($scope) {
+    ['$scope', 'Session',
+    function ($scope, Session) {
       $scope.filterModel.type = 'help';
       $scope.url = window.location.hostname;
+      $scope.userCtx = Session.userCtx();
       $scope.reload = function() {
         window.location.reload(false);
       };
