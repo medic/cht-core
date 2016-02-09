8dd4b19a5fb2b457f4128ce8ea372d3bb5f18552
Alex Anderson <anderson@medicmobile.org>
2016-02-09 16:31:29 +0100

    Minimise HTML in build process before writing templates.js
    Issue: #1883

--- Gruntfile.js
+++ Gruntfile.js
@@ -300,7 +300,19 @@ module.exports = function(grunt) {
     ngtemplates: {
       inboxApp: {
         src: [ 'templates/modals/**/*.html', 'templates/partials/**/*.html' ],
-        dest: 'static/dist/templates.js'
+        dest: 'static/dist/templates.js',
+        options: {
+          htmlmin: {
+            collapseBooleanAttributes: true,
+            collapseWhitespace: true,
+            removeAttributeQuotes: true,
+            removeComments: true,
+            removeEmptyAttributes: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true
+          }
+        }
       }
     },
     appcache: {
