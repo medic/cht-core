8bb5ada49d170edf5fd0b5dc253df5d453cfafde
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2016-02-12 11:12:14 +1300

    Clean up targets admin page. Issue: #1278

--- static/js/controllers/configuration-targets-edit.js
+++ static/js/controllers/configuration-targets-edit.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 (function () {
 
   'use strict';
@@ -5,18 +7,20 @@
   var inboxControllers = angular.module('inboxControllers');
 
   inboxControllers.controller('ConfigurationTargetsEditCtrl',
-    ['$scope', '$stateParams', '$log', 'Settings', 'UpdateSettings',
-    function ($scope, $stateParams, $log, Settings, UpdateSettings) {
+    ['$scope', '$stateParams', '$state', '$q', '$log', 'Settings', 'UpdateSettings', 'DB',
+    function ($scope, $stateParams, $state, $q, $log, Settings, UpdateSettings, DB) {
 
       $scope.errors = {};
       $scope.status = null;
-      $scope.target = {};
+      $scope.target = { type: 'count' };
       $scope.locales = [];
       $scope.saving = false;
+      $scope.editing = $stateParams.id;
+      $scope.icons = [];
 
       Settings()
         .then(function(settings) {
-          $scope.locales = settings.locales;
+          $scope.locales = _.map(settings.locales, _.clone);
           if ($stateParams.id) {
             $scope.target = _.findWhere(settings.tasks.targets.items, { id: $stateParams.id });
             $scope.target.name.forEach(function(name) {
@@ -30,6 +34,71 @@
         .catch(function(err) {
           $log.error('Error fetching target', err);
         });
+
+      DB.get()
+        .get('resources')
+        .then(function(icons) {
+          $scope.icons = Object.keys(icons.resources);
+        })
+        .catch(function(err) {
+          $log.error('Error fetching resources', err);
+        });
+
+      var removeItem = function(settings) {
+        var items = settings.tasks.targets.items;
+        for (var i = 0; i < items.length; i++) {
+          if (items[i].id === $scope.target.id) {
+            items.splice(i, 1);
+          }
+        }
+        return $q.resolve({ tasks: { targets: { items: items } } });
+      };
+
+      var updateSettings = function(updates) {
+        return $q(function(resolve, reject) {
+          UpdateSettings(updates, function(err) {
+            if (err) {
+              return reject(err);
+            }
+            resolve();
+          });
+        });
+      };
+
+      $scope.delete = function() {
+        if (!$stateParams.id) {
+          return;
+        }
+        $scope.saving = true;
+        Settings()
+          .then(removeItem)
+          .then(updateSettings)
+          .then(function() {
+            $scope.saving = false;
+            $state.go('configuration.targets');
+          })
+          .catch(function(err) {
+            $log.error('Error updating settings', err);
+            $scope.saving = false;
+            $scope.status = 'Save failed';
+          });
+      };
+
+      var updateItem = function(settings) {
+        var items = settings.tasks.targets.items;
+        if ($stateParams.id) {
+          // updating
+          for (var i = 0; i < items.length; i++) {
+            if (items[i].id === $scope.target.id) {
+              items[i] = $scope.target;
+            }
+          }
+        } else {
+          // adding
+          items.push($scope.target);
+        }
+        return $q.resolve({ tasks: { targets: { items: items } } });
+      };
 
       $scope.submit = function() {
 
@@ -69,32 +138,16 @@
         });
 
         Settings()
-          .then(function(settings) {
-            var items = settings.tasks.targets.items;
-            if ($stateParams.id) {
-              // updating
-              for (var i = 0; i < items.length; i++) {
-                if (items[i].id === $scope.target.id) {
-                  items[i] = $scope.target;
-                }
-              }
-            } else {
-              // adding
-              items.push($scope.target);
-            }
-            var updates = { tasks: { targets: { items: items } } };
-            UpdateSettings(updates, function(err) {
-              if (err) {
-                $log.error('Error updating settings', err);
-                $scope.status = 'Save failed';
-              } else {
-                $scope.status = 'Saved';
-              }
-            });
+          .then(updateItem)
+          .then(updateSettings)
+          .then(function() {
+            $scope.saving = false;
+            $scope.status = 'Saved';
           })
           .catch(function(err) {
-            $log.error('Error fetching settings', err);
-            $scope.status = 'error.settings.loading';
+            $log.error('Error updating settings', err);
+            $scope.saving = false;
+            $scope.status = 'Save failed';
           });
       };
 
