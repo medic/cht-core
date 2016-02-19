ecbd8b0698607f9734e4db626fbe46992f405f39
Alex Anderson <anderson@medicmobile.org>
2016-02-19 17:39:59 +0100

    More unit tests for LiveListSrv
    Issue: #1932

--- tests/karma/unit/services/live-list.js
+++ tests/karma/unit/services/live-list.js
@@ -3,7 +3,37 @@ function SIMPLE_ORDER_FUNCTION() {
 }
 
 function SIMPLE_LIST_ITEM(data) {
-  return '* ' + data + '\n';
+  return $('<li>* ' + data._id + '</li>');
+}
+
+function doc(id) {
+  return {
+    _id: id,
+  };
+}
+
+function active_dom() {
+  $('body').append('<ul id="list">');
+}
+
+function no_active_dom() {
+  // No need to do anything - the list should not exist in the DOM already
+}
+
+function active_dom_child_count() {
+  return $('#list').children().length;
+}
+
+function active_dom_child_text() {
+  return _.map($('#list').children(), function(e) {
+    return $(e).text();
+  });
+}
+
+function selected_items_text() {
+  return _.map($('#list li.selected'), function(e) {
+    return $(e).text();
+  });
 }
 
 describe('LiveListSrv', function() {
@@ -11,6 +41,12 @@ describe('LiveListSrv', function() {
 
   var assert = chai.assert,
       service;
+
+  function list_of() {
+    var args = Array.prototype.slice.call(arguments);
+    var items = _.map(args, doc);
+    service.testing.set(items);
+  }
 
   beforeEach(function() {
     module('inboxApp');
@@ -93,5 +129,283 @@ describe('LiveListSrv', function() {
       'setSelected',
       'clearSelected',
     ]);
+  });
+
+  describe('a configured list', function() {
+    beforeEach(function() {
+      var config = {
+        listItem: SIMPLE_LIST_ITEM,
+        orderBy: SIMPLE_ORDER_FUNCTION,
+        selecter: '#list',
+      };
+      service.$listFor('testing', config);
+    });
+
+    it('should not be initialised by default', function() {
+      // expect
+      assert.notOk(service.testing.initialised());
+    });
+
+    describe('#refresh()', function() {
+        it('should not complain if list is not initialised', function() {
+          // expect
+          service.testing.refresh();
+          // no expcetion thrown
+        });
+    });
+
+    describe('an initialised list', function() {
+      beforeEach(function() {
+        service.testing.set([]);
+      });
+
+      it('should be initialised', function() {
+        // expect
+        assert.ok(service.testing.initialised());
+      });
+
+      describe('#count()', function() {
+        it('should return zero for an empty list', function() {
+          // expect
+          assert.equal(service.testing.count(), 0);
+        });
+
+        it('should return 3 for an 3-length list', function() {
+          // given
+          service.testing.set([1, 2, 3]);
+
+          // expect
+          assert.equal(service.testing.count(), 3);
+        });
+      });
+
+      describe('#refresh()', function() {
+        it('should do nothing if DOM container element not found', function() {
+          // given
+          list_of(1, 2, 3);
+          no_active_dom();
+
+          // when
+          service.testing.refresh();
+
+          // then
+          assert.deepEqual(active_dom_child_count(), 0);
+        });
+
+        it('should append all DOM elements to the DOM container element', function() {
+          // given
+          active_dom();
+          list_of(1, 2, 3);
+
+          // when
+          service.testing.refresh();
+
+          // then
+          assert.deepEqual(active_dom_child_text(), [
+            '* 1',
+            '* 2',
+            '* 3',
+          ]);
+        });
+      });
+
+      describe('selected item tracking', function() {
+        describe('#setSelected()', function() {
+          it('should add `selected` class for new selected element', function() {
+            // given
+            list_of(1, 2, 3);
+            service.testing.setSelected(2);
+            active_dom();
+
+            // when
+            service.testing.refresh();
+
+            // then
+            assert.deepEqual(selected_items_text(), [
+              '* 2',
+            ]);
+          });
+
+          it('should add `selected` class for existing element that becomes selected', function() {
+            // given
+            list_of(1, 2, 3);
+            active_dom();
+            service.testing.refresh();
+            assert.deepEqual(selected_items_text(), []);
+
+            // when
+            service.testing.setSelected(2);
+
+            // then
+            assert.deepEqual(selected_items_text(), [
+              '* 2',
+            ]);
+          });
+
+          it('should remove `selected` class for previous selected element and add to newly-selected', function() {
+            // given
+            list_of(1, 2, 3);
+            service.testing.setSelected(2);
+            active_dom();
+            service.testing.refresh();
+            assert.deepEqual(selected_items_text(), [
+              '* 2',
+            ]);
+
+            // when
+            service.testing.setSelected(3);
+
+            // then
+            assert.deepEqual(selected_items_text(), [
+              '* 3',
+            ]);
+          });
+        });
+
+        describe('#clearSelected()', function() {
+          it('should not complain if nothing was selected', function() {
+            // given
+            list_of(1, 2, 3);
+            active_dom();
+            service.testing.refresh();
+            assert.deepEqual(selected_items_text(), []);
+
+            // when
+            service.testing.clearSelected();
+
+            // then
+            assert.deepEqual(selected_items_text(), []);
+          });
+
+          it('should remove `selected` class for previous selected element and add to newly-selected', function() {
+            // given
+            list_of(1, 2, 3);
+            service.testing.setSelected(2);
+            active_dom();
+            service.testing.refresh();
+            assert.deepEqual(selected_items_text(), [
+              '* 2',
+            ]);
+
+            // when
+            service.testing.clearSelected();
+
+            // then
+            assert.deepEqual(selected_items_text(), []);
+          });
+        });
+      });
+
+      describe('#count()', function() {
+        it('should return the number of items passed to `set`', function() {
+          // given
+          service.testing.set([doc(1), doc(2)]);
+
+          // expect
+          assert.equal(service.testing.count(), 2);
+        });
+
+        it('should increment when item inserted', function() {
+          // given
+          service.testing.set([doc(1), doc(2)]);
+          assert.equal(service.testing.count(), 2);
+
+          // when
+          service.testing.insert(doc(3));
+
+          // expect
+          assert.equal(service.testing.count(), 3);
+        });
+
+        it('should not increment when item updated', function() {
+          // given
+          service.testing.set([doc(1), doc(2)]);
+          assert.equal(service.testing.count(), 2);
+
+          // when
+          service.testing.update(doc(2));
+
+          // expect
+          assert.equal(service.testing.count(), 2);
+        });
+      });
+
+      describe('#contains()', function() {
+        it('should return false for an empty list', function() {
+          // expect
+          assert.notOk(service.testing.contains( doc(123) ));
+        });
+
+        it('should return false if supplied item\'s ID does not match an item in the list', function() {
+          // given
+          list_of(1, 2, 3);
+
+          // expect
+          assert.notOk(service.testing.contains( doc(999) ));
+        });
+
+        it('true if supplied ID matches an item in the list', function() {
+          // given
+          list_of(1, 2, 3);
+
+          // expect
+          assert.ok(service.testing.contains( doc(1) ));
+          assert.ok(service.testing.contains( doc(2) ));
+          assert.ok(service.testing.contains( doc(3) ));
+        });
+      });
+
+      describe('#getList()', function() {
+        it('should return an empty list if no items added', function() {
+          // expect
+          assert.deepEqual(service.testing.getList(), []);
+        });
+
+        it('should return all items passed to set', function() {
+          // given
+          service.testing.set([ doc(1), doc(2) ]);
+
+          // expect
+          assert.deepEqual(service.testing.getList(), [ doc(1), doc(2) ]);
+        });
+
+        it('should return all items inserted', function() {
+          // given
+          service.testing.insert( doc(1) );
+          service.testing.insert( doc(2) );
+
+          // expect
+          assert.deepEqual(service.testing.getList(), [ doc(1), doc(2)  ]);
+        });
+
+        it('should return all items passed to set or inserted', function() {
+          // given
+          service.testing.set([ doc(1), doc(2) ]);
+          service.testing.insert( doc(3) );
+
+          // expect
+          assert.deepEqual(service.testing.getList(), [ doc(1), doc(2), doc(3) ]);
+        });
+
+        it('should not return removed items', function() {
+          // given
+          list_of(1, 2, 3);
+          assert.deepEqual(service.testing.getList(), [
+            { _id: 1 },
+            { _id: 2 },
+            { _id: 3 },
+          ]);
+
+          // when
+          service.testing.remove(doc(2));
+
+          // then
+          assert.deepEqual(service.testing.getList(), [
+            { _id: 1 },
+            { _id: 3 },
+          ]);
+        });
+      });
+    });
   });
 });
