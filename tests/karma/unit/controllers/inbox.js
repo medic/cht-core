5e29cd9eabae4fa74cf921ab0ac37e4f6311a661
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2016-02-24 13:04:38 +1300

    Pull Changes feed into XmlForms service. Issue: #1925

--- tests/karma/unit/controllers/inbox.js
+++ tests/karma/unit/controllers/inbox.js
@@ -63,7 +63,7 @@ describe('InboxCtrl controller', function() {
         return sinon.stub();
       });
       $provide.factory('XmlForms', function() {
-        return KarmaUtils.nullPromise();
+        return sinon.stub();
       });
       $provide.factory('Facility', function() {
         return sinon.stub();
--- tests/karma/unit/services/xml-forms.js
+++ tests/karma/unit/services/xml-forms.js
@@ -1,9 +1,9 @@
 describe('XmlForms service', function() {
   'use strict';
 
-  var service,
-      $rootScope,
-      dbQuery = sinon.stub();
+  var $injector,
+      dbQuery = sinon.stub(),
+      Changes = sinon.stub();
 
   var mockEnketoDoc = function(formInternalId, docId) {
     return {
@@ -22,39 +22,77 @@ describe('XmlForms service', function() {
   beforeEach(function() {
     module('inboxApp');
     module(function($provide) {
-      $provide.factory('DB', KarmaUtils.mockDB({ query: dbQuery }));
+      $provide.factory('DB', KarmaUtils.mockDB({
+        query: dbQuery
+      }));
+      $provide.factory('Changes', function() {
+        return Changes;
+      });
+      $provide.value('$q', Q); // bypass $q so we don't have to digest
     });
-    inject(function(_XmlForms_, _$rootScope_) {
-      service = _XmlForms_;
-      $rootScope = _$rootScope_;
+    inject(function(_$injector_) {
+      $injector = _$injector_;
     });
   });
 
   afterEach(function() {
-    KarmaUtils.restore(dbQuery);
+    KarmaUtils.restore(dbQuery, Changes);
   });
 
   it('should get all forms from DB, but only pass on ones with XML attachment', function(done) {
-    // given
     var expected = [
-      mockEnketoDoc(),
+      mockEnketoDoc('assessment'),
       mockJsonDoc(),
       mockJsonDoc(),
-      mockEnketoDoc(),
-      mockEnketoDoc(),
+      mockEnketoDoc('referral'),
+      mockEnketoDoc('registration'),
     ];
     dbQuery.returns(KarmaUtils.mockPromise(null, { rows: expected }));
-    service()
-      .then(function(actual) {
-        chai.expect(actual.length).to.equal(3);
-        chai.expect(actual[0]).to.deep.equal(expected[0].doc);
-        chai.expect(actual[1]).to.deep.equal(expected[3].doc);
-        chai.expect(actual[2]).to.deep.equal(expected[4].doc);
-        done();
-      })
-      .catch(done);
-    setTimeout(function() {
-      $rootScope.$apply();
+    $injector.get('XmlForms')('test', function(err, actual) {
+      chai.expect(err).to.equal(null);
+      chai.expect(actual.length).to.equal(3);
+      chai.expect(actual[0]).to.deep.equal(expected[0].doc);
+      chai.expect(actual[1]).to.deep.equal(expected[3].doc);
+      chai.expect(actual[2]).to.deep.equal(expected[4].doc);
+      done();
     });
   });
+
+  it('returns errors from db.query', function(done) {
+    dbQuery.returns(KarmaUtils.mockPromise('boom'));
+    $injector.get('XmlForms')('test', function(err) {
+      chai.expect(err).to.equal('boom');
+      done();
+    });
+  });
+
+  it('is updated when changes fires', function(done) {
+    var original = mockEnketoDoc('registration');
+    var update = mockEnketoDoc('visit');
+    dbQuery
+      .onFirstCall().returns(KarmaUtils.mockPromise(null, { rows: [ original ] }))
+      .onSecondCall().returns(KarmaUtils.mockPromise(null, { rows: [ original, update ] }));
+    var count = 0;
+    $injector.get('XmlForms')('test', function(err, actual) {
+      chai.expect(err).to.equal(null);
+      if (count === 0) {
+        chai.expect(actual.length).to.equal(1);
+        chai.expect(actual[0]).to.deep.equal(original.doc);
+        setTimeout(function() {
+          Changes.args[0][0].callback();
+        });
+      } else if (count === 1) {
+        chai.expect(actual.length).to.equal(2);
+        chai.expect(actual[0]).to.deep.equal(original.doc);
+        chai.expect(actual[1]).to.deep.equal(update.doc);
+        chai.expect(Changes.callCount).to.equal(1);
+        chai.expect(dbQuery.callCount).to.equal(2);
+        done();
+      } else {
+        done('Update fired too many times!');
+      }
+      count++;
+    });
+  });
+
 });
