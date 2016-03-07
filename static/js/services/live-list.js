f6914544110eb6bc3043966bab6f343704043f97
Alex Anderson <anderson@medicmobile.org>
2016-03-07 16:22:21 +0300

    Hide initial replication and db warm up behind loader
    Warm up of Tasks and Contacts views, and initial DB sync now hidden behind
    loader animation.
    
    Issue: #1270

--- static/js/services/live-list.js
+++ static/js/services/live-list.js
@@ -123,7 +123,7 @@ angular.module('inboxServices').factory('LiveListConfig', [
 
       LiveList.tasks.set([]);
 
-      TaskGenerator('tasks-list', 'task', function(err, tasks) {
+      TaskGenerator.listen('tasks-list', 'task', function(err, tasks) {
         if (err) {
           $log.error('Error getting tasks', err);
 
