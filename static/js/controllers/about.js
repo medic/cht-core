0ca4082bcdba78b3f5e1802b51e91da084449bb9
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2016-03-16 10:19:58 +1300

    Refactor about page.
    span cannot be child of ul. Use translate directive. Units must be translated too.

--- static/js/controllers/about.js
+++ static/js/controllers/about.js
@@ -25,10 +25,10 @@ inboxControllers.controller('AboutCtrl',
     $scope.$watch('enableDebugModel.val', Debug.set);
 
     if (window.medicmobile_android && window.medicmobile_android.getDataUsage) {
-      $scope.android_data_usage = JSON.parse(window.medicmobile_android.getDataUsage());
+      $scope.androidDataUsage = JSON.parse(window.medicmobile_android.getDataUsage());
 
       var dataUsageUpdate = $interval(function() {
-        $scope.android_data_usage = JSON.parse(window.medicmobile_android.getDataUsage());
+        $scope.androidDataUsage = JSON.parse(window.medicmobile_android.getDataUsage());
       }, 2000);
 
       $scope.$on('$destroy', function() {
