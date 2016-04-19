311cff0aa57b7b0ee7baddffdb6c4a5aed28dfac
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2016-04-19 22:02:37 +1200

    Setting value has changed in select2 v4. Issue: #2217

--- static/js/controllers/configuration-settings-basic.js
+++ static/js/controllers/configuration-settings-basic.js
@@ -93,7 +93,7 @@ var _ = require('underscore'),
             return !!locale.disabled;
           });
           $('#default-country-code').select2({ width: '20em', data: countries.list });
-          $('#default-country-code').select2('val', res.default_country_code);
+          $('#default-country-code').val(res.default_country_code).trigger('change');
         })
         .catch(function(err) {
           console.log('Error loading settings', err);
