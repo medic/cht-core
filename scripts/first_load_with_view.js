4e632b1fda17fba89095f430262b26ec4dca7f0a
Estelle Comment <estelle.comment@gmail.com>
2016-05-17 12:17:52 +0200

    First load with view test : seq is saved from one replication to the next. #2284

--- scripts/first_load_with_view.js
+++ scripts/first_load_with_view.js
@@ -15,20 +15,53 @@ var printInfoLocal = function() {
       });
 };
 
-var printInfoRemote = function() {
-  return remoteDb.info()
-      .then(function(info) {
-        console.log(info.db_name, info.doc_count);
-      });
+var runReplication = function(options) {
+  console.log('\n\n\nREPLICATION START - options', options);
+  return PouchDB.replicate(remoteDb, localDb, options)
+    .on('change', function (info) {
+      // handle change
+      console.info('change', info);
+    }).on('paused', function (err) {
+      // replication paused (e.g. replication up to date, user went offline)
+      console.info('paused', err);
+    }).on('active', function () {
+      // replicate resumed (e.g. new changes replicating, user went back online)
+      console.info('active');
+    }).on('denied', function (err) {
+      // a document failed to replicate (e.g. due to permissions)
+      console.info('paused', err);
+    }).on('complete', function (info) {
+      // handle complete
+      console.info('complete', info);
+    }).on('error', function (err) {
+      // handle error
+      console.info('error', err);
+    })
+    .then(function(){
+      console.log('\n\n\nREPLICATION STOP');
+      // Print out all docs, local
+      return localDb.allDocs()
+        .then(function(data) {
+          console.log(data.rows);
+        });
+    });
 };
 
+var remoteSeq = -1;
 localDb.destroy()
   .then(function () {
     console.log('Wiped local db.');
     localDb = new PouchDB('basicLocal');
     return printInfoLocal();
   })
-  .then(printInfoRemote)
+  .then(function() {
+    return remoteDb.info()
+        .then(function(info) {
+          console.log(info.db_name, info.doc_count);
+          remoteSeq = info.update_seq;
+          console.log('remoteSeq', remoteSeq);
+        });
+  })
   // Fetch docs with a view.
   .then(function() {
 		return remoteDb.query('example/testview')
@@ -59,35 +92,12 @@ localDb.destroy()
 
   // Start replication.
   .then(function(){
-    console.log('\n\n\nREPLICATION START');
-    return PouchDB.replicate(remoteDb, localDb)
-      .on('change', function (info) {
-        // handle change
-        console.info('change', info);
-      }).on('paused', function (err) {
-        // replication paused (e.g. replication up to date, user went offline)
-        console.info('paused', err);
-      }).on('active', function () {
-        // replicate resumed (e.g. new changes replicating, user went back online)
-        console.info('active');
-      }).on('denied', function (err) {
-        // a document failed to replicate (e.g. due to permissions)
-        console.info('paused', err);
-      }).on('complete', function (info) {
-        // handle complete
-        console.info('complete', info);
-      }).on('error', function (err) {
-        // handle error
-        console.info('error', err);
-      });
-	})
+    return runReplication({since: remoteSeq});
+  })
+
+  // Rerun replication, without seq number.
   .then(function(){
-    console.log('\n\n\nREPLICATION STOP');
-    // Print out all docs, local
-    return localDb.allDocs()
-      .then(function(data) {
-        console.log(data.rows);
-      });
+    return runReplication({});
   })
   .catch(function(err) {
 		console.log(err);
