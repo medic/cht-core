0bd9957335fe9560881d22e05630dee96e527a0f
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2016-06-08 17:00:27 +1200

    Don't audit _local docs. Issue: #76

--- audit-proxy.js
+++ audit-proxy.js
@@ -1,6 +1,7 @@
 var EventEmitter = require('events').EventEmitter,
     util = require('util'),
     passStream = require('pass-stream'),
+    _ = require('underscore'),
     auth = require('./auth'),
     db = require('./db'),
     couchdbAudit = require('couchdb-audit');
@@ -46,22 +47,24 @@ AuditProxy.prototype.audit = function(proxy, req, res) {
       cb();
     };
 
-    var endFn = function(cb) {
-      var self = this;
-      var options = { buffer: buffer };
-      var doc = parse(dataBuffer.join(''));
+    var audit = function(doc, cb) {
       if (!doc) {
-        // ignore non-json requests
-        proxy.web(req, res, options);
-        dataBuffer.forEach(function(item) {
-          self.push(item);
-        });
-        return cb();
+        // don't audit non-json requests
+        return cb(null, dataBuffer);
+      }
+
+      var docs = doc.docs || [ doc ];
+      docs = _.reject(docs, function(doc) {
+        return doc._id && doc._id.indexOf('_local/') === 0;
+      });
+      if (!docs.length) {
+        // don't audit requests without auditable docs
+        return cb(null, dataBuffer);
       }
 
       couchdbAudit
         .withNano(db, db.settings.db, db.settings.auditDb, db.settings.ddoc, ctx.user)
-        .log(doc.docs || [doc], function(err) {
+        .log(docs, function(err) {
           if (err) {
             return cb(err);
           }
@@ -69,10 +72,23 @@ AuditProxy.prototype.audit = function(proxy, req, res) {
           // audit might modify the doc (eg: generating an id)
           // so we need to update the content-length header
           req.headers['content-length'] = Buffer.byteLength(data);
-          proxy.web(req, res, options);
-          self.push(data);
-          return cb();
+          cb(null, [ data ]);
         });
+    };
+
+    var endFn = function(cb) {
+      var self = this;
+      var options = { buffer: buffer };
+      var doc = parse(dataBuffer.join(''));
+      audit(doc, function(err, data) {
+        if (err) {
+          return cb(err);
+        }
+        // proxy the request through to the app as if nothing happened
+        proxy.web(req, res, options);
+        data.forEach(self.push);
+        return cb();
+      });
     };
 
     var ps = passStream(writeFn, endFn);
--- tests/audit-proxy.js
+++ tests/audit-proxy.js
@@ -105,6 +105,118 @@ exports['audit does not audit non json request'] = function(test) {
   test.done();
 };
 
+exports['audit does not audit _local docs'] = function(test) {
+  test.expect(1);
+  var target = 'http://localhost:4444';
+  var username = 'steve';
+  var doc = JSON.stringify({
+    docs: [{
+      _id: '_local/9SJryf.JbGfRFek5agknAw==',
+      _rev: '0-21',
+      session_id: '2EC7B446-9868-5F5C-9B8B-6FE6C2DC6111',
+      history: [ { last_seq: 2221, session_id: '2EC7B446-9868-5F5C-9B8B-6FE6C2DC6111' } ]
+    }],
+    new_edits: true
+  });
+  var proxy = {
+    web: function() {}
+  };
+  var passStreamFn = function(writeFn, endFn) {
+    writeFn(doc, 'UTF-8', function() {});
+    endFn.call({push: function(body) {
+      test.equals(body, doc);
+    }}, function() {});
+  };
+  var req = {
+    pipe: function() {
+      return { on: function() {} };
+    }
+  };
+  var auth = {
+    check: function(req, permission, district, cb) {
+      cb(null, { user: username });
+    }
+  };
+
+  var p = new AuditProxy();
+  p.setup({ passStream: passStreamFn, auth: auth });
+  p.audit(proxy, req, {}, target);
+  test.done();
+};
+
+
+exports['audit audits the non _local docs'] = function(test) {
+  test.expect(8);
+  var target = 'http://localhost:4444';
+  var username = 'steve';
+  var doc = {
+    _id: 'abc',
+    first: 'one',
+    second: 'two'
+  };
+  var localDoc = {
+    _id: '_local/9SJryf.JbGfRFek5agknAw=='
+  };
+  var docs = { docs: [ localDoc, doc ] };
+  var audit = {
+    withNano: function(db, _db, _auditDb, _ddoc, _username) {
+      test.same(username, _username);
+      test.same('medic', _db);
+      test.same('medic-audit', _auditDb);
+      test.same('medic', _ddoc);
+      return {
+        log: function(docs, _cb) {
+          // only audit the proper doc
+          test.equals(docs.length, 1);
+          test.same(docs[0], doc);
+          _cb();
+        }
+      };
+    }
+  };
+  var proxy = {
+    web: function(req) {
+      test.equals(
+        Buffer.byteLength(JSON.stringify(docs)),
+        req.headers['content-length']
+      );
+    }
+  };
+  var passStreamFn = function(writeFn, endFn) {
+    writeFn(JSON.stringify(docs), 'UTF-8', function() {});
+    endFn.call({push: function(body) {
+      // pass through both local and proper
+      test.equals(body, JSON.stringify(docs));
+    }}, function() {});
+  };
+  var req = {
+    headers: {},
+    pipe: function() {
+      return { on: function() {} };
+    }
+  };
+  var db = {
+    client: {
+      host: 'localhost',
+      port: 5984
+    },
+    settings: {
+      db: 'medic',
+      auditDb: 'medic-audit',
+      ddoc: 'medic'
+    }
+  };
+  var auth = {
+    check: function(req, permission, district, cb) {
+      cb(null, { user: username });
+    }
+  };
+  var p = new AuditProxy();
+  p.setup({ audit: audit, passStream: passStreamFn, db: db, auth: auth });
+  p.audit(proxy, req, {}, target);
+  test.done();
+};
+
 exports['audit emits error when not authorized'] = function(test) {
   test.expect(0);
   var target = 'http://localhost:4444';
