15a0b3386777bb013d9ed63900e85179060eae9c
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2016-07-05 10:23:36 +1200

    Attempt to fix flakey unit tests. Issue: #2453

--- tests/karma/unit/controllers/tasks-content.js
+++ tests/karma/unit/controllers/tasks-content.js
@@ -1,96 +1,87 @@
 describe('TasksContentCtrl', function() {
   beforeEach(module('inboxApp'));
 
-  var $controller;
+  var $scope,
+      task,
+      watchCallback,
+      createController,
+      render;
 
-  beforeEach(inject(function(_$controller_){
-    $controller = _$controller_;
-  }));
-
-  describe('$scope.performAction', function() {
-    var $scope,
-        task,
-        watchCallback;
-
-    var Enketo = {
-      render: sinon.stub()
+  beforeEach(function() {
+    render = sinon.stub();
+    $scope = {
+      $on: function() {},
+      $watch: function(prop, cb) {
+        watchCallback = cb;
+      },
+      setCancelTarget: function() {},
+      setSelected: function() {
+        $scope.selected = task;
+      }
     };
-
-    var createController = function() {
-      $controller('TasksContentCtrl', {
-        $scope: $scope,
-        Enketo: Enketo,
-        DB: sinon.stub(),
-        WatchDesignDoc: sinon.stub()
-      });
-    };
-
-    beforeEach(function() {
-      $scope = {
-        $on: function() {},
-        $watch: function(prop, cb) {
-          watchCallback = cb;
-        },
-        setCancelTarget: function() {},
-        setSelected: function() {
-          $scope.selected = task;
-        }
+    render.returns(KarmaUtils.mockPromise());
+    inject(function($controller) {
+      createController = function() {
+        $controller('TasksContentCtrl', {
+          $scope: $scope,
+          Enketo: { render: render },
+          DB: sinon.stub(),
+          WatchDesignDoc: sinon.stub()
+        });
       };
-      Enketo.render.returns(KarmaUtils.mockPromise());
     });
-
-    afterEach(function() {
-      KarmaUtils.restore(Enketo.render);
-    });
-
-    it('loads form when task has one action and no fields', function() {
-      task = {
-        actions: [{
-          type: 'report',
-          form: 'A'
-        }]
-      };
-      createController();
-      watchCallback();
-      chai.expect($scope.formId).to.equal('A');
-      chai.expect($scope.loadingForm).to.equal(true);
-      chai.expect(Enketo.render.callCount).to.equal(1);
-      chai.expect(Enketo.render.getCall(0).args.length).to.equal(3);
-    });
-
-    it('does not load form when task has more than one action', function() {
-      task = {
-        actions: [{}, {}] // two forms
-      };
-      createController();
-      chai.expect($scope.formId).to.equal(null);
-      chai.expect($scope.loadingForm).to.equal(undefined);
-      chai.expect(Enketo.render.callCount).to.equal(0);
-    });
-
-    it('does not load form when task has fields (e.g. description)', function() {
-      task = {
-        actions: [{
-          type: 'report',
-          form: 'B'
-        }],
-        fields: [{
-          label: [{
-            content: 'Description',
-            locale: 'en'
-          }],
-          value: [{
-            content: '{{contact.name}} survey due',
-            locale: 'en'
-          }]
-        }]
-      };
-      createController();
-      chai.expect($scope.formId).to.equal(null);
-      chai.expect($scope.loadingForm).to.equal(undefined);
-      chai.expect(Enketo.render.callCount).to.equal(0);
-    });
-
   });
-});
 
+  afterEach(function() {
+    KarmaUtils.restore(render);
+  });
+
+  it('loads form when task has one action and no fields', function() {
+    task = {
+      actions: [{
+        type: 'report',
+        form: 'A'
+      }]
+    };
+    createController();
+    watchCallback();
+    chai.expect($scope.formId).to.equal('A');
+    chai.expect($scope.loadingForm).to.equal(true);
+    chai.expect(render.callCount).to.equal(1);
+    chai.expect(render.getCall(0).args.length).to.equal(3);
+  });
+
+  it('does not load form when task has more than one action', function() {
+    task = {
+      actions: [{}, {}] // two forms
+    };
+    createController();
+    chai.expect($scope.formId).to.equal(null);
+    chai.expect($scope.loadingForm).to.equal(undefined);
+    chai.expect(render.callCount).to.equal(0);
+  });
+
+  it('does not load form when task has fields (e.g. description)', function() {
+    task = {
+      actions: [{
+        type: 'report',
+        form: 'B'
+      }],
+      fields: [{
+        label: [{
+          content: 'Description',
+          locale: 'en'
+        }],
+        value: [{
+          content: '{{contact.name}} survey due',
+          locale: 'en'
+        }]
+      }]
+    };
+    createController();
+    chai.expect($scope.formId).to.equal(null);
+    chai.expect($scope.loadingForm).to.equal(undefined);
+    chai.expect(render.callCount).to.equal(0);
+  });
+
+});
