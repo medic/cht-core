aeb4399c62593168266725e194566c3f154fd8a3
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2016-07-07 23:46:48 +1200

    Figured out how to test the views again. Issue: #2206

--- tests/nodeunit/unit/kujua-sms/views.js
+++ tests/nodeunit/unit/kujua-sms/views.js
@@ -1,5 +1,7 @@
-var views = require('../../../../packages/kujua-sms/kujua-sms/views');
+var views = require('../../../../packages/kujua-sms/kujua-sms/views'),
+    clientViews = require('../../../../ddocs/compiled.json');
 
+eval('var data_records_read_by_type = ' + clientViews.docs[0].views.data_records_read_by_type.map);
 var results;
 
 exports.setUp = function (callback) {
@@ -25,5 +27,103 @@ exports['views exist'] = function (test) {
   test.strictEqual(typeof views.clinic_by_phone, 'object');
   test.strictEqual(typeof views.clinic_by_refid, 'object');
   test.strictEqual(typeof views.tasks_pending, 'object');
+  test.done();
+};
+
+exports['data_records_read_by_type map emits nothing when not data_record'] = function (test) {
+  var doc = {
+    type: 'facility'
+  };
+  data_records_read_by_type(doc)
+  test.same(results.length, 0);
+  test.done();
+};
+
+exports['data_records_read_by_type map emits empty dh id when no facility'] = function (test) {
+  var doc = {
+    type: 'data_record'
+  };
+  data_records_read_by_type(doc);
+  test.same(results.length, 1);
+  test.same(results[0].key[0], '_total');
+  test.same(results[0].key[1], 'messages');
+  test.same(results[0].key[2], undefined);
+  test.done();
+};
+
+exports['data_records_read_by_type map emits one record per task'] = function (test) {
+  var doc = {
+    type: 'data_record',
+    tasks: [
+      { 
+        messages: [
+         { contact: { parent: { parent: { _id: 'a', type: 'district_hospital' } } } }
+        ]
+      }, { 
+        messages: [
+         { contact: { parent: { parent: { _id: 'b', type: 'district_hospital' } } } }
+        ]
+      }
+    ]
+  };
+  data_records_read_by_type(doc);
+  test.same(results.length, 2);
+  test.same(results[0].key[0], '_total');
+  test.same(results[0].key[1], 'messages');
+  test.same(results[0].key[2], 'a');
+  test.same(results[1].key[0], '_total');
+  test.same(results[1].key[1], 'messages');
+  test.same(results[1].key[2], 'b');
+  test.done();
+};
+
+exports['data_records_read_by_type map emits dh id'] = function (test) {
+  var doc = {
+    type: 'data_record',
+    form: 'ZYX',
+    contact: { parent: { parent: { _id: 'abc', type: 'district_hospital' } } }
+  };
+  data_records_read_by_type(doc);
+  test.same(results.length, 1);
+  test.same(results[0].key[0], '_total');
+  test.same(results[0].key[1], 'forms');
+  test.same(results[0].key[2], 'abc');
+  test.done();
+};
+
+exports['data_records_read_by_type map emits no read when empty array'] = function (test) {
+  var doc = {
+    type: 'data_record',
+    read: []
+  };
+  data_records_read_by_type(doc);
+  test.same(results.length, 1);
+  test.same(results[0].key[0], '_total');
+  test.same(results[0].key[1], 'messages');
+  test.same(results[0].key[2], undefined);
+  test.done();
+};
+
+exports['data_records_read_by_type map emits read when populated array'] = function (test) {
+  var doc = {
+    type: 'data_record',
+    read: ['gareth','milan','dave'],
+    form: 'ZYX',
+    contact: { parent: { parent: { _id: 'abc', type: 'district_hospital' } } }
+  };
+  data_records_read_by_type(doc);
+  test.same(results.length, 4);
+  test.same(results[0].key[0], '_total');
+  test.same(results[0].key[1], 'forms');
+  test.same(results[0].key[2], 'abc');
+  test.same(results[1].key[0], 'gareth');
+  test.same(results[1].key[1], 'forms');
+  test.same(results[1].key[2], 'abc');
+  test.same(results[2].key[0], 'milan');
+  test.same(results[2].key[1], 'forms');
+  test.same(results[2].key[2], 'abc');
+  test.same(results[3].key[0], 'dave');
+  test.same(results[3].key[1], 'forms');
+  test.same(results[3].key[2], 'abc');
   test.done();
 };
