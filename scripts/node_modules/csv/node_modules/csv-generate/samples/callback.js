5a3be2927e3ea486b910aebfa2d6669f79d9554f
Alex Anderson <anderson@medicmobile.org>
2016-07-13 12:33:28 +0200

    Remove scripts/node_modules

--- scripts/node_modules/csv/node_modules/csv-generate/samples/callback.js
+++
@@ -1,7 +0,0 @@
-
-should = require('should');
-generate = require('../lib');
-
-generate({seed: 1, columns: 2, length: 2}, function(err, output){
-  output.should.eql('OMH,ONKCHhJmjadoA\nD,GeACHiN');
-});
