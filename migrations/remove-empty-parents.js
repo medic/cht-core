98e1d428a1767258ece3a37bb278a6b8d9956921
Alex Anderson <anderson@medicmobile.org>
2016-08-10 13:45:06 +0200

    Add migration to remove empty parents from contacts
    Issue: medic/medic-webapp#2436

---
+++ migrations/remove-empty-parents.js
@@ -0,0 +1,34 @@
+var _ = require('underscore'),
+    async = require('async'),
+    db = require('../db');
+
+function removeDeadParents(row, callback) {
+  if(row.doc.parent && !_.isEmpty(row.doc.parent)) {
+    return callback();
+  }
+  db.medic.get(row.id, function(err, doc) {
+    if(err) {
+      return callback(err);
+    }
+    delete doc.parent;
+    db.medic.insert(callback);
+  });
+}
+
+module.exports = {
+  name: 'remove-empty-parents',
+  created: new Date(2016, 7, 10, 13, 37, 0, 0),
+  run: function(callback) {
+    // pull full list of contacts, and remove dead parents
+    db.medic.view(
+      'medic-client',
+      'contacts_by_type',
+      { include_docs:true },
+      function(err, result) {
+        if (err) {
+          return callback(err);
+        }
+        async.each(result.rows, removeDeadParents, callback);
+      });
+  }
+};
