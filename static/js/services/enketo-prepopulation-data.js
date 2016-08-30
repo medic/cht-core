91b12c341e1b83d54171d0786af37c9c650b9455
Stefan du Fresne <stefan@sdufresne.info>
2016-08-30 13:52:41 +0100

    Added a clear error when not on Android #2670
    This error mimics the style of errors we should get from the wrapper.
    If forms are changed to accomodate this data structure all reports
    should either have GPS coords stored or the error string explaining
    why.

--- static/js/services/enketo-prepopulation-data.js
+++ static/js/services/enketo-prepopulation-data.js
@@ -34,8 +34,14 @@ angular.module('inboxServices').service('EnketoPrepopulationData',
             EnketoTranslation.bindJsonToXml(userRoot, user);
           }
 
-          if (locationRoot.length && $window.medicmobile_android) {
-            var location = JSON.parse($window.medicmobile_android.getLocation());
+          if (locationRoot.length) {
+            var location;
+            if ($window.medicmobile_android) {
+              location = JSON.parse($window.medicmobile_android.getLocation());
+            } else {
+              location = {error: true, message: 'Not on android'};
+            }
+
             EnketoTranslation.bindJsonToXml(bindRoot.find('inputs>meta>location'), location);
           }
 
