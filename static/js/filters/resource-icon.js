046654158fc5e9b03af1ea8ec8ed03c48aacab32
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2016-10-13 11:18:53 +1300

    Refactor to new best practice.

--- static/js/filters/resource-icon.js
+++ static/js/filters/resource-icon.js
@@ -1,23 +1,15 @@
-(function () {
-
-  'use strict';
-
-  var module = angular.module('inboxFilters');
-
-  module.filter('resourceIcon', [
-    'ResourceIcons',
-    function(ResourceIcons) {
-      return function(name) {
-        var src = ResourceIcons.getImg(name);
-        if (src) {
-          src = 'src="' + src + '"';
-        } else {
-          src = '';
-        }
-        return '<img class="resource-icon" name="' + name + '" ' + src + ' />';
-      };
-    }
-  ]);
-
-
-}());
+angular.module('inboxFilters').filter('resourceIcon',
+  function(ResourceIcons) {
+    'use strict';
+    'ng-inject';
+    return function(name) {
+      var src = ResourceIcons.getImg(name);
+      if (src) {
+        src = 'src="' + src + '"';
+      } else {
+        src = '';
+      }
+      return '<img class="resource-icon" name="' + name + '" ' + src + ' />';
+    };
+  }
+);
