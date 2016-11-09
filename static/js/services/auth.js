80a328022370a22c4188c48ce1cd34f0256faf94
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2016-11-10 10:43:05 +1300

    Convert Auth to new style

--- static/js/services/auth.js
+++ static/js/services/auth.js
@@ -8,72 +8,72 @@ var _ = require('underscore');
 
   DB admins automatically have all permissions.
 */
-(function () {
 
-  'use strict';
+angular.module('inboxServices').factory('Auth',
+  function(
+    $q,
+    Session,
+    Settings
+  ) {
 
-  var inboxServices = angular.module('inboxServices');
+    'ngInject';
+    'use strict';
 
-  inboxServices.factory('Auth', ['$q', 'Settings', 'Session',
-    function($q, Settings, Session) {
-
-      var check = function(permissions, userRoles, settings, expected) {
-        return _.every(permissions, function(permission) {
-          var required = _.findWhere(settings.permissions, { name: permission });
-          if (!required) {
-            return !expected;
-          }
-          var found = _.intersection(userRoles, required.roles).length > 0;
-          return expected === found;
-        });
-      };
-
-      var isRequired = function(permission) {
-        return permission.indexOf('!') !== 0;
-      };
-
-      var getRequired = function(permissions) {
-        return _.filter(permissions, isRequired);
-      };
-
-      var getDisallowed = function(permissions) {
-        permissions = _.reject(permissions, isRequired);
-        permissions = _.map(permissions, function(permission) {
-          return permission.substring(1);
-        });
-        return permissions;
-      };
-
-      return function(permissions) {
-        if (!_.isArray(permissions)) {
-          permissions = [ permissions ];
+    var check = function(permissions, userRoles, settings, expected) {
+      return _.every(permissions, function(permission) {
+        var required = _.findWhere(settings.permissions, { name: permission });
+        if (!required) {
+          return !expected;
         }
-        var userCtx = Session.userCtx();
-        if (!userCtx) {
-          return $q.reject(new Error('Not logged in'));
-        }
-        var roles = userCtx.roles;
-        if (!roles || roles.length === 0) {
+        var found = _.intersection(userRoles, required.roles).length > 0;
+        return expected === found;
+      });
+    };
+
+    var isRequired = function(permission) {
+      return permission.indexOf('!') !== 0;
+    };
+
+    var getRequired = function(permissions) {
+      return _.filter(permissions, isRequired);
+    };
+
+    var getDisallowed = function(permissions) {
+      permissions = _.reject(permissions, isRequired);
+      permissions = _.map(permissions, function(permission) {
+        return permission.substring(1);
+      });
+      return permissions;
+    };
+
+    return function(permissions) {
+      if (!_.isArray(permissions)) {
+        permissions = [ permissions ];
+      }
+      var userCtx = Session.userCtx();
+      if (!userCtx) {
+        return $q.reject(new Error('Not logged in'));
+      }
+      var roles = userCtx.roles;
+      if (!roles || roles.length === 0) {
+        return $q.reject();
+      }
+      var requiredPermissions = getRequired(permissions);
+      var disallowedPermissions = getDisallowed(permissions);
+      if (_.contains(roles, '_admin')) {
+        if (disallowedPermissions.length > 0) {
           return $q.reject();
         }
-        var requiredPermissions = getRequired(permissions);
-        var disallowedPermissions = getDisallowed(permissions);
-        if (_.contains(roles, '_admin')) {
-          if (disallowedPermissions.length > 0) {
-            return $q.reject();
-          }
+        return $q.resolve();
+      }
+      return Settings().then(function(settings) {
+        if (check(requiredPermissions, roles, settings, true) &&
+            check(disallowedPermissions, roles, settings, false)) {
           return $q.resolve();
         }
-        return Settings().then(function(settings) {
-          if (check(requiredPermissions, roles, settings, true) &&
-              check(disallowedPermissions, roles, settings, false)) {
-            return $q.resolve();
-          }
-          return $q.reject();
-        });
-      };
+        return $q.reject();
+      });
+    };
 
-    }
-  ]);
-
-}());
+  }
+);
