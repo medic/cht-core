6b58e317445739c917680f9242e19e2fdf5a6be1
Gareth Bowen <gareth@medicmobile.org>
2017-03-01 00:15:01 +1300

    Only clear right action bar when filing new report (#3132)

--- static/js/controllers/contacts-report.js
+++ static/js/controllers/contacts-report.js
@@ -1,81 +1,84 @@
-(function () {
+angular.module('inboxControllers').controller('ContactsReportCtrl',
+  function (
+    $log,
+    $scope,
+    $state,
+    $translate,
+    DB,
+    Enketo,
+    Snackbar,
+    TranslateFrom
+  ) {
 
-  'use strict';
+    'use strict';
+    'ngInject';
 
-  var inboxControllers = angular.module('inboxControllers');
-
-  inboxControllers.controller('ContactsReportCtrl', 
-    ['$scope', '$state', '$log', '$translate', 'DB', 'Enketo', 'TranslateFrom', 'Snackbar',
-    function ($scope, $state, $log, $translate, DB, Enketo, TranslateFrom, Snackbar) {
-
-      var render = function(doc) {
-        $scope.setSelected({ doc: doc });
-        $scope.setCancelTarget(function() {
-          $state.go('contacts.detail', { id: $state.params.id });
-        });
-        var instanceData = {
-          source: 'contact',
-          contact: doc,
-        };
-        return Enketo
-          .render($('#contact-report'), $state.params.formId, instanceData)
-          .then(function(form) {
-            $scope.form = form;
-            $scope.loadingForm = false;
-          });
-      };
-
-      $scope.save = function() {
-        if ($scope.saving) {
-          $log.debug('Attempted to call contacts-report:$scope.save more than once');
-          return;
-        }
-
-        $scope.saving = true;
-        Enketo.save($state.params.formId, $scope.form)
-          .then(function(doc) {
-            $log.debug('saved report', doc);
-            $scope.saving = false;
-            $translate('report.created').then(Snackbar);
-            $state.go('contacts.detail', { id: $state.params.id });
-          })
-          .catch(function(err) {
-            $scope.saving = false;
-            $log.error('Error submitting form data: ', err);
-          });
-      };
-
-      $scope.form = null;
-      $scope.loadingForm = true;
-      $scope.setActionBar();
-      $scope.setShowContent(true);
+    var render = function(doc) {
+      $scope.setSelected({ doc: doc });
       $scope.setCancelTarget(function() {
         $state.go('contacts.detail', { id: $state.params.id });
       });
-      DB()
-        .get($state.params.id)
-        .then(render)
-        .then(function() {
-          return DB().query('medic-client/forms', { include_docs: true, key: $state.params.formId });
-        })
-        .then(function(res) {
-          if (res.rows[0]) {
-            $scope.setTitle(TranslateFrom(res.rows[0].doc.title));
-          }
-        })
-        .catch(function(err) {
-          $log.error('Error loading form', err);
-          $scope.contentError = true;
+      var instanceData = {
+        source: 'contact',
+        contact: doc,
+      };
+      return Enketo
+        .render($('#contact-report'), $state.params.formId, instanceData)
+        .then(function(form) {
+          $scope.form = form;
           $scope.loadingForm = false;
         });
+    };
 
-      $scope.$on('$destroy', function() {
-        if (!$state.includes('contacts.report')) {
-          $scope.setTitle();
-          Enketo.unload($scope.form);
+    $scope.save = function() {
+      if ($scope.saving) {
+        $log.debug('Attempted to call contacts-report:$scope.save more than once');
+        return;
+      }
+
+      $scope.saving = true;
+      Enketo.save($state.params.formId, $scope.form)
+        .then(function(doc) {
+          $log.debug('saved report', doc);
+          $scope.saving = false;
+          $translate('report.created').then(Snackbar);
+          $state.go('contacts.detail', { id: $state.params.id });
+        })
+        .catch(function(err) {
+          $scope.saving = false;
+          $log.error('Error submitting form data: ', err);
+        });
+    };
+
+    $scope.form = null;
+    $scope.loadingForm = true;
+    $scope.setRightActionBar();
+    $scope.setShowContent(true);
+    $scope.setCancelTarget(function() {
+      $state.go('contacts.detail', { id: $state.params.id });
+    });
+    DB()
+      .get($state.params.id)
+      .then(render)
+      .then(function() {
+        return DB().query('medic-client/forms', { include_docs: true, key: $state.params.formId });
+      })
+      .then(function(res) {
+        if (res.rows[0]) {
+          $scope.setTitle(TranslateFrom(res.rows[0].doc.title));
         }
+      })
+      .catch(function(err) {
+        $log.error('Error loading form', err);
+        $scope.contentError = true;
+        $scope.loadingForm = false;
       });
-    }
-  ]);
 
-}());
+    $scope.$on('$destroy', function() {
+      if (!$state.includes('contacts.report')) {
+        $scope.setTitle();
+        Enketo.unload($scope.form);
+      }
+    });
+  }
+);
