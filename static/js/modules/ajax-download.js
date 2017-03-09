e2724336a1f4b225be1604393a69ec83917d16cb
Estelle Comment <estelle.comment@gmail.com>
2017-03-09 15:29:16 +0100

    Set default file name.

--- static/js/modules/ajax-download.js
+++ static/js/modules/ajax-download.js
@@ -2,13 +2,15 @@
 
   'use strict';
 
+  var DEFAULT_FILE_NAME = 'download';
+
   /**
    * Prompts the user to download a file given a url.
    */
   exports.download = function(url) {
     var element = document.createElement('a');
     element.setAttribute('href', url);
-    element.setAttribute('download', 'failed'); // this filename only shows in case of failed download AFAIK
+    element.setAttribute('download', DEFAULT_FILE_NAME);
     element.style.display = 'none';
     document.body.appendChild(element);
     element.click();
