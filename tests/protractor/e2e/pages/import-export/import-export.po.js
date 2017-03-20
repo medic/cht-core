14558e35eed94c913d47d5d282f4eb97564c6c4d
Bede Ngaruko <Bede.Ngaruko@healthlink.net>
2017-03-20 15:31:13 +1300

    added import-export PO

---
+++ tests/protractor/e2e/pages/import-export/import-export.po.js
@@ -0,0 +1,25 @@
+var helper = require('../../helper'),
+
+var ImportExportPage = function () {
+
+    this.exportButton = element(by.css('[ng-click="export()"]'));
+    this.messagesTab = element(by.css('[ui-sref="configuration.export.messages"]'));
+    this.reportsTab = element(by.css('[ui-sref="configuration.export.reports"]'));
+    this.contactsTab = element(by.css('[ui-sref="configuration.export.contacts"]'));
+    this.feedbackTab = element(by.css('[ui-sref="configuration.export.feedback"]'));
+    this.serverLogsTab = element(by.css('[ui-sref="configuration.export.serverlogs"]'));
+    this.auditLogsTab = element(by.css('[ui-sref="configuration.export.auditlogs"]'));
+
+
+    //functions to interact with our page
+
+    this.export = function () {
+        //todo: click exportButton
+
+    }
+
+
+
+}
+
+module.exports = ImportExportPage;
