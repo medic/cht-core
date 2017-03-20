88dbf0d8dfed8852fa431c0bc66351fb7dbfa6d2
Bede Ngaruko <Bede.Ngaruko@healthlink.net>
2017-03-20 13:03:20 +1300

    added add-users po

---
+++ tests/protractor/e2e/pages/users/add-user.po.js
@@ -0,0 +1,45 @@
+var helper = require('../../helper'),
+
+var AddUserPage = function() {
+
+
+    //title and texts of notifications/error messages
+    this.pageTitle = 'Medic Mobile';
+    //this.incorrectCredentialsText = 'Incorrect user name or password. Please try again.';
+   // this.passwordBlankText = "Can't be blank.";
+
+    //sign in form elements selected by id
+    this.usernameField = element(by.id('name'));
+    this.fullNameField = element(by.id('fullname'));
+    this.emailField = element(by.id('emmail'));
+    this.phoneField = element(by.id('phone'));
+    this.languageField = element(by.id('language'));
+    this.userTypeField = element(by.id('type'));
+    this.placeField = element(by.id('facility'));
+    this.contactField = element(by.id('contact'));
+    this.passwordField = element(by.id('password'));
+    this.confirmPasswordField = element(by.id('confirm-password'));
+    this.submitButton = element(by.className('btn submit btn-primary'));
+       this.cancelButton = element(by.className('btn cancel'));
+    
+    //functions to interact with our page
+    
+    this.submit = function() {
+
+        helper.waitUntilReady(this.submitButton)
+       
+        this.submitButton.click()
+
+    }
+
+     this.cancel = function() {
+
+        helper.waitUntilReady(this.cancelButton)
+       
+        this.cancelButton.click()
+
+    }
+
+}
+
+module.exports = AddUserPage;
---
+++ tests/protractor/e2e/pages/users/users.po.js
@@ -0,0 +1,32 @@
+var helper = require('../../helper'),
+
+var UsersPage = function() {
+
+    //title 
+    this.pageTitle = 'Medic Mobile';
+    this.addUserButton=element(by.id('add-user'));
+    this.usersList=element.all(by.repeater('user in users'));
+       
+    //functions to interact with our page
+    
+    this.addUser = function(username, password) {
+
+        helper.waitUntilReady(this.addUserButton)
+      
+    }
+
+    this.editUser = function(username, password) {
+
+        helper.waitUntilReady(this.addUserButton)
+      
+    }
+this.deleteUser = function(username, password) {
+
+        helper.waitUntilReady(this.addUserButton)
+      
+    }
+
+
+}
+
+module.exports = UsersPage;
