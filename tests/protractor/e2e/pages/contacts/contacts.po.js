4867abd47298a0f90362c72dc23a7d642014076e
ngaruko <ngarukob@yahoo.com>
2017-03-21 22:22:06 +1300

    edieted contacts po

--- tests/protractor/e2e/pages/contacts/contacts.po.js
+++ tests/protractor/e2e/pages/contacts/contacts.po.js
@@ -1,17 +1,31 @@
 var helper = require('../../helper'),
     faker = require('faker');
 
-var ContactsPage = function() {
+var ContactsPage = function () {
 
-   
+
     this.pageTitle = 'Medic Mobile';
     this.noContactErrorField = $('[ng-show="!error && !loading && !contacts.length"]');
-    
-    this.rightHandPane= $('[ng-show="!selected.contacts[0] && !loadingContent"]');
-    
+
+    this.rightHandPane = $('[ng-show="!selected.contacts[0] && !loadingContent"]');
+    this.searchBox = $('#freetext');
+    this.seachButton = $('#search');
+    this.refreshButton = $('.fa fa-undo');
+
+    this.leftHandSideFab = {
+
+    };
+    this.rightHandSideFAB = {
+        newActionButton: element(by.css('.mm-icon mm-icon-inverse mm-icon-caption dropdown-toggle mm-icon-disabled')),
+        newAreaButton: element(by.css('.mm-icon mm-icon-inverse mm-icon-caption')),
+        newPersonButton: element(by.css('')),
+        editButton=element(by.css('')),
+        deleteButton=element(by.css(''))
+
+    };
     //functions to interact with our page
-    
- 
+
+
 
 }
 
