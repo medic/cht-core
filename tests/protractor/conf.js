a5749ab913e12e0b93c6f332f14c8ce9a5eb6f64
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2017-03-22 21:02:26 +1300

    Add e2e test for sentinel transition
    Most of the work here was to start sentinel so that e2e tests can be
    written to test sentinel workflows. To prove it works it includes a
    test for the registration transition.
    
    medic/medic-webapp#3229

--- tests/protractor/conf.js
+++ tests/protractor/conf.js
@@ -1,31 +1,31 @@
-var _ = require('underscore'),
-    utils = require('./utils'),
-    spawn = require('child_process').spawn,
-    environment = require('./auth')(),
-    api;
+const _ = require('underscore'),
+      utils = require('./utils'),
+      spawn = require('child_process').spawn,
+      environment = require('./auth')(),
+      modules = [];
 
 _.templateSettings = {
   interpolate: /\{\{(.+?)\}\}/g
 };
 
-var login = function(browser) {
-  var loginUrlTemplate = _.template('http://{{apiHost}}:{{apiPort}}/{{dbName}}/login?redirect=');
-  var redirectUrl = encodeURIComponent('/' + environment.dbName  + '/_design/medic/_rewrite/#/messages?e2eTesting=true');
+const login = browser => {
+  const loginUrlTemplate = _.template('http://{{apiHost}}:{{apiPort}}/{{dbName}}/login?redirect=');
+  const redirectUrl = encodeURIComponent('/' + environment.dbName  + '/_design/medic/_rewrite/#/messages?e2eTesting=true');
   browser.driver.get(loginUrlTemplate(environment) + redirectUrl);
   browser.driver.findElement(by.name('user')).sendKeys(environment.user);
   browser.driver.findElement(by.name('password')).sendKeys(environment.pass);
   browser.driver.findElement(by.id('login')).click();
   // Login takes some time, so wait until it's done.
-  return browser.driver.wait(function() {
+  return browser.driver.wait(() => {
     return browser.driver.isElementPresent(by.css('body.bootstrapped'));
   }, 20 * 1000, 'Login should be complete within 20 seconds');
 };
 
-var startApi = function() {
-  return new Promise(function(resolve) {
-    var couchUrlTemplate = _.template('http://{{user}}:{{pass}}@{{couchHost}}:{{couchPort}}/{{dbName}}');
-    api = spawn('node', ['server.js'], {
-      cwd: 'api',
+const startNodeModule = (dir, startOutput) => {
+  return new Promise(resolve => {
+    const couchUrlTemplate = _.template('http://{{user}}:{{pass}}@{{couchHost}}:{{couchPort}}/{{dbName}}');
+    const module = spawn('node', ['server.js'], {
+      cwd: dir,
       env: {
         API_PORT: environment.apiPort,
         COUCH_URL: couchUrlTemplate(environment),
@@ -33,19 +33,28 @@ var startApi = function() {
         PATH: process.env.PATH
       }
     });
-    api.stdout.on('data', function(data) {
-      if (data.toString().indexOf('Medic API listening on port') >= 0) {
+    const prefix = '[' + dir + '] ';
+    let started = false;
+    module.stdout.on('data', data => {
+      if (!started && data.toString().indexOf(startOutput) >= 0) {
+        started = true;
         resolve();
       }
-      console.log('[api] ' + data);
+      console.log(prefix + data);
     });
-    api.stderr.on('data', function(data) {
-      console.error('[api] ' + data);
-    });
+    module.stderr.on('data', data => console.error(prefix + data));
+    modules.push(module);
   });
 };
 
-var setupSettings = function() {
+const startApi = () => startNodeModule('api', 'Medic API listening on port');
+
+const startSentinel = () => startNodeModule('sentinel', 'startup complete.');
+
+// start sentinel serially because it relies on api
+const startModules = () => startApi().then(startSentinel);
+
+const setupSettings = () => {
   return utils.request({
     path: '/' + environment.dbName  + '/_design/medic/_rewrite/update_settings/medic',
     method: 'PUT',
@@ -53,9 +62,9 @@ var setupSettings = function() {
   });
 };
 
-var setupUser = function() {
+const setupUser = () => {
   return utils.getDoc('org.couchdb.user:' + environment.user)
-    .then(function(doc) {
+    .then(doc => {
       doc.known = true;
       doc.language = 'en';
       doc.roles = ['_admin'];
@@ -71,16 +80,14 @@ exports.config = {
     // browserName: 'chrome'
     browserName: 'firefox'
   },
-  onPrepare: function() {
-    var started = startApi();
-    browser.driver.wait(started, 15 * 1000, 'Server should start within 15 seconds');
+  onPrepare: () => {
+    const startup = startModules();
+    browser.driver.wait(startup, 15 * 1000, 'API should start within 15 seconds');
     browser.driver.sleep(1000);
     browser.driver.wait(setupSettings, 5 * 1000, 'Settings should be setup within 5 seconds');
     browser.driver.wait(setupUser, 5 * 1000, 'User should be setup within 5 seconds');
     browser.driver.sleep(1000);
     return login(browser);
   },
-  onCleanUp: function() {
-    api.kill();
-  }
+  onCleanUp: () => modules.forEach(module => module.kill())
 };
