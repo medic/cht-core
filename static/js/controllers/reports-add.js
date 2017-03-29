5f92446dd9e1392f2d0314ade7b9869d1455aad3
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2017-03-30 12:53:48 +1300

    Unicode support for storing enketo xml
    atob and btoa do not support non-Latin1 characters so attempting to
    submit a report with Hindi characters fails. This changes to using
    Blobs instead as per the best practice from PouchDB.
    
    medic/medic-webapp#3308

--- static/js/controllers/reports-add.js
+++ static/js/controllers/reports-add.js
@@ -8,6 +8,7 @@ angular.module('inboxControllers').controller('ReportsAddCtrl',
     $translate,
     DB,
     Enketo,
+    FileReader,
     Snackbar
   ) {
 
@@ -19,9 +20,7 @@ angular.module('inboxControllers').controller('ReportsAddCtrl',
         return $q.resolve({ form: $state.params.formId });
       }
       if ($state.params.reportId) { // editing
-        return DB().get($state.params.reportId, {
-          attachments: true
-        });
+        return DB().get($state.params.reportId);
       }
       return $q.reject(new Error('Must have either formId or reportId'));
     };
@@ -38,6 +37,20 @@ angular.module('inboxControllers').controller('ReportsAddCtrl',
       $scope.clearCancelTarget();
     }
 
+    var getReportContent = function(doc) {
+      // creating a new doc - no content
+      if (!doc._id) {
+        return $q.resolve();
+      }
+      // check old style inline form content
+      if (doc.content) {
+        return $q.resolve(doc.content);
+      }
+      // check new style attached form content
+      return DB().getAttachment(doc._id, Enketo.REPORT_ATTACHMENT_NAME)
+        .then(FileReader);
+    };
+
     getSelected()
       .then(function(doc) {
         $log.debug('setting selected', doc);
@@ -45,24 +58,19 @@ angular.module('inboxControllers').controller('ReportsAddCtrl',
         // TODO: check doc.content as this is where legacy documents stored
         //       their XML. Consider removing this check at some point in the
         //       future.
-        var content = doc.content || (
-          doc._attachments &&
-          doc._attachments.content &&
-          atob(doc._attachments.content.data));
-
-        // timeout to allow angular to finish rendering the page before
-        // enketo tries to bind to it
-        $timeout(function() {
-          Enketo.render($('#report-form'), doc.form, content)
-            .then(function(form) {
-              $scope.form = form;
-              $scope.loadingContent = false;
-            })
-            .catch(function(err) {
-              $scope.loadingContent = false;
-              $scope.contentError = true;
-              $log.error('Error loading form.', err);
-            });
+        return getReportContent(doc).then(function(content) {
+          $timeout(function() {
+            Enketo.render($('#report-form'), doc.form, content)
+              .then(function(form) {
+                $scope.form = form;
+                $scope.loadingContent = false;
+              })
+              .catch(function(err) {
+                $scope.loadingContent = false;
+                $scope.contentError = true;
+                $log.error('Error loading form.', err);
+              });
+          });
         });
       })
       .catch(function(err) {
