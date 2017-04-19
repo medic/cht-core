22a3164abd71db146710a7a9a42d8d1cdadba424
Alex Anderson <anderson@medicmobile.org>
2017-04-19 17:22:04 +0200

    Clean up e2e test for collect a bit
    * use correct line-endings for HTTP message content
    * use today's date instead of some random old one

--- tests/protractor/e2e/medic-collect.js
+++ tests/protractor/e2e/medic-collect.js
@@ -28,11 +28,10 @@ describe('medic-collect', () => {
 
 `HEAD /${dbName}/_design/medic/_rewrite/add?deviceID=imei%3A357578064823168 HTTP/1.1
 X-OpenRosa-Version: 1.0
-Date: Tue, 11 Apr 2017 06:34:21 CEST
+Date: ${new Date().toISOString()}
 Host: ${host}:${port}
 Connection: close
-
-`).then((res) => {
+`).then((res) => {
 
         // then
         assert.equal(res.statusCode, 401, JSON.stringify(res));
