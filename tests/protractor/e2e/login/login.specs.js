65f2912b42089388b4e0fc859778a83aaea8f8b9
Bede Ngaruko <ngarukob@yahoo.com>
2017-05-09 14:56:20 +1000

    Protractor tests (#3455)
    * added login.po
    
    * login specs
    
    * added users po
    
    * fixed selector errors
    
    * refactored page objects

---
+++ tests/protractor/e2e/login/login.specs.js
@@ -0,0 +1,25 @@
+var helper = require('../../helper'),
+  utils = require('../../utils.js');
+  var loginPage = require('../../page-objects/login/login.po.js');
+
+describe('Login tests : ', function () {
+  var wrongUsername = 'fakeuser';
+  var wrongPassword = 'fakepass';
+  beforeEach(function () {
+    browser.driver.get(utils.getLoginUrl());
+  });
+
+  afterEach(function () {
+    helper.takeScreenshot('login-test');
+    browser.manage().deleteAllCookies();
+  });
+
+  it('should have a title', function () {
+    expect(browser.getTitle()).toEqual('Medic Mobile');
+  });
+
+  it('should try to sign in and verify that credentials were incorrect', function () {
+    loginPage.login(wrongUsername, wrongPassword);
+    expect(helper.isTextDisplayed(loginPage.getIncorrectCredentialsText));
+  });
+});
---
+++ tests/protractor/page-objects/users/users.po.js
@@ -0,0 +1,35 @@
+var helper = require('../../helper');
+var addUserButton = element(by.id('add-user'));
+//var addUserModal = $('mm-modal#edit-user-profile');
+var usersList = element.all(by.repeater('user in users'));
+
+module.exports = {
+	addUser: function (username, password) {
+		username.trim();
+		password.trim();
+		helper.waitUntilReady(addUserButton);
+	},
+
+	editUser: function (username, password) {
+		username.trim();
+		password.trim();
+		helper.waitUntilReady(addUserButton);
+	},
+
+	deleteUser: function (username, password) {
+		username.trim();
+		password.trim();
+		helper.waitUntilReady(addUserButton);
+	},
+
+	openAddUserModal: function () {
+		helper.waitElementToBeClickable(addUserButton);
+		addUserButton.click();
+	},
+	getUsersList:function(){
+		helper.waitUntilReady(addUserButton);
+		return usersList;
+	}
+};
+
+
