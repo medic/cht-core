994e1149e295afa08f2acae9690cdb059381595c
Stefan du Fresne <stefan@sdufresne.info>
2017-05-10 16:33:38 +0100

    ES6-afying

--- controllers/sms-gateway.js
+++ controllers/sms-gateway.js
@@ -3,21 +3,22 @@
  * @see https://github.com/medic/medic-gateway
  */
 
-var async = require('async'),
-    messageUtils = require('./message-utils'),
-    recordUtils = require('./record-utils'),
+const _ = require('underscore'),
+      async = require('async'),
+      messageUtils = require('./message-utils'),
+      recordUtils = require('./record-utils'),
 
-    // map from the medic-gateway state to the medic-webapp state
-    STATUS_MAP = {
-      UNSENT: 'received-by-gateway',
-      PENDING: 'forwarded-by-gateway',
-      SENT: 'sent',
-      DELIVERED: 'delivered',
-      FAILED: 'failed',
-    };
+      // map from the medic-gateway state to the medic-webapp state
+      STATUS_MAP = {
+        UNSENT: 'received-by-gateway',
+        PENDING: 'forwarded-by-gateway',
+        SENT: 'sent',
+        DELIVERED: 'delivered',
+        FAILED: 'failed',
+      };
 
 function warn() {
-  var args = Array.prototype.slice.call(arguments, 0);
+  const args = Array.prototype.slice.call(arguments, 0);
   args.unshift('WARN', 'sms-gateway');
   console.error.apply(console, args);
 }
@@ -31,7 +32,7 @@ function saveToDb(message, callback) {
 }
 
 function updateStateFor(update, callback) {
-  var details,
+  let details,
       newState = STATUS_MAP[update.status];
   if (newState) {
     if(update.reason) {
@@ -45,7 +46,7 @@ function updateStateFor(update, callback) {
 }
 
 function updateState(messageId, newState, details, callback) {
-  var updateBody = {
+  const updateBody = {
     state: newState,
   };
   if(details) {
@@ -57,11 +58,12 @@ function updateState(messageId, newState, details, callback) {
 function markMessagesForwarded(messages, callback) {
   async.eachSeries(
     messages,
-    function(message, callback) {
-      updateState(message.id, 'forwarded-to-gateway', null, callback);
-    },
-    function(err) {
+    (message, callback) => updateState(message.id, 'forwarded-to-gateway', null, callback),
+    (err) => {
       if (err) {
+        // TODO: this comment (and the action of warning) is not correct. What
+        //       should we really do here?
+
         // No error throwing here, because no big deal : 'forwarded-to-gateway' status
         // wasn't saved to DB, so message will stay in 'pending' status and be retried next time.
         warn(err);
@@ -73,21 +75,19 @@ function markMessagesForwarded(messages, callback) {
 
 function getOutgoing(callback) {
   messageUtils.getMessages({ states: ['pending', 'forwarded-to-gateway'] },
-    function(err, pendingMessages) {
-    if (err) {
-      warn(err);
-      return callback(null, []);
-    }
-    var messages = pendingMessages.map(function(message) {
-      return {
-        id: message.id,
-        to: message.to,
-        content: message.message,
-      };
-    });
-    markMessagesForwarded(messages, function() {
-      callback(null, messages);
-    });
+    (err, pendingMessages) => {
+      if (err) {
+        warn(err);
+        return callback(null, []);
+      }
+      const messages = pendingMessages.map(message => {
+        return {
+          id: message.id,
+          to: message.to,
+          content: message.message,
+        };
+      });
+      markMessagesForwarded(messages, () => callback(null, messages));
   });
 }
 
@@ -96,7 +96,8 @@ function processMessages(req, callback) {
   if (!req.body.messages) {
     return callback();
   }
-  async.eachSeries(req.body.messages, saveToDb, function(err) {
+
+  async.eachSeries(req.body.messages, saveToDb, err => {
     if (err) {
       warn(err);
     }
@@ -109,7 +110,7 @@ function processUpdates(req, callback) {
   if (!req.body.updates) {
     return callback();
   }
-  async.eachSeries(req.body.updates, updateStateFor, function(err) {
+  async.eachSeries(req.body.updates, updateStateFor, err => {
     if (err) {
       warn(err);
     }
@@ -123,20 +124,14 @@ module.exports = {
   },
   post: function(req, callback) {
     async.series([
-      function(callback) {
-        processMessages(req, callback);
-      },
-      function(callback) {
-        processUpdates(req, callback);
-      },
-      function(callback) {
-        getOutgoing(callback);
-      }
-    ], function(err, results) {
+      _.partial(processMessages, req),
+      _.partial(processUpdates, req),
+      getOutgoing
+    ], (err, [,,outgoingMessages]) => {
       if (err) {
         return callback(err);
       }
-      callback(null, { messages: results[2] });
+      callback(null, { messages: outgoingMessages });
     });
   },
 };
