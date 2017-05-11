acc544c700e93c639cba8cc1567b65a5b158f743
ngaruko <ngarukob@yahoo.com>
2017-05-12 09:55:59 +1200

    added a sleep to registration-by-sms

--- tests/protractor/e2e/login/login.specs.js
+++ tests/protractor/e2e/login/login.specs.js
@@ -3,24 +3,18 @@ const helper = require('../../helper'),
   loginPage = require('../../page-objects/login/login.po.js');
 
 describe('Login tests : ', function() {
-  const wrongUsername = 'fakeuser';
-  const wrongPassword = 'fakepass';
-  beforeEach(function() {
-    browser.driver.get(utils.getLoginUrl());
-  });
-
-  afterEach(function() {
-    browser.manage().deleteAllCookies();
-  });
+  const wrongUsername = 'fakeuser',
+   wrongPassword = 'fakepass';
 
   it('should have a title', function() {
     expect(browser.getTitle()).toEqual('Medic Mobile');
   });
 
   it('should try to sign in and verify that credentials were incorrect', function() {
-     browser.manage().deleteAllCookies();
+    browser.manage().deleteAllCookies();
     browser.driver.get(utils.getLoginUrl());
     loginPage.login(wrongUsername, wrongPassword);
-   expect(helper.isTextDisplayed(loginPage.getIncorrectCredentialsText()));
+    expect(helper.isTextDisplayed(loginPage.getIncorrectCredentialsText()));
+    loginPage.login('admin', 'pass');
   });
 });
--- tests/protractor/e2e/users/add-user.specs.js
+++ tests/protractor/e2e/users/add-user.specs.js
@@ -1,6 +1,5 @@
 const utils = require('../../utils'),
   usersPage = require('../../page-objects/users/users.po.js'),
-  loginPage = require('../../page-objects/login/login.po.js'),
   common = require('../../page-objects/common/common.po.js'),
   helper = require('../../helper'),
   addUserModal = require('../../page-objects/users/add-user-modal.po.js');
@@ -9,7 +8,6 @@ const addedUser = 'bedetester2020', fullName = 'Bede Ngaruko';
 
 describe('Add user test : ', function() {
   it('should open add user modal', function() {
-    loginPage.login('admin', 'pass');
     common.goToConfiguration();
     browser.get(utils.getBaseUrl() + '/_design/medic/_rewrite/#/configuration/users');
     usersPage.openAddUserModal();
