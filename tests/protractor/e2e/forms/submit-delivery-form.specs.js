83639ad0986c0555fbdee948fa7254cd0c57128e
ngaruko <ngarukob@yahoo.com>
2017-05-31 17:33:10 -0700

    Delivery report test plus Html reporting utility

--- tests/protractor/e2e/forms/submit-delivery-form.specs.js
+++ tests/protractor/e2e/forms/submit-delivery-form.specs.js
@@ -3,9 +3,63 @@ const helper = require('../../helper'),
 
 describe('Submit Delivery Report', () => {
 
+  const contactId = '3b3d50d275280d2568cd36281d00348b';
+  const docs = [
+    {
+      _id: 'c49385b3594af7025ef097114104ef48',
+      reported_date: 1469578114543,
+      notes: '',
+      contact: {
+        _id: contactId,
+        name: 'Jack',
+        date_of_birth: '',
+        phone: '+64274444444',
+        alternate_phone: '',
+        notes: '',
+        type: 'person',
+        reported_date: 1478469976421
+      },
+      name: 'Number three district',
+      external_id: '',
+      type: 'district_hospital'
+    },
+    {
+      _id: contactId,
+      name: 'Jack',
+      date_of_birth: '',
+      phone: '+64274444444',
+      alternate_phone: '',
+      notes: '',
+      type: 'person',
+      reported_date: 1478469976421,
+      parent: {
+        _id: 'c49385b3594af7025ef097114104ef48',
+        reported_date: 1469578114543,
+        notes: '',
+        contact: {
+          _id: contactId,
+          name: 'Jack',
+          date_of_birth: '',
+          phone: '+64274444444',
+          alternate_phone: '',
+          notes: '',
+          type: 'person',
+          reported_date: 1478469976421
+        },
+        name: 'Number three district',
+        external_id: '',
+        type: 'district_hospital'
+      }
+    }
+  ];
+
+  const noteToCHW = 'Good news, Jack! Jack () has delivered at the health facility. We will alert you when it is time to refer them for PNC. Please monitor them for danger signs. Thank you!';
+
   beforeEach(done => {
     browser.ignoreSynchronization = true;
+    deliveryReport.saveDocs(done, docs);
     deliveryReport.configureForm(done);
+
   });
 
   afterEach(done => {
@@ -28,6 +82,26 @@ describe('Submit Delivery Report', () => {
       return element(by.css('#report-form')).isPresent();
     }, 10000);
 
-    expect(helper.isTextDisplayed('Delivery'));
+    //select name
+    deliveryReport.selectPatientName('jack');
+    deliveryReport.goNext();
+    helper.waitElementToBeVisisble(element(by.css('[value="healthy"]')));
+
+    //Delivery info
+    deliveryReport.selectLiveBirthButton();
+    deliveryReport.selectFacilityButton();
+    deliveryReport.enterDeliveryDate('');
+    deliveryReport.goNext();
+    expect(deliveryReport.getNoteToCHW()).toBe(noteToCHW);
+    deliveryReport.goNext();
+
+    //summary page
+    expect(deliveryReport.getOutcomeText()).toBe('Live Birth');
+    expect(deliveryReport.getDeliveryLocationSummaryText())
+      .toBe('Facility');
+    expect(deliveryReport.getFollowUpMessage()).toBe(noteToCHW);
+    //submit
+    deliveryReport.submit();
+    expect(element(by.css('div.details')).isPresent()).toBeTruthy();
   });
 });
