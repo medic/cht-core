dbb536c99ed3f573f362af82851dc02df4135f53
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2017-06-09 11:08:49 +1200

    Fixes a bug in read_by_type view
    The refactor in #2635 caused a regression where the messages reported an incorrect count

--- ddocs/medic-client/views/data_records_read_by_type/map.js
+++ ddocs/medic-client/views/data_records_read_by_type/map.js
@@ -1,13 +1,22 @@
 function(doc) {
   if (doc.type === 'data_record') {
+    var emitRead = function(doc, type) {
+      emit(['_total', type]);
+      if (doc.read) {
+        doc.read.forEach(function(user) {
+          if (user) {
+            emit([user, type]);
+          }
+        });
+      }
+    };
     var type = doc.form ? 'forms' : 'messages';
-    emit(['_total', type]);
-    if (doc.read) {
-      doc.read.forEach(function(user) {
-        if (user) {
-          emit([user, type]);
-        }
+    if (doc.tasks) {
+      doc.tasks.forEach(function(task) {
+        emitRead(doc, type);
       });
+    } else {
+      emitRead(doc, type);
     }
   }
 }
