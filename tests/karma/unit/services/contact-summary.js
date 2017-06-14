c73ec7b87a78f0f77305c56dafda9a6dc6b54d4a
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2017-06-14 16:06:33 +1200

    Refactor to use Function instead of eval
    Replaces the eval call with the slightly safer new Function()
    statement. This requires a configuration change as outlined in
    the changelog.
    
    medic/medic-webapp#3546

--- tests/karma/unit/services/contact-summary.js
+++ tests/karma/unit/services/contact-summary.js
@@ -1,52 +1,49 @@
-describe('ContactSummary service', function() {
+describe('ContactSummary service', () => {
 
   'use strict';
 
-  var service,
+  let service,
       Settings = sinon.stub();
 
-  beforeEach(function() {
+  beforeEach(() => {
     module('inboxApp');
-    module(function($provide) {
+    module($provide => {
       $provide.value('Settings', Settings);
-      $provide.value('$filter', function(name) {
+      $provide.value('$filter', name => {
         if (name !== 'reversify') {
           throw new Error('unknown filter');
         }
-        return function(value) {
-          return value.split('').reverse().join('');
-        };
+        return value => value.split('').reverse().join('');
       });
     });
-    inject(function(_ContactSummary_) {
+    inject(_ContactSummary_ => {
       service = _ContactSummary_;
     });
   });
 
-  afterEach(function() {
+  afterEach(() => {
     KarmaUtils.restore(Settings);
   });
 
-  it('returns empty when no expression configured', function() {
+  it('returns empty when no script configured', () => {
     Settings.returns(KarmaUtils.mockPromise(null, { contact_summary: '' }));
-    var contact = {};
-    var reports = [];
-    return service(contact, reports).then(function(actual) {
+    const contact = {};
+    const reports = [];
+    return service(contact, reports).then(actual => {
       chai.expect(actual.fields.length).to.equal(0);
       chai.expect(actual.cards.length).to.equal(0);
     });
   });
 
-  it('evals expression with `reports` and `contact` in scope', function() {
-    var expression = 'var result = { fields: [ ' +
-                       '{ label: "Notes", value: "Hello " + contact.name },' +
-                       '{ label: "Num reports", value: reports.length }' +
-                      '] };' +
-                      'result;';
-    Settings.returns(KarmaUtils.mockPromise(null, { contact_summary: expression }));
-    var contact = { name: 'jack' };
-    var reports = [ { _id: 1 }, { _id: 2} ];
-    return service(contact, reports).then(function(actual) {
+  it('evals script with `reports` and `contact` in scope', () => {
+    const script = `return { fields: [
+                      { label: "Notes", value: "Hello " + contact.name },
+                      { label: "Num reports", value: reports.length }
+                    ] };`;
+    Settings.returns(KarmaUtils.mockPromise(null, { contact_summary: script }));
+    const contact = { name: 'jack' };
+    const reports = [ { _id: 1 }, { _id: 2} ];
+    return service(contact, reports).then(actual => {
       chai.expect(actual.fields.length).to.equal(2);
       chai.expect(actual.fields[0].label).to.equal('Notes');
       chai.expect(actual.fields[0].value).to.equal('Hello jack');
@@ -56,15 +53,14 @@ describe('ContactSummary service', function() {
     });
   });
 
-  it('applies filters to values', function() {
-    var expression = 'var result = { fields: [ ' +
-                       '{ label: "Notes", value: "Hello", filter: "reversify" }' +
-                      '] };' +
-                      'result;';
-    Settings.returns(KarmaUtils.mockPromise(null, { contact_summary: expression }));
-    var contact = {};
-    var reports = [];
-    return service(contact, reports).then(function(actual) {
+  it('applies filters to values', () => {
+    const script = `return { fields: [
+                      { label: "Notes", value: "Hello", filter: "reversify" }
+                    ] };`;
+    Settings.returns(KarmaUtils.mockPromise(null, { contact_summary: script }));
+    const contact = {};
+    const reports = [];
+    return service(contact, reports).then(actual => {
       chai.expect(actual.fields.length).to.equal(1);
       chai.expect(actual.fields[0].label).to.equal('Notes');
       chai.expect(actual.fields[0].value).to.equal('olleH');
