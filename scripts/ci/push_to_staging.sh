1fdea22f329b163e742fc713e2ecc44866e2ee18
Alex Anderson <anderson@medicmobile.org>
2017-07-12 12:41:52 +0000

    Remove debug to see if builds get pushed again

--- scripts/ci/push_to_staging.sh
+++ scripts/ci/push_to_staging.sh
@@ -1,27 +1,17 @@
 #!/bin/bash -e
 
-./scripts/ci/npm_version_check
-
 # Attempt to reduce size of api and sentinel artifacts
 cd api &&
-    echo '[medic-api] Available modules:' && ls node_modules &&
     npm install --production &&
-    echo '[medic-api] Available modules:' && ls node_modules &&
     rm -rf tests &&
     rm -rf ./node_modules/*/test &&
     rm -rf ./node_modules/*/tests
-    echo '[medic-api] Available modules:' && ls node_modules &&
-    echo '[medic-api] package-lock.json' && cat package-lock.json &&
     cd -
 cd sentinel &&
-    echo '[medic-sentinel] Available modules:' && ls node_modules &&
     npm install --production &&
-    echo '[medic-sentinel] Available modules:' && ls node_modules &&
     rm -rf test &&
     rm -rf ./node_modules/*/test &&
     rm -rf ./node_modules/*/tests &&
-    echo '[medic-sentinel] Available modules:' && ls node_modules &&
-    echo '[medic-sentinel] package-lock.json' && cat package-lock.json &&
     cd -
 
 echo 'Building build for builds database...'
