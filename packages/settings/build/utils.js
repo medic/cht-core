b1a425f7f47345957d7dfd96473f1f380a091b77
Milan Andric <milan@medicmobile.org>
2017-07-14 16:19:18 -0300

    update settings package
    https://github.com/medic/medic-projects/issues/2473

--- packages/settings/build/utils.js
+++ packages/settings/build/utils.js
@@ -23,6 +23,11 @@ exports.prepareDoc = function (doc) {
 
 exports.addSettings = function (root, doc, settings, name) {
     name = name || 'packages/' + settings.name;
+    ['_url', '_utils'].forEach(function(prop) {
+      if (settings[prop]) {
+        delete settings[prop];
+      }
+    });
     var json = JSON.stringify(settings);
     var path = 'settings/' + name;
     var src = 'module.exports = ' + json + ';';
