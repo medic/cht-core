0236bd08329e053c5c4b73ff53d57dbc59352bb9
Estelle Comment <estelle.comment@gmail.com>
2017-07-24 17:19:34 +0200

    Add a refresh of the page at start of test to remove parasite modals. medic/medic-webapp#3689

--- tests/protractor/e2e/contacts/create-new-district.specs.js
+++ tests/protractor/e2e/contacts/create-new-district.specs.js
@@ -3,6 +3,7 @@ const commonElements = require('../../page-objects/common/common.po.js'),
 
 describe('Add new district tests : ', () => {
   it('should open new place form', () => {
+    browser.driver.navigate().refresh();
     commonElements.goToPeople();
     expect(commonElements.isAt('contacts-list'));
     expect(browser.getCurrentUrl()).toEqual(commonElements.getBaseUrl() + 'contacts/');
