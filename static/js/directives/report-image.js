0293797404e5c35cf022227cc2a3349475632758
Alex Anderson <anderson@medicmobile.org>
2017-07-24 14:07:58 +0000

    Use Object URLs instead of FileReader

--- static/js/directives/report-image.js
+++ static/js/directives/report-image.js
@@ -1,19 +1,27 @@
 angular.module('inboxDirectives').directive('reportImage',
-  function(DB) {
+  function($window, DB) {
     'use strict';
     'ngInject';
 
     return {
       template: '<div class="loader"></div>',
       link: function(scope, element, attr) {
+        var objectUrl;
+
         DB().getAttachment(attr.report, attr.path)
           .then(function(blob) {
-            var reader = new FileReader();
-            reader.readAsDataURL(blob);
-            reader.onloadend = function() {
-              $(element).replaceWith('<img class="report-image" src="data:' + blob.type + ';base64:' + reader.result + '"/>');
-            };
+            var $newImg = $('<img class="report-image"/>');
+            objectUrl = ($window.URL || $window.webkitURL).createObjectURL(blob);
+            $newImg.attr('src', objectUrl);
+            $(element).replaceWith($newImg);
           });
+
+        scope.$on('$destroy', function() {
+          if (objectUrl) {
+            ($window.URL || $window.webkitURL).revokeObjectURL(objectUrl);
+          }
+        });
+
       },
     };
 });
