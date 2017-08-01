5241a32ff541ba6400c40c45e42629e9a8984e31
Milan Andric <milan@medicmobile.org>
2017-08-01 11:27:13 -0300

    always update submodules to match parent branch during CI
    With the corresponding `branch = .` config in .gitmodules this will try
    to match on the same branch as the parent and otherwise default to
    master.
    
    https://git-scm.com/docs/git-submodule#git-submodule---branch

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -1,22 +1,27 @@
 #!/bin/bash -x
 
+set -o pipefail
+set -e
+
 ARGS='this.dependencies["kanso-gardener"] = null;'
 ARGS+='this.dependencies_included = true;'
 
 # Process append to the version string if pre-release
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     ARGS+="this.version += \"-alpha.$TRAVIS_BUILD_NUMBER\";"
-    # Run CI against latest master of submodules if this is master
-    git submodule update --remote api
-    git submodule update --remote sentinel
 elif [[ "$TRAVIS_TAG" =~ ^[0-9]+\.[0-9]+\.[0-9]+-(rc|beta)\.[0-9]+$ ]]; then
     ARGS+="this.version = \"$TRAVIS_TAG\";"
 fi
 
+# Run CI against submodule branch of the same name or master if not found.
+# Depends on `branch = .` config in .gitmodules.
+git submodule update --remote api
+git submodule update --remote sentinel
+
 # Install npm deps in module directories and tweak kanso gardener related
 # configs so it knows.
-npm install -g json && \
-cat kanso.json | json -o json-4 -e "$ARGS" > tmp.json && \
+npm install -g json
+cat kanso.json | json -o json-4 -e "$ARGS" > tmp.json
 mv tmp.json kanso.json
 
 exit 0;
