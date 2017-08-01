bac8d441419862085dad6c0f4ae4ba0ff74d2b8d
ngaruko <ngarukob@yahoo.com>
2017-08-01 13:12:19 +1200

    add-new-person test

---
+++ tests/protractor/e2e/contacts/add-new-person.js
@@ -0,0 +1,27 @@
+const commonElements = require('../../page-objects/common/common.po.js'),
+	contactPage = require('../../page-objects/contacts/contacts.po.js'),
+	helper = require('../../helper'),
+	utils = require('../../utils');
+	
+describe('Add new person tests : ', () => {
+	afterEach(done => {
+		utils.resetBrowser();
+		done();
+	});
+
+	afterAll(utils.afterEach);
+
+	it('should add new person', () => {
+		commonElements.goToPeople();
+		expect(commonElements.isAt('contacts-list'));
+		expect(browser.getCurrentUrl()).toEqual(commonElements.getBaseUrl() + 'contacts/');
+		contactPage.addNewDistrict('BedeDistrict');
+		contactPage.completeNewPersonForm('Bede');
+		helper.waitUntilReady(element(by.css('.card-title')));
+		let name = element(by.css('span.name.ng-binding'));
+		let district = element(by.tagName('h3'));
+		expect(district.getText()).toBe('BedeDistrict');
+		expect(name.getText()).toBe('Bede');
+		expect(element(by.css('.position')).getText()).toBe('Primary contact: Bede');
+	});
+});
