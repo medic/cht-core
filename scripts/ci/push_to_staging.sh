3eb0f6e444fc83587e26a481c92f455e107f598e
Stefan du Fresne <stefan@sdufresne.info>
2017-08-02 13:13:12 +0100

    Check package security before pubishing
    Allows for branches to exist with security issues, but prevents tags
    being created with insecure package.

--- scripts/ci/push_to_staging.sh
+++ scripts/ci/push_to_staging.sh
@@ -12,12 +12,24 @@
     rm -rf ./node_modules/*/test &&
     rm -rf ./node_modules/*/tests)
 
+npm install nsp -g
+
+# Final security check to block publishing if we have insecure
+# packages
+function nspcheck {
+    nsp check &&
+    (cd api && nsp check) &&
+    (cd sentinel && nsp check)
+}
+
 echo 'Building build for builds database...'
 if [[ -n "$TRAVIS_TAG" ]]; then
+    nspcheck
     node --stack_size=10000 `which kanso` push --minify \
             --id="$TRAVIS_TAG" \
             "$UPLOAD_URL"/_couch/builds
 else
+    nspcheck || true; # We're OK with this failing for branches
     node --stack_size=10000 `which kanso` push --minify \
             --id="$TRAVIS_BRANCH" \
             "$UPLOAD_URL"/_couch/builds
