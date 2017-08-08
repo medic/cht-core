6b5a443fb470ff5c0e93244d5df765b3d3df51d6
Milan Andric <milan@medicmobile.org>
2017-08-08 10:07:47 -0300

    Also push builds to builds database on staging from CI

---
+++ scripts/ci/push_to_staging.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo 'This is a pull request build, and will not be uploaded to builds database.'
+    exit
+fi
+
+# Attempt to reduce size of api and sentinel artifacts
+(cd sentinel &&
+    npm install --production &&
+    rm -rf test &&
+    rm -rf ./node_modules/*/test &&
+    rm -rf ./node_modules/*/tests)
+(cd api &&
+    npm install --production &&
+    rm -rf tests &&
+    rm -rf ./node_modules/*/test &&
+    rm -rf ./node_modules/*/tests)
+
+echo 'Building build for builds database...'
+if [[ -n "$TRAVIS_TAG" ]]; then
+    node --stack_size=10000 `which kanso` push --minify \
+            --id="$TRAVIS_TAG" \
+            "$UPLOAD_URL"/_couch/builds
+else
+    node --stack_size=10000 `which kanso` push --minify \
+            --id="$TRAVIS_BRANCH" \
+            "$UPLOAD_URL"/_couch/builds
+fi
+echo 'Build for build database built.'
