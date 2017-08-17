ee1bc254dc78d7df997c24976db7d38f39671adf
Gareth Bowen <gareth@medicmobile.org>
2017-08-18 09:28:03 +1200

    Escape invalid characters when syncing with meta db
    The invalid characters are converted to their UTF code to reduce
    the likelihood of collisions with other databases.
    
    The create user UI has been updated to fail validation if the
    username has unusual characters.
    
    medic/medic-webapp#3778

--- static/js/services/db.js
+++ static/js/services/db.js
@@ -2,6 +2,11 @@ var _ = require('underscore'),
     USER_DB_SUFFIX = 'user',
     META_DB_SUFFIX = 'meta';
 
+// Regex to test for characters that are invalid in db names
+// Only lowercase characters (a-z), digits (0-9), and any of the characters _, $, (, ), +, -, and / are allowed.
+// https://wiki.apache.org/couchdb/HTTP_database_API#Naming_and_Addressing
+var DB_NAME_BLACKLIST = /[^a-z0-9_$()+/-]/g;
+
 angular.module('inboxServices').factory('DB',
   function(
     $window,
@@ -21,6 +26,17 @@ angular.module('inboxServices').factory('DB',
 
     $window.PouchDB.adapter('worker', require('worker-pouch/client'));
 
+    var getUsername = function(remote) {
+      var username = Session.userCtx().name;
+      if (!remote) {
+        return username;
+      }
+      // escape username in case they user invalid characters
+      return username.replace(DB_NAME_BLACKLIST, function(match) {
+        return '(' + match.charCodeAt(0) + ')';
+      });
+    };
+
     var getDbName = function(remote, meta) {
       var parts = [];
       if (remote) {
@@ -30,7 +46,7 @@ angular.module('inboxServices').factory('DB',
       }
       if (!remote || meta) {
         parts.push(USER_DB_SUFFIX);
-        parts.push(Session.userCtx().name);
+        parts.push(getUsername(remote));
       }
       if (meta) {
         parts.push(META_DB_SUFFIX);
