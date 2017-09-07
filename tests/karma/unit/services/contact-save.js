c04f5027ce8e9a56fad4a5ed267478c25611f371
Alex Anderson <anderson@medicmobile.org>
2017-09-07 09:14:04 +0000

    fix assertion

--- tests/karma/unit/services/contact-save.js
+++ tests/karma/unit/services/contact-save.js
@@ -64,7 +64,7 @@ describe('ContactSave service', () => {
 
         assert.equal(savedDocs[2]._id, 'main1');
 
-        assert(ExtractLineage.calledOnce);
+        assert.isTrue(ExtractLineage.calledOnce);
       });
   });
 
