bd95bf17a262402873001fb1795d312e9a68a0af
Marc Abbyad <marc@medicmobile.org>
2017-09-15 16:51:12 -0400

    Updated Standard config
    - added PNC card to profiles
    - improved context for Standard forms
    - fixed immunization TextForms
    - check for immunization TextForms for condition cards

--- standard/contact-summary.js
+++ standard/contact-summary.js
@@ -2,48 +2,54 @@ var cards = [];
 var context = {};
 var fields = [];
 
+var now = new Date();
 var MS_IN_DAY = 24*60*60*1000;  // 1 day in ms
 var MAX_DAYS_IN_PREGNANCY = 44*7;  // 44 weeks
 var DAYS_IN_PNC = 42;
 
+var deliveryForms = [
+  'D',
+  'delivery'
+];
+
 var IMMUNIZATION_DOSES = [
-  'bcg',
-  'cholera_1',
-  'cholera_2',
-  'cholera_3',
-  'hep_a_1',
-  'hep_a_2',
-  'hpv_1',
-  'hpv_2',
-  'hpv_3',
-  'flu',
-  'jap_enc',
-  'meningococcal_1',
-  'meningococcal_2',
-  'meningococcal_3',
-  'meningococcal_4',
-  'mmr_1',
-  'mmr_2',
-  'mmrv_1',
-  'mmrv_2',
-  'polio_0',
-  'polio_1',
-  'polio_2',
-  'polio_3',
-  'penta_1',
-  'penta_2',
-  'penta_3',
-  'pneumococcal_1',
-  'pneumococcal_2',
-  'pneumococcal_3',
-  'pneumococcal_4',
-  'rotavirus_1',
-  'rotavirus_2',
-  'rotavirus_3',
-  'typhoid_1',
-  'typhoid_2',
-  'vitamin_a',
-  'yellow_fever'
+  ['bcg','BCG'],
+  ['cholera_1','CH1'],
+  ['cholera_2','CH2'],
+  ['cholera_3','CH3'],
+  ['hep_a_1','HA1'],
+  ['hep_a_2','HA2'],
+  ['hpv_1','HPV1'],
+  ['hpv_2','HPV2'],
+  ['hpv_3','HPV3'],
+  ['flu','FLU'],
+  ['jap_enc','JE'],
+  ['meningococcal_1','MN1'],
+  ['meningococcal_2','MN2'],
+  ['meningococcal_3','MN3'],
+  ['meningococcal_4','MN4'],
+  ['mmr_1','MMR1'],
+  ['mmr_2','MMR2'],
+  ['mmrv_1','MMRV1'],
+  ['mmrv_2','MMRV2'],
+  ['polio_0','OPV0'],
+  ['polio_1','OPV1'],
+  ['polio_2','OPV2'],
+  ['polio_3','OPV3'],
+  ['penta_1','PEN1'],
+  ['penta_2','PEN2'],
+  ['penta_3','PEN3'],
+  ['pneumococcal_1','PCV1'],
+  ['pneumococcal_2','PCV2'],
+  ['pneumococcal_3','PCV3'],
+  ['pneumococcal_4','PCV4'],
+  ['rotavirus_1','RV1'],
+  ['rotavirus_2','RV2'],
+  ['rotavirus_3','RV3'],
+  ['typhoid_1','TY1'],
+  ['typhoid_2','TY2'],
+  ['vitamin_a','VA'],
+  ['yellow_fever','YF']
 ];
 
 var IMMUNIZATION_LIST = [
@@ -106,24 +112,84 @@ var isFacilityDelivery = function(report) {
   return report && report.fields && report.fields.delivery_code.toLowerCase() === 'f';
 };
 
+var isNonFacilityDelivery = function(report) {
+  return report && deliveryForms.indexOf(report.form) && report.fields && report.fields.delivery_code && report.fields.delivery_code.toLowerCase() !== 'f';
+};
+
+var getBirthDate = function(report) {
+  return report && (report.birth_date || report.fields.birth_date || report.reported_date);
+};
+
+var getPNCperiod = function(deliveryReport){
+  var obj = {};
+  obj.start = new Date(getBirthDate(deliveryReport));
+  obj.end = new Date(obj.start.getFullYear(), obj.start.getMonth(), obj.start.getDate() + DAYS_IN_PNC);
+  return obj;
+};
+/*
+var isInPNCPeriod = function(deliveryReport, start, end){
+  if (deliveryReport && deliveryReport.fields) {
+    var now = new Date();
+    return now >= newestPNCperiod.start && now <= newestPNCperiod.end;
+  }
+  else {
+    return false;
+  }
+};
+*/
+
+var isHighRiskPregnancy = function(reports, pregnancy) {
+  // Pregnancy is high risk:
+  // - if `pregnancy` form was used and has risk factors or danger signs in report
+  // - if any Flag reports were submitted during time of pregnancy
+  return (pregnancy.form === 'pregnancy' && pregnancy.fields && (pregnancy.fields.risk_factors || pregnancy.fields.danger_signs))
+        || isFormSubmittedInWindow(reports, 'F', pregnancy.reported_date, addDate(new Date(pregnancy.reported_date), MAX_DAYS_IN_PREGNANCY).getTime());
+};
+
+var isHighRiskPostnatal = function (reports, period) {
+  // High risk postnatal period if: 
+  //  - Delivery is not at facility
+  //  - F report in PNC period
+  //  - postnatal_visit with risk factor or danger sign
+  var highRisk = false;
+  if (period && period.start && period.end) {
+    reports.forEach(function(report) {
+      if (isNonFacilityDelivery(report) 
+          || (report.form.toUpperCase() === 'F' && report.reported_date >= period.end.getTime() && report.reported_date <= period.end.getTime())
+          || (report.form === 'postnatal_visit' && report.fields && (report.fields.risk_factors || report.fields.danger_signs))) {
+        highRisk = true;
+      }
+    });
+  }
+  return highRisk;
+};
+
+var getDeliveryCode = function(report) {
+  var code = ((report && report.fields) && ((report.fields.group_note && report.fields.group_note.default_chw_sms) || report.fields.delivery_code)) || 'unknown';
+  
+  // no need to distinguish the prefix `anc_only_`, so remove it if it is there
+  code = code.replace('anc_only_', '').toLowerCase();
+  return code;
+};
+
 var initImmunizations = function() {
   var master = {};
   IMMUNIZATION_DOSES.forEach(function(dose) {
-    master[dose] = false;
+    master[dose[0]] = false;
   });
   return master;
 };
  
 var addImmunizations = function(master, vaccines_received) {
   IMMUNIZATION_DOSES.forEach(function(dose) {
-    master[dose] = master[dose] || ( vaccines_received['received_' + dose] == 'yes' );
+    master[dose[0]] = master[dose[0]] || ( vaccines_received['received_' + dose[0]] == 'yes' ) || ( typeof(vaccines_received) === 'string' && vaccines_received.toUpperCase() === dose[1] );
   });
 };
 
 var countDosesReceived = function(master, name) {
   var count = 0;
   IMMUNIZATION_DOSES.forEach(function(dose) {
-    if (master[dose] && dose.indexOf(name + '_') === 0) { 
+    if (master[dose[0]] && dose[0].indexOf(name + '_') === 0) { 
       count++;
     }
   });
@@ -133,7 +199,7 @@ var countDosesReceived = function(master, name) {
 var countDosesPossible = function(name) {
   var count = 0;
   IMMUNIZATION_DOSES.forEach(function(dose) {
-    if (dose.indexOf(name + '_') === 0) {
+    if (dose[0].indexOf(name + '_') === 0) {
       count++;
     }
   });
@@ -144,7 +210,7 @@ var isSingleDose = function(name) {
   // Single doses wont be followed by an underscore in the list of doses
   var singleDose = true;
   IMMUNIZATION_DOSES.forEach(function(dose) {
-    if (dose.indexOf(name + '_') === 0) { 
+    if (dose[0].indexOf(name + '_') === 0) { 
       singleDose = false;
     }
   });
@@ -216,24 +282,19 @@ if (contact.type === 'person') {
     ageInMonths = (Math.abs(ageInMs.getFullYear() - 1970) * 12) + ageInMs.getMonth();
   }
 
+  // Find PNC period based on delivery date, not reported date
+  var newestDelivery = null;
+
   reports.forEach(function(report) {
     if (report.form === 'pregnancy' || report.form === 'P') {
       var subsequentDeliveries = reports.filter(function(report2) {
         return (report2.form === 'delivery' || report2.form === 'D') && report2.reported_date > report.reported_date;
       });
       
-      // Handle past pregnancies
+      // HANDLE PAST PREGNANCIES
       if (subsequentDeliveries.length > 0) {
         var relevantDelivery = getOldestReport(subsequentDeliveries);
-        var birthdate = relevantDelivery.birth_date || relevantDelivery.fields.birth_date || relevantDelivery.reported_date;
-        // High risk status for previous pregnancies is temporarily unused
-        // var isHighRiskPregnancy = (report.form === 'pregnancy' && report.fields && (report.fields.risk_factors || report.fields.danger_signs))
-        //      || isFormSubmittedInWindow(reports, 'F', report.reported_date, relevantDelivery.reported_date);
-
-        var deliveryCode = (relevantDelivery.fields.group_note && relevantDelivery.fields.group_note.default_chw_sms) || relevantDelivery.fields.delivery_code || 'unknown';
-        
-        // no need to distinguish the prefix `anc_only_`, so remove it
-        deliveryCode = deliveryCode.replace('anc_only_', '').toLowerCase();
+        var birthdate = getBirthDate(relevantDelivery);
         
         var relevantVisitsANC = reports.filter(function(report2) {
           return (report2.form === 'V' || report2.form === 'pregnancy_visit') && report2.reported_date > report.reported_date && report2.reported_date < relevantDelivery.reported_date;
@@ -250,14 +311,14 @@ if (contact.type === 'person') {
         }
 
         pastPregnancies.fields.push(
-          { label: 'contact.profile.delivery_code.' + deliveryCode, value: birthdate, filter: 'relativeDay', width: 4 },
+          { label: 'contact.profile.delivery_code.' + getDeliveryCode(relevantDelivery), value: birthdate, filter: 'relativeDay', width: 4 },
           { label: 'contact.profile.anc_visit', value: 'contact.profile.visits.of', translate: true, context: { count: visitsANC, total: 4 }, width: 4 },
           { label: 'contact.profile.pnc_visit', value: 'contact.profile.visits.of', translate: true, context: { count: visitsPNC, total: 4 }, width: 4 }
         );
         return;
       }
       
-      // Handle current pregnancy
+      // HANDLE CURRENT PREGNANCY
       var subsequentVisits = reports.filter(function(report2) {
         return (report2.form === 'pregnancy_visit' || report2.form === 'V') && report2.reported_date > report.reported_date;
       });
@@ -265,12 +326,7 @@ if (contact.type === 'person') {
             
       var edd = report.expected_date || report.fields.edd_8601;
       
-      // Pregnancy is high risk:
-      // - if `pregnancy` form was used and has risk factors or danger signs in report
-      // - if any Flag reports were submitted during time of pregnancy
-      // It is ok to check past the delivery date because that only current pregancy info will be shown
-      var isHighRiskPregnancy = (report.form === 'pregnancy' && report.fields && (report.fields.risk_factors || report.fields.danger_signs))
-        || isFormSubmittedInWindow(reports, 'F', report.reported_date, addDate(new Date(report.reported_date), MAX_DAYS_IN_PREGNANCY).getTime());
+      var highRiskPregnancy = isHighRiskPregnancy(reports, report);
 
       if (!pregnancy || pregnancyDate < report.reported_date) {
         pregnancyDate = report.reported_date;
@@ -279,8 +335,8 @@ if (contact.type === 'person') {
           fields: [
             { label: 'contact.profile.edd', value: edd, filter: 'relativeDay', width: 12 },
             { label: 'contact.profile.visit', value: 'contact.profile.visits.of', translate: true, context: { count: subsequentVisits.length, total: 4 }, width: 6 },
-            { label: 'contact.profile.risk.title', value: isHighRiskPregnancy ? 'contact.profile.risk.high':'contact.profile.risk.normal', translate: true, width: 5},
-            { label: '', value: isHighRiskPregnancy ? 'risk':'', filter: 'resourceIcon', width: 1}
+            { label: 'contact.profile.risk.title', value: highRiskPregnancy ? 'contact.profile.risk.high':'contact.profile.risk.normal', translate: true, width: 5},
+            { label: '', value: highRiskPregnancy ? 'risk':'', filter: 'resourceIcon', width: 1}
           ]
         };
       }
@@ -293,7 +349,42 @@ if (contact.type === 'person') {
     else if (report.form === 'IMM') {
       addImmunizations(immunizations, report.fields);
     }
+    else if (report.form === 'D' || report.form === 'delivery') {
+      // track newest delivery report to know if person is in PNC period
+      if (!newestDelivery || (report.fields && newestDelivery.fields && report.fields.date_of_birth > newestDelivery.fields.date_of_birth)) {
+        newestDelivery = report;
+      }
+    }
+    else {
+      addImmunizations(immunizations, report.form);
+    }
   });
+
+  var newestPNCperiod = getPNCperiod(newestDelivery);
+  if (isCoveredByUseCaseInLineage(lineage, 'pnc') && now >= newestPNCperiod.start && now <= newestPNCperiod.end) {
+    context.in_pnc_period = true;
+    var highRiskPostnatal = isHighRiskPostnatal(reports, newestPNCperiod);
+    var relevantVisitsPNC = reports.filter(function(report2) {
+      return (report2.form === 'M' || report2.form === 'postnatal_visit') && report2.reported_date > newestDelivery.reported_date && report2.reported_date < (newestDelivery.reported_date + DAYS_IN_PNC*MS_IN_DAY) ;
+    });
+    var visitsPNC = relevantVisitsPNC.length;
+    if (isFacilityDelivery(newestDelivery)) {
+      visitsPNC++;
+    }
+
+    var postnatal_card = {
+      label: 'contact.profile.postnatal',
+      fields: [
+        { label: 'contact.profile.delivery_code.' + getDeliveryCode(newestDelivery), value: getBirthDate(newestDelivery), filter: 'relativeDay', width: 12 },
+        { label: 'contact.profile.pnc_visit', value: 'contact.profile.visits.of', translate: true, context: { count: visitsPNC, total: 4 }, width: 6 },
+        { label: 'contact.profile.risk.title', value: highRiskPostnatal ? 'contact.profile.risk.high':'contact.profile.risk.normal', translate: true, width: 5},
+        { label: '', value: highRiskPostnatal ? 'risk':'', filter: 'resourceIcon', width: 1}
+      ]
+    };
+    cards.push(postnatal_card);
+  }
+
+  // Determine which condition cards to show
   if (pregnancy) {
     cards.push(pregnancy);
   }
@@ -341,6 +432,10 @@ use_cases.pnc = isCoveredByUseCaseInLineage(lineage, 'pnc');
 use_cases.imm = isCoveredByUseCaseInLineage(lineage, 'imm');
 
 context.use_cases = use_cases;
+
+console.log(contact);
+console.log(context);
+console.log(newestDelivery);
 
 var result = {
   fields: fields,
