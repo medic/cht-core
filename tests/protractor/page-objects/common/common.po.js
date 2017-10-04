ea0adacc488f3cddcfc8e499952ea2140b33ad57
ngaruko <ngarukob@yahoo.com>
2017-10-04 14:45:14 +1100

    no changes...just for testing 10

--- tests/protractor/page-objects/common/common.po.js
+++ tests/protractor/page-objects/common/common.po.js
@@ -1,7 +1,7 @@
 const helper = require('../../helper'),
       utils = require('../../utils');
 
-const medicLogo = element(by.className('logo-full'));//no changes...just for testing 9
+const medicLogo = element(by.className('logo-full'));//no changes...just for testing 10
 const messagesLink = element(by.id('messages-tab'));
 const tasksLink = element(by.id('tasks-tab'));
 const contactsLink = element(by.id('contacts-tab'));
