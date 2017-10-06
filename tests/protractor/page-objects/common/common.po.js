fc0bad02b181a0f8101e45c2fd2c80c551757954
ngaruko <ngarukob@yahoo.com>
2017-10-06 13:54:20 +1100

    no change ---just testing 3

--- tests/protractor/page-objects/common/common.po.js
+++ tests/protractor/page-objects/common/common.po.js
@@ -1,7 +1,7 @@
 const helper = require('../../helper'),
       utils = require('../../utils');
 
-const medicLogo = element(by.className('logo-full'));//no change ---just testing 2
+const medicLogo = element(by.className('logo-full'));//no change ---just testing 3
 const messagesLink = element(by.id('messages-tab'));
 const tasksLink = element(by.id('tasks-tab'));
 const contactsLink = element(by.id('contacts-tab'));
