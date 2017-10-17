0a4c3d0a0eae10d23f6e10aac9f795b61b0cbebe
ngaruko <ngarukob@yahoo.com>
2017-10-18 08:20:44 +1300

    for testing

--- tests/protractor/utils.js
+++ tests/protractor/utils.js
@@ -149,7 +149,7 @@ const refreshToGetNewSettings = () => {
 const revertDb = () => {
   return revertSettings().then(needsRefresh => {
     return deleteAll().then(() => {
-      // only need to refresh if the settings were changed
+      // only needs to refresh if the settings were changed
       if (needsRefresh) {
         return refreshToGetNewSettings();
       }
