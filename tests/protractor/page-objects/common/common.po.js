9668375194e2e1bff1397b9c01aa5712e380c2b0
ngaruko <ngarukob@yahoo.com>
2017-10-17 14:59:14 +1300

    added wait for page to load completely upon clicking tabs

--- tests/protractor/page-objects/common/common.po.js
+++ tests/protractor/page-objects/common/common.po.js
@@ -17,16 +17,19 @@ module.exports = {
   goToMessages: () => {
     helper.waitUntilReady(messagesLink);
     messagesLink.click();
+    helper.waitElementToBeVisisble(element(by.css('message-list')));
   },
 
   goToTasks: () => {
     helper.waitUntilReady(tasksLink);
     tasksLink.click();
+    helper.waitElementToBeVisisble(element(by.css('tasks-list')));
   },
 
   goToPeople: () => {
     helper.waitUntilReady(contactsLink);
     contactsLink.click();
+    helper.waitElementToBeVisisble(element(by.css('contacts-list')));
   },
 
   goToReports: () => {
