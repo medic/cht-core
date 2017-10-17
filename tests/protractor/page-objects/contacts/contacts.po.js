52d2d8f1c920814d61000b9c292b24daf556c0ea
ngaruko <ngarukob@yahoo.com>
2017-10-17 17:17:35 +1300

    wait for element to be visible

--- tests/protractor/page-objects/contacts/contacts.po.js
+++ tests/protractor/page-objects/contacts/contacts.po.js
@@ -30,6 +30,7 @@ module.exports = {
     helper.waitUntilReady(newPlaceForm);
     newPlaceName.sendKeys(districtName);
     newPersonButton.click();
+    helper.waitElementToBeVisisble(externalId);
     externalId.sendKeys('1245');
     notesTextArea.sendKeys('Some notes, just for testing purposes.&element.all(by.css@#!_)_@519874-#@1-3-element.all(by.css^%%');
     nextButton.click();
