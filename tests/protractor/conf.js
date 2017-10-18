a5d8a9ec66455dc137a602b5ba464c94fbc1fd7c
ngaruko <ngarukob@yahoo.com>
2017-10-18 13:08:05 +1300

    added headless to chromeOptions

--- tests/protractor/conf.js
+++ tests/protractor/conf.js
@@ -81,7 +81,7 @@ exports.config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: ['--disable-gpu', '--window-size=1024,768']
+      args: ['--headless', '--disable-gpu', '--window-size=1024,768']
     }
     // browserName: 'firefox',
     // 'marionette':'true'
