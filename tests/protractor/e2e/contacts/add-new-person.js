a648b650b77af55d50bf380d6b53019be46e7a95
ngaruko <ngarukob@yahoo.com>
2017-10-20 16:25:24 +1300

    wait for contact page to load

--- tests/protractor/e2e/contacts/add-new-person.js
+++ tests/protractor/e2e/contacts/add-new-person.js
@@ -14,6 +14,7 @@ describe('Add new person tests : ', () => {
   it('should add new person', () => {
     commonElements.goToPeople();
     expect(commonElements.isAt('contacts-list'));
+    helper.waitElementToBeVisisble(element(by.css('[ng-show="!error && !loading && !hasContacts"]')));
     contactPage.addNewDistrict('BedeDistrict');
     contactPage.completeNewPersonForm('Bede');
     helper.waitUntilReady(element(by.css('.card .heading')));
