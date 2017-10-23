b1f249bf0d00a7291f30dbf91ad0014189eee37d
ngaruko <ngarukob@yahoo.com>
2017-10-24 10:14:16 +1300

    removed refresh

--- tests/protractor/e2e/contacts/add-new-person.js
+++ tests/protractor/e2e/contacts/add-new-person.js
@@ -14,7 +14,6 @@ describe('Add new person tests : ', () => {
   it('should add new person', () => {
     commonElements.goToPeople();
     expect(commonElements.isAt('contacts-list'));
-    browser.refresh();
     helper.waitElementToBeVisisble(element(by.css('[ng-show="!error && !loading && !hasContacts"]')));
     contactPage.addNewDistrict('BedeDistrict');
     contactPage.completeNewPersonForm('Bede');
