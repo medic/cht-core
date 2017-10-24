0b4cbc7848ab3cc49530f04852336820ce20e850
ngaruko <ngarukob@yahoo.com>
2017-10-24 15:00:35 +1300

    Fixed error in messageStatus

--- tests/protractor/e2e/sms-gateway.js
+++ tests/protractor/e2e/sms-gateway.js
@@ -172,7 +172,7 @@ describe('sms-gateway api', () => {
       //browser.sleep(1000); // without this the elements are found to be detached...
       const messageHeader = helper.getTextFromElement(element(by.css('#message-header .name')));
       const messageText = helper.getTextFromElement(element(by.css('#message-content li.incoming:first-child .data p:first-child')));
-      const messageStatus = helper.getTextFromElement(element(by.css('#message-content li.incoming:first-child .data .state.received')).getText());
+      const messageStatus = helper.getTextFromElement(element(by.css('#message-content li.incoming:first-child .data .state.received')));
       expect(messageHeader).toBe('+64271234567');
       expect(messageText).toBe('hello');
       expect(messageStatus).toBe('received');
