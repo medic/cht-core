a67740e3a682ffdefa7be127c783e59e4a5c8a3d
ngaruko <ngarukob@yahoo.com>
2017-10-25 10:11:34 +1300

    Enable synchronisation back

--- tests/protractor/conf.js
+++ tests/protractor/conf.js
@@ -98,7 +98,7 @@ exports.config = {
   onPrepare: () => {
     jasmine.getEnv().addReporter(utils.reporter);
     const startup = startModules();
-    browser.ignoreSynchronization = true;
+    browser.waitForAngularEnabled(false);
     browser.driver.wait(startup, 30 * 1000, 'API and Sentinel should start within 30 seconds');
     browser.driver.sleep(1000);
     browser.driver.wait(setupSettings, 5 * 1000, 'Settings should be setup within 5 seconds');
