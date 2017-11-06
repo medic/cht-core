b0b5c89c61903da2c9bb9bb1cb117d33ee7d563a
ngaruko <ngarukob@yahoo.com>
2017-11-07 10:01:31 +1300

    test 3

--- tests/protractor/helper.js
+++ tests/protractor/helper.js
@@ -135,7 +135,7 @@ module.exports = {
     browser.manage().logs().get('browser').then(function(browserLogs) {
       // browserLogs is an array of objects with level and message fields
       browserLogs.forEach(function(log) {
-        if (log.level.value > 900) { // it's an error log
+        if (log.level.value > 900) { // it's an error log.
           fs.appendFile(`tests/results/${spec}-logs.txt`, `\r\n Console errors: ${log.message}\r\n`, function(err) {
             if (err) { throw err; }
           });
