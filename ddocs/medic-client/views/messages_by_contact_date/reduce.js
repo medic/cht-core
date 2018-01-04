3aeea65ffd743fa50a23f0178e0a42a09a25374c
Alex Anderson <anderson@medicmobile.org>
2018-01-04 15:48:03 +0000

    Fix bug in unicode character splitting
    Issue: medic/medic-projects#3248

--- ddocs/medic-client/views/messages_by_contact_date/reduce.js
+++ ddocs/medic-client/views/messages_by_contact_date/reduce.js
@@ -1,16 +1,17 @@
 function(key, values) {
-  var latest = { date: 0 };
+  var code, endPoint, latest = { date: 0 };
   values.forEach(function(value) {
     if (value.date > latest.date) {
       latest = value;
     }
   });
   if (latest.message) {
-    var code = latest.message.charCodeAt(99);
-    var endPoint = 100;
-    if (0xD800 <= code && code <= 0xDBFF) {
-      // trim off the first half of the unicode character to avoid corruption
-      endPoint = 99;
+    // trim off earlier parts of combined characters to avoid corruption
+    for(endPoint = 50; endPoint > 0; --endPoint) {
+      code = latest.message.charCodeAt(endPoint - 1);
+      if (0xD800 < code || code > 0xDBFF) {
+        break;
+      }
     }
     latest.message = latest.message.substr(0, endPoint);
   }
