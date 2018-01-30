d00e8b572beb3b193b16891c7fe1c77567430b81
Pancho Rifa <victor@1910.io>
2018-01-30 08:43:16 -0700

    Updating directive name from ngEnter to mmEnter

--- tests/karma/unit/directives/enter.js
+++ tests/karma/unit/directives/enter.js
@@ -18,25 +18,25 @@ describe('enter directive', function() {
   });
 
   it('it should call the mock function on pressing enter', done => {
-    compile('<div><a ng-enter="mockFunction()">Test</a></div>')(scope);
+    compile('<div><a mm-enter="mockFunction()">Test</a></div>')(scope);
     scope.$digest();
-    chai.expect(scope.mockFunction.called).to.be.false;
+    chai.expect(scope.mockFunction.called).to.equal(false);
     document.triggerHandler({ type: 'keydown', which: 13 });
 
     setTimeout(function() {
-      chai.expect(scope.mockFunction.called).to.be.true;
+      chai.expect(scope.mockFunction.called).to.equal(true);
       done();
     });
   });
 
   it('it should not call the mock function on pressing any key', done => {
-    compile('<div><a ng-enter="mockFunction()">Test</a></div>')(scope);
+    compile('<div><a mm-enter="mockFunction()">Test</a></div>')(scope);
     scope.$digest();
-    chai.expect(scope.mockFunction.called).to.be.false;
+    chai.expect(scope.mockFunction.called).to.equal(false);
     document.triggerHandler({ type: 'keydown', which: 27 });
 
     setTimeout(function() {
-      chai.expect(scope.mockFunction.called).to.be.false;
+      chai.expect(scope.mockFunction.called).to.equal(false);
       done();
     });
   });
