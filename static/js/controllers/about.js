d448bdf1146f468a7fa6c89ded1061e6fde26054
Stefan du Fresne <stefan@sdufresne.info>
2018-02-01 16:50:02 +0000

    Shared lib spike (#4153)
    Round two. This will be reverted at some point.

--- static/js/controllers/about.js
+++ static/js/controllers/about.js
@@ -1,3 +1,5 @@
+var helloWorld = require('hello-world');
+
 angular.module('inboxControllers').controller('AboutCtrl',
   function (
     $interval,
@@ -49,6 +51,8 @@ angular.module('inboxControllers').controller('AboutCtrl',
         $interval.cancel(dataUsageUpdate);
       });
     }
+
+    helloWorld();
 
     DB().info().then(function (result) {
       $scope.dbInfo = result;
