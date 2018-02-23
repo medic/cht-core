1c72fe7a9cbeca8740269ffd24e85d8ccfc85369
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2018-02-23 16:44:58 +1300

    Fix indentation and convert to es6 style

--- sentinel/transitions/update_clinics.js
+++ sentinel/transitions/update_clinics.js
@@ -1,92 +1,90 @@
-var _ = require('underscore'),
-    logger = require('../lib/logger'),
-    transitionUtils = require('./utils'),
-    lineage = require('../lib/lineage'),
-    NAME = 'update_clinics';
+const _ = require('underscore'),
+      logger = require('../lib/logger'),
+      transitionUtils = require('./utils'),
+      lineage = require('../lib/lineage'),
+      NAME = 'update_clinics';
 
-var associateContact = function(audit, doc, contact, callback) {
-    var self = module.exports;
+const associateContact = (audit, doc, contact, callback) => {
+  const self = module.exports;
 
-    // reporting phone stayed the same and contact data is up to date
-    if (doc.from === contact.phone &&
-        doc.contact &&
-        contact._id === doc.contact._id) {
-        return callback();
-    }
+  // reporting phone stayed the same and contact data is up to date
+  if (doc.from === contact.phone &&
+      doc.contact &&
+      contact._id === doc.contact._id) {
+    return callback();
+  }
 
-    if (contact.phone !== doc.from) {
-        contact.phone = doc.from;
-        audit.saveDoc(contact, function(err) {
-            if (err) {
-                logger.error('Error updating contact: ' + JSON.stringify(err, null, 2));
-                return callback(err);
-            }
-            self.setContact(doc, contact, callback);
-        });
-    } else {
-        self.setContact(doc, contact, callback);
-    }
+  if (contact.phone !== doc.from) {
+    contact.phone = doc.from;
+    audit.saveDoc(contact, err => {
+      if (err) {
+        logger.error('Error updating contact: ' + JSON.stringify(err, null, 2));
+        return callback(err);
+      }
+      self.setContact(doc, contact, callback);
+    });
+  } else {
+    self.setContact(doc, contact, callback);
+  }
 };
 
-var getHydratedContact = function(id, callback) {
+const getHydratedContact = (id, callback) => {
   return lineage
     .fetchHydratedDoc(id)
-    .then(function(contact) {
+    .then(contact => {
       callback(null, contact);
     })
-    .catch(function(err) {
+    .catch(err => {
       callback(err);
     });
 };
 
-var getContact = function(db, doc, callback) {
-    if (doc.refid) { // use reference id to find clinic if defined
-        let params = {
-            key: [ 'external', doc.refid ],
-            include_docs: true,
-            limit: 1
-        };
-        db.medic.view('medic-client', 'contacts_by_reference', params, function(err, data) {
-            if (err) {
-                return callback(err);
-            }
-            if (!data.rows.length) {
-                return callback();
-            }
-            var result = data.rows[0].doc;
-            if (result.type === 'person') {
-              return getHydratedContact(result._id, callback);
-            }
-            if (result.type === 'clinic') {
-                var id = result.contact && result.contact._id;
-                if (!id) {
-                    return callback(null, result.contact || { parent: result });
-                }
+const getContact = (db, doc, callback) => {
+  if (doc.refid) { // use reference id to find clinic if defined
+    let params = {
+      key: [ 'external', doc.refid ],
+      include_docs: true,
+      limit: 1
+    };
+    db.medic.view('medic-client', 'contacts_by_reference', params, (err, data) => {
+      if (err) {
+        return callback(err);
+      }
+      if (!data.rows.length) {
+        return callback();
+      }
+      const result = data.rows[0].doc;
+      if (result.type === 'person') {
+        return getHydratedContact(result._id, callback);
+      }
+      if (result.type === 'clinic') {
+        const id = result.contact && result.contact._id;
+        if (!id) {
+          return callback(null, result.contact || { parent: result });
+        }
 
-                return getHydratedContact(id, callback);
-            }
-            callback();
-        });
-    } else if (doc.from) {
-        let params = {
-            key: String(doc.from),
-            include_docs: false,
-            limit: 1
-        };
-        db.medic.view('medic-client', 'contacts_by_phone', params, function(err, data) {
-            if (err) {
-                return callback(err);
-            }
-
-            if (data.rows.length && data.rows[0].id) {
-              return getHydratedContact(data.rows[0].id, callback);
-            }
-
-            return callback();
-        });
-    } else {
-        callback();
-    }
+        return getHydratedContact(id, callback);
+      }
+      callback();
+    });
+  } else if (doc.from) {
+    let params = {
+      key: String(doc.from),
+      include_docs: false,
+      limit: 1
+    };
+    db.medic.view('medic-client', 'contacts_by_phone', params, (err, data) => {
+      if (err) {
+        return callback(err);
+      }
+      if (data.rows.length && data.rows[0].id) {
+        return getHydratedContact(data.rows[0].id, callback);
+      }
+      return callback();
+    });
+  } else {
+    callback();
+  }
 };
 
 /**
@@ -98,31 +96,31 @@ var getContact = function(db, doc, callback) {
  * good place to get phone numbers from.
  */
 module.exports = {
-    filter: function(doc) {
-        return Boolean(
-            doc &&
-            doc.type === 'data_record' &&
-            !doc.contact &&
-            !transitionUtils.hasRun(doc, NAME)
-        );
-    },
-    onMatch: function(change, db, audit, callback) {
-        getContact(db, change.doc, function(err, contact) {
-            if (err) {
-                return callback(err);
-            }
-            if (!contact) {
-                return callback();
-            }
-            associateContact(audit, change.doc, contact, callback);
-        });
-    },
-    setContact: function(doc, contact, callback) {
-        doc.contact = contact;
-        // remove facility not found errors
-        doc.errors = _.reject(doc.errors, function(error) {
-            return error.code === 'sys.facility_not_found';
-        });
-        callback(null, true);
-    }
+  filter: doc => {
+    return Boolean(
+      doc &&
+      doc.type === 'data_record' &&
+      !doc.contact &&
+      !transitionUtils.hasRun(doc, NAME)
+    );
+  },
+  onMatch: (change, db, audit, callback) => {
+    getContact(db, change.doc, (err, contact) => {
+      if (err) {
+        return callback(err);
+      }
+      if (!contact) {
+        return callback();
+      }
+      associateContact(audit, change.doc, contact, callback);
+    });
+  },
+  setContact: (doc, contact, callback) => {
+    doc.contact = contact;
+    // remove facility not found errors
+    doc.errors = _.reject(doc.errors, error => {
+      return error.code === 'sys.facility_not_found';
+    });
+    callback(null, true);
+  }
 };
