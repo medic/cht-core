75aa213413dd4b2f3798e7df84a5d07c8b38f473
Robbie Howe <robbie@medicmobile.org>
2018-03-01 12:02:43 +0000

    Adds PouchDB to sentinel

---
+++ sentinel/db-pouch.js
@@ -0,0 +1,9 @@
+const PouchDB = require('pouchdb-core');
+PouchDB.plugin(require('pouchdb-adapter-http'));
+PouchDB.plugin(require('pouchdb-mapreduce'));
+
+const { COUCH_URL } = process.env;
+// strip trailing slash from to prevent bugs in path matching
+const couchUrl = COUCH_URL && COUCH_URL.replace(/\/$/, '');
+
+module.exports.medic = new PouchDB(couchUrl);
