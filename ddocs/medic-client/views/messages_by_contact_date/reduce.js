9c8c678fe03cfa5a3f9e1748c3b878d9501ff10c
Pancho Rifa <victor@1910.io>
2018-03-01 12:54:05 -0700

    Reducing the message summary to 50 chars and removing consecutive tab or break lines

--- ddocs/medic-client/views/messages_by_contact_date/reduce.js
+++ ddocs/medic-client/views/messages_by_contact_date/reduce.js
@@ -6,12 +6,12 @@ function(key, values) {
     }
   });
   if (latest.message) {
-    latest.message = latest.message.replace(/\n+/g, '\n');
-    var code = latest.message.charCodeAt(99);
-    var endPoint = 100;
+    latest.message = latest.message.replace(/\n+\t+/g, ' ');
+    var code = latest.message.charCodeAt(49);
+    var endPoint = 50;
     if (0xD800 <= code && code <= 0xDBFF) {
       // trim off the first half of the unicode character to avoid corruption
-      endPoint = 99;
+      endPoint = 49;
     }
     latest.message = latest.message.substr(0, endPoint);
   }
