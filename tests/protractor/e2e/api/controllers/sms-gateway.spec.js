dcb8acdc1a83372167beaabe2c8a5c93eca11a95
Alex Anderson <anderson@medicmobile.org>
2018-03-01 14:30:28 +0000

    Add test outlines

--- tests/protractor/e2e/api/controllers/sms-gateway.spec.js
+++ tests/protractor/e2e/api/controllers/sms-gateway.spec.js
@@ -26,6 +26,59 @@ fdescribe('/sms', function() {
         });
     });
 
+    it('should save supplied messages to DB', function() {
+      TODO();
+    });
+
+    it('should not reject bad message content', function() {
+      TODO();
+    });
+
+    it('should save all good messages in a request containing some good and some bad', () => TODO(`
+      1. send POST request containing some message JSON for some good and some bad
+         messages
+      2. check that no error is returned
+      3. check that only good messages were created in DB
+      4. if the endpoint is supposed to be non-blocking, add a waiting loop around
+         the relevant assertions.
+    `));
+
+    it('should update message in DB when status update is received', () => TODO(`
+      1. save a message in the DB
+      2. send POST request containing a status update for that message
+      3. check that message state was updated as expected
+    `));
+
+    it('should update message multiple times when multiple status updates for the same message are received', () => TODO(`
+      1. save a message in the DB
+      2. send POST request containing multiple status updates for that message
+      3. check that message state and state_history were updated as expected
+    `));
+
+    it('should still save messages when an unrecognised status update is received', () => TODO(`
+      1. send POST request containing a status update for an unknown message, and
+         a good message definition
+      2. check that no error is returned
+      3. check that message was created in DB
+      4. if the endpoint is supposed to be non-blocking, add a waiting loop around
+         the relevant assertions.
+    `));
+
+    it('should not save a message again if it\'s been seen before', () => TODO(`
+      1. POST a message to the endpoint
+      2. confirm that the message was saved to the DB
+      3. POST the same message to the endpoint again
+      4. confirm that the response was OK, and the message was _not_ saved again
+    `));
+
+    it('should not save a status update again if it\'s been seen before', () => TODO(`
+      1. save a WO message in the database
+      2. POST a status update for that message to the endpoint
+      3. confirm that the message state has been changed, and that the state update appears in the history
+      4. POST the same status update to the endpoint again
+      5. confirm that the response was OK, and the message state is still correct, but that the history has not been updated
+    `));
+
   });
 });
 
@@ -44,4 +97,8 @@ function post(body) {
     headers: { 'Content-Type':'application/json' },
     body: body,
   });
+}
+
+function TODO(message = 'this test') {
+  expect(message).toBe('implemented');
 }
