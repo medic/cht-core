0b36916ed6e276f18c62de89ccb949a5b8670668
Alex Anderson <anderson@medicmobile.org>
2018-03-06 17:10:04 +0000

    Sort tests better

--- tests/protractor/e2e/api/controllers/sms-gateway.spec.js
+++ tests/protractor/e2e/api/controllers/sms-gateway.spec.js
@@ -25,71 +25,94 @@ fdescribe('/sms', function() {
 
     afterAll(() => utils.revertDb());
 
-    it('should accept requests with missing fields', function() {
-      return post({})
-        .then(expectResponse({ messages:[] }));
+    describe('webapp-terminating messages', function() {
+
+      it('should accept requests with missing fields', function() {
+        return post({})
+          .then(expectResponse({ messages:[] }));
+      });
+
+      fit('should save supplied messages to DB', function() {
+        return postMessages(
+          {
+            id: 'test-sms-1',
+            from: '+111-test',
+            content: 'test 1',
+            sms_sent: 1511179010577,
+            sms_received: 1511189010577,
+          },
+          {
+            id: 'test-sms-2',
+            from: '+222-test',
+            content: 'test 2',
+            sms_sent: 1511179020577,
+            sms_received: 1511189020577,
+          }
+        )
+          .then(expectResponse({ messages:[] }))
+
+          .then(expectMessagesInDb('test 1', 'test 2'));
+      });
+
+      fit('should not reject bad message content', function() {
+        postMessage({ missing_fields:true })
+          .then(expectResponse({ messages:[] }));
+      });
+
+      fit('should save all good messages in a request containing some good and some bad', function() {
+        return postMessages(
+          {
+            bad_message: true,
+          },
+          {
+            good_message: true,
+            id: 'abc-123',
+            content: 'should be saved',
+            sms_sent: 1520354329376,
+            sms_received: 1520354329386,
+          },
+          {
+            good_message: false,
+          }
+        )
+          .then(expectResponse({ messages:[] }))
+
+          .then(expectMessagesInDb('should be saved'));
+        // TODO if the endpoint is supposed to be non-blocking, add a waiting loop around the relevant assertions.
+      });
+
+      it('should not save a message again if it\'s been seen before', () => TODO(`
+        1. POST a message to the endpoint
+        2. confirm that the message was saved to the DB
+        3. POST the same message to the endpoint again
+        4. confirm that the response was OK, and the message was _not_ saved again
+      `));
+
     });
 
-    fit('should save supplied messages to DB', function() {
-      return postMessages(
-        {
-          id: 'test-sms-1',
-          from: '+111-test',
-          content: 'test 1',
-          sms_sent: 1511179010577,
-          sms_received: 1511189010577,
-        },
-        {
-          id: 'test-sms-2',
-          from: '+222-test',
-          content: 'test 2',
-          sms_sent: 1511179020577,
-          sms_received: 1511189020577,
-        }
-      )
-        .then(expectResponse({ messages:[] }))
+    describe('webapp-originating messages', function() {
 
-        .then(expectMessagesInDb('test 1', 'test 2'));
+      it('should update message in DB when status update is received', () => TODO(`
+        1. save a message in the DB
+        2. send POST request containing a status update for that message
+        3. check that message state was updated as expected
+      `));
+
+      it('should update message multiple times when multiple status updates for the same message are received', () => TODO(`
+        1. save a message in the DB
+        2. send POST request containing multiple status updates for that message
+        3. check that message state and state_history were updated as expected
+      `));
+
+      it('should not save a status update again if it\'s been seen before', () => TODO(`
+        1. save a WO message in the database
+        2. POST a status update for that message to the endpoint
+        3. confirm that the message state has been changed, and that the state update appears in the history
+        4. POST the same status update to the endpoint again
+        5. confirm that the response was OK, and the message state is still correct, but that the history has not been updated
+      `));
+
     });
-
-    fit('should not reject bad message content', function() {
-      postMessage({ missing_fields:true })
-        .then(expectResponse({ messages:[] }));
-    });
-
-    fit('should save all good messages in a request containing some good and some bad', function() {
-      return postMessages(
-        {
-          bad_message: true,
-        },
-        {
-          good_message: true,
-          id: 'abc-123',
-          content: 'should be saved',
-          sms_sent: 1520354329376,
-          sms_received: 1520354329386,
-        },
-        {
-          good_message: false,
-        }
-      )
-        .then(expectResponse({ messages:[] }))
-
-        .then(expectMessagesInDb('should be saved'));
-      // TODO if the endpoint is supposed to be non-blocking, add a waiting loop around the relevant assertions.
-    });
-
-    it('should update message in DB when status update is received', () => TODO(`
-      1. save a message in the DB
-      2. send POST request containing a status update for that message
-      3. check that message state was updated as expected
-    `));
-
-    it('should update message multiple times when multiple status updates for the same message are received', () => TODO(`
-      1. save a message in the DB
-      2. send POST request containing multiple status updates for that message
-      3. check that message state and state_history were updated as expected
-    `));
 
     it('should still save messages when an unrecognised status update is received', () => TODO(`
       1. send POST request containing a status update for an unknown message, and
@@ -98,21 +121,6 @@ fdescribe('/sms', function() {
       3. check that message was created in DB
       4. if the endpoint is supposed to be non-blocking, add a waiting loop around
          the relevant assertions.
-    `));
-
-    it('should not save a message again if it\'s been seen before', () => TODO(`
-      1. POST a message to the endpoint
-      2. confirm that the message was saved to the DB
-      3. POST the same message to the endpoint again
-      4. confirm that the response was OK, and the message was _not_ saved again
-    `));
-
-    it('should not save a status update again if it\'s been seen before', () => TODO(`
-      1. save a WO message in the database
-      2. POST a status update for that message to the endpoint
-      3. confirm that the message state has been changed, and that the state update appears in the history
-      4. POST the same status update to the endpoint again
-      5. confirm that the response was OK, and the message state is still correct, but that the history has not been updated
     `));
 
     it('should return as quickly as possible', () => TODO(`
