64f16f93d68a947d3d22f0a4d5f221a1c75bec47
Alex Anderson <anderson@medicmobile.org>
2018-03-08 12:38:54 +0100

    Fail ci when setup fails (#4276)
    * Fail CI when setup breaks
    
    * Remove old submodule setup for ci
    
    * Remove no-op exit call from ci setup
    
    * Remove command chaining

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -eux
 
 ARGS='this.dependencies["kanso-gardener"] = null;'
 ARGS+='this.dependencies_included = true;'
@@ -6,17 +6,12 @@ ARGS+='this.dependencies_included = true;'
 # Process append to the version string if pre-release
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     ARGS+="this.version += \"-alpha.$TRAVIS_BUILD_NUMBER\";"
-    # Run CI against latest master of submodules if this is master
-    git submodule update --remote api
-    git submodule update --remote sentinel
 elif [[ "$TRAVIS_TAG" =~ ^[0-9]+\.[0-9]+\.[0-9]+-(rc|beta)\.[0-9]+$ ]]; then
     ARGS+="this.version = \"$TRAVIS_TAG\";"
 fi
 
 # Install npm deps in module directories and tweak kanso gardener related
 # configs so it knows.
-npm install -g json && \
-cat kanso.json | json -o json-4 -e "$ARGS" > tmp.json && \
+npm install -g json
+cat kanso.json | json -o json-4 -e "$ARGS" > tmp.json
 mv tmp.json kanso.json
-
-exit 0;
