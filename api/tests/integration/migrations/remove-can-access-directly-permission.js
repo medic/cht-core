6d4c10eca94b33e13fe14ad824eea5e676387601
Alex Anderson <anderson@medicmobile.org>
2018-03-22 17:12:55 +0000

    Add a test

---
+++ api/tests/integration/migrations/remove-can-access-directly-permission.js
@@ -0,0 +1,74 @@
+describe('remove-can-access-directly-permission migration', function() {
+
+  it('should throw a 404 error if ddoc not found', function() {
+    // given no setup
+
+    // when
+    return utils.runMigration('remove-can-access-directly-permission')
+
+      .then(() => { throw new Error('Expected migration to throw an error'); })
+
+      .catch(err => throw new Error('Caught expected error: ' + err);
+
+  });
+
+  it('should not throw an error if permission not present', function() {
+
+    // given
+    return utils.initDb([
+      {
+        _id: '_design/medic',
+        permissions: [
+          { name: 'ok_1' },
+          { name: 'ok_2' },
+        ],
+      },
+    ])
+
+      // when
+      .then(() => utils.runMigration('remove-can-access-directly-permission'))
+
+      // then
+      .then(() => utils.assertDb([
+        {
+          _id: '_design/medic',
+          permissions: [
+            { name: 'ok_1' },
+            { name: 'ok_2' },
+          ],
+        },
+      ]));
+
+  });
+
+  it('should remove relevant permission if found', function() {
+
+    // given
+    return utils.initDb([
+      {
+        _id: '_design/medic',
+        permissions: [
+          { name: 'ok_1' },
+          { name: 'can_access_directly' },
+          { name: 'ok_2' },
+        ],
+      },
+    ])
+
+      // when
+      .then(() => utils.runMigration('remove-can-access-directly-permission'))
+
+      // then
+      .then(() => utils.assertDb([
+        {
+          _id: '_design/medic',
+          permissions: [
+            { name: 'ok_1' },
+            { name: 'ok_2' },
+          ],
+        },
+      ]));
+
+  });
+
+});
