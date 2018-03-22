c98d55f30a00554521e95e567aba7d1b4057b0e6
Alex Anderson <anderson@medicmobile.org>
2018-03-22 17:52:29 +0000

    Revert "Remove test"
    This reverts commit 54600d79d816d7ac3da5e09c9c2c48bbf018173c.

---
+++ api/tests/integration/migrations/remove-can-access-directly-permission.js
@@ -0,0 +1,90 @@
+const db = require('../../../db-pouch').medic;
+const utils = require('./utils');
+
+describe('remove-can-access-directly-permission migration', function() {
+
+  let ddocBackup;
+
+  beforeEach(() => 
+    db.get('_design/medic')
+      .then(ddoc => ddocBackup = ddoc));
+
+  afterEach(() =>
+    db.get('_design/medic')
+      .then(ddoc => {
+        ddocBackup._rev = ddoc._rev;
+        return db.put(ddocBackup);
+      }));
+
+  it('should throw a 404 error if ddoc not found', function() {
+    // given no setup
+
+    // when
+    return utils.runMigration('remove-can-access-directly-permission')
+
+      .then(() => { throw new Error('Expected migration to throw an error'); })
+
+      .catch(err => { throw new Error('Caught expected error: ' + err); });
+
+  });
+
+  it('should not throw an error if permission not present', function() {
+
+    // given
+    return utils.initDb([
+      {
+        _id: '_design/medic',
+        permissions: [
+          { name: 'ok_1' },
+          { name: 'ok_2' },
+        ],
+      },
+    ])
+
+      // when
+      .then(() => utils.runMigration('remove-can-access-directly-permission'))
+
+      // then
+      .then(() => utils.assertDb([
+        {
+          _id: '_design/medic',
+          permissions: [
+            { name: 'ok_1' },
+            { name: 'ok_2' },
+          ],
+        },
+      ]));
+
+  });
+
+  it('should remove relevant permission if found', function() {
+
+    // given
+    return utils.initDb([
+      {
+        _id: '_design/medic',
+        permissions: [
+          { name: 'ok_1' },
+          { name: 'can_access_directly' },
+          { name: 'ok_2' },
+        ],
+      },
+    ])
+
+      // when
+      .then(() => utils.runMigration('remove-can-access-directly-permission'))
+
+      // then
+      .then(() => utils.assertDb([
+        {
+          _id: '_design/medic',
+          permissions: [
+            { name: 'ok_1' },
+            { name: 'ok_2' },
+          ],
+        },
+      ]));
+
+  });
+
+});
