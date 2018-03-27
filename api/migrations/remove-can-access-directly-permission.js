a51385f154220c0b9934201597c460666f3501fa
Alex Anderson <anderson@medicmobile.org>
2018-03-27 11:38:04 +0000

    Migration to remove can-access-directly permission
    Issue: #4144

---
+++ api/migrations/remove-can-access-directly-permission.js
@@ -0,0 +1,18 @@
+var db = require('../db-pouch').medic;
+
+module.exports = {
+  name: 'remove-can-access-directly-permission',
+  created: new Date(2018, 3, 27),
+  run: function(callback) {
+    db.get('_design/medic')
+      .then(ddoc => {
+        if(ddoc.app_settings.permissions) {
+          ddoc.app_settings.permissions = [];
+              ddoc.app_settings.permissions.filter(p => p.name !== 'can_access_directly');
+          return db.put(ddoc);
+        }
+      })
+      .then(() => callback())
+      .catch(callback);
+  },
+};
