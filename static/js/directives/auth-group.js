929b93643b9b62168a0a519a0b7dcfe863cda9fe
Diana Barsan <barsan@medicmobile.org>
2018-03-27 17:33:36 +0300

    Some refactors.

--- static/js/directives/auth-group.js
+++ static/js/directives/auth-group.js
@@ -15,40 +15,38 @@ angular.module('inboxDirectives').directive('mmAuthGroup', function(Auth) {
 
     link: function (scope, element) {
 
-      var process = function() {
+      var run = function() {
         element.addClass('hidden');
 
-        if (!_.isArray(scope.mmAuthGroup)) {
-          scope.mmAuthGroup = [scope.mmAuthGroup];
-        }
+        scope.mmAuthGroup = _.isArray(scope.mmAuthGroup) ? scope.mmAuthGroup : [scope.mmAuthGroup];
 
         if (_.any(scope.mmAuthGroup, function(element) {
-            return element === true;
-          })) {
+          return element === true;
+        })) {
           return element.removeClass('hidden');
         }
 
-        var permissions = _.map(_.filter(scope.mmAuthGroup, function(element) {
-          return typeof element === 'string' || _.isArray(element);
-        }), function(element) {
-          return (_.isArray(element) && _.flatten(element)) || element;
-        });
-
-        if (permissions.length) {
-          asyncSome(permissions, function (permissionsGroup, callback) {
-            Auth(permissionsGroup).then(function () {
-              return callback(null, true);
-            });
-          }, function (err, result) {
-            if (result) {
-              return element.removeClass('hidden');
-            }
+        var permissions = scope.mmAuthGroup
+          .filter(function(element) {
+            return _.isArray(element) || _.isString(element);
+          })
+          .map(function(element) {
+            return (_.isArray(element) && _.flatten(element)) || element;
           });
-        }
+
+        asyncSome(permissions, function (permissionsGroup, callback) {
+          Auth(permissionsGroup).then(function () {
+            return callback(null, true);
+          });
+        }, function (err, result) {
+          if (result) {
+            return element.removeClass('hidden');
+          }
+        });
       };
 
       scope.$watch('mmAuthGroup', function() {
-        process();
+        run();
       });
     }
   };
