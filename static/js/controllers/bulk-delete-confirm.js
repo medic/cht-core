7060f011a973bc47b465acd52caa0c66f7f85029
Stefan du Fresne <stefan@sdufresne.info>
2018-03-29 12:04:51 +0100

    Support both bulk delete and the much simpler single delete
    medic/medic-webapp#4113

---
+++ static/js/controllers/bulk-delete-confirm.js
@@ -0,0 +1,50 @@
+angular.module('inboxControllers').controller('BulkDeleteConfirm',
+  function(
+    $scope,
+    $timeout,
+    $translate,
+    $uibModalInstance,
+    DeleteDocs,
+    Snackbar
+  ) {
+    'use strict';
+    'ngInject';
+
+    $scope.totalDocsSelected = 0;
+    $scope.totalDocsDeleted = 0;
+    function updateTotalDocsDeleted(totalDocsDeleted) {
+      $timeout(function() {
+        $scope.totalDocsDeleted = totalDocsDeleted;
+      });
+    }
+
+    $scope.$on('modal.closing', function(e) {
+      if ($scope.deleteComplete) {
+        return window.location.reload();
+      }
+    })
+
+    $scope.submit = function() {
+      if ($scope.deleteComplete) {
+        return window.location.reload();
+      }
+
+      var docs = $scope.model.docs;
+      $scope.totalDocsSelected = docs.length;
+      $scope.totalDocsDeleted = 0;
+      $scope.setProcessing();
+      DeleteDocs(docs, { progress: updateTotalDocsDeleted })
+        .then(function() {
+          $scope.deleteComplete = true;
+          $scope.setFinished();
+        })
+        .catch(function(err) {
+          $scope.setError(err, 'Error deleting document');
+        });
+    };
+
+    $scope.cancel = function() {
+      $uibModalInstance.dismiss();
+    };
+  }
+);
--- static/js/controllers/inbox.js
+++ static/js/controllers/inbox.js
@@ -520,27 +520,32 @@ var feedback = require('../modules/feedback'),
         $rootScope.$broadcast.apply($rootScope, arguments);
       };
 
-      $scope.deleteDoc = function(docs) {
+      $scope.deleteDoc = function(doc) {
+        Modal({
+          templateUrl: 'templates/modals/delete_doc_confirm.html',
+          controller: 'DeleteDocConfirm',
+          model: { doc: doc }
+        }).then(function() {
+          if (!$scope.selectMode &&
+              ($state.includes('contacts') || $state.includes('reports'))) {
+            $state.go($state.current.name, { id: null });
+          }
+        });
+      }
+
+      $scope.bulkDelete = function(docs) {
         if (!docs) {
           $log.warn('Trying to delete empty object', docs);
           return;
-        }
-        if (!_.isArray(docs)) {
-          docs = [ docs ];
         }
         if (!docs.length) {
           $log.warn('Trying to delete empty array', docs);
           return;
         }
         Modal({
-          templateUrl: 'templates/modals/delete_doc_confirm.html',
-          controller: 'DeleteDocConfirm',
+          templateUrl: 'templates/modals/bulk_delete_confirm.html',
+          controller: 'BulkDeleteConfirm',
           model: { docs: docs }
-        }).then(function() {
-          if (!$scope.selectMode &&
-              ($state.includes('contacts') || $state.includes('reports'))) {
-            $state.go($state.current.name, { id: null });
-          }
         });
       };
 
