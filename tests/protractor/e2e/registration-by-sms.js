744f5e543b0a84574a1a343e22c56280247580a5
Diana Barsan <35681649+dianabarsan@users.noreply.github.com>
2018-04-03 12:21:36 +0300

    Show sender phone number instead of Unknown sender in Reports Content.
    medic/medic-webapp#4231

--- tests/protractor/e2e/registration-by-sms.js
+++ tests/protractor/e2e/registration-by-sms.js
@@ -220,7 +220,7 @@ describe('registration transition', () => {
 
     const checkItemSummary = () => {
       const summaryElement = element(by.css('#reports-content .item-summary'));
-      expect(summaryElement.element(by.css('.sender .name')).getText()).toBe(`Sent by ${CAROL.name}`);
+      expect(summaryElement.element(by.css('.sender .name')).getText()).toMatch(`${CAROL.name}`);
       expect(summaryElement.element(by.css('.subject .name')).getText()).toBe('Siobhan');
       expect(summaryElement.element(by.css('.sender .phone')).getText()).toBe(CAROL.phone);
       expect(summaryElement.element(by.css('.position a')).getText()).toBe(BOB_PLACE.name);
@@ -251,7 +251,7 @@ describe('registration transition', () => {
       helper.clickElement(element(by.css('#reports-list .unfiltered li:first-child .summary')));
 
       // wait for content to load
-      browser.wait(() => element(by.cssContainingText('#reports-content .item-summary .name', `Sent by ${CAROL.name}`)).isPresent(), 10000);
+      browser.wait(() => element(by.cssContainingText('#reports-content .item-summary .phone', CAROL.phone)).isPresent(), 10000);
 
       checkItemSummary();
       checkAutoResponse();
