e7ac0a1c719a982e6022609da5b2e2038c49818f
Stefan du Fresne <stefan@sdufresne.info>
2018-04-05 13:19:31 +0100

    Modifying upgrade to have controller / service

--- api/controllers/upgrade.js
+++ api/controllers/upgrade.js
@@ -1,68 +1,23 @@
-const PouchDB = require('pouchdb-core');
-PouchDB.plugin(require('pouchdb-adapter-http'));
+const auth = require('../auth'),
+      serverUtils = require('../server-utils');
 
-const buildDbUrl = 'https://staging.dev.medicmobile.org/_couch/builds';
+const service = require('../services/upgrade');
 
-const buildDb = new PouchDB(buildDbUrl);
-const targetDbUrl = process.env.COUCH_URL;
-const targetDb = new PouchDB(targetDbUrl);
-
-const stagedDdocName = name => name + ':staged';
-
-const ddocName = buildInfo =>
- `${buildInfo.namespace}:${buildInfo.application}:${buildInfo.version}`;
-
-module.exports = (buildInfo, username) => {
-  if (!buildInfo) {
-    throw new Error('Bad request');
-  }
-
-  // While we've set the api to in theory be more generic, this is for if we
-  // split this code out (and say, put it with horticulturalist). For now we
-  // just support medic deploying medic :-)
-  if (buildInfo.namespace !== 'medic' ||
-      buildInfo.application !== 'medic') {
-    return Promise.reject({
-      status: 400,
-      message: 'We only support medic as the application and namespace'
-    });
-  }
-
-  console.log('upgrade()', `Upgrading to ${JSON.stringify(buildInfo)}…`);
-
-  console.log('upgrade()', 'Fetching newDdoc…');
-  return buildDb
-    .get(ddocName(buildInfo), { attachments:true })
-    .catch(err => {
-      if (err.status === 404) {
-        err = new Error(`Version not found: ${buildInfo.version}`);
-      }
-
-      throw err;
-    })
-    .then(newDdoc => {
-      console.log('upgrade()', 'Fetched newDdoc.');
-
-      console.log('upgrade()', 'Fetching oldDdoc…');
-      return targetDb.get('_design/medic')
-        .then(oldDdoc => {
-          console.log('upgrade()', 'Fetched oldDdoc.');
-
-          newDdoc._id = stagedDdocName(oldDdoc._id);
-          delete newDdoc._rev;
-
-          newDdoc.deploy_info = {
-            timestamp: new Date().toString(),
-            user: username,
-            version: buildInfo.version,
+module.exports = {
+  upgrade: (req, res) => {
+    return auth.check(req, '_admin')
+      .then(userCtx => {
+        var buildInfo = req.body.build;
+        if (!buildInfo) {
+          throw {
+            message: 'You must provide a build info body',
+            status: 400
           };
+        }
 
-          console.log('upgrade()', `Staging new ddoc as ${newDdoc._id}`);
-          // FIXME: this can 409. If this happens blow away the existing staged
-          //        doc and put this new one.
-          //        https://github.com/medic/medic-webapp/issues/3806
-          return targetDb.put(newDdoc);
-        })
-        .then(() => console.log('upgrade()', 'newDdoc uploaded, awaiting Horticulturalist'));
-      });
+        return service.upgrade(req.body.build, userCtx.user)
+          .then(started => res.json({ ok: started }));
+      })
+      .catch(err => serverUtils.error(err, req, res));
+  }
 };
