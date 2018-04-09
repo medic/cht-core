0f15e722cedf40e3d5a7868c01fb0febe40599e8
Alex Anderson <anderson@medicmobile.org>
2018-04-09 10:19:15 +0000

    re-fix nodeunit tests

--- api/tests/unit/message-utils.spec.js
+++ api/tests/unit/message-utils.spec.js
@@ -1,7 +1,9 @@
 var controller = require('../../message-utils'),
-    db = require('../../db-nano'),
+    dbNano = require('../../db-nano'),
+    dbPouch = require('../../db-pouch'),
     sinon = require('sinon').sandbox.create(),
     taskUtils = require('task-utils');
+const USE_POUCH = true;
 
 exports.tearDown = function (callback) {
   sinon.restore();
@@ -10,7 +12,9 @@ exports.tearDown = function (callback) {
 
 exports['getMessages returns errors'] = function(test) {
   test.expect(2);
-  var getView = sinon.stub(db.medic, 'view').callsArgWith(3, 'bang');
+  var getView = USE_POUCH ?
+      sinon.stub(dbPouch.medic, 'query').callsArgWith(2, 'bang') :
+      sinon.stub(dbNano.medic, 'view').callsArgWith(3, 'bang');
   controller.getMessages(null, function(err) {
     test.equals(err, 'bang');
     test.equals(getView.callCount, 1);
@@ -20,18 +24,22 @@ exports['getMessages returns errors'] = function(test) {
 
 exports['getMessages passes limit param value of 100 to view'] = function(test) {
   test.expect(2);
-  var getView = sinon.stub(db.medic, 'view').callsArgWith(3, null, { rows: [] });
+  var getView = USE_POUCH ?
+    sinon.stub(dbPouch.medic, 'query').callsArgWith(2, null, { rows: [] }) :
+    sinon.stub(dbNano.medic, 'view').callsArgWith(3, null, { rows: [] });
   controller.getMessages({ limit: 500 }, function() {
     test.equals(getView.callCount, 1);
     // assert query parameters on view call use right limit value
-    test.deepEqual({ limit: 500 }, getView.getCall(0).args[2]);
+    test.deepEqual({ limit: 500 }, getView.getCall(0).args[USE_POUCH ? 1 : 2]);
     test.done();
   });
 };
 
 exports['getMessages returns 500 error if limit over 100'] = function(test) {
   test.expect(3);
-  var getView = sinon.stub(db.medic, 'view');
+  var getView = USE_POUCH ?
+    sinon.stub(dbPouch.medic, 'query') :
+    sinon.stub(dbNano.medic, 'view');
   controller.getMessages({ limit: 9999 }, function(err) {
     test.equals(err.code, 500);
     test.equals(err.message, 'Limit max is 1000');
@@ -42,11 +50,20 @@ exports['getMessages returns 500 error if limit over 100'] = function(test) {
 
 exports['getMessages passes state param'] = function(test) {
   test.expect(4);
-  sinon.stub(db.medic, 'view').callsArgWith(3, null, { rows: [
-    { key: 'yayaya', value: { sending_due_date: 456 } },
-    { key: 'pending', value: { sending_due_date: 123 } },
-    { key: 'pending', value: { sending_due_date: 789 } }
-  ] });
+  if(USE_POUCH) {
+    sinon.stub(dbPouch.medic, 'query').callsArgWith(2, null, { rows: [
+      { key: 'yayaya', value: { sending_due_date: 456 } },
+      { key: 'pending', value: { sending_due_date: 123 } },
+      { key: 'pending', value: { sending_due_date: 789 } }
+    ] });
+  } else {
+    sinon.stub(dbNano.medic, 'view').callsArgWith(3, null, { rows: [
+      { key: 'yayaya', value: { sending_due_date: 456 } },
+      { key: 'pending', value: { sending_due_date: 123 } },
+      { key: 'pending', value: { sending_due_date: 789 } }
+    ] });
+  }
+
   controller.getMessages({}, function(err, messages) {
     test.ok(!err);
     test.equals(messages.length, 3);
@@ -58,50 +75,80 @@ exports['getMessages passes state param'] = function(test) {
 
 exports['getMessages passes states param'] = function(test) {
   test.expect(3);
-  var getView = sinon.stub(db.medic, 'view').callsArgWith(3, null, { rows: [] });
+  var getView = USE_POUCH ?
+      sinon.stub(dbPouch.medic, 'query').callsArgWith(2, null, { rows: [] }) :
+      sinon.stub(dbNano.medic, 'view').callsArgWith(3, null, { rows: [] });
   controller.getMessages({ states: ['happy', 'angry'] }, function(err) {
     console.log(err);
     test.ok(!err);
     test.equals(getView.callCount, 1);
-    test.deepEqual(['happy', 'angry'], getView.getCall(0).args[2].keys);
+    test.deepEqual(['happy', 'angry'], getView.getCall(0).args[USE_POUCH ? 1 : 2].keys);
     test.done();
   });
 };
 
 exports['getMessages sorts results'] = function(test) {
   test.expect(3);
-  var getView = sinon.stub(db.medic, 'view').callsArgWith(3, null, { rows: [] });
+  var getView = USE_POUCH ?
+      sinon.stub(dbPouch.medic, 'query').callsArgWith(2, null, { rows: [] }) :
+      sinon.stub(dbNano.medic, 'view').callsArgWith(3, null, { rows: [] });
   controller.getMessages({ states: ['happy', 'angry'] }, function(err) {
     console.log(err);
     test.ok(!err);
     test.equals(getView.callCount, 1);
-    test.deepEqual(['happy', 'angry'], getView.getCall(0).args[2].keys);
+    test.deepEqual(['happy', 'angry'], getView.getCall(0).args[USE_POUCH ? 1 : 2].keys);
     test.done();
   });
 };
 
 exports['updateMessageTaskStates takes a collection of state changes and saves it to docs'] = test => {
-  sinon.stub(db.medic, 'view').callsArgWith(3, null, {rows: [
-    {id: 'testMessageId1'},
-    {id: 'testMessageId2'}]});
+  if(USE_POUCH) {
+    sinon.stub(dbPouch.medic, 'query').callsArgWith(2, null, {rows: [
+      {id: 'testMessageId1'},
+      {id: 'testMessageId2'}]});
+  } else {
+    sinon.stub(dbNano.medic, 'view').callsArgWith(3, null, {rows: [
+      {id: 'testMessageId1'},
+      {id: 'testMessageId2'}]});
+  }
 
-  sinon.stub(db.medic, 'fetch').callsArgWith(1, null, {rows: [
-    {doc: {
-      _id: 'testDoc',
-      tasks: [{
-        messages: [{
-          uuid: 'testMessageId1'
+  if(USE_POUCH) {
+    sinon.stub(dbPouch.medic, 'allDocs').callsArgWith(1, null, {rows: [
+      {doc: {
+        _id: 'testDoc',
+        tasks: [{
+          messages: [{
+            uuid: 'testMessageId1'
+          }]
+        }],
+        scheduled_tasks: [{
+          messages: [{
+            uuid: 'testMessageId2'
+          }]
         }]
-      }],
-      scheduled_tasks: [{
-        messages: [{
-          uuid: 'testMessageId2'
+      }}
+    ]});
+  } else {
+    sinon.stub(dbNano.medic, 'fetch').callsArgWith(1, null, {rows: [
+      {doc: {
+        _id: 'testDoc',
+        tasks: [{
+          messages: [{
+            uuid: 'testMessageId1'
+          }]
+        }],
+        scheduled_tasks: [{
+          messages: [{
+            uuid: 'testMessageId2'
+          }]
         }]
-      }]
-    }}
-  ]});
+      }}
+    ]});
+  }
 
-  const bulk = sinon.stub(db.medic, 'bulk').callsArgWith(1, null, []);
+  const bulk = USE_POUCH ?
+      sinon.stub(dbPouch.medic, 'bulkDocs').callsArgWith(1, null, []) :
+      sinon.stub(dbNano.medic, 'bulk').callsArgWith(1, null, []);
   const setTaskState = sinon.stub(taskUtils, 'setTaskState');
 
   controller.updateMessageTaskStates([
@@ -121,7 +168,7 @@ exports['updateMessageTaskStates takes a collection of state changes and saves i
     test.deepEqual(setTaskState.getCall(0).args, [{ messages: [{uuid: 'testMessageId1'}]}, 'testState1', undefined]);
     test.deepEqual(setTaskState.getCall(1).args, [{ messages: [{uuid: 'testMessageId2'}]}, 'testState2', 'Just because.']);
 
-    const doc = bulk.args[0][0].docs[0];
+    const doc = USE_POUCH ? bulk.args[0][0][0] : bulk.args[0][0].docs[0];
     test.equal(doc._id, 'testDoc');
 
     test.done();
@@ -129,21 +176,43 @@ exports['updateMessageTaskStates takes a collection of state changes and saves i
 };
 
 exports['updateMessageTaskStates DOES NOT throw an error if it cant find the message'] = test => {
-  sinon.stub(db.medic, 'view').callsArgWith(3, null, {rows: [
-    {id: 'testMessageId1'}]});
+  if(USE_POUCH) {
+    sinon.stub(dbPouch.medic, 'query').callsArgWith(2, null, {rows: [
+      {id: 'testMessageId1'}]});
+  } else {
+    sinon.stub(dbNano.medic, 'view').callsArgWith(3, null, {rows: [
+      {id: 'testMessageId1'}]});
+  }
 
-  sinon.stub(db.medic, 'fetch').callsArgWith(1, null, {rows: [
-    {doc: {
-      _id: 'testDoc',
-      tasks: [{
-        messages: [{
-          uuid: 'testMessageId1'
+  if(USE_POUCH) {
+    sinon.stub(dbPouch.medic, 'allDocs').callsArgWith(1, null, {rows: [
+      {doc: {
+        _id: 'testDoc',
+        tasks: [{
+          messages: [{
+            uuid: 'testMessageId1'
+          }]
         }]
-      }]
-    }}
-  ]});
+      }}
+    ]});
+  } else {
+    sinon.stub(dbNano.medic, 'fetch').callsArgWith(1, null, {rows: [
+      {doc: {
+        _id: 'testDoc',
+        tasks: [{
+          messages: [{
+            uuid: 'testMessageId1'
+          }]
+        }]
+      }}
+    ]});
+  }
 
-  sinon.stub(db.medic, 'bulk').callsArgWith(1, null, []);
+  if(USE_POUCH) {
+    sinon.stub(dbPouch.medic, 'bulkDocs').callsArgWith(1, null, []);
+  } else {
+    sinon.stub(dbNano.medic, 'bulk').callsArgWith(1, null, []);
+  }
 
   controller.updateMessageTaskStates([
     {
@@ -164,49 +233,95 @@ exports['updateMessageTaskStates DOES NOT throw an error if it cant find the mes
 };
 
 exports['updateMessageTaskStates re-applies changes if it errored'] = test => {
-  const view = sinon.stub(db.medic, 'view')
-  .onFirstCall().callsArgWith(3, null, {rows: [
-    {id: 'testMessageId1'},
-    {id: 'testMessageId2'}]})
-  .onSecondCall().callsArgWith(3, null, {rows: [
-    {id: 'testMessageId2'}]});
+  const view = USE_POUCH ?
+    sinon.stub(dbPouch.medic, 'query')
+        .onFirstCall().callsArgWith(2, null, {rows: [
+          {id: 'testMessageId1'},
+          {id: 'testMessageId2'}]})
+        .onSecondCall().callsArgWith(2, null, {rows: [
+          {id: 'testMessageId2'}]}) :
+    sinon.stub(dbNano.medic, 'view')
+        .onFirstCall().callsArgWith(3, null, {rows: [
+          {id: 'testMessageId1'},
+          {id: 'testMessageId2'}]})
+        .onSecondCall().callsArgWith(3, null, {rows: [
+          {id: 'testMessageId2'}]});
 
-  sinon.stub(db.medic, 'fetch')
-  .onFirstCall().callsArgWith(1, null, {rows: [
-    {doc: {
-      _id: 'testDoc',
-      tasks: [{
-        messages: [{
-          uuid: 'testMessageId1'
-        }]
-      }]
-    }},
-    {doc: {
-      _id: 'testDoc2',
-      tasks: [{
-        messages: [{
-          uuid: 'testMessageId2'
-        }]
-      }]
-    }}
-  ]})
-  .onSecondCall().callsArgWith(1, null, {rows: [
-    {doc: {
-      _id: 'testDoc2',
-      tasks: [{
-        messages: [{
-          uuid: 'testMessageId2'
-        }]
-      }]
-    }}
-  ]});
+  if(USE_POUCH) {
+    sinon.stub(dbPouch.medic, 'allDocs')
+        .onFirstCall().callsArgWith(1, null, {rows: [
+          {doc: {
+            _id: 'testDoc',
+            tasks: [{
+              messages: [{
+                uuid: 'testMessageId1'
+              }]
+            }]
+          }},
+          {doc: {
+            _id: 'testDoc2',
+            tasks: [{
+              messages: [{
+                uuid: 'testMessageId2'
+              }]
+            }]
+          }}
+        ]})
+        .onSecondCall().callsArgWith(1, null, {rows: [
+          {doc: {
+            _id: 'testDoc2',
+            tasks: [{
+              messages: [{
+                uuid: 'testMessageId2'
+              }]
+            }]
+          }}
+        ]});
+  } else {
+    sinon.stub(dbNano.medic, 'fetch')
+        .onFirstCall().callsArgWith(1, null, {rows: [
+          {doc: {
+            _id: 'testDoc',
+            tasks: [{
+              messages: [{
+                uuid: 'testMessageId1'
+              }]
+            }]
+          }},
+          {doc: {
+            _id: 'testDoc2',
+            tasks: [{
+              messages: [{
+                uuid: 'testMessageId2'
+              }]
+            }]
+          }}
+        ]})
+        .onSecondCall().callsArgWith(1, null, {rows: [
+          {doc: {
+            _id: 'testDoc2',
+            tasks: [{
+              messages: [{
+                uuid: 'testMessageId2'
+              }]
+            }]
+          }}
+        ]});
+  }
 
-  const bulk = sinon.stub(db.medic, 'bulk')
-  .onFirstCall().callsArgWith(1, null, [
-    {id: 'testDoc', ok: true},
-    {id: 'testDoc2', error: 'oh no!'}])
-  .onSecondCall().callsArgWith(1, null, [
-    {id: 'testDoc2', ok: true}]);
+  const bulk = USE_POUCH ?
+      sinon.stub(dbPouch.medic, 'bulkDocs')
+          .onFirstCall().callsArgWith(1, null, [
+              {id: 'testDoc', ok: true},
+              {id: 'testDoc2', error: 'oh no!'}])
+          .onSecondCall().callsArgWith(1, null, [
+              {id: 'testDoc2', ok: true}]) :
+      sinon.stub(dbNano.medic, 'bulk')
+          .onFirstCall().callsArgWith(1, null, [
+              {id: 'testDoc', ok: true},
+              {id: 'testDoc2', error: 'oh no!'}])
+          .onSecondCall().callsArgWith(1, null, [
+              {id: 'testDoc2', ok: true}]);
 
   controller.updateMessageTaskStates([
     {
@@ -223,14 +338,25 @@ exports['updateMessageTaskStates re-applies changes if it errored'] = test => {
     test.deepEqual(result, {success: true});
 
     test.equal(view.callCount, 2);
-    test.deepEqual(view.args[0][2], {keys: ['testMessageId1', 'testMessageId2']});
-    test.deepEqual(view.args[1][2], {keys: ['testMessageId2']});
+    if(USE_POUCH) {
+      test.deepEqual(view.args[0][1], {keys: ['testMessageId1', 'testMessageId2']});
+      test.deepEqual(view.args[1][1], {keys: ['testMessageId2']});
 
-    test.equal(bulk.args[0][0].docs.length, 2);
-    test.equal(bulk.args[0][0].docs[0]._id, 'testDoc');
-    test.equal(bulk.args[0][0].docs[1]._id, 'testDoc2');
-    test.equal(bulk.args[1][0].docs.length, 1);
-    test.equal(bulk.args[1][0].docs[0]._id, 'testDoc2');
+      test.equal(bulk.args[0][0].length, 2);
+      test.equal(bulk.args[0][0][0]._id, 'testDoc');
+      test.equal(bulk.args[0][0][1]._id, 'testDoc2');
+      test.equal(bulk.args[1][0].length, 1);
+      test.equal(bulk.args[1][0][0]._id, 'testDoc2');
+    } else {
+      test.deepEqual(view.args[0][2], {keys: ['testMessageId1', 'testMessageId2']});
+      test.deepEqual(view.args[1][2], {keys: ['testMessageId2']});
+
+      test.equal(bulk.args[0][0].docs.length, 2);
+      test.equal(bulk.args[0][0].docs[0]._id, 'testDoc');
+      test.equal(bulk.args[0][0].docs[1]._id, 'testDoc2');
+      test.equal(bulk.args[1][0].docs.length, 1);
+      test.equal(bulk.args[1][0].docs[0]._id, 'testDoc2');
+    }
 
     test.done();
   });
