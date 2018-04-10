6b5d328e2ae3392085bdb5d45798e1c06baa38fc
ngaruko <ngarukob@yahoo.com>
2018-04-11 11:55:27 +1200

    Exclude performance tests since they always fail on travis

--- tests/protractor/base.conf.js
+++ tests/protractor/base.conf.js
@@ -10,6 +10,8 @@ class BaseConfig {
       seleniumAddress: 'http://localhost:4444/wd/hub',
 
       specs: [`${testSrcDir}/**/*.js`],
+      //ignore these tests for they fail on travis
+      exclude: [`${testSrcDir}/**/sms-gateway.perf.js`],
 
       framework: 'jasmine2',
       capabilities: {
