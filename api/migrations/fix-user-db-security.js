6b2fb2ca7ef7f2f582fe30a619b7f180300ca667
Gareth Bowen <gareth@medicmobile.org>
2018-04-12 23:50:06 +1200

    Fixes open security on user dbs
    The user dbs currently have no members in the _security doc which
    leaves the db open for anyone to read. This changes that so users
    can only see their own meta db. It also adds a migration to fix
    existing meta dbs.
    
    medic/medic-webapp#4412

---
+++ api/migrations/fix-user-db-security.js
@@ -0,0 +1,28 @@
+const db = require('../db-pouch'),
+      userDb = require('../lib/user-db');
+
+module.exports = {
+  name: 'fix-user-db-security',
+  created: new Date(2018, 4, 12),
+  run: callback => {
+    db.users.allDocs()
+      .then(docs => {
+        return Promise.all(docs.rows
+          .map(row => row.id)
+          .filter(id => id.indexOf('org.couchdb.user:') === 0)
+          .map(id => id.split(':')[1])
+          .map(username => {
+            return new Promise((resolve, reject) => {
+              userDb.setSecurity(userDb.getDbName(username), username, err => {
+                if (err && err.statusCode !== 404) { // db not found is ok
+                  return reject(err);
+                }
+                resolve();
+              });
+            });
+          }));
+      })
+      .then(() => callback())
+      .catch(callback);
+  }
+};
