540cb62585ce5820236d16886e6768d16712d43b
ngaruko <ngarukob@yahoo.com>
2018-04-16 09:34:50 +1200

    test 01

--- tests/protractor/e2e/api/controllers/export-data-2.js
+++ tests/protractor/e2e/api/controllers/export-data-2.js
@@ -79,7 +79,7 @@ describe('Export Data V2.0', () => {
           notJson: true
         }).then(result => {
           const rows = result.split('\n');
-          const str = rows[rows.length - 1];//last string.
+          const str = rows[rows.length - 1];//last string
           if (!str || 0 === str.length) { rows.pop(); } // Last row is empty string, discard
           const expected = [
             '_id,form,patient_id,reported_date,from,contact.name,contact.parent.name,contact.parent.parent.name,contact.parent.parent.parent.name,baz',
