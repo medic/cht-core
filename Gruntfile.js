12f4a939959d28203f578fc7bf9d9c21c3bc7ded
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2018-04-30 16:13:05 +1200

    Set max_request size to default

--- Gruntfile.js
+++ Gruntfile.js
@@ -357,7 +357,8 @@ module.exports = function(grunt) {
              ' && curl -X POST http://admin:pass@localhost:5984/_users ' +
                  ' -H "Content-Type: application/json" ' +
                  ' -d \'{"_id": "org.couchdb.user:admin", "name": "admin", "password":"pass", "type":"user", "roles":[]}\' ' +
-             ' && curl -X PUT --data \'"true"\' http://admin:pass@localhost:5984/_node/${COUCH_NODE_NAME}/_config/chttpd/require_valid_user'
+             ' && curl -X PUT --data \'"true"\' http://admin:pass@localhost:5984/_node/${COUCH_NODE_NAME}/_config/chttpd/require_valid_user' +
+             ' && curl -X PUT --data \'"4294967296"\' http://admin:pass@localhost:5984/_node/${COUCH_NODE_NAME}/_config/httpd/max_http_request_size'
       },
       resetTestDatabases: {
         stderr: false,
