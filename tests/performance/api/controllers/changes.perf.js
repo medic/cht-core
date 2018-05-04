c8a4353f8673cf096be1f70a2c249262ff5bfea4
Alex Anderson <anderson@medicmobile.org>
2018-05-04 11:41:46 +0000

    implement the test

--- tests/performance/api/controllers/changes.perf.js
+++ tests/performance/api/controllers/changes.perf.js
@@ -1,13 +1,87 @@
+const fs = require('fs');
+const PouchDB = require('pouchdb');
+const testUtils = require('../../../utils');
+
+const { COUCH_HOST, COUCH_PORT, DB_NAME } = require('../../../constants');
+
 describe('/_changes', function() {
+
   it('should not break for restricted users when multiple relevant changes arrive', function() {
-    // given
-    // TODO user exists
-    // TODO start listening to changes
+    let changes;
 
-    // when
-    // TODO start uploading loads of forms in parallel
+    return createRestrictedUser('mr.test', 't0ps3cret!')
+      .then(() => {
+        // given a restricted user is listening API's /_changes feed
+        const restrictedUserDb = new PouchDB(`http://mr.test:t0ps3cret!@${COUCH_HOST}:${COUCH_PORT}/${DB_NAME}`);
+        changes = restrictedUserDb.changes({
+          live: true,
+          timeout: false,
+          heartbeat: 5000,
+          filter: 'doc_ids',
+          include_docs: true,
+          return_docs: false,
+        })
+        .on('error', err => expect(err).toBe(null));
 
-    // then
-    // TODO when form uploading has completed, mark the test as passed
+        // when multiple forms are uploaded
+        return uploadFormsInParallel();
+      })
+      .then(() => changes.cancel());
+
   });
+
 });
+
+function createRestrictedUser(username, password) {
+  const type = 'district-manager';
+
+  return testUtils.request({
+    path: '/api/v1/users',
+    method: 'POST',
+    headers: { 'Content-Type':'application/json' },
+    body: { username, password, type },
+  });
+}
+
+function uploadFormsInParallel() {
+  Promise.all([
+    uploadForm('child'),
+    uploadForm('collect_off'),
+    uploadForm('collect_on'),
+    uploadForm('contact:clinic:create'),
+    uploadForm('contact:clinic:edit'),
+    uploadForm('contact:district_hospital:create'),
+    uploadForm('contact:district_hospital:edit'),
+    uploadForm('contact:health_center:create'),
+    uploadForm('contact:health_center:edit'),
+    uploadForm('contact:person:create'),
+    uploadForm('contact:person:edit'),
+    uploadForm('d'),
+    uploadForm('delivery'),
+    uploadForm('f'),
+    uploadForm('imm'),
+    uploadForm('immunization_visit'),
+    uploadForm('m'),
+    uploadForm('n'),
+    uploadForm('off'),
+    uploadForm('on'),
+    uploadForm('p'),
+    uploadForm('postnatal_visit'),
+    uploadForm('pregnancy'),
+    uploadForm('pregnancy_visit'),
+    uploadForm('v'),
+  ]);
+}
+
+function uploadForm(name) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(`changes-data/${name}.form.json`, { encoding:'utf8' }, (err, data) => {
+      if(err) {
+        return reject(err);
+      }
+      return data;
+    });
+  })
+    .then(JSON.parse)
+    .then(testUtils.db.put);
+}
