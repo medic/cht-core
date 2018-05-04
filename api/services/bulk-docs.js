e829d02c259e4f7f1bb79f7335de1c144be6d581
Robbie Howe <tiaeph@gmail.com>
2018-05-04 11:25:22 +0100

    Refactors bulk delete to batch properly and retry errors
    Docs are now fetched in batches rather than all at once at
    the beginning

--- api/services/bulk-docs.js
+++ api/services/bulk-docs.js
@@ -1,5 +1,4 @@
 const db = require('../db-pouch');
-
 const utils = require('bulk-docs-utils')({
   Promise: Promise,
   DB: db.medic
@@ -35,35 +34,68 @@ const generateBatches = (docs, batchSize) => {
   return batches;
 };
 
-const generateBatchPromise = (batch, res, { isFinal } = {}) => {
-  return db.medic.bulkDocs(batch).then(result => {
-    let resString = JSON.stringify(result);
-    resString += isFinal ? '' : ',';
-    res.write(resString);
-  });
+const getDocsToModify = ids => {
+  let docsToDelete;
+  let allDocs;
+  return db.medic.allDocs({ keys: ids, include_docs: true })
+    .then(result => {
+      docsToDelete = markAsDeleted(result);
+      return utils.updateParentContacts(docsToDelete);
+    })
+    .then(docsToUpdate => {
+      allDocs = docsToDelete.concat(docsToUpdate);
+      checkForDuplicates(allDocs);
+      return allDocs;
+    });
+};
+
+const deleteBatches = (batches, res, docsToRetry, attemptNumber) => {
+  return batches.reduce((promise, batch, index) => {
+    const isFinal = index === batches.length - 1;
+    let docsToModify;
+    return promise
+      .then(() => {
+        return getDocsToModify(batch);
+      })
+      .then(allDocs => {
+        docsToModify = allDocs;
+        return db.medic.bulkDocs(docsToModify);
+      })
+      .then(result => {
+        const errors = result.map(docUpdate => docUpdate.error);
+        const errorIds = docsToModify
+          .filter((doc, index) => errors[index])
+          .map(doc => doc._id);
+        errorIds.forEach(id => docsToRetry.push(id));
+
+        let resString = JSON.stringify(result);
+        if ((!isFinal || docsToRetry.length) && attemptNumber !== 3) {
+          resString += ',';
+        }
+        res.write(resString);
+      });
+  }, Promise.resolve());
 };
 
 module.exports = {
   bulkDelete: (docs, res, { batchSize = 100 } = {}) => {
-    let docsToDelete;
-    const keys = docs.map(doc => doc._id);
-    return db.medic.allDocs({ keys, include_docs: true })
-      .then(result => {
-        docsToDelete = markAsDeleted(result);
-        return utils.updateParentContacts(docsToDelete);
-      })
-      .then(docsToUpdate => {
-        const allDocs = docsToDelete.concat(docsToUpdate);
-        checkForDuplicates(allDocs);
-        const batches = generateBatches(allDocs, batchSize);
-
-        res.type('application/json');
-        res.setHeader('X-Content-Type-Options', 'nosniff');
-        res.write('[');
-
-        return batches.reduce((promise, batch, index) => {
-          return promise.then(() => generateBatchPromise(batch, res, { isFinal: index === batches.length - 1 }));
-        }, Promise.resolve([]));
+    const ids = docs.map(doc => doc._id);
+    const batches = generateBatches(ids, batchSize);
+    let docsToRetry = [];
+    res.type('application/json');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.write('[');
+    return deleteBatches(batches, res, docsToRetry, 0)
+      .then(() => {
+        // Retry any failures a maximum of 3 times
+        return [1, 2, 3].reduce((promise, attemptNumber) => {
+          if (docsToRetry.length === 0) {
+            return promise;
+          }
+          const batches = generateBatches(docsToRetry, batchSize);
+          docsToRetry.length = 0;
+          return promise.then(() => deleteBatches(batches, res, docsToRetry, attemptNumber));
+        }, Promise.resolve());
       })
       .then(() => {
         res.write(']');
