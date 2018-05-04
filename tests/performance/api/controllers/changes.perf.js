f80ecec1b482affc9e2d96f5b6252a7aad6679cd
Alex Anderson <anderson@medicmobile.org>
2018-05-04 09:12:43 +0000

    wip

---
+++ tests/performance/api/controllers/changes.perf.js
@@ -0,0 +1,13 @@
+describe('/_changes', function() {
+  it('should not break for restricted users when multiple relevant changes arrive', function() {
+    // given
+    // TODO user exists
+    // TODO start listening to changes
+
+    // when
+    // TODO start uploading loads of forms in parallel
+
+    // then
+    // TODO when form uploading has completed, mark the test as passed
+  });
+});
