22639a225d248f0473036c9d6d5a6cfc31ab6081
Robbie Howe <tiaeph@gmail.com>
2018-05-10 16:04:03 +0100

    Retries errors immediately after failure
    This is in case api restarts, we do not want the docs and their
    relatives to be left in an inconsistent state

--- api/services/bulk-docs.js
+++ api/services/bulk-docs.js
@@ -4,15 +4,9 @@ const utils = require('bulk-docs-utils')({
   DB: db.medic
 });
 
-const markAsDeleted = data => {
+const extractDocs = data => {
   return data.rows
-    .map(row => {
-      const doc = row.doc;
-      if (doc) {
-        doc._deleted = true;
-        return doc;
-      }
-    })
+    .map(row => row.doc)
     .filter(doc => doc);
 };
 
@@ -25,72 +19,70 @@ const checkForDuplicates = docs => {
   }
 };
 
-const updateParentContacts = (docs, batchSize, attemptCounts) => {
-  const batch = docs.splice(0, batchSize);
-  batch.forEach(doc => {
-    attemptCounts[doc._id] = (attemptCounts[doc._id] || 0) + 1;
-  });
-  const parentMap = {};
-  return utils.updateParentContacts(batch, parentMap)
-    .then(docsToUpdate => {
-      if (!docsToUpdate.length) {
-        return [];
-      }
-      return db.medic.bulkDocs(docsToUpdate)
-        .then(result => {
-          const errorIds = result
-            .map((docUpdate, index) => docUpdate.error && docUpdate.status !== 404 && (docUpdate.id || docsToUpdate[index]._id))
-            .filter(id => id);
-          errorIds.forEach(id => {
-            const doc = parentMap[id];
-            if (attemptCounts[doc._id] < 4) {
-              docs.push(doc);
-              result = result.filter(docUpdate => docUpdate.id !== id);
-            }
-          });
-          if (docs.length > 0) {
-            return updateParentContacts(docs, batchSize, attemptCounts);
-          }
-          return result;
-        });
-    });
+const generateBatches = (ids, batchSize) => {
+  const batches = [];
+  while (ids.length > 0) {
+    const batch = ids.splice(0, batchSize);
+    batches.push(batch);
+  }
+  return batches;
 };
 
-const deleteInBatches = (idsToDelete, batchSize, attemptCounts, res) => {
-  const batch = idsToDelete.splice(0, batchSize);
-  batch.forEach(id => {
-    attemptCounts[id] = (attemptCounts[id] || 0) + 1;
+const fetchDocs = ids => {
+  if (ids.length === 0) {
+    return Promise.resolve([]);
+  }
+  return db.medic.allDocs({ keys: ids, include_docs: true })
+    .then(extractDocs);
+};
+
+const incrementAttemptCounts = (docs, attemptCounts) => {
+  docs.forEach(doc => {
+    attemptCounts[doc._id] = (attemptCounts[doc._id] || 0) + 1;
   });
-  let docsToDelete;
-  let deletionResponse;
-  return db.medic.allDocs({ keys: batch, include_docs: true })
-    .then(result => {
-      docsToDelete = markAsDeleted(result);
-      return db.medic.bulkDocs(docsToDelete);
+};
+
+const deleteDocs = (docsToDelete, deletionAttemptCounts, updateAttemptCounts, cumulativeResult = []) => {
+  const parentMap = {};
+  let docsToUpdate;
+  let docsToModify;
+  docsToDelete.forEach(doc => doc._deleted = true);
+  incrementAttemptCounts(docsToDelete, deletionAttemptCounts);
+  return utils.updateParentContacts(docsToDelete, parentMap)
+    .then(updatedParents => {
+      docsToUpdate = updatedParents;
+      incrementAttemptCounts(docsToUpdate, updateAttemptCounts);
+      docsToModify = docsToDelete.concat(docsToUpdate);
+      return db.medic.bulkDocs(docsToModify);
     })
     .then(result => {
-      deletionResponse = result;
-      const errorIds = deletionResponse
-        .map((docUpdate, index) => docUpdate.error && docUpdate.status !== 404 && (docUpdate.id || docsToDelete[index]._id))
+      result = result.filter(docUpdate => docUpdate.status !== 404);
+      const deletionFailures = [];
+      const updateFailures = [];
+      const deletionIds = docsToDelete.map(doc => doc._id);
+      const updateIds = docsToUpdate.map(doc => doc._id);
+      const errorIds = result
+        .map((docUpdate, index) => docUpdate.error && (docUpdate.id || docsToModify[index]._id))
         .filter(id => id);
       errorIds.forEach(id => {
-        if (attemptCounts[id] < 4) {
-          idsToDelete.push(id);
-          deletionResponse = deletionResponse.filter(docUpdate => docUpdate.id !== id);
+        if (deletionIds.includes(id) && deletionAttemptCounts[id] < 4) {
+          deletionFailures.push(id);
+          result = result.filter(docUpdate => docUpdate.id !== id);
+        } else if (updateIds.includes(id) && updateAttemptCounts[id] < 4) {
+          updateFailures.push(id);
+          result = result.filter(docUpdate => docUpdate.id !== id);
         }
       });
-      const successfullyDeletedDocs = docsToDelete.filter(doc => !errorIds.includes(doc._id));
-      const updateAttemptCounts = {};
-      return updateParentContacts(successfullyDeletedDocs, batchSize, updateAttemptCounts);
-    })
-    .then(updateResponse => {
-      let resString = JSON.stringify(deletionResponse.concat(updateResponse));
-      if (idsToDelete.length > 0) {
-        resString += ',';
-        res.write(resString);
-        return deleteInBatches(idsToDelete, batchSize, attemptCounts, res);
+
+      cumulativeResult = cumulativeResult.concat(result);
+      if (deletionFailures.length > 0 || updateFailures.length > 0) {
+        return fetchDocs(deletionFailures)
+          .then(docsToDelete => {
+            const updatesToRetry = updateFailures.map(id => parentMap[id]);
+            return deleteDocs(docsToDelete.concat(updatesToRetry), deletionAttemptCounts, updateAttemptCounts, cumulativeResult);
+          });
       }
-      res.write(resString);
+      return cumulativeResult;
     });
 };
 
@@ -98,11 +90,24 @@ module.exports = {
   bulkDelete: (docs, res, { batchSize = 100 } = {}) => {
     checkForDuplicates(docs);
     const ids = docs.map(doc => doc._id);
-    const attemptCounts = {};
+    const batches = generateBatches(ids, batchSize);
+    const deletionAttemptCounts = {};
+    const updateAttemptCounts = {};
     res.type('application/json');
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.write('[');
-    return deleteInBatches(ids, batchSize, attemptCounts, res)
+    return batches.reduce((promise, batch, index) => {
+      return promise
+        .then(() => fetchDocs(batch))
+        .then(docsToDelete => deleteDocs(docsToDelete, deletionAttemptCounts, updateAttemptCounts))
+        .then(result => {
+          let resString = JSON.stringify(result);
+          if (index !== batches.length - 1) {
+            resString += ',';
+          }
+          res.write(resString);
+        });
+    }, Promise.resolve())
       .then(() => {
         res.write(']');
         res.end();
--- api/tests/mocha/services/bulk-docs.js
+++ api/tests/mocha/services/bulk-docs.js
@@ -49,9 +49,8 @@ describe('Bulk Docs Service', function () {
       allDocs.onCall(1).resolves({ rows: [{ doc: docC }] });
 
       const bulkDocs = sinon.stub(db.medic, 'bulkDocs');
-      bulkDocs.onCall(0).resolves([{ id: docA._id, ok: true }, { id: docB._id, ok: true }]);
-      bulkDocs.onCall(1).resolves([{ id: parent._id, ok: true }]);
-      bulkDocs.onCall(2).resolves([{ id: docC._id, ok: true }]);
+      bulkDocs.onCall(0).resolves([{ id: docA._id, ok: true }, { id: docB._id, ok: true }, { id: parent._id, ok: true }]);
+      bulkDocs.onCall(1).resolves([{ id: docC._id, ok: true }]);
 
       sinon.stub(db.medic, 'get').resolves(parent);
 
@@ -60,10 +59,9 @@ describe('Bulk Docs Service', function () {
           allDocs.callCount.should.equal(2);
           allDocs.getCall(0).args[0].should.deep.equal({ keys: [docA._id, docB._id], include_docs: true });
           allDocs.getCall(1).args[0].should.deep.equal({ keys: [docC._id], include_docs: true });
-          bulkDocs.callCount.should.equal(3);
-          bulkDocs.getCall(0).args[0].should.deep.equal([expectedA, expectedB]);
-          bulkDocs.getCall(1).args[0].should.deep.equal([expectedParent]);
-          bulkDocs.getCall(2).args[0].should.deep.equal([expectedC]);
+          bulkDocs.callCount.should.equal(2);
+          bulkDocs.getCall(0).args[0].should.deep.equal([expectedA, expectedB, expectedParent]);
+          bulkDocs.getCall(1).args[0].should.deep.equal([expectedC]);
           testRes.write.callCount.should.equal(4);
           testRes.write.getCall(0).args[0].should.equal('[');
           testRes.write.getCall(1).args[0].should.equal('[{"id":"a","ok":true},{"id":"b","ok":true},{"id":"parent","ok":true}],');
@@ -83,38 +81,37 @@ describe('Bulk Docs Service', function () {
 
       const allDocs = sinon.stub(db.medic, 'allDocs');
       allDocs.onCall(0).resolves({ rows: [{ doc: docA }, { doc: docB }] });
-      allDocs.onCall(1).resolves({ rows: [{ doc: docC }, { doc: docA }] });
+      allDocs.onCall(1).resolves({ rows: [{ doc: docA }, { doc: docB }] });
       allDocs.onCall(2).resolves({ rows: [{ doc: docB }] });
       allDocs.onCall(3).resolves({ rows: [{ doc: docB }] });
-      allDocs.onCall(4).resolves({ rows: [{ doc: docB }] });
+      allDocs.onCall(4).resolves({ rows: [{ doc: docC }] });
 
       const bulkDocs = sinon.stub(db.medic, 'bulkDocs');
       bulkDocs.onCall(0).resolves([{ id: docA._id, error: true }, { id: docB._id, error: true }]);
-      bulkDocs.onCall(1).resolves([{ id: docC._id, ok: true }, { id: docA._id, ok: true }]);
+      bulkDocs.onCall(1).resolves([{ id: docA._id, ok: true }, { id: docB._id, error: true }]);
       bulkDocs.onCall(2).resolves([{ id: docB._id, error: true }]);
       bulkDocs.onCall(3).resolves([{ id: docB._id, error: true }]);
-      bulkDocs.onCall(4).resolves([{ id: docB._id, error: true }]);
+      bulkDocs.onCall(4).resolves([{ id: docC._id, ok: true }]);
 
       return service.bulkDelete(testDocs, testRes, { batchSize: 2 })
         .then(() => {
           allDocs.callCount.should.equal(5);
-          allDocs.getCall(1).args[0].should.deep.equal({ keys: [docC._id, docA._id], include_docs: true });
+          allDocs.getCall(0).args[0].should.deep.equal({ keys: [docA._id, docB._id], include_docs: true });
+          allDocs.getCall(1).args[0].should.deep.equal({ keys: [docA._id, docB._id], include_docs: true });
           allDocs.getCall(2).args[0].should.deep.equal({ keys: [docB._id], include_docs: true });
           allDocs.getCall(3).args[0].should.deep.equal({ keys: [docB._id], include_docs: true });
-          allDocs.getCall(4).args[0].should.deep.equal({ keys: [docB._id], include_docs: true });
+          allDocs.getCall(4).args[0].should.deep.equal({ keys: [docC._id], include_docs: true });
           bulkDocs.callCount.should.equal(5);
-          bulkDocs.getCall(1).args[0].should.deep.equal([expectedC, expectedA]);
+          bulkDocs.getCall(0).args[0].should.deep.equal([expectedA, expectedB]);
+          bulkDocs.getCall(1).args[0].should.deep.equal([expectedA, expectedB]);
           bulkDocs.getCall(2).args[0].should.deep.equal([expectedB]);
           bulkDocs.getCall(3).args[0].should.deep.equal([expectedB]);
-          bulkDocs.getCall(4).args[0].should.deep.equal([expectedB]);
-          testRes.write.callCount.should.equal(7);
+          bulkDocs.getCall(4).args[0].should.deep.equal([expectedC]);
+          testRes.write.callCount.should.equal(4);
           testRes.write.getCall(0).args[0].should.equal('[');
-          testRes.write.getCall(1).args[0].should.equal('[],');
-          testRes.write.getCall(2).args[0].should.equal('[{"id":"c","ok":true},{"id":"a","ok":true}],');
-          testRes.write.getCall(3).args[0].should.equal('[],');
-          testRes.write.getCall(4).args[0].should.equal('[],');
-          testRes.write.getCall(5).args[0].should.equal('[{"id":"b","error":true}]');
-          testRes.write.getCall(6).args[0].should.equal(']');
+          testRes.write.getCall(1).args[0].should.equal('[{"id":"a","ok":true},{"id":"b","error":true}],');
+          testRes.write.getCall(2).args[0].should.equal('[{"id":"c","ok":true}]');
+          testRes.write.getCall(3).args[0].should.equal(']');
           testRes.end.callCount.should.equal(1);
         });
     });
@@ -125,19 +122,21 @@ describe('Bulk Docs Service', function () {
       const parent = generateParentDoc();
       const docB = { _id: 'b', _rev: '1' };
       const docC = { _id: 'c', _rev: '1' };
+      const expectedA = Object.assign({}, docA, { _deleted: true });
       const expectedParent = Object.assign({}, parent, { contact: null });
+      const expectedB = Object.assign({}, docB, { _deleted: true });
+      const expectedC = Object.assign({}, docC, { _deleted: true });
 
       const allDocs = sinon.stub(db.medic, 'allDocs');
       allDocs.onCall(0).resolves({ rows: [{ doc: docA }, { doc: docB }] });
       allDocs.onCall(1).resolves({ rows: [{ doc: docC }] });
 
       const bulkDocs = sinon.stub(db.medic, 'bulkDocs');
-      bulkDocs.onCall(0).resolves([{ id: docA._id, ok: true }, { id: docB._id, ok: true }]);
+      bulkDocs.onCall(0).resolves([{ id: docA._id, ok: true }, { id: docB._id, ok: true }, { id: parent._id, error: true }]);
       bulkDocs.onCall(1).resolves([{ id: parent._id, error: true }]);
       bulkDocs.onCall(2).resolves([{ id: parent._id, error: true }]);
       bulkDocs.onCall(3).resolves([{ id: parent._id, error: true }]);
-      bulkDocs.onCall(4).resolves([{ id: parent._id, error: true }]);
-      bulkDocs.onCall(5).resolves([{ id: docC._id, ok: true }]);
+      bulkDocs.onCall(4).resolves([{ id: docC._id, ok: true }]);
 
       const get = sinon.stub(db.medic, 'get');
       get.onCall(0).resolves(generateParentDoc());
@@ -148,11 +147,12 @@ describe('Bulk Docs Service', function () {
       return service.bulkDelete(testDocs, testRes, { batchSize: 2 })
         .then(() => {
           allDocs.callCount.should.equal(2);
-          bulkDocs.callCount.should.equal(6);
-          bulkDocs.getCall(1).args[0].should.deep.equal([expectedParent]);
-          bulkDocs.getCall(2).args[0].should.deep.equal([expectedParent]);
-          bulkDocs.getCall(3).args[0].should.deep.equal([expectedParent]);
-          bulkDocs.getCall(4).args[0].should.deep.equal([expectedParent]);
+          bulkDocs.callCount.should.equal(5);
+          bulkDocs.getCall(0).args[0].should.deep.equal([expectedA, expectedB, expectedParent]);
+          bulkDocs.getCall(1).args[0].should.deep.equal([expectedA, expectedParent]);
+          bulkDocs.getCall(2).args[0].should.deep.equal([expectedA, expectedParent]);
+          bulkDocs.getCall(3).args[0].should.deep.equal([expectedA, expectedParent]);
+          bulkDocs.getCall(4).args[0].should.deep.equal([expectedC]);
           testRes.write.callCount.should.equal(4);
           testRes.write.getCall(0).args[0].should.equal('[');
           testRes.write.getCall(1).args[0].should.equal('[{"id":"a","ok":true},{"id":"b","ok":true},{"id":"parent","error":true}],');
