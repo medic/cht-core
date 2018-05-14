f578dfeda22e82904e065f049cc3eb63f197962f
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2018-05-14 19:16:53 +1200

    Bypasses initing pouchdb when UNIT_TEST_ENV is set

--- sentinel/src/db-pouch.js
+++ sentinel/src/db-pouch.js
@@ -2,11 +2,33 @@ const PouchDB = require('pouchdb-core');
 PouchDB.plugin(require('pouchdb-adapter-http'));
 PouchDB.plugin(require('pouchdb-mapreduce'));
 
-const { COUCH_URL } = process.env;
-// strip trailing slash from to prevent bugs in path matching
-const couchUrl = COUCH_URL && COUCH_URL.replace(/\/$/, '');
+const { COUCH_URL, UNIT_TEST_ENV } = process.env;
 
-module.exports.medic = new PouchDB(couchUrl);
-module.exports.audit = new PouchDB(`${couchUrl}-audit`);
-module.exports.sentinel = new PouchDB(`${couchUrl}-sentinel`);
-module.exports._url = couchUrl;
+if(UNIT_TEST_ENV) {
+  const stubMe = functionName => () => {
+    console.error(new Error(`db.${functionName}() not stubbed!  UNIT_TEST_ENV=${UNIT_TEST_ENV}.  Please stub PouchDB functions that will be interacted with in unit tests.`));
+    process.exit(1);
+  };
+
+  module.exports.medic = {
+    allDocs: stubMe('allDocs'),
+    bulkDocs: stubMe('bulkDocs'),
+    put: stubMe('put'),
+    post: stubMe('post'),
+    query: stubMe('query'),
+  };
+} else if(COUCH_URL) {
+  // strip trailing slash from to prevent bugs in path matching
+  const couchUrl = COUCH_URL && COUCH_URL.replace(/\/$/, '');
+
+  module.exports.medic = new PouchDB(couchUrl);
+  module.exports.audit = new PouchDB(`${couchUrl}-audit`);
+  module.exports.sentinel = new PouchDB(`${couchUrl}-sentinel`);
+} else {
+  console.log(
+    'Please define a COUCH_URL in your environment e.g. \n' +
+    'export COUCH_URL=\'http://admin:123qwe@localhost:5984/medic\'\n\n' +
+    'If you are running unit tests use UNIT_TEST_ENV=1 in your environment.\n'
+  );
+  process.exit(1);
+}
