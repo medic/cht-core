39e3a6691371786fd6c91d4b599dd766d5f3ccdc
Diana Barsan <barsan@medicmobile.org>
2018-05-15 20:09:05 +0300

    Be clearer about what this is for.

--- api/src/routing.js
+++ api/src/routing.js
@@ -533,12 +533,11 @@ proxy.on('proxyReq', function(proxyReq, req, res) {
     writeHeaders(req, res);
   }
 
+  // allow POST requests which have been body-parsed to be correctly proxied
   if(req.body) {
     let bodyData = JSON.stringify(req.body);
-    // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
     proxyReq.setHeader('Content-Type','application/json');
     proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
-    // stream the content
     proxyReq.write(bodyData);
   }
 });
