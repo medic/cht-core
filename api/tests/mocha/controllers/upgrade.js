d09419523307a30698a86f87b9a38bf41ae3ee95
Stefan du Fresne <stefan@sdufresne.info>
2018-05-16 18:16:05 +0100

    Staged deploys, followable deploys
    Adding webapp and api support for staged deploys.
    
    You can now either completely install or stage a deployment. If you stage a deployment it will do everything except deploy, which will be left to you to fire at your convenience.
    
    When deploying you will also see a live log of what is happening on the server.
    
    Requires Horticulturalist 0.10.0.
    
    medic/medic-webapp#3993

--- api/tests/mocha/controllers/upgrade.js
+++ api/tests/mocha/controllers/upgrade.js
@@ -22,42 +22,78 @@ describe('Upgrade controller', () => {
     sinon.stub(auth, 'check');
     sinon.stub(serverUtils, 'error');
     sinon.stub(service, 'upgrade').returns(Promise.resolve());
+    sinon.stub(service, 'complete').returns(Promise.resolve());
   });
 
   afterEach(() => sinon.restore());
 
-  it('checks that user is an admin', () => {
-    auth.check.returns(Promise.reject(''));
-    return controller.upgrade(req, {})
-      .then(() => {
-        auth.check.callCount.should.equal(1);
-        serverUtils.error.callCount.should.equal(1);
-      });
+  describe('Upgrade', () => {
+    it('checks that user has the right permissions', () => {
+      auth.check.returns(Promise.reject(''));
+      return controller.upgrade(req, {})
+        .then(() => {
+          auth.check.callCount.should.equal(1);
+          auth.check.args[0][1].should.equal('can_configure');
+          serverUtils.error.callCount.should.equal(1);
+        });
+    });
+
+    it('checks that the user passed a build info body', () => {
+      auth.check.returns(Promise.resolve({}));
+      const json = sinon.stub();
+      return controller.upgrade({body: {}}, {json: json})
+        .then(() => {
+          auth.check.callCount.should.equal(1);
+          serverUtils.error.callCount.should.equal(1);
+          serverUtils.error.args[0][0].status.should.equal(400);
+        });
+    });
+
+    it('calls the uprgade service', () => {
+      auth.check.returns(Promise.resolve({user: 'admin'}));
+      const json = sinon.stub();
+      return controller.upgrade(req, {json: json})
+        .then(() => {
+          auth.check.callCount.should.equal(1);
+          serverUtils.error.callCount.should.equal(0);
+          service.upgrade.callCount.should.equal(1);
+          service.upgrade.args[0][0].should.deep.equal(req.body.build);
+          service.upgrade.args[0][1].should.equal('admin');
+          service.upgrade.args[0][2].should.deep.equal({stageOnly: false});
+          json.callCount.should.equal(1);
+          json.args[0][0].should.deep.equal({ok: true});
+        });
+    });
   });
 
-  it('checks that the user passed a build info body', () => {
-    auth.check.returns(Promise.resolve({}));
-    const json = sinon.stub();
-    return controller.upgrade({body: {}}, {json: json})
-      .then(() => {
-        auth.check.callCount.should.equal(1);
-        serverUtils.error.callCount.should.equal(1);
-        serverUtils.error.args[0][0].status.should.equal(400);
-      });
+  describe('Stage', () => {
+    it('calls the upgrade service', () => {
+      auth.check.returns(Promise.resolve({user: 'admin'}));
+      const json = sinon.stub();
+      return controller.stage(req, {json: json})
+        .then(() => {
+          auth.check.callCount.should.equal(1);
+          serverUtils.error.callCount.should.equal(0);
+          service.upgrade.callCount.should.equal(1);
+          service.upgrade.args[0][0].should.deep.equal(req.body.build);
+          service.upgrade.args[0][1].should.equal('admin');
+          service.upgrade.args[0][2].should.deep.equal({stageOnly: true});
+          json.callCount.should.equal(1);
+          json.args[0][0].should.deep.equal({ok: true});
+        });
+    });
   });
 
-  it('calls the upgade service if the user is an admin', () => {
-    auth.check.returns(Promise.resolve({user: 'admin'}));
-    const json = sinon.stub();
-    return controller.upgrade(req, {json: json})
-      .then(() => {
-        auth.check.callCount.should.equal(1);
-        serverUtils.error.callCount.should.equal(0);
-        service.upgrade.callCount.should.equal(1);
-        service.upgrade.args[0][0].should.deep.equal(req.body.build);
-        service.upgrade.args[0][1].should.equal('admin');
-        json.callCount.should.equal(1);
-        json.args[0][0].should.deep.equal({ok: true});
-      });
+  describe('Complete staged build', () => {
+    it('Passes the call on to the service', () => {
+      auth.check.returns(Promise.resolve({}));
+
+      return controller.complete({}, {})
+        .then(() => {
+          auth.check.callCount.should.equal(1);
+          auth.check.args[0][1].should.equal('can_configure');
+          service.complete.callCount.should.equal(1);
+        });
+    });
   });
 });
