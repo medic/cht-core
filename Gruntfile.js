af6ffe6a8ec49d1974d886bbc19acc298360d8c9
Robbie Howe <robbie@medicmobile.org>
2018-05-21 15:54:34 +0100

    Updates grunt shared lib test task

--- Gruntfile.js
+++ Gruntfile.js
@@ -422,7 +422,7 @@ module.exports = function(grunt) {
               .map(lib =>
                   `echo Testing shared library: ${lib} &&
                   (cd shared-libs/${lib} &&
-                  [ "$(jq .scripts.test)" = "null" ] || (yarn install && yarn test))`)
+                  [ "$(cat package.json | jq .scripts.test)" = "null" ] || (yarn install && yarn test))`)
               .join(' && ');
         },
       },
