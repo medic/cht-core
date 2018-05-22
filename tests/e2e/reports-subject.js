62cb12fcafece3c88b7c110732401ee2389072e6
ngaruko <ngarukob@yahoo.com>
2018-05-22 14:12:32 +1200

    enabled reports-subject.js

--- tests/e2e/reports-subject.js
+++ tests/e2e/reports-subject.js
@@ -296,7 +296,7 @@ describe('Reports Summary', () => {
   });
 
   describe('Displays correct LHS and RHS summary', () => {
-    xit('Concerning reports using patient_id', () => {
+    it('Concerning reports using patient_id', () => {
       const REPORT = {
         _id: 'REF_REF_V1',
         form: 'RR',
@@ -334,7 +334,7 @@ describe('Reports Summary', () => {
         });
     });
 
-    xit('Concerning reports using doc id', () => {
+    it('Concerning reports using doc id', () => {
       const REPORT = {
         _id: 'REF_REF_V2',
         form: 'RR',
@@ -373,7 +373,7 @@ describe('Reports Summary', () => {
         });
     });
 
-    xit('Concerning reports with unknown patient_id', () => {
+    it('Concerning reports with unknown patient_id', () => {
       const REPORT = {
         _id: 'REF_REF_I',
         form: 'RR',
@@ -411,7 +411,7 @@ describe('Reports Summary', () => {
         });
     });
 
-    xit('Concerning reports using patient name', () => {
+    it('Concerning reports using patient name', () => {
       const REPORT = {
         _id: 'NAM_NAM_V',
         form: 'NN',
@@ -494,7 +494,7 @@ describe('Reports Summary', () => {
         });
     });
 
-    xit('Concerning reports using place_id', () => {
+    it('Concerning reports using place_id', () => {
       const REPORT = {
         _id: 'PREF_PREF_V',
         form: 'P',
@@ -532,7 +532,7 @@ describe('Reports Summary', () => {
         });
     });
 
-    xit('Concerning reports using unknown place_id', () => {
+    it('Concerning reports using unknown place_id', () => {
       const REPORT = {
         _id: 'PREF_PREF_I',
         form: 'P',
