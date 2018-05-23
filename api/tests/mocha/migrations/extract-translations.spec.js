573d782da7def7babca93b3a9df54ac99af48771
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2018-05-23 14:04:35 +1200

    Converted api migrations tests to mocha

---
+++ api/tests/mocha/migrations/extract-translations.spec.js
@@ -0,0 +1,291 @@
+const sinon = require('sinon').sandbox.create(),
+      chai = require('chai'),
+      db = require('../../../src/db-nano'),
+      settingsService = require('../../../src/services/settings'),
+      migration = require('../../../src/migrations/extract-translations');
+
+let originalDbSettings;
+
+describe('extract-person-contacts migration', () => {
+
+  beforeEach(() => {
+    originalDbSettings = db.settings;
+  });
+
+  afterEach(() => {
+    sinon.restore();
+    db.settings = originalDbSettings;
+  });
+
+  it('returns errors from getSettings', done => {
+    const getSettings = sinon.stub(settingsService, 'get').returns(Promise.reject('boom'));
+    migration.run(err => {
+      chai.expect(err).to.equal('boom');
+      chai.expect(getSettings.callCount).to.equal(1);
+      done();
+    });
+  });
+
+  it('does nothing if no configured translations', done => {
+    const translations = [];
+    const locales = [];
+    const getSettings = sinon.stub(settingsService, 'get').returns(Promise.resolve({ translations: translations, locales: locales }));
+    const view = sinon.stub(db.medic, 'view');
+    const bulk = sinon.stub(db.medic, 'bulk');
+    migration.run(err => {
+      chai.expect(getSettings.callCount).to.equal(1);
+      chai.expect(view.callCount).to.equal(0);
+      chai.expect(bulk.callCount).to.equal(0);
+      done(err);
+    });
+  });
+
+  it('returns errors from view', done => {
+    const translations = [
+      { key: 'hello', default: 'Hi', translations: [
+        { locale: 'en', content: 'Hello' },
+        { locale: 'es', content: 'Hola' }
+      ] },
+      { key: 'bye', default: 'Bye', translations: [
+        { locale: 'en', content: 'Goodbye' }
+      ] }
+    ];
+    const locales = [
+      { code: 'en', name: 'English' },
+      { code: 'es', name: 'Español (Spanish)' }
+    ];
+    const getSettings = sinon.stub(settingsService, 'get').returns(Promise.resolve({ translations: translations, locales: locales }));
+    const view = sinon.stub(db.medic, 'view').callsArgWith(3, 'boom');
+    const bulk = sinon.stub(db.medic, 'bulk');
+    migration.run(err => {
+      chai.expect(err).to.equal('boom');
+      chai.expect(getSettings.callCount).to.equal(1);
+      chai.expect(view.callCount).to.equal(1);
+      chai.expect(bulk.callCount).to.equal(0);
+      done();
+    });
+  });
+
+  it('does nothing if no docs', done => {
+    // should only happen if a configurer has deleted all the docs...
+    const translations = [
+      { key: 'hello', default: 'Hi', translations: [
+        { locale: 'en', content: 'Hello' },
+        { locale: 'es', content: 'Hola' }
+      ] },
+      { key: 'bye', default: 'Bye', translations: [
+        { locale: 'en', content: 'Goodbye' }
+      ] }
+    ];
+    const locales = [
+      { code: 'en', name: 'English' },
+      { code: 'es', name: 'Español (Spanish)' }
+    ];
+    const docs = { rows: [] };
+    const getSettings = sinon.stub(settingsService, 'get').returns(Promise.resolve({ translations: translations, locales: locales }));
+    const view = sinon.stub(db.medic, 'view').callsArgWith(3, null, docs);
+    const bulk = sinon.stub(db.medic, 'bulk');
+    const updateSettings = sinon.stub(settingsService, 'update').returns(Promise.resolve());
+    migration.run(err => {
+      chai.expect(getSettings.callCount).to.equal(1);
+      chai.expect(view.callCount).to.equal(1);
+      chai.expect(bulk.callCount).to.equal(0);
+      chai.expect(updateSettings.callCount).to.equal(1);
+      chai.expect(updateSettings.args[0][0].translations).to.equal(null);
+      chai.expect(updateSettings.args[0][0].locales).to.equal(null);
+      done(err);
+    });
+  });
+
+  it('returns errors from bulk', done => {
+    const translations = [
+      { key: 'hello', default: 'Hi', translations: [
+        { locale: 'en', content: 'Hello' },
+        { locale: 'es', content: 'Hola' }
+      ] },
+      { key: 'bye', default: 'Bye', translations: [
+        { locale: 'en', content: 'Goodbye' }
+      ] }
+    ];
+    const locales = [
+      { code: 'en', name: 'English' },
+      { code: 'es', name: 'Español (Spanish)' }
+    ];
+    const docs = { rows: [
+      { doc: {
+        _id: 'messages-en',
+        _rev: '4-f052fe152cd3989aa14dd80f4267607c',
+        type: 'translations',
+        code: 'en',
+        name: 'English',
+        enabled: true,
+        values: {
+          hello: 'Hey'
+        }
+      } }
+    ] };
+    const getSettings = sinon.stub(settingsService, 'get').returns(Promise.resolve({ translations: translations, locales: locales }));
+    const view = sinon.stub(db.medic, 'view').callsArgWith(3, null, docs);
+    const bulk = sinon.stub(db.medic, 'bulk').callsArgWith(1, 'boom');
+    const updateSettings = sinon.stub(settingsService, 'update');
+    migration.run(err => {
+      chai.expect(err).to.equal('boom');
+      chai.expect(getSettings.callCount).to.equal(1);
+      chai.expect(view.callCount).to.equal(1);
+      chai.expect(bulk.callCount).to.equal(1);
+      chai.expect(updateSettings.callCount).to.equal(0);
+      done();
+    });
+  });
+
+  it('returns errors from settings update', done => {
+    const translations = [
+      { key: 'hello', default: 'Hi', translations: [
+        { locale: 'en', content: 'Hello' },
+        { locale: 'es', content: 'Hola' }
+      ] },
+      { key: 'bye', default: 'Bye', translations: [
+        { locale: 'en', content: 'Goodbye' }
+      ] }
+    ];
+    const locales = [
+      { code: 'en', name: 'English' },
+      { code: 'es', name: 'Español (Spanish)' }
+    ];
+    const docs = { rows: [
+      { doc: {
+        _id: 'messages-en',
+        _rev: '4-f052fe152cd3989aa14dd80f4267607c',
+        type: 'translations',
+        code: 'en',
+        name: 'English',
+        enabled: true,
+        values: {
+          hello: 'Hey'
+        }
+      } }
+    ] };
+    const getSettings = sinon.stub(settingsService, 'get').returns(Promise.resolve({ translations: translations, locales: locales }));
+    const view = sinon.stub(db.medic, 'view').callsArgWith(3, null, docs);
+    const bulk = sinon.stub(db.medic, 'bulk').callsArgWith(1);
+    const updateSettings = sinon.stub(settingsService, 'update').returns(Promise.reject('boom'));
+    migration.run(err => {
+      chai.expect(err).to.equal('boom');
+      chai.expect(getSettings.callCount).to.equal(1);
+      chai.expect(view.callCount).to.equal(1);
+      chai.expect(bulk.callCount).to.equal(1);
+      chai.expect(updateSettings.callCount).to.equal(1);
+      done();
+    });
+  });
+
+  it('merges configuration into docs', done => {
+    const translations = [
+      { key: 'hello', default: 'Hi', translations: [
+        { locale: 'en', content: 'Hello configured' },
+        { locale: 'es', content: 'Hola configured' }
+      ] },
+      { key: 'bye', default: 'Bye', translations: [
+        { locale: 'en', content: 'Goodbye configured' }
+      ] }
+    ];
+    const locales = [
+      { code: 'en', name: 'English' },
+      { code: 'es', name: 'Español (Spanish)', disabled: true }
+    ];
+    const docs = { rows: [
+      { doc: {
+        _id: 'messages-en',
+        _rev: '4-f052fe152cd3989aa14dd80f4267607c',
+        type: 'translations',
+        code: 'en',
+        name: 'English',
+        enabled: true,
+        values: {
+          hello: 'Hey',
+          welcome: 'Welcome'
+        }
+      } },
+      { doc: {
+        _id: 'messages-es',
+        _rev: '4-f052fe152cd3989aa14dd80f4267607c',
+        type: 'translations',
+        code: 'es',
+        name: 'Spanish',
+        enabled: true,
+        values: {
+          hello: 'Howdy',
+          welcome: 'Salut'
+        }
+      } },
+      { doc: {
+        _id: 'messages-fr',
+        _rev: '4-f052fe152cd3989aa14dd80f4267607c',
+        type: 'translations',
+        code: 'fr',
+        name: 'French',
+        enabled: true,
+        values: {
+          hello: 'Yo'
+        }
+      } }
+    ] };
+    const getSettings = sinon.stub(settingsService, 'get').returns(Promise.resolve({ translations: translations, locales: locales }));
+    const view = sinon.stub(db.medic, 'view').callsArgWith(3, null, docs);
+    const bulk = sinon.stub(db.medic, 'bulk').callsArgWith(1);
+    const updateSettings = sinon.stub(settingsService, 'update').returns(Promise.resolve());
+    migration.run(err => {
+      chai.expect(getSettings.callCount).to.equal(1);
+      chai.expect(view.callCount).to.equal(1);
+      chai.expect(view.args[0][0]).to.equal('medic-client');
+      chai.expect(view.args[0][1]).to.equal('doc_by_type');
+      chai.expect(view.args[0][2].key[0]).to.equal('translations');
+      chai.expect(view.args[0][2].key[1]).to.equal(true);
+      chai.expect(view.args[0][2].include_docs).to.equal(true);
+      chai.expect(bulk.callCount).to.equal(1);
+      chai.expect(bulk.args[0][0].docs).to.deep.equal([
+        {
+          _id: 'messages-en',
+          _rev: '4-f052fe152cd3989aa14dd80f4267607c',
+          type: 'translations',
+          code: 'en',
+          name: 'English',
+          enabled: true,
+          values: {
+            hello: 'Hello configured',
+            bye: 'Goodbye configured',
+            welcome: 'Welcome'
+          }
+        },
+        {
+          _id: 'messages-es',
+          _rev: '4-f052fe152cd3989aa14dd80f4267607c',
+          type: 'translations',
+          code: 'es',
+          name: 'Spanish',
+          enabled: false,
+          values: {
+            hello: 'Hola configured',
+            welcome: 'Salut'
+          }
+        },
+        {
+          _id: 'messages-fr',
+          _rev: '4-f052fe152cd3989aa14dd80f4267607c',
+          type: 'translations',
+          code: 'fr',
+          name: 'French',
+          enabled: true,
+          values: {
+            hello: 'Yo'
+          }
+        }
+      ]);
+      chai.expect(updateSettings.callCount).to.equal(1);
+      chai.expect(updateSettings.args[0][0].translations).to.equal(null);
+      chai.expect(updateSettings.args[0][0].locales).to.equal(null);
+      done(err);
+    });
+  });
+
+});
---
+++ api/tests/mocha/migrations/namespace-form-fields.spec.js
@@ -0,0 +1,240 @@
+const sinon = require('sinon').sandbox.create(),
+      chai = require('chai'),
+      db = require('../../../src/db-nano'),
+      config = require('../../../src/config'),
+      migration = require('../../../src/migrations/namespace-form-fields');
+
+const makeStubs = (...viewBatches) => {
+  const getView = sinon.stub(db.medic, 'view');
+  if (viewBatches.length === 0) {
+    getView.callsArgWith(3, null,
+      {
+        total_rows: 0,
+        rows: []
+      });
+  } else {
+    const totalRows = viewBatches.reduce(
+      (total, batch) => total + batch.length,
+      0);
+    viewBatches.forEach(function(batch, index) {
+      getView.onCall(index).callsArgWith(3, null,
+        {
+          total_rows: totalRows,
+          rows: batch.map(function(doc) { return {doc: doc}; })
+        });
+    });
+  }
+
+  return {
+    setConfig: sinon.stub(config, 'get').returns(forms),
+    loadConfig: sinon.stub(config, 'load').callsArg(0),
+    bulk: sinon.stub(db.medic, 'bulk').callsArgWith(1, null, null),
+    getView: getView
+  };
+};
+
+const forms =  {
+  P: {
+    meta: {
+      code: 'P',
+      label: {
+        en: 'Pregnancy Registration LMP'
+      }
+    },
+    fields: {
+      last_menstrual_period: {
+        labels: {
+          tiny: {
+            en: 'LMP'
+          },
+          description: {
+            en: 'Weeks since last menstrual period'
+          },
+          short: {
+            en: 'Weeks since LMP'
+          }
+        },
+        type: 'integer'
+      },
+      patient_name: {
+        labels: {
+          tiny: {
+            en: 'N'
+          },
+          description: {
+            en: 'Patient Name'
+          },
+          short: {
+            en: 'Name'
+          }
+        },
+        type: 'string'
+      }
+    },
+    public_form: true,
+    use_sentinel: true
+  }
+};
+
+describe('namespace-form-fields migration', () => {
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('run does nothing if no data records', done => {
+    const stubs = makeStubs();
+    migration.run(err => {
+      chai.expect(stubs.getView.callCount).to.equal(1);
+      done(err);
+    });
+  });
+
+  it('run does nothing if report already migrated', done => {
+    const doc = {
+      _id: 'a',
+      reported_date: '123',
+      form: 'P',
+      fields: { name: 'michael' }
+    };
+    const stubs = makeStubs([doc]);
+    migration.run(err => {
+      chai.expect(stubs.getView.callCount).to.equal(1);
+      done(err);
+    });
+  });
+
+  it('run does nothing if no form', done => {
+    const doc = {
+      _id: 'a',
+      reported_date: '123'
+    };
+    const stubs = makeStubs([doc]);
+    migration.run(err => {
+      chai.expect(stubs.getView.callCount).to.equal(1);
+      done(err);
+    });
+  });
+
+  it('run migrates report', done => {
+    const doc = {
+      _id: 'a',
+      reported_date: '123',
+      form: 'P',
+      last_menstrual_period: 22,
+      patient_name: 'sarah'
+    };
+    const expected = {
+      last_menstrual_period: 22,
+      patient_name: 'sarah'
+    };
+    const stubs = makeStubs([doc]);
+    migration.run(err => {
+      chai.expect(stubs.getView.callCount).to.equal(1);
+      chai.expect(stubs.loadConfig.callCount).to.equal(1);
+      chai.expect(stubs.bulk.callCount).to.equal(1);
+      chai.expect(stubs.bulk.args[0][0].docs[0].fields).to.deep.equal(expected);
+      chai.expect(stubs.bulk.args[0][0].docs[0].last_menstrual_period).to.equal(undefined);
+      chai.expect(stubs.bulk.args[0][0].docs[0].patient_name).to.equal(undefined);
+      done(err);
+    });
+  });
+
+  it('run migrates in batches', done => {
+    const BATCH_SIZE = 1;
+    const docs = [
+        {
+          _id: 'a',
+          reported_date: '123',
+          form: 'P',
+          last_menstrual_period: 22,
+          patient_name: 'sarah'
+        },
+        {
+          _id: 'b',
+          reported_date: '456',
+          form: 'P',
+          last_menstrual_period: 12,
+          patient_name: 'jane'
+        }
+    ];
+    const expected = [
+      {
+        last_menstrual_period: 22,
+        patient_name: 'sarah'
+      },
+      {
+        last_menstrual_period: 12,
+        patient_name: 'jane'
+      }
+    ];
+    const stubs = makeStubs([docs[0]], [docs[1]]);
+    migration._runWithBatchSize(BATCH_SIZE, function(err) {
+      chai.expect(stubs.getView.callCount).to.equal(2);
+      chai.expect(stubs.loadConfig.callCount).to.equal(1);
+      chai.expect(stubs.bulk.callCount).to.equal(2);
+
+      chai.expect(stubs.bulk.args[0][0].docs[0].fields).to.deep.equal(expected[0]);
+      chai.expect(stubs.bulk.args[0][0].docs[0].last_menstrual_period).to.equal(undefined);
+      chai.expect(stubs.bulk.args[0][0].docs[0].patient_name).to.equal(undefined);
+
+      chai.expect(stubs.bulk.args[1][0].docs[0].fields).to.deep.equal(expected[1]);
+      chai.expect(stubs.bulk.args[1][0].docs[0].last_menstrual_period).to.equal(undefined);
+      chai.expect(stubs.bulk.args[1][0].docs[0].patient_name).to.equal(undefined);
+
+      done(err);
+    });
+  });
+
+  it('reports bulk update errors', done => {
+    const BATCH_SIZE = 2;
+    const docs = [
+        {
+          _id: 'a',
+          reported_date: '123',
+          form: 'P',
+          last_menstrual_period: 22,
+          patient_name: 'sarah'
+        },
+        {
+          _id: 'b',
+          reported_date: '456',
+          form: 'P',
+          last_menstrual_period: 12,
+          patient_name: 'jane'
+        }
+    ];
+
+    const stubs = {
+      setConfig: sinon.stub(config, 'get').returns(forms),
+      loadConfig: sinon.stub(config, 'load').callsArg(0),
+      bulk: sinon.stub(db.medic, 'bulk').callsArgWith(1, null,
+        [
+          {
+            id: 'a',
+            error: 'conflict',
+            reason: 'Document update conflict.'
+          },
+          {
+            ok: true,
+            id: 'b',
+            rev: 'blah'
+          }
+        ]),
+      getView: sinon.stub(db.medic, 'view').callsArgWith(3, null,
+        {
+          total_rows: 2,
+          rows: [ { doc: docs[0]}, { doc: docs[1] }]
+        })
+    };
+
+    migration._runWithBatchSize(BATCH_SIZE, function(err) {
+      chai.expect(stubs.getView.callCount).to.equal(1);
+      chai.expect(stubs.loadConfig.callCount).to.equal(1);
+      chai.expect(stubs.bulk.callCount).to.equal(1);
+      chai.expect(!!err).to.equal(true);
+      done();
+    });
+  });
+
+});
