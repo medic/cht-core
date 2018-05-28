fc1a3a560ae0044f7d5511118d5ccbc54111adb2
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2018-05-28 22:36:28 +1200

    Refactors translations util to use pouchdb

--- api/src/translations.js
+++ api/src/translations.js
@@ -1,13 +1,12 @@
-var async = require('async'),
-    _ = require('underscore'),
-    properties = require('properties'),
-    db = require('./db-nano'),
-    DDOC_ID = '_design/medic',
-    TRANSLATION_FILE_NAME_REGEX = /translations\/messages\-([a-z]*)\.properties/,
-    DOC_TYPE = 'translations',
-    BACKUP_TYPE = 'translations-backup';
+const _ = require('underscore'),
+      properties = require('properties'),
+      db = require('./db-pouch'),
+      DDOC_ID = '_design/medic',
+      TRANSLATION_FILE_NAME_REGEX = /translations\/messages\-([a-z]*)\.properties/,
+      DOC_TYPE = 'translations',
+      BACKUP_TYPE = 'translations-backup';
 
-var LOCAL_NAME_MAP = {
+const LOCAL_NAME_MAP = {
   bm: 'Bamanankan (Bambara)',
   en: 'English',
   es: 'Español (Spanish)',
@@ -18,14 +17,14 @@ var LOCAL_NAME_MAP = {
   id: 'Bahasa Indonesia (Indonesian)'
 };
 
-var extractLocaleCode = function(filename) {
-  var parts = TRANSLATION_FILE_NAME_REGEX.exec(filename);
+const extractLocaleCode = filename => {
+  const parts = TRANSLATION_FILE_NAME_REGEX.exec(filename);
   if (parts && parts[1]) {
     return parts[1].toLowerCase();
   }
 };
 
-var createBackup = function(attachment) {
+const createBackup = attachment => {
   return {
     _id: [ 'messages', attachment.code, 'backup' ].join('-'),
     type: BACKUP_TYPE,
@@ -34,7 +33,7 @@ var createBackup = function(attachment) {
   };
 };
 
-var createDoc = function(attachment) {
+const createDoc = attachment => {
   return {
     _id: [ 'messages', attachment.code ].join('-'),
     type: DOC_TYPE,
@@ -45,39 +44,36 @@ var createDoc = function(attachment) {
   };
 };
 
-var merge = function(attachments, backups, docs) {
-  var updatedDocs = [];
-  var knownKeys = [];
-  var english = _.findWhere(attachments, { code: 'en' });
-  if (english) {
-    knownKeys = Object.keys(english.values);
-  }
-  attachments.forEach(function(attachment) {
-    var code = attachment.code;
+const merge = (attachments, backups, docs) => {
+  const updatedDocs = [];
+  const english = _.findWhere(attachments, { code: 'en' });
+  const knownKeys = english ? Object.keys(english.values) : [];
+  attachments.forEach(attachment => {
+    const code = attachment.code;
     if (!code) {
       return;
     }
-    knownKeys.forEach(function(knownKey) {
-      var value = attachment.values[knownKey];
+    knownKeys.forEach(knownKey => {
+      const value = attachment.values[knownKey];
       if (_.isUndefined(value) || value === null) {
         attachment.values[knownKey] = knownKey;
       }
     });
-    var backup = _.findWhere(backups, { code: code });
+    const backup = _.findWhere(backups, { code: code });
     if (!backup) {
       // new language
       updatedDocs.push(createDoc(attachment));
       updatedDocs.push(createBackup(attachment));
       return;
     }
-    var doc = _.findWhere(docs, { code: code });
+    const doc = _.findWhere(docs, { code: code });
     if (doc) {
       // language hasn't been deleted - free to update
-      var updated = false;
-      Object.keys(attachment.values).forEach(function(key) {
-        var existing = doc.values[key];
-        var backedUp = backup.values[key];
-        var attached = attachment.values[key];
+      let updated = false;
+      Object.keys(attachment.values).forEach(key => {
+        const existing = doc.values[key];
+        const backedUp = backup.values[key];
+        const attached = attachment.values[key];
         if (_.isUndefined(existing) ||
             (existing === backedUp && backedUp !== attached)) {
           // new or updated translation
@@ -98,77 +94,72 @@ var merge = function(attachments, backups, docs) {
   return updatedDocs;
 };
 
-var getAttachment = function(name, callback) {
-  db.medic.attachment.get(DDOC_ID, name, function(err, attachment) {
-    if (err) {
-      return callback(err);
-    }
-    properties.parse(attachment.toString('utf8'), function(err, values) {
-      if (err) {
-        return callback(err);
-      }
-      callback(null, {
-        code: extractLocaleCode(name),
-        values: values
+const getAttachment = name => {
+  return db.medic.getAttachment(DDOC_ID, name)
+    .then(attachment => {
+      return new Promise((resolve, reject) => {
+        properties.parse(attachment.toString('utf8'), (err, values) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve({
+            code: extractLocaleCode(name),
+            values: values
+          });
+        });
       });
     });
-  });
 };
 
-var getAttachments = function(callback) {
-  db.medic.get(DDOC_ID, function(err, ddoc) {
-    if (err) {
-      return callback(err);
-    }
-    if (!ddoc._attachments) {
-      return callback(null, []);
-    }
-    var attachments = _.filter(Object.keys(ddoc._attachments), function(key) {
-      return key.match(TRANSLATION_FILE_NAME_REGEX);
+const getAttachments = () => {
+  return db.medic.get(DDOC_ID)
+    .then(ddoc => {
+      if (!ddoc._attachments) {
+        return [];
+      }
+      const attachments = _.filter(Object.keys(ddoc._attachments), key => {
+        return key.match(TRANSLATION_FILE_NAME_REGEX);
+      });
+      return Promise.all(attachments.map(getAttachment));
     });
-    async.map(attachments, getAttachment, callback);
+};
+
+const getDocs = options => {
+  return db.medic.query('medic-client/doc_by_type', options)
+    .then(response => {
+      return _.pluck(response.rows, 'doc');
+    });
+};
+
+const getBackups = () => {
+  return getDocs({
+    key: [ BACKUP_TYPE ],
+    include_docs: true
   });
 };
 
-var getDocs = function(options, callback) {
-  db.medic.view('medic-client', 'doc_by_type', options, function(err, response) {
-    if (err) {
-      return callback(err);
-    }
-    callback(null, _.pluck(response.rows, 'doc'));
+const getTranslationDocs = () => {
+  return getDocs({
+    startkey: [ DOC_TYPE, false ],
+    endkey: [ DOC_TYPE, true ],
+    include_docs: true
   });
 };
 
 module.exports = {
-  run: function(callback) {
-    getAttachments(function(err, attachments) {
-      if (err) {
-        return callback(err);
-      }
-      if (!attachments.length) {
-        return callback();
-      }
-      var backupOptions = { key: [ BACKUP_TYPE ], include_docs: true };
-      getDocs(backupOptions, function(err, backups) {
-        if (err) {
-          return callback(err);
+  run: () => {
+    return getAttachments()
+      .then(attachments => {
+        if (!attachments.length) {
+          return;
         }
-        var docOptions = {
-          startkey: [ DOC_TYPE, false ],
-          endkey: [ DOC_TYPE, true ],
-          include_docs: true
-        };
-        getDocs(docOptions, function(err, docs) {
-          if (err) {
-            return callback(err);
-          }
-          var updatedDocs = merge(attachments, backups, docs);
-          if (!updatedDocs.length) {
-            return callback();
-          }
-          db.medic.bulk({ docs: updatedDocs }, callback);
-        });
+        return Promise.all([ getBackups(), getTranslationDocs() ])
+          .then(([ backups, docs ]) => merge(attachments, backups, docs))
+          .then(updated => {
+            if (updated.length) {
+              return db.medic.bulkDocs(updated);
+            }
+          });
       });
-    });
   }
 };
