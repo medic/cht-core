304a13dbb8dbf6a61b9f1f4ed0380080fbe2ea86
Diana Barsan <35681649+dianabarsan@users.noreply.github.com>
2018-06-11 17:05:18 +0300

    Export duplicates fix
    Fixes bug where Search lib would slice filtered reports view results with negative end index when API export would paginate.
    Fixes bug where API export would not convert "true" and "false" filters to Boolean.
    Enables sharedLibs unit tests in Travis, but disables bulk-doc-utils unit tests because they're broken.
    
    medic/medic-webapp#4610

--- Gruntfile.js
+++ Gruntfile.js
@@ -276,7 +276,7 @@ module.exports = function(grunt) {
       sharedLibUnit: {
         cmd:  function() {
           var sharedLibs = [
-            'bulk-docs-utils',
+            //'bulk-docs-utils',
             'search',
             'task-utils'
           ];
@@ -284,7 +284,7 @@ module.exports = function(grunt) {
             return 'cd shared-libs/' + lib +
               ' && if [ $(npm run | grep "^\\s\\stest$" | wc -l) -gt 0 ]; then npm install && npm test; fi' +
               ' && cd ../../';
-          }).join(' ; ');
+          }).join(' && ');
         }
       },
       // To monkey patch a library...
@@ -599,6 +599,7 @@ module.exports = function(grunt) {
     'build',
     'minify',
     'karma:unit_ci',
+    'exec:sharedLibUnit',
     'env:test',
     'nodeunit',
     'mochaTest:unit',
