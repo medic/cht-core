7ca85d3426fa36654f6981e473c1a259ef02a312
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2018-06-18 16:43:45 +1200

    Fixes some bugs and tweaks to last visited date
    - Uses the correct patient id field in the view
    - Switches to use relativeDay not relativeDate filter
    - Improves the translations
    
    medic/medic-webapp#4526

--- ddocs/medic-client/views/contacts_by_last_visited/map.js
+++ ddocs/medic-client/views/contacts_by_last_visited/map.js
@@ -5,7 +5,7 @@ function(doc) {
       doc.fields.visited_contact_uuid) {
 
     // Is a visit report about a family
-    emit(doc.fields.patient_uuid, doc.reported_date);
+    emit(doc.fields.visited_contact_uuid, doc.reported_date);
   } else if (doc.type === 'clinic') {
     // Is a family
     emit(doc._id, 0);
