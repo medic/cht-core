b3f463cb1c86a12171a2f1880c2d37cc065b066a
Diana Barsan <barsan@medicmobile.org>
2018-06-26 19:47:06 +0300

    Removes threshold, adds more tests and comments.

--- api/src/routing.js
+++ api/src/routing.js
@@ -104,7 +104,10 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(compression({ threshold: 0 }));
+// requires `req` header `Accept-Encoding` to be `gzip` or `deflate`
+// requires `res` `Content-Type` to be compressible (see https://github.com/jshttp/mime-db/blob/master/db.json)
+// default threshold is 1KB
+app.use(compression());
 
 // TODO: investigate blocking writes to _users from the outside. Reads maybe as well, though may be harder
 //       https://github.com/medic/medic-webapp/issues/4089
--- tests/e2e/api/server.js
+++ tests/e2e/api/server.js
@@ -50,11 +50,20 @@ describe('server', () => {
       });
     };
 
-    it('compresses proxied CouchDB application/json requests which send accept-encoding headers', () => {
+    it('compresses proxied CouchDB application/json requests which send accept-encoding gzip headers', () => {
       const options = { uri: '/_all_docs' };
 
       return requestWrapper(options).then(({res}) => {
         expect(res.headers['content-encoding']).toEqual('gzip');
+        expect(res.headers['content-type']).toEqual('application/json');
+      });
+    });
+
+    it('compresses proxied CouchDB application/json requests which send accept-encoding deflate headers', () => {
+      const options = { uri: '/_all_docs', gzip: false, headers: { 'Accept-Encoding': 'deflate' } };
+
+      return requestWrapper(options).then(({res}) => {
+        expect(res.headers['content-encoding']).toEqual('deflate');
         expect(res.headers['content-type']).toEqual('application/json');
       });
     });
@@ -69,22 +78,34 @@ describe('server', () => {
     });
 
     it('compresses audited endpoints responses', () => {
+      // compression threshold is 1024B
       const options = {
         uri: '/_bulk_docs',
         method: 'POST',
         json: true,
-        body: { docs: [{ _id: 'sample_doc' }]}
+        body: {
+          docs: [
+            { _id: 'sample_doc' }, { _id: 'sample_doc2' }, { _id: 'sample_doc3' },
+            { _id: 'sample_doc4' }, { _id: 'sample_doc5' }, { _id: 'sample_doc6' },
+            { _id: 'sample_doc7' }, { _id: 'sample_doc8' }, { _id: 'sample_doc9' },
+            { _id: 'sample_doc7' }, { _id: 'sample_doc8' }, { _id: 'sample_doc9' },
+            { _id: 'sample_doc7' }, { _id: 'sample_doc8' }, { _id: 'sample_doc9' },
+            { _id: 'sample_doc7' }, { _id: 'sample_doc8' }, { _id: 'sample_doc9' },
+          ]
+        }
       };
 
       return requestWrapper(options).then(({res, body}) => {
         expect(res.headers['content-type']).toEqual('application/json');
         expect(res.headers['content-encoding']).toEqual('gzip');
-        expect(body.length).toEqual(1);
+        expect(body.length).toEqual(18);
         expect(_.omit(body[0], 'rev')).toEqual({ id: 'sample_doc', ok: true });
+        expect(_.omit(body[1], 'rev')).toEqual({ id: 'sample_doc2', ok: true });
+        expect(_.omit(body[2], 'rev')).toEqual({ id: 'sample_doc3', ok: true });
       });
     });
 
-    it('compresses compressible CouchDB doc attachments', () => {
+    it('compresses compressible CouchDB doc attachments (text/plain)', () => {
       return utils
         .getDoc('sample_doc')
         .then(doc => {
@@ -106,6 +127,62 @@ describe('server', () => {
           expect(res.headers['content-type']).toEqual('text/plain');
           expect(res.headers['content-encoding']).toEqual('gzip');
           expect(body).toEqual('my-attachment-content');
+        });
+    });
+
+    it('compresses compressible CouchDB doc attachments (application/xml)', () => {
+      const xml = '<contact><_id>689960f3-edc2-429b-92f7-96799b3db7d5</_id><patient_id>40599</patient_id>' +
+                  '<name>Person 1.1.2.1</name><date_of_birth /><sex /><parent><contact><phone />' +
+                  '<name>Person 1.1.2.1</name></contact></parent></contact>';
+      return utils
+        .getDoc('sample_doc2')
+        .then(doc => {
+          const options = {
+            uri: '/sample_doc2/attach?rev=' + doc._rev,
+            body: xml,
+            headers: { 'Content-Type': 'application/xml' },
+            method: 'PUT'
+          };
+
+          return requestWrapper(options);
+        })
+        .then(({res, body}) => {
+          const options = { uri: '/sample_doc2/attach?rev=' + body.rev};
+
+          return requestWrapper(options);
+        })
+        .then(({res, body}) => {
+          expect(res.headers['content-type']).toEqual('application/xml');
+          expect(res.headers['content-encoding']).toEqual('gzip');
+          expect(body).toEqual(xml);
+        });
+    });
+
+    it('does not compress uncompressible CouchDB doc attachments (image/png)', () => {
+      const png = '<contact><_id>689960f3-edc2-429b-92f7-96799b3db7d5</_id><patient_id>40599</patient_id>' +
+                  '<name>Person 1.1.2.1</name><date_of_birth /><sex /><parent><contact><phone />' +
+                  '<name>Person 1.1.2.1</name></contact></parent></contact>';
+      return utils
+        .getDoc('sample_doc2')
+        .then(doc => {
+          const options = {
+            uri: '/sample_doc2/attach?rev=' + doc._rev,
+            body: png,
+            headers: { 'Content-Type': 'image/png' },
+            method: 'PUT'
+          };
+
+          return requestWrapper(options);
+        })
+        .then(({res, body}) => {
+          const options = { uri: '/sample_doc2/attach?rev=' + body.rev};
+
+          return requestWrapper(options);
+        })
+        .then(({res, body}) => {
+          expect(res.headers['content-type']).toEqual('image/png');
+          expect(res.headers['content-encoding']).toBeFalsy();
+          expect(body).toEqual(png);
         });
     });
   });
