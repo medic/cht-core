237fc06dcf3cd6fcc514e282a512258feec99df2
Alex Anderson <anderson@medicmobile.org>
2018-06-26 14:07:22 +0000

    Revert "check that failing test will fail travis"
    This reverts commit a945fbc6c40f93d4140d3d08e6e56fdf7d477d00.

--- config/standard/test/tasks.spec.js
+++ config/standard/test/tasks.spec.js
@@ -85,7 +85,7 @@ describe('Standard Configuration Tasks', function() {
           assertTitle(task, 'Postnatal visit needed');
           assertNotResolved(task);
           assert.deepInclude(task.actions[0].content, {
-            "source": "return",
+            "source": "task",
             "source_id": "report-1",
             contact: fixtures.contact(),
           });
