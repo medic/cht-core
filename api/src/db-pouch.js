0ebc6347b26b643d9d8f2ba6d2491f04e77e8e2a
Victor Gomez <victor@medicmobile.org>
2018-07-18 10:13:04 -0600

    Reverting to api logs with couch_url warning

--- api/src/db-pouch.js
+++ api/src/db-pouch.js
@@ -33,6 +33,8 @@ if(UNIT_TEST_ENV) {
   module.exports.users = new PouchDB(usersDbUrl);
 } else {
   console.log(
+    'Please define a COUCH_URL in your environment e.g. \n' +
+    'export COUCH_URL=\'http://admin:123qwe@localhost:5984/medic\'\n\n' +
     'If you are running unit tests use UNIT_TEST_ENV=1 in your environment.\n'
   );
   process.exit(1);
