5774d377fdb33c904c956b9123fd08dd9c3e8fc3
Diana Barsan <barsan@medicmobile.org>
2018-07-30 19:35:59 +0300

    Clean up unused test code.

--- tests/protractor/page-objects/forms/photo-upload.po.js
+++ tests/protractor/page-objects/forms/photo-upload.po.js
@@ -43,22 +43,9 @@ const docs = [
     }
   }];
 
-const selectRadioButton = (value) => {
-  element(by.css(`[value=${value}]`)).click();
-};
-
 module.exports = {
   configureForm: (contactId, done) => {
     utils.seedTestData(done, contactId, docs);
-  },
-  nextPage: () => {
-    const nextButton = element(by.css('button.btn.btn-primary.next-page'));
-    helper.waitElementToBeClickable(nextButton);
-    nextButton.click();
-  },
-
-  goBack: () => {
-    element(by.css('button.btn.btn-default.previous-page')).click();
   },
 
   submit: () => {
@@ -68,76 +55,7 @@ module.exports = {
     helper.waitElementToBeVisible(element(by.css('div#reports-content')));
   },
 
-  //patient page
-  getPatientPageTitle: () => {
-    return element(by.css('span[data-itext-id=/delivery/inputs:label]'));
-  },
-
-  selectPatientName: (name) => {
-    helper.waitElementToBeClickable(element(by.css('button.btn.btn-primary.next-page')));
-    element(by.css('.selection')).click();
-    const search = element(by.css('.select2-search__field'));
-    search.click();
-    search.sendKeys(name);
-    helper.waitElementToBeVisible(element(by.css('.name')));
-    element(by.css('.name')).click();
-  },
-
-  //Delivery Info page -- Pregnancy outcomes
-  selectLiveBirthButton: () => {
-    selectRadioButton('healthy');
-  },
-  selectStillBirthButton: () => {
-    selectRadioButton('still_birth');
-  },
-
-  selectMiscarriageButton: () => {
-    selectRadioButton('miscarriage');
-  },
-
-  //Delivery Info page -- Location of delivery
-  selectFacilityButton: () => {
-    selectRadioButton('f');
-  },
-
-  selectHomeSkilledButton: () => {
-    selectRadioButton('s');
-  },
-
-  selectHomeNonSkilledButton: () => {
-    selectRadioButton('ns');
-  },
-
-  //Delivery Info page -- Delivery date
-  enterDeliveryDate: (deliveryDate) => {
-    const datePicker = element(by.css('[placeholder="yyyy-mm-dd"]'));
-    datePicker.click();
-    //type date in the text box as '2017-04-23'
-    datePicker.sendKeys(deliveryDate);
-  },
-
   reset: () => {
     element(by.css('.icon.icon-refresh')).click();
-  },
-
-  //note to CHW
-  getNoteToCHW: () => {
-    return element(by.css('textarea')).getAttribute('value');
-  },
-
-  //summary page
-  getOutcomeText: () => {
-    return element(by.css('[lang="en"] [data-value=" /delivery/group_delivery_summary/display_delivery_outcome "]'))
-      .getText();
-  },
-
-  getDeliveryLocationSummaryText: () => {
-    return element(by.css('[lang="en"] [data-value=" /delivery/group_summary/r_delivery_location "]'))
-      .getText();
-  },
-
-  getFollowUpMessage: () => {
-    return element(by.css('[lang="en"] [data-value=" /delivery/group_note/g_chw_sms "]'))
-      .getText();
-  },
+  }
 };
