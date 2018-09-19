277f8fad4b0afa601a60c4dc5eff3ab193e45229
Marc Abbyad <marc@medicmobile.org>
2018-09-19 15:37:15 -0400

    Support for contacts and reports for all nools workflows (#4807)
    Updates to medic-conf 1.17.9 new syntax
    
    Cherry picked from commit 5b4f94c0d5b2bfc6da69812c36c71f76c8235e10, then resolved conflicts, which were all related to tasks using translation keys instead of localization array.

--- config/standard/tasks.js
+++ config/standard/tasks.js
@@ -2,29 +2,54 @@
   {
     icon: 'mother-child',
     title: 'task.pregnancy_danger_sign.title',
-    appliesToForms: [ 'P', 'pregnancy' ],
+    appliesTo: 'reports',
+    appliesToType: ['P', 'pregnancy'],
     appliesIf: function(c, r) {
       // ANC TASK if a F flag during pregnancy
-      return Utils.isFormSubmittedInWindow(c.reports, 'F',
-          r.reported_date,
-          Utils.addDate(new Date(r.reported_date), MAX_DAYS_IN_PREGNANCY).getTime());
+      return Utils.isFormSubmittedInWindow(
+        c.reports,
+        'F',
+        r.reported_date,
+        Utils.addDate(
+          new Date(r.reported_date),
+          MAX_DAYS_IN_PREGNANCY
+        ).getTime()
+      );
     },
-    actions: [ { form:'pregnancy_visit' } ],
-    events: [ {
-      id: 'pregnancy-danger-sign',
-      start:0, end:6,
-      dueDate: function() { return new Date(Utils.getMostRecentTimestamp(c.reports, 'F')); },
-    } ],
+    actions: [{ form: 'pregnancy_visit' }],
+    events: [
+      {
+        id: 'pregnancy-danger-sign',
+        days: 0,
+        start: 0,
+        end: 6,
+        dueDate: function(r, event) {
+          return new Date(
+            Utils.addDate(
+              new Date(
+                Utils.getMostRecentTimestamp(c.reports, 'F')
+              ),
+              event.days
+            )
+          );
+        },
+      },
+    ],
     priority: {
       level: 'high',
       label: 'task.warning.danger_sign',
     },
     resolvedIf: function(c, r, event, dueDate) {
-      return r.reported_date < getNewestDeliveryTimestamp(c) ||
-             r.reported_date < getNewestPregnancyTimestamp(c) ||
-             isFormFromArraySubmittedInWindow(c.reports, 'pregnancy_visit',
-                 Utils.addDate(dueDate, -event.start).getTime(),
-                 Utils.addDate(dueDate,  event.end+1).getTime());
+      return (
+        r.reported_date < getNewestDeliveryTimestamp(c) ||
+        r.reported_date < getNewestPregnancyTimestamp(c) ||
+        isFormFromArraySubmittedInWindow(
+          c.reports,
+          'pregnancy_visit',
+          Utils.addDate(dueDate, -event.start).getTime(),
+          Utils.addDate(dueDate, event.end + 1).getTime()
+        )
+      );
     },
   },
 
@@ -32,16 +57,27 @@
   {
     icon: 'mother-child',
     title: 'task.pregnancy_missing_birth.title',
-    appliesToForms: [ 'P', 'pregnancy' ],
-    appliesIf: function(c, r) { return r.scheduled_tasks; },
-    actions: [ { form:'delivery' } ],
-    events: [ {
-      id: 'pregnancy-missing-birth',
-      start:1, end:13,
-      dueDate: function(r) { return Utils.addDate(new Date(r.scheduled_tasks[r.scheduled_tasks.length-1].due), 7); },
-    } ],
+    appliesTo: 'reports',
+    appliesToType: ['P', 'pregnancy'],
+    appliesIf: function(c, r) {
+      return r.scheduled_tasks;
+    },
+    actions: [{ form: 'delivery' }],
+    events: [
+      {
+        id: 'pregnancy-missing-birth',
+        start: 1,
+        end: 13,
+        dueDate: function(r) {
+          return Utils.addDate(
+            new Date(r.scheduled_tasks[r.scheduled_tasks.length - 1].due),
+            7
+          );
+        },
+      },
+    ],
     priority: function(c, r) {
-      if(isHighRiskPregnancy(c, r)) {
+      if (isHighRiskPregnancy(c, r)) {
         return {
           level: 'high',
           label: 'task.warning.high_risk',
@@ -52,10 +88,15 @@
       // Missing Birth Report
       // Resolved if the scheduled SMS that generated the task is cleared
       //          or if a birth report was submitted
-      return r.scheduled_tasks[r.scheduled_tasks.length-1].state === 'cleared' ||
-        isFormFromArraySubmittedInWindow(c.reports, deliveryForms,
+      return (
+        r.scheduled_tasks[r.scheduled_tasks.length - 1].state === 'cleared' ||
+        isFormFromArraySubmittedInWindow(
+          c.reports,
+          deliveryForms,
           r.reported_date,
-          r.reported_date + (MAX_DAYS_IN_PREGNANCY+DAYS_IN_PNC)*MS_IN_DAY);
+          r.reported_date + (MAX_DAYS_IN_PREGNANCY + DAYS_IN_PNC) * MS_IN_DAY
+        )
+      );
     },
   },
 
@@ -67,19 +108,26 @@
   {
     icon: 'pregnancy-1',
     title: 'task.pregnancy_missing_visit.title',
-    appliesToForms: [ 'P', 'pregnancy' ],
-    appliesToScheduledTaskIf: function(r, i) {
-      return (i+1 < r.scheduled_tasks.length &&
-          (r.scheduled_tasks[i].group !== r.scheduled_tasks[i+1].group ||
-           r.scheduled_tasks[i].type  !== r.scheduled_tasks[i+1].type));
+    appliesTo: 'scheduled_tasks',
+    appliesToType: ['P', 'pregnancy'],
+    appliesIf: function(c, r, i) {
+      return (
+        i + 1 < r.scheduled_tasks.length &&
+        (r.scheduled_tasks[i].group !== r.scheduled_tasks[i + 1].group ||
+          r.scheduled_tasks[i].type !== r.scheduled_tasks[i + 1].type)
+      );
     },
-    actions: [ { form:'pregnancy_visit' } ],
-    events: [ {
-      id: 'pregnancy-missing-visit',
-      days:7, start:0, end:6,
-    } ],
+    actions: [{ form: 'pregnancy_visit' }],
+    events: [
+      {
+        id: 'pregnancy-missing-visit',
+        days: 7,
+        start: 0,
+        end: 6,
+      },
+    ],
     priority: function(c, r) {
-      if(isHighRiskPregnancy(c, r)) {
+      if (isHighRiskPregnancy(c, r)) {
         return {
           level: 'high',
           label: 'task.warning.high_risk',
@@ -87,11 +135,16 @@
       }
     },
     resolvedIf: function(c, r, event, dueDate) {
-      return r.reported_date < getNewestPregnancyTimestamp(c) ||
-             r.reported_date < getNewestDeliveryTimestamp(c) ||
-             isFormFromArraySubmittedInWindow(c.reports, antenatalForms,
-                 Utils.addDate(dueDate, -event.start).getTime(),
-                 Utils.addDate(dueDate,  event.end+1).getTime());
+      return (
+        r.reported_date < getNewestPregnancyTimestamp(c) ||
+        r.reported_date < getNewestDeliveryTimestamp(c) ||
+        isFormFromArraySubmittedInWindow(
+          c.reports,
+          antenatalForms,
+          Utils.addDate(dueDate, -event.start).getTime(),
+          Utils.addDate(dueDate, event.end + 1).getTime()
+        )
+      );
     },
   },
 
@@ -99,29 +152,41 @@
   {
     icon: 'mother-child',
     title: 'task.postnatal_home_birth.title',
-    appliesToForms: [ 'D', 'delivery' ],
+    appliesTo: 'reports',
+    appliesToType: ['D', 'delivery'],
     appliesIf: function(c, r) {
-      return isCoveredByUseCase(c.contact, 'pnc') &&
-          r.fields &&
-             r.fields.delivery_code &&
-             r.fields.delivery_code.toUpperCase() !== 'F';
+      return (
+        isCoveredByUseCase(c.contact, 'pnc') &&
+        r.fields &&
+        r.fields.delivery_code &&
+        r.fields.delivery_code.toUpperCase() !== 'F'
+      );
     },
-    actions: [ { form:'postnatal_visit' } ],
-    events: [ {
-      id: 'postnatal-home-birth',
-      days:0, start:0, end:4,
-    } ],
+    actions: [{ form: 'postnatal_visit' }],
+    events: [
+      {
+        id: 'postnatal-home-birth',
+        days: 0,
+        start: 0,
+        end: 4,
+      },
+    ],
     priority: {
       level: 'high',
       label: 'task.warning.home_birth',
     },
     resolvedIf: function(c, r, event, dueDate) {
       // Resolved if there a visit report received in time window or a newer pregnancy
-      return r.reported_date < getNewestDeliveryTimestamp(c) ||
-             r.reported_date < getNewestPregnancyTimestamp(c) ||
-             isFormFromArraySubmittedInWindow(c.reports, postnatalForms,
-                 Utils.addDate(dueDate, -event.start).getTime(),
-                 Utils.addDate(dueDate,  event.end+1).getTime());
+      return (
+        r.reported_date < getNewestDeliveryTimestamp(c) ||
+        r.reported_date < getNewestPregnancyTimestamp(c) ||
+        isFormFromArraySubmittedInWindow(
+          c.reports,
+          postnatalForms,
+          Utils.addDate(dueDate, -event.start).getTime(),
+          Utils.addDate(dueDate, event.end + 1).getTime()
+        )
+      );
     },
   },
 
@@ -129,30 +194,46 @@
   {
     icon: 'mother-child',
     title: 'task.postnatal_danger_sign.title',
-    appliesToForms: [ 'D', 'delivery' ],
+    appliesTo: 'reports',
+    appliesToType: ['D', 'delivery'],
     appliesIf: function(c, r) {
-      return isCoveredByUseCase(c.contact, 'pnc') &&
-          Utils.isFormSubmittedInWindow(c.reports, 'F',
-              r.reported_date,
-              Utils.addDate(new Date(r.reported_date), DAYS_IN_PNC).getTime());
+      return (
+        isCoveredByUseCase(c.contact, 'pnc') &&
+        Utils.isFormSubmittedInWindow(
+          c.reports,
+          'F',
+          r.reported_date,
+          Utils.addDate(new Date(r.reported_date), DAYS_IN_PNC).getTime()
+        )
+      );
     },
-    actions: [ { form:'postnatal_visit' } ],
-    events: [ {
-      id: 'postnatal-danger-sign',
-      start:0, end:6,
-      dueDate: function() { return new Date(Utils.getMostRecentTimestamp(c.reports, 'F')); },
-    } ],
+    actions: [{ form: 'postnatal_visit' }],
+    events: [
+      {
+        id: 'postnatal-danger-sign',
+        start: 0,
+        end: 6,
+        dueDate: function() {
+          return new Date(Utils.getMostRecentTimestamp(c.reports, 'F'));
+        },
+      },
+    ],
     priority: {
       level: 'high',
       label: 'task.warning.danger_sign',
     },
     resolvedIf: function(c, r, event, dueDate) {
       // Only resolved with PNC report received from nurse in time window or a newer pregnancy
-      return r.reported_date < getNewestDeliveryTimestamp(c) ||
-             r.reported_date < getNewestPregnancyTimestamp(c) ||
-             isFormFromArraySubmittedInWindow(c.reports, 'postnatal_visit',
-                 Utils.addDate(dueDate, -event.start).getTime(),
-                 Utils.addDate(dueDate,  event.end+1).getTime());
+      return (
+        r.reported_date < getNewestDeliveryTimestamp(c) ||
+        r.reported_date < getNewestPregnancyTimestamp(c) ||
+        isFormFromArraySubmittedInWindow(
+          c.reports,
+          'postnatal_visit',
+          Utils.addDate(dueDate, -event.start).getTime(),
+          Utils.addDate(dueDate, event.end + 1).getTime()
+        )
+      );
     },
   },
 
@@ -161,34 +242,46 @@
   {
     icon: 'mother-child',
     title: 'task.postnatal_missing_visit.title',
-    appliesToForms: [ 'D', 'delivery' ],
-    appliesIf: function() { return isCoveredByUseCase(c.contact, 'pnc'); },
-    appliesToScheduledTaskIf: function(r, i) {
-      return i+1 >= r.scheduled_tasks.length ||
-          r.scheduled_tasks[i].group !== r.scheduled_tasks[i+1].group;
+    appliesTo: 'scheduled_tasks',
+    appliesToType: ['D', 'delivery'],
+    appliesIf: function(c, r, i) {
+      return (
+        isCoveredByUseCase(c.contact, 'pnc') &&
+        (i + 1 >= r.scheduled_tasks.length ||
+         r.scheduled_tasks[i].group !== r.scheduled_tasks[i + 1].group)
+      );
     },
     priority: function(c, r) {
-      if(isHomeBirth(r)) {
+      if (isHomeBirth(r)) {
         return {
           level: 'high',
           label: 'task.warning.home_birth',
         };
       }
     },
-    actions: [ { form:'postnatal_visit' } ],
-    events: [ {
-      id: 'postnatal-missing-visit',
-      days: 1, start: 0, end: 3,
-    } ],
+    actions: [{ form: 'postnatal_visit' }],
+    events: [
+      {
+        id: 'postnatal-missing-visit',
+        days: 1,
+        start: 0,
+        end: 3,
+      },
+    ],
     resolvedIf: function(c, r, event, dueDate, i) {
       // Resolved if the scheduled SMS that generated the task is cleared,
       //          if there a visit report received in time window or a newer pregnancy
-      return r.scheduled_tasks[i].state === 'cleared' ||
-          r.reported_date < getNewestDeliveryTimestamp(c) ||
-          r.reported_date < getNewestPregnancyTimestamp(c) ||
-          isFormFromArraySubmittedInWindow(c.reports, postnatalForms,
-              Utils.addDate(dueDate, -event.start).getTime(),
-              Utils.addDate(dueDate,  event.end+1).getTime());
+      return (
+        r.scheduled_tasks[i].state === 'cleared' ||
+        r.reported_date < getNewestDeliveryTimestamp(c) ||
+        r.reported_date < getNewestPregnancyTimestamp(c) ||
+        isFormFromArraySubmittedInWindow(
+          c.reports,
+          postnatalForms,
+          Utils.addDate(dueDate, -event.start).getTime(),
+          Utils.addDate(dueDate, event.end + 1).getTime()
+        )
+      );
     },
   },
 
@@ -197,21 +290,35 @@
   {
     icon: 'immunization',
     title: 'task.immunization_missing_visit.title',
-    appliesToForms: [ 'CW', 'child_health_registration' ],
-    appliesIf: function() { return isCoveredByUseCase(c.contact, 'imm'); },
-    appliesToScheduledTaskIf: function(r, i) { return immunizationMonths.indexOf(r.scheduled_tasks[i].group) !== -1; },
-    actions: [ { form:'immunization_visit' } ],
-    events: [ {
-      id: 'immunization-missing-visit',
-      days:21, start:7, end:13,
-    } ],
+    appliesTo: 'scheduled_tasks',
+    appliesToType: ['CW', 'child_health_registration'],
+    appliesIf: function(c, r, i) {
+      return (
+        isCoveredByUseCase(c.contact, 'imm') &&
+        immunizationMonths.indexOf(r.scheduled_tasks[i].group) !== -1
+      );
+    },
+    actions: [{ form: 'immunization_visit' }],
+    events: [
+      {
+        id: 'immunization-missing-visit',
+        days: 21,
+        start: 7,
+        end: 13,
+      },
+    ],
     resolvedIf: function(c, r, event, dueDate, i) {
       // Resolved if the scheduled SMS that generated the task is cleared,
       //          if an immunization report has been received in time window starting at SMS send date
-      return r.scheduled_tasks[i].state === 'cleared' ||
-        isFormFromArraySubmittedInWindow(c.reports, immunizationForms,
+      return (
+        r.scheduled_tasks[i].state === 'cleared' ||
+        isFormFromArraySubmittedInWindow(
+          c.reports,
+          immunizationForms,
           Utils.addDate(dueDate, -event.days).getTime(),
-          Utils.addDate(dueDate,  event.end+1).getTime());
+          Utils.addDate(dueDate, event.end + 1).getTime()
+        )
+      );
     },
   },
 ]
