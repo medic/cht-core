69243e65d3dc139c0fd6e4041088c1a5f57e71f1
Diana Barsan <35681649+dianabarsan@users.noreply.github.com>
2018-09-26 10:51:05 +0300

    Fixes z-score calculation errors
    Because of medic/openrosa-xpath-evaluator changes , z-score calculate function now receives object parameters ({ t: <type>, v: <value> }).
    
    medic/medic-webapp#4848

--- webapp/src/js/enketo/medic-xpath-extensions.js
+++ webapp/src/js/enketo/medic-xpath-extensions.js
@@ -1,12 +1,29 @@
-var zscoreUtil;
+var zscoreUtil,
+    _ = require('underscore');
+
+var getValue = function(resultObject) {
+  if (!_.isObject(resultObject) || !resultObject.t) {
+    return resultObject;
+  }
+
+  // input fields, evaluated as `UNORDERED_NODE_ITERATOR_TYPE`, are received as arrays with one element
+  if (resultObject.t === 'arr' && resultObject.v.length) {
+    return resultObject.v[0];
+  }
+
+  return resultObject.v;
+};
 
 module.exports = {
   init: function(_zscoreUtil) {
     zscoreUtil = _zscoreUtil;
   },
   func: {
-    'z-score': function(chartId, sex, x, y) {
-      var result = zscoreUtil(chartId, sex, x, y);
+    'z-score': function() {
+      var args = Array.from(arguments).map(function(arg) {
+        return getValue(arg);
+      });
+      var result = zscoreUtil.apply(null, args);
       if (!result) {
           return { t: 'str', v: '' };
       }
