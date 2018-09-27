5b33c873f7d391707b95cc3246d4b8a4f12cdd6c
Willy Tekeu <willy.tekeu@gmail.com>
2018-09-27 18:01:20 -0400

    Linking the report to the task message
    We want to be able to to show that CHWs receive our SMS and then respond to them.

--- sentinel/src/transitions/accept_patient_reports.js
+++ sentinel/src/transitions/accept_patient_reports.js
@@ -1,35 +1,35 @@
 var _ = require('underscore'),
-    async = require('async'),
-    config = require('../config'),
-    messages = require('../lib/messages'),
-    moment = require('moment'),
-    validation = require('../lib/validation'),
-    utils = require('../lib/utils'),
-    transitionUtils = require('./utils'),
-    date = require('../date'),
-    db = require('../db-nano'),
-    NAME = 'accept_patient_reports';
+  async = require('async'),
+  config = require('../config'),
+  messages = require('../lib/messages'),
+  moment = require('moment'),
+  validation = require('../lib/validation'),
+  utils = require('../lib/utils'),
+  transitionUtils = require('./utils'),
+  date = require('../date'),
+  db = require('../db-nano'),
+  NAME = 'accept_patient_reports';
 
-const _hasConfig = (doc) => {
-    return Boolean(getConfig(doc.form));
+const _hasConfig = doc => {
+  return Boolean(getConfig(doc.form));
 };
 
 // This is more complicated than it needs to be because JS / _ always use ===
 // for equality, complicating the use of unique tuples (i.e. [1,2] !== [1,2])
 const hasGroupAndType = (groupTypeColl, [group, type]) =>
-    groupTypeColl.find(([g, t]) => g === group && t === type);
+  groupTypeColl.find(([g, t]) => g === group && t === type);
 
 // This should just be
 //   Object.keys(_.groupBy(tasksToClear, ({group, task}) => [group, task]))
 // but JS only supports strings as keys
 const uniqueGroupTypeCombos = tasks => {
-    const unique = [];
-    tasks.forEach(t => {
-        if (!hasGroupAndType(unique, [t.group, t.type])) {
-            unique.push([t.group, t.type]);
-        }
-    });
-    return unique;
+  const unique = [];
+  tasks.forEach(t => {
+    if (!hasGroupAndType(unique, [t.group, t.type])) {
+      unique.push([t.group, t.type]);
+    }
+  });
+  return unique;
 };
 
 /*
@@ -48,180 +48,228 @@ const getScheduledTasksByType = (registration, type) => {
 
 // find the messages to clear
 const findToClear = (registration, reported_date, config) => {
-    // See: https://github.com/medic/medic-docs/blob/master/user/message-states.md#message-states-in-medic-webapp
-    // Both scheduled and pending have not yet been either seen by a gateway or
-    // delivered, so they are both clearable.
-    const typesToClear = ['pending', 'scheduled'];
+  // See: https://github.com/medic/medic-docs/blob/master/user/message-states.md#message-states-in-medic-webapp
+  // Both scheduled and pending have not yet been either seen by a gateway or
+  // delivered, so they are both clearable.
+  const typesToClear = ['pending', 'scheduled'];
 
-    const reportedDateMoment = moment(reported_date);
-    const taskTypes = config.silence_type.split(',').map(type => type.trim());
+  const reportedDateMoment = moment(reported_date);
+  const taskTypes = config.silence_type.split(',').map(type => type.trim());
 
-    const tasksUnderReview = getScheduledTasksByType(registration, taskTypes);
+  const tasksUnderReview = getScheduledTasksByType(registration, taskTypes);
 
-    if (!config.silence_for) {
-        // No range, all clearable tasks should be cleared
-        return tasksUnderReview.filter(task => typesToClear.includes(task.state));
-    } else {
-        // Clear all tasks that are members of a group that "exists" before the
-        // silenceUntil date. e.g., they have at least one task in their group
-        // whose due date is before silenceUntil.
-        const silenceUntil = reportedDateMoment.clone();
-        silenceUntil.add(date.getDuration(config.silence_for));
+  if (!config.silence_for) {
+    // No range, all clearable tasks should be cleared
+    return tasksUnderReview.filter(task => typesToClear.includes(task.state));
+  } else {
+    // Clear all tasks that are members of a group that "exists" before the
+    // silenceUntil date. e.g., they have at least one task in their group
+    // whose due date is before silenceUntil.
+    const silenceUntil = reportedDateMoment.clone();
+    silenceUntil.add(date.getDuration(config.silence_for));
 
-        const allTasksBeforeSilenceUntil = tasksUnderReview.filter(task =>
-            moment(task.due) <= silenceUntil);
-        const groupTypeCombosToClear = uniqueGroupTypeCombos(allTasksBeforeSilenceUntil);
+    const allTasksBeforeSilenceUntil = tasksUnderReview.filter(
+      task => moment(task.due) <= silenceUntil
+    );
+    const groupTypeCombosToClear = uniqueGroupTypeCombos(
+      allTasksBeforeSilenceUntil
+    );
 
-        return tasksUnderReview.filter(({group, type}) =>
-            hasGroupAndType(groupTypeCombosToClear, [group, type]));
-    }
+    return tasksUnderReview.filter(({ group, type }) =>
+      hasGroupAndType(groupTypeCombosToClear, [group, type])
+    );
+  }
 };
 
 const getConfig = function(form) {
-    const fullConfig = config.get('patient_reports') || [];
-    return _.findWhere(fullConfig, { form: form });
+  const fullConfig = config.get('patient_reports') || [];
+  return _.findWhere(fullConfig, { form: form });
 };
 
 const _silenceReminders = (registration, report, config, callback) => {
-    var toClear = module.exports._findToClear(registration, report.reported_date, config);
-    if (!toClear.length) {
-        return callback();
-    }
+  var toClear = module.exports._findToClear(
+    registration,
+    report.reported_date,
+    config
+  );
+  if (!toClear.length) {
+    return callback();
+  }
 
-    toClear.forEach(task => {
-        utils.setTaskState(task, 'cleared');
-        task.cleared_by = report._id;
-    });
-    db.audit.saveDoc(registration, callback);
+  toClear.forEach(task => {
+    utils.setTaskState(task, 'cleared');
+    task.cleared_by = report._id;
+  });
+  db.audit.saveDoc(registration, callback);
 };
 
 const addRegistrationToDoc = (doc, registrations) => {
-    if (registrations.length) {
-        const latest = _.max(
-            registrations,
-            registration => moment(registration.reported_date)
-        );
-        doc.registration_id = latest._id;
-    }
+  if (registrations.length) {
+    const latest = _.max(registrations, registration =>
+      moment(registration.reported_date)
+    );
+    doc.registration_id = latest._id;
+  }
 };
 
-const silenceRegistrations = (
-            config,
-            doc,
-            registrations,
-            callback) => {
-    if (!config.silence_type) {
-        return callback(null, true);
-    }
-    async.forEach(
-        registrations,
-        function(registration, callback) {
-            if (doc._id === registration._id) {
-                // don't silence the registration you're processing
-                return callback();
+const addMessageUUIDToDoc = (doc, registrations, patientId) => {
+  if (registrations.length) {
+    const messages = registrations
+      .map(registration => {
+        if (registration.scheduled_tasks) {
+          var tasks = registration.scheduled_tasks.map(task => {
+            if (task.messages) {
+              return {
+                due: task.due,
+                messages: task.messages.filter(function(message) {
+                  return message.message.indexOf(`V ${patientId}`) > -1;
+                }),
+              };
             }
-            module.exports._silenceReminders(registration, doc, config, callback);
-        },
-        function(err) {
-            callback(err, true);
+
+            return null;
+          });
+
+          tasks = tasks.filter(x => !!x);
+          if (tasks.length > 0) {
+            return tasks;
+          }
         }
-    );
+
+        return null;
+      })
+      .filter(x => !!x)
+      .reduce((acc, curr) => {
+        return acc.concat(curr);
+      }, [])
+      .reduce(
+        (acc, curr) => {
+          return moment(acc.due) > moment(curr.due) ? acc : curr;
+        },
+        { due: '1970-01-01T00:00:00.000Z', messages: [] }
+      ).messages;
+
+    if (messages.length > 0) {
+      doc.message_uuid = messages[messages.length - 1].uuid;
+    }
+  }
+};
+
+const silenceRegistrations = (config, doc, registrations, callback) => {
+  if (!config.silence_type) {
+    return callback(null, true);
+  }
+  async.forEach(
+    registrations,
+    function(registration, callback) {
+      if (doc._id === registration._id) {
+        // don't silence the registration you're processing
+        return callback();
+      }
+      module.exports._silenceReminders(registration, doc, config, callback);
+    },
+    function(err) {
+      callback(err, true);
+    }
+  );
 };
 
 const validate = (config, doc, callback) => {
-    var validations = config.validations && config.validations.list;
-    return validation.validate(doc, validations, callback);
+  var validations = config.validations && config.validations.list;
+  return validation.validate(doc, validations, callback);
 };
 
 // NB: this is very similar to a function in accept_patient_reports, except
 //     they also allow for an empty event_type
 const messageRelevant = (msg, doc) => {
-    if (msg.event_type === 'report_accepted') {
-        const expr = msg.bool_expr;
-        if (utils.isNonEmptyString(expr)) {
-            return utils.evalExpression(expr, {doc: doc});
-        } else {
-            return true;
-        }
+  if (msg.event_type === 'report_accepted') {
+    const expr = msg.bool_expr;
+    if (utils.isNonEmptyString(expr)) {
+      return utils.evalExpression(expr, { doc: doc });
+    } else {
+      return true;
     }
+  }
 };
 
 const addMessagesToDoc = (doc, config, registrations) => {
-    config.messages.forEach(msg => {
-        if (messageRelevant(msg, doc)) {
-            messages.addMessage(doc, msg, msg.recipient, {
-                patient: doc.patient,
-                registrations: registrations
-            });
-        }
-    });
+  config.messages.forEach(msg => {
+    if (messageRelevant(msg, doc)) {
+      messages.addMessage(doc, msg, msg.recipient, {
+        patient: doc.patient,
+        registrations: registrations,
+      });
+    }
+  });
 };
 
 const handleReport = (doc, config, callback) => {
-    utils.getRegistrations({
-        db: db,
-        id: doc.fields.patient_id
-    }, (err, registrations) => {
-        if (err) {
-            return callback(err);
-        }
+  utils.getRegistrations(
+    {
+      db: db,
+      id: doc.fields.patient_id,
+    },
+    (err, registrations) => {
+      if (err) {
+        return callback(err);
+      }
 
-        addMessagesToDoc(doc, config, registrations);
-        addRegistrationToDoc(doc, registrations);
+      addMessagesToDoc(doc, config, registrations);
+      addRegistrationToDoc(doc, registrations);
+      if (doc.form == 'V') {
+        addMessageUUIDToDoc(doc, registrations, doc.fields.patient_id);
+      }
 
-        module.exports.silenceRegistrations(
-            config,
-            doc,
-            registrations,
-            callback);
-    });
+      module.exports.silenceRegistrations(config, doc, registrations, callback);
+    }
+  );
 };
 
 module.exports = {
-    filter: function(doc, info={}) {
-        return Boolean(
-            doc &&
-            doc.type === 'data_record' &&
-            doc.form &&
-            doc.reported_date &&
-            !transitionUtils.hasRun(info, NAME) &&
-            _hasConfig(doc)
-        );
-    },
-    // also used by registrations transition.
-    silenceRegistrations: silenceRegistrations,
-    onMatch: change => {
-        const doc = change.doc;
+  filter: function(doc, info = {}) {
+    return Boolean(
+      doc &&
+        doc.type === 'data_record' &&
+        doc.form &&
+        doc.reported_date &&
+        !transitionUtils.hasRun(info, NAME) &&
+        _hasConfig(doc)
+    );
+  },
+  // also used by registrations transition.
+  silenceRegistrations: silenceRegistrations,
+  onMatch: change => {
+    const doc = change.doc;
 
-        const config = getConfig(doc.form);
+    const config = getConfig(doc.form);
 
-        if (!config) {
-            return Promise.resolve();
+    if (!config) {
+      return Promise.resolve();
+    }
+
+    return new Promise((resolve, reject) => {
+      validate(config, doc, function(errors) {
+        if (errors && errors.length > 0) {
+          messages.addErrors(config, doc, errors, { patient: doc.patient });
+          return resolve(true);
         }
 
-        return new Promise((resolve, reject) => {
-            validate(config, doc, function(errors) {
-                if (errors && errors.length > 0) {
-                    messages.addErrors(config, doc, errors, { patient: doc.patient });
-                    return resolve(true);
-                }
+        if (!doc.patient) {
+          transitionUtils.addRegistrationNotFoundError(doc, config);
+          return resolve(true);
+        }
 
-                if (!doc.patient) {
-                    transitionUtils.addRegistrationNotFoundError(doc, config);
-                    return resolve(true);
-                }
-
-                handleReport(doc, config, (err, changed) => {
-                    if (err) {
-                        return reject(err);
-                    }
-                    return resolve(changed);
-                });
-            });
+        handleReport(doc, config, (err, changed) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(changed);
         });
-    },
-    _silenceReminders: _silenceReminders,
-    _findToClear: findToClear,
-    _handleReport: handleReport,
-    _addMessageToDoc: addMessagesToDoc
+      });
+    });
+  },
+  _silenceReminders: _silenceReminders,
+  _findToClear: findToClear,
+  _handleReport: handleReport,
+  _addMessageToDoc: addMessagesToDoc,
 };
