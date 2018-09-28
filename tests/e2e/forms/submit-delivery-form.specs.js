b5857e38865f3733d298ad3a512cf736b8d907ad
Igor Protsenko <ig.protsenko@gmail.com>
2018-09-28 14:21:18 +0300

    Refactored existing Delivery form; added Add family and Family Survey form

--- tests/e2e/forms/submit-delivery-form.specs.js
+++ tests/e2e/forms/submit-delivery-form.specs.js
@@ -1,69 +1,24 @@
 const helper = require('../../helper'),
-      deliveryReport = require('../../page-objects/forms/delivery-report.po.js'),
-      common = require('../../page-objects/common/common.po.js'),
-      utils = require('../../utils');
+  deliveryReport = require('../../page-objects/forms/delivery-report.po.js'),
+  genericForm = require('../../page-objects/forms/generic-form.po'),
+  common = require('../../page-objects/common/common.po.js'),
+  utils = require('../../utils'),
+  userData = require('../../page-objects/forms/data/user.po.data');
 
 describe('Submit Delivery Report', () => {
-
-  const contactId = '3b3d50d275280d2568cd36281d00348b';
-  const docs = [
-    {
-      _id: 'c49385b3594af7025ef097114104ef48',
-      reported_date: 1469578114543,
-      notes: '',
-      contact: {
-        _id: contactId,
-        name: 'Jack',
-        date_of_birth: '',
-        phone: '+64274444444',
-        alternate_phone: '',
-        notes: '',
-        type: 'person',
-        reported_date: 1478469976421
-      },
-      name: 'Number three district',
-      external_id: '',
-      type: 'district_hospital'
-    },
-    {
-      _id: contactId,
-      name: 'Jack',
-      date_of_birth: '',
-      phone: '+64274444444',
-      alternate_phone: '',
-      notes: '',
-      type: 'person',
-      reported_date: 1478469976421,
-      parent: {
-        _id: 'c49385b3594af7025ef097114104ef48',
-        reported_date: 1469578114543,
-        notes: '',
-        contact: {
-          _id: contactId,
-          name: 'Jack',
-          date_of_birth: '',
-          phone: '+64274444444',
-          alternate_phone: '',
-          notes: '',
-          type: 'person',
-          reported_date: 1478469976421
-        },
-        name: 'Number three district',
-        external_id: '',
-        type: 'district_hospital'
-      }
-    }
-  ];
-
-  const noteToCHW = 'Good news, Jack! Jack () has delivered at the health facility. We will alert you when it is time to refer them for PNC. Please monitor them for danger signs. Thank you!';
+  const contactId = userData.contactId;
+  const docs = userData.docs;
+  const noteToCHW =
+    'Good news, Jack! Jack () has delivered at the health facility. We will alert you when it is time to refer them for PNC. Please monitor them for danger signs. Thank you!';
 
   beforeAll(done => {
-    protractor.promise.all(docs.map(utils.saveDoc))
+    protractor.promise
+      .all(docs.map(utils.saveDoc))
       .then(() => deliveryReport.configureForm(contactId, done))
       .catch(done.fail);
   });
 
-  afterEach(done =>{
+  afterEach(done => {
     utils.resetBrowser();
     done();
   });
@@ -72,28 +27,24 @@ describe('Submit Delivery Report', () => {
 
   it('open delivery form', () => {
     common.goToReports();
-
-    browser.sleep(1000); // let the refresh work here - #3691
-
-    deliveryReport.selectForm();
+    genericForm.selectForm();
     //select name
     deliveryReport.selectPatientName('jack');
-    deliveryReport.nextPage();
+    genericForm.nextPage();
     helper.waitElementToBeVisible(element(by.css('[value="healthy"]')));
     //Delivery info
     deliveryReport.selectLiveBirthButton();
     deliveryReport.selectFacilityButton();
     deliveryReport.enterDeliveryDate('');
-    deliveryReport.nextPage();
+    genericForm.nextPage();
     expect(deliveryReport.getNoteToCHW()).toBe(noteToCHW);
-    deliveryReport.nextPage();
+    genericForm.nextPage();
     //summary page
     expect(deliveryReport.getOutcomeText()).toBe('Live Birth');
-    expect(deliveryReport.getDeliveryLocationSummaryText())
-      .toBe('Facility');
+    expect(deliveryReport.getDeliveryLocationSummaryText()).toBe('Facility');
     expect(deliveryReport.getFollowUpMessage()).toBe(noteToCHW);
     //submit
-    deliveryReport.submit();
+    genericForm.submit();
     expect(element(by.css('div.details')).isPresent()).toBeTruthy();
   });
 });
