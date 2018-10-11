69082c242210d18d94d3fe2e54b3189e4d9f6f8e
Willy Tekeu <willy.tekeu@gmail.com>
2018-10-11 16:53:10 -0400

    code cleanup

--- admin/src/js/controllers/authorization-permissions.js
+++ admin/src/js/controllers/authorization-permissions.js
@@ -28,7 +28,7 @@ angular.module('controllers').controller('AuthorizationPermissionsCtrl',
 
         // add the configured permissions
         $scope.permissions = [];
-        Object.keys(settings.permissions).map(function(key, index) {
+        Object.keys(settings.permissions).map(function(key) {
           $scope.permissions.push({
             name: key,
             roles: makeRoleModel(settings.permissions[key]),
