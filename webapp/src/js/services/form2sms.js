4b15dbed38eb41b4388d3c5c595952cc26544e93
alxndrsn <git@alxndrsn.com>
2018-10-19 15:55:20 +0000

    Define simple format for smsing a form

--- webapp/src/js/services/form2sms.js
+++ webapp/src/js/services/form2sms.js
@@ -2,6 +2,7 @@ angular
   .module('inboxServices')
   .service('Form2Sms', function(
     $log,
+    $parse,
     DB
   ) {
     'use strict';
@@ -13,10 +14,44 @@ angular
         .get('form:' + doc.form)
         .then(function(form) {
           $log.error('Form2Sms', doc, form);
-          return 'utternonsenseutternonsenseutternonsenseutternonsenseutternonsenseutternonsenseutternonsense';
+
+          //#if DEBUG
+          // here we try out some different ways we could define the converter
+          // via the project configuration.
+          switch(doc.form) {
+            case 'patient_assessment':
+              form.xml2sms = 'spaced("PA", text("patient_name"), text("patient_id"), bitfield("accompany_to_cscom", "fast_breathing", "needs_signoff", "refer_to_cscom"))';
+              break;
+            case 'patient_assessment_over_5':
+              form.xml2sms = '"what"';
+              break;
+          }
+          //#endif DEBUG
+
+          if(!form.xml2sms) return;
+
+          return $parse(form.xml2sms)({ bitfield:bitfield.bind(doc), doc:doc, spaced:spaced, text:text.bind(doc) });
         })
         .catch(function(err) {
           $log.error('Form2Sms failed: ' + err);
         });
     };
   });
+
+function spaced() {
+  return Array.prototype.slice.call(arguments).join(' ');
+}
+
+function bitfield() {
+  var that = this;
+  var fieldNames = Array.prototype.slice.call(arguments);
+  var intVal = fieldNames.reduce(function(acc, fieldName) {
+    var bool = that.fields[fieldName] === 'true';
+    return (acc << 1) | (bool ? 1 : 0);
+  }, 0);
+  return intVal.toString(16);
+}
+
+function text(fieldName) {
+  return (this.fields[fieldName] || fieldName + '.notfound').toString();
+}
