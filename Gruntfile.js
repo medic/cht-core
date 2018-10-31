7db6a52fc1788d8f1df182c7b71aa4b47bd0e005
Nick Christenson <emailfornc@gmail.com>
2018-10-31 11:44:22 -0400

    zipping the forms folder and attaching to medic ddoc, copying required standard files over earlier so the couch:compile picks those up as well, gettting the forms zip from medic attachments unzipping to tmp dir, then uploading the standard forms

--- Gruntfile.js
+++ Gruntfile.js
@@ -824,6 +824,13 @@ module.exports = function(grunt) {
       'build/ddocs/medic-admin/_attachments/js/templates.js':
         'build/ddocs/medic-admin/_attachments/js/templates.js',
     },
+    zip: {
+      standardForms: {
+        cwd: 'config/standard/forms',
+        src: 'config/standard/forms/**/*',
+        dest: 'build/ddocs/medic/_attachments/forms.zip'
+      }
+    }
   });
 
   // Build tasks
@@ -865,6 +872,8 @@ module.exports = function(grunt) {
   grunt.registerTask('build-dev', 'Build the static resources', [
     'exec:clean-build-dir',
     'copy:ddocs',
+    'zip:standardForms',
+    'copy:standard',
     'mmcss',
     'mmjs',
     'enketo-xslt',
@@ -882,7 +891,6 @@ module.exports = function(grunt) {
   grunt.registerTask('build-ddoc', 'Build the main ddoc', [
     'couch-compile:secondary',
     'copy:ddoc-attachments',
-    'copy:standard',
     'appcache',
     'couch-compile:primary',
   ]);
