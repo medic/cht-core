53f18d95ce17eb5c21b556c2da2ef98437bbab74
Willy Tekeu <willy.tekeu@gmail.com>
2018-11-01 14:57:26 -0400

    Alert user when outgoing message size is greater than config limit

---
+++ webapp/src/js/directives/contenteditable.js
@@ -0,0 +1,28 @@
+/**
+ * Make a div element editable in place using the HTML5 contenteditable attribute
+ *
+ * Usage:
+ * <div contenteditable ng-model="..."
+ */
+angular.module('inboxDirectives').directive("contenteditable", function() {
+  'use strict';
+  return {
+    restrict: "A",
+    require: "ngModel",
+    link: function(scope, element, attrs, ngModel) {
+
+      function read() {
+        var html = element.html();
+        ngModel.$setViewValue(angular.element('<div>' + html + '</div>').text());
+      }
+
+      ngModel.$render = function() {
+        element.html(ngModel.$viewValue || "");
+      };
+
+      element.bind("blur keyup change", function() {
+        scope.$apply(read);
+      });
+    }
+  };
+});
