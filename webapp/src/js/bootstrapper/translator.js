dc30011cae3b4b60ed909e3542118925c0327995
Kenn Sippell <kennsippell@gmail.com>
2018-11-02 20:57:14 +0300

    French and Spanish translations

--- webapp/src/js/bootstrapper/translator.js
+++ webapp/src/js/bootstrapper/translator.js
@@ -4,7 +4,9 @@ var eurodigit = require('eurodigit');
 
 var TRANSLATIONS = {
   en: {
-    FETCH_INFO: function(x) { return 'Fetching info (' + x + ' docs)…'; },
+    FETCH_INFO: function(x) {
+      return 'Fetching info (' + x + ' docs)…';
+    },
     LOAD_APP: 'Loading app…',
     LOAD_RULES: 'Loading rules…',
     STARTING_APP: 'Starting app…',
@@ -12,34 +14,47 @@ var TRANSLATIONS = {
     TRY_AGAIN: 'Try again',
   },
   es: {
-    FETCH_INFO: function(x) { return 'Descargando datos (' + x + ' archivos)…'; },
+    FETCH_INFO: function(x) {
+      return 'Descargando datos (' + x + ' archivos)…';
+    },
     LOAD_APP: 'Cargando aplicación…',
     LOAD_RULES: 'Cargando reglas…',
+    STARTING_APP: 'Aplicación de inicio…',
     ERROR_MESSAGE: 'Error al cargar, por favor comprueba la conexión',
     TRY_AGAIN: 'Volver a intentar',
   },
   sw: {
-    FETCH_INFO: function(x) { return 'Inakusanya jumbe (jumbe ' + x + ')…'; },
+    FETCH_INFO: function(x) {
+      return 'Inakusanya jumbe (jumbe ' + x + ')…';
+    },
     LOAD_APP: 'Inapakia programu…',
     LOAD_RULES: 'Inapakia kanuni…',
-    ERROR_MESSAGE: 'Kuna hitilafu katika kupakia, tafadhali hakikisha uko kwenye mtandao',
+    ERROR_MESSAGE:
+      'Kuna hitilafu katika kupakia, tafadhali hakikisha uko kwenye mtandao',
     TRY_AGAIN: 'Jaribu tena',
   },
   ne: {
-    FETCH_INFO: function(x) { return eurodigit.to_non_euro.devanagari('जानकारी प्राप्त गरिंदै छ (' + x + ' दस्तावेज़)…'); },
+    FETCH_INFO: function(x) {
+      return eurodigit.to_non_euro.devanagari(
+        'जानकारी प्राप्त गरिंदै छ (' + x + ' दस्तावेज़)…'
+      );
+    },
     LOAD_APP: 'एप लोड गर्दै…',
     LOAD_RULES: 'नियमहरू लोड गर्दै…',
     STARTING_APP: 'एप सुरु हुँदैछ…',
     ERROR_MESSAGE: 'लोडिंग त्रुटि, कृपया आफ्नो ईन्टरनेट कनेक्सन जाँच गर्नुहोस्',
     TRY_AGAIN: 'पुन: प्रयास गर्नुहोस्',
-  }, 
+  },
   fr: {
-    FETCH_INFO: function(x) { return 'Récupération des infos (' + x + ' docs)...'; },
+    FETCH_INFO: function(x) {
+      return 'Récupération des infos (' + x + ' docs)...';
+    },
     LOAD_APP: 'Chargement de l’application…',
     LOAD_RULES: 'Chargement des paramètres…',
+    STARTING_APP: 'App de démarrage…',
     ERROR_MESSAGE: 'Erreur de chargement, veuillez vérifier votre connexion',
     TRY_AGAIN: 'Réessayer',
-  }
+  },
 };
 
 var locale;
@@ -51,11 +66,11 @@ module.exports = {
       (locale && translationData[locale] && translationData[locale][key]) ||
       translationData.en[key] ||
       'bootstrap.translator.' + (key || 'undefined');
-  
+
     if (typeof lookup === 'function') {
       lookup = lookup(arg) || 'bootstrap.translator.error';
     }
-  
+
     return lookup;
   },
 
