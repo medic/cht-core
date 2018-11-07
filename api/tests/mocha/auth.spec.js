dfb89182b191c20546c1506a9ec8f643d2075abf
Victor Gomez <35075044+vimemo@users.noreply.github.com>
2018-11-07 14:17:33 -0700

    Sets 'x-medic-user' header for api requests.
    #3391

--- api/tests/mocha/auth.spec.js
+++ api/tests/mocha/auth.spec.js
@@ -73,7 +73,7 @@ describe('Auth', () => {
       const district = '123';
       const userCtx = { userCtx: { name: 'steve', roles: [ '_admin' ] } };
       const get = sinon.stub(request, 'get').callsArgWith(1, null, null, userCtx);
-      return auth.check({ }, 'can_edit', district).then(ctx => {
+      return auth.check({headers: []}, 'can_edit', district).then(ctx => {
         chai.expect(get.callCount).to.equal(1);
         chai.expect(ctx.user).to.equal('steve');
         chai.expect(ctx.district).to.equal(undefined);
@@ -87,8 +87,8 @@ describe('Auth', () => {
       const get = sinon.stub(request, 'get');
       get.onFirstCall().callsArgWith(1, null, null, userCtx);
       get.onSecondCall().callsArgWith(1, null, null, { facility_id: district });
-      sinon.stub(config, 'get').returns({ can_edit: ['district_admin'] });
-      return auth.check({}, 'can_edit', district).then(ctx => {
+      sinon.stub(config, 'get').returns([ { name: 'can_edit', roles: [ 'district_admin' ] } ]);
+      return auth.check({headers: []}, 'can_edit', district).then(ctx => {
         chai.expect(get.callCount).to.equal(2);
         chai.expect(ctx.user).to.equal('steve');
         chai.expect(ctx.district).to.equal(district);
@@ -102,8 +102,8 @@ describe('Auth', () => {
       const get = sinon.stub(request, 'get');
       get.onFirstCall().callsArgWith(1, null, null, userCtx);
       get.onSecondCall().callsArgWith(1, null, null, { facility_id: '123' });
-      sinon.stub(config, 'get').returns({ can_edit: ['district_admin'] });
-      return auth.check({}, 'can_edit', '789').catch(err => {
+      sinon.stub(config, 'get').returns([ { name: 'can_edit', roles: [ 'district_admin' ] } ]);
+      return auth.check({headers: []}, 'can_edit', '789').catch(err => {
         chai.expect(get.callCount).to.equal(2);
         chai.expect(err.message).to.equal('Insufficient privileges');
         chai.expect(err.code).to.equal(403);
@@ -123,7 +123,7 @@ describe('Auth', () => {
         can_export_contacts: ['district_admin'],
       });
       return auth
-        .check({}, ['can_export_messages', 'can_export_contacts'], district)
+        .check({headers: []}, ['can_export_messages', 'can_export_contacts'], district)
         .then(ctx => {
           chai.expect(get.callCount).to.equal(2);
           chai.expect(ctx.user).to.equal('steve');
@@ -134,9 +134,7 @@ describe('Auth', () => {
     it('checks all required roles', () => {
       db.serverUrl = 'http://abc.com';
       const district = '123';
-      const userCtx = {
-        userCtx: { name: 'steve', roles: ['xyz', 'district_admin'] },
-      };
+      const userCtx = { userCtx: { name: 'steve', roles: [ 'xyz', 'district_admin' ] } };
       sinon.stub(url, 'format').returns('http://abc.com');
       const get = sinon
         .stub(request, 'get')
@@ -146,7 +144,7 @@ describe('Auth', () => {
         can_export_server_logs: ['national_admin'],
       });
       return auth
-        .check({}, ['can_export_messages', 'can_export_server_logs'], district)
+        .check({headers: []}, ['can_export_messages', 'can_export_server_logs'], district)
         .catch(err => {
           chai.expect(get.callCount).to.equal(1);
           chai.expect(err.message).to.equal('Insufficient privileges');
@@ -213,7 +211,6 @@ describe('Auth', () => {
         chai.expect(db.medic.get.callCount).to.equal(1);
         chai.expect(db.medic.get.withArgs('org.couchdb.user:steve').callCount).to.equal(1);
       });
-
     });
 
     it('returns name and roles from provided userCtx', () => {
