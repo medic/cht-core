babede1522c9a503e18f20355e92eddc015e96d6
Willy Tekeu <willy.tekeu@gmail.com>
2018-11-12 14:57:47 -0500

    Improve error handling

--- api/src/migrations/convert-translation-messages.js
+++ api/src/migrations/convert-translation-messages.js
@@ -22,36 +22,40 @@ module.exports = {
   name: 'convert-translation-messages',
   created: new Date(2018, 11, 8),
   run: promisify(async (callback) => {
-    const ddoc = await db.medic.get(DDOC_ID);
-    const translationAttachmentKeys = Object.keys(ddoc._attachments).filter(k => k.includes('translations'));
-    translationAttachmentKeys.forEach((translationAttachmentKey) => {
+    try {
+      const ddoc = await db.medic.get(DDOC_ID);
+      const translationAttachmentKeys = Object.keys(ddoc._attachments).filter(k => k.includes('translations'));
+      translationAttachmentKeys.forEach((translationAttachmentKey) => {
 
-      const translationMessageKey = translationAttachmentKey.match(/translations\/(.+).properties/)[1];
-      const translationMessageKeys = [translationMessageKey, translationMessageKey + '-backup'];
+        const translationMessageKey = translationAttachmentKey.match(/translations\/(.+).properties/)[1];
+        const translationMessageKeys = [translationMessageKey, translationMessageKey + '-backup'];
 
-      translationMessageKeys.forEach(async (translationMessageKey) => {
-        const translationMessageValues = await db.medic.get(translationMessageKey);
-        if (_.has(translationMessageValues, 'values')) {
-          const translationAttachmentValues = await getAttachment(translationAttachmentKey);
+        translationMessageKeys.forEach(async (translationMessageKey) => {
+          const translationMessageValues = await db.medic.get(translationMessageKey);
+          if (_.has(translationMessageValues, 'values')) {
+            const translationAttachmentValues = await getAttachment(translationAttachmentKey);
 
-          let defaultTranslations = {};
-          let customTranslations = {};
-          Object.keys(translationMessageValues.values).forEach((translationMessageKey) => {
-            if (_.has(translationAttachmentValues, translationMessageKey)) {
-              defaultTranslations[translationMessageKey] = translationMessageValues.values[translationMessageKey];
-            } else {
-              customTranslations[translationMessageKey] = translationMessageValues.values[translationMessageKey];
-            }
-          });
+            let defaultTranslations = {};
+            let customTranslations = {};
+            Object.keys(translationMessageValues.values).forEach((translationMessageKey) => {
+              if (_.has(translationAttachmentValues, translationMessageKey)) {
+                defaultTranslations[translationMessageKey] = translationMessageValues.values[translationMessageKey];
+              } else {
+                customTranslations[translationMessageKey] = translationMessageValues.values[translationMessageKey];
+              }
+            });
 
-          delete translationMessageValues.values;
-          translationMessageValues.custom = customTranslations;
-          translationMessageValues.default = defaultTranslations;
+            delete translationMessageValues.values;
+            translationMessageValues.custom = customTranslations;
+            translationMessageValues.default = defaultTranslations;
 
-          await db.medic.put(translationMessageValues);
-        }
+            await db.medic.put(translationMessageValues);
+          }
+        });
       });
-    });
-    callback();
+      callback();
+    } catch (e) {
+      callback(e);
+    }
   })
 };
