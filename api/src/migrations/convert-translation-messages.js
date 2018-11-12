7f932636f0fb55b7ece4138962e15eb260fd2357
Willy Tekeu <willy.tekeu@gmail.com>
2018-11-12 14:44:56 -0500

    Convert migration from nano to PouchDB

--- api/src/migrations/convert-translation-messages.js
+++ api/src/migrations/convert-translation-messages.js
@@ -1,82 +1,57 @@
 const _ = require('underscore'),
   { promisify } = require('util'),
-  db = require('../db-nano'),
-  properties = require('properties');
+  db = require('../db-pouch'),
+  properties = require('properties'),
+  DDOC_ID = '_design/medic';
+
+const getAttachment = name => {
+  return db.medic.getAttachment(DDOC_ID, name)
+    .then(attachment => {
+      return new Promise((resolve, reject) => {
+        properties.parse(attachment.toString('utf8'), (err, values) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(values);
+        });
+      });
+    });
+};
 
 module.exports = {
   name: 'convert-translation-messages',
   created: new Date(2018, 11, 8),
-  run: promisify((callback) => {
-	  db.request({
-      db: db.settings.db,
-      method: 'GET',
-      path: '_design/medic',
-    },
-    (err, result) => {
-      if (err) {
-        return callback(err);
-      }
+  run: promisify(async (callback) => {
+    const ddoc = await db.medic.get(DDOC_ID);
+    const translationAttachmentKeys = Object.keys(ddoc._attachments).filter(k => k.includes('translations'));
+    translationAttachmentKeys.forEach((translationAttachmentKey) => {
 
-      let translationAttachmentKeys = Object.keys(result._attachments).filter(k => k.includes('translations'));
-      translationAttachmentKeys.forEach((translationAttachmentKey) => {
-        let translationMessageKey = translationAttachmentKey.match(/translations\/(.+).properties/)[1];
-        let translationMessageKeys = [translationMessageKey, translationMessageKey + '-backup'];
-        translationMessageKeys.forEach((translationMessageKey) => {
-          db.request({
-            db: db.settings.db,
-            method: 'GET',
-            path: translationMessageKey,
-          },
-          (err, translationMessageValues) => {
-            if (_.has(translationMessageValues, 'values')) {
-              db.request({
-                db: db.settings.db,
-                method: 'GET',
-                path: '_design/medic/' + translationAttachmentKey,
-              },
-              (err, translationAttachmentValue) => {
-                if (err) {
-                  return callback(err);
-                }
+      const translationMessageKey = translationAttachmentKey.match(/translations\/(.+).properties/)[1];
+      const translationMessageKeys = [translationMessageKey, translationMessageKey + '-backup'];
 
-                properties.parse(translationAttachmentValue.toString('utf8'), (err, translationAttachmentValues) => {
-                  if (err) {
-                    return callback(err);
-                  }
+      translationMessageKeys.forEach(async (translationMessageKey) => {
+        const translationMessageValues = await db.medic.get(translationMessageKey);
+        if (_.has(translationMessageValues, 'values')) {
+          const translationAttachmentValues = await getAttachment(translationAttachmentKey);
 
-                  let defaultTranslations = {};
-                  let customTranslations = {};
-                  Object.keys(translationMessageValues.values).forEach((translationMessageKey) => {
-                    if (_.has(translationAttachmentValues, translationMessageKey)) {
-                      defaultTranslations[translationMessageKey] = translationMessageValues.values[translationMessageKey];
-                    } else {
-                      customTranslations[translationMessageKey] = translationMessageValues.values[translationMessageKey];
-                    }
-                  });
-
-                  delete translationMessageValues.values;
-                  translationMessageValues.custom = customTranslations;
-                  translationMessageValues.default = defaultTranslations;
-
-                  db.request({
-                    db: db.settings.db,
-                    method: 'PUT',
-                    path: translationMessageKey,
-                    body: translationMessageValues
-                  },
-                  (err) => {
-                    if (err) {
-                      return callback(err);
-                    }
-                  });
-                });
-              });
+          let defaultTranslations = {};
+          let customTranslations = {};
+          Object.keys(translationMessageValues.values).forEach((translationMessageKey) => {
+            if (_.has(translationAttachmentValues, translationMessageKey)) {
+              defaultTranslations[translationMessageKey] = translationMessageValues.values[translationMessageKey];
+            } else {
+              customTranslations[translationMessageKey] = translationMessageValues.values[translationMessageKey];
             }
           });
-        });
-      });
 
-      callback();
+          delete translationMessageValues.values;
+          translationMessageValues.custom = customTranslations;
+          translationMessageValues.default = defaultTranslations;
+
+          await db.medic.put(translationMessageValues);
+        }
+      });
     });
+    callback();
   })
 };
