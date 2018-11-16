af588a37558413a1de8a8caad6e36146b04f903e
alxndrsn <git@alxndrsn.com>
2018-11-16 11:40:46 +0000

    Load visual elements of contact-content pane incrementally

--- webapp/src/js/services/contact-view-model-generator.js
+++ webapp/src/js/services/contact-view-model-generator.js
@@ -72,19 +72,16 @@ angular.module('inboxServices').factory('ContactViewModelGenerator',
       model.isPrimaryContact = parent &&
         parent.contact &&
         (parent.contact._id === model.doc._id);
-      return model;
     };
 
     var setSchemaFields = function(model) {
       var schema = ContactSchema.get(model.doc.type);
       model.icon = schema.icon;
       model.label = schema.label;
-      return model;
     };
 
     var setMutedState = function(model) {
       model.doc.muted = ContactMuted(model.doc, model.lineage);
-      return model;
     };
 
     var splitContactsByType = function(children) {
@@ -112,7 +109,7 @@ angular.module('inboxServices').factory('ContactViewModelGenerator',
       });
     };
 
-    var sortPrimaryContactToTop = function(model) {
+    var sortPrimaryContactToTop = function(model, children) {
       return getPrimaryContact(model.doc)
         .then(function(primaryContact) {
           if (!primaryContact) {
@@ -123,11 +120,11 @@ angular.module('inboxServices').factory('ContactViewModelGenerator',
             doc: primaryContact,
             isPrimaryContact: true
           };
-          if (!model.children.persons) {
-            model.children.persons = [ newChild ];
+          if (!children.persons) {
+            children.persons = [ newChild ];
             return;
           }
-          var persons = model.children.persons;
+          var persons = children.persons;
           // remove existing child
           var primaryContactIdx = _.findIndex(persons, function(child) {
             return child.doc._id === primaryContact._id;
@@ -139,19 +136,19 @@ angular.module('inboxServices').factory('ContactViewModelGenerator',
           persons.unshift(newChild);
         })
         .then(function() {
-          return model;
+          return children;
         });
     };
 
-    var sortChildren = function(model) {
-      if (model.children.places) {
-        model.children.places.sort(NAME_COMPARATOR);
+    var sortChildren = function(model, children) {
+      if (children.places) {
+        children.places.sort(NAME_COMPARATOR);
       }
-      if (model.children.persons) {
+      if (children.persons) {
         var personComparator = model.doc.type === 'clinic' ? AGE_COMPARATOR : NAME_COMPARATOR;
-        model.children.persons.sort(personComparator);
+        children.persons.sort(personComparator);
       }
-      return sortPrimaryContactToTop(model);
+      return sortPrimaryContactToTop(model, children);
     };
 
     var getChildren = function(contactId) {
@@ -182,10 +179,10 @@ angular.module('inboxServices').factory('ContactViewModelGenerator',
         });
     };
 
-    var setChildren = function(model) {
+    var loadChildren = function(model) {
       model.children = {};
       if (model.doc.type === 'person') {
-        return model;
+        return $q.resolve({});
       }
       return getChildren(model.doc._id)
         .then(splitContactsByType)
@@ -195,8 +192,7 @@ angular.module('inboxServices').factory('ContactViewModelGenerator',
             children.childPlacesLabel = childPlacesSchema.pluralLabel;
             children.childPlacesIcon = childPlacesSchema.icon;
           }
-          model.children = children;
-          return sortChildren(model);
+          return sortChildren(model, children);
         });
     };
 
@@ -258,7 +254,7 @@ angular.module('inboxServices').factory('ContactViewModelGenerator',
         });
     };
 
-    var setReports = function(model) {
+    var loadReports = function(model) {
       var contacts = [ model.doc ];
       [ 'persons', 'deceased' ].forEach(function(type) {
         if (model.children[type]) {
@@ -272,18 +268,43 @@ angular.module('inboxServices').factory('ContactViewModelGenerator',
         .then(function(reports) {
           addPatientName(reports, contacts);
           reports.sort(REPORTED_DATE_COMPARATOR);
-          model.reports = reports;
-          return model;
+          return reports;
         });
     };
 
     return function(id, options) {
       return LineageModelGenerator.contact(id, options)
-        .then(setChildren)
-        .then(setReports)
-        .then(setPrimaryContact)
-        .then(setSchemaFields)
-        .then(setMutedState);
+        .then(function(model) {
+          setPrimaryContact(model);
+          setSchemaFields(model);
+          setMutedState(model);
+
+          model.loadingChildren = true;
+          model.loadingReports = true;
+
+          model.reportLoader = new Promise(function(resolve, reject) {
+            loadChildren(model)
+              .then(function(children) {
+                model.children = children;
+                model.loadingChildren = false;
+                return loadReports(model)
+                  .then(function(reports) {
+                    resolve(reports);
+                    return reports;
+                  });
+              })
+              .then(function(reports) {
+                model.reports = reports;
+                model.loadingReports = false;
+              })
+              .catch(function(err) {
+                reject(err);
+                throw err;
+              });
+          });
+
+          return model;
+        });
     };
   }
 );
