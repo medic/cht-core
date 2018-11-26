72f468b54508e77bdf743d34933912bda69b87ad
peter-ochieng <peteroochieng@gmail.com>
2018-11-26 08:01:24 +0300

    Commit reversal

--- webapp/src/js/controllers/inbox.js
+++ webapp/src/js/controllers/inbox.js
@@ -224,13 +224,16 @@ var feedback = require('../modules/feedback'),
       if(!$scope.enketoStatus.edited){
         return;
       }
+      if(!fromState.url.includes('edit')){
+        return;
+      }
       if(fromParams.id === toParams.id){
         return;
       }
       if(fromParams.reportId === toParams.id){
         return;
       }
-      if($scope.cancelCallback) {
+      if ($scope.cancelCallback) {
         event.preventDefault();
         $scope.navigationCancel();
       }
