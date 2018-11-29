d3f6661698ef3d58138e9c4ada7908343cfe6294
Victor Gomez <victor@medicmobile.org>
2018-11-29 09:49:31 -0700

    Review updates

--- shared-libs/translation-utils/src/translation-utils.js
+++ shared-libs/translation-utils/src/translation-utils.js
@@ -1,15 +1,9 @@
+const KEY_REGEX = /\${([^}\r\n]*)}/g;
+
 module.exports = {
-  loadTranslations: function(values) {
-    Object.keys(values).forEach(function(key) {
-      var value = values[key];
-      if(value.indexOf('${') > -1) {
-        var ids = value.match(/\${[^}\r\n]*}/g);
-        ids.forEach(function(id) {
-          var idKey = id.replace(/[${}]/g, '');
-          value = value.replace(id, values[idKey] || id);
-        });
-        values[key] = value;
-      }
+  loadTranslations: values => {
+    Object.keys(values).forEach(key => {
+      values[key] = values[key].replace(KEY_REGEX, (match, key) => values[key] || ('${' + key + '}'));
     });
     return values;
   }
