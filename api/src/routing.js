e5e15502d0f4c30374574672aeba9c2106a19f98
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2018-12-03 10:49:24 +1300

    Fix comment typo

--- api/src/routing.js
+++ api/src/routing.js
@@ -153,11 +153,11 @@ app.get('/', function(req, res) {
 });
 
 app.get('/favicon.ico', (req, res) => {
-  // Cache for a week.  Normally we don't interferse with couch headers, but
+  // Cache for a week. Normally we don't interfere with couch headers, but
   // due to Chrome (including Android WebView) aggressively requesting
   // favicons on every page change and window.history update
-  // (https://github.com/medic/medic-webapp/issues/1913 ), we have to stage an
-  // intervention:
+  // ( https://github.com/medic/medic-webapp/issues/1913 ), we have to
+  // stage an intervention
   writeHeaders(req, res, [['Cache-Control', 'public, max-age=604800']]);
   db.medic.get('branding').then(doc => {
     db.medic.getAttachment(doc._id, doc.resources.favicon).then(blob => {
