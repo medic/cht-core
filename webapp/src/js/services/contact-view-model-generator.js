296f9e1809807ff93302a0356c2582a1ce9c2a8c
alxndrsn <git@alxndrsn.com>
2018-12-11 10:56:51 +0000

    Simplify reportLoader

--- webapp/src/js/services/contact-view-model-generator.js
+++ webapp/src/js/services/contact-view-model-generator.js
@@ -282,26 +282,16 @@ angular.module('inboxServices').factory('ContactViewModelGenerator',
           model.loadingChildren = true;
           model.loadingReports = true;
 
-          model.reportLoader = new Promise(function(resolve, reject) {
-            loadChildren(model)
-              .then(function(children) {
-                model.children = children;
-                model.loadingChildren = false;
-                return loadReports(model)
-                  .then(function(reports) {
-                    resolve(reports);
-                    return reports;
-                  });
-              })
-              .then(function(reports) {
-                model.reports = reports;
-                model.loadingReports = false;
-              })
-              .catch(function(err) {
-                reject(err);
-                throw err;
-              });
-          });
+          model.reportLoader = loadChildren(model)
+            .then(children => {
+              model.children = children;
+              model.loadingChildren = false;
+              return loadReports(model);
+            })
+            .then(reports => {
+              model.reports = reports;
+              model.loadingReports = false;
+            });
 
           return model;
         });
