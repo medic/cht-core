d32f78436c6dea4375d55f21c3c620ee372f7ca1
Willy Tekeu <willy.tekeu@gmail.com>
2018-12-13 15:24:13 -0500

    Fix loading of translations
    The value may be a number which will cause the call to replace to fail.

--- shared-libs/translation-utils/src/translation-utils.js
+++ shared-libs/translation-utils/src/translation-utils.js
@@ -3,7 +3,7 @@ const KEY_REGEX = /\${([^}\r\n]*)}/g;
 module.exports = {
   loadTranslations: values => {
     Object.keys(values).forEach(key => {
-      values[key] = values[key].replace(KEY_REGEX, (match, key) => values[key] || ('${' + key + '}'));
+      values[key] = String(values[key]).replace(KEY_REGEX, (match, key) => values[key] || ('${' + key + '}'));
     });
     return values;
   }
