16b033018bb2fe72e5b076d6f7ae12898b791cc2
George Otieno <georgeorti@gmail.com>
2018-12-18 18:12:26 +0300

    fix e2e

--- tests/base.conf.js
+++ tests/base.conf.js
@@ -68,7 +68,7 @@ const login = browser => {
   browser.driver.findElement(by.name('password')).sendKeys(auth.pass);
   browser.driver.findElement(by.id('login')).click();
   // Login takes some time, so wait until it's done.
-  const bootstrappedCheck = () => element(by.css('body.bootstrapped')).isPresent();
+  const bootstrappedCheck = () => element(by.css('.navbar-header')).isPresent();
   return browser.driver.wait(bootstrappedCheck, 20 * 1000, 'Login should be complete within 20 seconds');
 };
 
