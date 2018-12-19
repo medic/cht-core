03e5b7b5b0aaf9a759c59087dbcbfa4009f7437f
Diana Barsan <35681649+dianabarsan@users.noreply.github.com>
2018-12-19 10:13:13 +0200

    Don't write deploy_info if undefined (#5118)
    When doing a non-Horticulturalist deploy, medic ddoc lacks the deploy_info property which horti adds.
    As a result, ddoc extraction would update medic-client every time it ran (because undefined properties don't get saved in CouchDB).
    Fix: only copy deploy_info if either old or new value exist.
    
    This would not occur in production deployments which are handled by Horticulturalist.
    
    #5109

--- api/src/ddoc-extraction.js
+++ api/src/ddoc-extraction.js
@@ -52,7 +52,7 @@ const isUpdated = (newDdoc, deploy_info) => {
       newDdoc._rev = oldDdoc && oldDdoc._rev;
 
       // update the deploy info in the medic-client ddoc
-      if (newDdoc._id === CLIENT_DDOC_ID) {
+      if (newDdoc._id === CLIENT_DDOC_ID && (deploy_info || newDdoc.deploy_info)) {
         newDdoc.deploy_info = deploy_info;
       }
 
