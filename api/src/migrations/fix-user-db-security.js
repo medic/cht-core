e3b5b5df7ae0486baf21a2d85481c14e4e0ab861
Stefan du Fresne <stefan@medicmobile.org>
2018-12-19 18:28:41 +0000

    Thanks dad

--- api/src/migrations/fix-user-db-security.js
+++ api/src/migrations/fix-user-db-security.js
@@ -23,7 +23,7 @@ module.exports = {
                 resolve();
               });
             });
-          })
+          });
         }, Promise.resolve());
       })
       .then(() => callback())
