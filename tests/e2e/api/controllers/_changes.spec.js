512ef7dde8246352c9ad5146551be016953adb47
Diana Barsan <35681649+dianabarsan@users.noreply.github.com>
2018-12-19 22:13:34 +0200

    Allows limiting of filtered changes requests
    When CouchDB version is gte 2.3.0, uses the limit param when requesting _changes, to allow a batched filtered replication.
    
    #5076

--- tests/e2e/api/controllers/_changes.spec.js
+++ tests/e2e/api/controllers/_changes.spec.js
@@ -4,7 +4,8 @@ const _ = require('underscore'),
       http = require('http'),
       querystring = require('querystring'),
       constants = require('../../../constants'),
-      auth = require('../../../auth')();
+      auth = require('../../../auth')(),
+      semver = require('semver');
 
 const DEFAULT_EXPECTED = [
   'appcache',
@@ -28,7 +29,7 @@ function assertChangeIds(changes) {
   });
 
   var expectedIds = Array.prototype.slice.call(arguments, 1);
-  expect(_.pluck(changes, 'id').sort()).toEqual(expectedIds.sort());
+  expect(_.unique(_.pluck(changes, 'id')).sort()).toEqual(expectedIds.sort());
 }
 
 function requestChanges(username, params = {}) {
@@ -175,6 +176,18 @@ const consumeChanges = (username, results, lastSeq) => {
     }
     results = results.concat(changes.results);
     return consumeChanges(username, results, changes.last_seq);
+  });
+};
+
+const batchedChanges = (username, limit, results = [], lastSeq = 0) => {
+  const opts = { since: lastSeq, limit };
+
+  return requestChanges(username, opts).then(changes => {
+    if (!changes.results.length) {
+      return { results: results, last_seq: changes.last_seq };
+    }
+    results = results.concat(changes.results);
+    return batchedChanges(username, limit, results, changes.last_seq);
   });
 };
 
@@ -424,27 +437,85 @@ describe('changes handler', () => {
   });
 
   describe('Filtered replication', () => {
+    const bobsIds = [...DEFAULT_EXPECTED],
+          stevesIds = [...DEFAULT_EXPECTED],
+          couchVersionForBatching = '2.3.0';
+
+    let shouldBatchChangesRequests;
+
+    beforeAll(done => {
+      const options = {
+        hostname: constants.COUCH_HOST,
+        port: constants.COUCH_PORT,
+        auth: auth.user + ':' + auth.pass,
+        path: '/'
+      };
+
+      const req = http.request(options, res => {
+        let body = '';
+        res.on('data', data => body += data);
+        res.on('end', () => {
+          try {
+            body = JSON.parse(body);
+            shouldBatchChangesRequests = semver.lte(couchVersionForBatching, body.version);
+            done();
+          } catch (e) {
+            done(e);
+          }
+        });
+      });
+
+      req.on('error', e => done(e));
+      req.end();
+    });
+
     beforeEach(done => getCurrentSeq().then(done));
 
-    const bobsIds = [...DEFAULT_EXPECTED],
-          stevesIds = [...DEFAULT_EXPECTED];
-
-    it('returns a full list of allowed changes, regardless of the requested limit', () => {
+    it('should successfully fully replicate (with or without limit)', () => {
       const allowedDocs = createSomeContacts(12, 'fixture:bobville');
       bobsIds.push(..._.pluck(allowedDocs, '_id'));
 
       const deniedDocs = createSomeContacts(10, 'irrelevant-place');
       return Promise.all([
-          utils.saveDocs(allowedDocs),
-          utils.saveDocs(deniedDocs)
-        ])
-        .then(() => requestChanges('bob', { limit: 7 }))
+        utils.saveDocs(allowedDocs),
+        utils.saveDocs(deniedDocs)
+      ])
+        .then(() => batchedChanges('bob', 4))
         .then(changes => {
           assertChangeIds(changes,
             'org.couchdb.user:bob',
             'fixture:user:bob',
             'fixture:bobville',
             ..._.without(bobsIds, ...DEFAULT_EXPECTED));
+        });
+    });
+
+    it('depending on CouchDB version, should limit changes requests or specifically ignore limit', () => {
+      const allowedDocs = createSomeContacts(12, 'fixture:bobville');
+      bobsIds.push(..._.pluck(allowedDocs, '_id'));
+      const deniedDocs = createSomeContacts(10, 'irrelevant-place'),
+            expectedIds = [ 'org.couchdb.user:bob', 'fixture:user:bob', 'fixture:bobville'].concat(bobsIds);
+
+      return Promise
+        .all([
+          utils.saveDocs(allowedDocs),
+          utils.saveDocs(deniedDocs)
+        ])
+        .then(() => requestChanges('bob', { limit: 4 }))
+        .then(changes => {
+          if (shouldBatchChangesRequests) {
+            // requests should be limited
+            expect(changes.results.every(change => expectedIds.indexOf(change.id) !== -1 || change.id.startsWith('messages-'))).toBe(true);
+            // because we still process pending changes, it's not a given we will receive only 4 changes.
+            expect(expectedIds.every(id => changes.results.find(change => change.id === id))).toBe(false);
+          } else {
+            // requests should return full changes list
+            assertChangeIds(changes,
+              'org.couchdb.user:bob',
+              'fixture:user:bob',
+              'fixture:bobville',
+              ..._.without(bobsIds, ...DEFAULT_EXPECTED));
+          }
         });
     });
 
@@ -682,8 +753,8 @@ describe('changes handler', () => {
                         utils.saveDocs(allowedSteve),
                       ]))
                       .then(resolve);
-                  });
-                }, 1000)
+                  }, 1000);
+                })
               ]);
             })
             .then(([ changes ]) => {
