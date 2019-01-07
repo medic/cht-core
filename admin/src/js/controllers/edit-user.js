f9f626b90f8f56de95538d55f4f04741c893ee2f
Peter Ochieng <peter@endzone.io>
2019-01-07 10:42:00 +0300

    rebasing

--- admin/src/js/controllers/edit-user.js
+++ admin/src/js/controllers/edit-user.js
@@ -332,7 +332,13 @@ angular
                   $uibModalInstance.close();
                 })
                 .catch(function(err) {
-                  $scope.setError(err, err.data.error);
+                  if (err.status === 400) {
+                    Translate(err.data.error.key, { username: $scope.editUserModel.username }).then(function(value) {
+                    $scope.setError(err, value);
+                  });
+                  } else {
+                    $scope.setError(err, err.data.error.message);
+                  }
                 });
             });
           })
