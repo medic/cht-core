88dab5f3937c9315229b0e751957fc9014fae5fd
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2019-01-11 15:28:50 +1300

    Thank you jshint...

--- webapp/src/js/services/debug.js
+++ webapp/src/js/services/debug.js
@@ -8,41 +8,34 @@
  *   - display $log.debug() output throughout the app
  *
  */
-angular.module('inboxServices').config(
-  function(
-    $logProvider,
-    $provide
-  ) {
-    'ngInject';
+angular.module('inboxServices').config([
+  '$provide', '$logProvider', function($provide, $logProvider) {
     'use strict';
 
-    $provide.service('Debug', function(
-      $window,
-      ipCookie,
-      pouchDB
-    ) {
-      'ngInject';
-      var cookieName = 'medic-webapp-debug';
-      var get = function() {
-        return Boolean(ipCookie(cookieName));
-      };
-      var set = function(bool) {
-        // this changes the default angular behavior and hides debug level
-        // log messages.
-        $logProvider.debugEnabled(bool);
-        var db = pouchDB.debug ? pouchDB : $window.PouchDB;
-        if (bool) {
-          db.debug.enable('*');
-          ipCookie(cookieName, bool, { expires: 360, path: '/' });
-        } else {
-          db.debug.disable();
-          ipCookie.remove(cookieName, { path: '/' });
-        }
-      };
-      return {
-        get: get,
-        set: set
-      };
-    });
+    $provide.service('Debug', [
+      '$window', 'ipCookie', 'pouchDB', function($window, ipCookie, pouchDB) {
+        var cookieName = 'medic-webapp-debug';
+        var get = function() {
+          return Boolean(ipCookie(cookieName));
+        };
+        var set = function(bool) {
+          // this changes the default angular behavior and hides debug level
+          // log messages.
+          $logProvider.debugEnabled(bool);
+          var db = pouchDB.debug ? pouchDB : $window.PouchDB;
+          if (bool) {
+            db.debug.enable('*');
+            ipCookie(cookieName, bool, { expires: 360, path: '/' });
+          } else {
+            db.debug.disable();
+            ipCookie.remove(cookieName, { path: '/' });
+          }
+        };
+        return {
+          get: get,
+          set: set
+        };
+      }
+    ]);
   }
-);
+]);
