1c688b3e47894834c94d1dabb94a63bf288d3183
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2019-01-11 15:01:11 +1300

    Fix tests

--- webapp/src/js/services/debug.js
+++ webapp/src/js/services/debug.js
@@ -10,37 +10,39 @@
  */
 angular.module('inboxServices').config(
   function(
-      $logProvider,
-      $provide,
-      $window
+    $logProvider,
+    $provide
   ) {
     'ngInject';
     'use strict';
 
-    $provide.service('Debug', [
-      'ipCookie', 'pouchDB', function(ipCookie, pouchDB) {
-        var cookieName = 'medic-webapp-debug';
-        var get = function() {
-          return Boolean(ipCookie(cookieName));
-        };
-        var set = function(bool) {
-          // this changes the default angular behavior and hides debug level
-          // log messages.
-          $logProvider.debugEnabled(bool);
-          var db = pouchDB.debug ? pouchDB : $window.PouchDB;
-          if (bool) {
-            db.debug.enable('*');
-            ipCookie(cookieName, bool, { expires: 360, path: '/' });
-          } else {
-            db.debug.disable();
-            ipCookie.remove(cookieName, { path: '/' });
-          }
-        };
-        return {
-          get: get,
-          set: set
-        };
-      }
-    ]);
+    $provide.service('Debug', function(
+      $window,
+      ipCookie,
+      pouchDB
+    ) {
+      'ngInject';
+      var cookieName = 'medic-webapp-debug';
+      var get = function() {
+        return Boolean(ipCookie(cookieName));
+      };
+      var set = function(bool) {
+        // this changes the default angular behavior and hides debug level
+        // log messages.
+        $logProvider.debugEnabled(bool);
+        var db = pouchDB.debug ? pouchDB : $window.PouchDB;
+        if (bool) {
+          db.debug.enable('*');
+          ipCookie(cookieName, bool, { expires: 360, path: '/' });
+        } else {
+          db.debug.disable();
+          ipCookie.remove(cookieName, { path: '/' });
+        }
+      };
+      return {
+        get: get,
+        set: set
+      };
+    });
   }
 );
