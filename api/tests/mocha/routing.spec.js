263851ec86dff8f319a08dcd8dc3cbdf94dadff4
Kenn Sippell <kennsippell@gmail.com>
2019-01-16 12:33:34 +0300

    Fix JsHint

--- api/tests/mocha/routing.spec.js
+++ api/tests/mocha/routing.spec.js
@@ -1,7 +1,5 @@
-
-
 const rewire = require('rewire');
-const { expect } = require('chai');
+const chai = require('chai');
 const routing = rewire('./../../src/routing');
 
 describe('Routing', () => {
@@ -17,7 +15,7 @@ describe('Routing', () => {
     const adminUpgrade = rewire('./../../../admin/src/js/controllers/upgrade');
     const cspBuildDb = routing.__get__('BUILDS_DB');
     const actualBuildDb = adminUpgrade.__get__('BUILDS_DB');
-    expect(cspBuildDb).to.not.eq(undefined);
-    expect(cspBuildDb).to.eq(actualBuildDb);
+    chai.expect(cspBuildDb).to.not.eq(undefined);
+    chai.expect(cspBuildDb).to.eq(actualBuildDb);
   });
 });
