4862fb79acedaf46174664a3f3239c0f9ce65881
Kenn Sippell <kennsippell@gmail.com>
2019-01-17 23:22:34 +0300

    Guard against bad HTTP codes in error reporting

--- api/src/server-utils.js
+++ api/src/server-utils.js
@@ -52,7 +52,12 @@ module.exports = {
     if (typeof err === 'string') {
       return module.exports.serverError(err, req, res);
     }
-    var code = err.code || err.statusCode || err.status || 500;
+    // https://github.com/nodejs/node/issues/9027
+    let code = err.code || err.statusCode || 500;
+    if (typeof code !== 'number') {
+      console.warn('Non-numeric error code: ' + code);
+      code = 502;
+    }
     if (code === 401) {
       return module.exports.notLoggedIn(req, res, showPrompt);
     }
