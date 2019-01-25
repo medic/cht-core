1034d4361a3bdc70e7f2dbb3b8e1c9b1fb8ccf9b
Kenn Sippell <kennsippell@gmail.com>
2019-01-25 10:02:10 +0300

    Only fail build for high or critical vulnerabilities

--- scripts/audit-all.sh
+++ scripts/audit-all.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-dirsToAudit=$(find . -type f -not -path "*/node_modules*/*" -name "package.json" | awk '{print substr($0,0,length()-13);}')
+dirsToAudit=$(find . -type f -not -path "*/node_modules*/*" -not -path '*/scripts/*' -name "package.json" | awk '{print substr($0,0,length()-13);}' | sort)
+filter='| select(.value.severity | contains("high","critical"))'
+
+if [[ $1 == "--all" ]]; then
+   filter=''
+fi
 
 for i in ${dirsToAudit[@]}; do
-  (cd $i && npm audit --package-lock-only --json | jq -r '.advisories | to_entries[] | "[\(.value.severity)] \(.value.title) in \(.value.module_name) (#\(.key))"' | sed "s|$| in ${i}|")
+  (cd $i && npm audit --package-lock-only --json | jq -r ".advisories | to_entries[] $filter | \"[\(.value.severity)] \(.value.title) in \(.value.module_name) (#\(.key))\""  | sed "s|$| in ${i}|")
 done
