075ed7c811c2c8c1b2daaf60abb27dea69fd5320
Kenn Sippell <kennsippell@gmail.com>
2019-01-25 10:28:27 +0300

    Simplify script's output formatting

--- scripts/audit-all.sh
+++ scripts/audit-all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 dirsToAudit=$(find . -type f -not -path "*/node_modules*/*" -not -path '*/scripts/*' -name "package.json" | awk '{print substr($0,0,length()-13);}' | sort)
 filter='| select(.value.severity | contains("high","critical"))'
-
+format='[\(.value.severity)] \(.value.title) for \(.value.module_name) (#\(.key))'
 if [[ $1 == "--all" ]]; then
    filter=''
 fi
 
 for i in ${dirsToAudit[@]}; do
-  (cd $i && npm audit --package-lock-only --json | jq -r ".advisories | to_entries[] $filter | \"[\(.value.severity)] \(.value.title) in \(.value.module_name) (#\(.key))\""  | sed "s|$| in ${i}|")
+  (cd $i && npm audit --package-lock-only --json | jq -r ".advisories | to_entries[] $filter | \"$format in $i\"")
 done
