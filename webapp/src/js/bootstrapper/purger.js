b9ea7530d5a3e207001af2724e8f6e97fe88bbdf
Stefan du Fresne <stefan@sdufresne.info>
2019-01-31 11:51:48 +0000

    Fixing purge's hash function
    The hash function was incorrectly generating every hash as '0'.
    
    As 0 is falsy in JS, that constituted no hash at all, and so it would always think
    something had changed.

--- webapp/src/js/bootstrapper/purger.js
+++ webapp/src/js/bootstrapper/purger.js
@@ -16,8 +16,8 @@ const hash = str => {
     }
 
     /* jshint ignore:start */
-    for (let i = 0; i < this.length; i++) {
-        const char = this.charCodeAt(i);
+    for (let i = 0; i < str.length; i++) {
+        const char = str.charCodeAt(i);
         hash = ((hash << 5) - hash) + char;
         hash = hash & hash; // Convert to 32bit integer
     }
