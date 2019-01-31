medic/cht-core

commit a263801d50e36e446ca8bf9b5961611d9d9ad3e8

Replaces nano with pouchdb from user-db, db-batch, create-user-db

api/src/services/user-db.js
@@ -1,5 +1,7 @@
-const db = require('../db-nano'),
-      async = require('async');
+const request = require('request');
+const url = require('url');
+const db = require('../db-pouch');
+const environment = require('../environment');
 
 // Regex to test for characters that are invalid in db names
 // Only lowercase characters (a-z), digits (0-9), and any of the characters _, $, (, ), +, -, and / are allowed.
@@ -26,24 +28,31 @@ const ddoc = {
   }
 };
 
-const createDb = (dbName, callback) => {
-  db.db.create(dbName, callback);
-};
-
-const setSecurity = (dbName, username, callback) => {
-  db.request({
-    db: dbName,
-    path: '/_security',
-    method: 'PUT',
-    body: {
-      admins: { names: [ username ], roles: [] },
-      members: { names: [ username ], roles:[] }
-    }
-  }, callback);
-};
-
-const putDdoc = (dbName, callback) => {
-  db.use(dbName).insert(ddoc, callback);
+const setSecurity = (dbName, username) => {
+  return new Promise((resolve, reject) => {
+    request.put({
+      url: url.format({
+        protocol: environment.protocol,
+        hostname: environment.host,
+        port: environment.port,
+        pathname: `${dbName}/_security`,
+      }),
+      auth: {
+        user: environment.username,
+        pass: environment.password
+      },
+      json: true,
+      body: {
+        admins: { names: [ username ], roles: [] },
+        members: { names: [ username ], roles: [] }
+      }
+    }, err => {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    });
+  });
 };
 
 /**
@@ -57,12 +66,14 @@ const escapeUsername = name => name.replace(DB_NAME_BLACKLIST, match => {
 module.exports = {
   getDbName: username => `medic-user-${escapeUsername(username)}-meta`,
   setSecurity: setSecurity,
-  create: (username, callback) => {
+  create: username => {
     const dbName = module.exports.getDbName(username);
-    async.series([
-      async.apply(createDb, dbName),
-      async.apply(setSecurity, dbName, username),
-      async.apply(putDdoc, dbName)
-    ], callback);
+    return db.exists(dbName).then(found => {
+      if (!found) {
+        return db.get(dbName)
+          .then(database => database.put(ddoc))
+          .then(() => setSecurity(dbName, username));
+      }
+    });
   }
 };

api/tests/mocha/services/user-db.spec.js
@@ -1,7 +1,9 @@
-const service = require('../../../src/services/user-db'),
-      db = require('../../../src/db-nano'),
-      chai = require('chai'),
-      sinon = require('sinon');
+const service = require('../../../src/services/user-db');
+const db = require('../../../src/db-pouch');
+const environment = require('../../../src/environment');
+const request = require('request');
+const chai = require('chai');
+const sinon = require('sinon');
 
 describe('User DB service', () => {
 
@@ -9,46 +11,61 @@ describe('User DB service', () => {
     sinon.restore();
   });
 
-  it('getDbName creates the user db name', () => {
-    const given = 'jimbob';
-    const expected = 'medic-user-jimbob-meta';
-    const actual = service.getDbName(given);
-    chai.expect(actual).to.equal(expected);
+  describe('getDbName', () => {
+
+    it('returns the user db name', () => {
+      const given = 'jimbob';
+      const expected = 'medic-user-jimbob-meta';
+      const actual = service.getDbName(given);
+      chai.expect(actual).to.equal(expected);
+    });
+
+    it('escapes invalid characters - #3778', () => {
+      // Only lowercase characters (a-z), digits (0-9), and any of the characters _, $, (, ), +, -, and / are allowed.
+      const valid   = 'abc123_$()+-/';
+      const invalid = '.<>^,?!';
+      const escaped = '(46)(60)(62)(94)(44)(63)(33)';
+      const given   = valid + invalid;
+      const expected = `medic-user-${valid + escaped}-meta`;
+      const actual = service.getDbName(given);
+      chai.expect(actual).to.equal(expected);
+    });
+
   });
 
-  it('getDbName escapes invalid characters - #3778', () => {
-    // Only lowercase characters (a-z), digits (0-9), and any of the characters _, $, (, ), +, -, and / are allowed.
-    const valid   = 'abc123_$()+-/';
-    const invalid = '.<>^,?!';
-    const escaped = '(46)(60)(62)(94)(44)(63)(33)';
-    const given   = valid + invalid;
-    const expected = `medic-user-${valid + escaped}-meta`;
-    const actual = service.getDbName(given);
-    chai.expect(actual).to.equal(expected);
-  });
+  describe('create', () => {
 
-  it('creates the db', done => {
-    const create = sinon.stub(db.db, 'create').callsArgWith(1);
-    const request = sinon.stub(db, 'request').callsArgWith(1);
-    const insert = sinon.stub().callsArgWith(1);
-    const use = sinon.stub(db, 'use').returns({ insert: insert });
-    service.create('gareth', err => {
-      chai.expect(err).to.equal(null);
-      chai.expect(create.args[0][0]).to.equal('medic-user-gareth-meta');
-      chai.expect(request.callCount).to.equal(1);
-      const requestParams = request.args[0][0];
-      chai.expect(requestParams.db).to.equal('medic-user-gareth-meta');
-      chai.expect(requestParams.path).to.equal('/_security');
-      chai.expect(requestParams.method).to.equal('PUT');
-      chai.expect(requestParams.body.admins.names[0]).to.equal('gareth');
-      chai.expect(use.callCount).to.equal(1);
-      chai.expect(use.args[0][0]).to.equal('medic-user-gareth-meta');
-      chai.expect(insert.callCount).to.equal(1);
-      const ddoc = insert.args[0][0];
-      chai.expect(ddoc._id).to.equal('_design/medic-user');
-      chai.expect(ddoc.views.read.map).to.equal('function (doc) {\n  var parts = doc._id.split(\':\');\n  if (parts[0] === \'read\') {\n    emit(parts[1]);\n  }\n}');
-      chai.expect(ddoc.views.read.reduce).to.equal('_count');
-      done();
+    it('does nothing if the db already exists', () => {
+      sinon.stub(db, 'exists').resolves(true);
+      return service.create('gareth');
+    });
+
+    it('creates the db if it does not exist', () => {
+      const userDb = { put: function() {} };
+      const create = sinon.stub(db, 'exists').resolves(false);
+      const get = sinon.stub(db, 'get').resolves(userDb);
+      const dbPut = sinon.stub(userDb, 'put').resolves();
+      const requestPut = sinon.stub(request, 'put').callsArgWith(1);
+      environment.protocol = 'http:';
+      environment.host = 'localhost';
+      environment.port = '7357';
+      environment.username = 'auser';
+      environment.password = 'apass';
+      return service.create('gareth').then(() => {
+        chai.expect(create.args[0][0]).to.equal('medic-user-gareth-meta');
+        chai.expect(get.callCount).to.equal(1);
+        const requestParams = requestPut.args[0][0];
+        chai.expect(requestParams.url).to.equal('http://localhost:7357/medic-user-gareth-meta/_security');
+        chai.expect(requestParams.auth).to.deep.equal({ user: 'auser', pass: 'apass' });
+        chai.expect(requestParams.json).to.equal(true);
+        chai.expect(requestParams.body.admins.names[0]).to.equal('gareth');
+        chai.expect(requestParams.body.members.names[0]).to.equal('gareth');
+        chai.expect(dbPut.callCount).to.equal(1);
+        const ddoc = dbPut.args[0][0];
+        chai.expect(ddoc._id).to.equal('_design/medic-user');
+        chai.expect(ddoc.views.read.map).to.equal('function (doc) {\n  var parts = doc._id.split(\':\');\n  if (parts[0] === \'read\') {\n    emit(parts[1]);\n  }\n}');
+        chai.expect(ddoc.views.read.reduce).to.equal('_count');
+      });
     });
   });
 

api/src/controllers/create-user-db.js
@@ -15,13 +15,7 @@ const checkPermissions = req => {
 
 module.exports = (req, res) => {
   return checkPermissions(req)
-    .then(username => {
-      userDb.create(username, err => {
-        if (err) {
-          throw err;
-        }
-        res.json({ ok: true });
-      });
-    })
+    .then(username => userDb.create(username))
+    .then(() => res.json({ ok: true }))
     .catch(err => serverUtils.error(err, req, res));
 };

api/src/db-batch.js
@@ -1,26 +1,24 @@
-const async = require('async'),
-  _ = require('underscore'),
-  db = require('./db-nano'),
-  logger = require('./logger'),
-  DEFAULT_BATCH_LIMIT = 100; // 100 is a good compromise of performance and stability
+const db = require('./db-pouch');
+const logger = require('./logger');
+const DEFAULT_BATCH_LIMIT = 100; // 100 is a good compromise of performance and stability
 
-const runBatch = (ddocName, viewName, viewParams, iteratee, callback) => {
-  db.medic.view(ddocName, viewName, viewParams, (err, response) => {
-    if (err) {
-      return callback(err);
-    }
-    logger.info(`        Processing doc ${response.offset}`);
+const runBatch = (viewName, viewParams, iteratee) => {
+  return db.medic.query(viewName, viewParams).then(result => {
+    logger.info(`        Processing doc ${result.offset}`);
     let nextPage;
-    if (response.rows.length === viewParams.limit) {
-      const lastRow = response.rows.pop();
+    if (result.rows.length === viewParams.limit) {
+      const lastRow = result.rows.pop();
       nextPage = {
         startkey: lastRow.key,
         startkey_docid: lastRow.id,
       };
     }
-    const docs = response.rows.map(row => row.doc);
-    iteratee(docs, err => {
-      callback(err, nextPage);
+    const docs = result.rows.map(row => row.doc);
+    return iteratee(docs).then(() => {
+      if (nextPage) {
+        return runBatch(viewName, Object.assign({}, viewParams, nextPage), iteratee);
+      }
+      // else we've done the last batch
     });
   });
 };
@@ -28,28 +26,18 @@ const runBatch = (ddocName, viewName, viewParams, iteratee, callback) => {
 /**
  * Run an operation over all documents returned from the query in batches.
  *
- * ddocName (string)    Name of the ddoc the view is defined in.
  * viewName (string)    Name of the view.
  * viewParams (object)  Parameters to pass to the view query.
  *  - `limit` defaults to 100 and can be overriden.
  *  - `include_docs` defaults to `true` and cannot be overriden.
  *  - `startkey` and `startkey_docid` cannot be overriden.
  * iteratee (function)  Called to process an array of docs then invoke the given callback.
- * callback (function)  Called on error or when all docs have been processed.
  */
-module.exports.view = (ddocName, viewName, viewParams, iteratee, callback) => {
+module.exports.view = (viewName, viewParams, iteratee) => {
   // add 1 so we know where to start from next iteration
   viewParams.limit = (viewParams.limit || DEFAULT_BATCH_LIMIT) + 1;
   viewParams.include_docs = true;
-  async.doWhilst(
-    callback => runBatch(ddocName, viewName, viewParams, iteratee, callback),
-    nextPage => {
-      if (!nextPage) {
-        return false;
-      }
-      viewParams = _.defaults(nextPage, viewParams);
-      return true;
-    },
-    callback
-  );
+  return Promise.resolve().then(() => {
+    return runBatch(viewName, viewParams, iteratee);
+  });
 };

api/src/db-pouch.js
@@ -23,15 +23,26 @@ if (UNIT_TEST_ENV) {
     'getAttachment',
     'changes',
   ];
+  const GLOBAL_FUNCTIONS_TO_STUB = [
+    'get',
+    'exists'
+  ];
+
+  const notStubbed = (first, second) => {
+    const name = second ? `${first}.${second}` : first;
+    logger.error(new Error(`${name}() not stubbed!  UNIT_TEST_ENV=${UNIT_TEST_ENV}.  Please stub PouchDB functions that will be interacted with in unit tests.`));
+    process.exit(1);
+  };
 
   DBS_TO_STUB.forEach(db => {
     module.exports[db] = {};
     DB_FUNCTIONS_TO_STUB.forEach(fn => {
-      module.exports[db][fn] = () => {
-        logger.error(new Error(`${db}.${fn}() not stubbed!  UNIT_TEST_ENV=${UNIT_TEST_ENV}.  Please stub PouchDB functions that will be interacted with in unit tests.`));
-        process.exit(1);
-      };
+      module.exports[db][fn] = () => notStubbed(db, fn);
     });
+  });
+
+  GLOBAL_FUNCTIONS_TO_STUB.forEach(fn => {
+     module.exports[fn] = () => notStubbed(fn);
   });
 } else {
   const DB = new PouchDB(environment.couchUrl, {
@@ -40,7 +51,19 @@ if (UNIT_TEST_ENV) {
       return PouchDB.fetch(url, opts);
     },
   });
+  const getDbUrl = name => `${environment.serverUrl}/${name}`;
   DB.setMaxListeners(0);
   module.exports.medic = DB;
-  module.exports.users = new PouchDB(environment.serverUrl + '/_users');
+  module.exports.users = new PouchDB(getDbUrl('/_users'));
+
+  // Get the DB with the given name
+  module.exports.get = name => new PouchDB(getDbUrl(name));
+
+  // Resolves true if the DB with the given name exists
+  module.exports.exists = name => {
+    const db = new PouchDB(getDbUrl(name), { skip_setup: true });
+    return db.info()
+      .then(() => true)
+      .catch(() => false);
+  };
 }

api/src/migrations/extract-read-status.js
@@ -1,6 +1,4 @@
-const db = require('../db-nano'),
-      {promisify} = require('util'),
-      async = require('async'),
+const db = require('../db-pouch'),
       userDb = require('../services/user-db'),
       batch = require('../db-batch');
 
@@ -10,27 +8,14 @@ const createReadStatusDoc = record => {
   return { _id: id };
 };
 
-const ensureDbExists = (username, dbName, callback) => {
-  db.db.get(dbName, err => {
-    if (err && err.statusCode === 404) {
-      return userDb.create(username, callback);
-    }
-    callback(err);
-  });
-};
-
-const saveReadStatusDocs = (username, docs, callback) => {
+const saveReadStatusDocs = (username, docs) => {
   const userDbName = userDb.getDbName(username);
-  ensureDbExists(username, userDbName, err => {
-    if (err) {
-      return callback(err);
-    }
-    const userDb = db.use(userDbName);
-    userDb.bulk({ docs: docs }, callback);
+  return userDb.create(userDbName).then(() => {
+    return db.get(userDbName).bulkDocs(docs);
   });
 };
 
-const extract = (docs, callback) => {
+const extract = docs => {
   const toSave = {};
   docs.forEach(doc => {
     if (doc.read) {
@@ -42,17 +27,15 @@ const extract = (docs, callback) => {
       });
     }
   });
-  async.each(
-    Object.keys(toSave),
-    (username, callback) => saveReadStatusDocs(username, toSave[username], callback),
-    callback
-  );
+  return Promise.all(Object.keys(toSave).map(username => {
+    return saveReadStatusDocs(username, toSave[username]);
+  }));
 };
 
 module.exports = {
   name: 'extract-read-status',
   created: new Date(2017, 6, 7),
-  run: promisify(callback => {
-    batch.view('medic-client', 'doc_by_type', { key: [ 'data_record' ] }, extract, callback);
-  })
+  run: () => {
+    return batch.view('medic-client/doc_by_type', { key: [ 'data_record' ] }, extract);
+  }
 };

api/src/migrations/fix-user-db-security.js
@@ -15,14 +15,13 @@ module.exports = {
 
         return usernames.reduce((p, username) => {
           return p.then(() => {
-            return new Promise((resolve, reject) => {
-              userDb.setSecurity(userDb.getDbName(username), username, err => {
-                if (err && err.statusCode !== 404) { // db not found is ok
-                  return reject(err);
+            return userDb.setSecurity(userDb.getDbName(username), username)
+              .catch(err => {
+                if (err && err.status !== 404) {
+                  throw err;
                 }
-                resolve();
+                // db not found is ok
               });
-            });
           });
         }, Promise.resolve());
       })

api/tests/mocha/controllers/create-user-db.spec.js
@@ -39,7 +39,7 @@ describe('create-user-db controller', () => {
     const req = { url: '/medic-user-gareth-meta/' };
     const res = { json: sinon.stub() };
     sinon.stub(auth, 'getUserCtx').resolves({ name: 'gareth' });
-    const create = sinon.stub(userDb, 'create').callsArgWith(1);
+    const create = sinon.stub(userDb, 'create').resolves();
     return controller(req, res).then(() => {
       chai.expect(res.json.callCount).to.equal(1);
       chai.expect(res.json.args[0][0]).to.deep.equal({ ok: true });

commit 380c749f7f1b3d1c946581dfebd20d0f1116a06f

Create user's meta dbs correctly
Broken by commit: a263801d50e36e446ca8bf9b5961611d9d9ad3e8

api/src/db.js
@@ -64,7 +64,11 @@ if (UNIT_TEST_ENV) {
   module.exports.exists = name => {
     const db = new PouchDB(getDbUrl(name), { skip_setup: true });
     return db.info()
-      .then(() => true)
+      .then(result => {
+        // In at least PouchDB 7.0.0, info() on a non-existant db doesn't throw,
+        // instead it returns the error structure
+        return  !result.error;
+      })
       .catch(() => false);
   };
 }

api/src/services/user-db.js
@@ -63,9 +63,11 @@ module.exports = {
     const dbName = module.exports.getDbName(username);
     return db.exists(dbName).then(found => {
       if (!found) {
-        return db.get(dbName)
-          .then(database => database.put(ddoc))
-          .then(() => setSecurity(dbName, username));
+        const database = db.get(dbName);
+        return database.put(ddoc)
+          .then(() => {
+            return setSecurity(dbName, username);
+          });
       }
     });
   }

webapp/src/js/services/db.js
@@ -50,6 +50,8 @@ angular.module('inboxServices').factory('DB',
     const getParams = (remote, meta) => {
       const clone = Object.assign({}, remote ? POUCHDB_OPTIONS.remote : POUCHDB_OPTIONS.local);
       if (remote && meta) {
+        // Don't create user DBs remotely, we do this ourselves in /api/services/user-db:create,
+        // which is called in routing when a user tries to access the DB
         clone.skip_setup = false;
       }
       return clone;

api/tests/integration/migrations/utils.js
@@ -185,7 +185,11 @@ const initDb = content => {
     });
 };
 
-const _resetDb = () => {
+const _resetDb = (attempts = 0) => {
+  if (attempts === 3) {
+    return Promise.reject(new Error('Unable to reset medic-test db'));
+  }
+
   return db.exists('medic-test')
     .then(exists => {
       if (exists) {
@@ -197,17 +201,9 @@ const _resetDb = () => {
     })
     .catch(err => {
       logger.error('Could not create "medic-test" directly after deleting, pausing and trying again');
-      return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          db.get('medic-test')
-            .then(() => {
-              logger.info(`After a struggle, at ${new Date()}, re-created "medic-test"`);
-              resolve();
-            })
-            .catch(err => {
-              reject(new Error('Error creating "medic-test": ' + err.message));
-            });
-        }, 3000);
+      logger.error(err);
+      return new Promise(resolve => {
+        setTimeout(() => resolve(_resetDb(attempts + 1)), 3000);
       });
     });
 };

api/tests/mocha/services/user-db.spec.js
@@ -43,7 +43,7 @@ describe('User DB service', () => {
     it('creates the db if it does not exist', () => {
       const userDb = { put: function() {} };
       const create = sinon.stub(db, 'exists').resolves(false);
-      const get = sinon.stub(db, 'get').resolves(userDb);
+      const get = sinon.stub(db, 'get').returns(userDb);
       const dbPut = sinon.stub(userDb, 'put').resolves();
       const requestPut = sinon.stub(request, 'put').resolves();
       environment.protocol = 'http:';