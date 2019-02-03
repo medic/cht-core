1e4bc0f8e369e146b1e6830b18e85c0497e7edbb
Kenn Sippell <kennsippell@gmail.com>
2019-02-03 23:54:11 +0300

    Code cleanup (async!)

--- scripts/audit-all.js
+++ scripts/audit-all.js
@@ -2,16 +2,18 @@ const glob = require('glob');
 const path = require('path');
 const child_process = require('child_process');
 
-const foldersToIgnore = [
+const globOfFoldersToIgnore = [
   '**/node_modules/**',
   'scripts/**',
   '**/node_modules_backup/**'
 ];
 
-const filteredSeverity = ['high', 'critical'];
+const filterBySeverity = ['high', 'critical'];
 
 const audit = directory => new Promise((resolve, reject) => {
-  child_process.exec('$(which npm) audit --package-lock-only --json', { cwd: path.join('.', directory)}, (error, stdout, stderr) => {
+  const execCommand = '$(which npm) audit --package-lock-only --json';
+  const execOptions = { cwd: path.join('.', directory) };
+  child_process.exec(execCommand, execOptions, (error, stdout, stderr) => {
     if (!error) {
       return resolve([]);
     }
@@ -27,13 +29,17 @@ const audit = directory => new Promise((resolve, reject) => {
   });
 });
 
-const packageJsonFilePaths = glob.sync('**/package.json', { ignore: foldersToIgnore });
-const auditableFolders = packageJsonFilePaths.map(file => path.dirname(file));
-const audits = auditableFolders.map(audit);
+const pathToAllPackageJsonFiles = glob.sync('**/package.json', { ignore: globOfFoldersToIgnore });
+const pathOfFoldersToAudit = pathToAllPackageJsonFiles.map(file => path.dirname(file));
+const audits = pathOfFoldersToAudit.map(audit);
 
-const reduceAuditResultToAdvisory = (agg, result) => {
+const reduceAuditResultsToAdvisories = (agg, result) => {
+  if (!result.auditResult) {
+    return agg;
+  }
+
   const advisories = Object.keys(result.auditResult.advisories)
-    .filter(key => filteredSeverity.includes(result.auditResult.advisories[key].severity))
+    .filter(key => filterBySeverity.includes(result.auditResult.advisories[key].severity))
     .map(advisoryId => ({
       advisoryId,
       directory: result.directory,
@@ -46,8 +52,9 @@ const reduceAuditResultToAdvisory = (agg, result) => {
 const summarizeAdvisories = advisories => advisories.map(summary => `[${summary.advisory.severity}] ${summary.advisory.title} for ${summary.advisory.module_name} (#${summary.advisoryId}) in ${summary.directory}`);
 const logSummaries = summaries => summaries.forEach(x => console.log(x));
 
-Promise.all(audits)
-  .then(results => results.filter(result => 'auditResult' in result))
-  .then(auditResults => auditResults.reduce(reduceAuditResultToAdvisory, []))
-  .then(summarizeAdvisories)
-  .then(logSummaries);
+(async () => {
+  const auditResults = await Promise.all(audits);
+  const advisories = auditResults.reduce(reduceAuditResultsToAdvisories, []);
+  const summaries = await summarizeAdvisories(advisories);
+  logSummaries(summaries);
+})();
