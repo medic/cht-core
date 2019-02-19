245b95df92135e5c0b0fd1a34e6c500a4f7185c1
Willy Tekeu <willy.tekeu@gmail.com>
2019-02-19 11:56:40 -0500

    Fix eslint issues

--- webapp/src/js/services/exception-handler.js
+++ webapp/src/js/services/exception-handler.js
@@ -5,18 +5,18 @@ var feedback = require('../modules/feedback');
  */
  angular.module('inboxServices').factory('$exceptionHandler',
   function($log, $window) {
-  	function errorHandler(exception, cause) {
+    function errorHandler(exception, cause) {
       var error = { message: exception.message, stack: exception.stack, cause: cause };
-	    try {
-	      feedback.submit(error, false, function(err) {
-	        if (err) {
-	          $log.error('Error saving feedback', err);
-	        }
-	      });
-	    } catch(e) {
-	      // stop infinite loop of exceptions
-	      $log.error('Error while trying to record error', JSON.stringify(error), e.toString(), e);
-	    }
+      try {
+        feedback.submit(error, false, function(err) {
+          if (err) {
+            $log.error('Error saving feedback', err);
+          }
+        });
+      } catch(e) {
+        // stop infinite loop of exceptions
+        $log.error('Error while trying to record error', JSON.stringify(error), e.toString(), e);
+      }
     }
     return (errorHandler);
   }
