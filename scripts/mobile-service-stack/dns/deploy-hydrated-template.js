7c32bd9bcac5bc65bb58e66da006c5a73e1987cd
Kenn Sippell <kennsippell@gmail.com>
2019-03-29 02:23:52 -0700

    This too

---
+++ scripts/mobile-service-stack/dns/deploy-hydrated-template.js
@@ -0,0 +1,38 @@
+// This script takes the medic.local.template file and writes a copy hydrated with the machine's ip information
+
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+const _ = require('underscore');
+
+const getIpAddress = () => {
+  const networkInterface = os.networkInterfaces();
+  const internalInterface = _.flatten(Object.values(networkInterface))
+    .find(interface => interface.family === 'IPv4' && interface.internal === false);
+  return internalInterface && internalInterface.address;
+};
+
+const getHydratedTemplate = ipAddress => {
+  const templateString = fs.readFileSync(path.join(__dirname, 'db.medic.local.template')).toString();
+  const compiledTemplate = _.template(templateString);
+  return compiledTemplate({ ipAddress });
+};
+
+const main = () => {
+  const ipAddress = getIpAddress();
+
+  if (ipAddress) {
+    console.log(`Using IP address: ${ipAddress}`);
+  } else {
+    console.error('Could not find valid IP address');
+  }
+
+  const template = getHydratedTemplate(ipAddress);
+  const pathToDeployFolder = path.join(__dirname, '../deploy');
+  if (!fs.existsSync(pathToDeployFolder)) {
+    fs.mkdirSync(pathToDeployFolder);
+  }
+  fs.writeFileSync(path.join(pathToDeployFolder, 'db.medic.local'), template);
+};
+
+main();
