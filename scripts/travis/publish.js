3edb8689d31ca1ba825569a3b709ad90e29930a1
Gomex Vato <gomexvato@gmail.com>
2019-04-03 13:13:56 -0600

    Updates to run tests for external contribs

--- scripts/travis/publish.js
+++ scripts/travis/publish.js
@@ -19,8 +19,8 @@ if (!releaseName) {
   process.exit(0);
 }
 
-const testingDb = new PouchDB(`${UPLOAD_URL}/_couch/builds_testing`);
-const stagingDb = new PouchDB(`${UPLOAD_URL}/_couch/builds`);
+const testingDb = new PouchDB(`${UPLOAD_URL}/${BUILDS_SERVER}`);
+const stagingDb = new PouchDB(`${UPLOAD_URL}/${STAGING_SERVER}`);
 
 const testingDocId = `medic:medic:test-${TRAVIS_BUILD_NUMBER}`;
 const stagingDocId = `medic:medic:${releaseName}`;
