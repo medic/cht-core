c080024916600086ac4637d59dd6b734087f6ca9
Diana Barsan <barsan@medicmobile.org>
2019-04-04 15:34:47 +0300

    reminders take 1

--- ddocs/medic-sms/views/sent_reminders/map.js
+++ ddocs/medic-sms/views/sent_reminders/map.js
@@ -1,9 +1,10 @@
 function(doc) {
-  if (Array.isArray(doc.tasks)) {
-    doc.tasks.forEach(function(task) {
-      if (task.form && task.timestamp) {
-        emit([task.form, task.timestamp]);
-      }
-    });
+  if (doc.type === 'reminder') {
+    emit([doc.contact_type, doc.form, doc.timestamp], 0);
+    if (doc.tasks && Array.isArray(doc.tasks)) {
+      doc.tasks.forEach(function(task) {
+        emit([task.contact_id, doc.form, doc.timestamp]);
+      });
+    }
   }
 }
