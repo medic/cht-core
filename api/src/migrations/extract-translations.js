4afe4c7f4dedfbefe3a9c38acdb8e6216a470dfa
Stefan du Fresne <stefan@medicmobile.org>
2019-05-01 17:30:24 +0300

    WIP

--- api/src/migrations/extract-translations.js
+++ api/src/migrations/extract-translations.js
@@ -31,16 +31,9 @@ const mergeTranslations = (settings, docs) => {
     setting.translations.forEach(translation => {
       const doc = _.findWhere(docs, { code: translation.locale });
       if (doc) {
-        if (_.has(doc, 'values')) {
-          if (!doc.values[setting.key] || translation.content !== translation.default) {
-            // only update the doc if it was changed from the default
-            doc.values[setting.key] = translation.content;
-          }
-        } else { 
-          if (!doc.generic[setting.key] || translation.content !== translation.default) {
-            // only update the doc if it was changed from the default
-            doc.generic[setting.key] = translation.content;
-          }
+        if (!doc.values[setting.key] || translation.content !== translation.default) {
+          // only update the doc if it was changed from the default
+          doc.values[setting.key] = translation.content;
         }
       }
     });
@@ -62,6 +55,17 @@ module.exports = {
   name: 'extract-translations',
   created: new Date(2016, 6, 29),
   run: () => {
+    return settingsService.get().then(settings => {
+      if (!settings.translations || !settings.translations.length) {
+        return;
+      }
+      return updateDocs(settings).then(() => {
+        return settingsService.update({ translations: null, locales: null });
+      });
+    });
+  }
+};
+ () => {
     return settingsService.get().then(settings => {
       if (!settings.translations || !settings.translations.length) {
         return;
