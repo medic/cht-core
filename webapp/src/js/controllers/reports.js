neo4reo/medic

Stop testing for strings...

webapp/src/js/controllers/reports.js
@@ -191,12 +191,9 @@ angular
     };
 
     $scope.deselectReport = function(report) {
-      spliceSelected(_.isString(report) ? report : report._id);
-      $(
-        '#reports-list li[data-record-id="' +
-          report._id +
-          '"] input[type="checkbox"]'
-      ).prop('checked', false);
+      const reportId = report._id || report;
+      spliceSelected(reportId);
+      $(`#reports-list li[data-record-id="${reportId}"] input[type="checkbox"]`).prop('checked', false);
       $scope.settingSelected(true);
     };
 

medic/cht-core

webapp/src/js/controllers/reports.js
@@ -196,12 +196,9 @@ angular
     };
 
     $scope.deselectReport = function(report) {
-      removeSelected(_.isString(report) ? report : report._id);
-      $(
-        '#reports-list li[data-record-id="' +
-          report._id +
-          '"] input[type="checkbox"]'
-      ).prop('checked', false);
+      const reportId = report._id || report;
+      removeSelected(reportId);
+      $(`#reports-list li[data-record-id="${reportId}"] input[type="checkbox"]`).prop('checked', false);
       $scope.settingSelected(true);
     };
 