71d529b9348391ec1583439d736c5ddc5af92989
Kenn Sippell <kennsippell@gmail.com>
2019-05-21 14:20:41 -0700

    Use meta tag instead of script

--- api/src/public/appcache-upgrade.js
+++
@@ -1,4 +0,0 @@
-/*
-This code executes within the empty page at /medic/_design/medic/_rewire/ which is served to previous versions when upgrading from an appcache build to service-workers
-*/
-window.location = '/';
