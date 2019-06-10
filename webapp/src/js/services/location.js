55c11c78646e8d535f5dbca8d9707e16b4d9dfdc
Willy Tekeu <willy.tekeu@gmail.com>
2019-06-10 16:57:01 -0400

    Fix test

--- webapp/src/js/services/location.js
+++ webapp/src/js/services/location.js
@@ -5,7 +5,7 @@ angular.module('inboxServices').factory('Location',
     'ngInject';
 
     const location = $window.location;
-    const dbName = '@@COUCH_DB_NAME';
+    const dbName = 'medic-test';//'@@COUCH_DB_NAME';
     const path = '/';
     const adminPath = '/admin/';
     const port = location.port ? ':' + location.port : '';
