0628caeef3ee55c4b3b0befd8180339731681879
Willy Tekeu <willy.tekeu@gmail.com>
2019-06-11 12:29:55 -0400

    Fix gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function(grunt) {
           {
             from: /@@APP_CONFIG.name/g,
             to: packageJson.name,
-          }
+          },
         ],
       },
       'change-ddoc-id-for-testing': {
