fd33ceab27c5962c6deb4de5743e2b632b1ba5c1
George Otieno <georgeorti@gmail.com>
2019-06-20 04:30:14 +0300

    Add permissions to allow users to view specifc pages of the admin app
    Add new permissions to allow users to view outgoing messages and export tabs of the admin app.
    
    #5464

--- admin/src/js/controllers/main.js
+++ admin/src/js/controllers/main.js
@@ -13,7 +13,7 @@ angular.module('controllers').controller('MainCtrl',
     $translate.use('en');
     $scope.authorized = false;
     $scope.navbarCollapsed = true;
-    Auth('can_configure')
+    Auth.any([['can_configure'], ['can_view_outgoing_messages'], ['can_export_all']])
     .then(function() {
       $scope.authorized = true;
     })
