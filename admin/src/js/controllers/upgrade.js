73effe9cbc84d34258d933b0c8f0343e00116920
Diana Barsan <barsan@medicmobile.org>
2019-06-26 17:00:30 +0300

    Just keep it simple.

--- admin/src/js/controllers/upgrade.js
+++ admin/src/js/controllers/upgrade.js
@@ -25,8 +25,8 @@ angular.module('controllers').controller('UpgradeCtrl',
     $scope.loading = true;
     $scope.versions = {};
 
-    var getCurrentDeployment = function(rev) {
-      return DB().get(DEPLOY_DOC_ID, { rev })
+    var getCurrentDeployment = function() {
+      return DB().get(DEPLOY_DOC_ID)
       .then(function(deployDoc) {
         $scope.deployDoc = deployDoc;
       }).catch(function(err) {
@@ -172,7 +172,15 @@ angular.module('controllers').controller('UpgradeCtrl',
     Changes({
       key: 'upgrade',
       filter: change => change.id === DEPLOY_DOC_ID,
-      callback: change => getCurrentDeployment(change.changes[0].rev)
+      callback: change => {
+        if (!change.deleted) {
+          return getCurrentDeployment();
+        }
+
+        if ($scope.deployDoc) {
+          $scope.deployDoc._deleted = true;
+        }
+      }
     });
   }
 );
