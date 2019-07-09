df5dacfb7ea98439bc245b00c96dac28a7ae1f6c
Willy Tekeu <willy.tekeu@gmail.com>
2019-07-09 11:45:43 -0400

    Changes location of the "extra" placeholders

--- scripts/poe/lib/validate.js
+++ scripts/poe/lib/validate.js
@@ -1,5 +1,6 @@
 const {log, error, mkdir, extractPlaceholders} = require('./utils');
 const fs = require('fs');
+const path = require('path');
 
 const fileExists = (fpath) => {
   const file = `${process.cwd()}/${fpath}`;
@@ -33,10 +34,10 @@ const validDirectory = (fpath) => {
 
 const validatePlaceHolders = (langs, dir) => {
   let valid = true;
-  const extraFile = `${dir}/messages-ex.properties`;
+  const extraFile = path.join(__dirname, '..', 'messages-ex.properties');
   const templateFile = `${dir}/messages-en.properties`;
   const templatePlaceholders = extractPlaceholders(templateFile, extraFile);
-  langs.filter(lang => lang !== 'en' && lang !== 'ex').forEach(lang => {
+  langs.filter(lang => lang !== 'en').forEach(lang => {
     const file = `${dir}/messages-${lang}.properties`;
     const placeholders = extractPlaceholders(file);
     Object.keys(placeholders).forEach(k => {
@@ -45,7 +46,7 @@ const validatePlaceHolders = (langs, dir) => {
       const foundAllPlaceholders = placeholder.placeholders.every(el => templatePlaceholder.placeholders.includes(el));
       if (!foundAllPlaceholders) {
         valid = false;
-        console.error(`\nFAILURE: messages-${lang}.properties: Translation key ${k} on line ${placeholder.index + 1} has placeholders that do not match those of messages-en.properties\nYou can use messages-en.extra.properties to add placeholders missing from the reference context.`);
+        console.error(`\nFAILURE: messages-${lang}.properties: Translation key ${k} on line ${placeholder.index + 1} has placeholders that do not match those of messages-en.properties\nYou can use messages-ex.properties to add placeholders missing from the reference context.`);
       }
     });
   });
