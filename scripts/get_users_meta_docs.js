09d348d7ee0c7a5b9f023b5ff42fa14277077a59
Willy Tekeu <willy.tekeu@gmail.com>
2019-07-31 17:29:31 -0400

    Fixes bugs associated with processing of db results
    1) No results from db
    2) Pagination to account for large db

--- scripts/get_users_meta_docs.js
+++ scripts/get_users_meta_docs.js
@@ -2,7 +2,8 @@ const inquirer = require('inquirer'),
       PouchDB = require('pouchdb-core'),
       fs = require('fs'),
       path = require('path'),
-      minimist = require('minimist');
+      minimist = require('minimist'),
+      json = require('big-json');
 
 PouchDB.plugin(require('pouchdb-adapter-http'));
 
@@ -36,6 +37,54 @@ const couchUrl = argv['_'][0] || 'http://admin:pass@localhost:5984/medic-users-m
 
 const db = PouchDB(couchUrl);
 
+var nextOptions = { 
+  include_docs: true,
+  limit : 100,
+  startkey: `${type}-`,
+  endkey: `${type}-\ufff0`,
+};
+
+var prevOptions = { 
+  include_docs: true,
+  limit : 100,
+  startkey: `${type}-`,
+  endkey: `${type}-\ufff0`,
+};
+
+var prevStartKeys = [];
+
+const fetchNextDocs = async () => {
+  const response = await db.allDocs(nextOptions);
+  if (response && response.rows.length > 0) {
+    prevStartKeys.push(nextOptions.startkey);
+    nextOptions.startkey = response.rows[response.rows.length - 1].id;
+    nextOptions.skip = 1;
+
+    return response.rows.map(row => row.doc);
+  }
+  return [];
+};
+
+const fetchPrevDocs = async () => {
+  if (prevStartKeys.length === 0) {
+    return [];
+  }
+  const startkey = prevStartKeys.pop();
+  if (prevOptions.startkey === startkey) {
+    return [];
+  }
+  prevOptions.startkey = startkey;
+  const response = await db.allDocs(prevOptions);
+  if (response && response.rows.length > 0) {
+    prevOptions.startkey = response.rows[response.rows.length - 1].id;
+    nextOptions.skip = 1;
+
+    return response.rows.map(row => row.doc);
+  }
+  return [];
+};
+
+
 let docIndex = 0;
 let docs;
 
@@ -57,38 +106,83 @@ const actionQuestions = [{
 (async function() {
 
   try {
-    const data = await db.allDocs({
-     include_docs: true,
-     startkey: `${type}-`,
-     endkey: `${type}-\ufff0`,
-    });
-    
-    docs = data.rows.map(row => row.doc);
-
     if (mode === 'batch') {
-      console.log(JSON.stringify(docs, null, 2));
+      var allDocs = [];
+      for (;;) {
+        docs = await fetchNextDocs();
+        if (docs.length > 0) {
+          allDocs = allDocs.concat(docs);
+        } else {
+          if (allDocs.length === 0) {
+            console.log('\x1b[31m%s\x1b[0m', `There are no documents of type ${type}`);
+          } else {
+            const stringifyStream = json.createStringifyStream({
+              body: allDocs
+            });
+             
+            stringifyStream.on('data', function(strChunk) {
+                console.log(strChunk.toString('utf8'));
+            });
+          }
+          break;
+        }
+      }
+      //console.log(JSON.stringify(docs, null, 2));
     } else if (mode === 'interactive') {
-     console.log(JSON.stringify(docs[docIndex], null, 2));
-     for (;;) {
-      const response = await inquirer.prompt(actionQuestions);
-      if (response.action === 'next') {
-        docIndex = Math.min(docs.length - 1, ++docIndex);
+      docs = await fetchNextDocs();
+      if (docs.length === 0) {
+        console.log('\x1b[31m%s\x1b[0m', `There are no documents of type ${type}`);
+      } else {
         console.log(JSON.stringify(docs[docIndex], null, 2));
-      } else if (response.action === 'previous') {
-        docIndex = Math.max(0, --docIndex);
-        console.log(JSON.stringify(docs[docIndex], null, 2));
-      } else if (response.action === 'save_current') {
-        let filePath = path.join(path.resolve(__dirname), docs[docIndex]._id + '.json');
-        fs.writeFileSync(filePath, JSON.stringify(docs[docIndex], null, 2));
-        console.log(`Current document saved to ${filePath}`);
-      } else if (response.action === 'save_all') {
-        let filePath = path.join(path.resolve(__dirname), `${type}.json`);
-        fs.writeFileSync(filePath, JSON.stringify(docs, null, 2));
-        console.log(`Documents saved to ${filePath}`);
-      } else if (response.action === 'quit') {  
-        break;
-      }  
-     }
+        for (;;) {
+          const response = await inquirer.prompt(actionQuestions);
+
+          var printMessage = false;
+          if (response.action === 'next') {
+            if (++docIndex > docs.length - 1) {
+              const nextDocs = await fetchNextDocs();
+              if (nextDocs.length === 0) {
+                printMessage = true;
+                docIndex = docs.length - 1;
+              } else {
+                docs = nextDocs;
+                docIndex = 0;
+              }
+            }
+            //docIndex = Math.min(docs.length - 1, ++docIndex);
+            console.log(JSON.stringify(docs[docIndex], null, 2));
+            if (printMessage) {
+              console.log('\x1b[31m%s\x1b[0m', `No next document. This is the last one.`);
+            }
+          } else if (response.action === 'previous') {
+            if (--docIndex < 0) {
+              const prevDocs = await fetchPrevDocs();
+              if (prevDocs.length === 0) {
+                printMessage = true;
+                docIndex = 0;
+              } else {
+                docs = prevDocs;
+                docIndex = docs.length - 1;
+              }
+            }
+            //docIndex = Math.max(0, --docIndex);
+            console.log(JSON.stringify(docs[docIndex], null, 2));
+            if (printMessage) {
+              console.log('\x1b[31m%s\x1b[0m', `No previous document. This is the first one`);
+            }
+          } else if (response.action === 'save_current') {
+            let filePath = path.join(path.resolve(__dirname), docs[docIndex]._id + '.json');
+            fs.writeFileSync(filePath, JSON.stringify(docs[docIndex], null, 2));
+            console.log('\x1b[31m%s\x1b[0m', `Current document saved to ${filePath}`);
+          } else if (response.action === 'save_all') {
+            let filePath = path.join(path.resolve(__dirname), `${type}.json`);
+            fs.writeFileSync(filePath, JSON.stringify(docs, null, 2));
+            console.log('\x1b[31m%s\x1b[0m', `Documents saved to ${filePath}`);
+          } else if (response.action === 'quit') {  
+            break;
+          }  
+        }
+      }
     }
   } catch(err) {
     console.log(err);
