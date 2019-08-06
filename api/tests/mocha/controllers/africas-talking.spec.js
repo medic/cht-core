b1a4825f67433449690b04802cd840c79487b969
Gareth Bowen <gareth@medicmobile.org>
2019-08-07 09:32:35 +1200

    Fix Africa's Talking security checking
    The old method didn't work with our production stack.
    
    medic/medic#5844

--- api/tests/mocha/controllers/africas-talking.spec.js
+++ api/tests/mocha/controllers/africas-talking.spec.js
@@ -1,13 +1,13 @@
 const sinon = require('sinon');
 const chai = require('chai');
-const config = require('../../../src/config');
+const secureSettings = require('@medic/settings');
 const controller = require('../../../src/controllers/africas-talking');
 const messaging = require('../../../src/services/messaging');
 const serverUtils = require('../../../src/server-utils');
 
 let res;
 
-describe('Africas Talking controller', () => {
+describe('Africa\'s Talking controller', () => {
 
   beforeEach(() => res = { json: sinon.stub() });
 
@@ -15,39 +15,42 @@ describe('Africas Talking controller', () => {
 
   describe('incomingMessages', () => {
 
-    it('returns error when not using https', () => {
-      const req = {
-        connection: { localAddress: 'a', remoteAddress: 'b' },
-        ip: '1.2.3.4',
-        protocol: 'http'
-      };
-      sinon.stub(config, 'get').returns({ africas_talking: { allowed_ips: [ '1.2.3.4' ] } });
+    it('returns error when key not configured', () => {
+      const req = { query: {} };
+      sinon.stub(secureSettings, 'getCredentials').resolves();
       sinon.stub(serverUtils, 'error').returns();
-      controller.incomingMessages(req, res);
-      chai.expect(serverUtils.error.callCount).to.equal(1);
-      chai.expect(res.json.callCount).to.equal(0);
+      return controller.incomingMessages(req, res).then(() => {
+        chai.expect(serverUtils.error.callCount).to.equal(1);
+        chai.expect(res.json.callCount).to.equal(0);
+      });
     });
 
-    it('returns error when ip not allowed', () => {
-      const req = {
-        connection: { localAddress: 'a', remoteAddress: 'b' },
-        ip: '1.2.3.4',
-        protocol: 'https',
-      };
+    it('returns error when no key given', () => {
+      const req = { query: {} };
+      sinon.stub(secureSettings, 'getCredentials').resolves('mykey');
       sinon.stub(serverUtils, 'error').returns();
-      controller.incomingMessages(req, res);
-      chai.expect(serverUtils.error.callCount).to.equal(1);
-      chai.expect(res.json.callCount).to.equal(0);
+      return controller.incomingMessages(req, res).then(() => {
+        chai.expect(serverUtils.error.callCount).to.equal(1);
+        chai.expect(res.json.callCount).to.equal(0);
+      });
+    });
+
+    it('returns error when key does not match', () => {
+      const req = { query: 'wrong' };
+      sinon.stub(secureSettings, 'getCredentials').resolves('mykey');
+      sinon.stub(serverUtils, 'error').returns();
+      return controller.incomingMessages(req, res).then(() => {
+        chai.expect(serverUtils.error.callCount).to.equal(1);
+        chai.expect(res.json.callCount).to.equal(0);
+      });
     });
 
     it('passes the message to the service', () => {
       const req = {
-        connection: { localAddress: 'a', remoteAddress: 'b' },
-        ip: '1.2.3.4',
-        protocol: 'https',
+        query: { key: 'mykey' },
         body: { id: '123', from: '+456', text: 'gidday' }
       };
-      sinon.stub(config, 'get').returns({ africas_talking: { allowed_ips: [ '1.2.3.4' ] } });
+      sinon.stub(secureSettings, 'getCredentials').resolves('mykey');
       sinon.stub(messaging, 'processIncomingMessages').resolves({ saved: 1 });
       return controller.incomingMessages(req, res).then(() => {
         chai.expect(messaging.processIncomingMessages.callCount).to.equal(1);
@@ -59,65 +62,44 @@ describe('Africas Talking controller', () => {
       });
     });
 
-    it('allows http when localhost', () => {
-      const req = {
-        connection: { localAddress: 'a', remoteAddress: 'a' },
-        ip: '127.0.0.1',
-        protocol: 'http',
-        body: { id: '123', from: '+456', text: 'gidday' }
-      };
-      sinon.stub(config, 'get').returns({ africas_talking: { allowed_ips: [ '127.0.0.1' ] } });
-      sinon.stub(messaging, 'processIncomingMessages').resolves();
-      sinon.stub(serverUtils, 'error');
-      return controller.incomingMessages(req, res).then(() => {
-        chai.expect(serverUtils.error.callCount).to.equal(0);
-        chai.expect(res.json.callCount).to.equal(1);
-      });
-    });
-
   });
 
   describe('deliveryReports', () => {
 
-    it('returns error when ip not allowed', () => {
+    it('returns error when key does not match', () => {
+      const req = { query: { key: 'wrong' } };
+      sinon.stub(secureSettings, 'getCredentials').resolves('mykey');
       sinon.stub(serverUtils, 'error').returns();
-      const req = {
-        connection: { localAddress: 'a', remoteAddress: 'b' },
-        protocol: 'http',
-        ip: '1.2.3.4'
-      };
-      controller.deliveryReports(req, res);
-      chai.expect(serverUtils.error.callCount).to.equal(1);
-      chai.expect(res.json.callCount).to.equal(0);
+      return controller.deliveryReports(req, res).then(() => {
+        chai.expect(serverUtils.error.callCount).to.equal(1);
+        chai.expect(res.json.callCount).to.equal(0);
+      });
     });
 
     it('returns error when unknown state', () => {
       sinon.stub(serverUtils, 'error').returns();
       const req = {
-        connection: { localAddress: 'a', remoteAddress: 'b' },
-        ip: '1.2.3.4',
-        protocol: 'https',
+        query: { key: 'mykey' },
         body: { id: '123', status: 'unknown' }
       };
-      sinon.stub(config, 'get').returns({ africas_talking: { allowed_ips: [ '1.2.3.4' ] } });
-      controller.deliveryReports(req, res);
-      chai.expect(serverUtils.error.callCount).to.equal(1);
-      chai.expect(serverUtils.error.args[0][0]).to.deep.equal({
-        code: 400,
-        message: 'Unknown status code: "unknown", gateway message reference: "123"'
+      sinon.stub(secureSettings, 'getCredentials').resolves('mykey');
+      return controller.deliveryReports(req, res).then(() => {
+        chai.expect(serverUtils.error.callCount).to.equal(1);
+        chai.expect(serverUtils.error.args[0][0]).to.deep.equal({
+          code: 400,
+          message: 'Unknown status code: "unknown", gateway message reference: "123"'
+        });
+        chai.expect(res.json.callCount).to.equal(0);
       });
-      chai.expect(res.json.callCount).to.equal(0);
     });
 
     it('passes the message to the service', () => {
       sinon.stub(messaging, 'updateMessageTaskStates').resolves({ saved: 1 });
       const req = {
-        connection: { localAddress: 'a', remoteAddress: 'b' },
-        ip: '1.2.3.4',
-        protocol: 'https',
+        query: { key: 'mykey' },
         body: { id: '123', status: 'Buffered', failureReason: 'none' }
       };
-      sinon.stub(config, 'get').returns({ africas_talking: { allowed_ips: [ '1.2.3.4' ] } });
+      sinon.stub(secureSettings, 'getCredentials').resolves('mykey');
       return controller.deliveryReports(req, res).then(() => {
         chai.expect(messaging.updateMessageTaskStates.callCount).to.equal(1);
         chai.expect(messaging.updateMessageTaskStates.args[0][0]).to.deep.equal([
