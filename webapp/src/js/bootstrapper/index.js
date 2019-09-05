d0fb20a6fc5a9212be7468631e575875b84a9fcd
Diana Barsan <barsan@medicmobile.org>
2019-09-05 09:14:31 +0300

    Missed a quote.

--- webapp/src/js/bootstrapper/index.js
+++ webapp/src/js/bootstrapper/index.js
@@ -155,7 +155,7 @@
   const setUiError = err => {
     const errorMessage = translator.translate(err && err.key || 'ERROR_MESSAGE');
     const tryAgain = translator.translate('TRY_AGAIN');
-    $('.bootstrap-layer .error').html('<div><p">' + errorMessage + '</p><a id="btn-reload" class="btn btn-primary" href="#">' + tryAgain + '</a></div>');
+    $('.bootstrap-layer .error').html('<div><p>' + errorMessage + '</p><a id="btn-reload" class="btn btn-primary" href="#">' + tryAgain + '</a></div>');
     $('#btn-reload').click(() => window.location.reload(false));
     $('.bootstrap-layer .loader, .bootstrap-layer .status').hide();
     $('.bootstrap-layer .error').show();
