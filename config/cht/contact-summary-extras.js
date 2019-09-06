5905dbdf46dace6ceca3019a2c24ab700b68dfe5
Binod Adhikary <adhikary@medicmobile.org>
2019-09-07 01:54:54 +0545

    Changed getField method and added tests

--- config/cht/contact-summary-extras.js
+++ config/cht/contact-summary-extras.js
@@ -55,7 +55,7 @@ const pncDangerSigns = [
 
 const getField = (report, fieldPath) => ['fields', ...(fieldPath || '').split('.')]
   .reduce((prev, fieldName) => {
-    if (prev === undefined) return report;
+    if (prev === undefined) return undefined;
     return prev[fieldName];
   }, report);
 
--- config/cht/nools-extras.js
+++ config/cht/nools-extras.js
@@ -15,7 +15,7 @@ function isAlive(contact) {
 
 const getField = (report, fieldPath) => ['fields', ...(fieldPath || '').split('.')]
   .reduce((prev, fieldName) => {
-    if (prev === undefined) return report;
+    if (prev === undefined) return undefined;
     return prev[fieldName];
   }, report);
 
