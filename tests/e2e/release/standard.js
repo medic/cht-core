9e4ed178ee6fdd19b68dc415d98b7938b32c5de1
Nick <emailfornc@gmail.com>
2019-09-16 15:34:33 -0400

    added a way to select a element from the left hand side rows on contacts, clearing the contact search box before using, added method to add a clinic, added release tests for creating a new district, health center, and clinic

--- tests/e2e/release/standard.js
+++ tests/e2e/release/standard.js
@@ -1,8 +1,37 @@
 const helper = require('../../helper');
+const utils = require('../../utils');
 const commonElements = require('../../page-objects/common/common.po.js');
 const contactPage = require('../../page-objects/contacts/contacts.po.js');
+const uuid = require('uuid');
+const districtId = uuid.v4();
 
 describe('Creating contacts with standard config', function() {
+  beforeAll(done => {
+    console.log('------------------before all------------');
+    return Promise.all([
+      utils.saveDocs(expectedDocs)
+    ])
+    .then(() => done()).catch(done.fail);
+  });
+  
+  const expectedDocs = [
+    {
+      _id: districtId,
+      name: 'ADistrict',
+      type: 'district_hospital',
+      reported_date: Date.now(),
+    },
+    {
+      _id: uuid.v4,
+      parent: {
+        _id: districtId
+      },
+      name: 'AHealthCenter',
+      type: 'health_center',
+      reported_date: Date.now(),
+    }
+  ];
+  
   it('should create a new district hospital', async function() {
     const districtName = 'Test District';
     await commonElements.goToPeople();
@@ -10,4 +39,22 @@ describe('Creating contacts with standard config', function() {
     helper.waitUntilReady(contactPage.contactName);
     expect(contactPage.contactName.getText()).toBe(districtName);
   });
+
+  it('should create a new health center', async function(){
+    await commonElements.goToPeople();
+    contactPage.selectLHSRowByText(expectedDocs[0].name);
+    const hcName = 'Health Center 1';
+    contactPage.addHealthCenter(hcName);
+    helper.waitUntilReady(contactPage.contactName);
+    expect(contactPage.contactName.getText()).toBe(hcName);
+  });
+
+  it('should create a new clinic', async function(){
+    await commonElements.goToPeople();
+    contactPage.selectLHSRowByText(expectedDocs[1].name);
+    contactPage.addClinic();
+    helper.waitUntilReady(contactPage.contactName);
+    expect(contactPage.contactName.getText()).toBe('Clinic 1');
+  });
 });
+
