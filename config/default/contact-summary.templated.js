a57f6ff359d8d8028f1f604391f98d393010d5a0
Binod Adhikary <adhikary@medicmobile.org>
2019-09-24 01:05:28 +0545

    Fixed some translations in contact-summary.templated.js

--- config/default/contact-summary.templated.js
+++ config/default/contact-summary.templated.js
@@ -21,7 +21,7 @@ const fields = [
   { appliesToType: 'person', label: 'patient_id', value: thisContact.patient_id, width: 4 },
   { appliesToType: 'person', label: 'contact.age', value: thisContact.date_of_birth, width: 4, filter: 'age' },
   { appliesToType: 'person', label: 'contact.sex', value: 'contact.sex.' + thisContact.sex, translate: true, width: 4 },
-  { appliesToType: 'person', label: 'contact.phone', value: thisContact.phone, width: 4 },
+  { appliesToType: 'person', label: 'person.field.phone', value: thisContact.phone, width: 4 },
   { appliesToType: 'person', label: 'person.field.alternate_phone', value: thisContact.phone_alternate, width: 4 },
   { appliesToType: 'person', label: 'External ID', value: thisContact.external_id, width: 4 },
   { appliesToType: 'person', label: 'contact.parent', value: thisLineage, filter: 'lineage' },
