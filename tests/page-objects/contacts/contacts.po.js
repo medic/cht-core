f678076b39d12e5a36a381363c04e8e4f115164f
Nick Christenson <10531066+newtewt@users.noreply.github.com>
2019-09-23 14:09:23 -0400

    Automating a release test for creating districts, health centers, and clinics.  (#5952)
    Automating this manual release test.
    
    medic/medic-release-testing#31

--- tests/page-objects/contacts/contacts.po.js
+++ tests/page-objects/contacts/contacts.po.js
@@ -12,11 +12,18 @@ const submitButton = element(by.css('.btn.submit.btn-primary.ng-scope'));
 const newPersonButton = element(by.css('[name="/data/init/create_new_person"][value="new_person"]'));
 const writeName = element(by.css('[name="/data/district_hospital/is_name_generated"][value="false"]'));
 const contactName = element(by.css('.heading-content'));
+const rows = element.all(by.className('content-row'));
 
 module.exports = {
-
   contactName,
   getSubmitButton: () => submitButton,
+  selectLHSRowByText: text => {
+    module.exports.search(text);
+    helper.waitUntilReady(rows.last());
+    module.exports.clickRowByName(text);
+    helper.waitUntilReady(contactName);
+    expect(contactName.getText()).toBe(text);
+  },
 
   addNewDistrict: async districtName => {
     helper.waitUntilReady(newDistrictButton);
@@ -32,7 +39,7 @@ module.exports = {
     await submitButton.click();
   },
 
-  addHealthCenter: () => {
+  addHealthCenter: (name = 'Mavuvu Clinic') => {
     const newHealthCenterButton = element(by.css('[href$="/add/health_center"]'));
     helper.waitUntilReady(newHealthCenterButton);
     helper.clickElement(newHealthCenterButton);
@@ -43,9 +50,27 @@ module.exports = {
     const writeNameHC = element(by.css('[name="/data/health_center/is_name_generated"][value="false"]'));
     helper.waitElementToBeVisible(writeNameHC);
     writeNameHC.click();
-    newPlaceName.sendKeys('Mavuvu Clinic');
+    newPlaceName.sendKeys(name);
     element(by.css('[name="/data/health_center/external_id"]')).sendKeys('1234457');
     element(by.css('[name="/data/health_center/notes"]')).sendKeys('some notes');
+    submitButton.click();
+  },
+
+  addClinic: (name = 'Clinic 1') => {
+    const newClinicButton = element(by.css('[href$="/add/clinic"]'));
+    helper.waitUntilReady(newClinicButton);
+    helper.waitElementToDisappear(by.id('snackbar'));
+    helper.clickElement(newClinicButton);
+    helper.waitElementToBeVisible(newPersonButton);
+    newPersonButton.click();
+    newPersonTextBox.sendKeys('Todd');
+    nextButton.click();
+    const writeNameHC = element(by.css('[name="/data/clinic/is_name_generated"][value="false"]'));
+    helper.waitElementToBeVisible(writeNameHC);
+    writeNameHC.click();
+    newPlaceName.sendKeys(name);
+    element(by.css('[name="/data/clinic/external_id"]')).sendKeys('1234457');
+    element(by.css('[name="/data/clinic/notes"]')).sendKeys('some notes');
     submitButton.click();
   },
 
@@ -53,8 +78,13 @@ module.exports = {
     refreshButton.click();
   },
 
-  search: query => {
+  search: async query => {
+    searchBox.clear();
     searchBox.sendKeys(query);
-    seachButton.click();
+    await seachButton.click();
   },
+
+  clickRowByName: async name => {
+    await rows.filter(elem => elem.getText().then(text => text === name)).first().click();
+  }
 };
