ac2fc6b88bd93554a7ea4fb108af5a72ea19b93d
Diana Barsan <barsan@medicmobile.org>
2019-09-24 20:52:53 +0300

    Wait for the other eventual sync to complete.

--- tests/page-objects/common/common.po.js
+++ tests/page-objects/common/common.po.js
@@ -63,6 +63,7 @@ module.exports = {
   },
 
   sync: () => {
+    helper.waitElementToPresent(element(by.css('.sync-status .success')));
     module.exports.openMenu();
     openSubmenu('sync now');
     helper.waitElementToPresent(element(by.css('.sync-status .success')));
