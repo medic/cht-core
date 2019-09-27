b19dffc5ccccffa09f9f12627bfe5d5174bd44d2
Nick <emailfornc@gmail.com>
2019-09-27 12:55:57 -0400

    got couchdb setup and working in docker, added a dockerfile for api

--- scripts/setup_couch.sh
+++ scripts/setup_couch.sh
@@ -1,7 +1,5 @@
-curl -X PUT $1/_users 
-curl -X PUT $1/_replicator
-curl -X PUT $1/_global_changes
-curl -X POST $1/_users -H "Content-Type: application/json" -d '{"_id": "org.couchdb.user:admin", "name": "admin", "password":"pass", "type":"user", "roles":[]}' 
+curl $1/_cluster_setup -H 'Content-Type: application/json' --data-binary '{"action":"enable_single_node","username":"admin","password":"pass","bind_address":"0.0.0.0","port":5984,"singlenode":true}'
 curl -X PUT --data '"true"' $1/_node/couchdb@127.0.0.1/_config/chttpd/require_valid_user
 curl -X PUT --data '"4294967296"' $1/_node/couchdb@127.0.0.1/_config/httpd/max_http_request_size
 curl -X PUT $1/medic
+curl -X PUT $1/_node/couchdb@127.0.0.1/_config/httpd/WWW-Authenticate -H 'Content-Type: application/json' -d '"Basic realm='\''administrator'\''"'
