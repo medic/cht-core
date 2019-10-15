9a016a5d0a5bba76949aea27bf59cd8c0b0abaa9
Marc Abbyad <marc@medicmobile.org>
2019-10-15 11:45:52 -0400

    Fixes for ANC reference app (#6018)
    Removes an incorrect target, fixes death reporting by including the transition, and updates a couple target titles
    
    #6016
     #6017
    
    (cherry picked from commit 2c25f3927be6d71715808a6c974d5ed647403065)

--- config/default/test/targets/deaths.spec.js
+++ config/default/test/targets/deaths.spec.js
@@ -44,31 +44,4 @@ describe('Death related targets tests', () => {
     expect(birthsThisMonth[0]).to.nested.not.include({ 'value.pass': 1, 'value.total': 1 });
   });
 
-  it('newborn mortality target should count baby deceased at age: 1 day', async () => {
-    //await harness.setNow('2000-04-30');//DOB: 2000-04-24, DOD: 2000-04-25
-    clock = sinon.useFakeTimers(moment('2000-05-01').toDate());
-    harness.state.contacts.push(babyDeceasedAtAge1Day);
-    let birthsThisMonth = await harness.getTargets({ type: 'newborn-mortality' });
-    expect(birthsThisMonth).to.have.property('length', 1);
-    expect(birthsThisMonth[0]).to.nested.include({ 'value.pass': 1, 'value.total': 1 });
-  });
-
-  it('newborn mortality target should also count baby deceased at age: 1825 days', async () => {
-    //await harness.setNow('2000-04-30');//DOB: 1999-04-27, DOD: 2000-04-25 (1826 days ~ 5 years)
-    clock = sinon.useFakeTimers(moment('2000-04-25').toDate());
-    harness.state.contacts.push(babyDeceasedAtAgeJustUnder5Years);
-    let birthsThisMonth = await harness.getTargets({ type: 'newborn-mortality' });
-    expect(birthsThisMonth).to.have.property('length', 1);
-    expect(birthsThisMonth[0]).to.nested.include({ 'value.pass': 1, 'value.total': 1 });//Also babyDeceasedAtAge1Day
-  });
-
-  it('newborn mortality target should not count baby deceased at age: 1826 days', async () => {
-    //await harness.setNow('2000-04-30');//DOB: 1999-04-26, DOD: 2000-04-25 (1826 days ~ 5 years)
-    clock = sinon.useFakeTimers(moment('2000-04-25').toDate());
-    harness.state.contacts.push(babyDeceasedAtAge5Years);
-    let birthsThisMonth = await harness.getTargets({ type: 'newborn-mortality' });
-    expect(birthsThisMonth).to.have.property('length', 1);
-    expect(birthsThisMonth[0]).to.nested.not.include({ 'value.pass': 1, 'value.total': 1 });
-  });
-
 });
