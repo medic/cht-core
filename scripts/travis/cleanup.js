1e4ae7b46d2264954a4a1106beadd804f8adf41f
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2019-12-02 21:48:29 +1300

    Clean up branch and beta releases
    medic/cht-core#5129

--- scripts/travis/cleanup.js
+++ scripts/travis/cleanup.js
@@ -1,4 +1,4 @@
-/*
+/**
  * Delete old artefacts from the testing db
  */
 const { UPLOAD_URL, BUILDS_SERVER, TRAVIS_BUILD_NUMBER } = process.env;
@@ -6,18 +6,25 @@ const BUILDS_TO_KEEP = 50;
 const END_BUILD_TO_DELETE = TRAVIS_BUILD_NUMBER - BUILDS_TO_KEEP;
 const END_KEY = `medic:medic:test-${END_BUILD_TO_DELETE}`;
 const MAX_BUILDS_TO_DELETE = 50;
+const DAYS_TO_KEEP = 100;
+
 const PouchDB = require('pouchdb-core');
 PouchDB.plugin(require('pouchdb-adapter-http'));
+PouchDB.plugin(require('pouchdb-mapreduce'));
 
 const db = new PouchDB(`${UPLOAD_URL}/${BUILDS_SERVER}`);
 
-const get = () => {
-  console.log('Getting builds...');
+process.on('unhandledRejection', error => {
+  console.error('unhandledRejection', error);
+});
+
+const getTestingBuilds = () => {
+  console.log('Getting old testing builds...');
   return db.allDocs({ endkey: END_KEY, limit: MAX_BUILDS_TO_DELETE });
 };
 
-const markDeleted = response => {
-  return response.rows
+const remove = response => {
+  const docs = response.rows
     .filter(row => row.id.startsWith('medic:medic:'))
     .map(row => {
       return {
@@ -26,11 +33,8 @@ const markDeleted = response => {
         _deleted: true
       };
     });
-};
-
-const remove = docs => {
   if (docs.length) {
-    console.log(`Deleting the oldest ${docs.length} builds...`);
+    console.log(`Deleting ${docs.length} builds...`);
     return db.bulkDocs(docs).then(results => {
       const failed = results.filter(result => !result.ok);
       if (failed.length) {
@@ -42,13 +46,60 @@ const remove = docs => {
   }
 };
 
-process.on('unhandledRejection', error => {
-  console.error('unhandledRejection', error);
-});
+const getEndDate = () => {
+  const d = new Date();
+  d.setDate(d.getDate() - DAYS_TO_KEEP);
+  return d.toISOString();
+};
 
-get()
-  .then(docs => markDeleted(docs))
-  .then(docs => remove(docs))
+const getBranchBuilds = () => {
+  console.log('Querying for old branches...');
+  return db.query('builds/releases', {
+    startkey: [ 'branch', 'medic', 'medic' ],
+    endkey: [ 'branch', 'medic', 'medic', getEndDate() ],
+    limit: MAX_BUILDS_TO_DELETE
+  });
+};
+
+const getBetaBuilds = () => {
+  console.log('Querying for old beta releases...');
+  return db.query('builds/releases', {
+    startkey: [ 'beta', 'medic', 'medic', 10000 ],
+    endkey: [ 'beta', 'medic', 'medic', 0 ],
+    descending: true,
+    limit: MAX_BUILDS_TO_DELETE,
+    skip: 5 // leave the last 5 beta releases
+  });
+};
+
+const getRevs = response => {
+  console.log('Getting revs...');
+  const ids = response.rows
+    .filter(row => row.id.startsWith('medic:medic:'))
+    .map(row => row.id);
+  return db.allDocs({ keys: ids });
+};
+
+const testingBuilds = () => {
+  return getTestingBuilds()
+    .then(remove);
+};
+
+const branchBuilds = () => {
+  return getBranchBuilds()
+    .then(getRevs)
+    .then(remove);
+};
+
+const betaBuilds = () => {
+  return getBetaBuilds()
+    .then(getRevs)
+    .then(remove);
+};
+
+testingBuilds()
+  .then(branchBuilds)
+  .then(betaBuilds)
   .catch(err => {
     console.error(`Error deleting old travis builds: "${err.message}"`);
     process.exit(1);
