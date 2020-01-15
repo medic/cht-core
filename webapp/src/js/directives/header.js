92e0b834988dd6c815e02ccbf20cdbd117feb215
Peter Ochieng <peteroochieng@gmail.com>
2020-01-15 17:59:54 +0300

    display tab labels on mobile if tabs are less than three

--- webapp/src/js/directives/header.js
+++ webapp/src/js/directives/header.js
@@ -1,3 +1,4 @@
+const responsive = require('../modules/responsive');
 angular.module('inboxDirectives').directive('mmHeader', function() {
   return {
     restrict: 'E',
@@ -51,27 +52,14 @@ angular.module('inboxDirectives').directive('mmHeader', function() {
         DBSync.sync(true);
       };
 
-      const tabs = [{name:'messages',state:'messages.detail',icon:'fa-envelope',translation:'Messages',
-        permissions:['can_view_messages','can_view_messages_tab'],typeName:'message'},
-      {name:'tasks',state:'tasks.detail',icon:'fa-flag',translation:'Tasks',
-        permissions:['can_view_tasks','can_view_tasks_tab']},
-      {name:'reports',state:'reports.detail',icon:'fa-list-alt',translation:'Reports',
-        permissions:['can_view_reports','can_view_reports_tab'],typeName:'report'},
-      {name:'contacts',state:'contacts.detail',icon:'fa-user',translation:'Contacts',
-        permissions:['can_view_contacts','can_view_contacts_tab']},
-      {name:'analytics',state:'analytics',icon:'fa-bar-chart-o',translation:'Analytics',
-        permissions:['can_view_analytics','can_view_analytics_tab']}];
-
-      const hasAuthorization = permission => {
-        return Auth(permission)
-          .then(() => true)
-          .catch(() => false);
-      };
-
-      $q.all(tabs.map(tab => hasAuthorization(tab.permissions))).then(results => {
-        ctrl.permittedTabs = tabs.filter((tab,index) => results[index]);
-      });
-
+      $scope.tabs = [{name:'messages',state:'messages.detail',icon:'fa-envelope',translate:'Messages'},
+        {name:'tasks',state:'tasks.detail',icon:'fa-flag',translate:'Tasks'},
+        {name:'reports',state:'reports.detail',icon:'fa-list-alt',translate:'Reports'},
+        {name:'contacts',state:'contacts.detail',icon:'fa-user',translate:'Contacts'},
+        {name:'analytics',state:'analytics',icon:'fa-bar-chart-o',translate:'Analytics'}];
+        
+      $scope.isMobile = responsive.isMobile();
+      
       $scope.$on('$destroy', unsubscribe);
     },
     controllerAs: 'headerCtrl',
