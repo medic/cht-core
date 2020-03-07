6746420f59c5e6a7233cd77121610dfc986fa235
Gareth Bowen <gareth@medicmobile.org>
2020-03-07 10:11:38 +0545

    fix tests

--- webapp/tests/karma/unit/services/modal.js
+++ webapp/tests/karma/unit/services/modal.js
@@ -1,30 +1,33 @@
-describe('Modal service', function() {
+describe('Modal service', () => {
 
   'use strict';
 
   let service;
   let uibModalOpen;
 
-  beforeEach(function() {
+  beforeEach(() => {
     module('inboxApp');
     uibModalOpen = sinon.stub();
-    module(function ($provide) {
-      $provide.factory('$uibModal', function() {
+    module($provide => {
+      $provide.factory('$uibModal', () => {
         return { open: uibModalOpen };
       });
     });
-    inject(function(_Modal_) {
+    inject(_Modal_ => {
       service = _Modal_;
     });
   });
 
-  it('passed args to uibModal', function() {
+  it('passed args to uibModal', () => {
     const options = {
       templateUrl: 'url',
       controller: 'controller',
       model: 123
     };
-    uibModalOpen.returns({ result: 'result' });
+    uibModalOpen.returns({
+      result: 'result',
+      closed: { then: sinon.stub() }
+    });
     service(options);
 
     chai.expect(uibModalOpen.called).to.equal(true);
@@ -34,15 +37,15 @@ describe('Modal service', function() {
     chai.expect(actual.scope.model).to.equal(123);
   });
 
-  it('closes previous modal if singleton is set', function() {
+  it('second identical modal does not open', () => {
     const options = {
       templateUrl: 'url',
       controller: 'controller',
     };
-    const firstModal = { close: sinon.stub() };
-    const secondModal = { close: sinon.stub() };
-    uibModalOpen.onCall(0).returns(firstModal);
-    uibModalOpen.onCall(1).returns(secondModal);
+    uibModalOpen.onCall(0).returns({
+      result: 'result',
+      closed: { then: sinon.stub() }
+    });
 
     // first call
     service(options);
@@ -50,8 +53,73 @@ describe('Modal service', function() {
 
     // second call
     service(options);
-    chai.expect(firstModal.close.callCount).to.equal(1);
-    chai.expect(secondModal.close.callCount).to.equal(0);
+    chai.expect(uibModalOpen.callCount).to.equal(1);
+  });
+
+  it('different modal does open', () => {
+    const options1 = {
+      templateUrl: 'url1',
+      controller: 'controller',
+    };
+    const options2 = {
+      templateUrl: 'url2',
+      controller: 'controller',
+    };
+    const result1 = {
+      result: 'result1',
+      close: sinon.stub(),
+      closed: { then: sinon.stub() }
+    };
+    const result2 = {
+      result: 'result2',
+      close: sinon.stub(),
+      closed: { then: sinon.stub() }
+    };
+    uibModalOpen.onCall(0).returns(result1);
+    uibModalOpen.onCall(1).returns(result2);
+
+    // first call
+    service(options1);
+    chai.expect(uibModalOpen.callCount).to.equal(1);
+
+    // second call
+    service(options2);
+    chai.expect(uibModalOpen.callCount).to.equal(2);
+    
+    chai.expect(result1.close.callCount).to.equal(0);
+    chai.expect(result2.close.callCount).to.equal(0);
+  });
+
+  it('second identical modal opens if first modal is closed first', () => {
+    const options = {
+      templateUrl: 'url',
+      controller: 'controller',
+    };
+    let closeCallback;
+    const result1 = {
+      result: 'result1',
+      close: sinon.stub(),
+      closed: { then: cb => { closeCallback = cb; } }
+    };
+    const result2 = {
+      result: 'result2',
+      close: sinon.stub(),
+      closed: { then: sinon.stub() }
+    };
+    uibModalOpen.onCall(0).returns(result1);
+    uibModalOpen.onCall(1).returns(result2);
+
+    // first call
+    service(options);
+
+    // fire the close callback
+    closeCallback();
+
+    // second call
+    service(options);
+    chai.expect(result1.close.callCount).to.equal(0);
+    chai.expect(result2.close.callCount).to.equal(0);
     chai.expect(uibModalOpen.callCount).to.equal(2);
   });
+
 });
