c68c5fd2497e43201eac87cfbed8efab2ac811ea
Willy Tekeu <willy.tekeu@gmail.com>
2020-05-05 05:49:41 -0400

    Optimizes the way we process changes (from changes feed) (#6201)
    We listen to changes, and as soon as we reach a maximum of 100 changes in the queue, we stop listening. Once the queue is drained, we resume listening.
    
    #6108

--- sentinel/src/lib/feed.js
+++ sentinel/src/lib/feed.js
@@ -1,16 +1,30 @@
+const async = require('async');
 const logger = require('./logger');
 const db = require('../db');
 const metadata = require('./metadata');
 const tombstoneUtils = require('@medic/tombstone-utils');
+const transitionsLib = require('../config').getTransitionsLib();
 
 // we don't run transitions on ddocs or info docs
 const IDS_TO_IGNORE = /^_design\/|-info$/;
 
 const RETRY_TIMEOUT = 60000; // 1 minute
+const PROGRESS_REPORT_INTERVAL = 500; // items
+const MAX_QUEUE_SIZE = 100;
 
-let listener;
-let init;
 let request;
+let processed = 0;
+let lastSeq;
+
+const enqueue = change => changeQueue.push(change);
+
+const updateMetadata = (change, callback) => {
+  processed++;
+  metadata
+    .update(change.seq)
+    .then(() => callback())
+    .catch(callback);
+};
 
 const getProcessedSeq = () => {
   return metadata
@@ -22,25 +36,119 @@ const getProcessedSeq = () => {
 };
 
 const registerFeed = seq => {
-  logger.info(`transitions: fetching changes feed, starting from ${seq}`);
+  logger.debug(`transitions: fetching changes feed, starting from ${seq}`);
   request = db.medic
     .changes({ live: true, since: seq })
     .on('change', change => {
       if (!change.id.match(IDS_TO_IGNORE) &&
           !tombstoneUtils.isTombstoneId(change.id)) {
-        listener(change);
+
+        const queueSize = changeQueue.length();
+        if (queueSize >= MAX_QUEUE_SIZE) {
+          logger.debug(`transitions: queue size ${queueSize} greater than ${MAX_QUEUE_SIZE}, we stop listening`);
+          lastSeq = change.seq;
+          request.cancel();
+          request = null;
+        }
+
+        enqueue(change);
       }
     })
     .on('error', err => {
       logger.error('transitions: error from changes feed: %o', err);
-      init = null;
+      request = null;
       setTimeout(() => listen(), RETRY_TIMEOUT);
     });
+  return request;
 };
 
+const deleteReadDocs = change => {
+  // we don't know if the deleted doc was a report or a message so
+  // attempt to delete both
+  const possibleReadDocIds = ['report', 'message'].map(
+    type => `read:${type}:${change.id}`
+  );
+
+  return db.allDbs().then(dbs => {
+    const userDbs = dbs.filter(dbName => dbName.indexOf(`${db.medicDbName}-user-`) === 0);
+    return userDbs.reduce((p, userDb) => {
+      return p.then(() => {
+        const metaDb = db.get(userDb);
+        return metaDb
+          .allDocs({ keys: possibleReadDocIds })
+          .then(results => {
+            const row = results.rows.find(row => !row.error);
+            if (!row) {
+              return;
+            }
+
+            return metaDb.remove(row.id, row.value.rev).catch(err => {
+              // ignore 404s or 409s - the doc was probably deleted client side already
+              if (err && err.status !== 404 && err.status !== 409) {
+                throw err;
+              }
+            });
+          })
+          .then(() => {
+            db.close(metaDb);
+          })
+          .catch(err => {
+            db.close(metaDb);
+            throw err;
+          });
+      });
+    }, Promise.resolve());
+
+  });
+};
+
+
+const changeQueue = async.queue((change, callback) => {
+  if (!change) {
+    return callback();
+  }
+  logger.debug(`change event on doc ${change.id} seq ${change.seq}`);
+  if (processed > 0 && processed % PROGRESS_REPORT_INTERVAL === 0) {
+    logger.info(
+      `transitions: ${processed} items processed (since sentinel started)`
+    );
+  }
+  if (change.deleted) {
+    // don't run transitions on deleted docs, but do clean up
+    return Promise
+      .all([
+        transitionsLib.infodoc.delete(change),
+        deleteReadDocs(change)
+      ])
+      .catch(err => {
+        logger.error('Error cleaning up deleted doc: %o', err);
+      })
+      .then(() => tombstoneUtils.processChange(Promise, db.medic, change, logger))
+      .then(() => updateMetadata(change, callback))
+      .catch(callback);
+  }
+
+  transitionsLib.processChange(change, err => {
+    if (err) {
+      return callback(err);
+    }
+
+    updateMetadata(change, callback);
+  });
+});
+
+changeQueue.drain(() => {
+  logger.debug(`transitions: queue drained`);
+  listen();
+});
+
 const listen = () => {
-  if (!init) {
-    init = getProcessedSeq().then(seq => registerFeed(seq));
+  if (!request) {
+    if (lastSeq) {
+      return registerFeed(lastSeq);
+    } else {
+      return getProcessedSeq().then(seq => registerFeed(seq));
+    }
   }
 };
 
@@ -51,11 +159,9 @@ module.exports = {
    * automatically on error.
    * @param {Function} callback Called with a change Object.
    */
-  listen: callback => {
+  listen: () => {
     logger.info('transitions: processing enabled');
-    listener = callback;
-    listen();
-    return init;
+    return listen();
   },
 
   /**
@@ -63,16 +169,16 @@ module.exports = {
    * by calling listen.
    */
   cancel: () => {
-    // let initialisation finish but check for null init
-    const p = init || Promise.resolve();
-    return p.then(() => {
-      if (request) {
-        request.cancel();
-      }
-      init = null;
+    changeQueue.kill();
+    if (request) {
+      request.cancel();
       request = null;
-      listener = null;
-    });
-  }
+    }
+  },
 
+  // exposed for testing
+  _changeQueue: changeQueue,
+  _deleteReadDocs: deleteReadDocs,
+  _transitionsLib: transitionsLib,
+  _enqueue: enqueue,
 };
--- sentinel/tests/unit/transitions.js
+++ sentinel/tests/unit/transitions.js
@@ -1,18 +1,13 @@
+const config = require('../../src/config');
+config.initTransitionLib();
+
 const sinon = require('sinon');
 const assert = require('chai').assert;
-const db = require('../../src/db');
 const transitions = require('../../src/transitions');
-const config = require('../../src/config');
-const metadata = require('../../src/lib/metadata');
 const feed = require('../../src/lib/feed');
-const tombstoneUtils = require('@medic/tombstone-utils');
-
-config.initTransitionLib();
-const infodoc = config.getTransitionsLib().infodoc;
 
 describe('transitions', () => {
   afterEach(() => {
-    transitions._changeQueue.kill();
     sinon.restore();
   });
 
@@ -26,172 +21,6 @@ describe('transitions', () => {
       assert.equal(load.callCount, 1);
       assert.equal(listen.callCount, 0);
       assert.equal(cancel.callCount, 1);
-    });
-
-    it('handles an empty change', done => {
-      sinon.stub(metadata, 'update').resolves();
-      const listen = sinon.stub(feed, 'listen');
-      sinon.stub(transitions._transitionsLib, 'processChange').callsArgWith(1);
-
-      transitions.loadTransitions();
-      listen.args[0][0]();
-
-      transitions._changeQueue.drain(() => {
-        return Promise.resolve().then(() => {
-          assert.equal(transitions._transitionsLib.processChange.callCount, 0);
-          return Promise.resolve().then(() => {
-            assert.equal(metadata.update.callCount, 0);
-            done();
-          });
-        });
-      });
-    });
-
-    it('processes deleted changes through TombstoneUtils to create tombstones', done => {
-      sinon.stub(transitions._transitionsLib, 'loadTransitions').returns();
-      sinon.stub(tombstoneUtils, 'processChange').resolves();
-      sinon.stub(metadata, 'update').resolves();
-      sinon.stub(infodoc, 'delete').resolves();
-      sinon.stub(db, 'allDbs').resolves([]);
-      const listen = sinon.stub(feed, 'listen');
-
-      transitions.loadTransitions();
-
-      assert.equal(listen.callCount, 1);
-      listen.args[0][0]({ id: 'somechange', seq: 55, deleted: true });
-
-      transitions._changeQueue.drain(() => {
-        return Promise.resolve().then(() => {
-          assert.equal(tombstoneUtils.processChange.callCount, 1);
-          assert.deepEqual(tombstoneUtils.processChange.args[0][2], {
-            id: 'somechange',
-            seq: 55,
-            deleted: true,
-          });
-          return Promise.resolve().then(() => {
-            assert.equal(metadata.update.callCount, 1);
-            assert.equal(metadata.update.args[0][0], 55);
-            done();
-          });
-        });
-      });
-    });
-
-    it('does not advance metadata document if creating tombstone fails', done => {
-      sinon.stub(tombstoneUtils, 'processChange').rejects();
-      sinon.stub(metadata, 'update').resolves();
-      sinon.stub(infodoc, 'delete').resolves();
-      sinon.stub(db, 'allDbs').resolves([]);
-      const listen = sinon.stub(feed, 'listen');
-
-      transitions.loadTransitions();
-      listen.args[0][0]({ id: 'somechange', seq: 55, deleted: true });
-
-      transitions._changeQueue.drain(() => {
-        return Promise.resolve().then(() => {
-          assert.equal(tombstoneUtils.processChange.callCount, 1);
-          assert.deepEqual(tombstoneUtils.processChange.args[0][2], {
-            id: 'somechange',
-            seq: 55,
-            deleted: true,
-          });
-          return Promise.resolve().then(() => {
-            assert.equal(metadata.update.callCount, 0);
-            done();
-          });
-        });
-      });
-    });
-
-    it('runs transitions lib over changes', done => {
-      sinon.stub(metadata, 'update').resolves();
-      const listen = sinon.stub(feed, 'listen');
-      sinon.stub(transitions._transitionsLib, 'processChange').callsArgWith(1);
-
-      transitions.loadTransitions();
-      listen.args[0][0]({ id: 'somechange', seq: 55 });
-
-      transitions._changeQueue.drain(() => {
-        return Promise.resolve().then(() => {
-          assert.equal(transitions._transitionsLib.processChange.callCount, 1);
-          assert.deepEqual(transitions._transitionsLib.processChange.args[0][0], { id: 'somechange', seq: 55 });
-          return Promise.resolve().then(() => {
-            assert.equal(metadata.update.callCount, 1);
-            assert.equal(metadata.update.args[0][0], 55);
-            done();
-          });
-        });
-      });
-    });
-
-  });
-
-  describe('deleteReadDocs', () => {
-
-    it('handles missing read doc', () => {
-      db.medicDbName = 'medic';
-      const given = { id: 'abc' };
-      const metaDb = {
-        remove: sinon.stub(),
-        allDocs: sinon.stub().resolves({
-          rows: [
-            { key: 'read:message:abc', error: 'notfound' },
-            { key: 'read:report:abc', error: 'notfound' }
-          ]
-        }),
-      };
-      sinon.stub(db, 'allDbs').resolves([`${db.medicDbName}-user-gareth-meta`]);
-      sinon.stub(db, 'get').returns(metaDb);
-      sinon.stub(db, 'close');
-      return transitions._deleteReadDocs(given).then(() => {
-        assert.equal(db.allDbs.callCount, 1);
-        assert.equal(db.get.callCount, 1);
-        assert.deepEqual(db.get.args[0], [`${db.medicDbName}-user-gareth-meta`]);
-        assert.equal(metaDb.allDocs.callCount, 1);
-        assert.deepEqual(metaDb.allDocs.args[0], [{ keys: ['read:report:abc', 'read:message:abc'] }]);
-        assert.equal(metaDb.remove.callCount, 0);
-        assert.equal(db.close.callCount, 1);
-        assert.deepEqual(db.close.args[0], [metaDb]);
-      });
-    });
-
-    it('deletes read doc for all users', () => {
-      db.medicDbName = 'medic';
-      const given = { id: 'abc' };
-      const metaDb = {
-        allDocs: sinon.stub().resolves({
-          rows: [
-            { key: 'read:message:abc', error: 'notfound' },
-            { key: 'read:report:abc', id: 'read:report:abc', value: { rev: '1-rev' } }
-          ]
-        }),
-        remove: sinon.stub().resolves()
-      };
-      const list = sinon.stub(db, 'allDbs').resolves([
-        `${db.medicDbName}-user-gareth-meta`,
-        `${db.medicDbName}-user-jim-meta`,
-        db.medicDbName, // not a user db - must be ignored
-      ]);
-      const use = sinon.stub(db, 'get').returns(metaDb);
-      sinon.stub(db, 'close');
-      return transitions._deleteReadDocs(given).then(() => {
-        assert.equal(list.callCount, 1);
-        assert.equal(use.callCount, 2);
-        assert.equal(use.args[0][0], `${db.medicDbName}-user-gareth-meta`);
-        assert.equal(use.args[1][0], `${db.medicDbName}-user-jim-meta`);
-        assert.equal(metaDb.allDocs.callCount, 2);
-        assert.equal(metaDb.allDocs.args[0][0].keys.length, 2);
-        assert.equal(metaDb.allDocs.args[0][0].keys[0], 'read:report:abc');
-        assert.equal(metaDb.allDocs.args[0][0].keys[1], 'read:message:abc');
-        assert.equal(metaDb.allDocs.args[1][0].keys.length, 2);
-        assert.equal(metaDb.allDocs.args[1][0].keys[0], 'read:report:abc');
-        assert.equal(metaDb.allDocs.args[1][0].keys[1], 'read:message:abc');
-        assert.equal(metaDb.remove.callCount, 2);
-        assert.deepEqual(metaDb.remove.args[0], ['read:report:abc', '1-rev']);
-        assert.deepEqual(metaDb.remove.args[1], ['read:report:abc', '1-rev']);
-        assert.equal(db.close.callCount, 2);
-        assert.deepEqual(db.close.args, [[metaDb], [metaDb]]);
-      });
     });
 
   });
