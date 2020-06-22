b9164558d58f69d407e3e534af7f63c828174de7
latin-panda <66472237+latin-panda@users.noreply.github.com>
2020-06-22 09:40:35 +0930

    This commit will rename the input's "id" and "name" in attempt to turn off the autocomplete on user name and password fields in the e2e testing.

--- tests/e2e/users/add-user.specs.js
+++ tests/e2e/users/add-user.specs.js
@@ -5,7 +5,7 @@ const addUserModal = require('../../page-objects/users/add-user-modal.po.js');
 
 const addedUser = 'fulltester' + new Date().getTime();
 const fullName = 'Bede Ngaruko';
-const errorMessagePassword = element(by.css('#password ~ .help-block'));
+const errorMessagePassword = element(by.css('#edit-password ~ .help-block'));
 
 describe('Add user  : ', () => {
 
@@ -54,7 +54,7 @@ describe('Add user  : ', () => {
   it('should reject non-matching passwords', () => {
     usersPage.openAddUserModal();
     addUserModal.fillForm('user0', 'Not Saved', '%4wbbygxkgdwvdwT65');
-    element(by.id('password-confirm')).sendKeys('abc');
+    element(by.id('edit-password-confirm')).sendKeys('abc');
     addUserModal.submit();
     expect(errorMessagePassword.getText()).toMatch('Passwords must match');
     element(by.css('button.cancel.close')).click();
