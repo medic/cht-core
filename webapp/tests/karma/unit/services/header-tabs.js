44be124a172bca598c4dadc8c19378d424508a0c
Diana Barsan <barsan@medicmobile.org>
2020-06-25 11:57:20 +0300

    Display default fa icon when configured fa icon doesn t exist.

--- webapp/tests/karma/unit/services/header-tabs.js
+++ webapp/tests/karma/unit/services/header-tabs.js
@@ -11,11 +11,11 @@ describe('HeaderTabs service', () => {
   it('returns default tabs when settings not provided', () => {
     const tabs = service();
     chai.expect(tabs).to.shallowDeepEqual([
-      { name: 'messages', icon: 'fa-envelope' },
-      { name: 'tasks', icon: 'fa-flag' },
-      { name: 'reports', icon: 'fa-list-alt' },
-      { name: 'contacts', icon: 'fa-user' },
-      { name: 'analytics', icon: 'fa-bar-chart-o' },
+      { name: 'messages', defaultIcon: 'fa-envelope', icon: undefined },
+      { name: 'tasks', defaultIcon: 'fa-flag', icon: undefined },
+      { name: 'reports', defaultIcon: 'fa-list-alt', icon: undefined },
+      { name: 'contacts', defaultIcon: 'fa-user', icon: undefined },
+      { name: 'analytics', defaultIcon: 'fa-bar-chart-o', icon: undefined },
     ]);
 
     chai.expect(service({})).to.deep.equal(tabs);
@@ -33,11 +33,11 @@ describe('HeaderTabs service', () => {
 
     const tabs = service({ header_tabs: headerTabsSettings });
     chai.expect(tabs).to.shallowDeepEqual([
-      { name: 'messages', icon: 'fa-envelope', resourceIcon: undefined },
-      { name: 'tasks', icon: 'fa-whatever', resourceIcon: undefined },
-      { name: 'reports', icon: 'fa-list-alt', resourceIcon: 'some-icon' },
-      { name: 'contacts', icon: 'fa-user', resourceIcon: 'one-icon' },
-      { name: 'analytics', icon: 'fa-icon', resourceIcon: 'other-icon' },
+      { name: 'messages', icon: undefined, resourceIcon: undefined, defaultIcon: 'fa-envelope' },
+      { name: 'tasks', icon: 'fa-whatever', resourceIcon: undefined, defaultIcon: 'fa-flag' },
+      { name: 'reports', icon: undefined, resourceIcon: 'some-icon', defaultIcon: 'fa-list-alt' },
+      { name: 'contacts', icon: undefined, resourceIcon: 'one-icon', defaultIcon: 'fa-user' },
+      { name: 'analytics', icon: 'fa-icon', resourceIcon: 'other-icon', defaultIcon: 'fa-bar-chart-o' },
     ]);
 
   });
