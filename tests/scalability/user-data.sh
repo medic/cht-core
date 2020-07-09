ff210206968178e1585c7449495a5e35c56fe6f0
Nick Christenson <emailfornc@gmail.com>
2020-07-09 14:04:06 -0400

    adding ec2 init script

---
+++ tests/scalability/user-data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo mkfs -t ext4 /dev/nvme0n1;
+sudo mount /dev/nvme0n1 /srv;
+sudo chmod 777 /srv;
+cd /srv;
+git clone https://github.com/newtewt/cht-infrastructure.git
+cd cht-infrastructure/self-hosting/prepare-system/ubuntu/;
+mkdir -p /home/medic/self-hosting/main/
+echo DOCKER_COUCHDB_ADMIN_PASSWORD=medicScalability >> /home/medic/self-hosting/main/.env
+echo HA_PASSWORD=medicScalability >> /home/medic/self-hosting/main/.env
+./prepare.sh
