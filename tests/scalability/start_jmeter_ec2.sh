399e4e253d3bc59d1c844358fef566519a2d6fe4
Nick Christenson <emailfornc@gmail.com>
2020-07-09 16:07:56 -0400

    updated run_suite.sh to work with changes of ec2 hosting medic, removed curl_api script, added script to start ec2 for jemeter

---
+++ tests/scalability/start_jmeter_ec2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+sed -i "1s/^/MEDIC_URL=$MEDIC_URL\n/" run_suite.sh
+cat run_suite.sh
+jq '.UserData = "'$(base64 run_suite.sh -w 0)'"' launch-specification.json >> launch-specification-jmeter.json
+cat launch-specification-jmeter.json
+aws ec2 request-spot-instances --spot-price '0.222' --instance-count 1 --type 'one-time' --launch-specification file://launch-specification-jmeter.json --block-duration-minutes 60
