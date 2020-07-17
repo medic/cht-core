f97ae8cf70cfd3ca5997d22cb4e95f1b92e1e983
Nick Christenson <emailfornc@gmail.com>
2020-07-17 09:34:15 -0400

    [skip travis] installing npm locally and executing

--- tests/scalability/start_ec2_medic.sh
+++ tests/scalability/start_ec2_medic.sh
@@ -47,7 +47,7 @@ done
 echo Api Is up
 
 echo installing medic-conf
-npm install https://github.com/medic/medic-conf.git#307_force_arg -g
+npm install https://github.com/medic/medic-conf.git#307_force_arg
 
 MEDIC_CONF_URL=${MEDIC_URL:0:8}medic:medicScalability@${MEDIC_URL:8}
 
@@ -57,8 +57,8 @@ cd ../../config/standard/
 
 echo Uploading settings and seeding data
 echo medic-conf url is $MEDIC_CONF_URL
-which medic-conf
-medic-conf --url=$MEDIC_CONF_URL --force upload-app-settings \
+$(npm bin)/medic-conf
+$(npm bin)/medic-conf --url=$MEDIC_CONF_URL --force upload-app-settings \
     convert-app-forms \
     convert-collect-forms \
     convert-contact-forms \
