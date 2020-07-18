223e7989a98400bd11de923fb39c9edb13ee39d2
latin-panda <66472237+latin-panda@users.noreply.github.com>
2020-07-18 12:26:29 +0930

    Fixing code style

--- api/tests/mocha/controllers/log-docs.spec.js
+++ api/tests/mocha/controllers/log-docs.spec.js
@@ -1,6 +1,5 @@
 const chai = require('chai');
 const sinon = require('sinon');
-
 const logDocsController = require('../../../src/controllers/log-docs');
 const auth = require('../../../src/auth');
 const serverUtils = require('../../../src/server-utils');
@@ -24,32 +23,36 @@ describe('Log Docs Controller', () => {
     sinon.restore();
   });
 
-  describe('get', () => {
+  describe('getReplicationLimitLog', () => {
     it('should throw an error when not authenticated', () => {
       auth.getUserCtx.rejects({ some: 'err' });
 
-      return logDocsController.getReplicationLimitLog(req, res).then(() => {
-        chai.expect(auth.getUserCtx.callCount).to.equal(1);
-        chai.expect(auth.getUserCtx.args[0]).to.deep.equal([req]);
-        chai.expect(res.json.callCount).to.equal(0);
-        chai.expect(logDocsService.getReplicationLimitExceededLog.callCount).to.equal(0);
-        chai.expect(serverUtils.error.callCount).to.equal(1);
-        chai.expect(serverUtils.error.args[0]).to.deep.equal([{ some: 'err' }, req, res, true]);
-      });
+      return logDocsController
+        .getReplicationLimitLog(req, res)
+        .then(() => {
+          chai.expect(auth.getUserCtx.callCount).to.equal(1);
+          chai.expect(auth.getUserCtx.args[0]).to.deep.equal([req]);
+          chai.expect(res.json.callCount).to.equal(0);
+          chai.expect(logDocsService.getReplicationLimitExceededLog.callCount).to.equal(0);
+          chai.expect(serverUtils.error.callCount).to.equal(1);
+          chai.expect(serverUtils.error.args[0]).to.deep.equal([{ some: 'err' }, req, res, true]);
+        });
     });
 
     it('should respond with a log document', () => {
       auth.getUserCtx.resolves(true);
       logDocsService.getReplicationLimitExceededLog.resolves({ some: 'logs' });
 
-      return logDocsController.getReplicationLimitLog(req, res).then(() => {
-        chai.expect(auth.getUserCtx.callCount).to.equal(1);
-        chai.expect(auth.getUserCtx.args[0][0]).to.deep.equal(req);
-        chai.expect(serverUtils.error.callCount).to.equal(0);
-        chai.expect(logDocsService.getReplicationLimitExceededLog.callCount).to.equal(1);
-        chai.expect(res.json.callCount).to.equal(1);
-        chai.expect(res.json.args[0][0]).to.deep.equal({ some: 'logs' });
-      });
+      return logDocsController
+        .getReplicationLimitLog(req, res)
+        .then(() => {
+          chai.expect(auth.getUserCtx.callCount).to.equal(1);
+          chai.expect(auth.getUserCtx.args[0][0]).to.deep.equal(req);
+          chai.expect(serverUtils.error.callCount).to.equal(0);
+          chai.expect(logDocsService.getReplicationLimitExceededLog.callCount).to.equal(1);
+          chai.expect(res.json.callCount).to.equal(1);
+          chai.expect(res.json.args[0][0]).to.deep.equal({ some: 'logs' });
+        });
     });
   });
 });
