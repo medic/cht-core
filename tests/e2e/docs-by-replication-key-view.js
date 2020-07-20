c780853306fb0f846d18f22ced62ff50642dae8c
Njuguna Ndung'u <njgndungu@yahoo.com>
2020-07-20 20:11:59 +0300

    fix promise

--- tests/e2e/docs-by-replication-key-view.js
+++ tests/e2e/docs-by-replication-key-view.js
@@ -330,7 +330,9 @@ describe('view docs_by_replication_key', () => {
 
     console.log(`Pushing ${alldocs.length} documents for testing…`);
     return utils.saveDocs(alldocs)
-      .then(getChanges(['_all', 'testuser', 'testplace', 'testpatient', 'testuserplace', 'org.couchdb.user:username']))
+      .then(() => {
+        getChanges(['_all', 'testuser', 'testplace', 'testpatient', 'testuserplace', 'org.couchdb.user:username']);
+      })
       .then(docs => {
         docByPlaceIds = docs;
         getChanges(['_all', '_unassigned', 'testuser', 'testplace', 'testpatient', 'testuserplace' ]);
