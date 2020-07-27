192e1b5fddf3f3effebd33b3a054bc1ddf083781
Nick Christenson <emailfornc@gmail.com>
2020-07-27 14:41:12 -0400

    [skip travis] passing arg to jq instead of trying to use in string

--- tests/scalability/start_jmeter_ec2.sh
+++ tests/scalability/start_jmeter_ec2.sh
@@ -5,6 +5,6 @@ echo $MEDIC_URL
 sed -i '2s~^~'MEDIC_URL=$MEDIC_URL'\n~' run_suite.sh
 cat run_suite.sh
 # base64 encode our script and set as value for UserDAta in launch-specification
- jq '.UserData = "'$(base64 run_suite.sh -w 0)'" | .IamInstanceProfile.Arn = "${{ secrets.SCALABILITY_ARN }}"' launch-specification.json >> launch-specification-jmeter.json
+ jq --arg arn ${{ secrets.SCALABILITY_ARN }} '.UserData = "'$(base64 run_suite.sh -w 0)'" | .IamInstanceProfile.Arn = $arn' launch-specification.json >> launch-specification-jmeter.json
 cat launch-specification-jmeter.json
 aws ec2 request-spot-instances --spot-price '0.222' --instance-count 1 --type 'one-time' --launch-specification file://launch-specification-jmeter.json --block-duration-minutes 60
