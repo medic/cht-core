faf56249a8399385f064e87589e18d02f18398d6
Nick Christenson <emailfornc@gmail.com>
2020-07-29 13:44:24 -0400

    installing the npm released medic-conf

--- tests/scalability/start_ec2_medic.sh
+++ tests/scalability/start_ec2_medic.sh
@@ -63,7 +63,7 @@ echo installing pyxform
 sudo python -m pip install git+https://github.com/medic/pyxform.git@medic-conf-1.17#egg=pyxform-medic -q
 
 echo installing medic-conf
-npm install https://github.com/medic/medic-conf.git#master
+npm install medic-conf
 
 # echo Uploading settings and seeding data
 echo medic-conf url is $MEDIC_CONF_URL
