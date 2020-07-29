cb40892e3037a5b243af0a0dd51b1cef2b0be15c
Nick Christenson <emailfornc@gmail.com>
2020-07-29 12:38:05 -0400

    updating to work with beta tags

--- tests/scalability/start_jmeter_ec2.sh
+++ tests/scalability/start_jmeter_ec2.sh
@@ -6,6 +6,7 @@ echo $GITHUB_RUN_ID
 
 sed -i '2s~^~'MEDIC_URL=$MEDIC_URL'\n~' run_suite.sh
 sed -i '2s~^~'S3_PATH=s3://medic-e2e/scalability/$GITHUB_RUN_ID'\n~' run_suite.sh
+sed -i '2s~^~'TAG_NAME=$TAG_NAME'\n~' run_suite.sh
 cat run_suite.sh
 # base64 encode our script and set as value for UserDAta in launch-specification
 jq --arg arn $SCALABILITY_ARN '.UserData = "'$(base64 run_suite.sh -w 0)'" | .IamInstanceProfile.Arn = $arn' launch-specification.json >> launch-specification-jmeter.json
