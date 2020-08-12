03b2f6df84ef19f40fdda3073b7c8cb1b3e9fe35
Njuguna Ndung'u <njgndungu@yahoo.com>
2020-08-12 11:01:31 +0300

    add db-config controller test

---
+++ api/tests/mocha/controllers/db-config.spec.js
@@ -0,0 +1,38 @@
+const sinon = require('sinon');
+const chai = require('chai');
+
+const auth = require('../../../src/auth');
+const authorization = require('../../../src/services/authorization');
+const controller = require('../../../src/controllers/db-config');
+const serverUtils = require('../../../src/server-utils');
+
+let req;
+// let userCtx;
+let res;
+
+describe.only('DB config Controller', () => {
+  beforeEach(() => {
+    req = {};
+    res = { json: sinon.stub() };
+    sinon.stub(authorization, 'getAuthorizationContext');
+    sinon.stub(authorization, 'getAllowedDocIds');
+    sinon.stub(auth, 'isOnlineOnly');
+    sinon.stub(auth, 'isOffline');
+    sinon.stub(auth, 'hasAllPermissions');
+    sinon.stub(serverUtils, 'error');
+  });
+
+  afterEach(() => sinon.restore());
+
+  it.only('should respond with error when user does not have required permissions', () => {
+    serverUtils.error.resolves();
+    auth.isDbAdmin.returns(false);
+    return controller.getAttachments(req, res).then(() => {
+      chai.expect(auth.isDbAdmin.callCount).to.equal(1);
+      chai.expect(serverUtils.error.args[0])
+        .to.deep.equal([{ code: 403, reason: 'Insufficient privileges' }, req, res]);
+      chai.expect(res.json.callCount).to.equal(0);
+      chai.expect(serverUtils.error.callCount).to.equal(1);
+    });
+  });
+});
