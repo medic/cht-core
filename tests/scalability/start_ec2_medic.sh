83f3bb0b30904b86ed5aac90079ffcb122873fef
Nick Christenson <emailfornc@gmail.com>
2020-08-25 10:22:11 -0400

    [skip travis] changed to trigger on branch push, hard coded the tag, and installing medic-conf master

--- tests/scalability/start_ec2_medic.sh
+++ tests/scalability/start_ec2_medic.sh
@@ -63,7 +63,8 @@ echo installing pyxform
 sudo python -m pip install git+https://github.com/medic/pyxform.git@medic-conf-1.17#egg=pyxform-medic -q
 
 echo installing medic-conf
-npm install medic-conf
+# npm install medic-conf TEMP
+npm install https://github.com/medic/medic-conf.git#master
 
 sleep 10
 # echo Uploading settings and seeding data
@@ -118,7 +119,8 @@ pre_update_seqs+=($(curl $MEDIC_CONF_URL/medic/_design/$ddoc/_info -s -k | jq .v
 done
 
 echo staging updates
-curl $MEDIC_CONF_URL/api/v1/upgrade/stage -k -X POST -H "Content-Type: application/json" -d '{"build":{"namespace":"medic","application":"medic","version":"'$1'"}}'
+#TEMP curl $MEDIC_CONF_URL/api/v1/upgrade/stage -k -X POST -H "Content-Type: application/json" -d '{"build":{"namespace":"medic","application":"medic","version":"'$1'"}}'
+curl $MEDIC_CONF_URL/api/v1/upgrade/stage -k -X POST -H "Content-Type: application/json" -d '{"build":{"namespace":"medic","application":"medic","version":"3.10.0-beta.1"}}'
 
 
 staged=$(curl $MEDIC_CONF_URL/medic/horti-upgrade -s -k | jq .staging_complete -r)
