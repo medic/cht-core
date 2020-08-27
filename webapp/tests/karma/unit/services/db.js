5e40c5c9207f8911e9635be10d1f4f34f3339134
Diana Barsan <barsan@medicmobile.org>
2020-08-27 09:02:55 +0300

    dupe part 2

---
+++ webapp/tests/karma/unit/services/db.js
@@ -0,0 +1,212 @@
+describe('DB service', () => {
+
+  'use strict';
+
+  let getService;
+  let Location;
+  let userCtx;
+  let pouchDB;
+  let expected;
+  let isOnlineOnly;
+
+  beforeEach(() => {
+    Location = {};
+    userCtx = sinon.stub();
+    pouchDB = sinon.stub();
+    expected = {
+      id: 'hello',
+      viewCleanup: sinon.stub(),
+      installValidationMethods: sinon.stub(),
+    };
+    pouchDB.returns(expected);
+
+    isOnlineOnly = sinon.stub();
+    module('inboxApp');
+    module($provide => {
+      $provide.factory('$window', () => {
+        return {
+          angular: { callbacks: [] },
+          PouchDB: {
+            // stub for registering validation plugin
+            plugin: () => {}
+          }
+        };
+      });
+      $provide.factory('pouchDB', () => {
+        return pouchDB;
+      });
+      $provide.value('Session', {
+        userCtx: userCtx,
+        isOnlineOnly: isOnlineOnly
+      } );
+      $provide.value('Location', Location);
+    });
+    inject(($injector, $timeout) => {
+      getService = () => {
+        // delay initialisation of the db service
+        const service = $injector.get('DB');
+        $timeout.flush(1000);
+        return service;
+      };
+    });
+  });
+
+  afterEach(() => {
+    KarmaUtils.restore(pouchDB, userCtx, isOnlineOnly);
+  });
+
+  describe('get remote', () => {
+
+    it('sets skip setup', () => {
+      isOnlineOnly.returns(false);
+      Location.dbName = 'medicdb';
+      Location.url = 'ftp//myhost:21/medicdb';
+      userCtx.returns({ name: 'johnny' });
+
+      // init
+      const service = getService();
+
+      chai.expect(pouchDB.callCount).to.equal(2);
+      chai.expect(pouchDB.args[0][0]).to.equal('medicdb-user-johnny');
+      chai.expect(pouchDB.args[1][0]).to.equal('medicdb-user-johnny-meta');
+
+      // get remote
+      const actual = service({ remote: true });
+      chai.expect(actual.id).to.equal(expected.id);
+      chai.expect(pouchDB.callCount).to.equal(3);
+      chai.expect(pouchDB.args[2][0]).to.equal('ftp//myhost:21/medicdb');
+      chai.expect(pouchDB.args[2][1].skip_setup).to.equal(true);
+
+      // get remote meta
+      service({ remote: true, meta: true });
+      chai.expect(pouchDB.callCount).to.equal(4);
+      chai.expect(pouchDB.args[3][0]).to.equal('ftp//myhost:21/medicdb-user-johnny-meta');
+      chai.expect(pouchDB.args[3][1].skip_setup).to.equal(false);
+    });
+
+    it('caches pouchdb instances', () => {
+      isOnlineOnly.returns(false);
+      Location.dbName = 'medicdb';
+      Location.url = 'ftp//myhost:21/medicdb';
+      userCtx.returns({ name: 'johnny' });
+
+      // init
+      const service = getService();
+
+      chai.expect(pouchDB.callCount).to.equal(2);
+      chai.expect(pouchDB.args[0][0]).to.equal('medicdb-user-johnny');
+      chai.expect(pouchDB.args[1][0]).to.equal('medicdb-user-johnny-meta');
+
+      // get remote
+      const actual1 = service({ remote: true });
+      chai.expect(actual1.id).to.equal(expected.id);
+      chai.expect(pouchDB.callCount).to.equal(3);
+
+      // get remote again
+      const actual2 = service({ remote: true });
+      chai.expect(actual2.id).to.equal(expected.id);
+      chai.expect(pouchDB.callCount).to.equal(3);
+    });
+
+    /**
+     * Escape database names when talking to CouchDB.
+     * Must be kept in sync with medic-api/lib/userDb.js
+     */
+    it('escapes invalid database characters - #3778', () => {
+      isOnlineOnly.returns(false);
+      Location.dbName = 'medicdb';
+      Location.url = 'ftp//myhost:21/medicdb';
+      userCtx.returns({ name: 'johnny.<>^,?!' });
+
+      // init
+      const service = getService();
+
+      chai.expect(pouchDB.callCount).to.equal(2);
+      chai.expect(pouchDB.args[0][0]).to.equal('medicdb-user-johnny.<>^,?!');
+      chai.expect(pouchDB.args[1][0]).to.equal('medicdb-user-johnny.<>^,?!-meta');
+
+      // get remote
+      const actual = service({ remote: true });
+      chai.expect(actual.id).to.equal(expected.id);
+      chai.expect(pouchDB.callCount).to.equal(3);
+      chai.expect(pouchDB.args[2][0]).to.equal('ftp//myhost:21/medicdb');
+
+      // get remote meta
+      service({ remote: true, meta: true });
+      chai.expect(pouchDB.callCount).to.equal(4);
+      chai.expect(pouchDB.args[3][0]).to.equal('ftp//myhost:21/medicdb-user-johnny(46)(60)(62)(94)(44)(63)(33)-meta');
+    });
+  });
+
+  describe('get local', () => {
+
+    it('sets auto compaction', () => {
+      isOnlineOnly.returns(false);
+      Location.dbName = 'medicdb';
+      userCtx.returns({ name: 'johnny' });
+
+      // init
+      const service = getService();
+
+      chai.expect(pouchDB.callCount).to.equal(2);
+      chai.expect(expected.viewCleanup.callCount).to.equal(2);
+
+      // get local
+      const actual = service();
+      chai.expect(pouchDB.callCount).to.equal(2);
+      chai.expect(actual.id).to.equal(expected.id);
+      chai.expect(pouchDB.args[0][0]).to.equal('medicdb-user-johnny');
+      chai.expect(pouchDB.args[0][1].auto_compaction).to.equal(true);
+      chai.expect(pouchDB.args[1][0]).to.equal('medicdb-user-johnny-meta');
+      chai.expect(pouchDB.args[1][1].auto_compaction).to.equal(true);
+      chai.expect(expected.viewCleanup.callCount).to.equal(2);
+    });
+
+    it('caches pouchdb instances', () => {
+      isOnlineOnly.returns(false);
+      Location.dbName = 'medicdb';
+      userCtx.returns({ name: 'johnny' });
+
+      // init
+      const service = getService();
+      chai.expect(pouchDB.callCount).to.equal(2);
+
+      // get local
+      const actual1 = service();
+      chai.expect(pouchDB.callCount).to.equal(2);
+      chai.expect(actual1.id).to.equal(expected.id);
+
+      // get local again
+      const actual2 = service();
+      chai.expect(pouchDB.callCount).to.equal(2);
+      chai.expect(actual2.id).to.equal(expected.id);
+      chai.expect(isOnlineOnly.callCount).to.equal(1);
+      chai.expect(userCtx.callCount).to.equal(4);
+    });
+
+    it('returns remote for admin user', () => {
+      isOnlineOnly.returns(true);
+      userCtx.returns({ name: 'johnny' });
+      Location.url = 'ftp//myhost:21/medicdb';
+
+      // init
+      const service = getService();
+      chai.expect(pouchDB.callCount).to.equal(0);
+      chai.expect(expected.viewCleanup.callCount).to.equal(0);
+
+      // get local returns remote
+      const actual = service();
+      chai.expect(pouchDB.callCount).to.equal(1);
+      chai.expect(actual.id).to.equal(expected.id);
+      chai.expect(pouchDB.args[0][0]).to.equal('ftp//myhost:21/medicdb');
+      chai.expect(expected.viewCleanup.callCount).to.equal(0);
+
+      // get locale  meta returns remote meta
+      service({ meta: true });
+      chai.expect(pouchDB.callCount).to.equal(2);
+      chai.expect(pouchDB.args[1][0]).to.equal('ftp//myhost:21/medicdb-user-johnny-meta');
+    });
+
+  });
+
+});
---
+++ webapp/tests/karma/unit/services/lineage-model-generator.js
@@ -0,0 +1,317 @@
+describe('LineageModelGenerator service', () => {
+
+  'use strict';
+
+  let service;
+  let dbQuery;
+  let dbAllDocs;
+
+  beforeEach(() => {
+    module('inboxApp');
+    module($provide => {
+      dbQuery = sinon.stub();
+      dbAllDocs = sinon.stub();
+      $provide.value('$q', Q); // bypass $q so we don't have to digest
+      $provide.factory('DB', KarmaUtils.mockDB({ query: dbQuery, allDocs: dbAllDocs }));
+    });
+    inject(_LineageModelGenerator_ => service = _LineageModelGenerator_);
+  });
+
+  describe('contact', () => {
+
+    it('handles not found', done => {
+      dbQuery.returns(Promise.resolve({ rows: [] }));
+      service.contact('a')
+        .then(() => {
+          done(new Error('expected error to be thrown'));
+        })
+        .catch(err => {
+          chai.expect(err.message).to.equal('Document not found: a');
+          chai.expect(err.code).to.equal(404);
+          done();
+        });
+    });
+
+    it('handles no lineage', () => {
+      const contact = { _id: 'a', _rev: '1' };
+      dbQuery.returns(Promise.resolve({ rows: [
+        { doc: contact }
+      ] }));
+      return service.contact('a').then(model => {
+        chai.expect(model._id).to.equal('a');
+        chai.expect(model.doc).to.deep.equal(contact);
+      });
+    });
+
+    it('binds lineage', () => {
+      const contact = { _id: 'a', _rev: '1' };
+      const parent = { _id: 'b', _rev: '1' };
+      const grandparent = { _id: 'c', _rev: '1' };
+      dbQuery.returns(Promise.resolve({ rows: [
+        { doc: contact },
+        { doc: parent },
+        { doc: grandparent }
+      ] }));
+      return service.contact('a').then(model => {
+        chai.expect(dbQuery.callCount).to.equal(1);
+        chai.expect(dbQuery.args[0][0]).to.equal('medic-client/docs_by_id_lineage');
+        chai.expect(dbQuery.args[0][1]).to.deep.equal({
+          startkey: [ 'a' ],
+          endkey: [ 'a', {} ],
+          include_docs: true
+        });
+        chai.expect(model._id).to.equal('a');
+        chai.expect(model.doc).to.deep.equal(contact);
+        chai.expect(model.lineage).to.deep.equal([ parent, grandparent ]);
+      });
+    });
+
+    it('binds contacts', () => {
+      const contact = { _id: 'a', _rev: '1', contact: { _id: 'd' } };
+      const contactsContact = { _id: 'd', name: 'dave' };
+      const parent = { _id: 'b', _rev: '1', contact: { _id: 'e' } };
+      const parentsContact = { _id: 'e', name: 'eliza' };
+      const grandparent = { _id: 'c', _rev: '1' };
+      dbQuery.returns(Promise.resolve({ rows: [
+        { doc: contact },
+        { doc: parent },
+        { doc: grandparent }
+      ] }));
+      dbAllDocs.returns(Promise.resolve({ rows: [
+        { doc: contactsContact },
+        { doc: parentsContact }
+      ] }));
+      return service.contact('a', { merge: true }).then(model => {
+        chai.expect(model._id).to.equal('a');
+        chai.expect(model.doc.contact.name).to.equal('dave');
+        chai.expect(model.lineage[0].contact.name).to.equal('eliza');
+      });
+    });
+
+    it('hydrates lineage contacts - #3812', () => {
+      const contact = { _id: 'a', _rev: '1', contact: { _id: 'x' } };
+      const parent = { _id: 'b', _rev: '1', contact: { _id: 'd' } };
+      const grandparent = { _id: 'c', _rev: '1', contact: { _id: 'e' } };
+      const parentContact = { _id: 'd', name: 'donny' };
+      const grandparentContact = { _id: 'e', name: 'erica' };
+      dbQuery.returns(Promise.resolve({ rows: [
+        { doc: contact },
+        { doc: parent },
+        { doc: grandparent }
+      ] }));
+      dbAllDocs.returns(Promise.resolve({ rows: [
+        { doc: parentContact },
+        { doc: grandparentContact }
+      ] }));
+      return service.contact('a').then(model => {
+        chai.expect(dbAllDocs.callCount).to.equal(1);
+        chai.expect(dbAllDocs.args[0][0]).to.deep.equal({
+          keys: [ 'x', 'd', 'e' ],
+          include_docs: true
+        });
+        chai.expect(model.lineage[0].contact).to.deep.equal(parentContact);
+        chai.expect(model.lineage[1].contact).to.deep.equal(grandparentContact);
+      });
+    });
+
+    it('merges lineage when merge passed', () => {
+      const contact = { _id: 'a', name: '1', parent: { _id: 'b', parent: { _id: 'c' } } };
+      const parent = { _id: 'b', name: '2' };
+      const grandparent = { _id: 'c', name: '3' };
+      const expected = {
+        _id: 'a',
+        doc: {
+          _id: 'a',
+          name: '1',
+          parent: {
+            _id: 'b',
+            name: '2',
+            parent: {
+              _id: 'c',
+              name: '3'
+            }
+          }
+        },
+        lineage: [
+          {
+            _id: 'b',
+            name: '2',
+            parent: {
+              _id: 'c',
+              name: '3'
+            }
+          },
+          {
+            _id: 'c',
+            name: '3'
+          }
+        ]
+      };
+      dbQuery.returns(Promise.resolve({ rows: [
+        { doc: contact },
+        { doc: parent },
+        { doc: grandparent }
+      ] }));
+      return service.contact('a', { merge: true }).then(actual => {
+        chai.expect(actual).to.deep.equal(expected);
+      });
+    });
+
+    it('should merge lineage with undefined members', () => {
+      const contact = { _id: 'a', name: '1', parent: { _id: 'b', parent: { _id: 'c', parent: { _id: 'd' } } } };
+      const parent = { _id: 'b', name: '2', parent: { _id: 'c', parent: { _id: 'd' } } };
+      dbQuery.resolves({ rows:
+        [{ doc: contact, key: ['a', 0] }, { doc: parent,  key: ['a', 1] }, { key: ['a', 2] }, { key: ['a', 3] }]
+      });
+      const expected = {
+        _id: 'a',
+        doc: {
+          _id: 'a',
+          name: '1',
+          parent: { _id: 'b', name: '2', parent: { _id: 'c', parent: { _id: 'd' } } }
+        },
+        lineage: [{ _id: 'b', name: '2', parent: { _id: 'c', parent: { _id: 'd' } } }, undefined, undefined]
+      };
+      return service.contact('a', { merge: true }).then(actual => {
+        chai.expect(actual).to.deep.equal(expected);
+      });
+    });
+
+    it('should merge lineage with undefined members v2', () => {
+      const contact = { _id: 'a', name: '1', parent: { _id: 'b', parent: { _id: 'c', parent: { _id: 'd' } } } };
+      const parent = { _id: 'b', name: '2', parent: { _id: 'c', parent: { _id: 'd' } } };
+      dbQuery.resolves({ rows: [
+        { doc: contact, key: ['a', 0] },
+        { doc: parent,  key: ['a', 1] },
+        { key: ['a', 2] },
+        { key: ['a', 3], doc: { _id: 'd', name: '4' } }
+      ] });
+      const expected = {
+        _id: 'a',
+        doc: {
+          _id: 'a',
+          name: '1',
+          parent: { _id: 'b', name: '2', parent: { _id: 'c', parent: { _id: 'd', name: '4' } } },
+        },
+        lineage: [
+          { _id: 'b', name: '2', parent: { _id: 'c', parent: { _id: 'd' } } }, undefined, { _id: 'd', name: '4' }
+        ]
+      };
+      return service.contact('a', { merge: true }).then(actual => {
+        chai.expect(actual).to.deep.equal(expected);
+      });
+    });
+
+    it('does not merge lineage without merge', () => {
+      const contact = { _id: 'a', name: '1', parent: { _id: 'b', parent: { _id: 'c' } } };
+      const parent = { _id: 'b', name: '2' };
+      const grandparent = { _id: 'c', name: '3' };
+      const expected = {
+        _id: 'a',
+        doc: {
+          _id: 'a',
+          name: '1',
+          parent: {
+            _id: 'b',
+            parent: {
+              _id: 'c',
+            },
+          }
+        },
+        lineage: [
+          {
+            _id: 'b',
+            name: '2'
+          },
+          {
+            _id: 'c',
+            name: '3'
+          }
+        ]
+      };
+      dbQuery.returns(Promise.resolve({ rows: [
+        { doc: contact },
+        { doc: parent },
+        { doc: grandparent }
+      ] }));
+      return service.contact('a').then(actual => {
+        chai.expect(actual).to.deep.equal(expected);
+      });
+    });
+  });
+
+  describe('report', () => {
+
+    it('handles not found', done => {
+      dbQuery.returns(Promise.resolve({ rows: [] }));
+      service.report('a')
+        .then(() => {
+          done(new Error('expected error to be thrown'));
+        })
+        .catch(err => {
+          chai.expect(err.message).to.equal('Document not found: a');
+          chai.expect(err.code).to.equal(404);
+          done();
+        });
+    });
+
+    it('handles no lineage', () => {
+      const report = { _id: 'a', _rev: '1' };
+      dbQuery.returns(Promise.resolve({ rows: [
+        { doc: report }
+      ] }));
+      return service.report('a').then(model => {
+        chai.expect(model._id).to.equal('a');
+        chai.expect(model.doc).to.deep.equal(report);
+      });
+    });
+
+    it('binds lineage and contact', () => {
+      const report = { _id: 'a', _rev: '1', type: 'data_record', form: 'a', contact: { _id: 'b' } };
+      const contact = { _id: 'b', _rev: '1' };
+      const parent = { _id: 'c', _rev: '1' };
+      const grandparent = { _id: 'd', _rev: '1' };
+      dbQuery.returns(Promise.resolve({ rows: [
+        { doc: report },
+        { doc: contact },
+        { doc: parent },
+        { doc: grandparent }
+      ] }));
+      return service.report('a').then(model => {
+        chai.expect(model._id).to.equal('a');
+        chai.expect(model.doc).to.deep.equal(report);
+        chai.expect(model.contact).to.deep.equal(contact);
+      });
+    });
+
+    it('hydrates lineage contacts - #3812', () => {
+      const report = { _id: 'a', _rev: '1', type: 'data_record', form: 'a', contact: { _id: 'x' } };
+      const contact = { _id: 'b', _rev: '1', contact: { _id: 'y' } };
+      const parent = { _id: 'c', _rev: '1', contact: { _id: 'e' } };
+      const grandparent = { _id: 'd', _rev: '1', contact: { _id: 'f' } };
+      const parentContact = { _id: 'e', name: 'erica' };
+      const grandparentContact = { _id: 'f', name: 'frank' };
+      dbQuery.returns(Promise.resolve({ rows: [
+        { doc: report },
+        { doc: contact },
+        { doc: parent },
+        { doc: grandparent }
+      ] }));
+      dbAllDocs.returns(Promise.resolve({ rows: [
+        { doc: parentContact },
+        { doc: grandparentContact }
+      ] }));
+      return service.report('a').then(model => {
+        chai.expect(dbAllDocs.callCount).to.equal(1);
+        chai.expect(dbAllDocs.args[0][0]).to.deep.equal({
+          keys: [ 'x', 'y', 'e', 'f' ],
+          include_docs: true
+        });
+        chai.expect(model.doc.contact.parent.contact).to.deep.equal(parentContact);
+        chai.expect(model.doc.contact.parent.parent.contact).to.deep.equal(grandparentContact);
+      });
+    });
+
+  });
+
+});
