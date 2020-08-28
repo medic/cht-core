0084d583dcfcc3982d5110ea081cb8975db1a768
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2020-08-28 13:05:13 +1200

    Update default webdriver version

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ const {
   STAGING_SERVER,
   BUILDS_SERVER,
   TRAVIS_BUILD_NUMBER,
-  WEBDRIVER_VERSION=83
+  WEBDRIVER_VERSION=85
 } = process.env;
 
 const releaseName = TRAVIS_TAG || TRAVIS_BRANCH || 'local-development';
