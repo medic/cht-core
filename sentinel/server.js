a482bdd3028d032d47146bc9e2fbfec41633918e
mrjones-plip <ashley@medicmobile.org>
2020-08-30 20:58:21 -0700

    revert … to ... per feedback #6376

--- sentinel/server.js
+++ sentinel/server.js
@@ -27,7 +27,7 @@ const waitForApi = () =>
         `http://localhost:${process.env.API_PORT || 5988}/setup/poll`,
         (err, response, body) => {
           if (err) {
-            logger.info('Waiting for API to be ready…');
+            logger.info('Waiting for API to be ready...');
             return setTimeout(() => waitLoop(), 10 * 1000);
           }
 
