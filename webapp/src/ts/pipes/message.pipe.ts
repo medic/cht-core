cf4bd5c47811f51f5caa5445f8c148453db12c7a
Diana Barsan <barsan@medicmobile.org>
2020-09-05 06:32:54 +0300

    Adds lineage filter.

---
+++ webapp/src/ts/pipes/message.pipe.ts
@@ -0,0 +1,92 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash-es';
+import { TranslateService } from '@ngx-translate/core';
+import { FormatProvider } from '../providers/format.provider';
+
+const getFormName = (record, forms) => {
+  const form = _.find(forms, { code: record.form });
+  if (form) {
+    return form.title;
+  }
+  return record.form;
+}
+
+@Pipe({
+  name: 'summary'
+})
+export class SummaryPipe implements PipeTransform {
+  constructor(
+    private translateService:TranslateService,
+  ) {}
+
+  transform(record, forms) {
+    if (!record || !forms) {
+      return '';
+    }
+    if (record.form) {
+      return getFormName(record, forms);
+    }
+    if (record.message && record.message.message) {
+      return record.message.message;
+    }
+    if (record.tasks &&
+      record.tasks[0] &&
+      record.tasks[0].messages &&
+      record.tasks[0].messages[0]) {
+      return record.tasks[0].messages[0].message;
+    }
+    return this.translateService.instant('tasks.0.messages.0.message');
+  }
+}
+
+@Pipe({
+  name: 'title'
+})
+export class TitlePipe implements PipeTransform {
+  constructor(
+    private translateService:TranslateService,
+  ) {}
+
+  transform(record, forms) {
+    if (!record || !forms) {
+      return '';
+    }
+    if (record.form) {
+      return getFormName(record, forms);
+    }
+    if (record.kujua_message) {
+      return this.translateService.instant('Outgoing Message');
+    }
+    return this.translateService.instant('sms_message.message');
+  }
+}
+
+// @deprecated
+@Pipe({
+  name: 'clinic'
+})
+export class ClinicPipe implements PipeTransform {
+  constructor(
+    private translateService:TranslateService,
+    private formatProvider:FormatProvider,
+  ) {}
+
+  transform(entity) {
+    console.warn('`clinic` filter is deprecated. Use `lineage` filter instead.');
+    return this.formatProvider.lineage(entity);
+  };
+}
+
+@Pipe({
+  name: 'lineage'
+})
+export class LineagePipe implements PipeTransform {
+  constructor(
+    private translateService:TranslateService,
+    private formatProvider:FormatProvider,
+  ) {}
+
+  transform(entity) {
+    return this.formatProvider.lineage(entity);
+  };
+}
