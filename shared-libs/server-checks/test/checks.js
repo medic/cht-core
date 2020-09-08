c11e1efdd813030fde46cf6cda805a745b2dfbf5
mrjones-plip <ashley@medicmobile.org>
2020-09-08 15:03:52 -0700

    refactor use of sinon.stub() test calls per feedback

--- shared-libs/server-checks/test/checks.js
+++ shared-libs/server-checks/test/checks.js
@@ -149,7 +149,9 @@ describe('Server Checks service', () => {
         exit: () => 0
       };
       sinon.stub(http, 'get').callsArgWith(1, {statusCode: 401});
-      sinon.stub(request, 'get').callsArgWith(1, null, null, {all_nodes: [ 'nonode@nohost' ], version: '2' });
+      sinon.stub(request, 'get')
+        .onCall(0).callsArgWith(1, null, null, { all_nodes: [ 'nonode@nohost' ], cluster_nodes: [ 'nonode@nohost' ] })
+        .onCall(1).callsArgWith(1, null, null, { version: '2' });
       return service.check('something');
     });
 
@@ -164,7 +166,9 @@ describe('Server Checks service', () => {
         exit: () => 0
       };
       sinon.stub(http, 'get').callsArgWith(1, {statusCode: 401});
-      sinon.stub(request, 'get').callsArgWith(1, 'error', null, {all_nodes: [ 'nonode@nohost' ], version: '2' });
+      sinon.stub(request, 'get')
+        .onCall(0).callsArgWith(1, null, null, { all_nodes: [ 'nonode@nohost' ], cluster_nodes: [ 'nonode@nohost' ] })
+        .onCall(1).callsArgWith(1, null, null, { version: '2' });
       return service.check('something').catch(err => {
         chai.expect(err).to.equal('error');
       });
