a85d148430a08e84318846b60cb727bfb705448c
Diana Barsan <barsan@medicmobile.org>
2020-09-28 16:19:11 +0300

    adds freetext filter.

--- webapp/src/ts/components/filters/freetext-filter/freetext-filter.component.ts
+++ webapp/src/ts/components/filters/freetext-filter/freetext-filter.component.ts
@@ -43,11 +43,14 @@ export class FreetextFilterComponent implements OnDestroy {
 
     this
       .inputTextChanged
-      .pipe(debounceTime(100), distinctUntilChanged())
-      .subscribe(inputText => {
-        this.inputText = inputText;
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe(() => {
         this.applyFilter();
       });
+  }
+
+  onFieldChange(inputText){
+    this.inputTextChanged.next(inputText);
   }
 
   applyFilter() {
@@ -60,50 +63,3 @@ export class FreetextFilterComponent implements OnDestroy {
   }
 }
 
-
-
-/*
-angular.module('inboxDirectives').directive('mmFreetextFilter', function(SearchFilters) {
-  'use strict';
-  'ngInject';
-
-  return {
-    restrict: 'E',
-    templateUrl: 'templates/directives/filters/freetext.html',
-    controller: function($ngRedux, $scope, GlobalActions, Selectors) {
-      'ngInject';
-
-      const ctrl = this;
-      const mapStateToTarget = function(state) {
-        return {
-          currentTab: Selectors.getCurrentTab(state),
-          filters: Selectors.getFilters(state),
-          selectMode: Selectors.getSelectMode(state)
-        };
-      };
-      const mapDispatchToTarget = function(dispatch) {
-        const globalActions = GlobalActions(dispatch);
-        return {
-          setFilter: globalActions.setFilter
-        };
-      };
-      const unsubscribe = $ngRedux.connect(mapStateToTarget, mapDispatchToTarget)(ctrl);
-
-      ctrl.inputText = '';
-      ctrl.updateFilter = function() {
-        ctrl.setFilter({ search: ctrl.inputText });
-      };
-
-      $scope.$on('$destroy', unsubscribe);
-    },
-    controllerAs: 'freetextFilterCtrl',
-    bindToController: {
-      search: '<',
-      selected: '<'
-    },
-    link: function(s, e, a, controller) {
-      SearchFilters.freetext(controller.search);
-    }
-  };
-});
-*/
