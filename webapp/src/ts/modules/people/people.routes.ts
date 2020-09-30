27cebc7d0b5d3acd7e4dd1190fd53d238e6a051e
Njuguna Ndung'u <njgndungu@yahoo.com>
2020-09-30 10:10:58 +0300

    add people module and connect path

---
+++ webapp/src/ts/modules/people/people.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+import { RouteGuardProvider } from '../../providers/route-guard.provider';
+import { PeopleComponent } from './people.component';
+
+export const routes: Routes = [
+  {
+    path: 'contacts.detail',
+    component: PeopleComponent,
+    data: {permissions: ['can_view_contacts']},
+    canActivate: [RouteGuardProvider],
+  },
+];
