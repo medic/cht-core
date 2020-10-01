a9fbf7103fb54a8198efa840a21426b8bf2c04ab
ngaruko <bede.ngaruko@gmail.com>
2020-10-01 20:15:24 +1300

    added forms to mobile test suite

---
+++ tests/mobile/forms/submit-photo-upload-form.spec.js
@@ -0,0 +1,66 @@
+const helper = require('../../helper');
+const photoUpload = require('../../page-objects/forms/photo-upload.po');
+const common = require('../../page-objects/common/common.po');
+const utils = require('../../utils');
+const constants = require('../../constants');
+const path = require('path');
+
+const userContactDoc = {
+  _id: constants.USER_CONTACT_ID,
+  name: 'Jack',
+  date_of_birth: '',
+  phone: '+64274444444',
+  alternate_phone: '',
+  notes: '',
+  type: 'person',
+  reported_date: 1478469976421,
+  parent: {
+    _id: 'some_parent',
+  },
+};
+
+describe('Submit Photo Upload form', () => {
+  beforeAll(done => {
+    Promise.resolve()
+      .then(() => photoUpload.configureForm(userContactDoc, done))
+      .catch(done.fail);
+  });
+
+  afterEach(done => {
+    utils.resetBrowser();
+    done();
+  });
+
+  afterAll(utils.afterEach);
+
+  it('upload photo', () => {
+    common.goToReports();
+
+    // select form
+    const addButton = element(
+      by.css('.action-container .general-actions:not(.ng-hide) .fa-plus')
+    );
+    helper.waitElementToPresent(addButton);
+    helper.clickElement(addButton);
+    element(
+      by.css(
+        '.action-container .general-actions .dropup.open .dropdown-menu li:first-child a'
+      )
+    ).click();
+    helper.waitElementToPresent(
+      element(by.css('#photo-upload input[type=file]'))
+    );
+    element(by.css('#photo-upload input[type=file]')).sendKeys(
+      path.join(__dirname, '../../../webapp/src/img/simprints.png')
+    );
+    helper.waitElementToPresent(
+      element(by.css('#photo-upload .file-picker .file-preview img'))
+    );
+    //submit
+    photoUpload.submit();
+    helper.waitElementToPresent(element(by.css('div.details')));
+    expect(element(by.css('div.details')).isPresent()).toBeTruthy();
+    helper.waitElementToPresent(element(by.css('.report-image')));
+    expect(element(by.css('.report-image')).isPresent()).toBeTruthy();
+  });
+});
---
+++ tests/mobile/forms/submit-z-score-form.spec.js
@@ -0,0 +1,90 @@
+const helper = require('../../helper');
+const ZScoreForm = require('../../page-objects/forms/z-score.po');
+const constants = require('../../constants');
+const utils = require('../../utils');
+
+const userContactDoc = {
+  _id: constants.USER_CONTACT_ID,
+  name: 'Jack',
+  date_of_birth: '',
+  phone: '+64274444444',
+  alternate_phone: '',
+  notes: '',
+  type: 'person',
+  reported_date: 1478469976421,
+  parent: {
+    _id: 'some_parent'
+  }
+};
+
+describe('Submit Z-Score form', () => {
+  beforeAll(done => {
+    Promise.resolve()
+      .then(() => ZScoreForm.configureForm(userContactDoc, done))
+      .catch(done.fail);
+  });
+
+  afterEach(done => {
+    utils.resetBrowser();
+    done();
+  });
+
+  afterAll(utils.afterEach);
+
+  it('Autofills zscore fields with correct values', () => {
+    ZScoreForm.load();
+
+    ZScoreForm.setPatient({ sex: 'female', height: 45, weight: 2, age: 0 });
+    browser.sleep(100);
+
+    expect(ZScoreForm.getHeightForAge()).toEqual('-2.226638023630504');
+    expect(ZScoreForm.getWeightForAge()).toEqual('-3.091160220994475');
+    expect(ZScoreForm.getWeightForHeight()).toEqual('-2.402439024390243');
+
+    ZScoreForm.setPatient({ sex: 'male', height: 45, weight: 2, age: 0 });
+    browser.sleep(100);
+
+    expect(ZScoreForm.getHeightForAge()).toEqual('-2.5800316957210767');
+    expect(ZScoreForm.getWeightForAge()).toEqual('-3.211081794195251');
+    expect(ZScoreForm.getWeightForHeight()).toEqual('-2.259036144578314');
+
+    ZScoreForm.setPatient({ sex: 'female', height: 45.2, weight: 5, age: 1 });
+    browser.sleep(100);
+
+    expect(ZScoreForm.getHeightForAge()).toEqual('-2.206434316353886');
+    expect(ZScoreForm.getWeightForAge()).toEqual('3.323129251700681');
+    expect(ZScoreForm.getWeightForHeight()).toEqual('4');
+
+    ZScoreForm.setPatient({ sex: 'male', height: 45.2, weight: 5, age: 1 });
+    browser.sleep(100);
+
+    expect(ZScoreForm.getHeightForAge()).toEqual('-2.5651715039577816');
+    expect(ZScoreForm.getWeightForAge()).toEqual('2.9789983844911148');
+    expect(ZScoreForm.getWeightForHeight()).toEqual('4');
+  });
+
+  it('saves z-score values', () => {
+    ZScoreForm.load();
+
+    ZScoreForm.setPatient({ sex: 'female', height: 45.1, weight: 3, age: 2 });
+    browser.sleep(100);
+
+    expect(ZScoreForm.getHeightForAge()).toEqual('-2.346895074946466');
+    expect(ZScoreForm.getWeightForAge()).toEqual('-0.4708520179372194');
+    expect(ZScoreForm.getWeightForHeight()).toEqual('2.0387096774193547');
+
+    ZScoreForm.submit();
+
+    expect(helper.getTextFromElement(element(by.css('#reports-content .details li:nth-child(1) p')))).toEqual('45.1');
+    expect(helper.getTextFromElement(element(by.css('#reports-content .details li:nth-child(2) p')))).toEqual('3');
+    expect(helper.getTextFromElement(element(by.css('#reports-content .details li:nth-child(3) p')))).toEqual('female');
+    expect(helper.getTextFromElement(element(by.css('#reports-content .details li:nth-child(4) p')))).toEqual('2');
+
+    expect(helper.getTextFromElement(element(by.css('#reports-content .details li:nth-child(5) p'))))
+      .toEqual('2.0387096774193547');
+    expect(helper.getTextFromElement(element(by.css('#reports-content .details li:nth-child(6) p'))))
+      .toEqual('-0.4708520179372194');
+    expect(helper.getTextFromElement(element(by.css('#reports-content .details li:nth-child(7) p'))))
+      .toEqual('-2.346895074946466');
+  });
+});
