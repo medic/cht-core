88e627475eb4a28f1a8ab78a40cca7c5aa53da92
Mariano Ruiz <mrsarm@gmail.com>
2020-10-07 16:21:16 -0300

    Remove commented code already migrated

--- webapp/src/ts/modules/configuration-user/configuration-user.component.ts
+++ webapp/src/ts/modules/configuration-user/configuration-user.component.ts
@@ -31,39 +31,3 @@ export class ConfigurationUserComponent {
     this.modalService.show(EditUserSettingsComponent);
   }
 }
-
-/*
-angular.module('inboxControllers').controller('ConfigurationUserCtrl',
-  function (
-    Modal,
-    UserSettings
-  ) {
-
-    'use strict';
-    'ngInject';
-
-    const ctrl = this;
-    this.loading = true;
-    UserSettings().then(user => {
-      this.loading = false;
-      this.user = user;
-    });
-
-    ctrl.updatePassword = function() {
-      Modal({
-        templateUrl: 'templates/modals/update_password.html',
-        controller: 'EditUserCtrl',
-        controllerAs: 'editUserCtrl'
-      });
-    };
-
-    ctrl.editSettings = function() {
-      Modal({
-        templateUrl: 'templates/modals/edit_user_settings.html',
-        controller: 'EditUserCtrl',
-        controllerAs: 'editUserCtrl'
-      });
-    };
-
-  }
-);*/
