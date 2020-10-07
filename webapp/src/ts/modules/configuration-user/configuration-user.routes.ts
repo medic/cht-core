43e137236ead834d9dbd698847946e52aad57df8
Mariano Ruiz <mrsarm@gmail.com>
2020-10-07 17:09:48 -0300

    Fix tab color configuration in user settings view

--- webapp/src/ts/modules/configuration-user/configuration-user.routes.ts
+++ webapp/src/ts/modules/configuration-user/configuration-user.routes.ts
@@ -2,5 +2,9 @@ import {Routes} from '@angular/router';
 import {ConfigurationUserComponent} from './configuration-user.component';
 
 export const routes:Routes = [
-  { path: 'user', component: ConfigurationUserComponent },
+  {
+    path: 'user',
+    component: ConfigurationUserComponent,
+    data: { tab: 'user'},
+  }
 ];
