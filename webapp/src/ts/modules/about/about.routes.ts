484a585c647ff2d4989c3369a686a478be8eadc8
Diana Barsan <barsan@medicmobile.org>
2020-10-07 16:50:31 +0300

    Finally fix the translations thing

--- webapp/src/ts/modules/about/about.routes.ts
+++ webapp/src/ts/modules/about/about.routes.ts
@@ -2,5 +2,5 @@ import {Routes} from '@angular/router';
 import {AboutComponent} from './about.component';
 
 export const routes:Routes = [
-  { path: 'about', component: AboutComponent },
+  { path: 'about', component: AboutComponent, data: { tab: 'about' } },
 ];
--- webapp/src/ts/services/translate-locale.service.ts
+++ webapp/src/ts/services/translate-locale.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { map, shareReplay, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,26 +11,30 @@ export class TranslateLocaleService {
   ) {
   }
 
-  private getTranslation(locale) {
-    return this.translateService.getTranslation(locale);
-  }
+  private loadingTranslations = {};
 
-  translate(key, params, locale, skipInterpolation = false) {
-    return this.getTranslation(locale).subscribe(() => {
-      return this.instant(key, params, locale, skipInterpolation);
+  private getTranslation(locale) {
+    if (this.translateService.translations[locale]) {
+      return;
+    }
+
+    if (this.loadingTranslations[locale]) {
+      return;
+    }
+
+    const loadingTranslations = this.translateService.currentLoader.getTranslation(locale).pipe(shareReplay(1), take(1));
+    const translationsCompiled = loadingTranslations.pipe(map(((res) => this.translateService.compiler.compileTranslations(res, locale))), shareReplay(1), take(1));
+    translationsCompiled.subscribe((res) => {
+      this.translateService.translations[locale] = res;
+      this.translateService.addLangs(Object.keys(this.translateService.translations));
     });
+    this.loadingTranslations[locale] = translationsCompiled;
   }
 
   // todo Create issue about the use of "instant" without pre-loading translations
   // this issue existed before the migration to AngularX
   instant(key, params, locale, skipInterpolation = false) {
-    console.log(JSON.parse(JSON.stringify(this.translateService.translations)));
-    if (!this.translateService.translations[locale]) {
-      this.getTranslation(locale).subscribe(() => {
-        console.log(...this.translateService.translations);
-      });
-    }
-
+    this.getTranslation(locale);
 
     if (skipInterpolation) {
       return this.translateService.translations[locale] && this.translateService.translations[locale][key];
