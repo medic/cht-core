b078aa007fdab497442dddba0df962285404bb62
Njuguna Ndung'u <njgndungu@yahoo.com>
2020-10-14 17:12:26 +0300

    fix uhc-settings test

--- webapp/tests/karma/ts/services/uhc-settings.service.spec.ts
+++ webapp/tests/karma/ts/services/uhc-settings.service.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import sinon from 'sinon';
-import { expect, assert } from 'chai';
+import { expect } from 'chai';
 import { UHCSettingsService } from '@mm-services/uhc-settings.service';
 
 describe('UHCSettings service', () => {
   let service: UHCSettingsService;
+
+  beforeEach(() => {
+    service = TestBed.inject(UHCSettingsService);
+  });
 
   afterEach(() => {
     sinon.restore()
@@ -12,24 +16,24 @@ describe('UHCSettings service', () => {
 
   describe('getMonthStartDate', () => {
     it('should return falsy for invalid input', () => {
-      chai.expect(!!service.getMonthStartDate(false)).to.equal(false);
-      chai.expect(!!service.getMonthStartDate({})).to.equal(false);
-      chai.expect(!!service.getMonthStartDate({ uhc: false })).to.equal(false);
+      expect(!!service.getMonthStartDate(false)).to.equal(false);
+      expect(!!service.getMonthStartDate({})).to.equal(false);
+      expect(!!service.getMonthStartDate({ uhc: false })).to.equal(false);
     });
 
     it('should return correct month_start_date', () => {
-      chai.expect(service.getMonthStartDate({ uhc: { month_start_date: 3 }})).to.equal(3);
-      chai.expect(service.getMonthStartDate({ uhc: { month_start_date: 3, visit_count: { month_start_date: 10 } }}))
+      expect(service.getMonthStartDate({ uhc: { month_start_date: 3 }})).to.equal(3);
+      expect(service.getMonthStartDate({ uhc: { month_start_date: 3, visit_count: { month_start_date: 10 } }}))
         .to.equal(3);
-      chai.expect(service.getMonthStartDate({ uhc: { visit_count: { month_start_date: 10 } }})).to.equal(10);
+      expect(service.getMonthStartDate({ uhc: { visit_count: { month_start_date: 10 } }})).to.equal(10);
     });
   });
 
   describe('getVisitCountSettings', () => {
     it('should return empty object for invalid input', () => {
-      chai.expect(service.getVisitCountSettings(false)).to.deep.equal({});
-      chai.expect(service.getVisitCountSettings({})).to.deep.equal({});
-      chai.expect(service.getVisitCountSettings({ uhc: false })).to.deep.equal({});
+      expect(service.getVisitCountSettings(false)).to.deep.equal({});
+      expect(service.getVisitCountSettings({})).to.deep.equal({});
+      expect(service.getVisitCountSettings({ uhc: false })).to.deep.equal({});
     });
 
     it('should return correct values', () => {
@@ -42,7 +46,7 @@ describe('UHCSettings service', () => {
         }
       };
 
-      chai.expect(service.getVisitCountSettings(settings)).to.deep.equal({ monthStartDate: 10, visitCountGoal: 3 });
+      expect(service.getVisitCountSettings(settings)).to.deep.equal({ monthStartDate: 10, visitCountGoal: 3 });
 
       settings = {
         uhc: {
@@ -53,7 +57,7 @@ describe('UHCSettings service', () => {
         }
       };
 
-      chai.expect(service.getVisitCountSettings(settings)).to.deep.equal({ monthStartDate: 10, visitCountGoal: 3 });
+      expect(service.getVisitCountSettings(settings)).to.deep.equal({ monthStartDate: 10, visitCountGoal: 3 });
 
       settings = {
         uhc: {
@@ -65,19 +69,19 @@ describe('UHCSettings service', () => {
         }
       };
 
-      chai.expect(service.getVisitCountSettings(settings)).to.deep.equal({ monthStartDate: 12, visitCountGoal: 3 });
+      expect(service.getVisitCountSettings(settings)).to.deep.equal({ monthStartDate: 12, visitCountGoal: 3 });
     });
   });
 
   describe('getContactsDefaultSort', () => {
     it('should return falsy when for invalid input', () => {
-      chai.expect(!!service.getContactsDefaultSort(false)).to.equal(false);
-      chai.expect(!!service.getContactsDefaultSort({})).to.equal(false);
-      chai.expect(!!service.getContactsDefaultSort({ uhc: false })).to.equal(false);
+      expect(!!service.getContactsDefaultSort(false)).to.equal(false);
+      expect(!!service.getContactsDefaultSort({})).to.equal(false);
+      expect(!!service.getContactsDefaultSort({ uhc: false })).to.equal(false);
     });
 
     it('should return correct value', () => {
-      chai.expect(service.getContactsDefaultSort({ uhc: { contacts_default_sort: 'default' } })).to.equal('default');
+      expect(service.getContactsDefaultSort({ uhc: { contacts_default_sort: 'default' } })).to.equal('default');
     });
   });
 });
