309d03d212886b6e8e7d4f01d790bb9cfb238f65
latin-panda <66472237+latin-panda@users.noreply.github.com>
2020-10-18 22:03:08 +1030

    Fixed value update outside of angular zone

--- webapp/src/ts/modules/messages/messages-content.component.ts
+++ webapp/src/ts/modules/messages/messages-content.component.ts
@@ -71,6 +71,7 @@ export class MessagesContentComponent implements OnInit, OnDestroy, AfterViewIni
     ).subscribe(([selectedConversation, loadingContent]) => {
       this.selectedConversation = selectedConversation;
       this.loadingContent = loadingContent;
+      this.changeDetectorRef.detectChanges();
     });
     this.subscriptions.add(selectorsSubscription);
 
