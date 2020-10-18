95890ec55b0d34d385946ec31c15ea1973629a37
ngaruko <bede.ngaruko@gmail.com>
2020-10-18 18:49:11 +1300

    run all tests

--- tests/mobile/login/login.specs.js
+++ tests/mobile/login/login.specs.js
@@ -11,17 +11,17 @@ describe('Login tests : ', () => {
     expect(browser.getTitle()).toEqual('Medic Mobile');
   });
 
-  xit('should try to sign in with blank password and verify that credentials were incorrect', () => {
+  it('should try to sign in with blank password and verify that credentials were incorrect', () => {
     commonElements.goToLoginPage();
     loginPage.login(wrongUsername, '', true);
   });
 
-  xit('should try to sign in with blank username and password and verify that credentials were incorrect', () => {
+  it('should try to sign in with blank username and password and verify that credentials were incorrect', () => {
     commonElements.goToLoginPage();
     loginPage.login('', '', true);
   });
 
-  xit('should try to sign in and verify that credentials were incorrect', () => {
+  it('should try to sign in and verify that credentials were incorrect', () => {
     commonElements.goToLoginPage();
     loginPage.login(wrongUsername, wrongPassword, true);
     loginPage.login(auth.username, auth.password);
