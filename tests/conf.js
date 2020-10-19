7d6ffd6cb7895d33d343f0c6fe7b3dd908fe0e1a
ngaruko <bede.ngaruko@gmail.com>
2020-10-19 15:33:17 +1300

    try sequential multicapabilities

--- tests/conf.js
+++ tests/conf.js
@@ -1,40 +1,68 @@
-const fs = require('fs');
-const request = require('request-promise-native');
-const utils = require('./utils');
-const constants = require('./constants');
-const auth = require('./auth')();
-const browserLogStream = fs.createWriteStream(
-  __dirname + '/../tests/logs/browser.console.log'
+const fs=require('fs');
+const request=require('request-promise-native');
+const utils=require('./utils');
+const constants=require('./constants');
+const auth=require('./auth')();
+const browserLogStream=fs.createWriteStream(
+  __dirname+'/../tests/logs/browser.console.log'
 );
 
-const chai = require('chai');
+const chai=require('chai');
 // so the .to.have.members will display the array members when assertions fail instead of [ Array(6) ]
-chai.config.truncateThreshold = 0;
+chai.config.truncateThreshold=0;
 
-const baseConfig = {
+const baseConfig={
   params: {
     pathToConfig: false
   },
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  suites: {
-    e2e: 'e2e/login/login.specs.js',
-    mobile:'mobile/**/*.js',
-    performance: 'performance/**/*.js'
-  },
+  // suites: {
+  //   //e2e: 'e2e/login/**/*.js',
+  //   //mobile: 'mobile/**/*.js',
+  //  // performance: 'performance/**/*.js'
+  // },
+  maxSessions: 1,
   framework: 'jasmine2',
-  capabilities: {
+  multiCapabilities: [ {
     browserName: 'chrome',
-    chromeOptions: {
+    'chromeOptions': {
       // chromedriver 75 is w3c enabled by default and causes some actions to be impossible to perform
       // eg: browser.actions().sendKeys(protractor.Key.TAB).perform()
-      // https://github.com/angular/protractor/issues/5261
       w3c: false,
-      args: ['--window-size=1024,768', '--headless', '--disable-gpu']
-    }
-  },
+      args: ['--window-size=1024,768', 'headless', '--disable-gpu']
+    },
+
+    name: 'chrome-tests',
+    count: 1,
+    shardTestFiles: true,
+    maxInstances: 1,
+    specs:['e2e/login/login.specs.js']
+  }, {
+    browserName: 'chrome',
+    'chromeOptions': {
+      args: ['--headless', '--disable-gpu'],
+      mobileEmulation: { 'deviceName': 'Nexus 5' }
+    },
+    name: 'mobile-tests',
+    count: 1,
+    shardTestFiles: true,
+    maxInstances: 1,
+    specs:['mobile/login/login.specs.js']
+  } ],
+  //maxSessions: 1,
+  // capabilities: {
+  //   browserName: 'chrome',
+  //   chromeOptions: {
+  //     // chromedriver 75 is w3c enabled by default and causes some actions to be impossible to perform
+  //     // eg: browser.actions().sendKeys(protractor.Key.TAB).perform()
+  //     // https://github.com/angular/protractor/issues/5261
+  //     w3c: false,
+  //     args: ['--window-size=1024,768', '--headless', '--disable-gpu']
+  //   }
+  // },
   jasmineNodeOpts: {
     // makes default jasmine reporter not display dots for every spec
-    print: () => {}
+    print: () => { }
   },
   beforeLaunch: () => {
     process.on('uncaughtException', () => {
@@ -60,7 +88,7 @@ const baseConfig = {
     browser.waitForAngularEnabled(false);
 
     // wait for startup to complete - taking a little too long on travis
-    browser.driver.wait(prepServices(), 135 * 1000, 'API took too long to start up');
+    browser.driver.wait(prepServices(), 135*1000, 'API took too long to start up');
 
     afterEach(() => {
       return browser
@@ -80,14 +108,14 @@ const baseConfig = {
 };
 
 
-exports.config = baseConfig;
+exports.config=baseConfig;
 
-const runAndLog = (msg, func) => {
+const runAndLog=(msg, func) => {
   console.log(`API startup: ${msg}`);
   return func();
 };
 
-const prepServices = () => {
+const prepServices=() => {
   let apiReady;
   if (constants.IS_TRAVIS) {
     console.log('On travis, waiting for horti to first boot api');
@@ -95,12 +123,12 @@ const prepServices = () => {
     // getting pushed into horti.log Once horti has bootstrapped we want to restart everything so
     // that the service processes get restarted with their logs separated and pointing to the
     // correct logs for testing
-    apiReady = listenForApi()
+    apiReady=listenForApi()
       .then(() => console.log('Horti booted API, rebooting under our logging structure'))
       .then(() => request.post('http://localhost:31337/all/restart'));
   } else {
     // Locally we just need to start them and can do so straight away
-    apiReady = request.post('http://localhost:31337/all/start');
+    apiReady=request.post('http://localhost:31337/all/start');
   }
 
   return apiReady
@@ -109,7 +137,7 @@ const prepServices = () => {
     .then(() => runAndLog('User contact doc setup', utils.setUserContactDoc));
 };
 
-const listenForApi = () => {
+const listenForApi=() => {
   console.log('Checking API');
   return utils.request({ path: '/api/info' }).catch(() => {
     console.log('API check failed, trying again in 1 second');
@@ -121,31 +149,31 @@ const listenForApi = () => {
   });
 };
 
-const getLoginUrl = () => {
-  const redirectUrl = encodeURIComponent(
+const getLoginUrl=() => {
+  const redirectUrl=encodeURIComponent(
     `/${constants.DB_NAME}/_design/${constants.MAIN_DDOC_NAME}/_rewrite/#/messages`
   );
   return `http://${constants.API_HOST}:${constants.API_PORT}/${constants.DB_NAME}/login?redirect=${redirectUrl}`;
 };
 
-const login = browser => {
+const login=browser => {
   browser.driver.get(getLoginUrl());
   browser.driver.findElement(by.name('user')).sendKeys(auth.username);
   browser.driver.findElement(by.name('password')).sendKeys(auth.password);
   browser.driver.findElement(by.id('login')).click();
   // Login takes some time, so wait until it's done.
-  const bootstrappedCheck = () =>
+  const bootstrappedCheck=() =>
     element(by.css('body.bootstrapped')).isPresent();
   return browser.driver.wait(
     bootstrappedCheck,
-    20 * 1000,
+    20*1000,
     'Login should be complete within 20 seconds'
   );
 };
 
-const setupSettings = () => {
-  const defaultAppSettings = utils.getDefaultSettings();
-  defaultAppSettings.transitions = {};
+const setupSettings=() => {
+  const defaultAppSettings=utils.getDefaultSettings();
+  defaultAppSettings.transitions={};
 
   return utils.request({
     path: '/api/v1/settings?replace=1',
@@ -154,12 +182,12 @@ const setupSettings = () => {
   });
 };
 
-const setupUser = () => {
+const setupUser=() => {
   return utils
-    .getDoc('org.couchdb.user:' + auth.username)
+    .getDoc('org.couchdb.user:'+auth.username)
     .then(doc => {
-      doc.contact_id = constants.USER_CONTACT_ID;
-      doc.language = 'en';
+      doc.contact_id=constants.USER_CONTACT_ID;
+      doc.language='en';
       return utils.saveDoc(doc);
     })
     .then(() => utils.refreshToGetNewSettings())
