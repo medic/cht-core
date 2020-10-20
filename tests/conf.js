57cca4f13fe2845c68678f8cf40215f192de744c
ngaruko <bede.ngaruko@gmail.com>
2020-10-20 01:42:45 +0000

    delete useles comment

--- tests/conf.js
+++ tests/conf.js
@@ -31,7 +31,6 @@ const baseConfig={
       // https://github.com/angular/protractor/issues/5261
       w3c: false,
       args: [ '--window-size=1024,768', '--headless', '--disable-gpu' ]
-      //args: [ '--window-size=1024,768', '--disable-gpu' ]
     }
   },
 
