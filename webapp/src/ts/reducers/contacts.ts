fd8137e6ec16709bdc054676fb20cf00d4473ad1
Njuguna Ndung'u <njgndungu@yahoo.com>
2020-10-29 22:35:10 +0300

    improve contacts reducer

--- webapp/src/ts/reducers/contacts.ts
+++ webapp/src/ts/reducers/contacts.ts
@@ -1,8 +1,10 @@
 import { Actions } from '../actions/contacts';
 import { createReducer, on } from '@ngrx/store';
+import { UniqueSortedList } from './utils';
 
 const initialState = {
   contacts: [],
+  contactsById: new Map(),
   selected: [],
   filters: {},
   loadingSelectedChildren: false,
@@ -11,8 +13,16 @@ const initialState = {
 };
 
 const updateContacts = (state, newContacts) => {
-  const contacts = newContacts;
-  return { ...state, contacts };
+  const contacts = [...state.contacts];
+  const contactsById = new Map(state.contactsById);
+  const list = new UniqueSortedList(contacts, contactsById, 'name');
+  
+  newContacts.forEach(contact => {
+    list.remove(contact);
+    list.add(contact);
+  });
+
+  return { ...state, contacts, contactsById };
 }
 
 const _contactsReducer = createReducer(
