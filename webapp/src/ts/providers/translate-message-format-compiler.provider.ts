a9260314a81372a5ede1a8b3b7f47f4e2e9c523a
Diana Barsan <35681649+dianabarsan@users.noreply.github.com>
2020-11-10 20:39:34 +0200

    Adds translations compiler to support pluralization with message format (#6715)
    We used to rely on messageformat to provide pluralization. The AngularJS translator allowed you to control how you could interpolate a translation, but ngx-translate does not.
    Because we only want to pass "some" translated strings to message-format, I leveraging the option of adding a compiler to ngx-translate that will try to filter correct phrases to be passed to message-format.

---
+++ webapp/src/ts/providers/translate-message-format-compiler.provider.ts
@@ -0,0 +1,36 @@
+import { TranslateCompiler } from '@ngx-translate/core';
+import * as MessageFormat from 'messageformat';
+
+export class TranslateMessageFormatCompilerProvider extends TranslateCompiler {
+  private messageFormat;
+  private readonly doubleOrNoneCurlyBraces = new RegExp(/\{{|^[^{]+$/);
+
+  constructor() {
+    super();
+    this.messageFormat = new MessageFormat([]);
+  }
+
+  compile(value, lang) {
+    // message-format uses single curly braces for defining parameters( like `His name is {NAME}` )
+    // passing a string that contains open double curly braces to message-format produces an error
+    // if the message has either double curly braces or no curly braces at all, bypass message-format entirely
+    const hasDoubleOrNoCurlyBraces = this.doubleOrNoneCurlyBraces.test(value);
+    if (hasDoubleOrNoCurlyBraces) {
+      return value;
+    }
+
+    try {
+      return this.messageFormat.compile(value, lang);
+    } catch (err) {
+      console.error('messageformat compile error', err);
+      return value;
+    }
+  }
+
+  compileTranslations(translations, lang) {
+    Object.keys(translations).map(key => {
+      translations[key] = this.compile(translations[key], lang)
+    });
+    return translations;
+  }
+}
---
+++ webapp/tests/karma/ts/providers/translate-message-formate-compiler.provider.spec.ts
@@ -0,0 +1,82 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import * as MessageFormat from 'messageformat';
+
+import { TranslateMessageFormatCompilerProvider } from '@mm-providers/translate-message-format-compiler.provider';
+
+let service;
+
+describe('Translate MessageFormat compiler provider', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('compile', () => {
+    it('should initialize local messageFormat', () => {
+      service = new TranslateMessageFormatCompilerProvider();
+      const compiled = service.compile('the {value}', 'fr');
+      expect(compiled).to.be.a('function');
+      expect(compiled({ value: 'thing' })).to.equal('the thing');
+    });
+
+    it('should not pass double open curly braces or no curly braces strings to message format', () => {
+      const mfCompile = sinon.stub(MessageFormat.prototype, 'compile');
+      service = new TranslateMessageFormatCompilerProvider();
+      expect(service.compile('the {{value}}')).to.equal('the {{value}}');
+      expect(mfCompile.callCount).to.equal(0);
+      expect(service.compile('the value')).to.equal('the value');
+      expect(mfCompile.callCount).to.equal(0);
+      expect(service.compile('{{a value')).to.equal('{{a value');
+      expect(mfCompile.callCount).to.equal(0);
+      expect(service.compile('{{some}} other {value}')).to.equal('{{some}} other {value}');
+      expect(mfCompile.callCount).to.equal(0);
+    });
+
+    it('should pass single open curly braces strings to message format', () => {
+      const mfCompile = sinon.stub(MessageFormat.prototype, 'compile');
+      mfCompile.callsFake((string) => () => `${string} compiled`);
+      service = new TranslateMessageFormatCompilerProvider();
+
+      expect(service.compile('a {thing}', 'en')()).to.equal('a {thing} compiled');
+      expect(mfCompile.callCount).to.equal(1);
+      expect(mfCompile.args[0]).to.deep.equal(['a {thing}', 'en']);
+      expect(service.compile('a {thing} or {other}', 'fr')()).to.equal('a {thing} or {other} compiled');
+      expect(mfCompile.callCount).to.equal(2);
+      expect(mfCompile.args[1]).to.deep.equal(['a {thing} or {other}', 'fr']);
+    });
+
+    it('should catch message format throwing errors', () => {
+      const mfCompile = sinon.stub(MessageFormat.prototype, 'compile').throws({ some: 'error' });
+      service = new TranslateMessageFormatCompilerProvider();
+      expect(service.compile('a {thing}')).to.equal('a {thing}');
+      expect(mfCompile.callCount).to.equal(1);
+    });
+  });
+
+  describe('compile translations', () => {
+    it('should compile every value', () => {
+      const translations = {
+        'a': 'a translation',
+        'b': 'b translation',
+        'c': 'c translation',
+        'd': 'd translation',
+      };
+      service = new TranslateMessageFormatCompilerProvider();
+      sinon.stub(service, 'compile').callsFake((translation) => `${translation} compiled`);
+      const result = service.compileTranslations(translations, 'the_lang');
+      expect(result).to.deep.equal({
+        'a': 'a translation compiled',
+        'b': 'b translation compiled',
+        'c': 'c translation compiled',
+        'd': 'd translation compiled',
+      });
+      expect(service.compile.callCount).to.equal(4);
+      expect(service.compile.args).to.deep.equal([
+        ['a translation', 'the_lang'],
+        ['b translation', 'the_lang'],
+        ['c translation', 'the_lang'],
+        ['d translation', 'the_lang'],
+      ]);
+    });
+  });
+});
