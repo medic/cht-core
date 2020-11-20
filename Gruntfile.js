ff0bdcf43ed724c971689d2ac2fdcf90700cccbe
Diana Barsan <barsan@medicmobile.org>
2020-11-20 07:42:24 +0200

    Don't show progress on Travis and change watch job for faster dev builds.

--- Gruntfile.js
+++ Gruntfile.js
@@ -605,7 +605,7 @@ module.exports = function(grunt) {
           const configuration = TRAVIS_BUILD_NUMBER ? 'production' : 'development';
           return [
             `cd webapp`,
-            `../node_modules/.bin/ng build --configuration=${configuration}`,
+            `../node_modules/.bin/ng build --configuration=${configuration} --progress=${TRAVIS_BUILD_NUMBER ? 'false' : 'true'}`,
             `../node_modules/.bin/ngc`,
             'cd ../',
           ].join(' && ');
@@ -626,9 +626,19 @@ module.exports = function(grunt) {
         cmd: () => {
           return [
             'cd webapp',
-            '../node_modules/.bin/ng test webapp --watch=false',
+            `../node_modules/.bin/ng test webapp --watch=false --progress=${TRAVIS_BUILD_NUMBER ? 'false' : 'true'}`,
             'cd ../',
           ].join(' && ');
+        },
+        stdio: 'inherit', // enable colors!
+      },
+      'watch-webapp': {
+        cmd: () => {
+          const configuration = TRAVIS_BUILD_NUMBER ? 'production' : 'development';
+          return `
+            cd webapp && ../node_modules/.bin/ng build --configuration=${configuration} --watch=true & 
+            cd ../
+          `;
         },
         stdio: 'inherit', // enable colors!
       },
@@ -636,7 +646,7 @@ module.exports = function(grunt) {
         cmd: () => {
           return [
             'cd webapp',
-            '../node_modules/.bin/ng test webapp --watch=true',
+            '../node_modules/.bin/ng test webapp --watch=true --progress=true',
             'cd ../',
           ].join(' && ');
         },
