6cd93f2bee6198cb1a05120f2aaaf4f0759531ef
Mariano Ruiz <mrsarm@gmail.com>
2020-11-27 22:37:14 -0500

    Migrate to A10 Home component

---
+++ webapp/src/ts/modules/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '@mm-services/auth.service';
+import { HeaderTabsService } from '@mm-services/header-tabs.service';
+
+@Component({
+  template: '',
+})
+export class HomeComponent implements OnInit {
+
+  constructor(
+    private authService:AuthService,
+    private headerTabsService:HeaderTabsService,
+    private router:Router,
+  ) { }
+
+  ngOnInit() {
+    this.findFirstAuthorizedTab().then(route => {
+      if (!route) {
+        route = 'error';
+      }
+      this.router.navigate([route]);
+    });
+  }
+
+  findFirstAuthorizedTab() {
+    const tabs = this.headerTabsService.get();
+    return Promise
+      .all(tabs.map(tab => this.authService.has(tab.permissions)))
+      .then(results => {
+        const idx = results.findIndex(result => result);
+        if (idx === -1) {
+          return;
+        }
+        return tabs[idx].route;
+      });
+  }
+}
