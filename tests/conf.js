3d4517b8df1f8fe60c8f0b7e2df61c1db089dbc2
Nick Christenson <emailfornc+git@gmail.com>
2020-12-04 11:11:56 -0500

    eslint spacing

--- tests/conf.js
+++ tests/conf.js
@@ -133,7 +133,7 @@ const login = browser => {
   browser.driver.findElement(by.id('login')).click();
   // Login takes some time, so wait until it's done.
   const bootstrappedCheck = () =>
-  element(by.css('.app-root.bootstrapped')).isPresent();
+    element(by.css('.app-root.bootstrapped')).isPresent();
   return browser.driver.wait(
     bootstrappedCheck,
     20 * 1000,
