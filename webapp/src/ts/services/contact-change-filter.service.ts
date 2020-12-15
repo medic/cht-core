d2f66fac65a3e12954673b9a3d7eaa444edeb092
Njuguna Ndung'u <njgndungu@yahoo.com>
2020-12-16 02:14:48 +0300

    put functions inside class as private methods

--- webapp/src/ts/services/contact-change-filter.service.ts
+++ webapp/src/ts/services/contact-change-filter.service.ts
@@ -6,51 +6,6 @@ import { some } from 'lodash-es';
 
 import { ContactTypesService } from '@mm-services/contact-types.service';
 
-const isValidInput = function(object) {
-  return !!(object && object.doc);
-};
-
-const isReport = function(change) {
-  return !!change.doc.form && change.doc.type === 'data_record';
-};
-
-const matchReportSubject = function(report, contact) {
-  if (report.doc.fields && (
-    (report.doc.fields.patient_id && report.doc.fields.patient_id === contact.doc._id) ||
-    (report.doc.fields.patient_id && report.doc.fields.patient_id === contact.doc.patient_id) ||
-    (report.doc.fields.place_id && report.doc.fields.place_id === contact.doc._id) ||
-    (report.doc.fields.place_id && report.doc.fields.place_id === contact.doc.place_id))) {
-    return true;
-  }
-
-  if ((report.doc.patient_id && report.doc.patient_id === contact.doc.patient_id) ||
-      (report.doc.patient_id && report.doc.patient_id === contact.doc._id) ||
-      (report.doc.place_id && report.doc.place_id === contact.doc.place_id) ||
-      (report.doc.place_id && report.doc.place_id === contact.doc._id)) {
-    return true;
-  }
-
-  return false;
-};
-
-const isChild = function(change, contact) {
-  return !!change.doc.parent && change.doc.parent._id === contact.doc._id;
-};
-
-const wasChild = function(change, contact) {
-  return some(contact.children, function(children) {
-    return children instanceof Array && some(children, function(child) {
-      return child.doc._id === change.doc._id;
-    });
-  });
-};
-
-const isAncestor = function(change, contact) {
-  return some(contact.lineage, function(lineage) {
-    return !!lineage && lineage._id === change.doc._id;
-  });
-};
-
 @Injectable({
   providedIn: 'root'
 })
@@ -59,30 +14,75 @@ export class ContactChangeFilterService {
     private contactTypesService: ContactTypesService
   ){}
 
+  private isValidInput(object) {
+    return !!(object && object.doc);
+  }
+  
+  private isReport(change) {
+    return !!change.doc.form && change.doc.type === 'data_record';
+  }
+
+  private matchReportSubject(report, contact) {
+    if (report.doc.fields && (
+      (report.doc.fields.patient_id && report.doc.fields.patient_id === contact.doc._id) ||
+      (report.doc.fields.patient_id && report.doc.fields.patient_id === contact.doc.patient_id) ||
+      (report.doc.fields.place_id && report.doc.fields.place_id === contact.doc._id) ||
+      (report.doc.fields.place_id && report.doc.fields.place_id === contact.doc.place_id))) {
+      return true;
+    }
+  
+    if ((report.doc.patient_id && report.doc.patient_id === contact.doc.patient_id) ||
+        (report.doc.patient_id && report.doc.patient_id === contact.doc._id) ||
+        (report.doc.place_id && report.doc.place_id === contact.doc.place_id) ||
+        (report.doc.place_id && report.doc.place_id === contact.doc._id)) {
+      return true;
+    }
+  
+    return false;
+  }
+
+  private isChild(change, contact) {
+    return !!change.doc.parent && change.doc.parent._id === contact.doc._id;
+  }
+
+  private wasChild(change, contact) {
+    return some(contact.children, (children) => {
+      return children instanceof Array && some(children, (child) => {
+        return child.doc._id === change.doc._id;
+      });
+    });
+  }
+
+  private isAncestor(change, contact) {
+    return some(contact.lineage, (lineage) => {
+      return !!lineage && lineage._id === change.doc._id;
+    });
+  }
+
   private matchChildReportSubject(change, contact) {
-    return some(contact.children, function(children) {
-      return children instanceof Array && some(children, function(child) {
-        return matchReportSubject(change, child);
+    return some(contact.children, (children) => {
+      return children instanceof Array && some(children, (child) => {
+        return this.matchReportSubject(change, child);
       });
     });
   }
   
   matchContact(change, contact) {
-    return isValidInput(contact) && contact.doc._id === change.id;
+    return this.isValidInput(contact) && contact.doc._id === change.id;
   }
 
   isRelevantReport(change, contact) {
-    return isValidInput(change) &&
-           isValidInput(contact) &&
-           isReport(change) &&
-           (matchReportSubject(change, contact) || this.matchChildReportSubject(change, contact));
+    return this.isValidInput(change) &&
+           this.isValidInput(contact) &&
+           this.isReport(change) &&
+           (this.matchReportSubject(change, contact) || this.matchChildReportSubject(change, contact));
   }
 
   isRelevantContact(change, contact) {
-    return isValidInput(change) &&
-           isValidInput(contact) &&
+    return this.isValidInput(change) &&
+           this.isValidInput(contact) &&
            this.contactTypesService.includes(change.doc) &&
-           (isAncestor(change, contact) || isChild(change, contact) || wasChild(change, contact));
+           (this.isAncestor(change, contact) || this.isChild(change, contact) || this.wasChild(change, contact));
   }
 
   isDeleted(change) {
