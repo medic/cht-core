5f992e29ee30764f0041996d3c9d2292558a2888
Nick Christenson <emailfornc@gmail.com>
2020-12-15 13:47:29 -0500

    moved function to single line, moved call to .all so it is executed everytime it calls vs 1 time

--- tests/page-objects/reports/reports.po.js
+++ tests/page-objects/reports/reports.po.js
@@ -10,16 +10,13 @@ const reportListID = '#reports-list';
 const list = element(by.css(reportListID));
 const listLoader =  element(by.css(`${reportListID} .loader`));
 const firstReport = element(by.css(`${reportListID} li:first-child`));
-const allReports = element.all(by.css(`${reportListID} li`));
 
 module.exports = {
-  allReports: allReports,
+  allReports: () => element.all(by.css(`${reportListID} li`)),
   firstReport: firstReport,
   listLoader: listLoader,
   list: list,
-  reportByUUID: uuid => {
-    return list.all(by.css('li[data-record-id="' + uuid + '"]'));
-  },
+  reportByUUID: uuid => list.all(by.css('li[data-record-id="' + uuid + '"]')),
   filterByDate: (startDate, endDate) => {
     let clear = '';
     for (let i = 0; i < 20; i++) {
