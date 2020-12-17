d90777cc1bbe3075881d64246e1817fbeda762ea
ngaruko <bede.ngaruko@gmail.com>
2020-12-17 00:13:56 +0000

    enabled sms_workflows tests

--- tests/conf.js
+++ tests/conf.js
@@ -19,7 +19,9 @@ const baseConfig = {
   //Enabling specs as they are working.
   suites: {
     // e2e:'e2e/**/*.js',
-    e2e: ['**/login.spec.js'],
+    e2e: ['**/login.spec.js',
+      '**/transitions/sms_workflows.spec.js',
+    ],
     // performance: 'performance/**/*.js'
   },
   framework: 'jasmine2',
