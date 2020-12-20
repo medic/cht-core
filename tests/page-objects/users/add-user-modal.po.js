3e24340e50a92d0285bbe3dce246282230d5915b
ngaruko <bede.ngaruko@gmail.com>
2020-12-20 22:54:32 +0000

    used test-id  attribute for errorMessageUserName

--- tests/page-objects/users/add-user-modal.po.js
+++ tests/page-objects/users/add-user-modal.po.js
@@ -1,8 +1,8 @@
 const helper = require('../../helper');
 const errorMessagePassword = element(by.css('#edit-password ~ .help-block'));
-const errorMessageUserName = element.all(by.css('span.help-block.ng-binding')).get(0);
+const errorMessageUserName = element(by.css('[test-id="errors-username"]'));
 const facilitySelector = element(by.css('#facilitySelect ~ .help-block'));
-const contactSelector = element(by.css('#contactSelect ~ .help-block')); 
+const contactSelector = element(by.css('#contactSelect ~ .help-block'));
 
 const getUsernameField = () => {
   return element(by.id('edit-username'));
@@ -67,10 +67,9 @@ module.exports = {
   
   getErrorMessagePassword: () =>{
     return errorMessagePassword.getText();
-  }, 
+  },
 
   getErrorMessageUserName: () =>{
-    helper.waitUntilReady(errorMessageUserName);
     return errorMessageUserName.getText();
   },
 
