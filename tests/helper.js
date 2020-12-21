f472866ad446120337d9445e921213ff08c8e03f
Bede Ngaruko <ngarukob@yahoo.com>
2020-12-21 04:47:50 +0000

    check for text in getText()

--- tests/helper.js
+++ tests/helper.js
@@ -76,7 +76,7 @@ module.exports = {
       )
       .then(() => {
         return element.getText().then(val => {
-          return val;
+          if(value){return val;}
         });
       })
       .catch(() => {
