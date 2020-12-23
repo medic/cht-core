5c439fb9c355a7bc1b4aaf6f0d8f96b5438422b0
latin-panda <66472237+latin-panda@users.noreply.github.com>
2020-12-23 17:04:51 +0700

    migration of testing component

---
+++ webapp/src/ts/services/debug.service.ts
@@ -0,0 +1,42 @@
+/*
+ * Manage debug mode.
+ *
+ * When enabled:
+ *
+ *   - persist setting to a cookie
+ *   - put PouchDB in debug mode
+ *
+ * Note: When debugging, make sure to enable the logs by selecting 'verbose' and 'info' options
+ * in the console of the browser's developer tools.
+ */
+
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DebugService {
+  readonly cookieName = 'medic-webapp-debug'
+
+  constructor(private cookieService: CookieService) { }
+
+  get() {
+    return !!this.cookieService.get(this.cookieName);
+  }
+
+  set(bool:boolean) {
+    const db = window.PouchDB;
+
+    if (bool) {
+      db.debug.enable('*');
+      this.cookieService.set(this.cookieName, bool.toString(), 360, '/');
+      console.debug('Debug mode on');
+      return;
+    }
+
+    db.debug.disable();
+    this.cookieService.delete(this.cookieName, '/');
+    console.debug('Debug mode off');
+  }
+}
