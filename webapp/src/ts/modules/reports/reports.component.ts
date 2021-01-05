410322680a971b3586327f7264fae2c9c8e6f446
Diana Barsan <barsan@medicmobile.org>
2021-01-05 11:38:04 +0200

    tests

--- webapp/src/ts/modules/reports/reports.component.ts
+++ webapp/src/ts/modules/reports/reports.component.ts
@@ -280,7 +280,9 @@ export class ReportsComponent implements OnInit, OnDestroy {
     });
   }
 
-  toggleSelected(report) {
+  toggleSelected(report, event) {
+    event.stopPropagation();
+
     if (!report?._id) {
       return;
     }
