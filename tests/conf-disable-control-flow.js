4fcddcedecd08413300aed8bd24610dba303e274
Nick Christenson <emailfornc@gmail.com>
2021-01-05 14:15:56 -0500

    [skip travis] small commit to skip travis

--- tests/conf-disable-control-flow.js
+++ tests/conf-disable-control-flow.js
@@ -7,6 +7,7 @@ const browserLogStream = fs.createWriteStream(
   __dirname + '/../tests/logs/browser.console.log'
 );
 
+
 const chai = require('chai');
 // so the .to.have.members will display the array members when assertions fail instead of [ Array(6) ]
 chai.config.truncateThreshold = 0;
