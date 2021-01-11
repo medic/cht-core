4e3251a1893096414141aaa78336787cc816a3cd
Nick Christenson <emailfornc@gmail.com>
2021-01-11 14:22:36 -0500

    replace utils with deprecated methods

--- tests/utils.js
+++ tests/utils.js
@@ -496,15 +496,24 @@ const getDefaultSettings = () => {
   return JSON.parse(fs.readFileSync(pathToDefaultAppSettings).toString());
 };
 
+<<<<<<< HEAD
 const deprecated = (name, replacement) => {
+=======
+function deprecated(name, replacement) {
+>>>>>>> replace utils with deprecated methods
   let msg = `The function ${name} has been deprecated.`;
   if (replacement) {
     msg = `${msg} Replace by ${replacement}`;
   }
+<<<<<<< HEAD
   if (process.env.DEBUG) {
     console.warn(msg);
   }
 };
+=======
+  console.warn(msg);
+}
+>>>>>>> replace utils with deprecated methods
 
 module.exports = {
   deprecated,
@@ -520,7 +529,11 @@ module.exports = {
     captureOnlyFailedSpecs: true,
     reportOnlyFailedSpecs: false,
     showQuickLinks: true,
+<<<<<<< HEAD
     dest: `tests/results/`,
+=======
+    dest: `tests/results/${new Date().toISOString()}`,
+>>>>>>> replace utils with deprecated methods
     filename: 'report.html',
     pathBuilder: function(currentSpec) {
       return currentSpec.fullName
@@ -777,7 +790,6 @@ module.exports = {
 
   //check for the update modal before
   beforeEach: async () => {
-    console.log('before each');
     if (await element(by.css('#update-available')).isPresent()) {
       await $('body').sendKeys(protractor.Key.ENTER);
     }
