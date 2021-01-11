87b0202bc5c2943d4f887df3f22a6a974ade1f93
Nick Christenson <emailfornc@gmail.com>
2021-01-11 16:39:32 -0500

    passing locally

--- tests/utils.js
+++ tests/utils.js
@@ -778,10 +778,6 @@ module.exports = {
 
   //check for the update modal before
   beforeEach: async () => {
-<<<<<<< HEAD
-    console.log('before each');
-=======
->>>>>>> replace utils with deprecated methods
     if (await element(by.css('#update-available')).isPresent()) {
       await $('body').sendKeys(protractor.Key.ENTER);
     }
