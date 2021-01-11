c7a35aa27a029183da51517b07cab1f7c2c0b8d3
Nick Christenson <emailfornc@gmail.com>
2021-01-11 14:16:16 -0500

    added grunt changes, script changes and the conf file for disabled control flow

--- Gruntfile.js
+++ Gruntfile.js
@@ -474,6 +474,15 @@ module.exports = function(grunt) {
         ].join('&& '),
         exitCodes: [0, 1] // 1 if e2e-couchdb doesn't exist, which is fine
       },
+<<<<<<< HEAD
+=======
+      'sleep': {
+        cmd: [
+          'sleep 30',
+        ].join('&& '),
+        exitCodes: [0, 1] // 1 if e2e-couchdb doesn't exist, which is fine
+      },
+>>>>>>> added grunt changes, script changes and the conf file for disabled control flow
       'clean-test-database': {
         cmd: [
           'docker stop e2e-couchdb'
@@ -975,9 +984,14 @@ module.exports = function(grunt) {
   grunt.registerTask('e2e', 'Deploy app for testing and run e2e tests', [
     'e2e-deploy',
     'protractor:e2e-tests',
+<<<<<<< HEAD
   ]);
 
   grunt.registerTask('e2e-disabled', 'Deploy app for testing and run e2e tests', [
+=======
+    'exec:wait_for_api_down',
+    'exec:sleep',
+>>>>>>> added grunt changes, script changes and the conf file for disabled control flow
     'e2e-deploy',
     'protractor:e2e-disable-control-flow',
     'exec:clean-test-database',
@@ -1060,6 +1074,11 @@ module.exports = function(grunt) {
     'start-webdriver',
     'exec:e2e-servers',
     'protractor:e2e-tests',
+    'exec:wait_for_api_down',
+    // Adding a wait to ensure the api port opens and process closes 
+    'exec:sleep',
+    'exec:e2e-servers',
+    'protractor:e2e-disable-control-flow',
   ]);
 
   grunt.registerTask('ci-e2e-disabled', 'Run e2e tests for CI', [
