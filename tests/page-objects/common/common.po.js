e9012a3794fbb463bf7d135e99b492c924d5242b
Nick Christenson <emailfornc@gmail.com>
2021-01-12 09:23:05 -0500

    remove console log

--- tests/page-objects/common/common.po.js
+++ tests/page-objects/common/common.po.js
@@ -106,11 +106,9 @@ module.exports = {
   },
 
   goToLoginPageNative: async () => {
-    console.log('go to login');
     await browser.manage().deleteAllCookies();
     await browser.driver.get(await utils.getLoginUrl());
     await browser.driver.get(await utils.getLoginUrl());
-    console.log('go to login fin');
   },
 
   goToMessages: () => {
