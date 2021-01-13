3937033ff75719064f69752c7712de08f268b1fc
Nick Christenson <emailfornc@gmail.com>
2021-01-13 17:58:04 -0500

    merging tests results into 1 result, ptting async/await on login, remove the timestamp on the html since the reports are all separate

--- tests/e2e/login/login.specs.js
+++ tests/e2e/login/login.specs.js
@@ -7,23 +7,23 @@ describe('Login tests : ', () => {
   const wrongPassword = 'fakepass';
 
   it('should have a title', async () => {
-    await commonElements.goToLoginPageNative();
+    await commonElements.goToLoginPage();
     expect(await browser.getTitle()).toEqual('Medic Mobile');
   });
 
   it('should try to sign in with blank password and verify that credentials were incorrect', async () => {
-    await commonElements.goToLoginPageNative();
-    await loginPage.loginNative(wrongUsername, '', true);
+    await commonElements.goToLoginPage();
+    await loginPage.login(wrongUsername, '', true);
   });
 
   it('should try to sign in with blank username and password and verify that credentials were incorrect', async () => {
-    await commonElements.goToLoginPageNative();
-    await loginPage.loginNative('', '', true);
+    await commonElements.goToLoginPage();
+    await loginPage.login('', '', true);
   });
 
   it('should try to sign in and verify that credentials were incorrect', async () => {
-    await commonElements.goToLoginPageNative();
-    await loginPage.loginNative(wrongUsername, wrongPassword, true);
-    await loginPage.loginNative(auth.username, auth.password);
+    await commonElements.goToLoginPage();
+    await loginPage.login(wrongUsername, wrongPassword, true);
+    await loginPage.login(auth.username, auth.password);
   });
 });
