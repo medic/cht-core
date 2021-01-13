6989b4f85b21ab92b9d82faa58fe89c31b696584
Nick Christenson <emailfornc@gmail.com>
2021-01-13 11:10:47 -0500

    moved items to PO, refactor to use async/await, create some new methods and deprecate their old versions

--- tests/page-objects/reports/reports.po.js
+++ tests/page-objects/reports/reports.po.js
@@ -12,6 +12,8 @@ const firstForm = element(by.css('.action-container .general-actions .dropup.ope
 const reportListID = '#reports-list';
 
 module.exports = {
+  firstForm,
+  submitReport,
   allReports: () => element.all(by.css(`${reportListID} li`)),
   firstReport: () => element(by.css(`${reportListID} li:first-child`)),
   listLoader: () => element(by.css(`${reportListID} .loader`)),
