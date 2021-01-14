f7c195dea973fb60329a6e3f140cb7512cb26d43
Jennifer Q <66472237+latin-panda@users.noreply.github.com>
2021-01-14 14:36:49 +0700

    Migrates database monitor & deceased contact title (#6834)
    Migrated database connection monitor service and database closed modal.
    Fixed deceased contact title in mobile.
    
    medic/angular10-migration#94
    medic/angular10-migration#104

--- webapp/src/ts/modals/database-closed/database-closed.component.ts
+++ webapp/src/ts/modals/database-closed/database-closed.component.ts
@@ -4,18 +4,19 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 import { MmModalAbstract } from '@mm-modals/mm-modal/mm-modal';
 
 @Component({
-  selector: 'reloading-modal',
-  templateUrl: './reloading.component.html'
+  selector: 'database-closed',
+  templateUrl: './database-closed.component.html'
 })
-export class ReloadingComponent extends MmModalAbstract {
+export class DatabaseClosedComponent extends MmModalAbstract {
+  static id = 'database-closed-modal';
+
   constructor(bsModalRef: BsModalRef) {
     super(bsModalRef);
   }
-
-  static id = 'reloading-modal';
 
   submit() {
     this.close();
     window.location.reload();
   }
+
 }
