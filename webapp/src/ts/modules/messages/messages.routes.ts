c75c74eafa79b3e7c5c7a6fdc9f03d574c3cafae
latin-panda <66472237+latin-panda@users.noreply.github.com>
2021-01-19 14:24:24 +0700

    added explanatory comment to router.

--- webapp/src/ts/modules/messages/messages.routes.ts
+++ webapp/src/ts/modules/messages/messages.routes.ts
@@ -15,6 +15,12 @@ export const routes: Routes = [
         path: '',
         component: MessagesContentComponent
       },
+      /** This child route with matcher will redirect from /messages/[uuid] to /messages/contacts:[uuid]
+       * Ignoring any extra URL parameters because messages-content component is
+       * currently using only one "type_id" ( format: [type]:[uuid] )
+       * Original ticket: https://github.com/medic/cht-core/issues/4024
+       * Original commit: https://github.com/medic/cht-core/commit/4c6ffce10b61f8dafdbb9d48e6bf9d7d5f36d200
+       */
       {
         matcher: (url: UrlSegment[]) => {
           if (url[0]?.path?.indexOf(':') === -1) {
