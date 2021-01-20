4f48d4207f0facbf54478ec556de0d57c11d10c6
Nick Christenson <emailfornc@gmail.com>
2021-01-20 10:19:36 -0500

    removed space

--- tests/conf.js
+++ tests/conf.js
@@ -18,7 +18,7 @@ const baseConfig = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   //Enabling specs as they are working.
   suites: {
-    // e2e:'e2e/**/*.js', 
+    // e2e:'e2e/**/*.js',
     e2e: [
       '**/report-date-filter.js',
       '**/sentinel/queue.spec.js',
