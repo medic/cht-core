78453e7d50178febd198a217b49d5040c1e69642
joymkimmel <49628929+joymkimmel@users.noreply.github.com>
2021-01-21 16:21:51 +0800

    Add files via upload
    new cebuano/bisaya, hiligaynon/ilonggo, and tagalog datepicker files.

---
+++ webapp/src/js/enketo/bootstrap-datepicker.hil.js
@@ -0,0 +1,17 @@
+/**
+ * Philippines Ilonggo/Hiligaynon translation for bootstrap-datepicker
+ */
+;(function($){
+	$.fn.datepicker.dates['hil-ph'] = {
+		days: ["Domingo", "Lunes", "Martes", "Miyerkules", "Huwebes", "Biyernes", "Sabado"],
+		daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+		daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+        months: ["Enero", "Pebrero", "Marso", "Abril", "Mayo", "Hunyo", "Hulyo", "Agosto", "Setyembre", "Oktubre", "Nobyembre", "Disyembre"],
+        monthsShort: ["Ene", "Peb", "Mar", "Abr", "May", "Hun", "Hul", "Ago", "Set", "Okt", "Nob", "Dis"],
+		today: "Subong nga Adlaw",
+		monthsTitle: "Mga Bulan",
+		clear: "Panaon",
+		weekStart: 0,
+		format: "m/d/yyyy"
+	};
+}(jQuery));
