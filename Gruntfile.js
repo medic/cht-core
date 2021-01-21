1aef4c7df41a3e2d1806a6bdc34578d42f90f3f9
Nick Christenson <emailfornc@gmail.com>
2021-01-21 16:19:22 -0500

    setting hardcoded version

--- Gruntfile.js
+++ Gruntfile.js
@@ -506,10 +506,10 @@ module.exports = function(grunt) {
       },
       'start-webdriver': {
         cmd:
-          'mkdir -p tests/logs && ' +
-          './node_modules/.bin/webdriver-manager update && ' +
-          './node_modules/.bin/webdriver-manager start > tests/logs/webdriver.log & ' +
-          'until nc -z localhost 4444; do sleep 1; done',
+        'mkdir -p tests/logs && ' +
+        './node_modules/.bin/webdriver-manager update --versions.chrome=87.0.4280.88 && ' +
+        './node_modules/.bin/webdriver-manager start --versions.chrome=87.0.4280.88 > tests/logs/webdriver.log & ' +
+        'until nc -z localhost 4444; do sleep 1; done',
       },
       'check-env-vars':
         'if [ -z $COUCH_URL ] || [ -z $COUCH_NODE_NAME ]; then ' +
