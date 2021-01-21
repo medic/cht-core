d84863f416c4f856fcab7dc01286ecb05ee9dca7
Diana Barsan <barsan@medicmobile.org>
2021-01-21 16:26:40 +0200

    Fixes linting, changes locales to the agreed ones and actually loads the 3 new locales.

--- webapp/src/js/enketo/bootstrap-datepicker.hil.js
+++ webapp/src/js/enketo/bootstrap-datepicker.hil.js
@@ -1,17 +1,30 @@
 /**
  * Philippines Ilonggo/Hiligaynon translation for bootstrap-datepicker
  */
-;(function($){
-	$.fn.datepicker.dates['hil-ph'] = {
-		days: ["Domingo", "Lunes", "Martes", "Miyerkules", "Huwebes", "Biyernes", "Sabado"],
-		daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-		daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
-        months: ["Enero", "Pebrero", "Marso", "Abril", "Mayo", "Hunyo", "Hulyo", "Agosto", "Setyembre", "Oktubre", "Nobyembre", "Disyembre"],
-        monthsShort: ["Ene", "Peb", "Mar", "Abr", "May", "Hun", "Hul", "Ago", "Set", "Okt", "Nob", "Dis"],
-		today: "Subong nga Adlaw",
-		monthsTitle: "Mga Bulan",
-		clear: "Panaon",
-		weekStart: 0,
-		format: "m/d/yyyy"
-	};
+(function($){
+  $.fn.datepicker.dates['hil'] = {
+    days: ['Domingo', 'Lunes', 'Martes', 'Miyerkules', 'Huwebes', 'Biyernes', 'Sabado'],
+    daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    months: [
+      'Enero',
+      'Pebrero',
+      'Marso',
+      'Abril',
+      'Mayo',
+      'Hunyo',
+      'Hulyo',
+      'Agosto',
+      'Setyembre',
+      'Oktubre',
+      'Nobyembre',
+      'Disyembre',
+    ],
+    monthsShort: ['Ene', 'Peb', 'Mar', 'Abr', 'May', 'Hun', 'Hul', 'Ago', 'Set', 'Okt', 'Nob', 'Dis'],
+    today: 'Subong nga Adlaw',
+    monthsTitle: 'Mga Bulan',
+    clear: 'Panaon',
+    weekStart: 0,
+    format: 'm/d/yyyy'
+  };
 }(jQuery));
--- webapp/src/js/enketo/bootstrap-datepicker.tl.js
+++ webapp/src/js/enketo/bootstrap-datepicker.tl.js
@@ -2,17 +2,30 @@
 /**
  * Philippines Tagalog translation for bootstrap-datepicker
  */
-;(function($){
-	$.fn.datepicker.dates['tl-ph'] = {
-		days: ["Linggo", "Lunes", "Martes", "Miyerkules", "Huwebes", "Biyernes", "Sabado"],
-		daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-		daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
-		months: ["Enero", "Pebrero", "Marso", "Abril", "Mayo", "Hunyo", "Hulyo", "Agosto", "Setyembre", "Oktubre", "Nobyembre", "Disyembre"],
-        monthsShort: ["Ene", "Peb", "Mar", "Abr", "May", "Hun", "Hul", "Ago", "Set", "Okt", "Nob", "Dis"],
-		today: "Ngayon Araw",
-		monthsTitle: "Mga Buwan",
-		clear: "Burahin",
-		weekStart: 0,
-		format: "m/d/yyyy"
-	};
+(function($){
+  $.fn.datepicker.dates['tl'] = {
+    days: ['Linggo', 'Lunes', 'Martes', 'Miyerkules', 'Huwebes', 'Biyernes', 'Sabado'],
+    daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    months: [
+      'Enero',
+      'Pebrero',
+      'Marso',
+      'Abril',
+      'Mayo',
+      'Hunyo',
+      'Hulyo',
+      'Agosto',
+      'Setyembre',
+      'Oktubre',
+      'Nobyembre',
+      'Disyembre',
+    ],
+    monthsShort: ['Ene', 'Peb', 'Mar', 'Abr', 'May', 'Hun', 'Hul', 'Ago', 'Set', 'Okt', 'Nob', 'Dis'],
+    today: 'Ngayon Araw',
+    monthsTitle: 'Mga Buwan',
+    clear: 'Burahin',
+    weekStart: 0,
+    format: 'm/d/yyyy'
+  };
 }(jQuery));
