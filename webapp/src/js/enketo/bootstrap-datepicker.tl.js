debf05995117463c760e7fd86a9f78d9728288b2
Diana Barsan <35681649+dianabarsan@users.noreply.github.com>
2021-01-22 08:47:21 +0200

    Adds bootstrap datepicker locales (#6868)
    Cherry picks locales from #6865
    Fixes linting, enables the languages and normalizes locale codes, as agreed upon here: #6861 (comment)
    
    #6849
    
    (cherry picked from commit 239eae0935cc65c96f8e510faa3a5319498fa41d)

---
+++ webapp/src/js/enketo/bootstrap-datepicker.tl.js
@@ -0,0 +1,31 @@
+
+/**
+ * Philippines Tagalog translation for bootstrap-datepicker
+ */
+(function($){
+  $.fn.datepicker.dates['tl'] = {
+    days: ['Linggo', 'Lunes', 'Martes', 'Miyerkules', 'Huwebes', 'Biyernes', 'Sabado'],
+    daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    months: [
+      'Enero',
+      'Pebrero',
+      'Marso',
+      'Abril',
+      'Mayo',
+      'Hunyo',
+      'Hulyo',
+      'Agosto',
+      'Setyembre',
+      'Oktubre',
+      'Nobyembre',
+      'Disyembre',
+    ],
+    monthsShort: ['Ene', 'Peb', 'Mar', 'Abr', 'May', 'Hun', 'Hul', 'Ago', 'Set', 'Okt', 'Nob', 'Dis'],
+    today: 'Ngayon Araw',
+    monthsTitle: 'Mga Buwan',
+    clear: 'Burahin',
+    weekStart: 0,
+    format: 'm/d/yyyy'
+  };
+}(jQuery));
