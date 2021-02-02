57a1dddbd7ac947b934f9ad479abdb30a28876ab
Nick Christenson <10531066+newtewt@users.noreply.github.com>
2021-02-02 15:50:09 -0500

    61 add new health center and add person (#6889)
    moved add new health center and add person to use async await, updated for angular 10

--- tests/conf-disabled.js
+++ tests/conf-disabled.js
@@ -27,6 +27,8 @@ const baseConfig = {
       '**/docs-by-replication-key-view.js',
       'e2e/submit-enketo-form.js',
       'e2e/forms/submit-z-score-form.spec.js',
+      'e2e/contacts/add-new-health-center.js',
+      'e2e/contacts/add-new-person.js'
     ],
     // performance: 'performance/**/*.js'
   },
