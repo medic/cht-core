6bb799c0dd63e1a400070e12d6d99b6219ac0af5
Nick Christenson <10531066+newtewt@users.noreply.github.com>
2021-02-02 17:23:25 -0500

    Refactors public form transitions again (#6887)
    refactored to use async await and changes for angular 10 on public_form_transitions

--- tests/utils.js
+++ tests/utils.js
@@ -474,10 +474,10 @@ const deleteUsersNative = async (users, meta = false) => {
     .map(row => row.value && ({ _id: row.id, _rev: row.value.rev, _deleted: true }))
     .filter(stub => stub);
 
-  
+
   await  requestNative({ path: '/_users/_bulk_docs', method: 'POST', body: { docs: toDelete } });
   await  requestNative({ path: `/${constants.DB_NAME}/_bulk_docs`, method: 'POST', body: { docs: toDeleteMedic } });
-  
+
 
   if (!meta) {
     return;
@@ -589,6 +589,7 @@ module.exports = {
   }),
 
   requestOnTestDb: (options, debug) => {
+    deprecated('requestOnTestDb','requestOnTestDbNative');
     if (typeof options === 'string') {
       options = {
         path: options,
@@ -617,6 +618,7 @@ module.exports = {
   },
 
   requestOnTestMetaDb: (options, debug) => {
+    deprecated('requestOnTestMetaDb','requestOnTestMetaDbNative');
     if (typeof options === 'string') {
       options = {
         path: options,
@@ -669,8 +671,8 @@ module.exports = {
   },
 
   saveDocs: docs => {
-    deprecated('utils.saveDocs', 'utils.saveDocsNative');
-    module.exports
+    deprecated('saveDocs','saveDocsNative');
+    return module.exports
       .requestOnTestDb({
         path: '/_bulk_docs',
         method: 'POST',
@@ -682,7 +684,7 @@ module.exports = {
         } else {
           return results;
         }
-      }).catch();
+      });
   },
 
   saveDocsNative: async (docs) =>{
@@ -701,6 +703,7 @@ module.exports = {
   },
 
   getDoc: id => {
+    deprecated('getDoc','getDocNative');
     deprecated('utils.getDoc', 'utils.getDocNative');
     return module.exports.requestOnTestDbNative({
       path: `/${id}`,
@@ -716,6 +719,7 @@ module.exports = {
   },
 
   getDocs: ids => {
+    deprecated('getDocs','getDocsNative');
     return module.exports
       .requestOnTestDb({
         path: `/_all_docs?include_docs=true`,
@@ -724,6 +728,17 @@ module.exports = {
         headers: { 'content-type': 'application/json' },
       })
       .then(response => response.rows.map(row => row.doc));
+  },
+
+  getDocsNative: async ids => {
+    const response = await module.exports
+      .requestOnTestDbNative({
+        path: `/_all_docs?include_docs=true`,
+        method: 'POST',
+        body: { keys: ids || []},
+        headers: { 'content-type': 'application/json' },
+      });
+    return response.rows.map(row => row.doc);
   },
 
   deleteDoc: id => {
@@ -772,13 +787,15 @@ module.exports = {
    * @param      {Boolean}  ignoreRefresh  don't bother refreshing
    * @return     {Promise}  completion promise
    */
-  updateSettings: (updates, ignoreRefresh = false) =>
-    updateSettings(updates).then(() => {
+  updateSettings: (updates, ignoreRefresh = false) => {
+    deprecated('updateSettings','updateSettingsNative');
+    return updateSettings(updates).then(() => {
       if (!ignoreRefresh) {
         return refreshToGetNewSettings();
       }
-    }),
-  
+    });
+  },
+
   updateSettingsNative: async (updates, ignoreRefresh = false) => {
     await updateSettingsNative(updates);
     if (!ignoreRefresh) {
@@ -828,7 +845,7 @@ module.exports = {
    * and also returns a promise - pick one!
    */
   afterEach: done => {
-
+    deprecated('afterEach','afterEachNative');
     return revertDb()
       .then(() => {
         if (done) {
