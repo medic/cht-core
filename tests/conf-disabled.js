3345cbe96ac6e448d997738c88dc6de329a3833e
Nick Christenson <emailfornc@gmail.com>
2021-02-09 15:28:04 -0500

    moved test to async/await, using new and existing pageobject elements

--- tests/conf-disabled.js
+++ tests/conf-disabled.js
@@ -36,6 +36,7 @@ const baseConfig = {
       'e2e/forms/submit-delivery-form.specs.js',
       'e2e/users/add-user.specs.js',
       'e2e/login/purge.spec.js',
+      'e2e/registration-by-sms.js',
     ],
     // performance: 'performance/**/*.js'
   },
