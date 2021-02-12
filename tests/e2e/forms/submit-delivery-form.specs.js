4163796d783c43cbf4661e82d1cce0e3e808362c
Nick Christenson <10531066+newtewt@users.noreply.github.com>
2021-02-12 09:30:29 -0500

    86 registration by sms async (#6912)
    angular 10 and async/await changes for registrations by sms

--- tests/e2e/forms/submit-delivery-form.specs.js
+++ tests/e2e/forms/submit-delivery-form.specs.js
@@ -11,7 +11,7 @@ describe('Submit Delivery Report', () => {
   'time to refer them for PNC. Please monitor them for danger signs. Thank you!';
 
   beforeAll(async () => {
-    await Promise.all(docs.map(utils.saveDocNative));
+    await Promise.all(docs.map(async doc => await utils.saveDocNative(doc)));
     await deliveryReport.configureForm(userContactDoc);
   });
 
