296fc2ca7665b5c5c8e036f5d74be535e1cd89c7
Nick Christenson <emailfornc@gmail.com>
2021-02-17 10:36:13 -0500

    everything moved to PO

--- tests/page-objects/messages/messages.po.js
+++ tests/page-objects/messages/messages.po.js
@@ -1,10 +1,28 @@
 const helper = require('../../helper');
 const utils = require('../../utils');
+const common = require('../common/common.po');
 
 const sendMessageButton = element(by.className('mm-icon mm-icon-inverse mm-icon-caption send-message')); 
 const exportButton = element(by.css('[ng-click=actionBar.left.exportFn()]'));
 const selectOptions = element.all(by.css('.select2-results__option'));
 const selectChoices = element.all(by.css('li.select2-selection__choice'));
+const sendAdditionalMessage = element(by.css('#message-footer .message-actions .btn-primary'));
+const messageTextArea = element(by.css('#message-footer textarea'));
+const addRecipient = element(by.css('.message-actions .btn.btn-link'));
+const selectedOption = element(by.css('#send-message select>option'));
+
+const openMessageContent = async (id, name) => {
+  await common.goToMessagesNative();
+  await helper.waitUntilReadyNative(module.exports.messageInList(id));
+  await helper.waitElementToPresentNative(module.exports.messageInList(id), 2000);
+  await clickLhsEntry(id, name);
+};
+
+const enterMessageText = async message => {
+  await element(by.css('#message-footer textarea')).click();
+  await helper.waitElementToBeVisible(sendAdditionalMessage);
+  await browser.wait(messageTextArea.sendKeys(message));
+};
 
 const searchSelect2 = async (searchText, totalExpectedResults, entrySelector, entryText) => {
   await module.exports.messageRecipientSelect().sendKeys(searchText);
@@ -19,9 +37,9 @@ const submitMessage = async () => {
   await helper.waitElementToDisappear(module.exports.sendMessageModal().locator());
 };
 
-const lastMessageIs = async message => {
-  const last = await element.all(by.css('#message-content li div.data>p>span')).last();
-  expect(await helper.getTextFromElement(last)).toBe(message);
+const lastMessageText = () => {
+  const last = element.all(by.css('#message-content li div.data>p>span')).last();
+  return helper.getTextFromElement(last);
 };
 
 const clickLhsEntry = async (entryId, entryName) => {
@@ -69,6 +87,7 @@ const enterCheckAndSelect = async (
 module.exports = {
   messageDetailsHeader: () => element(by.css('#message-header .name')),
   messageInList: identifier => element(by.css(`#message-list li[test-id="${identifier}"]`)),
+  allMessages: () => element.all(by.css('#message-content li')),
   messageByIndex: index => element(by.css(`#message-list li:nth-child(${index})`)),
   messageText: text => element(by.css('#send-message textarea')).sendKeys(text),
   sendMessage: () => element(by.css('.general-actions .send-message')),
@@ -91,7 +110,12 @@ module.exports = {
   searchSelect2,
   selectChoices,
   submitMessage,
-  lastMessageIs,
+  lastMessageText,
   clickLhsEntry,
-  enterCheckAndSelect
+  enterCheckAndSelect,
+  openMessageContent,
+  enterMessageText,
+  sendAdditionalMessage,
+  addRecipient,
+  selectedOption
 };
