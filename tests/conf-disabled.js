02a592afeb2a2dd49402352b0cd7e69911526604
Diana Barsan <35681649+dianabarsan@users.noreply.github.com>
2021-02-22 16:27:39 +0200

    Update conf-disabled.js

--- tests/conf-disabled.js
+++ tests/conf-disabled.js
@@ -24,8 +24,7 @@ const baseConfig = {
     // e2e:'e2e/**/*.js',
     e2e: [
       'e2e/api/**/*.js',  
-      'e2e/contacts/**/*.js',      
-      
+      'e2e/contacts/**/*.js',            
       //'e2e/login/**/*.js',
       //'e2e/login/**/db*.js',
       //'e2e/login/**/log*.js',     
