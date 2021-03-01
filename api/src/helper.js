bf8c8618589d7dbf6319f51cbf3869883312f3fe
henokgetachew <henygetachew@gmail.com>
2021-03-01 19:53:51 +0300

    Remove some comments

--- api/src/helper.js
+++ api/src/helper.js
@@ -15,7 +15,7 @@ const runAPIHelper = async () => {
   try
   {
     logger.info('Running server checks…');
-    await serverChecks.check(environment.serverUrl); //Should be in API
+    await serverChecks.check(environment.serverUrl); 
     logger.info('Checks passed successfully');
 
     logger.info('Extracting ddoc…');
@@ -23,28 +23,28 @@ const runAPIHelper = async () => {
     logger.info('DDoc extraction completed successfully');
 
     logger.info('Cleaning resources directory…');
-    resourceExtraction.removeDirectory(); //Init only. Actually, this won't be needed with arch v3 because there would be no directory to clean up or remove.
+    resourceExtraction.removeDirectory(); 
     logger.info('Cleaning resources directory completed successfully');
 
     logger.info('Extracting resources…');
-    await resourceExtraction.run();//Not needed with arch v3? default-docs (containing the default config) are saved here so it is needed. Static files are accessed from the api server and therefore, there won't be a need for extracting it from the ddoc.
+    await resourceExtraction.run();
     logger.info('Extracting resources completed successfully');
 
     logger.info('Extracting initial documents…');
-    await uploadDefaultDocs.run();//ONE time only. Because this uses the ddoc extraction and resource extraction, they are needed in the one time code as well.
+    await uploadDefaultDocs.run();
     logger.info('Extracting initial documents completed successfully');
 
     logger.info('Loading configuration…');
-    await config.load();//Can this run with more than one API instances? Does it makes sense to do so?
+    await config.load();
     logger.info('Configuration loaded successfully');
-    await config.listen(); //Not in init.
+    await config.listen();
 
-    logger.info('Merging translations…'); //Take translations saved in the ddoc and save them to the db. I think this is because another tool is used to add translations.
-    await translations.run();//Merging translations seems like it should still happen with archv3. Init only
+    logger.info('Merging translations…');
+    await translations.run();
     logger.info('Translations merged successfully');
 
     logger.info('Running db migrations…');
-    await migrations.run();//Init only
+    await migrations.run();
     logger.info('Database migrations completed successfully');
 
     logger.info('Updating xforms…');
