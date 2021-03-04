8eb06d65e3fc67b54934872ae6dc983af54a84af
Diana Barsan <barsan@medicmobile.org>
2021-03-04 19:43:19 +0200

    fix linting.

--- webapp/tests/karma/ts/components/filters/facility-filter.component.spec.ts
+++ webapp/tests/karma/ts/components/filters/facility-filter.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
