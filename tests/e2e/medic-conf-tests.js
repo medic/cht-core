56c60cc33a5ec5e122e6bbb928fdf8bf456b2a67
ngaruko <bede.ngaruko@gmail.com>
2021-03-30 20:08:24 +1300

    added medic-conf actions tests

---
+++ tests/e2e/medic-conf-tests.js
@@ -0,0 +1,44 @@
+const _ = require('lodash');
+const {expect} = require('chai');
+const util = require('util');
+const { API_PORT }=require('../constants');
+const exec = util.promisify(require('child_process').exec);
+
+const actions = ['compile-app-settings','backup-app-settings','upload-app-settings','convert-app-forms',
+  'convert-collect-forms','convert-contact-forms',
+  'backup-all-forms','upload-app-forms','upload-collect-forms','upload-contact-forms',
+  'upload-resources','upload-branding','upload-partners','upload-custom-translations','upload-privacy-policies'];
+
+describe('medic-conf supported actions', () => {
+  beforeAll( () =>{
+    // Change the directory
+    console.log('working directory end: ' + process.cwd());
+    try {
+      process.chdir('config/default');
+      console.log('working directory test: ' + process.cwd());
+    } catch (err) {
+      // Printing error if occurs
+      console.error('no need to change directory');
+    }
+  });
+
+  afterAll(() =>{
+    // Change the directory back to cht-core
+    process.chdir('../..');
+    console.log('working directory end: ' + process.cwd());
+  });
+
+  const runCommand = async (action) => {
+    const { stdout, stderr } = await exec(`medic-conf --url=http://admin:pass@localhost:${API_PORT} ${action} --force`);
+    if (stderr) {
+      console.error(`error: ${stderr}`);
+    }
+    return stdout;
+  };
+  _.forEach(actions,  async (action)  =>{
+    it(`should execute  ${action} `, async () => {
+      const result = await runCommand(action);
+      expect(result).to.contain(`INFO ${action} complete.`);
+    });
+  });
+});
