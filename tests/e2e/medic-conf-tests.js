5a798a8ec7369089724266303f64fa54acce9356
ngaruko <bede.ngaruko@gmail.com>
2021-04-04 19:32:10 +1200

    run upload-app-settings separately

--- tests/e2e/medic-conf-tests.js
+++ tests/e2e/medic-conf-tests.js
@@ -4,10 +4,10 @@ const util = require('util');
 const { API_PORT }=require('../constants');
 const exec = util.promisify(require('child_process').exec);
 
-const actions = ['compile-app-settings','backup-app-settings','upload-app-settings','convert-app-forms',
+const actions = ['compile-app-settings','backup-app-settings','convert-app-forms',
   'convert-collect-forms','convert-contact-forms', 'backup-all-forms','upload-app-forms','upload-collect-forms',
   'upload-contact-forms', 'upload-resources','upload-branding','upload-partners','upload-custom-translations',
-  'upload-privacy-policies', 'delete-all-forms'];
+  'upload-privacy-policies'];
 
 describe('medic-conf supported actions', () => {
   beforeAll( () =>{
@@ -28,10 +28,19 @@ describe('medic-conf supported actions', () => {
     const { stdout, stderr } = await exec(`medic-conf --url=http://admin:pass@localhost:${API_PORT} ${action} --force`);
     if (stderr) {
       console.error(`error: ${stderr}`);
-      return;
     }
     return stdout;
   };
+
+  it('should execute  upload-app-settings', async () => {
+    try {
+      const result = await runCommand('upload-app-settings');
+      expect(result).to.contain(`INFO Settings updated successfully`);
+    } catch (error) {
+      console.log(error.stdout);
+    }
+  });
+	
   _.forEach(actions,  async (action)  =>{
     it(`should execute  ${action} `, async () => {
       try {
