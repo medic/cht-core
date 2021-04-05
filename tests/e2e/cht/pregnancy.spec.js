eb34b089442ce5c644be1828416bd741f03a5041
Nick Christenson <emailfornc@gmail.com>
2021-04-05 17:01:13 -0400

    added elements for contacts page, expecting tasks, expecting on cards, nav to analytircs and asserts

--- tests/e2e/cht/pregnancy.spec.js
+++ tests/e2e/cht/pregnancy.spec.js
@@ -4,6 +4,8 @@ const loginPage = require('../../page-objects/login/login.po');
 const contactsPage = require('../../page-objects/contacts/contacts.po');
 const helper = require('../../helper');
 const pregnancyFormPo = require('../../page-objects/forms/cht/pregnancy-form.po');
+const analyticsPo = require('../../page-objects/analytics/analytics.po');
+const { browser } = require('protractor');
 
 const password = 'Secret_1';
 const district = {
@@ -78,7 +80,6 @@ describe('Pregnancy workflow on cht : ', () => {
   });
 
   it('should register a pregnancy', async () => {
-    await console.log('do');
     await commonElements.goToLoginPageNative();
     await loginPage.loginNative('user1', password);
     await utils.closeTour();
@@ -88,6 +89,34 @@ describe('Pregnancy workflow on cht : ', () => {
     await helper.clickElementNative(contactsPage.newActions);
     await helper.clickElementNative(contactsPage.formById('pregnancy'));
     await pregnancyFormPo.fillPregnancyForm();
-    
+    const activePregnancyCard = await contactsPage.cardElementByHeaderText('Active Pregnancy');
+    await helper.waitUntilReadyNative(activePregnancyCard);
+    const activePregnancyCardValues = await contactsPage.cardChildrenValueArray(activePregnancyCard);
+    expect(activePregnancyCardValues[0]).toBe('11');
+    //TODO: This needs to be calculated
+    expect(activePregnancyCardValues[1]).toBe('25 Oct, 2021');
+    expect(activePregnancyCardValues[2]).toBe('0 of 8');
+    expect(activePregnancyCardValues[3]).toBe('today');
+    const tasks = await contactsPage.taskNames();
+    expect(tasks[0]).toBe('Pregnancy home visit');
+    await commonElements.goToAnalytics();
+    const pregnancyRegistrations = analyticsPo.targetById('pregnancy-registrations-this-month');
+    await helper.waitUntilReadyNative(pregnancyRegistrations);
+    const pregnancyCount = await pregnancyRegistrations.element(by.css('.number')).getText();
+    const pregnancyGoal = await pregnancyRegistrations.element(by.css('.goal p')).getText();
+    const pregnancyTitle = await pregnancyRegistrations.element(by.css('.title h2')).getText();
+    expect(pregnancyTitle).toBe('New pregnancies');
+    expect(pregnancyCount).toBe('1');
+    expect(pregnancyGoal).toBe('20');
+    const activePregnancies = analyticsPo.targetById('active-pregnancies');
+    await helper.waitUntilReadyNative(activePregnancies);
+    const activeTitle = await activePregnancies.element(by.css('.title h2')).getText();
+    const activeCount = await activePregnancies.element(by.css('.number')).getText();
+    expect(activeTitle).toBe('Active pregnancies');
+    expect(activeCount).toBe('1');
+    await browser.get(utils.getBaseUrl() + 'contacts/' + pregnancy_woman._id);
+    await helper.clickElementNative(contactsPage.newActions);
+    await helper.clickElementNative(contactsPage.formById('pregnancy_home_visit'));
+    console.log('stop');
   });
 });
