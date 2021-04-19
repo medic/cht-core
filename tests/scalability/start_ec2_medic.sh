6ab404407a830209c25a2a57f26104c519e8115c
Nick Christenson <emailfornc@gmail.com>
2021-04-19 09:27:11 -0400

    remove curly

--- tests/scalability/start_ec2_medic.sh
+++ tests/scalability/start_ec2_medic.sh
@@ -33,7 +33,7 @@ fi
 
 url=https://$PublicDnsName
 
-echo "{MEDIC_URL}={$url}" >> $GITHUB_ENV
+echo "MEDIC_URL=$url" >> $GITHUB_ENV
 
 echo Begin Checking $url/api/info is up
 version=$(curl -s $url/api/info -k  | jq .version -r)
