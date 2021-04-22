5499f14f97221aa6982fcd89551323542792a1dd
ngaruko <bede.ngaruko@gmail.com>
2021-04-22 20:54:20 +1200

    separate form validation tests

--- tests/e2e/medic-conf/medic-conf-actions.spec.js
+++ tests/e2e/medic-conf/medic-conf-actions.spec.js
@@ -6,7 +6,6 @@ const util = require('util');
 const { API_PORT, API_HOST } = require('../../constants');
 const {username, password} = require('../../auth')();
 const exec = util.promisify(require('child_process').exec);
-const Excel = require('exceljs/dist/es5');
 const url = `http://${username}:${password}@${API_HOST}:${API_PORT}`;
 
 const actions = ['compile-app-settings','backup-app-settings','convert-app-forms',
@@ -15,21 +14,13 @@ const actions = ['compile-app-settings','backup-app-settings','convert-app-forms
   'upload-privacy-policies'];
 
 describe('Medic-conf actions tests', () => {
-  let originalTimeout;
   beforeAll(  () =>{
-    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000;
-
     // Change the directory
     try {
       process.chdir('config/default');
     } catch (err) {
       console.error('no need to change directory');
     }
-  });
-
-  afterAll(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
 
   const runCommand = async (action) => {
@@ -41,7 +32,7 @@ describe('Medic-conf actions tests', () => {
     const result = await runCommand('upload-app-settings');
     expect(result).to.contain(`INFO Settings updated successfully`);
   });
-
+	
   // eslint-disable-next-line guard-for-in
   for(const action in actions){
     it(`should execute  ${actions[action]} `, async () => {
@@ -49,18 +40,4 @@ describe('Medic-conf actions tests', () => {
       expect(result).to.contain(`INFO ${actions[action]} complete.`);
     });
   }
-
-  it('should validate forms before execution', async ()=> {
-    const file = 'forms/contact/person-create.xlsx';
-    //change one form
-    const workbook = new Excel.Workbook();
-    await workbook.xlsx.readFile(file)
-      .then(() => {
-        const worksheet = workbook.getWorksheet('survey');
-        worksheet.getCell('B2').value = 'submission';
-        return workbook.xlsx.writeFile(file);
-      });
-    await expect(exec(`medic-conf --url=${url} --force`)).to.be.
-      eventually.rejectedWith('ERROR: Submissions element(s) not supported');
-  });
 });
