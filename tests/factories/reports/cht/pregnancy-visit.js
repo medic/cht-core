f5ce9b83872bdb5814ffeb4afcaed46f67660b78
Nick Christenson <emailfornc@gmail.com>
2021-04-27 12:51:45 -0400

    added the rest of the forms in facotries, moved form filling functions into their own module, uncomment and use facotries to finish the spec

--- tests/factories/reports/cht/pregnancy-visit.js
+++ tests/factories/reports/cht/pregnancy-visit.js
@@ -14,76 +14,75 @@ const defaultFields = {
     },
     'source': 'contact',
     'source_id': '',
-    'user': {
-      'contact_id': '2e0ceb06-ced2-5a63-bca0-0283a5aab0e8',
-      'parent': {
-        '_id': '8935e9d8-0b2a-5eb0-ae54-ad1377a60097'
-      }
-    },
     'contact': {
-      '_id': '80b6e34e-7abf-5718-a572-84edbcd4593e',
-      'name': 'Adelia Akiyama',
+      '_id': '091d10d9-2f5c-4965-9c20-96730c6fc300',
+      'name': 'James',
       'short_name': '',
-      'patient_id': '10072',
-      'date_of_birth': '2000-01-01',
+      'patient_id': '96854',
+      'date_of_birth': '1997-11-18',
       'sex': 'female',
       'parent': {
-        '_id': '3a9cecea-8414-5768-9846-474d21b51e91',
+        '_id': '8935e9d8-0b2a-5eb0-ae54-ad1377a60097',
         'parent': {
           'contact': {
             'chw_name': '',
-            'phone': '+9779841299392'
+            'phone': ''
           }
         }
       }
     }
   },
-  'patient_age_in_years': '21',
-  'patient_uuid': '80b6e34e-7abf-5718-a572-84edbcd4593e',
-  'patient_id': '10072',
-  'patient_name': 'Adelia Akiyama',
+  'patient_age_in_years': '23',
+  'patient_uuid': '091d10d9-2f5c-4965-9c20-96730c6fc300',
+  'patient_id': '96854',
+  'patient_name': 'James',
   'patient_short_name': 'the woman',
   'patient_short_name_start': 'The woman',
-  'lmp_date_8601': '2021-02-02',
-  'edd_8601': '2021-11-09',
-  'days_since_lmp': '77',
+  'lmp_date_8601': '2020-09-01',
+  'edd_8601': '2020-09-01',
+  'days_since_lmp': '238',
   'weeks_since_lmp': '34',
   'weeks_since_lmp_rounded': '34',
-  'lmp_method_approx': 'yes',
-  'hiv_status_known': 'no',
-  'deworming_med_received': '',
-  'tt_received': 'no',
+  'lmp_method_approx': 'no',
+  'lmp_updated': 'no',
   't_pregnancy_follow_up_date': '',
   't_pregnancy_follow_up': 'no',
-  't_danger_signs_referral_follow_up_date': '2021-04-23T00:00:00.000-04:00',
+  't_danger_signs_referral_follow_up_date': '2021-04-30T00:00:00.000-04:00',
   't_danger_signs_referral_follow_up': 'no',
-  'gestational_age': {
-    'register_method': {
-      'register_note': '',
-      'lmp_method': 'method_approx'
-    },
-    'method_approx': {
-      'lmp_approx': 'approx_weeks',
-      'lmp_approx_weeks': '34'
-    },
-    'method_lmp_summary': {
-      'lmp_approx_weeks_check_note': '',
-      'edd_note': '',
-      'edd_check_note': ''
-    },
-    'g_lmp_date_8601': '2021-02-02',
-    'g_lmp_date': '2 Feb, 2021',
-    'g_edd_8601': '2021-11-09',
-    'g_edd': '9 Nov, 2021'
+  'hiv_status_known': 'yes',
+  'tt_received': '',
+  'deworming_med_received': 'yes',
+  'context_vars': {
+    'lmp_date_8601_ctx': '2020-09-01',
+    'lmp_method_approx_ctx': 'yes',
+    'pregnancy_follow_up_date_recent_ctx': '',
+    'risk_factors_ctx': '',
+    'risk_factor_extra_ctx': '',
+    'hiv_tested_ctx': 'no',
+    'tt_received_ctx': 'no',
+    'deworming_med_received_ctx': '',
+    'edd_ctx': '8 Jun, 2021',
+    'weeks_since_lmp_rounded_ctx': '34',
+    'pregnancy_uuid_ctx': 'b618d88b-fae5-4ab7-8289-a0416bd7294d'
+  },
+  'pregnancy_summary': {
+    'current_weeks_pregnant': '',
+    'current_edd': '',
+    'visit_option': 'yes',
+    'g_age_correct': 'yes'
   },
   'anc_visits_hf': {
     'anc_visits_hf_past': {
-      'visited_hf_count': '1',
-      'visited_dates_group': {
-        'visited_count_confim_note_single': '',
-        'visited_date_ask_single': 'no',
-        'visited_dates_count': '1'
-      }
+      'pregnancy_follow_up_date_recent': '',
+      'report_other_visits': 'no',
+      'visited_dates_count': ''
+    },
+    'risk_factors': {
+      'previous_risk_factors_note': '',
+      'no_previous_risks_note': '',
+      'new_risks': 'none',
+      'additional_risk_check': 'no',
+      'r_risk_factor_present': 'no'
     },
     'anc_visits_hf_next': {
       'anc_next_visit_date': {
@@ -95,17 +94,6 @@ const defaultFields = {
         'refer_note': ''
       }
     }
-  },
-  'risk_factors': {
-    'risk_factors_history': {
-      'first_pregnancy': 'no',
-      'previous_miscarriage': 'no'
-    },
-    'risk_factors_present': {
-      'secondary_condition': 'none',
-      'additional_risk_check': 'no'
-    },
-    'r_risk_factor_present': 'no'
   },
   'danger_signs': {
     'danger_signs_note': '',
@@ -126,31 +114,26 @@ const defaultFields = {
   },
   'safe_pregnancy_practices': {
     'malaria': {
-      'uses_llin': 'no',
+      'llin_use': 'yes',
       'llin_advice_note': '',
       'malaria_prophylaxis_note': ''
     },
     'iron_folate': {
-      'iron_folate_daily': 'no',
+      'iron_folate_daily': 'yes',
       'iron_folate_note': ''
     },
-    'safe_practices_tips': {
-      'eat_more_note': ''
-    },
-    'hiv_status': {
-      'hiv_tested': 'no',
-      'hiv_importance_note': ''
-    },
-    'tetanus': {
-      'tt_imm_received': 'no',
-      'tt_note_1': '',
-      'tt_note_2': ''
-    },  
     'deworming': {
-      'deworming_med': 'no',
+      'deworming_med': 'yes',
       'deworming_med_note': ''
     },
-    'request_services': 'yes'
+    'safe_practices_tips': {
+      'deliver_hf_note': ''
+    },
+    'hiv_status': {
+      'hiv_tested': 'yes',
+      'hiv_importance_note': ''
+    },
+    'safe_pregnancy_practices': 'no'
   },
   'summary': {
     'r_submit_note': '',
@@ -160,18 +143,13 @@ const defaultFields = {
     'r_pregnancy_details': '',
     'r_space_1': '',
     'r_referrals': '',
-    'r_request_services': '',
-    'r_request_service_tt': '',
-    'r_request_service_hiv_test': '',
-    'r_request_service_iron': '',
     'r_who_recommends': '',
     'r_refer_hf_appropriate_time': '',
-    'r_space_2': '',
-    'check-': '',
+    'r_follow_up_tasks': '',
     'r_following_tasks': '',
     'r_fup_pregnancy_visit': '',
-    'next_visit_weeks': '1',
-    'edd_summary': '9 Nov, 2021',
+    'next_visit_weeks': '2',
+    'edd_summary': '1 Sep, 2020',
     'next_appointment_date': '',
     'custom_translations': {
       'custom_woman_label_translator': 'woman',
@@ -181,29 +159,30 @@ const defaultFields = {
     }
   },
   'data': {
-    '__lmp_method': 'method_approx',
-    '__no_lmp_registration_reason': '',
-    '__lmp_date': '2021-02-02',
-    '__lmp_approx_weeks': '34',
-    '__lmp_approx_months': '',
-    '__edd': '2021-11-09',
-    '__num_previous_anc_hf_visits': '1',
-    '__previous_anc_hf_visit_dates': '',
-    '__next_anc_hf_visit_date_known': 'no',
-    '__next_anc_hf_visit_date': '',
-    '__has_risk_factors': 'no',
-    '__first_pregnancy': 'no',
-    '__previous_miscarriage': 'no',
-    '__previous_difficulties': 'no',
-    '__more_than_4_children': 'no',
-    '__last_baby_born_less_than_1_year_ago': 'no',
+    '__activity_to_report': 'home_visit',
+    '__gestational_age_correct': 'yes',
+    '__miscarriage_date': '',
+    '__abortion_date': '',
+    '__visit_task_clear_option': '',
+    '__gestational_age_update_method': '',
+    '__gestational_age_update_weeks': '',
+    '__gestational_age_update_edd': '',
+    '__lmp_updated': 'no',
+    '__lmp_date_new': '',
+    '__edd_new': '',
+    '__last_visit_attended': '',
+    '__report_additional_anc_hf_visits': 'no',
+    '__num_additional_anc_hf_visits': '',
+    '__additional_anc_hf_visit_dates': '',
+    '__has_risk_factors_not_previously_reported': 'no',
     '__heart_condition': 'no',
     '__asthma': 'no',
     '__high_blood_pressure': 'no',
     '__diabetes': 'no',
     '__additional_high_risk_condition_to_report': 'no',
     '__additional_high_risk_condition': '',
-    '__has_danger_sign': 'no',
+    '__next_anc_hf_visit_date_known': 'no',
+    '__next_anc_hf_visit_date': '',
     '__vaginal_bleeding': 'no',
     '__fits': 'no',
     '__severe_abdominal_pain': 'no',
@@ -215,33 +194,39 @@ const defaultFields = {
     '__easily_tired': 'no',
     '__face_hand_swelling': 'no',
     '__breathlessness': 'no',
-    '__uses_llin': 'no',
-    '__takes_iron_folate_daily': 'no',
-    '__received_deworming_meds': '',
-    '__tested_for_hiv_in_past_3_months': 'no',
-    '__received_tetanus_toxoid_this_pregnancy': 'no',
+    '__has_danger_sign': 'no',
+    '__uses_llin': 'yes',
+    '__takes_iron_folate_daily': 'yes',
+    '__received_deworming_meds': 'yes',
+    '__tested_for_hiv_in_past_3_months': 'yes',
+    '__received_tetanus_toxoid_this_pregnancy': '',
     'meta': {
-      '__patient_uuid': '80b6e34e-7abf-5718-a572-84edbcd4593e',
-      '__patient_id': '10072',
-      '__household_uuid': '3a9cecea-8414-5768-9846-474d21b51e91',
+      '__patient_uuid': '091d10d9-2f5c-4965-9c20-96730c6fc300',
+      '__patient_id': '96854',
+      '__household_uuid': '8935e9d8-0b2a-5eb0-ae54-ad1377a60097',
       '__source': 'contact',
-      '__source_id': ''
+      '__source_id': '',
+      '__pregnancy_uuid': 'b618d88b-fae5-4ab7-8289-a0416bd7294d'
     }
   },
   'meta': {
-    'instanceID': 'uuid:09318528-a484-4d9d-b49c-473b84c22ae9'
+    'instanceID': 'uuid:ae917346-42a6-43a5-8372-5ed4d00eadee'
   }
 };
 
-Factory.define('pregnancy')
+
+
+
+Factory.define('pregnancyVisit')
   .sequence('_id',uuid.v4)
-  .attr('form', 'pregnancy')
+  .attr('form', 'pregnancy_home_visit')
   .attr('type', 'data_record')
   .attr('content_type', 'xml')
   .attr('reported_date', Date.now())
   .attr('contact', 'TODO')
   .attr('from', '')
-  .attr('hidden_fields', [
+  .attr('fields', defaultFields)
+  .attr('hidden_fields',[
     'patient_age_in_years',
     'patient_uuid',
     'patient_id',
@@ -254,39 +239,33 @@ Factory.define('pregnancy')
     'weeks_since_lmp',
     'weeks_since_lmp_rounded',
     'lmp_method_approx',
-    'hiv_status_known',
-    'deworming_med_received',
-    'tt_received',
+    'lmp_updated',
     't_pregnancy_follow_up_date',
     't_pregnancy_follow_up',
     't_danger_signs_referral_follow_up_date',
     't_danger_signs_referral_follow_up',
-    'gestational_age.register_method.register_note',
-    'gestational_age.method_approx.lmp_approx',
-    'gestational_age.method_lmp_summary',
-    'gestational_age.g_lmp_date_8601',
-    'gestational_age.g_lmp_date',
-    'gestational_age.g_edd_8601',
-    'gestational_age.g_edd',
-    'anc_visits_hf.anc_visits_hf_past.visited_dates_group.visited_count_confim_note_single',
+    'hiv_status_known',
+    'tt_received',
+    'deworming_med_received',
+    'context_vars',
+    'pregnancy_summary.current_weeks_pregnant',
+    'pregnancy_summary.current_edd',
+    'anc_visits_hf.risk_factors.previous_risk_factors_note',
+    'anc_visits_hf.risk_factors.no_previous_risks_note',
     'anc_visits_hf.anc_visits_hf_next.anc_visit_advice_note',
-    'risk_factors.r_risk_factor_present',
     'danger_signs.danger_signs_note',
     'danger_signs.r_danger_sign_present',
     'danger_signs.congratulate_no_ds_note',
     'safe_pregnancy_practices.malaria.llin_advice_note',
     'safe_pregnancy_practices.malaria.malaria_prophylaxis_note',
     'safe_pregnancy_practices.iron_folate.iron_folate_note',
+    'safe_pregnancy_practices.deworming.deworming_med_note',
     'safe_pregnancy_practices.safe_practices_tips',
     'safe_pregnancy_practices.hiv_status.hiv_importance_note',
-    'safe_pregnancy_practices.tetanus.tt_note_1',
-    'safe_pregnancy_practices.tetanus.tt_note_2',
-    'safe_pregnancy_practices.request_services',
     'summary',
     'data',
     'meta'
   ])
-  .attr('fields', defaultFields)
   .attr('geolocation_log', [
     {
       'timestamp': 1618841239430,
--- tests/page-objects/forms/cht/pregnancy-form-factory.po.js
+++ tests/page-objects/forms/cht/pregnancy-form-factory.po.js
@@ -1,33 +1,27 @@
-const Factory = require('rosie').Factory;
+// questions = [
+//     {
+//       css: 'gestational_age/register_method/lmp_method',
+//       type: 'button'
+//     },
+//     {
+//       css: 'gestational_age/method_approx/lmp_approx',
+//       type: 'text'
+//     },
+//     {
+//       css: 'gestational_age/method_approx/lmp_approx_months',
+//       type: 'button'
+//     }
+// ]
 
-Factory.define('pregnancy-form')
-  .option('howWouldYouLikeToReportValue', 'method_approx')
-  .attr('howWouldYouLikeToReport',['howWouldYouLikeToReportValue'], (howWouldYouLikeToReportValue) => {
-    return {
-      css: 'input[name="/pregnancy/gestational_age/register_method/lmp_method"]',
-      value: howWouldYouLikeToReportValue,
-      endOfPage: true
-    };
-  })
-  .option('weeksOrMonthsValue', 'weeks')
-  .attr('weeksOrMonths',['weeksOrMonthsValue'], (weeksOrMonthsValue) => {
-    const answers = {
-      weeks: 'approx_weeks',
-      months: 'approx_months'
-    };
-    return {
-      css: 'input[name="/pregnancy/gestational_age/method_approx/lmp_approx"]',
-      value: answers[weeksOrMonthsValue],
-      endOfPage: false
-    };
-  })
-  .option('weeksValue', 22)
-  .attr('numberOfWeeks', ['weeksValue'], (weeksValue) => {
-    return {
-      css: 'input[name="/pregnancy/gestational_age/method_approx/lmp_approx_weeks"]',
-      value: weeksValue,
-      textField: true,
-      endOfPage: true 
-    };
-  });
 
+
+// const fillForm: (report, questions = questions) => {
+//   questions.forEach(questions => {
+
+//   }
+// }
+
+
+//   module.exports = {
+//     fillForm
+//   }
