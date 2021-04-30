800813ec022edd5053af2d93a0e82dc3443cfabd
Nick Christenson <emailfornc@gmail.com>
2021-04-30 10:45:12 -0400

    using url to build instead of strings, updated messaging including errors

--- scripts/bulk-password-change.js
+++ scripts/bulk-password-change.js
@@ -12,8 +12,8 @@ if(argv.h || !argv.newPassword || !argv.url || !argv.user || !argv.password) {
   console.log(`Set all user passwords to the provided password.
 
 Usage:
-    ${process.argv[1]} -h | --help
-    ${process.argv[1]} --newPassword Secret_1 --url https://localhost --user medic --password adminPass
+      node bulk-password-change.js -h | --help
+      node bulk-password-change.js --newPassword Secret_1 --url https://localhost --user medic --password adminPass
 
 Options:
     -h --help     Show this screen.
@@ -26,13 +26,13 @@ Options:
   process.exit(0);
 }
 
-const url = argv.url;
+const url = new URL('/api/v1/users', argv.url);
 const user = argv.user;
 const password = argv.password;
 const newPassword = argv.newPassword;
 
 const options = {
-  uri: `${url}/api/v1/users`,
+  uri: url.href,
   json: true,
   headers: {
     'Authorization': 'Basic ' + Buffer.from(`${user}:${password}`).toString('base64')
@@ -44,8 +44,7 @@ const execute = async () => {
   try {
     users = await rpn.get(options);
   } catch (e) {
-    console.log('An error while getting the list of users');
-    console.log(e);
+    console.log('An error while getting the list of users - ', e.message);
   }
   
   users.forEach(async (user) => {
@@ -55,12 +54,11 @@ const execute = async () => {
     };
     postOptions.uri = `${options.uri}/${user.username}`;
     try {
-      rpn.post(postOptions);
+      await rpn.post(postOptions);
     } catch (e) {
-      console.log('An error while updating the password');
-      console.log(e);
+      console.log('An error while updating the password - ', e.message);
     }
   });
 };
 
-execute();
+execute().then(() => console.log('Bulk Update complete'));
