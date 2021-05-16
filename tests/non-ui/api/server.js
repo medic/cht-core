d0e73733251dec586156503c48963298454458e4
ngaruko <bede.ngaruko@gmail.com>
2021-05-16 18:13:06 +1200

    remove unused var

--- tests/non-ui/api/server.js
+++ tests/non-ui/api/server.js
@@ -1,4 +1,5 @@
 const utils = require('../../../tests/utils');
+const conf = require('../../../tests/conf');
 const auth = require('../../../tests/auth')();
 const request = require('request');
 const constants = require('../../../tests/constants');
