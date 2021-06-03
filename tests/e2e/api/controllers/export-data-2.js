547a4fb68fb556699fb02655b94872ff939a4847
Diana Barsan <barsan@medicmobile.org>
2021-06-03 21:34:32 +0300

    hardcode webdriver version to 90

--- tests/e2e/api/controllers/export-data-2.js
+++ tests/e2e/api/controllers/export-data-2.js
@@ -2,7 +2,7 @@ const utils = require('../../../utils');
 
 describe('Export Data V2.0', () => {
 
-  afterAll(utils.deleteAllDocs);
+  afterAll(() => utils.afterEach());
 
   describe('GET|POST /api/v2/export/reports', () => {
     const docs = [{
@@ -41,10 +41,11 @@ describe('Export Data V2.0', () => {
         baz: 'bazVal',
       }
     }];
+
     beforeAll(() => utils.saveDocs(docs));
 
-    it('Returns all reports that exist in the system', () =>
-      utils.request({ path: '/api/v2/export/reports' }).then(result => {
+    it('Returns all reports that exist in the system', () => {
+      return utils.request({ path: '/api/v2/export/reports' }).then(result => {
         const rows = result.split('\n');
         rows.pop(); // Last row is empty string, discard
         const expected = [
@@ -58,9 +59,10 @@ describe('Export Data V2.0', () => {
         rows.splice(1).forEach(row => {
           expect(expected).toContain(row);
         });
-      }));
-    it('POST Filters by form', () =>
-      utils.request({
+      });
+    });
+    it('POST Filters by form', () => {
+      return utils.request({
         method: 'POST',
         path: '/api/v2/export/reports',
         body: {
@@ -78,24 +80,26 @@ describe('Export Data V2.0', () => {
         ];
         expect(rows.length).toBe(2);
         expect(rows).toEqual(expected);
-      }));
+      });
+    });
     it('GET Filters by date', () => {
       const from = Date.UTC(2018,1,2,12);
       const to = Date.UTC(2018,1,3,12);
-      return utils.request(`/api/v2/export/reports?` +
-        `filters%5Bsearch%5D=&filters%5Bdate%5D%5Bfrom%5D=${from}&filters%5Bdate%5D%5Bto%5D=${to}`,
-      {notJson: true}
-      ).then(result => {
-        const rows = result.split('\n');
-        rows.pop(); // Last row is empty string, discard
+      return utils
+        .request(`/api/v2/export/reports?` +
+          `filters%5Bsearch%5D=&filters%5Bdate%5D%5Bfrom%5D=${from}&filters%5Bdate%5D%5Bto%5D=${to}`,
+        )
+        .then(result => {
+          const rows = result.split('\n');
+          rows.pop(); // Last row is empty string, discard
 
-        const expected = [
-          '_id,form,patient_id,reported_date,from,contact.name,contact.parent.name,contact.parent.parent.name,contact.parent.parent.parent.name,bar,baz,foo,smang.smong', // eslint-disable-line max-len
-          '"export-data-2-test-doc-3","b","abc125",1517616000000,,,,,,,"bazVal",,'
-        ];
-        expect(rows.length).toBe(2);
-        expect(rows).toEqual(expected);
-      });
+          const expected = [
+            '_id,form,patient_id,reported_date,from,contact.name,contact.parent.name,contact.parent.parent.name,contact.parent.parent.parent.name,bar,baz,foo,smang.smong', // eslint-disable-line max-len
+            '"export-data-2-test-doc-3","b","abc125",1517616000000,,,,,,,"bazVal",,'
+          ];
+          expect(rows.length).toBe(2);
+          expect(rows).toEqual(expected);
+        });
     });
   });
   describe('Weird data', () => {
@@ -114,30 +118,33 @@ describe('Export Data V2.0', () => {
       }
     }));
 
-    it('Outputs weird data types correctly', () =>
-      utils.request({
-        method: 'POST',
-        path: '/api/v2/export/reports',
-        body: {
-          filters: {
-            forms: {
-              selected: [{code: 'weird-data-types'}]
+    it('Outputs weird data types correctly', () => {
+      return utils
+        .request({
+          method: 'POST',
+          path: '/api/v2/export/reports',
+          body: {
+            filters: {
+              forms: {
+                selected: [{code: 'weird-data-types'}]
+              }
             }
-          }
-        }}).then(result => {
-        const rows = result.split('\n');
-        rows.pop(); // Last row is empty string, discard
-        //
-        // NB: if you have to debug this test failing, note that the test
-        // runner will not output the escaped string values correctly to the
-        // console. You can rely on its output to debug the problem.
-        //
-        const expected = [
-          '_id,form,patient_id,reported_date,from,contact.name,contact.parent.name,contact.parent.parent.name,contact.parent.parent.parent.name,wd_array,wd_emptyString,wd_false,wd_naughtyArray,wd_naughtyString,wd_null,wd_zero', // eslint-disable-line max-len
-          '"export-data-2-test-doc-4","weird-data-types",,"",,,,,,"[0,1,2]","",false,"[0,{\\"foo\\":false,\\"bar\\":null},\\"Hello, \\\\"world\\\\"\\"]","Woah there, \\"Jimmy O\'Tool\\"",,0', // eslint-disable-line max-len
-        ];
-        expect(rows.length).toBe(2);
-        expect(rows).toEqual(expected);
-      }));
+          }})
+        .then(result => {
+          const rows = result.split('\n');
+          rows.pop(); // Last row is empty string, discard
+          //
+          // NB: if you have to debug this test failing, note that the test
+          // runner will not output the escaped string values correctly to the
+          // console. You can rely on its output to debug the problem.
+          //
+          const expected = [
+            '_id,form,patient_id,reported_date,from,contact.name,contact.parent.name,contact.parent.parent.name,contact.parent.parent.parent.name,wd_array,wd_emptyString,wd_false,wd_naughtyArray,wd_naughtyString,wd_null,wd_zero', // eslint-disable-line max-len
+            '"export-data-2-test-doc-4","weird-data-types",,"",,,,,,"[0,1,2]","",false,"[0,{\\"foo\\":false,\\"bar\\":null},\\"Hello, \\\\"world\\\\"\\"]","Woah there, \\"Jimmy O\'Tool\\"",,0', // eslint-disable-line max-len
+          ];
+          expect(rows.length).toBe(2);
+          expect(rows).toEqual(expected);
+        });
+    });
   });
 });
