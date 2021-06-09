0cb6342340efe3bebb158089c10e44dbad82b31c
Nick Christenson <emailfornc@gmail.com>
2021-06-09 14:58:37 -0400

    remove curl

--- tests/webdriver-e2e/page-objects/login.page.js
+++ tests/webdriver-e2e/page-objects/login.page.js
@@ -35,24 +35,6 @@ const cookieLogin = async (username, password) => {
   await browser.url('/');
 };
 
-// curl 'http://localhost:5988/medic/login' \
-//   -H 'Connection: keep-alive' \
-//   -H 'sec-ch-ua: " Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"' \
-//   -H 'sec-ch-ua-mobile: ?0' \
-//   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36' \
-//   -H 'Content-Type: application/json' \
-//   -H 'Accept: */*' \
-//   -H 'Origin: http://localhost:5988' \
-//   -H 'Sec-Fetch-Site: same-origin' \
-//   -H 'Sec-Fetch-Mode: cors' \
-//   -H 'Sec-Fetch-Dest: empty' \
-//   -H 'Referer: http://localhost:5988/medic/login?redirect=http%3A%2F%2Flocalhost%3A5988%2F' \
-//   -H 'Accept-Language: en-US,en;q=0.9' \
-//   -H 'Cookie: locale=en; grafana_session=2811c43ac1b2934970fe37d60af66854' \
-//   --data-raw '{"user":"medic","password":"Secret_1","locale":"en"}' \
-//   --compressed
-
-
 const open = () => {
   return browser.url();
 };
