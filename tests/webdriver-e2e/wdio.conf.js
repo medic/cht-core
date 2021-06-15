d437b71f3e35f5b15f063b2077e57caaf95bb0ee
Nick Christenson <emailfornc@gmail.com>
2021-06-15 09:37:29 -0400

    logLevel error

--- tests/webdriver-e2e/wdio.conf.js
+++ tests/webdriver-e2e/wdio.conf.js
@@ -81,7 +81,7 @@ const baseConfig = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'debug',
+  logLevel: 'error'
   //
   // Set specific log levels per logger
   // loggers:
