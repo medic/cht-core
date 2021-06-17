2ffbcbaa48c3645c2f44e1a9c96fb3f086b0e967
ngaruko <bede.ngaruko@gmail.com>
2021-06-17 15:31:57 +1200

    added hooks

---
+++ tests/integration/hooks.js
@@ -0,0 +1,16 @@
+const utils = require('../utils');
+const request = require('request-promise-native');
+
+exports.mochaHooks = {
+  beforeAll: async ()=> {
+
+    console.log('Starting services......');
+    await utils.prepServices(true);
+    console.log('Services started');
+  },
+
+  afterAll:async () => {
+    await request.post('http://localhost:31337/die');
+    console.log('Test done. Signing off ...');
+  }
+};
