2271ef3bda91f54234cc478f141fe3b7d0b082f0
ngaruko <bede.ngaruko@gmail.com>
2021-06-17 19:43:55 +1200

    fix typo

--- tests/integration/hooks.js
+++ tests/integration/hooks.js
@@ -4,7 +4,7 @@ const request = require('request-promise-native');
 exports.mochaHooks = {
   beforeAll: async ()=> {
     console.log('Starting services......');
-    await utils.prepServicesNative(true);
+    await utils.prepServicesNative();
     console.log('Services started');
   },
 
