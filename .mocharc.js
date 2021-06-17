01b38c8d45b0f9d142bf34bf0817fdfb3ff9cc12
ngaruko <bede.ngaruko@gmail.com>
2021-06-18 08:58:18 +1200

    ignore routing.js

--- .mocharc.js
+++ .mocharc.js
@@ -2,6 +2,7 @@ module.exports = {
   'allow-uncaught': false,
   'async-only': false,
 	spec: ['tests/integration/**/*.js'],
+  ignore: ['tests/integration/api/routing.js'],
 	timeout: 135000,
   reporter: 'spec',
   require: ['tests/integration/hooks.js'],
