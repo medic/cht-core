e69804a428cddd3c533e46c69a3bd8882fb3e359
Nick Christenson <emailfornc@gmail.com>
2021-06-17 10:33:39 -0400

    only ever run 1 instance

--- tests/webdriver-e2e/wdio.conf.js
+++ tests/webdriver-e2e/wdio.conf.js
@@ -61,7 +61,7 @@ const baseConfig = {
     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
     // grid with only 5 firefox instances available you can make sure that not more than
     // 5 instances get started at a time.
-    maxInstances: 5,
+    maxInstances: 1,
     //
     browserName: 'chrome',
     acceptInsecureCerts: true,
