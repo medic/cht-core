2a0c6de1a1b2bf46aa46f23cb6fa314bc1f511df
ngaruko <bede.ngaruko@gmail.com>
2021-06-17 15:36:05 +1200

    added .mocharc

---
+++ .mocharc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'allow-uncaught': false,
+  'async-only': false,
+	spec: ['tests/integration/**/*.js'],
+	timeout: 135000,
+  reporter: 'spec',
+  require: ['tests/integration/hooks.js'],
+  captureFile: 'tests/results/results.txt',
+	ignore: ['tests/integration/api/routing.js']
+  };
