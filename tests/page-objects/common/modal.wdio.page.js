a2043a017a3cf93e87c1a9fe07b5179196820661
Nick Christenson <emailfornc@gmail.com>
2021-07-21 09:52:18 -0400

    eslint errs

--- tests/page-objects/common/modal.wdio.page.js
+++ tests/page-objects/common/modal.wdio.page.js
@@ -2,8 +2,7 @@ const body = () => $('div.modal-body');
 const modalFooter = '.modal-footer';
 const confirm = () => $(`${modalFooter} a.btn.submit.btn-danger`);
 
-
 module.exports = {
   body,
   confirm
-}
+};
