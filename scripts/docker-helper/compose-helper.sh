46452f0a73a6604a4b4792df23436a6ebb8d0496
mrjones-plip <ashley@medicmobile.org>
2021-07-29 22:18:27 -0700

    get_local_ip_url function \o/

--- scripts/docker-helper/compose-helper.sh
+++ scripts/docker-helper/compose-helper.sh
@@ -17,11 +17,22 @@
 #. `dirname $0`/helper_helpers.sh
 
 get_lan_ip(){
-  lanInterface=$(ip a | awk '/: e/{gsub(/:/,"");print $2}' | head -n 1)
-  lanAddress=$(ip a s $lanInterface | awk '/inet /{gsub(/\/.*/,"");print $2}')
-  lanHostname=$(getent hosts $lanAddress | awk '{print $2}')
   routerIP=$(ip r | grep default | awk '{print $3}')
-  echo $lanAddress
+  subnet=$(echo $routerIP|cut -d'.' -f1,2,3)
+  lanInterface=$(ip r | grep $subnet| grep default | cut -d' ' -f 5)
+  lanAddress=$(ip a s $lanInterface | awk '/inet /{gsub(/\/.*/,"");print $2}')
+#  lanHostname=$(getent hosts $lanAddress | awk '{print $2}')
+#  echo "lanHostname $lanHostname"
+#  echo "routerIP $routerIP"
+#  echo "LAN Interfact $lanInterface"
+  echo "$lanAddress"
+}
+
+get_local_ip_url(){
+  lanAddress="`get_lan_ip`"
+  IFS='.' read -ra ip_array <<< $lanAddress
+  url="https://${ip_array[0]}-${ip_array[1]}-${ip_array[2]}-${ip_array[3]}.my.local-ip.co"
+  echo "$url"
 }
 
 is_installed(){
@@ -38,16 +49,19 @@ is_installed(){
 
 main (){
     #basic information, hostname, date, ...
-    window "`hostname`" "red"
+    window "CHT Docker Helper" "red"
     append "`date`"
-    append "`is_installed docker`"
-    append "`is_installed horti`"
-    append "`is_installed docker-compose`"
-    append "`get_lan_ip`"
-    addsep
-    append_tabbed "Up since|`uptime | cut -f1 -d"," | sed 's/^ *//' | cut -f3- -d" "`" 2 "|"
-    append_tabbed "Users:`uptime | cut -f2 -d"," | sed 's/^ *//'| cut -f1 -d" "`" 2
-    append_tabbed "`awk '{print "Load average:" $1 " " $2 " " $3}' < /proc/loadavg`" 2
+#    append "`is_installed ip`"
+#    append "`is_installed docker`"
+#    append "`is_installed horti`"
+#    append "`is_installed docker-compose`"
+#    append "Lan IP `get_lan_ip`"
+#    append "URL `get_local_ip_url`"
+    append_tabbed "CHT URL|`get_local_ip_url`" 4 "|"
+#    addsep
+#    append_tabbed "Up since|`uptime | cut -f1 -d"," | sed 's/^ *//' | cut -f3- -d" "`" 2 "|"
+#    append_tabbed "Users:`uptime | cut -f2 -d"," | sed 's/^ *//'| cut -f1 -d" "`" 2
+#    append_tabbed "`awk '{print "Load average:" $1 " " $2 " " $3}' < /proc/loadavg`" 2
     endwin
 
 
