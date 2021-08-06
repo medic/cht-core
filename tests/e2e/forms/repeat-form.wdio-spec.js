14213caf5d59d8d8a5a3d9ee1c25e44805fbb39a
m5r <mokht@rmi.al>
2021-08-07 01:42:28 +0800

    test that a repeat form is correctly translated to the user's language with wdio

---
+++ tests/e2e/forms/repeat-form.wdio-spec.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+
+const constants = require('../../constants');
+const utils = require('../../utils');
+const loginPage = require('../../page-objects/login/login.wdio.page');
+const commonPage = require('../../page-objects/common/common.wdio.page');
+const reportsPage = require('../../page-objects/reports/reports.wdio.page');
+
+const xml = fs.readFileSync(`${__dirname}/../../../demo-forms/repeat-translation.xml`, 'utf8');
+const formDocument = {
+  _id: 'form:repeat-translation',
+  internalId: 'repeat-translation',
+  title: 'Repeat',
+  type: 'form',
+  _attachments: {
+    xml: {
+      content_type: 'application/octet-stream',
+      data: Buffer.from(xml).toString('base64')
+    }
+  }
+};
+const userContactDoc = {
+  _id: constants.USER_CONTACT_ID,
+  name: 'Jack',
+  date_of_birth: '',
+  phone: '+64274444444',
+  alternate_phone: '',
+  notes: '',
+  type: 'person',
+  reported_date: 1478469976421,
+  parent: {
+    _id: 'some_parent'
+  }
+};
+
+describe('RepeatForm', () => {
+  before(async () => {
+    await utils.seedTestData(userContactDoc, [formDocument]);
+  });
+
+  afterEach(async () => {
+    await browser.deleteCookies();
+    await browser.refresh();
+  });
+
+  it('should display the initial form and its repeated content in Swahili', async () => {
+    const swUserName = 'Jina la mtumizi';
+    await loginPage.changeLanguage('sw', swUserName);
+    await loginPage.cookieLogin();
+    await commonPage.goToReports();
+    await (await reportsPage.submitReportButton()).click();
+    await (await reportsPage.formLinkByHref(formDocument.internalId)).click();
+
+    const stateLabel = await $('#report-form .question-label.active[data-itext-id="/repeat/basic/state_1:label"]');
+    expect(await stateLabel.getText()).toBe('Select a state: - SV');
+
+    const addRepeatButton = await $('.btn.btn-default.add-repeat-btn');
+    await addRepeatButton.click();
+
+    const cityLabel = await $('#report-form .question-label.active[data-itext-id="/repeat/basic/rep/city_1:label"]');
+    expect(await cityLabel.getText()).toBe('Select a city: - SV');
+  });
+
+  it('should display the initial form and its repeated content in English', async () => {
+    const enUserName = 'User name';
+    await loginPage.changeLanguage('en', enUserName);
+    await loginPage.cookieLogin();
+    await commonPage.goToReports();
+    await (await reportsPage.submitReportButton()).click();
+    await (await reportsPage.formLinkByHref(formDocument.internalId)).click();
+
+    const stateLabel = await $('#report-form .question-label.active[data-itext-id="/repeat/basic/state_1:label"]');
+    expect(await stateLabel.getText()).toBe('Select a state:');
+
+    const addRepeatButton = await $('.btn.btn-default.add-repeat-btn');
+    await addRepeatButton.click();
+
+    const cityLabel = await $('#report-form .question-label.active[data-itext-id="/repeat/basic/rep/city_1:label"]');
+    expect(await cityLabel.getText()).toBe('Select a city:');
+  });
+});
--- tests/page-objects/reports/reports.wdio.page.js
+++ tests/page-objects/reports/reports.wdio.page.js
@@ -1,12 +1,16 @@
 const reportListID = '#reports-list';
 const reportBodyDetails = '#reports-content .report-body .details';
-const reportList = () => $(`${reportListID}`);
 const selectedCaseId = () => $(`${reportBodyDetails} > ul > li > p > span > a`);
 const selectedCaseIdLabel = () => $(`${reportBodyDetails} ul > li > label > span`);
 const submitterPlace = () => $('.position a');
 const submitterPhone = () => $('.sender .phone');
 const submitterName = () => $('.sender .name');
 const firstReport = () => $(`${reportListID} li:first-child`);
+const reportList = () => $(`${reportListID}`);
+const submitReportButton = () => $('.action-container .general-actions:not(.ng-hide) .fa-plus');
+const formLinkByHref = (href) => {
+  return $(`.action-container .general-actions .dropup.open .dropdown-menu li a[href="#/reports/add/${href}"]`);
+};
 const unreadCount = () => $('#reports-tab .mm-badge');
 
 // warning: the unread element is not displayed when there are no unread reports
@@ -23,5 +27,7 @@ module.exports = {
   submitterPlace,
   selectedCaseId,
   selectedCaseIdLabel,
+  submitReportButton,
+  formLinkByHref,
   getUnreadCount,
 };
