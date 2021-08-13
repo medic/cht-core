a7d4c39aed3cebd5e1a73bbdd5e65f119e52001f
Nick Christenson <emailfornc@gmail.com>
2021-08-13 09:09:04 -0400

    swapped to a proper function and passing into execute

--- tests/utils/browser.js
+++ tests/utils/browser.js
@@ -1,26 +1,26 @@
 const fs = require('fs');
 const feedBackDocs = async (testName = 'allLogs') => {
-  const script = `(async () => {
-  const allDbList =  await indexedDB.databases();
-  const metaDbList = allDbList.filter(db => db.name.includes('pouch_medic-user') && db.name.endsWith('-meta'))
-  const feedBack = []
-  for (const idx in metaDbList){
-    const db = metaDbList[idx]
-    const nameStripped = db.name.replace('_pouch_','');
-    const metaDb = new PouchDB(nameStripped);
-    const docs = await metaDb.allDocs({ include_docs:true });
-    const feedBackDocs = docs.rows.filter(x => x.doc.type === 'feedback')
-    feedBack.push(feedBackDocs)
-  }
-  return feedBack.flat();
-})();`;
-  const feedBackDocs = await browser.executeScript(script);
-  if (feedBackDocs.length > 0) {
-    await fs.writeFileSync(`./tests/logs/feedbackDocs-${testName}`,JSON.stringify(feedBackDocs));
+  const feedBackDocs = await browser.executeAsync(feedBackDocsScript);
+  if (feedBackDocs && feedBackDocs.flat().length > 0) {
+    fs.writeFileSync(`./tests/logs/feedbackDocs-${testName}.txt`, JSON.stringify(feedBackDocs));
     return false;
   }
 };
 
+
+const feedBackDocsScript = async () => {
+  //This is running inside the browser. indexedDB and PouchDB is available there.
+  // eslint-disable-next-line no-undef
+  const allDbList = await indexedDB.databases();
+  const metaDbList = allDbList.filter(db => db.name.includes('pouch_medic-user') && db.name.endsWith('-meta'))
+  return Promise.all(metaDbList.map(async (db) => {
+    const nameStripped = db.name.replace('_pouch_', '');
+    // eslint-disable-next-line no-undef
+    const metaDb = new PouchDB(nameStripped);
+    const docs = await metaDb.allDocs({ include_docs: true, startkey: 'feedback-', endkey: 'feedback-\ufff0' });
+    return docs.rows;
+  }));
+};
 
 module.exports = {
   feedBackDocs
