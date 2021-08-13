04f11664ecfcbd33fe7beb1d334d87f1d8c3e0e0
meghna-khemka <meghna@medic.org>
2021-08-13 08:37:06 -0700

    minor conf change

--- tests/conf.js
+++ tests/conf.js
@@ -12,7 +12,7 @@ const baseConfig = {
   },
   SELENIUM_PROMISE_MANAGER: false,
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  exclude: ['**/*.wdio-spec.js', '**/*.standard-wdio-spec.js'],
+  exclude: ['**/*wdio-spec.js'],
   suites: {
     web: [
       'e2e/!(cht)/**/*.js',
