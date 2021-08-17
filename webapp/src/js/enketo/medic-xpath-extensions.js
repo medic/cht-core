cb7cb2debb3e59fce5d535e97e8b21f871e1809b
Joshua Kuestersteffen <jkuester@kuester7.com>
2021-08-17 17:55:14 -0500

    Fix up hacking for openrosa stuff

--- webapp/src/js/enketo/medic-xpath-extensions.js
+++ webapp/src/js/enketo/medic-xpath-extensions.js
@@ -50,6 +50,52 @@ const getTimezoneOffsetAsTime = function(date) {
   return direction + hours + ':' + minutes;
 };
 
+const RAW_NUMBER = /^(-?[0-9]+)(\.[0-9]+)?$/;
+const DATE_STRING = /^\d\d\d\d-\d{1,2}-\d{1,2}(?:T\d\d:\d\d:\d\d(?:Z|[+-]\d\d:\d\d))?$/;
+const XPR = {
+  boolean: function(val) { return { t:'bool', v:val }; },
+  number: function(val) { return { t:'num', v:val }; },
+  string: function(val) { return { t:'str', v:val }; },
+  date: function(val) {
+    if(!(val instanceof Date)) {
+      throw new Error('Cannot create date from ' + val + ' (' + (typeof val) + ')');
+    }
+    return { t:'date', v:val };
+  }
+};
+
+const _str = (r) => {
+  return r.t === 'arr' ?
+    r.v.length ? r.v[0].toString() : '' :
+    r.v.toString();
+};
+
+const _date = (it) => {
+  let temp;
+  let t;
+
+  if(it.v instanceof Date) {
+    return new Date(it.v);
+  }
+
+  it = _str(it);
+
+  if(RAW_NUMBER.test(it)) {
+    // Create a date at 00:00:00 1st Jan 1970 _in the current timezone_
+    temp = new Date(1970, 0, 1);
+    temp.setDate(1 + parseInt(it, 10));
+    return temp;
+  } else if(DATE_STRING.test(it)) {
+    t = it.indexOf('T');
+    if(t !== -1) {
+      it = it.substring(0, t);
+    }
+    temp = it.split('-');
+    temp = new Date(temp[0], temp[1]-1, temp[2]);
+    return temp;
+  }
+};
+
 module.exports = {
   getTimezoneOffsetAsTime: getTimezoneOffsetAsTime,
   toISOLocalString: toISOLocalString,
@@ -68,7 +114,21 @@ module.exports = {
         return { t: 'str', v: '' };
       }
       return { t: 'num', v: result };
-    }
+    },
+    'difference-in-months': function(d1, d2) {
+      d1 = _date(d1);
+      d2 = _date(d2);
+
+      if(!d1 || !d2) {
+        return XPR.string('');
+      }
+
+      const months =
+        ((d2.getFullYear() - d1.getFullYear()) * 12) +
+        (d2.getMonth() - d1.getMonth()) +
+        (d2.getDate() < d1.getDate() ? -1 : 0);
+      return XPR.number(months);
+    },
   },
   process: {
     toExternalResult: function(r) {
