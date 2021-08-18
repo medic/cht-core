beb193e2315e79c523f2e87cd24f3fa33474bb7f
Nick Christenson <emailfornc@gmail.com>
2021-08-18 14:49:55 -0400

    set node version env var

--- scripts/travis/horti_entry.sh
+++ scripts/travis/horti_entry.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get install -y curl
 echo Installing Node
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
+curl -sL "https://deb.nodesource.com/setup_$NODE.x" | bash -
 apt-get install -y nodejs
 echo Node install finished
 node -v
