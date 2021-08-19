012588a92ba2cd635a30a0267fd448f84dce15cb
Diana Barsan <barsan@medicmobile.org>
2021-08-19 08:58:56 +0300

    Adds CSP sha rule for Enketo's onsubmit event handler.

--- api/src/routing.js
+++ api/src/routing.js
@@ -134,6 +134,7 @@ app.use(
     // runs with a bunch of defaults: https://github.com/helmetjs/helmet
     hpkp: false, // explicitly block dangerous header
     contentSecurityPolicy: {
+      loose: true,
       directives: {
         defaultSrc: [`'none'`],
         fontSrc: [`'self'`],
@@ -160,7 +161,11 @@ app.use(
           // Explicitly allow the telemetry script setting startupTimes
           `'sha256-B5cfIVb4/wnv2ixHP03bHeMXZDszDL610YG5wdDq/Tc='`,
           // AngularJS and several dependencies require this
-          `'unsafe-eval'`
+          `'unsafe-eval'`,
+          // Allow Enketo onsubmit form attribute
+          // https://github.com/medic/cht-core/issues/6988
+          `'unsafe-hashes'`,
+          `'sha256-2rvfFrggTCtyF5WOiTri1gDS8Boibj4Njn0e+VCBmDI='`,
         ],
         styleSrc: [
           `'self'`,
