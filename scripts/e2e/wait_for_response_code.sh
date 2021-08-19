14b39a8d122d1b7ed2f6d89c14f71cfd43b56658
Nick Christenson <emailfornc@gmail.com>
2021-08-19 17:13:52 -0400

    adding a isrunning check

--- scripts/e2e/wait_for_response_code.sh
+++ scripts/e2e/wait_for_response_code.sh
@@ -1,5 +1,5 @@
 count=0
-while [ `curl -o /dev/null -s -w "%{http_code}\n" http://localhost:$1` -ne "$2" -a $count -lt 300 ]
+while [ `curl -o /dev/null -s -w "%{http_code}\n" http://localhost:$1` -ne "$2" -a $count -lt 100 ]
   do count=$((count+=1))
   echo "waiting for $3 to respond with 200 status code. Current count is $count" 
   sleep 1 
