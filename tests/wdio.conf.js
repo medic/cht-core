74b6c0a6864cc15c4c05191d9558eb7a416891b5
Nick Christenson <10531066+newtewt@users.noreply.github.com>
2021-08-23 09:00:35 -0400

    Checking for feedback docs and failing if they exist (#7251)
    * add script to pull feedback docs from meta dbs, write docs to file if they exists, and fail test if they exist

--- tests/wdio.conf.js
+++ tests/wdio.conf.js
@@ -12,6 +12,8 @@ const getBrowserLogFilePath = (specs) => {
   return path.join(__dirname, 'logs', 'browser.' + specName + '.log');
 };
 const browserLogPath = path.join(__dirname, 'logs', 'browser.console.log');
+const browserUtils = require('./utils/browser');
+const existingFeedBackDocIds = [];
 
 const baseConfig = {
   //
@@ -266,6 +268,14 @@ const baseConfig = {
    * Function to be executed after a test (in Mocha/Jasmine).
    */
   afterTest: async (test, context, { passed }) => {
+    const feedBackDocs = await browserUtils.feedBackDocs(`${test.parent} ${test.title}`,existingFeedBackDocIds);
+    existingFeedBackDocIds.push(feedBackDocs);
+    if(feedBackDocs){
+      if(passed){
+        context.test.callback(new Error('Feedback docs were generated during the test.'));
+      }
+      passed = false;
+    }
     if (passed === false) {
       await browser.takeScreenshot();
     }
