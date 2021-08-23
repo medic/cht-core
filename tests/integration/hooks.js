65b54f6991a0fc88504dfbcbc0b59451a5e9bdf4
Nick Christenson <emailfornc@gmail.com>
2021-08-23 12:12:46 -0400

    CI ending is the ultimate end

--- tests/integration/hooks.js
+++ tests/integration/hooks.js
@@ -1,4 +1,5 @@
 const utils = require('../utils');
+const constants = require('../constants');
 const request = require('request-promise-native');
 
 exports.mochaHooks = {
@@ -9,13 +10,15 @@ exports.mochaHooks = {
   },
 
   afterAll:async () => {
-    try {
+    if (!constants.IS_TRAVIS)
+    {    
+      try {
       await request.post('http://localhost:31337/die');
     } catch (e) {
       console.error('an error occured');
       console.error(JSON.stringify(e));
     }
-
+  }
     console.log('Test done. Signing off ...');
   }
 };
