d52add62d765258e3faa5990aa6bd9044d4b0846
mrjones-plip <ashley@medicmobile.org>
2021-08-24 10:18:04 -0700

    add ports to docker status, update screenshot for same

--- scripts/docker-helper/docker-status.sh
+++ scripts/docker-helper/docker-status.sh
@@ -10,15 +10,15 @@ main (){
   window "Containers" "green" "100%"
   containersCount=$(docker ps | wc -l)
   if [[ $containersCount -gt 1 ]];then
-    append_tabbed "NAME|STATUS|STATE|IP" 4 "|"
+    append_tabbed "NAME|IP|PORTS|STATE" 4 "|"
 
     containers=$(docker ps | awk '{if(NR>1) print $NF}')
     for container in $containers
     do
       upSince=$(docker ps  --format='{{.Status}}' -f="name=$container")
-      status2=$(docker ps  --format='{{.State}}'  -f="name=$container")
       ip=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'  "$container")
-      append_tabbed "$container|$upSince|$status2|$ip" 4 "|"
+      ports=$(docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}}{{(index $conf 0).HostPort}} > {{$p}} {{end}}'  "$container")
+      append_tabbed "$container|$ip|$ports|$upSince" 4 "|"
     done
   else
     append "No containers running"
@@ -28,7 +28,7 @@ main (){
   move_up
 
   window "networks" "green" "50%"
-  dockerLs=$(docker network ls --format 'table {{.Name}}\t')
+  dockerLs=$(docker network ls --format '{{.Name}}\t')
   append "$dockerLs"
   endwin
 
@@ -36,12 +36,12 @@ main (){
   move_up
 
   window "Medic Images" "green" "50%"
-  dockerImg=$(docker image ls --format 'table {{.Repository}}:{{.Tag}}\t' --filter 'reference=medicmobile/*')
+  dockerImg=$(docker image ls --format '{{.Repository}}:{{.Tag}}\t' --filter 'reference=medicmobile/*')
   append "$dockerImg"
   endwin
 
   window "volumes" "green" "50%"
-  dockerVol=$(docker volume ls --format 'table {{.Name}}\t')
+  dockerVol=$(docker volume ls --format '{{.Name}}\t')
   append "$dockerVol"
   endwin
 
