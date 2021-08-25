408078d302bc5379f66e262ebc1b2b68beb66392
Nick Christenson <emailfornc@gmail.com>
2021-08-25 15:08:31 -0400

    remove console logs and remove gitignore

--- scripts/e2e/e2e-servers.js
+++ scripts/e2e/e2e-servers.js
@@ -26,23 +26,18 @@ const processes = {};
 
 const startServer = (serviceName, append) => new Promise((resolve, reject) => {
   if (processes[serviceName]) {
-    console.log('travis already running 1');
     console.warn(serviceName, 'is already running');
     return resolve();
   }
 
   try {
-    console.log('travis write stream 1');
     const logStream = fs.createWriteStream(`tests/logs/${serviceName}.e2e.log`, { flags: append ? 'a' : 'w' });
-    console.log('travis write stream 2');
     let server;
     if (constants.IS_TRAVIS) {
-      console.log('travis start');
       server = spawn('horti-svc-start', [
         `${require('os').homedir()}/.horticulturalist/deployments`,
         `medic-${serviceName}`
       ]);
-      console.log('travis start 1');
     } else {
       // runs your local checked out api / sentinel
       server = fork(`server.js`, {
@@ -58,20 +53,14 @@ const startServer = (serviceName, append) => new Promise((resolve, reject) => {
         },
       });
     }
-    console.log('travis start 2');
     const writeToLogStream = data => writeToStream(logStream, data);
-    console.log('travis start 3');
     server.stdout.on('data', writeToLogStream);
-    console.log('travis start 4');
     server.stderr.on('data', writeToLogStream);
-    console.log('travis start 5');
     server.on('close', code => writeToLogStream(`${serviceName} process exited with code ${code}`));
 
     processes[serviceName] = server;
     resolve();
   } catch (err) {
-    console.log('travis start err');
-    console.log(err);
     reject(err);
   }
 });
@@ -103,7 +92,7 @@ const started = {
   sentinel: false,
 };
 
-app.post('/:server/:action', (req, res, next) => {
+app.post('/:server/:action', (req, res, ) => {
   const { server, action } = req.params;
   let p = Promise.resolve();
 
@@ -121,22 +110,19 @@ app.post('/:server/:action', (req, res, next) => {
   if (['start', 'restart'].includes(action)) {
     if (['api', 'all'].includes(server)) {
       console.log('Starting API...');
-      p = p.then(() => startServer('api', started.api)).then(() => started.api = true).catch(next);
+      p = p.then(() => startServer('api', started.api)).then(() => started.api = true);
     }
     if (['sentinel', 'all'].includes(server)) {
       console.log('Starting Sentinel...');
-      p = p.then(() => startServer('sentinel', started.sentinel)).then(() => started.sentinel = true).catch(next);
+      p = p.then(() => startServer('sentinel', started.sentinel)).then(() => started.sentinel = true);
     }
   }
 
   return p.then(() => res.status(200).end());
 });
 
-app.get('/isRunning', (req, res) => {
-  res.send('We are running');
-});
 
-app.post('/die', (req, res, next) => {
+app.post('/die', (req, res) => {
   console.log('Killing API / Sentinel service port');
   Promise.all([
     () => stopServer('api'),
@@ -144,11 +130,11 @@ app.post('/die', (req, res, next) => {
   ]).then(() => {
     res.status(200).end();
     process.exit(0);
-  }).catch(next);
+  });
 });
 
 const port = 31337;
 
-const server = app.listen(port);
-server.setTimeout(0);
+app.listen(port);
+
 console.log('API / Sentinel service port listening on', port);
