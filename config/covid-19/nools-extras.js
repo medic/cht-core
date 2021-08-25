2076d8a1fb3ea3ce26bc82ba93874f71388f5ba0
latin-panda <66472237+latin-panda@users.noreply.github.com>
2021-08-25 23:08:11 +0700

    covid-19 provision and tasks flow, initial capture [in progress]

---
+++ config/covid-19/nools-extras.js
@@ -0,0 +1,47 @@
+
+function getField(report, fieldPath) {
+  return ['fields', ...(fieldPath || '').split('.')]
+    .reduce((prev, fieldName) => {
+      if (prev === undefined) { return undefined; }
+      return prev[fieldName];
+    }, report);
+}
+
+function isFormArraySubmittedInWindow(reports, formArray, start, end, count) {
+  let found = false;
+  let reportCount = 0;
+  reports.forEach(function (report) {
+    if (formArray.includes(report.form)) {
+      if (report.reported_date >= start && report.reported_date <= end) {
+        found = true;
+        if (count) {
+          reportCount++;
+        }
+      }
+    }
+  });
+
+  if (count) { return reportCount >= count; }
+  return found;
+}
+
+function getTimeForMidnight(d) {
+  const date = new Date(d);
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  return date;
+}
+
+function addDays(date, days) {
+  const result = getTimeForMidnight(new Date(date));
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+module.exports = {
+  addDays,
+  isFormArraySubmittedInWindow,
+  getField
+};
--- config/covid-19/tasks.js
+++ config/covid-19/tasks.js
@@ -1,1 +1,111 @@
-module.exports = [];
+const extras = require('./nools-extras');
+
+const {
+  isFormArraySubmittedInWindow,
+  addDays,
+  getField
+} = extras;
+
+module.exports = [
+  {
+    name: 'covid19-rdt-capture-results',
+    icon: 'icon-follow-up',
+    title: 'task.covid19.capture.title',
+    appliesTo: 'reports',
+    appliesToType: ['covid19:rdt:provision'],
+    appliesIf: (contact, report) => {
+      return !!getField(report, 'test-reference.test_id');
+    },
+    resolvedIf: (contact, report, event, dueDate) => {
+      if (!contact.reports) {
+        return false;
+      }
+
+      const captureReport = contact.reports.find(reportDoc => {
+        if (reportDoc.form !== 'covid19:rdt:capture') {
+          return false;
+        }
+        const testId = getField(report, 'test-reference.test_id');
+        return getField(reportDoc, 'test_id') === testId;
+      });
+
+      if (!captureReport) {
+        return false;
+      }
+
+      const startTime = Math.max(addDays(dueDate, -event.start).getTime(), report.reported_date + 1);
+      const endTime = addDays(dueDate, event.end + 1).getTime();
+
+      return isFormArraySubmittedInWindow(contact.reports, ['covid19:rdt:capture'], startTime, endTime);
+    },
+    actions: [
+      {
+        type: 'report',
+        form: 'covid19:rdt:capture',
+        modifyContent: function(content, contact, report) {
+          content.test_id = getField(report, 'test-reference.test_id');
+          // eslint-disable-next-line no-console
+          console.warn('getField(report, \'patient_uuid\')', getField(report, 'patient_uuid'));
+          content.patient_uuid = getField(report, 'patient_uuid');
+        }
+      }
+    ],
+    events: [
+      {
+        id: 'covid19-rdt-capture-event',
+        start: 1,
+        end: 2,
+        days: 1
+      }
+    ]
+  },
+
+  {
+    name: 'covid19-rdt-repeat',
+    icon: 'icon-follow-up',
+    title: 'task.covid19.repeat.title',
+    appliesTo: 'reports',
+    appliesToType: ['covid19:rdt:capture'],
+    appliesIf: (contact, report) => {
+      return getField(report, 'repeat-test.repeat_test') === 'yes';
+    },
+    resolvedIf: (contact, report, event, dueDate) => {
+      if (!contact.reports) {
+        return false;
+      }
+
+      const provisionReport = contact.reports.find(reportDoc => {
+        if (reportDoc.form !== 'covid19:rdt:provision') {
+          return false;
+        }
+        return getField(reportDoc, 'patient_uuid') === getField(report, 'patient_uuid');
+      });
+
+      if (!provisionReport) {
+        return false;
+      }
+
+      const startTime = Math.max(addDays(dueDate, -event.start).getTime(), report.reported_date + 1);
+      const endTime = addDays(dueDate, event.end + 1).getTime();
+
+      return isFormArraySubmittedInWindow(contact.reports, ['covid19:rdt:provision'], startTime, endTime);
+    },
+    actions: [
+      {
+        type: 'report',
+        form: 'covid19:rdt:provision',
+        modifyContent: function(content, contact, report) {
+          content.patient_uuid = getField(report, 'patient_uuid');
+        }
+      }
+    ],
+    events: [
+      {
+        id: 'covid19-rdt-capture-event',
+        start: 1,
+        end: 2,
+        days: 1
+      }
+    ]
+  }
+];
