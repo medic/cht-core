e35c8c70ad880f96ddf0d9888c6f1e38f73ff62c
meghna-khemka <84743933+meghna-khemka@users.noreply.github.com>
2021-09-02 10:32:56 -0700

    Migrating add-user test from protractor to wdio (#7287)
    * Migrating add-user test from protractor to wdio
    
    * Adding some cleanup statements
    
    * Replacing hard coded name with faker
    
    * Addressing review comments
    
    * changes in person-under-area.wdio-spec

--- tests/page-objects/admin/user.wdio.page.js
+++ tests/page-objects/admin/user.wdio.page.js
@@ -1,4 +1,6 @@
+const _ = require('lodash');
 const addUserButton = () => $('a#add-user');
+const cancelUserModalButton = () => $('[test-id="modal-cancel-btn"]');
 const addUserDialog = () => $('div#edit-user-profile');
 const userName = () => $('#edit-username');
 const userFullName = () => $('#fullname');
@@ -12,6 +14,13 @@ const logoutButton = () => $('i.fa-power-off');
 const select2SearchInputBox = () => $('//input[@aria-controls="select2-facilitySelect-results"]');
 const select2Name = () => $('.name');
 const select2SearchContactInputBox = () => $('//input[@aria-controls="select2-contactSelect-results"]');
+const usernameTextSelector = '[test-id="username-list"]';
+const usernameText = () => $(usernameTextSelector);
+const usernameTextList = () => $$(usernameTextSelector);
+const usernameErrorMessage = () => $('span.help-block.ng-binding');
+const passwordErrorMessage = () => $('#edit-password ~ .help-block');
+const placeErrorMessage = () => $('#facilitySelect ~ .help-block');
+const contactErrorMessage = () => $('#contactSelect ~ .help-block');
 
 const goToAdminUser = async () => {
   await browser.url('/admin/#/users');
@@ -25,14 +34,28 @@ const openAddUserDialog = async () => {
   await browser.pause(500);
 };
 
-const inputAddUserFields = async (username, fullname, role, place, associatedContact, password) => {
+const closeUserDialog = async () => {
+  await (await cancelUserModalButton()).waitForDisplayed();
+  await (await cancelUserModalButton()).click();
+  await (await addUserDialog()).waitForDisplayed({ reverse: true });
+};
+
+const inputAddUserFields = async (username, fullname, role, place, associatedContact, password, 
+  confirmPassword = password) => {
   await (await userName()).addValue(username);
   await (await userFullName()).addValue(fullname);
   await (await userRole()).selectByVisibleText(role);
-  await selectPlace(place);
-  await selectContact(associatedContact);
+
+  if (!_.isEmpty(place)) {
+    await selectPlace(place);
+  }
+
+  if (!_.isEmpty(associatedContact)) {
+    await selectContact(associatedContact);
+  }
+  
   await (await userPassword()).addValue(password);
-  await (await userConfirmPassword()).addValue(password);
+  await (await userConfirmPassword()).addValue(confirmPassword);
 };
 
 const selectPlace = async (place) => {
@@ -55,10 +78,12 @@ const selectContact = async (associatedContact) => {
 
 };
 
-const saveUser = async ()  => {
+const saveUser = async (isSuccessExpected = true)  => {
   await (await saveUserButton()).waitForDisplayed();
   await (await saveUserButton()).click();
-  await (await addUserDialog()).waitForDisplayed({ reverse: true });
+  if (isSuccessExpected) {
+    await (await addUserDialog()).waitForDisplayed({ reverse: true });
+  }
 };
 
 const logout = async () => {
@@ -66,10 +91,41 @@ const logout = async () => {
   await (await logoutButton()).click();
 };
 
+const getAllUsernames = async () => {
+  await (await usernameText()).waitForDisplayed();
+  return getTextForElements(usernameTextList);
+};
+
+const getTextForElements = async (elements) => {
+  return Promise.all((await elements()).map(filter => filter.getText()));
+};
+
+const getUsernameErrorText = async () => {
+  return await (await usernameErrorMessage()).getText();
+};
+
+const getPasswordErrorText = async () => {
+  return await (await passwordErrorMessage()).getText();
+};
+
+const getPlaceErrorText = async () => {
+  return await (await placeErrorMessage()).getText();
+};
+
+const getContactErrorText = async () => {
+  return await (await contactErrorMessage()).getText();
+};
+
 module.exports = {
   goToAdminUser,
   openAddUserDialog,
+  closeUserDialog,
   inputAddUserFields,
   saveUser,
-  logout
+  logout,
+  getAllUsernames,
+  getUsernameErrorText,
+  getPasswordErrorText,
+  getPlaceErrorText,
+  getContactErrorText
 };
