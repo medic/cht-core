4491366e00e825718790a0cf47d299c1087dfd1c
Nick Christenson <emailfornc@gmail.com>
2021-09-03 14:58:10 -0400

    remove faker

--- tests/e2e/users/add-user.wdio-spec.js
+++ tests/e2e/users/add-user.wdio-spec.js
@@ -1,5 +1,4 @@
 const utils = require('../../utils');
-const faker = require('faker');
 const chai = require('chai');
 const usersAdminPage = require('../../page-objects/admin/user.wdio.page');
 const placeFactory = require('../../factories/cht/contacts/place');
@@ -8,7 +7,7 @@ const personFactory = require('../../factories/cht/contacts/person');
 
 const onlineUserRole = 'Program Officer';
 const offlineUserRole = 'CHW';
-const username = faker.name.firstName().toLocaleLowerCase();
+const username = 'jackuser';
 const password = 'Jacktest@123';
 const incorrectpassword = 'Passwor';
 const places = placeFactory.generateHierarchy();
