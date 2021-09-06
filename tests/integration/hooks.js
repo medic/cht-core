9177ff3fd0a0305ae4aef6c766036ee94712e8b3
ngaruko <bede.ngaruko@gmail.com>
2021-09-06 16:45:42 +1200

    login headless before tests

--- tests/integration/hooks.js
+++ tests/integration/hooks.js
@@ -1,11 +1,23 @@
 const utils = require('../utils');
+const auth = require('../auth')();
 const request = require('request-promise-native');
+const login = async (username = auth.username, password = auth.password) => {
+  const opts = {
+    path: '/medic/login',
+    body: { user: username, password: password },
+    method: 'POST',
+    simple: false,
+  };
+  await utils.request(opts);
+};
 
 exports.mochaHooks = {
   beforeAll: async ()=> {
     console.log('Starting services......');
     await utils.prepServices();
     console.log('Services started');
+    await login();
+    await utils.setupUserDoc();
   },
 
   afterAll:async () => {
