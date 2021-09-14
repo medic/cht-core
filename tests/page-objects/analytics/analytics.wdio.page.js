75fd6299bc9aa59b1d82765da8a64e15131854ce
ngaruko <bede.ngaruko@gmail.com>
2021-09-15 08:42:15 +1200

    removed unused code

--- tests/page-objects/analytics/analytics.wdio.page.js
+++ tests/page-objects/analytics/analytics.wdio.page.js
@@ -1,7 +1,4 @@
-const listModule = module => $(`.mm-navigation-menu li a[href="${module}"]`);
-const aggregates = () => $(`.mm-navigation-menu li a[href="#/analytics/target-aggregates"]`);
 const aggregateList = () => $('#target-aggregates-list');
-const pageDisabled = () => $('.page .item-content.disabled');
 const analytics = () => $$('ul.mm-navigation-menu li a span');
 const targetAggregatesItems = () => $$(`.content .heading h4 span`);
 const meta = ('div.body.meta > div >');
@@ -14,28 +11,7 @@ const getAllAggregates = async () => {
   return Promise.all((await targetAggregatesItems()).map(filter => filter.getText()));
 };
 
-const expectModulesToBeAvailable = async (modules) => {
-  for (const module of modules) {
-    expect (await (await listModule(module)).isExisting()).toBeTruthy();
-  }
-};
-
-const goToTargetAggregates = async (enabled) => {
-  await (await aggregates()).click();
-  if (enabled) {
-    await (await aggregateList()).waitForDisplayed();
-  } else {
-    await (await pageDisabled()).waitForDisplayed();
-  }
-};
-
 module.exports = {
-  expectModulesToBeAvailable,
-  goToTargetAggregates,
-  targetById: (id) => $(`div[test-target-id="${id}"]`),
-  targetNumber: (target) => target.$('.number'),
-  targetGoal: (target) => target.$('.goal p'),
-  targetTitle: (target) => target.$('.title h2'),
   analytics,
   targetAggregatesItems,
   aggregateSummary,
