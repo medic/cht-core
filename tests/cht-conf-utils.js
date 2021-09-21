079ddfb303526cff238f6f8fe00bf3d6d9b86bc6
m5r <mokht@rmi.al>
2021-09-22 01:04:27 +0800

    replace medic-conf usage by cht-conf in `cht-conf-utils.js`

--- tests/cht-conf-utils.js
+++ tests/cht-conf-utils.js
@@ -7,7 +7,8 @@ const utils = require('./utils');
 const runCommand = async (action, dirPath) => {
   const url = utils.getInstanceUrl();
   try {
-    const { stdout } = await exec(`medic-conf --url=${url} ${action} --force --debug`, { cwd: dirPath });
+    const chtConfPath = path.resolve(process.cwd(), './node_modules/.bin/cht');
+    const { stdout } = await exec(`${chtConfPath} --url=${url} ${action} --force --debug`, { cwd: dirPath });
     return stdout;
   } catch (err) {
     return err.stdout;
