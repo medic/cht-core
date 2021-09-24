89a23ec9d2aff9a75abc4f815ce8405ae3692401
Diana Barsan <barsan@medicmobile.org>
2021-09-24 06:50:00 +0300

    pass env so extracted resources are in the same place.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,6 @@ const {
   BUILDS_SERVER,
   BUILD_NUMBER,
   CI,
-  MEDIC_API_RESOURCE_PATH,
 } = process.env;
 
 const releaseName = TAG || BRANCH || 'local-development';
@@ -335,7 +334,7 @@ module.exports = function(grunt) {
       'eslint-sw': {
         cmd: () => {
           const defaultLocation = path.join(__dirname, 'api', 'extracted-resources');
-          const destination = BUILD_NUMBER ? MEDIC_API_RESOURCE_PATH : defaultLocation;
+          const destination = BUILD_NUMBER ? '/tmp/extracted-resources' : defaultLocation;
           return `${ESLINT_COMMAND} -c ./.eslintrc ${destination}/js/service-worker.js`;
         }
       },
