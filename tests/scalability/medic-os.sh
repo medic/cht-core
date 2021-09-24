817c33bf1aca98b0294fa959eb1ab220b5396341
Joshua Kuestersteffen <jkuester@kuester7.com>
2021-09-24 15:52:24 -0500

    Fix scalability test script to set ENVARS in proper folder (#7338)

--- tests/scalability/medic-os.sh
+++ tests/scalability/medic-os.sh
@@ -7,6 +7,6 @@ cd /srv;
 git clone https://github.com/medic/cht-infrastructure.git;
 cd cht-infrastructure/self-hosting/prepare-system/ubuntu/;
 mkdir -p /home/medic/self-hosting/main/
-echo DOCKER_COUCHDB_ADMIN_PASSWORD=medicScalability >> /home/medic/self-hosting/.env
-echo HA_PASSWORD=medicScalability >> /home/medic/self-hosting/.env
+echo DOCKER_COUCHDB_ADMIN_PASSWORD=medicScalability >> /home/medic/self-hosting/main/.env
+echo HA_PASSWORD=medicScalability >> /home/medic/self-hosting/main/.env
 ./prepare.sh
