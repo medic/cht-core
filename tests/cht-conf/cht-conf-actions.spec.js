d52a1d49114208b85262b2c6637a5371a1370808
m5r <mokht@rmi.al>
2021-09-29 18:00:28 +0200

    rename variable cwd to configPath in cht-conf-actions.spec.js

--- tests/cht-conf/cht-conf-actions.spec.js
+++ tests/cht-conf/cht-conf-actions.spec.js
@@ -15,18 +15,17 @@ const actions = [
   'upload-resources',
   'upload-custom-translations',
 ];
+const configPath = 'config/default';
 
 describe('cht-conf actions tests', () => {
-  const cwd = 'config/default';
-
   it('should execute  upload-app-settings', async () => {
-    const result = await runCommand('upload-app-settings', cwd);
+    const result = await runCommand('upload-app-settings', configPath);
     expect(result).to.contain(`INFO Settings updated successfully`);
   });
 
   for (const action of actions) {
     it(`should execute ${action}`, async () => {
-      const result = await runCommand(action, cwd);
+      const result = await runCommand(action, configPath);
       expect(result).to.contain(`INFO ${action} complete.`);
     });
   }
