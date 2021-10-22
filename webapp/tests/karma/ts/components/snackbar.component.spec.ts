f397b2af6907ae10039de13f47f636ecc5b3229e
m5r <mokht@rmi.al>
2021-10-22 17:18:53 +0200

    add snackbar component unit tests

---
+++ webapp/tests/karma/ts/components/snackbar.component.spec.ts
@@ -0,0 +1,84 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { fakeAsync, tick, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+
+import { SnackbarComponent } from '@mm-components/snackbar/snackbar.component';
+import { Selectors } from '@mm-selectors/index';
+
+describe('SnackbarComponent', () => {
+  let fixture;
+  let component: SnackbarComponent;
+  let store: MockStore;
+
+  const getElement = (cssSelector) => {
+    return fixture.debugElement.query(By.css(cssSelector))?.nativeElement;
+  };
+
+  beforeEach(async () => {
+    const mockedSelectors = [
+      { selector: Selectors.getSnackbarContent, value: null },
+    ];
+
+    await TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({ selectors: mockedSelectors }),
+      ],
+    }).compileComponents();
+    store = TestBed.inject(MockStore);
+    fixture = TestBed.createComponent(SnackbarComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should hide the snackbar when no message', async () => {
+    store.overrideSelector(Selectors.getSnackbarContent, { message: undefined, action: undefined });
+
+    component.ngOnInit();
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.active).to.equal(false);
+    expect(getElement('#snackbar')).to.exist;
+    expect(getElement('#snackbar.active')).to.not.exist;
+  });
+
+  it('should display the snackbar with a message and then hide it', fakeAsync(async () => {
+    const message = 'important message';
+    component.ngOnInit();
+    store.overrideSelector(Selectors.getSnackbarContent, { message, action: undefined });
+    store.refreshState();
+
+    expect(component.active).to.equal(true);
+    expect(getElement('#snackbar.active')).to.exist;
+    expect(getElement('#snackbar.active .snackbar-message').innerText).to.equal(message);
+    expect(getElement('#snackbar.active .snackbar-action')).to.not.exist;
+
+    tick(5000);
+
+    expect(component.active).to.equal(false);
+    expect(getElement('#snackbar')).to.exist;
+    expect(getElement('#snackbar.active')).to.not.exist;
+  }));
+
+  it('should display the snackbar with a clickable action', async () => {
+    const message = 'important message';
+    const action = { label: 'click me', onClick: sinon.stub() };
+    component.ngOnInit();
+    store.overrideSelector(Selectors.getSnackbarContent, { message, action });
+    store.refreshState();
+
+    expect(component.active).to.equal(true);
+    expect(getElement('#snackbar.active')).to.exist;
+    expect(getElement('#snackbar.active .snackbar-message').innerText).to.equal(message);
+
+    const actionElement = getElement('#snackbar.active .snackbar-action');
+    expect(actionElement).to.exist;
+    expect(actionElement.innerText).to.equal(action.label);
+
+    expect(action.onClick.callCount).to.equal(0);
+    actionElement.click();
+    expect(action.onClick.callCount).to.equal(1);
+  });
+});
