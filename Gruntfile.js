94b39eb3dc35e732714660a6c7c9c06919615413
Diana Barsan <barsan@medicmobile.org>
2021-10-22 13:38:48 +0300

    Still need to install these.

--- Gruntfile.js
+++ Gruntfile.js
@@ -848,6 +848,7 @@ module.exports = function(grunt) {
   // Build tasks
   grunt.registerTask('install-dependencies', 'Update and patch dependencies', [
     'exec:undo-patches',
+    'exec:npm-ci-shared-libs',
     'exec:npm-ci-modules',
     'copy:libraries-to-patch',
     'exec:apply-patches',
