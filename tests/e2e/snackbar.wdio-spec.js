68cd07ce04609bb97c85bd8e6f5e6054042f25f7
m5r <mokht@rmi.al>
2021-10-25 22:27:27 +0200

    add snackbar e2e tests

---
+++ tests/e2e/snackbar.wdio-spec.js
@@ -0,0 +1,48 @@
+const loginPage = require('../page-objects/login/login.wdio.page');
+const commonPage = require('../page-objects/common/common.wdio.page');
+const utils = require('../utils');
+
+const district = {
+  _id: 'fixture:district',
+  type: 'district_hospital',
+  name: 'District',
+  place_id: 'district',
+  reported_date: new Date().getTime(),
+};
+
+const chw = {
+  username: 'bob',
+  password: 'medic.123',
+  place: 'fixture:district',
+  contact: { _id: 'fixture:user:bob', name: 'Bob' },
+  roles: ['chw'],
+};
+
+describe('Snackbar', () => {
+  before(async () => {
+    await utils.saveDoc(district);
+    await utils.createUsers([chw]);
+
+    await loginPage.login(chw);
+    await commonPage.waitForPageLoaded();
+    await commonPage.closeTour();
+  });
+
+  it('should display the snackbar with the syncing messages and then hide it', async () => {
+    await commonPage.openHamburgerMenu();
+    await (await commonPage.syncButton()).click();
+    await (await commonPage.activeSnackbar()).waitForDisplayed();
+    expect(await commonPage.snackbarMessage()).to.equal('Currently syncing…');
+    await browser.waitUntil(async () => await commonPage.snackbarMessage() === 'All reports synced');
+    await (await commonPage.inactiveSnackbar()).waitForDisplayed({ timeout: 6000 });
+  });
+
+  it('should display the snackbar with an action and be clickable', async () => {
+    await browser.throttle('offline');
+    await commonPage.openHamburgerMenu();
+    await (await commonPage.syncButton()).click();
+    await (await commonPage.activeSnackbar()).waitForDisplayed();
+    expect(await commonPage.snackbarMessage()).to.equal('Currently syncing…');
+    await browser.waitUntil(async () => await commonPage.snackbarMessage() === 'Sync failed. Unable to connect.');
+  });
+});
