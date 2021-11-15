671ec1aa4afa78445c3327e8ef6a9a9657d185b6
Diana Barsan <barsan@medicmobile.org>
2021-11-15 10:36:40 +0200

    Revert "Propagate AuthSession cookie max-age attribute received from CouchDB (#7386)"
    This reverts commit cd2dcebdbce85b4d1f617eb8f75c87b34f065ff6.

--- api/src/services/cookie.js
+++ api/src/services/cookie.js
@@ -1,44 +1,12 @@
 const production = process.env.NODE_ENV === 'production';
-const ONE_YEAR_IN_MS = 31536000000;
-const ONE_SECOND_IN_MS = 1000;
-const COOKIE_ATTRIBUTE_SPLIT_RE = /;\s*/;
-const sessionCookieName = 'AuthSession';
+const ONE_YEAR = 31536000000;
+const SESSION_COOKIE_RE = /AuthSession=([^;]*);/;
 
-const getCookieOptions = (options = {}) => {
-  Object.keys(options).forEach(key => {
-    if (typeof options[key] === 'undefined') {
-      delete options[key];
-    }
-  });
-
-  const securityOptions = {
+const getCookieOptions = () => {
+  return {
     sameSite: 'lax', // prevents the browser from sending this cookie along with some cross-site requests
     secure: production, // only transmit when requesting via https unless in development mode
   };
-
-  return Object.assign({}, options, securityOptions);
-};
-
-const normalizeAttributeName = (option, lowerCaseFirst = true) => {
-  const trimmed = option.trim();
-  const lowerCased = lowerCaseFirst ? `${trimmed.charAt(0).toLowerCase()}${trimmed.slice(1)}` : trimmed;
-  return lowerCased.replace(/-/g, '');
-};
-
-const extractCookieAttributes = (cookieString) => {
-  const attributes = {};
-  cookieString.split(COOKIE_ATTRIBUTE_SPLIT_RE).forEach((attributePair, idx) => {
-    const [attribute, value = ''] = attributePair.split('=');
-    const attributeName = normalizeAttributeName(attribute, !!idx /* don't lowercase cookie name */);
-
-    if (attributeName === 'maxAge') {
-      // Response.cookie needs this value in milliseconds, contrary to HTTP spec
-      attributes[attributeName] = parseInt(value.trim()) * ONE_SECOND_IN_MS;
-    } else {
-      attributes[attributeName] = value.trim();
-    }
-  });
-  return attributes;
 };
 
 module.exports = {
@@ -53,21 +21,18 @@ module.exports = {
   },
   setUserCtx: (res, content) => {
     const options = getCookieOptions();
-    options.maxAge = ONE_YEAR_IN_MS;
+    options.maxAge = ONE_YEAR;
     res.cookie('userCtx', content, options);
   },
   setSession: (res, cookie) => {
-    const { [sessionCookieName]: sessionId, maxAge } = extractCookieAttributes(cookie);
-
-    const options = getCookieOptions({
-      httpOnly: true, // don't allow javascript access to stop xss
-      maxAge, // ensure the cookie expires when the CouchDB session expires
-    });
+    const sessionId = SESSION_COOKIE_RE.exec(cookie)[1];
+    const options = getCookieOptions();
+    options.httpOnly = true; // don't allow javascript access to stop xss
     res.cookie('AuthSession', sessionId, options);
   },
   setLocale: (res, locale) => {
     const options = getCookieOptions();
-    options.maxAge = ONE_YEAR_IN_MS;
+    options.maxAge = ONE_YEAR;
     res.cookie('locale', locale, options);
   }
 };
