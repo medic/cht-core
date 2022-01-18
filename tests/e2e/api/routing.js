20300cb0cdf651cc865d69c917cc5f02de79ec0b
Diana Barsan <barsan@medicmobile.org>
2022-01-18 14:44:27 +0200

    Fix deploy_info e2e test, to include version.

--- tests/e2e/api/routing.js
+++ tests/e2e/api/routing.js
@@ -234,8 +234,9 @@ describe('routing', () => {
         utils.request(Object.assign({ path: '/api/deploy-info' }, offlineRequestOptions)),
         utils.requestOnTestDb('/_design/medic-client')
       ]).then(([ deployInfoOnline, deployInfoOffline, ddoc ]) => {
-        expect(deployInfoOnline).to.deep.equal(ddoc.deploy_info);
-        expect(deployInfoOffline).to.deep.equal(ddoc.deploy_info);
+        const deployInfo = Object.assign(ddoc.deploy_info, { version: ddoc.version });
+        expect(deployInfoOnline).to.deep.equal(deployInfo);
+        expect(deployInfoOffline).to.deep.equal(deployInfo);
       });
     });
   });
