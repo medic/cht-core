792bfa499dd574e234a08f427f73206f92316574
ngaruko <bede.ngaruko@gmail.com>
2022-02-10 14:08:55 +1300

    leave -s to see actual response

--- tests/scalability/start_ec2_medic.sh
+++ tests/scalability/start_ec2_medic.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #Requires jq
 export NODE_TLS_REJECT_UNAUTHORIZED=0
-echo Testing copy to s3
-/usr/local/bin/aws s3 cp ./csv s3://medic-e2e --recursive
 echo Triggering EC2 Run Instance Command and getting Instance ID
 instanceID=$(aws ec2 run-instances --image-id ami-065ba2b6b298ed80f --instance-type c5.2xlarge --security-group-ids sg-0fa20cd785acec256 --block-device-mappings file://block-device-mapping.json --user-data file://medic-os.sh --instance-initiated-shutdown-behavior terminate | jq .Instances[0].InstanceId -r )
 echo Instance id is $instanceID
@@ -28,7 +26,7 @@ url=https://$PublicDnsName
 echo "MEDIC_URL=$url" >> $GITHUB_ENV
 
 echo Begin Checking $url/api/info is up
-curl -s $url/api/info -k 
+curl -s $url/api/info -k
 version=$(curl -s $url/api/info -k  | jq .version -r)
 
 until [ "$version" = 0.1.0 ]
@@ -115,32 +113,33 @@ echo staging updates
 curl $MEDIC_CONF_URL/api/v1/upgrade/stage -k -X POST -H "Content-Type: application/json" -d '{"build":{"namespace":"medic","application":"medic","version":"'$1'"}}'
 
 echo pinging horti-upgrade
-curl $MEDIC_CONF_URL/medic/horti-upgrade
-staged=$(curl $MEDIC_CONF_URL/medic/horti-upgrade -s -k | jq .staging_complete -r)
+curl $MEDIC_CONF_URL/medic/horti-upgrade -k
+
+staged=$(curl $MEDIC_CONF_URL/medic/horti-upgrade -k | jq .staging_complete -r)
 echo $(curl $MEDIC_CONF_URL/medic/horti-upgrade -s -k | jq .staging_complete -r)
-# until [ "$staged" == "true" ]
-# do
-#     staged=$(curl $MEDIC_CONF_URL/medic/horti-upgrade -s -k | jq .staging_complete -r)
-#     sleep 60
-#     echo "waiting for staging to complete"
-# done
+until [ "$staged" == "true" ]
+do
+    staged=$(curl $MEDIC_CONF_URL/medic/horti-upgrade -s -k | jq .staging_complete -r)
+    sleep 60
+    echo "waiting for staging to complete"
+done
 
-# post_stage=()
-# echo medic-update-seq $(curl $MEDIC_CONF_URL/medic/ -s -k | jq .update_seq)
-# for ddoc in ${ddocs[@]}; do
-# echo $ddoc post stage value $(curl $MEDIC_CONF_URL/medic/_design/$ddoc/_info -s -k | jq .view_index.update_seq -r)
-# post_stage+=($(curl $MEDIC_CONF_URL/medic/_design/$ddoc/_info -s -k | jq .view_index.update_seq -r))
-# done
+post_stage=()
+echo medic-update-seq $(curl $MEDIC_CONF_URL/medic/ -s -k | jq .update_seq)
+for ddoc in ${ddocs[@]}; do
+echo $ddoc post stage value $(curl $MEDIC_CONF_URL/medic/_design/$ddoc/_info -s -k | jq .view_index.update_seq -r)
+post_stage+=($(curl $MEDIC_CONF_URL/medic/_design/$ddoc/_info -s -k | jq .view_index.update_seq -r))
+done
 
-# echo Checking post stage sequences to pre stage
-# for i in ${!pre_update_seqs[@]}; do
-# if ! [ ${post_stage[$i]} -ge ${pre_update_seqs[$i]} ]
-# then
-# echo "The sequence for ${ddocs[$i]} did not get updated. It should have been warmed."
-# echo "ending run"
-# exit 1
-# fi
-# done
+echo Checking post stage sequences to pre stage
+for i in ${!pre_update_seqs[@]}; do
+if ! [ ${post_stage[$i]} -ge ${pre_update_seqs[$i]} ]
+then
+echo "The sequence for ${ddocs[$i]} did not get updated. It should have been warmed."
+echo "ending run"
+exit 1
+fi
+done
 
-# echo Sequence numbers were greater. Completing upgrade
-# curl $MEDIC_CONF_URL/api/v1/upgrade/complete -k -X POST
+echo Sequence numbers were greater. Completing upgrade
+curl $MEDIC_CONF_URL/api/v1/upgrade/complete -k -X POST
