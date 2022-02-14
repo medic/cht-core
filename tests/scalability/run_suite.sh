f6d2e861f13be70b6c8c4ca0ac6bf85c7442d365
ngaruko <bede.ngaruko@gmail.com>
2022-02-14 16:40:58 +1300

    new branch

--- tests/scalability/run_suite.sh
+++ tests/scalability/run_suite.sh
@@ -56,6 +56,6 @@ git clone --single-branch --branch $TAG_NAME https://github.com/medic/cht-core.g
 touch testing-ci.txt
 git add testing-ci.txt
 git commit -m'Adding some restults'
-#git switch -c jmeter-test-branch
+git switch -c jmeter-test-branch
 git request-pull HEAD~1 origin
 echo "FINISHED! "
