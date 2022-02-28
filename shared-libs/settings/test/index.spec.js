5b8b1c8e324f78726571e89c8a07419139e977d6
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2022-03-01 12:58:25 +1300

    Fix eslint

--- shared-libs/settings/test/index.spec.js
+++ shared-libs/settings/test/index.spec.js
@@ -18,87 +18,102 @@ describe('Settings Shared Library', () => {
 
   describe('getCredentials', () => {
 
-    it('should throw error if no server url is set', async () => {
+    it('should throw error if no server url is set', () => {
       environment.COUCH_URL = '';
 
-      try {
-        await lib.getCredentials();
-        expect.fail('exception expected')
-      } catch (err) {
-        expect(err.message).to.equal('Failed to find the CouchDB server');
-      }
+      return lib
+        .getCredentials()
+        .then(() => expect.fail('exception expected'))
+        .catch(err => {
+          expect(err.message).to.equal('Failed to find the CouchDB server');
+        });
     });
 
-    it('should throw error from request', async () => {
+    it('should throw error from request', () => {
       environment.COUCH_URL = 'http://user:pass@server.com/medic';
       sinon.stub(request, 'get').rejects({ statusCode: 403, message: 'no perms' });
 
-      try {
-        await lib.getCredentials();
-        return expect.fail('exception expected');
-      } catch (err) {
-        expect(request.get.callCount).to.equal(1);
-        expect(err.message).to.equal('no perms');
-      }
+      return lib
+        .getCredentials()
+        .then(() => expect.fail('exception expected'))
+        .catch(err => {
+          expect(request.get.callCount).to.equal(1);
+          expect(err.message).to.equal('no perms');
+        });
     });
 
-    it('should handle when permissions are not defined', async () => {
+    it('should handle when permissions are not defined', () => {
       environment.COUCH_URL = 'http://user:pass@server.com/medic';
       sinon.stub(request, 'get').rejects({ statusCode: 404 });
 
-      const actual = await lib.getCredentials('mykey');
-      expect(actual).to.equal(undefined);
+      return lib
+        .getCredentials('mykey')
+        .then(actual => {
+          expect(actual).to.be.undefined;
+        });
     });
 
-    it('should handle empty credentials', async () => {
+    it('should handle empty credentials', () => {
       environment.COUCH_URL = 'http://user:pass@server.com/medic';
       sinon.stub(request, 'get').resolves({});
 
-      const actual = await lib.getCredentials('mykey');
-      expect(actual).to.be.undefined;
+      return lib
+        .getCredentials('mykey')
+        .then(actual => {
+          expect(actual).to.be.undefined;
+        });
     });
 
-    it('should parse response format', async () => {
+    it('should parse response format', () => {
       environment.COUCH_URL = 'http://server.com/medic';
-      sinon.stub(request, 'get').resolves({ password: 'mypass' }); // TODO string or JSON response?
+      sinon.stub(request, 'get').resolves({ password: 'mypass' });
 
-      const actual = await lib.getCredentials('mykey');
-      expect(actual).to.equal('mypass');
-      expect(request.get.callCount).to.equal(1);
-      expect(request.get.args[0][0]).to.equal('http://server.com/medic-vault/mykey');
+      return lib
+        .getCredentials('mykey')
+        .then(actual => {
+          expect(actual).to.equal('mypass');
+          expect(request.get.callCount).to.equal(1);
+          expect(request.get.args[0][0]).to.equal('http://server.com/medic-vault/mykey');
+        });
     });
 
   });
 
   describe('getCouchConfig', () => {
 
-    it('should return the expected value', async () => {
+    it('should return the expected value', () => {
       environment.COUCH_URL = 'http://user:pass@localhost:6929/medic';
       environment.COUCH_NODE_NAME = 'nonode@noname';
       sinon.stub(request, 'get').resolves('couch config');
 
-      const actual = await lib.getCouchConfig('attachments');
-      expect(actual).to.equal('couch config');
-      expect(request.get.callCount).to.equal(1);
-      expect(request.get.args[0][0].url).to.equal('http://user:pass@localhost:6929/_node/nonode@noname/_config/attachments');
+      return lib
+        .getCouchConfig('attachments')
+        .then(actual => {
+          expect(actual).to.equal('couch config');
+          expect(request.get.callCount).to.equal(1);
+          expect(request.get.args[0][0].url).to.equal('http://user:pass@localhost:6929/_node/nonode@noname/_config/attachments');
+        });
     });
 
   });
 
   describe('updateAdminPassword', () => {
 
-    it('should save password', async () => {
+    it('should save password', () => {
       environment.COUCH_URL = 'http://user:pass@localhost:6929/medic';
       environment.COUCH_NODE_NAME = 'nonode@noname';
       sinon.stub(request, 'put').resolves('-pbkdf2-8266a0adb');
 
-      const result = await lib.updateAdminPassword('admin1', 'pass1234');
-      expect(result).to.equal('-pbkdf2-8266a0adb');
-      expect(request.put.callCount).to.equal(1);
-      expect(request.put.args[0][0]).to.deep.equal({
-        body: '"pass1234"',
-        url: 'http://user:pass@localhost:6929/_node/nonode@noname/_config/admins/admin1',
-      });
+      return lib
+        .updateAdminPassword('admin1', 'pass1234')
+        .then(result => {
+          expect(result).to.equal('-pbkdf2-8266a0adb');
+          expect(request.put.callCount).to.equal(1);
+          expect(request.put.args[0][0]).to.deep.equal({
+            body: '"pass1234"',
+            url: 'http://user:pass@localhost:6929/_node/nonode@noname/_config/admins/admin1',
+          });
+        });
     });
 
   });
