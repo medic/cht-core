caf6ab0fb5f67f9c2069c715d204b3798ee6c43e
Joshua Kuestersteffen <jkuester@kuester7.com>
2022-03-22 14:23:59 -0500

    Remove commented from karma config

--- shared-libs/enketo-form-manager/test/karma.conf.js
+++ shared-libs/enketo-form-manager/test/karma.conf.js
@@ -41,7 +41,6 @@ module.exports = function(config) {
     files: [
       'node_modules/jquery/dist/jquery.js',
       'test/enketo-form-manager.spec.js',
-      // 'test/enketo-xml/visit.xml'
     ],
     preprocessors: {
       'test/enketo-form-manager.spec.js': ['webpack'],
