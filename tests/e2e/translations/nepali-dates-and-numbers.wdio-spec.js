e3c78b0c013745db51237e4c4871f0d048e1a87a
Binod Adhikary <binokary@gmail.com>
2022-03-23 18:28:49 +0545

    adding await for wdio calls

--- tests/e2e/translations/nepali-dates-and-numbers.wdio-spec.js
+++ tests/e2e/translations/nepali-dates-and-numbers.wdio-spec.js
@@ -241,8 +241,8 @@ describe('Bikram Sambat date display', () => {
 
     await commonPage.goToPeople();
     await commonPage.goToReports();
-    const firstReport = reportsPage.firstReport();
-    firstReport.click();
+    const firstReport = await reportsPage.firstReport();
+    await firstReport.click();
 
     const dateFormat = bikramSambat.toBik_text(tenWeeksAgo);
     const relativeFormat = moment(tenWeeksAgo.toDate()).fromNow();
@@ -264,8 +264,8 @@ describe('Bikram Sambat date display', () => {
 
     await commonPage.goToPeople();
     await commonPage.goToReports();
-    const firstReport = reportsPage.firstReport();
-    firstReport.click();
+    const firstReport = await reportsPage.firstReport();
+    await firstReport.click();
 
     const dateFormat = bikramSambat.toBik_text(nineWeeksAgo);
     const relativeFormat = moment(nineWeeksAgo.toDate()).fromNow();
