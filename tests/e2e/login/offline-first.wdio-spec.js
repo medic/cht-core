9b0ff27a3b9848ef87f53395fdd48ac576be9245
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2022-03-23 13:53:10 +1300

    Does this pass in CI?

--- tests/e2e/login/offline-first.wdio-spec.js
+++ tests/e2e/login/offline-first.wdio-spec.js
@@ -1,19 +1,32 @@
-const commonElements = require('../../page-objects/common/common.wdio.page.js');
-const utils = require('../../utils');
+const commonPage = require('../../page-objects/common/common.wdio.page');
+const loginPage = require('../../page-objects/login/login.wdio.page');
 
 describe('Offline first', () => {
 
-  beforeAll(async () => {
-    await commonElements.goToMessagesNative();
+  beforeEach(async () => {
+    await loginPage.cookieLogin();
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     await browser.throttle('online'); // remove throttling
   });
 
   it('should be able to load with throttled connection', async () => {
-    await browser.throttle({ latency: 60000 }); // take a minute to respond to any request
-    await utils.resetBrowser();
+    const turtleMode = {
+      offline: false,
+      latency: 60000, // take a minute to respond to any request
+      downloadThroughput: -1,
+      uploadThroughput: -1
+    };
+    await browser.throttle(turtleMode);
+    await browser.refresh();
+    await commonPage.goToBase(); // assert page loads
+  });
+
+  it('should be able to load when offline', async () => {
+    await browser.throttle('offline');
+    await browser.refresh();
+    await commonPage.goToBase(); // assert page loads
   });
 
 });
