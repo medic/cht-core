2900c59e4670593b4716752f1ccf88a7085c048a
Diana Barsan <barsan@medic.org>
2022-04-12 13:45:04 +0300

    remove "long" migrations

--- api/src/migrations/long-one2.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'long-one',
-  created: new Date(2022, 4, 5),
-  run: () => new Promise(r => {
-    const fiveMin = 5 * 60 * 1000;
-    setTimeout(r, fiveMin);
-  }),
-};
