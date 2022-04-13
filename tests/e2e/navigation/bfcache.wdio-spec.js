93b0130cc882f103ecbe4e214160ebaf3da174b1
m5r <mokht@rmi.al>
2022-04-13 12:31:44 +0200

    test webapp, admin app and login page

--- tests/e2e/navigation/bfcache.wdio-spec.js
+++ tests/e2e/navigation/bfcache.wdio-spec.js
@@ -1,16 +1,52 @@
 const commonPage = require('../../page-objects/common/common.wdio.page');
 const loginPage = require('../../page-objects/login/login.wdio.page');
+const usersAdminPage = require('../../page-objects/admin/user.wdio.page');
+const auth = require('../../auth')();
 
 describe('bfcache', async () => {
-  it('should redirect to login page when session is expired', async () => {
-    await loginPage.cookieLogin();
-    await commonPage.goToPeople();
-    await browser.deleteCookies('AuthSession');
-    await commonPage.goToMessages();
-    const redirectToLoginBtn = await $('#session-expired .btn.submit.btn-primary');
-    await redirectToLoginBtn.click();
-    expect(await browser.getUrl()).to.contain('/medic/login?redirect=');
-    await browser.back();
-    expect(await browser.getUrl()).to.contain('/medic/login?redirect=');
+  beforeEach(async () => {
+    await loginPage.login({
+      username: auth.username,
+      password: auth.password,
+      createUser: true,
+    });
+  });
+
+  afterEach(async () => {
+    await browser.deleteCookies();
+    await browser.url('/');
+  });
+
+  describe('login page', () => {
+    it('should redirect to the app page when session is valid', async () => {
+      await commonPage.goToBase();
+      expect(await browser.getUrl()).to.contain('/messages');
+      await browser.back();
+      expect(await browser.getUrl()).to.contain('/messages');
+    });
+  });
+
+  describe('webapp', () => {
+    it('should redirect to login page when session is expired', async () => {
+      await commonPage.goToPeople();
+      await browser.deleteCookies('AuthSession');
+      await commonPage.goToMessages();
+      const redirectToLoginBtn = await $('#session-expired .btn.submit.btn-primary');
+      await redirectToLoginBtn.click();
+      expect(await browser.getUrl()).to.contain('/medic/login?redirect=');
+      await browser.back();
+      expect(await browser.getUrl()).to.contain('/medic/login?redirect=');
+    });
+  });
+
+  describe('admin app', () => {
+    it('should redirect to login page when session is expired', async () => {
+      await usersAdminPage.goToAdminUser();
+      await browser.deleteCookies('AuthSession');
+      await usersAdminPage.goToAdminUpgrade();
+      expect(await browser.getUrl()).to.contain('/medic/login?redirect=');
+      await browser.back();
+      expect(await browser.getUrl()).to.contain('/medic/login?redirect=');
+    });
   });
 });
