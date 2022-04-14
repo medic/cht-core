37d6469d7389c22cbe41d8903856d5c2a27db588
m5r <mokht@rmi.al>
2022-04-14 09:03:12 +0200

    revert wdio.conf.js changes

--- tests/wdio.conf.js
+++ tests/wdio.conf.js
@@ -40,8 +40,7 @@ const baseConfig = {
   // will be called from there.
   //
   specs: [
-    // './tests/e2e/**/*.wdio-spec.js',
-    './tests/e2e/**/bfcache.wdio-spec.js',
+    './tests/e2e/**/*.wdio-spec.js',
   ],
   // Patterns to exclude.
   exclude: [
