20760911832ed5825f9e1f9cd871a0b8e0a6d18e
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2022-04-22 10:20:19 -0400

    brac-fp-flow requested changes

--- tests/factories/brac/contacts/brac-fp-flow.js
+++ tests/factories/brac/contacts/brac-fp-flow.js
@@ -1,43 +1,40 @@
 const Factory = require('rosie').Factory;
 const Faker = require('@faker-js/faker');
 
+const CONDOMS = 'condoms';
+const BIRTH_CONTROL = ['iud', 'btl', 'emergency_pill', 'norplant', 'depoprovera', 'progesterone', 'dmpa', 'cop'];
+const NONE = 'fp_none';
+
 module.exports = new Factory()
   .attr('fp', Faker.faker.datatype.boolean())
   .attr('fp_method', ['fp'], (fp) => {
-    if (!fp) {
-      return null;
-    } else {
+    if (fp) {
       const methods = [];
-      methods.push(Faker.faker.random.arrayElement(
-        ['iud', 'condoms', 'btl', 'emergency_pill', 'norplant', 'depoprovera',
-          'progesterone', 'dmpa', 'cop', 'fp_none']));
-      if (methods[0] === 'condoms') {
+      methods.push(Faker.faker.random.arrayElement([...BIRTH_CONTROL, CONDOMS, NONE]));
+      //If the selected birth control method is "condoms", another one can be optionally selected
+      if (methods[0] === CONDOMS) {
         methods.push(Faker.faker.helpers.uniqueArray(
-          ['iud', 'btl', 'emergency_pill', 'norplant', 'depoprovera', 'progesterone', 'dmpa', 'cop'],
-          Faker.faker.datatype.number({ min: 0, max: 1 })));
+          BIRTH_CONTROL,
+          Faker.faker.datatype.number({ min: 0, max: 1 })).toString());
       }
-      return methods.toString().replace(',', ' ');
+      return methods.join(' ');
     }
   })
   .attr('fp_enroll', ['fp', 'fp_method'], (fp, fpMethod) => {
-    if (fp || fpMethod !== 'fp_none') {
-      return null;
-    } else {
+    if (!fp || fpMethod === NONE) {
       return Faker.faker.datatype.boolean();
     }
   })
   .attr('fp_method_choice', ['fp_enroll'], (fp_enroll) => {
-    if (!fp_enroll) {
-      return null;
-    } else {
+    if (fp_enroll) {
       const methods = [];
-      methods.push(Faker.faker.random.arrayElement(
-        ['iud', 'condoms', 'btl', 'emergency_pill', 'norplant', 'depoprovera', 'progesterone', 'dmpa', 'cop']));
-      if (methods[0] === 'condoms') {
+      methods.push(Faker.faker.random.arrayElement([...BIRTH_CONTROL, CONDOMS, NONE]));
+      //If the selected birth control method is "condoms", another one can be optionally selected
+      if (methods[0] === CONDOMS) {
         methods.push(Faker.faker.helpers.uniqueArray(
-          ['iud', 'btl', 'emergency_pill', 'norplant', 'depoprovera', 'progesterone', 'dmpa', 'cop'],
-          Faker.faker.datatype.number({ min: 0, max: 1 })));
+          BIRTH_CONTROL,
+          Faker.faker.datatype.number({ min: 0, max: 1 })).toString());
       }
-      return methods.toString().replace(',', ' ');
+      return methods.join(' ');
     }
   });
