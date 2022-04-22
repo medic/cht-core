dc330997ee37d614c34920a482f334e11ce055ae
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2022-04-22 10:57:21 -0400

    brac-fp-flow documentation

--- tests/factories/brac/contacts/brac-fp-flow.js
+++ tests/factories/brac/contacts/brac-fp-flow.js
@@ -5,19 +5,32 @@ const CONDOMS = 'condoms';
 const BIRTH_CONTROL = ['iud', 'btl', 'emergency_pill', 'norplant', 'depoprovera', 'progesterone', 'dmpa', 'cop'];
 const NONE = 'fp_none';
 
+/**
+ * Generates birth control method.
+ * None cannot be selected along with the other options.
+ * Condoms are the only family planing method that can be combined with another family planning method.
+ */
+const generateFpMethods = () => {
+  const methods = [];
+  methods.push(Faker.faker.random.arrayElement([...BIRTH_CONTROL, CONDOMS, NONE]));
+  if (methods[0] === CONDOMS) {
+    methods.push(Faker.faker.helpers.uniqueArray(
+      BIRTH_CONTROL,
+      Faker.faker.datatype.number({ min: 0, max: 1 })).toString());
+  }
+  return methods.join(' ');
+}
+/**
+ * BRAC International is a leading nonprofit organisation with a mission to empower people and
+ * communities in situations of poverty, illiteracy, disease, and social injustice
+ * brac-fp-flow Factory generates Javascript object that represent Family Planing Workflow
+ * for the brac configuration.
+ */
 module.exports = new Factory()
   .attr('fp', Faker.faker.datatype.boolean())
   .attr('fp_method', ['fp'], (fp) => {
     if (fp) {
-      const methods = [];
-      methods.push(Faker.faker.random.arrayElement([...BIRTH_CONTROL, CONDOMS, NONE]));
-      //If the selected birth control method is "condoms", another one can be optionally selected
-      if (methods[0] === CONDOMS) {
-        methods.push(Faker.faker.helpers.uniqueArray(
-          BIRTH_CONTROL,
-          Faker.faker.datatype.number({ min: 0, max: 1 })).toString());
-      }
-      return methods.join(' ');
+      return generateFpMethods();
     }
   })
   .attr('fp_enroll', ['fp', 'fp_method'], (fp, fpMethod) => {
@@ -27,14 +40,6 @@ module.exports = new Factory()
   })
   .attr('fp_method_choice', ['fp_enroll'], (fp_enroll) => {
     if (fp_enroll) {
-      const methods = [];
-      methods.push(Faker.faker.random.arrayElement([...BIRTH_CONTROL, CONDOMS, NONE]));
-      //If the selected birth control method is "condoms", another one can be optionally selected
-      if (methods[0] === CONDOMS) {
-        methods.push(Faker.faker.helpers.uniqueArray(
-          BIRTH_CONTROL,
-          Faker.faker.datatype.number({ min: 0, max: 1 })).toString());
-      }
-      return methods.join(' ');
+      return generateFpMethods();
     }
   });
