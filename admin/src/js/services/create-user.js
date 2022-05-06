b76287f45bcd95378dded14bea2301b679305605
Jennifer Q <66472237+latin-panda@users.noreply.github.com>
2022-05-06 22:19:53 +0300

    Bulk user upload API and Admin GUI

--- admin/src/js/services/create-user.js
+++ admin/src/js/services/create-user.js
@@ -1,38 +1,77 @@
-angular.module('services').factory('CreateUser',
-  function(
-    $http,
-    $log,
-    $q
-  ) {
-    'ngInject';
-    'use strict';
+(function () {
 
-    /**
-     * Creates a user from a collection of updates
-     *
-     * Updates are in the style of the /api/v1/users/{username} service, see
-     * its documentation for more details.
-     *
-     * @param      {Object}  updates        Updates you wish to make
-     */
-    return function(updates) {
-      const url = '/api/v1/users';
+  'use strict';
+  const url = '/api/v2/users';
 
-      if (!updates.username) {
-        return $q.reject('You must provide a username to create a user');
-      }
+  angular.module('services').factory('CreateUser',
+    function (
+      $http,
+      $log,
+      $q
+    ) {
+      'ngInject';
 
-      $log.debug('CreateUser', url, updates);
+      /**
+         * Creates a user from a collection of updates
+         *
+         * Updates are in the style of the /api/v1/users/{username} service, see
+         * its documentation for more details.
+         *
+         * @param      {Object}  updates        Updates you wish to make
+         */
+      return function (updates) {
+        //const url = '/api/v1/users';
 
-      return $http({
-        method: 'POST',
-        url: url,
-        data: updates,
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
+        if (!updates.username) {
+          return $q.reject('You must provide a username to create a user');
         }
-      });
-    };
-  }
+
+        $log.debug('CreateUser', url, updates);
+
+        return $http({
+          method: 'POST',
+          url: url,
+          data: updates,
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          }
+        });
+      };
+    }
+  );
+
+  angular.module('services').factory('CreateMultipleUser',
+    function (
+      $http,
+      $log
+    ) {
+      'ngInject';
+
+      /**
+             * Creates a list of users from a formatted csv file
+             *
+             * data is the content of the csv file
+             *
+             * @param      {Object}  data        content of the csv file
+             */
+      return function (data) {
+        //const url = '/api/v1/users';
+
+        $log.debug('CreateMultipleUser', url, data);
+
+        return $http({
+          method: 'POST',
+          url: url,
+          data: data,
+          headers: {
+            'Content-Type': 'text/csv',
+            'Accept': 'application/json'
+          }
+        });
+      };
+    }
+  );
+  
+}()
 );
