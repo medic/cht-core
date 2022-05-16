2f857d5ab6246003bc7ad89809debcffee0765ce
m5r <mokht@rmi.al>
2022-05-16 18:27:48 +0200

    detect if running inside cht-android and what version it's on

--- webapp/src/ts/services/browser-detector.service.ts
+++ webapp/src/ts/services/browser-detector.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 import * as Bowser from 'bowser';
+import { Store } from '@ngrx/store';
+import { Selectors } from '@mm-selectors/index';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrowserDetectorService {
-  private parser = Bowser.getParser(window.navigator.userAgent);
+  private readonly parser = Bowser.getParser(window.navigator.userAgent);
+  private androidAppVersion: VersionNumber | undefined;
+
+  public constructor(private store: Store) {
+    this.store.select(Selectors.getAndroidAppVersion).subscribe(androidAppVersion => {
+      this.androidAppVersion = androidAppVersion;
+    });
+  }
 
   public detect() {
     return this.parser.getBrowser();
@@ -17,4 +26,19 @@ export class BrowserDetectorService {
       firefox: '>=98', // latest at the time
     });
   }
+
+  public isUsingChtAndroid() {
+    return typeof this.androidAppVersion !== 'undefined';
+  }
+
+  public isUsingChtAndroidV1() {
+    if (!this.isUsingChtAndroid()) {
+      return false;
+    }
+
+    return this.androidAppVersion.startsWith('v1.');
+  }
 }
+
+type VersionSuffix = `` | `-${string}`;
+type VersionNumber = 'SNAPSHOT' | `v${string}.${string}.${string}${VersionSuffix}`;
