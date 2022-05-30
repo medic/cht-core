2ca415dfd7d2913278bb1be9b48407c4c6a9ad68
ngaruko <bede.ngaruko@gmail.com>
2022-05-30 13:45:13 +1200

    check that setting have been uploaded

--- tests/cht-conf/cht-conf-actions.spec.js
+++ tests/cht-conf/cht-conf-actions.spec.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 
 const { runCommand } = require('../cht-conf-utils');
+const utils = require('../utils');
 
 //Not all actions tested here due to missing forms and config
 //[convert-collect-forms , upload-collect-form, upload-branding, upload-partners, upload-privacy-policies]
@@ -19,15 +20,36 @@ const actions = [
 const configPath = 'config/default';
 
 describe('cht-conf actions tests', () => {
+  before(async () => {
+    const settings = await utils.getDoc('settings');
+    expect(settings._rev.startsWith('1-')).to.be.true;
+    expect(settings.settings.roles).to.not.include.any.keys('program_officer', 'chw_supervisor', 'chw');
+  });
+
+  after(async () => await utils.revertSettings(true));
+  
   it('should execute  upload-app-settings', async () => {
     const result = await runCommand('upload-app-settings', configPath);
     expect(result).to.contain(`INFO Settings updated successfully`);
+    const settings = await utils.getDoc('settings');
+    expect(settings._rev.startsWith('2-')).to.be.true;
+    expect(settings.settings.roles).to.include.all.keys('program_officer', 'chw_supervisor', 'chw');
   });
 
-  for (const action of actions) {
-    it(`should execute ${action}`, async () => {
-      const result = await runCommand(action, configPath);
-      expect(result).to.contain(`INFO ${action} complete.`);
-    });
-  }
+  // for (const action of actions) {
+  //   it(`should execute ${action}`, async () => {
+  //     const result = await runCommand(action, configPath);
+  //     expect(result).to.contain(`INFO ${action} complete.`);
+  //   });
+  // }
+// before gets settings
+  //compile settings
+  //upload-settings
+  //upload contact forms
+  //upload app forms
+  //upload resources
+  //upload-branding
+  //upload-partners
+  //upload-privacy-policies
+  //upload-collect-form
 });
