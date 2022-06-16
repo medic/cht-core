5d145034b45232c3c245fe294845f5ee68158e93
Prajwol Shrestha <prajwol.shrestha18@gmail.com>
2022-06-16 18:15:29 +0545

    Added logic to store conflicted files and their diff(s) on a separate folder for each document conflict

--- scripts/conflicts/diff.js
+++ scripts/conflicts/diff.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const //_ = require('underscore'),
   parseArgs = require('minimist');
 const jsondiff = require('json-diff');
@@ -10,8 +11,12 @@ const argv = parseArgs(process.argv);
 
 const server = argv._[2] || argv.server || process.env.COUCH_URL;
 
+// Example
+//server = 'https://user:pass@medic-bhaktapur.com/medic'
+
+
 if (!server || argv.h || argv.help) {
-  console.log('You need to provide a Medic CouchDB url:');
+  console.log('You need to provide a Medic CouchDB url with baisc authentication (usename and password. Check 1password');
   console.log('  node diff.js http://localhost:5984/medic');
   console.log('  node diff.js --server=http://localhost:5984/medic');
   console.log('  COUCH_URL=http://localhost:5984/medic node diff.js');
@@ -20,37 +25,91 @@ if (!server || argv.h || argv.help) {
 
 console.log(`Generating diffs for conflicts on ${server}`);
 
+
 const DB = new PouchDB(server);
 
-DB.query('medic-conflicts/conflicts')
+let mainConflictFilesDirectory = "./doc-conflicts"
+
+
+DB.query('medic-conflicts/conflicts', {reduce:false})
   .then(conflicts => {
     console.log(`Found ${conflicts.rows.length} conflicts`);
 
+let mainConflictFilesDirectory = "./doc-conflicts"
     let ps = Promise.resolve();
+    if (!fs.existsSync(mainConflictFilesDirectory)){
+      try {
+          fs.mkdirSync(mainConflictFilesDirectory);
+      }catch(err){
+        console.log(`Error while creating main conflict folder: ${err}`);
+      }
+    }
     conflicts.rows.forEach(r => {
       ps = ps.then(() => {
-        // console.log(r);
-        if (r.key.length !== 1) {
-          console.log(`${r.id} has ${r.key.length} conflicts, we only want to deal with 1, skipping...`);
-        } else {
-          const docId = r.id;
-          const conflictRev = r.key[0];
-          return DB.get(docId, { open_revs: 'all' })
-            .then(results => {
-              // console.log(results);
-              // Make sure mine and theirs are the right way around
-              const mine = results.find(r => r.ok._rev !== conflictRev).ok;
-              const theirs = results.find(r => r.ok._rev === conflictRev).ok;
+        const docId = r.id;
+        const conflictRev = r.key[0];
+        arrayRevsIdThatShouldNotBeThere = r.key;
+        return DB.get(docId, { open_revs: 'all' })
+          .then(results => {
 
-              console.log('=====================');
-              console.log(r.id);
-              console.log('=====================');
-              console.log(jsondiff.diffString(mine, theirs));
-              console.log('\n\n');
-            });
-        }
+            // Get the main_doc. Maindoc is the doc that isn't in the conflict list
+            //const main_doc = results.find(r => r.ok._rev !== arrayRevsIdThatShouldNotBeThere).ok;
+            const main_doc = results.filter(r=>!arrayRevsIdThatShouldNotBeThere.includes(r.ok._rev))[0].ok 
+
+            //Now we are going to put each doc, its conflicts and thier diff in one file for manual inspection if required.
+            //The doc id might have characters that cannot be used to create folders.
+            if(r.id.includes('target')){
+              conflictDirectoryName = "target_" + r.id.split(":")[0]
+            } //If doc is task add ask and part of doc id
+            else if (r.id.includes('task')){
+              conflictDirectoryName = "task_" + r.id.split(":")[1]
+            } // if it is just a contact doc use the doct Id to create folder
+            else {
+              conflictDirectoryName = r.id
+            }
+            
+            conflictDirectoryName.replace(/[^0-9a-z]/gi, '')
+            let conflictDirectoryPath = `${mainConflictFilesDirectory}/${conflictDirectoryName}` 
+            if (!fs.existsSync(conflictDirectoryPath)){
+              try {
+                fs.mkdirSync(conflictDirectoryPath);
+              }
+              catch(err){
+                  console.log(err);
+              }
+            }
+            var writeStream = fs.createWriteStream(conflictDirectoryPath + '/' + 'mainDocument.json');
+            writeStream.write(JSON.stringify(main_doc,null,3))
+            writeStream.close()
+            r.key.forEach ( conflict_doc_id => {
+                let conflict_doc = results.find(r => r.ok._rev === conflict_doc_id).ok;
+                //Generate Json Diff
+                jsonDiff = jsondiff.diff(main_doc, conflict_doc);
+                //Generate String Diff
+                stringJsonDiff = jsondiff.diffString(main_doc, conflict_doc)
+                console.log(stringJsonDiff);
+                var writeStream = fs.createWriteStream(conflictDirectoryPath + '/diff_' +conflict_doc_id + '.json');
+                writeStream.write(JSON.stringify(jsonDiff,null,3))
+                  
+                var writeStream = fs.createWriteStream(conflictDirectoryPath + '/' + conflict_doc_id + '.json');
+                writeStream.write(JSON.stringify(conflict_doc,null,3))
+                writeStream.close()
+
+                var writeStream = fs.createWriteStream(conflictDirectoryPath + '/string_diff_' + conflict_doc_id + '.txt','utf16le');
+                writeStream.write(stringJsonDiff)
+                writeStream.close()
+              }
+            )
+
+            console.log('=====================');             
+            console.log('=====================');
+
+            console.log('\n\n');
+          })            
       });
     });
     return ps;
   })
-  .catch(console.error);
+  .catch(err => {
+    console.log(err);
+  })
