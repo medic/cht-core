59e5929aa1bbe998fc0555076c492a56641f2efb
elvisdorkenoo <elvisdorkenoo@gmail.com>
2022-06-30 18:50:42 +0000

    fix test & disable eslint for FA js files

--- admin/src/js/fontawesome/js/v4-shims.js
+++ admin/src/js/fontawesome/js/v4-shims.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /*!
  * Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
