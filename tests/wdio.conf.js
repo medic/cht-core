e7503f0f4c84ed04eeeb3b34aae417bf45ab60a6
ngaruko <bede.ngaruko@gmail.com>
2022-07-05 16:07:05 +1200

    run all tests

--- tests/wdio.conf.js
+++ tests/wdio.conf.js
@@ -40,7 +40,7 @@ const baseConfig = {
   // will be called from there.
   //
   specs: [
-    './tests/e2e/**/muac-follow-up.wdio-spec.js',
+    './tests/e2e/**/*.wdio-spec.js',
   ],
   // Patterns to exclude.
   exclude: [
@@ -78,7 +78,7 @@ const baseConfig = {
     browserName: 'chrome',
     acceptInsecureCerts: true,
     'goog:chromeOptions': {
-      args: [ '--disable-gpu', '--deny-permission-prompts']
+      args: ['--headless', '--disable-gpu', '--deny-permission-prompts']
     }
 
     // If outputDir is provided WebdriverIO can capture driver session logs
@@ -120,7 +120,7 @@ const baseConfig = {
   baseUrl: constants.BASE_URL,
   //
   // Default timeout for all waitFor* commands.
-  waitforTimeout: 40000,
+  waitforTimeout: 15000,
   //
   // Default timeout in milliseconds for request
   // if browser driver or grid doesn't send response
