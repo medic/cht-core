8265b70a58bfe37c9fd254477911a14098bcaa46
ngaruko <bede.ngaruko@gmail.com>
2022-07-11 08:57:07 +1200

    refacactor spec

--- tests/e2e/forms/add-family-form.wdio-specs.js
+++ tests/e2e/forms/add-family-form.wdio-specs.js
@@ -1,25 +1,17 @@
-const familyForm = require('../../page-objects/forms/add-family-form.po');
-const genericForm = require('../../page-objects/forms/generic-form.po');
-const common = require('../../page-objects/common/common.po.js');
+const familyForm = require('../../page-objects/forms/add-family-form.wdio.page');
+const genericForm = require('../../page-objects/forms/generic-form.wdio.page');
+const common = require('../../page-objects/common/common.wdio.page.js');
 const utils = require('../../utils');
 const userData = require('../../page-objects/forms/data/user.po.data');
-const reportsPage = require('../../page-objects/reports/reports.po');
+const reportsPage = require('../../page-objects/reports/reports.wdio.page');
 
 describe('Family form', () => {
   const contactId = userData.contactId;
   const docs = userData.docs;
 
-  beforeAll(async () => {
+  before(async () => {
     await utils.saveDocs(docs);
     await familyForm.configureForm(contactId);
-  });
-
-  afterEach(async () => {
-    await utils.resetBrowser();
-  });
-
-  afterAll(async () => {
-    await utils.afterEach();
   });
 
   it('Submit Add Family form', async () => {
@@ -58,12 +50,12 @@ describe('Family form', () => {
     await genericForm.invalidateReportNative();
     const reportId = await reportsPage.getCurrentReportId();
     const invalidatedReport = await utils.getDoc(reportId);
-    expect(invalidatedReport.verified).toBe(false);
-    expect(invalidatedReport.contact).toEqual({ _id: 'e2e_contact_test_id' });
+    expect(invalidatedReport.verified).to.be.false;
+    expect(invalidatedReport.contact).to.equal({ _id: 'e2e_contact_test_id' });
 
     await genericForm.validateReportNative();
     const validatedReport = await utils.getDoc(reportId);
-    expect(validatedReport.verified).toBe(true);
-    expect(validatedReport.contact).toEqual({ _id: 'e2e_contact_test_id' });
+    expect(validatedReport.verified).to.be.true;
+    expect(validatedReport.contact).to.equal({ _id: 'e2e_contact_test_id' });
   });
 });
