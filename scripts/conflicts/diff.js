a25a8df8ab5550bb3248faf478ef94d689d5d5bf
Prajwol Shrestha <prajwol.shrestha18@gmail.com>
2022-07-13 15:13:06 +0545

    Made code review changes

--- scripts/conflicts/diff.js
+++ scripts/conflicts/diff.js
@@ -11,9 +11,8 @@ const argv = parseArgs(process.argv);
 
 const server = argv._[2] || argv.server || process.env.COUCH_URL;
 
-// Example
-//server = 'https://user:pass@medic-bhaktapur.com/medic'
-
+//Example Usage
+//node diff.js 'https://user:pass@medic-bhaktapur.com/medic'
 
 if (!server || argv.h || argv.help) {
   console.log(`You need to provide a Medic CouchDB url with baisc authentication 
@@ -29,11 +28,13 @@ console.log(`Generating diffs for conflicts on ${server}`);
 
 const DB = new PouchDB(server);
 
-
 const writeToFile = function(path, content) {
-  const writeStream = fs.createWriteStream(path);
-  writeStream.write(JSON.stringify(content, null, 3));
-  writeStream.close();
+  content  = JSON.stringify(content, null, 3); 
+  fs.writeFile(path, content, (err) => {
+    if(err){
+      console.log(err);
+    }
+  });
 };
 
 DB.query('medic-conflicts/conflicts', {reduce:false})
@@ -52,26 +53,28 @@ DB.query('medic-conflicts/conflicts', {reduce:false})
     conflicts.rows.forEach(r => {
       ps = ps.then(() => {
         const docId = r.id;
-        const arrayRevsIdThatShouldNotBeThere = r.key;
+        const conflictingRevs = r.key;
         return DB.get(docId, { open_revs: 'all' })
           .then(results => {
 
             // Get the main_doc. Maindoc is the doc that isn't in the conflict list
-            const main_doc = results.filter(r => !arrayRevsIdThatShouldNotBeThere.includes(r.ok._rev))[0].ok; 
+            const mainDoc = results.filter(r => !conflictingRevs.includes(r.ok._rev))[0].ok; 
 
             //Now we are going to put each doc, its conflicts and thier diff 
             //in one file for manual inspection if required.
-            //The doc id might have characters that cannot be used to create folders.
+            // Main goal  is to have folder name that contains substring of document ID
+            //of document of couch db so that it is easily searchable with file name
+            // copy and paste.
             let conflictDirectoryName = r.id;
             if(r.id.includes('target')){
-              conflictDirectoryName = 'target_' + r.id.split(':')[0];
-            } else if (r.id.includes('task')){ //If doc is task add ask and part of doc id
-              conflictDirectoryName = 'task_' + r.id.split(':')[1];
-            } else {  // if it is just a contact doc use the doct Id to create folder
+              conflictDirectoryName = r.id.split(':')[0];
+            } else {  // if it is just a contact doc use the doc Id to create folder
               conflictDirectoryName = r.id;
             }
-            
-            conflictDirectoryName.replace(/[^0-9a-z]/gi, '');
+            //The doc id that is used for foldername might have characters that cannot 
+            // be used to create folders. Hence we are replacing illegeal characters with space. 
+            // Reverse this process to search for doc in DB.
+            conflictDirectoryName = conflictDirectoryName.replace(/[/\\?%*:|"<>]/g, ' ');
             const conflictDirectoryPath = `${mainConflictFilesDirectory}/${conflictDirectoryName}`; 
             if (!fs.existsSync(conflictDirectoryPath)){
               try {
@@ -81,23 +84,23 @@ DB.query('medic-conflicts/conflicts', {reduce:false})
               }
             }
 
-            writeToFile(conflictDirectoryPath + '/' + 'mainDocument.json', main_doc);
-            r.key.forEach( conflict_doc_id => {
-              const conflict_doc = results.find(r => r.ok._rev === conflict_doc_id).ok;
+            writeToFile(conflictDirectoryPath + '/' + 'mainDocument.json', mainDoc);
+            r.key.forEach( conflictDocId => {
+              const conflictDoc = results.find(r => r.ok._rev === conflictDocId).ok;
               //Generate Json Diff
-              const jsonDiff = jsondiff.diff(main_doc, conflict_doc);
+              const jsonDiff = jsondiff.diff(mainDoc, conflictDoc);
               //Generate String Diff
-              const stringJsonDiff = jsondiff.diffString(main_doc, conflict_doc);
+              const stringJsonDiff = jsondiff.diffString(mainDoc, conflictDoc);
               console.log(stringJsonDiff);
 
               //Write JSON diff between the conflict files
-              writeToFile(conflictDirectoryPath +'/diff_' +conflict_doc_id +'.json', jsonDiff);
+              writeToFile(conflictDirectoryPath +'/diff_' +conflictDocId +'.json', jsonDiff);
 
               //Write conflict doc to the drive
-              writeToFile(conflictDirectoryPath +'/'+ conflict_doc_id + '.json', conflict_doc);
+              writeToFile(conflictDirectoryPath +'/'+ conflictDocId + '.json', conflictDoc);
 
               //Write string diff between the conflict files
-              writeToFile(conflictDirectoryPath + '/string_diff_' + conflict_doc_id + '.txt', stringJsonDiff);
+              writeToFile(conflictDirectoryPath + '/string_diff_' + conflictDocId + '.txt', stringJsonDiff);
             }
             );
 
