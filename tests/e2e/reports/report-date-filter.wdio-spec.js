7312b4d3789682c2ac9ca61476108ba862dc5a8d
latin-panda <66472237+latin-panda@users.noreply.github.com>
2022-07-25 12:09:21 +0700

    Fixing e2e

--- tests/e2e/reports/report-date-filter.wdio-spec.js
+++ tests/e2e/reports/report-date-filter.wdio-spec.js
@@ -1,115 +1,64 @@
-const utils = require('../../utils');
 const moment = require('moment');
+
+const utils = require('../../utils');
 const commonElements = require('../../page-objects/common/common.wdio.page');
 const reportsTab = require('../../page-objects/reports/reports.wdio.page');
 const loginPage = require('../../page-objects/login/login.wdio.page');
+const userFactory = require('../../factories/cht/users/users');
+const placeFactory = require('../../factories/cht/contacts/place');
+const personFactory = require('../../factories/cht/contacts/person');
+const reportFactory = require('../../factories/cht/reports/generic-report');
 
 
-describe('Filters reports', () => {
-  const savedUuids = [];
+describe('Report Filter', () => {
+  const parent = placeFactory.place().build({ _id: 'dist1', type: 'district_hospital' });
+  const user = userFactory.build({ username: 'john', roles: [ 'chw' ] });
+  const patient = personFactory.build({ parent: { _id: user.place._id, parent: { _id: parent._id } } });
+  let savedReports;
   const reports = [
     // one registration half an hour before the start date
-    {
-      fields: {
-        lmp_date: 'Feb 3, 2016'
-      },
-      form: 'P',
-      type: 'data_record',
-      content_type: 'xml',
-      reported_date: moment([2016, 4, 15, 23, 30]).valueOf(), // month is 0 based in this context
-      contact: {
-        name: 'Sharon',
-        phone: '+555',
-        type: 'person',
-        _id: '3305E3D0-2970-7B0E-AB97-C3239CD22D32',
-        _rev: '1-fb7fbda241dbf6c2239485c655818a69'
-      },
-      from: '+555',
-      hidden_fields: []
-    },
+    reportFactory.build(
+      { form: 'P', reported_date: moment([2016, 4, 15, 23, 30]).valueOf() },
+      { patient, submitter: user.contact, fields: { lmp_date: 'Feb 3, 2016' },
+      }),
     // one registration half an hour after the start date
-    {
-      fields: {
-        lmp_date: 'Feb 15, 2016'
-      },
-      form: 'P',
-      type: 'data_record',
-      content_type: 'xml',
-      reported_date: moment([2016, 4, 16, 0, 30]).valueOf(), // month is 0 based in this context
-      contact: {
-        name: 'Sharon',
-        phone: '+555',
-        type: 'person',
-        _id: '3305E3D0-2970-7B0E-AB97-C3239CD22D32',
-        _rev: '1-fb7fbda241dbf6c2239485c655818a69'
-      },
-      from: '+555',
-      hidden_fields: []
-    },
+    reportFactory.build(
+      { form: 'P', reported_date: moment([2016, 4, 16, 0, 30]).valueOf() },
+      { patient, submitter: user.contact, fields: { lmp_date: 'Feb 15, 2016' },
+      }),
     // one visit half an hour after the end date
-    {
-      fields: {
-        ok: 'Yes!'
-      },
-      form: 'V',
-      type: 'data_record',
-      content_type: 'xml',
-      reported_date: moment([2016, 4, 18, 0, 30]).valueOf(), // month is 0 based in this context
-      contact: {
-        name: 'Sharon',
-        phone: '+555',
-        type: 'person',
-        _id: '3305E3D0-2970-7B0E-AB97-C3239CD22D32',
-        _rev: '1-fb7fbda241dbf6c2239485c655818a69'
-      },
-      from: '+555',
-      hidden_fields: []
-    },
+    reportFactory.build(
+      { form: 'V', reported_date: moment([2016, 4, 18, 0, 30]).valueOf() },
+      { patient, submitter: user.contact, fields: { ok: 'Yes!' },
+      }),
     // one visit half an hour before the end date
-    {
-      fields: {
-        ok: 'Yes!'
-      },
-      form: 'V',
-      type: 'data_record',
-      content_type: 'xml',
-      reported_date: moment([2016, 4, 17, 23, 30]).valueOf(), // month is 0 based in this context
-      contact: {
-        name: 'Sharon',
-        phone: '+555',
-        type: 'person',
-        _id: '3305E3D0-2970-7B0E-AB97-C3239CD22D32',
-        _rev: '1-fb7fbda241dbf6c2239485c655818a69'
-      },
-      from: '+555',
-      hidden_fields: []
-    },
+    reportFactory.build(
+      { form: 'V', reported_date: moment([2016, 4, 17, 23, 30]).valueOf() },
+      { patient, submitter: user.contact, fields: { ok: 'Yes!' },
+      }),
   ];
-  const updatePermission = async () => {
-    const settings = await utils.getSettings();
-    settings.permissions.can_view_sidebar_filter = [];
-    await utils.revertSettings(true);
-    await utils.updateSettings({ permissions: settings.permissions }, true);
-  };
 
   beforeEach(async () => {
-    await updatePermission();
-    await loginPage.cookieLogin();
-    const results = await utils.saveDocs(reports);
-    results.forEach(result => savedUuids.push(result.id));
+    const results = await utils.saveDocs([ parent, patient, ...reports ]);
+    results.splice(0, 2); // Removing docs that aren't reports
+    savedReports = results.map(result => result.id);
+
+    await utils.createUsers([ user ]);
+    await loginPage.login(user);
+    await commonElements.goToReports();
   });
 
   it('should filter by date', async () => {
-    await commonElements.goToReports();
     await (await reportsTab.firstReport()).waitForDisplayed();
 
     await reportsTab.filterByDate(moment('05/16/2016', 'MM/DD/YYYY'), moment('05/17/2016', 'MM/DD/YYYY'));
-    await (await reportsTab.firstReport()).waitForDisplayed();
+    // Wait to finish filtering
+    await browser.pause(300);
+    const allReports = await reportsTab.allReports();
 
-    expect(await (await reportsTab.allReports()).length).to.equal(2);
-    expect(await (await reportsTab.reportsByUUID(savedUuids[1])).length).to.equal(1);
-    expect(await (await reportsTab.reportsByUUID(savedUuids[3])).length).to.equal(1);
-
+    expect(allReports.length).to.equal(2);
+    expect(await (await reportsTab.reportsByUUID(savedReports[1])).length).to.equal(1);
+    expect(await (await reportsTab.reportsByUUID(savedReports[3])).length).to.equal(1);
   });
 });
 
