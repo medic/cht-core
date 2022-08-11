88013cecdda3548364a8884f2c5d1ef093578d6e
Henok Getachew Alemayehu <henygetachew@gmail.com>
2022-08-11 19:54:50 +0300

    Default to letsencrypt because acme.sh defaults to zerossl

--- nginx/ssl-install.sh
+++ nginx/ssl-install.sh
@@ -48,7 +48,6 @@ create_self_signed_ssl_certificate()
   mkdir -p /etc/nginx/private
   set_environment_variables_if_not_set
   openssl req -x509 -nodes -newkey rsa:4096 \
-    -trustout \
     -keyout $SSL_KEY_FILE_PATH -out $SSL_CERT_FILE_PATH -days 365 \
     -subj "/emailAddress=$EMAIL/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANISATION/OU=$DEPARTMENT/CN=$COMMON_NAME"
 
@@ -85,7 +84,7 @@ generate_self_signed_cert(){
         # add acme.sh to path
         export PATH="/root/.acme.sh/:$PATH"
         if [ ! -f /root/.acme.sh/${COMMON_NAME}/fullchain.cer ]; then
-            acme.sh --issue -d ${COMMON_NAME} --standalone
+            acme.sh --issue -d ${COMMON_NAME} --standalone --server letsencrypt
         fi
         acme.sh --install-cert -d ${COMMON_NAME} \
             --key-file $SSL_KEY_FILE_PATH \
@@ -107,7 +106,7 @@ ensure_own_cert_exits(){
 
 select_ssl_certificate_mode(){
   # Decide certificate mode and launch nginx
-
+echo "CERTIFICATE MODE = $CERTIFICATE_MODE"
 case $CERTIFICATE_MODE in
 
   OWN_CERT)
