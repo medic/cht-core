98950d3abdf347923e07df9e7c8e8e90994d0daa
Hareet <hareet.sandhu@gmail.com>
2022-08-17 16:21:24 -0600

    HAProxy healthcheck agent service added

---
+++ haproxy/healtcheck/check-entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install requests
+python3 /app/check.py
---
+++ haproxy/healtcheck/check.py
@@ -0,0 +1,77 @@
+import os
+import socket, requests
+
+from _thread import *
+import threading
+
+print_lock = threading.Lock()
+
+couchdb_servers = os.environ['COUCHDB_SERVERS'].split(',')
+# Example: COUCHDB_SERVERS="couchdb.1,couchdb.2,couchdb.3"
+username = os.environ['COUCHDB_USER']
+password = os.environ['COUCHDB_PASSWORD']
+
+def get_membership_endpoint(couchdb_url):
+    return f'http://{username}:{password}@{couchdb_url}:5984/_membership'
+
+def close_connection(connection):
+    print('Closing connection ...')
+    connection.close()
+
+def send_down_response(conn):
+    print('Sending response: Down (CouchDB cluster not okay) ...')
+    conn.send(b'down\n')
+    close_connection(conn)
+
+def send_up_response(conn):
+    print('Sending response: Up (Couchdb cluster okay) ...')
+    conn.send(b'up\n')
+    close_connection(conn)
+
+def threaded(conn):
+    try:
+        result = []
+        couchdb_server = couchdb_servers[0]
+        try:
+            r = requests.get(get_membership_endpoint(couchdb_server), timeout=1)
+        except Exception as e:
+            print("Exception: ", e)
+        data = r.json()
+
+        all_nodes = data['all_nodes']
+        cluster_nodes = data['cluster_nodes']
+
+        if all_nodes == cluster_nodes and all_nodes is not None:
+            print('Everything is fine')
+            result.append(b'up\n')
+            print(result)
+        else:
+            print('_membership shows not all nodes are part of Cluster')
+            print(f'Details: all_nodes: {all_nodes}.  cluster_nodes: {cluster_nodes}')
+            result.append(b'down\n')
+            print(result)
+    except Exception as e:
+        result.append(b'down\n')
+        print("Exception: ", e)
+    finally:
+        if b'down\n' in result:
+            send_down_response(conn)
+        else:
+            send_up_response(conn)
+
+    print_lock.release()
+    print('Client disconnected')
+
+def Main():
+    serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serv.bind(('0.0.0.0', 5555))
+    serv.listen(5)
+    while True:
+        conn, addr = serv.accept()
+        print_lock.acquire()
+        print(f'Connected to:{addr[0]}:{addr[1]}')
+
+        start_new_thread(threaded, (conn,))
+
+if __name__ == '__main__':
+    Main()
