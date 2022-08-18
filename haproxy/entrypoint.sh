0f3b6ad5e6356d23e04b2875ca8b316fb2377573
Hareet <hareet.sandhu@gmail.com>
2022-08-18 07:51:09 -0600

    Now adding couchdb list back to haproxy entrypoint

--- haproxy/entrypoint.sh
+++ haproxy/entrypoint.sh
@@ -5,14 +5,12 @@ set -e
 service rsyslog start
 
 # Place environment variables into config
-#CONFIG="/usr/local/etc/haproxy/haproxy.cfg"
-#for COUCHDB_SERVER in ${COUCHDB_SERVERS//,/ }
-#do
-  #printf "  server $COUCHDB_SERVER $COUCHDB_SERVER:5984 check agent-check agent-inter 5s agent-addr healthcheck agent-port 5555\n" >> $CONFIG
-#done
-#envsubst < $CONFIG
-
-envsubst < /usr/local/etc/haproxy/haproxy.cfg
+CONFIG="/usr/local/etc/haproxy/haproxy.cfg"
+for COUCHDB_SERVER in ${COUCHDB_SERVERS//,/ }
+do
+  printf "  server $COUCHDB_SERVER $COUCHDB_SERVER:5984 check agent-check agent-inter 5s agent-addr healthcheck agent-port 5555\n" >> $CONFIG
+done
+envsubst < $CONFIG
 
 #Write pw for healthcheck subshell to work
 mkdir -p /srv/storage/haproxy/passwd
