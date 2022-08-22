0028cdc19d8d635e2314b4afc836a1f22787bf86
Diana Barsan <twisteddiana@gmail.com>
2022-08-22 09:54:53 +0300

    Update ongoing sync script to skip replication entirely

--- tests/scalability/ongoing/run_ongoing.sh
+++ tests/scalability/ongoing/run_ongoing.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 set -e
 
-if [[ ! $1 = 'local' || -z $1 ]]
+THREADS=100
+if [ -z $SKIP ]
 then
-  RUN_LOCAL=false
-else
-  RUN_LOCAL=true
+    SKIP=0
 fi
-
-THREADS=30
-BATCHES=3
-THREADS_IN_BATCH=$(($THREADS/$BATCHES))
-SKIP=0
 
 if [ $RUN_LOCAL = false ]
 then
@@ -35,24 +29,9 @@ then
     exit 1
 fi
 
-mkdir -p $DATA_DIR
-mkdir -p $DATA_DIR/dbs
-
 echo "npm install for jmeter suite"
 npm ci
 
-node ./generate-data.js $INSTANCE_URL $DATA_DIR
-for BATCH in $(seq 0 $(($BATCHES-1)))
-do
-  for THREAD in $(seq 1 $THREADS_IN_BATCH)
-  do
-    CURRENT_THREAD=$(($THREADS_IN_BATCH*$BATCH+$THREAD))
-    echo "Initial replication ${CURRENT_THREAD} of $(($THREADS))"
-    node ./initial-replication.js $INSTANCE_URL $DATA_DIR $CURRENT_THREAD &
-  done
-  wait
-done
-#
 echo "jmeter install"
 wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.4.3.tgz -O $DATA_DIR/apache-jmeter.tgz
 mkdir $DATA_DIR/jmeter && tar -xf $DATA_DIR/apache-jmeter.tgz -C $DATA_DIR/jmeter --strip-components=1
@@ -66,15 +45,3 @@ echo $(which node)
 $DATA_DIR/jmeter/bin/jmeter -n  -t ./ongoing_sync.jmx -Jworking_dir=./ -Jnode_binary=$(which node) -Jdata_dir=$DATA_DIR -Jinstance_url=$INSTANCE_URL -Jskip=SKIP -Jnumber_of_threads=$THREADS -l ./report/cli_run.jtl -e -o ./report
 mv ./jmeter.log ./report/jmeter.log
 echo "FINISHED! "
-
-if [ $RUN_LOCAL = false ]
-then
-  echo "Installing AWS CLI"
-  sudo apt-get install unzip -y
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-  unzip awscliv2.zip
-  sudo ./aws/install
-  echo "Uploading logs and screenshots to ${S3_PATH}..."
-  /usr/local/bin/aws s3 cp ./report "$S3_PATH" --recursive
-  echo "FINISHED! "
-fi
