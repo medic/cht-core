7911aa4f3055fd112932541d43eed55b55126b52
Henok Getachew Alemayehu <henygetachew@gmail.com>
2022-08-31 11:51:38 +0300

    Add nginx Docker image

--- scripts/build/index.js
+++ scripts/build/index.js
@@ -121,6 +121,7 @@ const saveDockerComposeFiles = () => {
     tag: versions.getImageTag(undefined, undefined, true),
     network: 'cht-net',
     couch_container_name: 'cht-couchdb',
+    nginx_container_name: 'cht-nginx',
     haproxy_container_name: 'cht-haproxy',
     api_container_name: 'cht-api',
     sentinel_container_name: 'cht-sentinel',
