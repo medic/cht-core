eecd23f4723cb7446d666590ffc555e46aa458b0
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2022-09-02 10:00:59 +1200

    more blocking

--- tests/utils.js
+++ tests/utils.js
@@ -33,9 +33,9 @@ const CONTAINER_NAMES = {
 const PouchDB = require('pouchdb-core');
 PouchDB.plugin(require('pouchdb-adapter-http'));
 PouchDB.plugin(require('pouchdb-mapreduce'));
-const db = new PouchDB(`http://${constants.API_HOST}/${constants.DB_NAME}`, { auth });
-const sentinel = new PouchDB(`http://${constants.API_HOST}/${constants.DB_NAME}-sentinel`, { auth });
-const medicLogs = new PouchDB(`http://${constants.API_HOST}/${constants.DB_NAME}-logs`, { auth });
+const db = new PouchDB(`https://${constants.API_HOST}/${constants.DB_NAME}`, { auth });
+const sentinel = new PouchDB(`https://${constants.API_HOST}/${constants.DB_NAME}-sentinel`, { auth });
+const medicLogs = new PouchDB(`https://${constants.API_HOST}/${constants.DB_NAME}-logs`, { auth });
 let browserLogStream;
 const userSettings = require('./factories/cht/users/user-settings');
 const buildVersions = require('../scripts/build/versions');
