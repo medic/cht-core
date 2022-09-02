27fbf4b630d0df855d71dd12fff634287c9142b1
latin-panda <66472237+latin-panda@users.noreply.github.com>
2022-09-02 14:22:39 +0700

    adds unit test

--- webapp/tests/karma/ts/components/search-bar/search-bar.component.spec.ts
+++ webapp/tests/karma/ts/components/search-bar/search-bar.component.spec.ts
@@ -7,15 +7,19 @@ import { provideMockStore } from '@ngrx/store/testing';
 import { SearchBarComponent } from '@mm-components/search-bar/search-bar.component';
 import { FreetextFilterComponent } from '@mm-components/filters/freetext-filter/freetext-filter.component';
 import { Selectors } from '@mm-selectors/index';
+import { ResponsiveService } from '@mm-services/responsive.service';
 
 describe('Search Bar Component', () => {
   let component: SearchBarComponent;
   let fixture: ComponentFixture<SearchBarComponent>;
+  let responsiveService;
 
   beforeEach(waitForAsync(() => {
     const mockedSelectors = [
       { selector: Selectors.getSidebarFilter, value: { filterCount: { total: 5 } } },
     ];
+
+    responsiveService = { isMobile: sinon.stub() };
 
     return TestBed
       .configureTestingModule({
@@ -28,6 +32,7 @@ describe('Search Bar Component', () => {
         ],
         providers: [
           provideMockStore({ selectors: mockedSelectors }),
+          { provide: ResponsiveService, useValue: responsiveService },
         ]
       })
       .compileComponents()
@@ -58,6 +63,8 @@ describe('Search Bar Component', () => {
     component.clear();
 
     expect(freeTextClearSpy.calledOnce).to.be.true;
+    expect(freeTextClearSpy.args[0]).to.deep.equal([ true ]);
+    expect(component.openSearch).to.be.false;
   });
 
   it('should do nothing if component is disabled', () => {
@@ -67,5 +74,20 @@ describe('Search Bar Component', () => {
     component.clear();
 
     expect(freeTextClearSpy.notCalled).to.be.true;
+  });
+
+  it('should toggle search', () => {
+    responsiveService.isMobile.returns(true);
+    component.toggleMobileSearch();
+    expect(component.openSearch).to.be.true;
+    component.toggleMobileSearch();
+    expect(component.openSearch).to.be.false;
+
+    responsiveService.isMobile.returns(false);
+    component.toggleMobileSearch();
+    expect(component.openSearch).to.be.false;
+
+    component.toggleMobileSearch(true);
+    expect(component.openSearch).to.be.true;
   });
 });
