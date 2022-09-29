9f59236ced10b2c553fa95715b352a795ca7cb8d
Joshua Kuestersteffen <jkuester@kuester7.com>
2022-09-28 22:05:02 -0500

    Move utils.spec to shared libs

--- shared-libs/contacts/test/unit/libs/utils.spec.js
+++ shared-libs/contacts/test/unit/libs/utils.spec.js
@@ -1,5 +1,5 @@
 const chai = require('chai');
-const utils = require('../../../src/controllers/utils');
+const utils = require('../../../../../api/src/controllers/utils');
 const sinon = require('sinon');
 
 let clock;
