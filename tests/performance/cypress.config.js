e9021fe1e4ed7f6caba4a9c6bea8183ac37dfd27
ngaruko <bede.ngaruko@gmail.com>
2022-09-30 12:11:48 +1300

    add prep services before run

--- tests/performance/cypress.config.js
+++ tests/performance/cypress.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("cypress");
 const { lighthouse, pa11y, prepareAudit } = require("cypress-audit");
+const utils = require('../utils')
 require('dotenv').config();
 module.exports = defineConfig({
 
@@ -8,32 +9,35 @@ module.exports = defineConfig({
     password: process.env.CY_PASSWORD,
     INITIAL_REPLICATION: 120000,
     LOAD_MESSAGES: 40000,
-    LOAD_TASKS:40000,
-    LOAD_CONTACTS:40000,
-    LOAD_REPORTS:40000
+    LOAD_TASKS: 40000,
+    LOAD_CONTACTS: 40000,
+    LOAD_REPORTS: 40000
   },
 
   e2e: {
-    baseUrl:process.env.BASE_URL || 'https://gamma-cht.dev.medicmobile.org',
+    baseUrl: process.env.BASE_URL || 'https://gamma-cht.dev.medicmobile.org',
     specPattern: 'cypress/**/*.cy.{js,jsx,ts,tsx}',
     chromeWebSecurity: false,
     defaultCommandTimeout: 300000,
     setupNodeEvents(on, config) {
+      on('before:run', (details) => {
+        utils.prepServices();
+      })
+      on('after:run', (results) => {
+        //utils.revertDb([/^form:/], true);
+      })
       // implement node event listeners here
-        on('before:browser:launch', (browser = {}, launchOptions) => {
-          prepareAudit(launchOptions)
-        })
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        prepareAudit(launchOptions)
+      })
 
-        on('task', {
-          lighthouse: lighthouse(), // calling the function is important
-        })
-        on('task', {
-          log(message){
-        console.log(message);
+      on('task', {
+        lighthouse: lighthouse(), // calling the function is important
+        log(message) {
+          console.log(message);
           return null;
-          }
-        })
-
+        },
+      })
     },
   }
 });
