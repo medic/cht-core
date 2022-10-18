57a2f0a8d01dd6c36cf63d8497720abaa6dd12fb
latin-panda <66472237+latin-panda@users.noreply.github.com>
2022-10-18 23:31:19 +0700

    Fixing flaky e2e

--- tests/page-objects/default/enketo/generic-form.wdio.page.js
+++ tests/page-objects/default/enketo/generic-form.wdio.page.js
@@ -1,4 +1,5 @@
 const utils = require('../../../utils');
+const commonPage = require('../common/common.wdio.page');
 const reportsPage = require('../reports/reports.wdio.page');
 const submitButton = () => $('.enketo .submit');
 const nextButton = () => $('button.btn.btn-primary.next-page');
@@ -14,24 +15,32 @@ const nextPage = async (numberOfPages = 1) => {
 
 const fieldByName = (formId, name) => $(`#report-form [name="/${formId}/${name}"]`);
 
-const openReportReviewMenu = async () => {
-  const reviewButton = await $('.actions>.mm-icon-inverse>.fa-check');
-  await reviewButton.click();
+const toggleReportReviewMenu = async () => {
+  const reviewButton = $('.actions>.mm-icon-inverse>.fa-check');
+  await (await reviewButton).waitForDisplayed();
+  await (await reviewButton).click();
 };
 
-
 const invalidateReport = async () => {
+  await toggleReportReviewMenu();
   const reportInvalidBtn = await $('.actions .sub-actions .verify-error');
-  await reportInvalidBtn.click();
+  await reportInvalidBtn.click(); // It closes the Review Menu
+  await commonPage.waitForPageLoaded();
+  await toggleReportReviewMenu();
   const reportInvalidMessage = await $('.actions .sub-actions .verify-error.active');
   expect(await reportInvalidMessage.getText()).to.equal('Has errors');
+  await toggleReportReviewMenu();
 };
 
 const validateReport = async () => {
+  await toggleReportReviewMenu();
   const reportValidBtn = await $('.actions .sub-actions .verify-valid');
-  await reportValidBtn.click();
+  await reportValidBtn.click(); // It closes the Review Menu
+  await commonPage.waitForPageLoaded();
+  await toggleReportReviewMenu();
   const reportValidMessage = await $('.actions .sub-actions .verify-valid.active');
   expect(await reportValidMessage.getText()).to.equal('Correct');
+  await toggleReportReviewMenu();
 };
 
 const selectContact = async (inputName, contactName) => {
@@ -56,9 +65,7 @@ const verifyReport = async () => {
   const initialReport = await utils.getDoc(reportId);
   expect(initialReport.verified).to.be.undefined;
 
-  await openReportReviewMenu();
   await invalidateReport();
-
   const invalidatedReport = await utils.getDoc(reportId);
   expect(invalidatedReport.verified).to.be.false;
   expect(invalidatedReport.patient).to.be.undefined;
@@ -74,7 +81,6 @@ const submitForm = () => submitButton().click();
 module.exports = {
   submitButton,
   nextPage,
-  openReportReviewMenu,
   invalidateReport,
   validateReport,
   nameField,
