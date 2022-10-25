8098a40bf5de49115265bca02a31561f541c1a78
Joshua Kuestersteffen <jkuester@kuester7.com>
2022-10-25 17:34:55 -0500

    Fix xsl path

--- api/src/xsl/xsl-paths.js
+++ api/src/xsl/xsl-paths.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
   FORM_STYLESHEET: path.join(__dirname, './openrosa2html5form.xsl'),
-  MODEL_STYLESHEET: path.join(__dirname, '../../node_modules/enketo-transformer/src/xsl/openrosa2xmlmodel.xsl')
+  MODEL_STYLESHEET: path.join(__dirname, '../enketo-transformer/xsl/openrosa2xmlmodel.xsl')
 };
