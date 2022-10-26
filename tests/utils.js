ba589045f00bdd9af39f7cfc002e3317e70ce037
Tatiana Lépiz <94494491+tatilepizs@users.noreply.github.com>
2022-10-26 08:59:56 -0600

    Update tests/utils.js
    Co-authored-by: Jennifer Q <66472237+latin-panda@users.noreply.github.com>

--- tests/utils.js
+++ tests/utils.js
@@ -309,7 +309,7 @@ const setUserContactDoc = (attempt=0) => {
  * @param {boolean} ignoreRefresh 
  */
 const revertDb = async (except, ignoreRefresh) => {
-  if (!except || except.length === 0) {
+  if (!except || !except.length) {
     console.warn('Utils :: revertDb() :: The 'except' parameter is empty, all documents from the database will be deleted, including Enketo forms from the config, this might cause some automated tests to fail.');
       'including all the enketo forms, any test that will need to test a form will fail after this call.');
   }
