26bd063762c0cb36d60697f3d18cba1fe4039786
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2022-10-27 11:27:29 -0300

    adding all constants inside the describe function

--- tests/e2e/standard/contacts/create-places-contacts.wdio-spec.js
+++ tests/e2e/standard/contacts/create-places-contacts.wdio-spec.js
@@ -2,10 +2,9 @@ const loginPage = require('../../../page-objects/default/login/login.wdio.page')
 const commonPage = require('../../../page-objects/default/common/common.wdio.page');
 const contactPage = require('../../../page-objects/standard/contacts/contacts.wdio.page');
 
-const PLACE_TYPES = { CLINIC: 'clinic', HEALTH_CENTER: 'health_center', DISTRICT: 'district_hospital' };
-const CONTACT_NAME_SUFFIX = ' Contact';
-
 describe('Creating contacts with standard config. ', () => {
+  const PLACE_TYPES = { CLINIC: 'clinic', HEALTH_CENTER: 'health_center', DISTRICT: 'district_hospital' };
+  const CONTACT_NAME_SUFFIX = ' Contact';
   const districtName = 'District test';
   const healthCenterName = 'Health Center test';
   const clinicName = 'Clinic test';
