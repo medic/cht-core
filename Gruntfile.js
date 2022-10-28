de09a1f3beb9502194e90d452eca7e654d997965
Bede Ngaruko <ngarukob@yahoo.com>
2022-10-28 04:14:04 +0000

    skip build

--- Gruntfile.js
+++ Gruntfile.js
@@ -448,7 +448,7 @@ module.exports = function(grunt) {
       },
       'wdio-run-performance': {
         cmd: [
-          'npm run perform'
+          'npm run wdio --spec ./tests/performance/timings/*.perf.js'
         ].join(' && '),
         stdio: 'inherit', // enable colors!
       },
