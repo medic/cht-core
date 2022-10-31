e6520aa95f0835621381636b1062cea66d7bb112
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2022-10-31 16:43:18 -0300

    Execute test as an offline user. Add a scenario to edit the name of the contact. Update the state of the migration

--- tests/e2e/default/contacts/edit.wdio-spec.js
+++ tests/e2e/default/contacts/edit.wdio-spec.js
@@ -2,26 +2,47 @@ const loginPage = require('../../../page-objects/default/login/login.wdio.page')
 const commonPage = require('../../../page-objects/default/common/common.wdio.page');
 const placeFactory = require('../../../factories/cht/contacts/place');
 const userFactory = require('../../../factories/cht/users/users');
+const personFactory = require('../../../factories/cht/contacts/person');
 const contactPage = require('../../../page-objects/default/contacts/contacts.wdio.page');
 const utils = require('../../../utils');
 
-
-describe('Editing contacts with the default config. ', () => {
+describe('Edit contacts with the default config. ', () => {
+  const CONTACT_NAME = 'Maria Gomez';
+  const CONTACT_UPDATED_NAME = 'Ana Paula Gonzalez';
   const places = placeFactory.generateHierarchy();
+  const districtHospital = places.find(place => place.type === 'district_hospital');
+  const districtHospitalPrimaryContact = userFactory.build({ place: districtHospital._id });
   const healthCenter = places.find(place => place.type === 'health_center');
-  const healthCenterPrimaryContact = userFactory.build({ place: healthCenter._id });
-  const docs = [...places, healthCenterPrimaryContact];
+  const parent = { _id: healthCenter._id, parent: healthCenter.parent };
+  const healthCenterPrimaryContact = personFactory.build({ name: CONTACT_NAME, parent: parent });
+  healthCenter.contact = {
+    _id: healthCenterPrimaryContact._id,
+    name: healthCenterPrimaryContact.name,
+    phone: healthCenterPrimaryContact.phone
+  };
+  const docs = [...places, districtHospitalPrimaryContact, healthCenterPrimaryContact];
 
   before(async () => {
     await utils.saveDocs(docs);
-    await utils.createUsers([healthCenterPrimaryContact]);
-    await loginPage.cookieLogin();
+    await utils.createUsers([districtHospitalPrimaryContact]);
+    await loginPage.login(districtHospitalPrimaryContact);
     await commonPage.waitForPageLoaded();
   });
 
-  it('should remove the primary contact from the health center when the contact is deleted', async () => {
+  it('should edit contact name', async () => {
+    await commonPage.goToPeople();
+    await contactPage.selectLHSRowByText(healthCenter.name);
+    await contactPage.editPerson(CONTACT_NAME, CONTACT_UPDATED_NAME);
+    await commonPage.waitForPageLoaded();
     await commonPage.goToPeople(healthCenter._id);
-    await contactPage.deletePerson(healthCenterPrimaryContact.contact.name);
+    const primaryContactName = await contactPage.getPrimaryContactName();
+    expect(primaryContactName).to.equal(CONTACT_UPDATED_NAME);
+  });
+
+  it('should remove the primary contact from the clinic when the contact is deleted', async () => {
+    await commonPage.goToPeople();
+    await contactPage.selectLHSRowByText(healthCenter.name);
+    await contactPage.deletePerson(CONTACT_UPDATED_NAME);
     await commonPage.waitForPageLoaded();
     await contactPage.selectLHSRowByText(healthCenter.name);
     const peopleNames = await contactPage.getAllRHSPeopleNames();
