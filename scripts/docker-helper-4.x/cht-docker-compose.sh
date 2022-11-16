8d5812cfef83f301980e151ffdd4618c05c05ce0
m5r <mokht@rmi.al>
2022-11-16 12:39:19 +0100

    refactor $projectName

--- scripts/docker-helper-4.x/cht-docker-compose.sh
+++ scripts/docker-helper-4.x/cht-docker-compose.sh
@@ -2,18 +2,20 @@
 
 set -eu
 
-selectedProject=
+projectFile=
+projectName=
 
 # can pass a project .env file as argument
 if [[ -n "${1-}" ]]; then
 	if [[ -f "$1" ]]; then
-		selectedProject=$(echo "$1" | sed "s/\.\///" | sed "s/\.env//")
+		projectFile=$1
+		projectName=$(echo "$1" | sed "s/\.\///" | sed "s/\.env//")
 	else
 		echo "File $1 doesnt exist"
 	fi
 fi
 
-if [[ -z "$selectedProject" ]]; then
+if [[ -z "$projectName" ]]; then
 	projects=$(find . -name "*.env" -type f | sed "s/\.\///" | sed "s/\.env//")
 
 	if [[ -z "$projects" ]]; then
@@ -23,42 +25,44 @@ if [[ -z "$selectedProject" ]]; then
 	read -p "Would you like to initialize a new project [y/N]? " yn
 	case $yn in
 	[Yy]*)
-		projectName=
 		while [[ -z "$projectName" ]]; do
 			read -p "How do you want to name the project? " projectName
 
       projectName="${projectName//[^[:alnum:]]/_}"
       projectName=$(echo $projectName | tr '[:upper:]' '[:lower:]')
-			if test -f "./$projectName.env"; then
-				echo "./$projectName.env already exists"
+      projectFile="$projectName.env"
+			if test -f "./$projectFile"; then
+				echo "./$projectFile already exists"
 				projectName=
+				projectFile=
 			fi
 		done
 
 		homeDir="$HOME/.medic/cht-docker/$projectName-dir"
-		touch "./$projectName.env"
-		echo "NGINX_HTTP_PORT=8080" >>"./$projectName.env"
-		echo "NGINX_HTTPS_PORT=8443" >>"./$projectName.env"
-		echo "COUCHDB_USER=medic" >>"./$projectName.env"
-		echo "COUCHDB_PASSWORD=password" >>"./$projectName.env"
-		echo "CHT_COMPOSE_PROJECT_NAME=$projectName" >>"./$projectName.env"
-		echo "DOCKER_CONFIG_PATH=$homeDir" >>"./$projectName.env"
-		echo "COUCHDB_SECRET=$(openssl rand -hex 16)" >>"./$projectName.env"
-		echo "COUCHDB_UUID=$(openssl rand -hex 16)" >>"./$projectName.env"
-		echo "COUCHDB_DATA=$homeDir/couch" >>"./$projectName.env"
-		echo "CHT_COMPOSE_PATH=$homeDir" >>"./$projectName.env"
-		echo "CHT_NETWORK=$projectName-cht-net" >>"./$projectName.env"
+		touch "./$projectFile"
+		echo "NGINX_HTTP_PORT=8080" >>"./$projectFile"
+		echo "NGINX_HTTPS_PORT=8443" >>"./$projectFile"
+		echo "COUCHDB_USER=medic" >>"./$projectFile"
+		echo "COUCHDB_PASSWORD=password" >>"./$projectFile"
+		echo "CHT_COMPOSE_PROJECT_NAME=$projectName" >>"./$projectFile"
+		echo "DOCKER_CONFIG_PATH=$homeDir" >>"./$projectFile"
+		echo "COUCHDB_SECRET=$(openssl rand -hex 16)" >>"./$projectFile"
+		echo "COUCHDB_UUID=$(openssl rand -hex 16)" >>"./$projectFile"
+		echo "COUCHDB_DATA=$homeDir/couch" >>"./$projectFile"
+		echo "CHT_COMPOSE_PATH=$homeDir" >>"./$projectFile"
+		echo "CHT_NETWORK=$projectName-cht-net" >>"./$projectFile"
 		projects=$(find . -name "*.env" -type f | sed "s/\.\///" | sed "s/\.env//")
 		;;
 	esac
 
   envCount=$(ls *.env | wc -l)
   if [ "$envCount" -gt 0 ]; then
-    while [[ -z "$selectedProject" ]]; do
+    while [[ -z "$projectName" ]]; do
       echo "Which project do you want to use?"
       select project in $projects; do
-        selectedProject=$project
-        homeDir="$HOME/.medic/cht-docker/$selectedProject-dir"
+        projectName=$project
+        projectFile="$project.env"
+        homeDir="$HOME/.medic/cht-docker/$projectName-dir"
         break
       done
     done
@@ -79,15 +83,15 @@ curl -s -o "$homeDir/couchdb.yml" \
   https://staging.dev.medicmobile.org/_couch/builds_4/medic:medic:master/docker-compose/cht-couchdb.yml
 
 # shellcheck disable=SC1090
-source "./$selectedProject.env"
+source "./$projectFile"
 
 echo ""
-docker-compose --env-file "./$selectedProject.env" --file "$homeDir/upgrade-service.yml" up --detach
+docker-compose --env-file "./$projectFile" --file "$homeDir/upgrade-service.yml" up --detach
 
 set +e
-echo "Starting project \"${selectedProject}\". First run takes a while. Will try for up to five minutes..." | tr -d '\n'
-isNginxRunning=$(docker inspect --format="{{.State.Running}}" "${selectedProject}_nginx_1" 2>/dev/null)
-isUpgradeRunning=$(docker inspect --format="{{.State.Running}}" "${selectedProject}-dir-cht-upgrade-service-1" 2>/dev/null)
+echo "Starting project \"${projectName}\". First run takes a while. Will try for up to five minutes..." | tr -d '\n'
+isNginxRunning=$(docker inspect --format="{{.State.Running}}" "${projectName}_nginx_1" 2>/dev/null)
+isUpgradeRunning=$(docker inspect --format="{{.State.Running}}" "${projectName}-dir-cht-upgrade-service-1" 2>/dev/null)
 i=0
 while [[ "$isNginxRunning" != "true" ]]; do
   if [[ $i -gt 300 ]]; then
@@ -108,21 +112,21 @@ while [[ "$isNginxRunning" != "true" ]]; do
   echo '.' | tr -d '\n'
   ((i++))
 	sleep 1
-	isNginxRunning=$(docker inspect --format="{{.State.Running}}" "${selectedProject}_nginx_1" 2>/dev/null)
-  isUpgradeRunning=$(docker inspect --format="{{.State.Running}}" "${selectedProject}-dir-cht-upgrade-service-1" 2>/dev/null)
+	isNginxRunning=$(docker inspect --format="{{.State.Running}}" "${projectName}_nginx_1" 2>/dev/null)
+  isUpgradeRunning=$(docker inspect --format="{{.State.Running}}" "${projectName}-dir-cht-upgrade-service-1" 2>/dev/null)
 done
 
-docker exec -it "${selectedProject}_nginx_1" bash -c "curl -s -o server.pem http://local-ip.co/cert/server.pem"
-docker exec -it "${selectedProject}_nginx_1" bash -c "curl -s -o chain.pem http://local-ip.co/cert/chain.pem"
-docker exec -it "${selectedProject}_nginx_1" bash -c "cat server.pem chain.pem > /etc/nginx/private/cert.pem"
-docker exec -it "${selectedProject}_nginx_1" bash -c "curl -s -o /etc/nginx/private/key.pem http://local-ip.co/cert/server.key"
-docker restart "${selectedProject}_nginx_1" 1>/dev/null
+docker exec -it "${projectName}_nginx_1" bash -c "curl -s -o server.pem http://local-ip.co/cert/server.pem"
+docker exec -it "${projectName}_nginx_1" bash -c "curl -s -o chain.pem http://local-ip.co/cert/chain.pem"
+docker exec -it "${projectName}_nginx_1" bash -c "cat server.pem chain.pem > /etc/nginx/private/cert.pem"
+docker exec -it "${projectName}_nginx_1" bash -c "curl -s -o /etc/nginx/private/key.pem http://local-ip.co/cert/server.key"
+docker restart "${projectName}_nginx_1" 1>/dev/null
 
 echo ""
 echo ""
 echo " -------------------------------------------------------- "
 echo ""
-echo "  Success! \"${selectedProject}\" is set up:"
+echo "  Success! \"${projectName}\" is set up:"
 echo ""
 echo "    https://127-0-0-1.my.local-ip.co:${NGINX_HTTPS_PORT}/ (CHT)"
 echo "    https://127-0-0-1.my.local-ip.co:${NGINX_HTTPS_PORT}/_utils/ (Fauxton)"
