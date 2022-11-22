809e486fe4c038446700914e966fba663ee50052
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2022-11-22 15:07:50 +1300

    add e2e test

---
+++ tests/e2e/default/enketo/submit-countdown-timer-form.wdio-spec.js
@@ -0,0 +1,32 @@
+const countdownTimerPage = require('../../../page-objects/default/enketo/countdown-timer.wdio.page');
+const common = require('../../../page-objects/default/common/common.wdio.page');
+const reportsPage = require('../../../page-objects/default/reports/reports.wdio.page');
+const utils = require('../../../utils');
+const userData = require('../../../page-objects/default/users/user.data');
+const loginPage = require('../../../page-objects/default/login/login.wdio.page');
+
+const { userContactDoc, docs } = userData;
+
+describe('Submit Photo Upload form', () => {
+  
+  before(async () => {
+    await utils.saveDocs(docs);
+    await countdownTimerPage.configureForm(userContactDoc);
+    await loginPage.cookieLogin();
+    await common.hideSnackbar();
+  });
+
+  it('submit and edit (no changes)', async () => {
+    await common.goToReports();
+
+    await reportsPage.openForm(countdownTimerPage.TITLE);
+    await countdownTimerPage.clickTimer(); // start
+    await countdownTimerPage.clickTimer(); // stop
+    await reportsPage.submitForm();
+
+    const reportId = await reportsPage.getCurrentReportId();
+    const report = await utils.getDoc(reportId);
+    expect(report.fields.group).to.deep.equal({ timer: '' }); // there is no output from the widget
+  });
+
+});
---
+++ tests/page-objects/default/enketo/countdown-timer.wdio.page.js
@@ -0,0 +1,64 @@
+const utils = require('../../../utils');
+
+const xml = `<?xml version="1.0"?>
+<h:html xmlns="http://www.w3.org/2002/xforms" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:h="http://www.w3.org/1999/xhtml" xmlns:jr="http://openrosa.org/javarosa" xmlns:orx="http://openrosa.org/xforms" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <h:head>
+    <h:title>Countdown timer</h:title>
+    <model>
+      <instance>
+        <countdown delimiter="#" id="countdown" prefix="J1!countdown!" version="2018-03-08 17-42">
+          <group>
+            <timer/>
+          </group>
+          <meta>
+            <instanceID/>
+          </meta>
+        </countdown>
+      </instance>
+      <instance id="contact-summary"/>
+      <bind nodeset="/countdown/group/timer" readonly="true()" type="string"/>
+      <bind calculate="concat('uuid:', uuid())" nodeset="/countdown/meta/instanceID" readonly="true()" type="string"/>
+    </model>
+  </h:head>
+  <h:body class="pages">
+    <group appearance="field-list" ref="/countdown/group">
+      <label>Testing Timer</label>
+      <input appearance="countdown-timer" ref="/countdown/group/timer">
+        <label>Text for timer</label>
+      </input>
+    </group>
+  </h:body>
+</h:html>
+`;
+
+const TITLE = 'Countdown Timer';
+
+const docs = [
+  {
+    _id: 'form:countdown-timer',
+    internalId: 'countdown-timer',
+    title: TITLE,
+    type: 'form',
+    _attachments: {
+      xml: {
+        content_type: 'application/octet-stream',
+        data: Buffer.from(xml).toString('base64')
+      }
+    }
+  }
+];
+
+const configureForm = (userContactDoc) => {
+  return utils.seedTestData(userContactDoc, docs);
+};
+
+const clickTimer = async () => {
+  const timer = await $('form[data-form-id="countdown"] .or-appearance-countdown-timer canvas');
+  await timer.click();
+};
+
+module.exports = {
+  TITLE,
+  configureForm,
+  clickTimer,
+};
