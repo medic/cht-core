a5cfd821b6d8a25c8a20c343871709c7eac2fa38
mrjones-plip <mrjones-plip@plip.com>
2022-12-13 21:49:08 -0800

    full paths, quiet apt

--- scripts/cht-3x-node-upgrade/upgrade.sh
+++ scripts/cht-3x-node-upgrade/upgrade.sh
@@ -6,18 +6,23 @@
 
 echo "Start...Node version installed:"
 /srv/software/medic-core/v2.1.1/x64/bin/node -v
-apt install -y zip
+
+cd /srv
+
+apt -qq update
+apt  -qq install -y zip
 /boot/svc-stop medic-core
 /boot/svc-stop medic-api
 /boot/svc-stop medic-sentinel
 
 # swap out these lines to test on branch
 #curl -o node.12.22.12.zip https://github.com/medic/cht-core/raw/master/scripts/cht-3x-node-upgrade/node.12.22.12.zip
-curl -o node.12.22.12.zip https://github.com/medic/cht-core/raw/7957-node-upgrade-script/scripts/cht-3x-node-upgrade/node.12.22.12.zip
+curl -o /srvnode.12.22.12.zip https://github.com/medic/cht-core/raw/7957-node-upgrade-script/scripts/cht-3x-node-upgrade/node.12.22.12.zip
 
 unzip node.12.22.12.zip
 chmod +x node
-mv node /srv/software/medic-core/v2.1.1/x64/bin/node
+mv /srv/node /srv/software/medic-core/v2.1.1/x64/bin/node
+rm /srv/node.12.22.12.zip
 /boot/svc-start medic-core
 /boot/svc-start medic-api
 /boot/svc-start medic-sentinel
