8b094102d9268d396a425d48f9ef63105ec7f6f3
mrjones-plip <mrjones-plip@plip.com>
2022-12-13 21:56:39 -0800

    add statuses

--- scripts/cht-3x-node-upgrade/upgrade.sh
+++ scripts/cht-3x-node-upgrade/upgrade.sh
@@ -7,13 +7,26 @@
 echo "Start...Node version installed:"
 /srv/software/medic-core/v2.1.1/x64/bin/node -v
 
+
+echo ""
+echo "Installing zip..."
+echo ""
+
 cd /srv
 
 apt -qq update
 apt  -qq install -y zip
+echo ""
+echo "Stopping services..."
+echo ""
+
 /boot/svc-stop medic-core
 /boot/svc-stop medic-api
 /boot/svc-stop medic-sentinel
+
+echo ""
+echo "Downloading node and installing..."
+echo ""
 
 # swap out these lines to test on branch
 #curl -o node.12.22.12.zip https://raw.githubusercontent.com/medic/cht-core/master/scripts/cht-3x-node-upgrade/node.12.22.12.zip
@@ -23,8 +36,16 @@ unzip /srv/node.12.22.12.zip
 chmod +x node
 mv /srv/node /srv/software/medic-core/v2.1.1/x64/bin/node
 rm /srv/node.12.22.12.zip
+
+
+echo ""
+echo "Starting services backup..."
+echo ""
 /boot/svc-start medic-core
 /boot/svc-start medic-api
 /boot/svc-start medic-sentinel
+
+echo ""
 echo "Upgrade complete!  Node version installed:"
+echo ""
 /srv/software/medic-core/v2.1.1/x64/bin/node -v
