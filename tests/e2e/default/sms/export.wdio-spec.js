9bd82a0ae5b4032bbd65e386c2a8642207903b6a
latin-panda <66472237+latin-panda@users.noreply.github.com>
2022-12-14 15:36:34 +0700

    e2e for export of messages

---
+++ tests/e2e/default/sms/export.wdio-spec.js
@@ -0,0 +1,45 @@
+const moment = require('moment');
+
+const messagesPage = require('../../../page-objects/default/sms/messages.wdio.page');
+const userFactory = require('../../../factories/cht/users/users');
+const placeFactory = require('../../../factories/cht/contacts/place');
+const personFactory = require('../../../factories/cht/contacts/person');
+const commonElements = require('../../../page-objects/default/common/common.wdio.page');
+const loginPage = require('../../../page-objects/default/login/login.wdio.page');
+const fileDownloadUtils = require('../../../utils/file-download');
+const utils = require('../../../utils');
+
+describe('Export Messages', () => {
+  const places = placeFactory.generateHierarchy();
+  const healthCenter = places.get('health_center');
+  const onlineUser = userFactory.build({ place: healthCenter._id, roles: ['program_officer'] });
+  const patient = personFactory.build({
+    phone: '+12068881234',
+    parent: { _id: healthCenter._id, parent: healthCenter.parent },
+  });
+  const today = moment();
+
+  beforeEach(async () => {
+    await fileDownloadUtils.setupDownloadFolder();
+    await utils.saveDocs([ ...places.values(), patient ]);
+    await utils.createUsers([ onlineUser ]);
+    await loginPage.login(onlineUser);
+    await commonElements.waitForPageLoaded();
+    await commonElements.goToMessages();
+  });
+
+  it('Should download export file', async () => {
+    await messagesPage.sendMessageToPhone('It is working!', patient.phone);
+    await messagesPage.waitForMessagesInLHS();
+
+    await messagesPage.exportMessages();
+
+    const files = await fileDownloadUtils.waitForDownload(`messages-${today.format('YYYYMMDD')}`);
+    expect(files).to.not.be.undefined;
+    expect(files.length).to.equal(1);
+
+    const fileContent = await fileDownloadUtils.getFileContent(files[0].name);
+    expect(fileContent).to.not.be.undefined;
+    expect(fileContent.indexOf('It is working!') > -1).to.be.true;
+  });
+});
