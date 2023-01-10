1bb2ee6eda24be4bd9ffdd87c50a8386745fb1d3
Diana Barsan <barsan@medic.org>
2023-01-10 20:03:58 +0200

    display build version along base version in the admin app
    Signed-off-by: Diana Barsan <barsan@medic.org>

--- admin/src/js/filters/build-version.js
+++ admin/src/js/filters/build-version.js
@@ -4,10 +4,12 @@ angular.module('filters').filter('buildVersion',
     'use strict';
     'ngInject';
 
-    return function(buildInfo) {
+    return function(buildInfo, displayBaseVersion {
       if (buildInfo) {
         if (buildInfo.version === buildInfo.base_version || !buildInfo.base_version) {
           return buildInfo.version;
+        } else if (displayBaseVersion) {
+          return buildInfo.base_version;
         } else {
           return buildInfo.version + ' (~' + buildInfo.base_version + ')';
         }
