800e0a38674ec0cf8f5a3495ecf27672133ceb4b
jonathanbataire <jonathanbataire@gmail.com>
2023-01-25 14:04:50 +0300

    month translations clean up

--- webapp/src/js/moment-locales/lg.js
+++ webapp/src/js/moment-locales/lg.js
@@ -3,8 +3,9 @@
 */
 const moment = require('moment');
 moment.defineLocale('lg', {
-  months: 'Janwali_Febwali_Marisi_Apuli_Maayi_Juuni_Julaayi_Agusito_Sebuttemba_Okitobba_Novemba_Desemba'.split('_'),
-  monthsShort: 'Jan_Feb_Mar_Apu_Maa_Jun_Jul_Agu_Seb_Oki_Nov_Des'.split('_'),
+  months: `Ogusooka_Ogwokubiri_Ogwokusatu_Ogwokuna_Ogwokutaano_Ogwomukaaga_Ogwomusanvu_Ogwomunaana_
+    Ogwomwenda_Ogwekkumi_Ogwekkuminogumu_Ogwekkuminabiri`.split('_'),
+  monthsShort: 'Og1_Ogw2_Ogw3_Ogw4_Ogw5_Ogw6_Ogw7_Ogw8_Ogw9_Ogw10_Ogw11_Ogw12'.split('_'),
   weekdays: 'Sabbiiti_Balaza_Lwakubiri_Lwakusatu_Lwakuna_Lwakutaano_Lwamukaaga'.split('_'),
   weekdaysShort: 'Sab_Bal_Lw2_Lw3_Lw4_Lw5_Lw6'.split('_'),
   weekdaysMin: 'Sab_Bal_Lw2_Lw3_Lw4_Lw5_Lw6'.split('_'),
@@ -17,11 +18,11 @@ moment.defineLocale('lg', {
     LLLL: 'dddd, D MMMM YYYY HH:mm'
   },
   calendar: {
-    sameDay: '[Leero ku ssawa] LT',
-    nextDay: '[Enkya ku ssawa] LT',
-    nextWeek: 'dddd [ku ssawa] LT',
-    lastDay: '[Eggulo ku ssawa] LT',
-    lastWeek: 'dddd [sabbiiti ewedde ku ssawa] LT',
+    sameDay: '[Leero ku] LT',
+    nextDay: '[Enkya ku] LT',
+    nextWeek: 'dddd [ku] LT',
+    lastDay: '[Eggulo ku] LT',
+    lastWeek: 'dddd [sabbiiti ewedde ku] LT',
     sameElse: 'L'
   },
   relativeTime: {
