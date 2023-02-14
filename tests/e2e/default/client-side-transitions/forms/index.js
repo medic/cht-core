8ac02b6c4f9625e89229232d35365584bdb5a5e6
ngaruko <ngarukob@yahoo.com>
2023-02-14 20:31:20 +0000

    refactor index.js

--- tests/e2e/default/client-side-transitions/forms/index.js
+++ tests/e2e/default/client-side-transitions/forms/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const utils = require('../../../../utils');
-const helper = require('../../../../helper');
 
 const getFormDoc = (formId) => {
   const xmlPath = path.join(__dirname, `./${formId}.xml`);
@@ -71,32 +70,29 @@ module.exports.uploadForms = async () => {
 };
 
 module.exports.openForm = async (formId) => {
-  const addButton = element(by.css('.detail-actions .actions .dropdown-toggle .fa-plus'));
-  await helper.clickElementNative(addButton);
-  const form = element(by.css(`#relevant-contacts-form li[id="form:${formId}"] a`));
-  await helper.clickElementNative(form);
-  return helper.waitUntilReadyNative(element(by.id('form-title')));
+  const addButton = await $('.detail-actions .actions .dropdown-toggle .fa-plus');
+  await addButton.waitForClickable({ timeout: 10000 });
+  await addButton.click();
+  const form = await $(`#relevant-contacts-form li[id="form:${formId}"] a`);
+  await form.click();
 };
 
 module.exports.submit = async () => {
-  const submitButton = element(by.css('.btn.submit.btn-primary'));
-  await helper.clickElementNative(submitButton);
-  await helper.waitElementToBeVisibleNative(element(by.css('div.row.flex.grid'))); // contact summary loaded
+  const submitButton = await $('.btn.submit.btn-primary');
+  await submitButton.waitForClickable();
+  await submitButton.click();
 };
 
 module.exports.selectHealthCenter = async name => {
-  const select = element(by.css('.selection'));
-  await helper.waitUntilReadyNative(select);
+  const select = await $('.selection');
   await select.click();
-  const search = await element(by.css('.select2-search__field'));
+  const search = await $('.select2-search__field');
   await search.click();
-  await search.sendKeys(name);
-  await helper.waitElementToBeVisibleNative(element(by.css('.name')));
-  await element(by.css('.name')).click();
+  await search.setValue(name);
+  await (await $('.name')).click();
 };
 
 module.exports.fillPatientName = async name => {
-  const patientName = element(by.css('[name="/mute_new_clinic/new_person/name"]'));
-  await helper.waitUntilReadyNative(patientName);
-  await patientName.clear().sendKeys(name);
+  const patientName = await $('[name="/mute_new_clinic/new_person/name"]');
+  await patientName.setValue(name);
 };
