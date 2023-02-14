a3986f7a9783ed30bce8032857c530f8feffc36d
Jonathan Bataire <jonathanbataire@gmail.com>
2023-02-14 11:08:30 +0300

    bootstrap-datepicker translation

--- webapp/src/js/enketo/main.js
+++ webapp/src/js/enketo/main.js
@@ -9,6 +9,7 @@ require('bootstrap-datepicker/js/locales/bootstrap-datepicker.hi');
 require('./bootstrap-datepicker.ceb');
 require('./bootstrap-datepicker.hil');
 require('./bootstrap-datepicker.tl');
+require('./bootstrap-datepicker.lg')
 
 $.fn.datepicker.defaults.container = '.content-pane .enketo';
 $.fn.datepicker.defaults.orientation = 'bottom';
