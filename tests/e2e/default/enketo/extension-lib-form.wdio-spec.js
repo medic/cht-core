73ab2d2540d5f6d29d2fb1c3b569b81b2cc6f6d2
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2023-03-06 14:44:07 +1300

    add e2e test

---
+++ tests/e2e/default/enketo/extension-lib-form.wdio-spec.js
@@ -0,0 +1,33 @@
+const extensionLibsPage = require('../../../page-objects/default/enketo/extension-lib.wdio.page');
+const common = require('../../../page-objects/default/common/common.wdio.page');
+const reportsPage = require('../../../page-objects/default/reports/reports.wdio.page');
+const utils = require('../../../utils');
+const userData = require('../../../page-objects/default/users/user.data');
+const loginPage = require('../../../page-objects/default/login/login.wdio.page');
+
+const { userContactDoc, docs } = userData;
+
+describe('Extension lib xpath function', () => {
+  
+  before(async () => {
+    await utils.saveDocs(docs);
+    await extensionLibsPage.configure(userContactDoc);
+    await loginPage.cookieLogin();
+    await common.hideSnackbar();
+  });
+
+  it('calculate average', async () => {
+    await common.goToReports();
+
+    await reportsPage.openForm(extensionLibsPage.TITLE);
+    await extensionLibsPage.typeFirst(5);
+    await extensionLibsPage.typeSecond(8);
+    await extensionLibsPage.blur();
+
+    const average = await extensionLibsPage.getAverage();
+    expect(average).to.deep.equal('6.5');
+
+    // no need to submit - if the value is updated then we're good to go
+  });
+
+});
