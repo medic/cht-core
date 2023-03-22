76bb1b3f8183195ae5edd6219dec1fdec2a392f1
Diana Barsan <barsan@medic.org>
2023-03-22 19:41:49 +0200

    increase load timeout.

--- tests/page-objects/default/common/common.wdio.page.js
+++ tests/page-objects/default/common/common.wdio.page.js
@@ -180,7 +180,7 @@ const waitForLoaders = async () => {
 const waitForPageLoaded = async () => {
   // if we immediately check for app loaders, we might bypass the initial page load (the bootstrap loader)
   // so waiting for the main page to load.
-  await (await $('#header-dropdown-link')).waitForDisplayed();
+  await (await $('#header-dropdown-link')).waitForDisplayed({ timeout: 30000});
   // ideally we would somehow target all loaders that we expect (like LHS + RHS loaders), but not all pages
   // get all loaders.
   do {
