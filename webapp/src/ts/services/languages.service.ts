60e93950eac45fa1f9dac401889affa5f9e7f65b
m5r <mokht@rmi.al>
2023-03-29 11:59:04 +0200

    use `enabledLocales` if available in `webapp/src/ts/services/languages.services.ts`

--- webapp/src/ts/services/languages.service.ts
+++ webapp/src/ts/services/languages.service.ts
@@ -1,21 +1,34 @@
 import { Injectable } from '@angular/core';
-import { map } from 'lodash-es';
 
 import { DbService } from '@mm-services/db.service';
+import { SettingsService } from '@mm-services/settings.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguagesService {
   constructor(
-    private dbService:DbService,
+    private dbService: DbService,
+    private settingsService: SettingsService,
   ){}
 
-  get(): Promise<Object> {
-    return this.dbService.get()
-      .query('medic-client/doc_by_type', { key: [ 'translations', true ] })
-      .then((result) => {
-        return map(result.rows, 'value');
+  async get(): Promise<Object> {
+    const settings = await this.settingsService.get();
+    if (
+      settings.enabledLocales &&
+      Array.isArray(settings.enabledLocales) &&
+      settings.enabledLocales.length > 0
+    ) {
+      const result = await this.dbService.get().query('medic-client/doc_by_type', {
+        startkey: ['translations', false],
+        endkey: ['translations', true],
       });
+      return result.rows
+        .filter(row => settings.enabledLocales.includes(row.value.code))
+        .map(row => row.value);
+    }
+
+    const result = await this.dbService.get().query('medic-client/doc_by_type', { key: ['translations', true] });
+    return result.rows.map(row => row.value);
   }
 }
