4b1c2f92481994005d87561a12ff8d97747b020c
Jonathan Bataire <jonathanbataire@gmail.com>
2023-04-13 12:34:19 +0300

    targets error log

--- webapp/src/ts/modules/analytics/analytics-targets.component.ts
+++ webapp/src/ts/modules/analytics/analytics-targets.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 import { RulesEngineService } from '@mm-services/rules-engine.service';
 import { TelemetryService } from '@mm-services/telemetry.service';
+import { SessionService } from '@mm-services/session.service';
+import { Store } from '@ngrx/store';
+import { Selectors } from '@mm-selectors/index';
+import { Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './analytics-targets.component.html'
@@ -10,18 +14,40 @@ export class AnalyticsTargetsComponent implements OnInit {
   targets = [];
   loading = true;
   targetsDisabled = false;
+  error = false;
+  errorDetails;
+  url;
+  currentDate;
+  userCtx;
+  replicationStatus;
   telemetryData = {
     start: Date.now(),
     end: undefined
   };
 
+  subscription = new Subscription();
+
+  private subscribeToStore() {
+    const reduxSubscription = this.store.select(Selectors.getReplicationStatus)
+      .subscribe((replicationStatus) => {
+        this.replicationStatus = replicationStatus;
+      });
+    this.subscription.add(reduxSubscription);
+  }
+
   constructor(
     private rulesEngineService: RulesEngineService,
-    private telemetryService: TelemetryService
+    private telemetryService: TelemetryService,
+    private sessionService: SessionService,
+    private store: Store
   ) { }
 
   ngOnInit(): void {
+    this.subscribeToStore();
     this.getTargets();
+    this.url = window.location.hostname;
+    this.currentDate = Date.now();
+    this.userCtx = this.sessionService.userCtx();
   }
 
   private getTargets() {
@@ -33,6 +59,8 @@ export class AnalyticsTargetsComponent implements OnInit {
       })
       .catch(err => {
         console.error('Error getting targets', err);
+        this.errorDetails = err.stack;
+        this.error = true;
         return [];
       })
       .then((targets = []) => {
