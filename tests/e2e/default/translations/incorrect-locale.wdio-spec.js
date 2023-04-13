a9cde502638f79812651f8e70c9d971ba5a94778
m5r <mokht@rmi.al>
2023-04-13 16:15:35 +0200

    enable the new locale for the incorrect-locale.wdio-spec.js test

--- tests/e2e/default/translations/incorrect-locale.wdio-spec.js
+++ tests/e2e/default/translations/incorrect-locale.wdio-spec.js
@@ -6,16 +6,24 @@ const loginPage = require('../../../page-objects/default/login/login.wdio.page')
 const placeFactory = require('../../../factories/cht/contacts/place');
 
 describe('Testing Incorrect locale', () => {
-  const createLanguage = async () =>  await utils.addTranslations('hil', {
-    'n.month': '{MONTHS, plural, =1{1 luna} other{# luni}}',
-    'n.week': '{WEEKS, plural, =1{1 saptamana} other{# saptamani}}',
-    'reports.none.n.months':
-      '{MONTHS, plural, =1{No reports in the last month.} other{No reports in the last # months.}}',
-    'task.days.left': '{DAYS, plural, =1{1 day left} other{# days left}',
-    'tasks.none.n.weeks': '{WEEKS, plural, =1{No tasks in the next week.} other{No tasks in the next # weeks.}}',
-    'Reports': 'HilReports',
-    'view.all':'View all'
-  });
+  const createLanguage = async () =>  {
+    await utils.addTranslations('hil', {
+      'n.month': '{MONTHS, plural, =1{1 luna} other{# luni}}',
+      'n.week': '{WEEKS, plural, =1{1 saptamana} other{# saptamani}}',
+      'reports.none.n.months':
+          '{MONTHS, plural, =1{No reports in the last month.} other{No reports in the last # months.}}',
+      'task.days.left': '{DAYS, plural, =1{1 day left} other{# days left}',
+      'tasks.none.n.weeks': '{WEEKS, plural, =1{No tasks in the next week.} other{No tasks in the next # weeks.}}',
+      'Reports': 'HilReports',
+      'view.all':'View all'
+    });
+    const { languages } = await utils.getSettings();
+    languages.push({
+      locale: 'hil',
+      enabled: true,
+    });
+    await utils.updateSettings({ languages }, true);
+  };
 
   const contact = placeFactory.place().build({
     _id: 'district_hil_locale',
