605c7ba4ce2e65adcbca308d420ff24ef262976d
Diana Barsan <barsan@medic.org>
2023-04-14 16:00:55 +0300

    run scalability suite in CI

---
+++ tests/scalability/prepare-ec2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+BUILD=//medic:medic:
+
+shutdown -P +60
+mkdir /cht
+chmod 777 /cht;
+
+# Install Docker CE
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
+-o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+mkdir /cht/upgrade-service
+mkdir /cht/compose
+
+curl -s https://raw.githubusercontent.com/medic/cht-upgrade-service/main/docker-compose.yml \
+  -o /cht/upgrade-service/docker-compose.yml
+curl -s $BUILD/docker-compose/cht-core.yml -o /cht/compose/cht-core.yml
+curl -s $BUILD/docker-compose/cht-couchdb.yml -o /cht/compose/cht-couchdb.yml
+
+cd /cht/upgrade-service/
+cat > ./.env << EOF
+DOCKER_CONFIG_PATH=/home/.docker
+CHT_COMPOSE_PATH=/cht/compose
+COUCHDB_PASSWORD=medicScalability
+EOF
+docker-compose up -d
---
+++ tests/scalability/start_ec2_cht.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+set -e
+
+NODE_TLS_REJECT_UNAUTHORIZED=0
+BRANCH=$1
+sed -i '4s~^~'BUILD=$MARKET_URL_READ/$STAGING_SERVER/medic:medic:$1'\n\n~' prepare-ec2.sh
+
+echo Triggering EC2 Run Instance Command and getting Instance ID
+
+runInstance () {
+  echo $(aws ec2 run-instances \
+    --image-id ami-0c3d8c5445511bd1d \
+    --instance-type c5.2xlarge \
+    --block-device-mappings file://block-device-mapping.json \
+    --user-data file://$1 \
+    --instance-initiated-shutdown-behavior terminate \
+    --security-group-ids sg-0fa20cd785acec256 \
+    --key-name cht-scalability-ca \
+    --iam-instance-profile Arn=$SCALABILITY_ARN \
+    --profile CA
+    )
+}
+
+getInstanceId () {
+  echo $(echo $1 | jq .Instances[0].InstanceId -r)
+}
+
+getPublicDnsName () {
+  echo $(aws ec2 describe-instances --instance-ids "$1" --profile CA | jq .Reservations[0].Instances[0].PublicDnsName -r)
+}
+
+waitForInstanceUp () {
+  set +e # don't exit when /api/info doesn't return json
+  echo Begin Checking $1/api/info is up
+  version=""
+  sleep_time=10
+
+  until [ "$version" = 0.1.0 ]
+  do
+    echo Waiting for CHT to be up. Sleeping for $sleep_time.
+    sleep $sleep_time
+    version=$(curl -s $1/api/info -k -H 'Accept: application/json' | jq .version -r)
+  done
+
+  set -e
+  echo Api Is up
+}
+
+seedData () {
+  echo installing cht-conf
+  npm install cht-conf
+
+  echo Seeding data
+  echo cht url is $1
+  cht --url="$1" --accept-self-signed-certs --force \
+      csv-to-docs \
+      upload-docs \
+      create-users
+}
+
+waitForSentinel () {
+  set +e
+  sleep_time=30
+  sentinel_queue_size="51"
+
+  until [ "$sentinel_queue_size" -lt "50" ]
+  do
+  proc_seq=$(curl $1/medic-sentinel/_local/transitions-seq -s -k | jq .value -r)
+  sentinel_queue_size=$(curl $1/medic/_changes?since=$proc_seq -s -k | jq '.results | length')
+  echo Sentinel queue length is $sentinel_queue_size
+  echo Sleeping again for $sleep_time
+  sleep $sleep_time
+  done
+
+  set -e
+  echo Sentinel has caught up.
+}
+
+PublicDnsName=""
+getInstanceUrl () {
+  echo Getting PublicDnsName
+  PublicDnsName=$(getPublicDnsName "$instanceID")
+  echo DNS is "$PublicDnsName"
+
+  if [ -z "$PublicDnsName" ]
+  then
+  echo dns name not setting. Trying to get again
+  PublicDnsName=$(getPublicDnsName "$instanceID")
+  fi
+
+  if [ -z "$PublicDnsName" ]
+  then
+  echo Did not get public dns name. Exiting now.
+  exit 1
+  fi
+}
+
+instanceResponse=$(runInstance "prepare-ec2.sh")
+instanceID=$(getInstanceId "$instanceResponse")
+echo Instance id is "$instanceID"
+
+getInstanceUrl
+
+url=https://$PublicDnsName
+waitForInstanceUp "$url"
+
+MEDIC_CONF_URL='https://admin:medicScalability@'$PublicDnsName
+seedData $MEDIC_CONF_URL
+waitForSentinel $MEDIC_CONF_URL
+
+sed -i '4s~^~'MEDIC_URL=$url'\n~' run_suite.sh
+sed -i '4s~^~'S3_PATH=s3://medic-e2e/scalability/$BRANCH-$GITHUB_RUN_ID'\n~' run_suite.sh
+sed -i '4s~^~'BRANCH=$BRANCH'\n~' run_suite.sh
+
+echo Triggering EC2 Run Instance Command and getting Instance ID
+runInstance "run_suite.sh"
