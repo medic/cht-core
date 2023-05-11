e4757e4309c8ffed67bbc4fbd908422420573b0e
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2023-05-11 13:20:09 -0400

    Add // eslint-disable-next-line no-undef to contact summary config

--- tests/e2e/default/targets/config/contact-summary-target-aggregates.js
+++ tests/e2e/default/targets/config/contact-summary-target-aggregates.js
@@ -1,18 +1,23 @@
-let cards = [];
-let context = {};
+const cards = [];
+const context = {};
 let fields = [];
-if (contact.type === "person") {
-  fields = [{ label: "test.pid", value: contact.patient_id, width: 3 }];
+// eslint-disable-next-line no-undef
+if (contact.type === 'person') {
+  // eslint-disable-next-line no-undef
+  fields = [{ label: 'test.pid', value: contact.patient_id, width: 3 }];
+  // eslint-disable-next-line no-undef
   if (targetDoc) {
     const card = {
-      label: "Activity this month",
+      label: 'Activity this month',
       fields: [],
     };
-    card.fields.push({ label: "Last updated", value: targetDoc.date_updated });
+    // eslint-disable-next-line no-undef
+    card.fields.push({ label: 'Last updated', value: targetDoc.date_updated });
+    // eslint-disable-next-line no-undef
     targetDoc.targets.forEach(target => {
       let value;
       if (target.type === 'percent') {
-        value = (target.value.total ? target.value.pass * 100 / target.value.total : 0) + "%";
+        value = (target.value.total ? target.value.pass * 100 / target.value.total : 0) + '%';
       } else {
         value = target.value.pass;
       }
