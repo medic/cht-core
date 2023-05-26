93b52248cd93baceddd9cda0c6040b725762e295
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2023-05-26 13:52:51 -0400

    Wrap desktop and mobile share specs in brackets

--- tests/e2e/default-mobile/wdio.conf.js
+++ tests/e2e/default-mobile/wdio.conf.js
@@ -7,8 +7,10 @@ chai.use(require('chai-exclude'));
 exports.config = Object.assign(wdioBaseConfig.config, {
   specs: [
     '**/*.wdio-spec.js',
-    '../default/navigation/navigation.wdio-spec.js',
-    '../default/navigation/hamburger-menu.wdio-spec.js',
+    [
+      '../default/navigation/navigation.wdio-spec.js',
+      '../default/navigation/hamburger-menu.wdio-spec.js',
+    ],
   ],
   beforeSuite: async () => {
     // We tried the browser.emulateDevice('...') function but it's not stable enough,
