5c3264fa507c2c11fd0d28833137efa91083d0c5
tatilepizs <tatiana@medic.org>
2023-06-01 15:37:56 -0600

    test rapidpro url

--- tests/e2e/default/sms/rapidpro.wdio-spec.js
+++ tests/e2e/default/sms/rapidpro.wdio-spec.js
@@ -5,13 +5,13 @@ const commonPage = require('@page-objects/default/common/common.wdio.page');
 const messagesPage = require('@page-objects/default/sms/messages.wdio.page');
 const loginPage = require('@page-objects/default/login/login.wdio.page');
 const reportsPage = require('@page-objects/default/reports/reports.wdio.page');
+//const smsPregancy = require('@factories/cht/reports/sms-pregnancy');
 
 describe('RapidPro SMS Gateway', () => {
   let server;
   let broadcastsEndpointRequests = [];
   let messagesEndpointRequests = [];
-  const INCOMING_KEY = 'thecakeisalie';
-  //const OUTGOING_KEY = 'ermahgerd';
+
   const mockApp = express();
   const broadcastsEndpoint = '/api/v2/broadcasts.json';
   const messagesEndpoint = '/api/v2/messages.json';
@@ -39,14 +39,6 @@ describe('RapidPro SMS Gateway', () => {
     server && server.close();
   };
 
-  const setIncomingKey = () => {
-    return utils.saveCredentials('rapidpro:incoming', INCOMING_KEY);
-  };
-
-  /*const setOutgoingKey = () => {
-    return utils.saveCredentials('rapidpro:outgoing', OUTGOING_KEY);
-  };*/
-
   before(() => startMockApp());
 
   after(() => {
@@ -64,6 +56,11 @@ describe('RapidPro SMS Gateway', () => {
   describe('Webapp Terminating messages', () => {
     const endpoint = '/api/v2/sms/rapidpro/incoming-messages';
     const smsSettings = { outgoing_service: 'rapidpro' };
+
+    const INCOMING_KEY = 'thecakeisalie';
+    const setIncomingKey = () => {
+      return utils.saveCredentials('rapidpro:incoming', INCOMING_KEY);
+    };
 
     it('should fail with no incoming key configured', async () => {
       await utils.updateSettings({ sms: smsSettings }, true);
@@ -240,8 +237,102 @@ describe('RapidPro SMS Gateway', () => {
       expect(reportDoc.form).to.equal('FORM');
       expect(reportDoc.fields).to.contain({ some_data: 'the data' });
     });
-  });
+  }); // End describe 'Webapp Terminating messages'
 
+  describe('Webapp originating messages and state updates', () => {
+    let settings;
+    let reportWithTasks;
+
+    const OUTGOING_KEY = 'ermahgerd';
+    const setOutgoingKey = () => {
+      return utils.saveCredentials('rapidpro:outgoing', OUTGOING_KEY);
+    };
+
+    beforeEach(() => {
+      settings = {
+        sms: {
+          outgoing_service: 'rapidpro',
+          rapidpro: {url: utils.hostURL(server.address().port)},
+        }
+      };
+      //reportWithTasks = smsPregancy.pregnancy().build();
+      reportWithTasks = {
+        _id: 'report_with_tasks',
+        type: 'data_record',
+        form: 'P',
+        errors: [],
+        reported_date: new Date().getTime(),
+        tasks: [
+          {
+            messages: [{to: 'phone1', message: 'message1', uuid: 'uuid1'}],
+            state: 'pending',
+          },
+          {
+            messages: [{to: 'phone2', message: 'message2', uuid: 'uuid2'}],
+            state: 'sent',
+          },
+        ],
+        scheduled_tasks: [
+          {
+            messages: [{to: 'phone3', message: 'message3', uuid: 'uuid3'}],
+            state: 'pending',
+            group: 2,
+            type: 'ANC Reminders LMP',
+          },
+          {
+            messages: [{to: 'phone4', message: 'message4', uuid: 'uuid4'}],
+            state: 'pending',
+            group: 2,
+            type: 'ANC Reminders LMP',
+          },
+          {
+            messages: [{to: 'phone5', message: 'message5', uuid: 'uuid5'}],
+            state: 'pending',
+            group: 3,
+            type: 'ANC Reminders LMP',
+          },
+        ]
+      };
+    });
+
+    afterEach(() => utils.revertDb([], true));
+
+    //});
+
+    it('should not call RapidPro endpoint when credentials are not set', async () => {
+      await utils.updateSettings(settings, true);
+      await utils.saveDoc(reportWithTasks);
+
+      expect(broadcastsEndpointRequests.length).to.equal(0);
+    });
+
+    // eslint-disable-next-line no-only-tests/no-only-tests
+    it.only('should call endpoint with set credentials', async () => {
+      await utils.updateSettings(settings, true);
+      await setOutgoingKey();
+
+      await utils.saveDoc(reportWithTasks);
+      await browser.waitUntil(() => broadcastsEndpointRequests.length === 4, 1200);
+
+      const bodies = broadcastsEndpointRequests
+        .map(item => item[0])
+        .sort((a, b) => a.text.localeCompare(b.text));
+
+      const headers = broadcastsEndpointRequests.map(item => item[1]);
+      expect(bodies).to.equal([
+        { urns: ['tel:phone1'], text: 'message1' },
+        { urns: ['tel:phone3'], text: 'message3' },
+        { urns: ['tel:phone4'], text: 'message4' },
+        { urns: ['tel:phone5'], text: 'message5' },
+      ]);
+      //const headers = broadcastsEndpointRequests.map(item => item[1]);
+      expect(headers[0].authorization).to.equal(`Token ${OUTGOING_KEY}`);
+      expect(headers[1].authorization).to.equal(`Token ${OUTGOING_KEY}`);
+      expect(headers[2].authorization).to.equal(`Token ${OUTGOING_KEY}`);
+      expect(headers[3].authorization).to.equal(`Token ${OUTGOING_KEY}`);
+    });
+
+  }); //End describe 'Webapp originating messages and state updates'
 
 });
 
