72f5eb305b213b211692b8a183f27db3f14eb6f4
garethbowen <gareth@bowenwebdesign.co.nz>
2023-06-03 16:06:40 +0300

    chore: replace glob

--- scripts/build/ddoc-compile.js
+++ scripts/build/ddoc-compile.js
@@ -3,17 +3,27 @@ const couchCompile = util.promisify(require('couchdb-compile'));
 const { writeFile } = require('node:fs/promises');
 
 const compilePrimary = async () => {
-  await compile('build/ddocs/medic-db/', 'build/ddocs/medic.json');
+  await compile(
+    [
+      'build/ddocs/medic-db/medic',
+      'build/ddocs/medic-db/medic-admin',
+      'build/ddocs/medic-db/medic-client',
+      'build/ddocs/medic-db/medic-conflicts',
+      'build/ddocs/medic-db/medic-scripts',
+      'build/ddocs/medic-db/medic-sms',
+    ],
+    'build/ddocs/medic.json'
+  );
 };
 
 const compileStaging = async () => {
-  await compile('build/staging', 'build/staging.json');
+  await compile([ 'build/staging' ], 'build/staging.json');
 };
 
 const compileSecondary = async () => {
-  await compile('build/ddocs/sentinel-db', 'build/ddocs/sentinel.json');
-  await compile('build/ddocs/users-meta-db', 'build/ddocs/users-meta.json');
-  await compile('build/ddocs/logs-db', 'build/ddocs/logs.json');
+  await compile([ 'build/ddocs/sentinel-db' ], 'build/ddocs/sentinel.json');
+  await compile([ 'build/ddocs/users-meta-db' ], 'build/ddocs/users-meta.json');
+  await compile([ 'build/ddocs/logs-db' ], 'build/ddocs/logs.json');
 };
 
 const commands = {
@@ -31,9 +41,9 @@ const getCommand = () => {
   return cmd;
 };
 
-const compile = async (inputDir, outputFile) => {
-  const ddoc = await couchCompile(inputDir);
-  await writeFile(outputFile, JSON.stringify({ docs: [ ddoc ] }, null, 2));
+const compile = async (inputDirs, outputFile) => {
+  const docs = await Promise.all(inputDirs.map(dir => couchCompile(dir)));
+  await writeFile(outputFile, JSON.stringify({ docs }, null, 2));
   console.log('ddoc compiled successfully');
 };
 
