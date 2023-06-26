72ca9cd73da3bb05b8fc0035ca6bc24617c878d3
ngaruko <ngarukob@yahoo.com>
2023-06-26 03:50:32 +0000

    add debug option

--- tests/e2e/default/wdio.conf.js
+++ tests/e2e/default/wdio.conf.js
@@ -16,6 +16,10 @@ const browserLogsUtils = require('@utils/browser-logs');
 const ALLURE_OUTPUT = 'allure-results';
 const browserLogPath = path.join('tests', 'logs', 'browser.console.log');
 const logLevels = ['error', 'warning', 'debug'];
+const DEBUG_TEST = process.env.DEBUG_TEST;
+const DEFAULT_TIMEOUT = 12 * 1000;
+const DEBUG_TIMEOUT = 24 * 60 * 60 * 1000; //timeout in debug mode, allows more interaction with browser after test
+
 const existingFeedBackDocIds = [];
 let testTile;
 
@@ -83,7 +87,8 @@ const baseConfig = {
     browserName: 'chrome',
     acceptInsecureCerts: true,
     'goog:chromeOptions': {
-      args: ['--headless', '--disable-gpu', '--deny-permission-prompts', '--ignore-certificate-errors']
+      args: DEBUG_TEST ? ['--inspect', '--deny-permission-prompts', '--ignore-certificate-errors'] :
+        ['--headless', '--disable-gpu', '--deny-permission-prompts', '--ignore-certificate-errors']
     }
 
     // If outputDir is provided WebdriverIO can capture driver session logs
@@ -172,8 +177,8 @@ const baseConfig = {
   // See the full list at http://mochajs.org/
   mochaOpts: {
     ui: 'bdd',
-    timeout: 120000,
-    retries: 5,
+    timeout: DEBUG_TEST ? DEBUG_TIMEOUT : DEFAULT_TIMEOUT,
+    retries: DEBUG_TEST ? 0 : 5,
   },
   //
   // =====
