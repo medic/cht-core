2d9169eca05d040fb1c46f147683e3151c78cd70
Michael Kohn <michael@medicmobile.org>
2023-06-26 13:55:51 -0400

    Initial Commit
    Just copied from Default config

---
+++ config/demo/test/contact-summary/past_pregnancy.spec.js
@@ -0,0 +1,323 @@
+const { expect } = require('chai');
+const TestRunner = require('cht-conf-test-harness');
+const moment = require('moment');
+const { pregnancyRegistrationScenarios, pregnancyHomeVisitScenarios, deliveryReportScenarios } = require('../form-inputs');
+const harness = new TestRunner();
+
+describe('Tests for past pregnancy condition card', () => {
+  before(async () => { return await harness.start(); });
+  after(async () => { return await harness.stop(); });
+  beforeEach(async () => { return await harness.clear(); });
+  afterEach(() => {
+    expect(harness.consoleErrors).to.be.empty;
+  });
+
+  it('pregnancy registration with risk factors, one child healthy delivery', async () => {
+    await harness.setNow('2000-01-01');
+
+    // Load the pregnancy form and fill in
+    let result = await harness.fillForm('pregnancy', ...pregnancyRegistrationScenarios.riskDanger());
+    expect(result.errors).to.be.empty;
+
+    await harness.setNow('2000-04-30');
+    //Load the delivery form and fill in
+    result = await harness.fillForm('delivery', ...deliveryReportScenarios.oneChildHealthyFacility);
+    expect(result.errors).to.be.empty;
+
+
+    // Verify condition card
+    const contactSummary = await harness.getContactSummary();
+    expect(contactSummary.cards).to.have.property('length', 1);
+    const pastPregnancyCard = contactSummary.cards[0];
+    expect(pastPregnancyCard).to.have.property('label', 'contact.profile.pregnancy.past');
+    const fields = pastPregnancyCard.fields;
+    expect(fields).to.have.property('length', 5);
+    expect(fields[0]).to.deep.equal({
+      'label': 'contact.profile.delivery_date',
+      'value': moment('2000-04-22').valueOf(),
+      'filter': 'simpleDate',
+      'width': 6
+    });
+    expect(fields[1]).to.deep.equal({
+      'label': 'contact.profile.delivery_place',
+      'value': 'health_facility',
+      'translate': true,
+      'width': 6
+    });
+    expect(fields[2]).to.deep.equal({
+      'label': 'contact.profile.delivered_babies',
+      'value': '1',
+      'width': 6
+    });
+    expect(fields[3]).to.deep.equal({
+      'label': 'contact.profile.anc_visit',
+      'value': 1,
+      'width': 3
+    });
+    expect(fields[4]).to.deep.equal({
+      'icon': 'icon-risk',
+      'label': 'contact.profile.risk.high',
+      'value': 'contact.profile.risk.multiple',
+      'translate': true,
+      'width': 6
+    });
+  });
+
+  it('delivery with 2 children, both alive and healthy', async () => {
+    await harness.setNow('2000-01-01');
+    // Load the pregnancy form and fill in
+    let result = await harness.fillForm('pregnancy', ...pregnancyRegistrationScenarios.riskDanger());
+    expect(result.errors).to.be.empty;
+
+    await harness.setNow('2000-04-30');
+    //Load the delivery form and fill in
+    result = await harness.fillForm('delivery', ...deliveryReportScenarios.twoChildrenHealthy);
+    expect(result.errors).to.be.empty;
+
+
+    // Verify condition card
+    const contactSummary = await harness.getContactSummary();
+    expect(contactSummary.cards).to.have.property('length', 1);
+    const pastPregnancyCard = contactSummary.cards[0];
+    expect(pastPregnancyCard).to.have.property('label', 'contact.profile.pregnancy.past');
+    const fields = pastPregnancyCard.fields;
+    expect(fields).to.have.property('length', 5);
+    expect(fields[0]).to.deep.equal({
+      'label': 'contact.profile.delivery_date',
+      'value': moment('2000-04-22').valueOf(),
+      'filter': 'simpleDate',
+      'width': 6
+    });
+    expect(fields[1]).to.deep.equal({
+      'label': 'contact.profile.delivery_place',
+      'value': 'health_facility',
+      'translate': true,
+      'width': 6
+    });
+    expect(fields[2]).to.deep.equal({
+      'label': 'contact.profile.delivered_babies',
+      'value': '2',
+      'width': 6
+    });
+    expect(fields[3]).to.deep.equal({
+      'label': 'contact.profile.anc_visit',
+      'value': 1,
+      'width': 3
+    });
+    expect(fields[4]).to.deep.equal({
+      'icon': 'icon-risk',
+      'label': 'contact.profile.risk.high',
+      'value': 'contact.profile.risk.multiple',
+      'translate': true,
+      'width': 6
+    });
+  });
+
+  it('delivery with 3 children: 1 alive and healthy, 1 deceased, 1 stillbirth', async () => {
+    await harness.setNow('2000-01-01');
+    // Load the pregnancy form and fill in
+    let result = await harness.fillForm('pregnancy', ...pregnancyRegistrationScenarios.riskDanger());
+    expect(result.errors).to.be.empty;
+
+    await harness.setNow('2000-04-30');
+    //Load the delivery form and fill in
+    result = await harness.fillForm('delivery', ...deliveryReportScenarios.oneChildHealthyOneDeceasedOneStillbirth);
+    expect(result.errors).to.be.empty;
+
+
+    // Verify condition card
+    const contactSummary = await harness.getContactSummary();
+    //fails with error: TypeError: babyDeaths.forEach is not a function
+    //because the repeat group is expected to be an array
+    expect(contactSummary.cards).to.have.property('length', 1);
+    const pastPregnancyCard = contactSummary.cards[0];
+    expect(pastPregnancyCard).to.have.property('label', 'contact.profile.pregnancy.past');
+    const fields = pastPregnancyCard.fields;
+    expect(fields).to.have.property('length', 14);
+    expect(fields[0]).to.deep.equal({
+      'label': 'contact.profile.delivery_date',
+      'value': moment('2000-04-22').valueOf(),
+      'filter': 'simpleDate',
+      'width': 6
+    });
+    expect(fields[1]).to.deep.equal({
+      'label': 'contact.profile.delivery_place',
+      'value': 'health_facility',
+      'translate': true,
+      'width': 6
+    });
+    expect(fields[2]).to.deep.equal({
+      'label': 'contact.profile.delivered_babies',
+      'value': '3',
+      'width': 6
+    });
+    expect(fields[3]).to.deep.equal({
+      'label': 'contact.profile.deceased_babies',
+      'value': '2',
+      'width': 6
+    });
+    expect(fields[4]).to.deep.equal({
+      'label': 'contact.profile.newborn.death_date',
+      'value': '2000-04-22',
+      'filter': 'simpleDate',
+      'width': 6
+    });
+    expect(fields[5]).to.deep.equal({
+      'label': 'contact.profile.newborn.death_place',
+      'value': 'health_facility',
+      'translate': true,
+      'width': 6
+    });
+    expect(fields[6]).to.deep.equal({
+      'label': 'contact.profile.delivery.stillbirthQ',
+      'value': 'yes',
+      'translate': true,
+      'width': 6
+    });
+    expect(fields[7]).to.deep.equal({
+      'label': '',
+      'value': '',
+      'width': 6
+    });
+    expect(fields[8]).to.deep.equal({
+      'label': 'contact.profile.newborn.death_date',
+      'value': '2000-04-23',
+      'filter': 'simpleDate',
+      'width': 6
+    });
+    expect(fields[9]).to.deep.equal({
+      'label': 'contact.profile.newborn.death_place',
+      'value': 'home',
+      'translate': true,
+      'width': 6
+    });
+    expect(fields[10]).to.deep.equal({
+      'label': 'contact.profile.delivery.stillbirthQ',
+      'value': 'no',
+      'translate': true,
+      'width': 6
+    });
+    expect(fields[11]).to.deep.equal({
+      'label': '',
+      'value': '',
+      'width': 6
+    });
+    expect(fields[12]).to.deep.equal({
+      'label': 'contact.profile.anc_visit',
+      'value': 1,
+      'width': 3
+    });
+    expect(fields[13]).to.deep.equal({
+      'icon': 'icon-risk',
+      'label': 'contact.profile.risk.high',
+      'value': 'contact.profile.risk.multiple',
+      'translate': true,
+      'width': 6
+    });
+  });
+
+  it('pregnancy registration with early end to pregnancy (miscarriage)', async () => {
+    await harness.setNow('2000-01-01');
+
+    // Load the pregnancy form and fill in
+    let result = await harness.fillForm('pregnancy', ...pregnancyRegistrationScenarios.riskDanger());
+    expect(result.errors).to.be.empty;
+
+    await harness.setNow('2000-01-03');
+    //Load the delivery form and fill in
+    result = await harness.fillForm('pregnancy_home_visit', ...pregnancyHomeVisitScenarios.miscarriage);
+    expect(result.errors).to.be.empty;
+
+
+    // Verify condition card
+    const contactSummary = await harness.getContactSummary();
+    expect(contactSummary.cards).to.have.property('length', 1);
+    const pastPregnancyCard = contactSummary.cards[0];
+    expect(pastPregnancyCard).to.have.property('label', 'contact.profile.pregnancy.past');
+    const fields = pastPregnancyCard.fields;
+    expect(fields).to.have.property('length', 5);
+    expect(fields[0]).to.deep.equal({
+      'label': 'contact.profile.pregnancy.end_early',
+      'value': 'miscarriage',
+      'translate': true,
+      'width': 6
+    });
+    expect(fields[1]).to.deep.equal({
+      'label': 'contact.profile.pregnancy.end_date',
+      'value': moment('2000-01-02').valueOf(),
+      'filter': 'simpleDate',
+      'width': 6
+    });
+    expect(fields[2]).to.deep.equal({
+      'label': 'contact.profile.pregnancy.end_weeks',
+      'value': 22,
+      'translate': false,
+      'width': 6
+    });
+    expect(fields[3]).to.deep.equal({
+      'label': 'contact.profile.anc_visit',
+      'value': 1,
+      'width': 3
+    });
+    expect(fields[4]).to.deep.equal({
+      'label': 'contact.profile.risk.high',
+      'value': 'contact.profile.risk.multiple',
+      'translate': true,
+      'icon': 'icon-risk',
+      'width': 6
+    });
+  });
+
+  it('pregnancy registration with early end to pregnancy (abortion)', async () => {
+    await harness.setNow('2000-01-01');
+
+    // Load the pregnancy form and fill in
+    let result = await harness.fillForm('pregnancy', ...pregnancyRegistrationScenarios.riskDanger());
+    expect(result.errors).to.be.empty;
+
+    await harness.setNow('2000-01-03');
+    //Load the delivery form and fill in
+    result = await harness.fillForm('pregnancy_home_visit', ...pregnancyHomeVisitScenarios.abortion);
+    expect(result.errors).to.be.empty;
+
+
+    // Verify condition card
+    const contactSummary = await harness.getContactSummary();
+    expect(contactSummary.cards).to.have.property('length', 1);
+    const pastPregnancyCard = contactSummary.cards[0];
+    expect(pastPregnancyCard).to.have.property('label', 'contact.profile.pregnancy.past');
+    const fields = pastPregnancyCard.fields;
+    expect(fields).to.have.property('length', 5);
+    expect(fields[0]).to.deep.equal({
+      'label': 'contact.profile.pregnancy.end_early',
+      'value': 'abortion',
+      'translate': true,
+      'width': 6
+    });
+    expect(fields[1]).to.deep.equal({
+      'label': 'contact.profile.pregnancy.end_date',
+      'value': moment('2000-01-02').valueOf(),
+      'filter': 'simpleDate',
+      'width': 6
+    });
+    expect(fields[2]).to.deep.equal({
+      'label': 'contact.profile.pregnancy.end_weeks',
+      'value': 22,
+      'translate': false,
+      'width': 6
+    });
+    expect(fields[3]).to.deep.equal({
+      'label': 'contact.profile.anc_visit',
+      'value': 1,
+      'width': 3
+    });
+    expect(fields[4]).to.deep.equal({
+      'label': 'contact.profile.risk.high',
+      'value': 'contact.profile.risk.multiple',
+      'translate': true,
+      'icon': 'icon-risk',
+      'width': 6
+    });
+  });
+
+});
---
+++ config/demo/test/forms/analytics/pregnancy.spec.js
@@ -0,0 +1,311 @@
+const { expect } = require('chai');
+const TestRunner = require('cht-conf-test-harness');
+const { pregnancyRegistrationScenarios } = require('../../form-inputs');
+const harness = new TestRunner();
+
+describe('Pregnancy form analytic field tests', () => {
+  before(async () => { return await harness.start(); });
+  after(async () => { return await harness.stop(); });
+  beforeEach(
+    async () => {
+      await harness.clear();
+      await harness.setNow(new Date('2000-01-01'));//UTC 00:00
+      return harness.loadForm('pregnancy');
+    });
+  afterEach(() => {
+    expect(harness.consoleErrors).to.be.empty;
+  });
+
+  it('pregnancy with pregnancy and danger signs followup dates', async () => {
+    // Load the pregnancy form and fill in
+    const result = await harness.fillForm(...pregnancyRegistrationScenarios.danger);
+
+    // Verify that the form successfully got submitted
+    expect(result.errors).to.be.empty;
+
+    // Verify some attributes on the resulting report
+    expect(result.report.fields.data).to.deep.include({
+      __lmp_method: 'method_lmp',
+      __no_lmp_registration_reason: '',
+      __lmp_date: '1999-11-01',
+      __lmp_approx_weeks: '',
+      __lmp_approx_months: '',
+      __edd: '2000-08-07',
+      __num_previous_anc_hf_visits: '0',
+      __previous_anc_hf_visit_dates: '',
+      __next_anc_hf_visit_date_known: 'yes',
+      __next_anc_hf_visit_date: '2000-01-20',
+      __has_risk_factors: 'no',
+      __first_pregnancy: 'no',
+      __previous_miscarriage: 'no',
+      __previous_difficulties: 'no',
+      __more_than_4_children: 'no',
+      __last_baby_born_less_than_1_year_ago: 'no',
+      __heart_condition: 'no',
+      __asthma: 'no',
+      __high_blood_pressure: 'no',
+      __diabetes: 'no',
+      __additional_high_risk_condition_to_report: 'no',
+      __additional_high_risk_condition: '',
+      __has_danger_sign: 'yes',
+      __vaginal_bleeding: 'yes',
+      __fits: 'no',
+      __severe_abdominal_pain: 'no',
+      __severe_headache: 'no',
+      __very_pale: 'no',
+      __fever: 'no',
+      __reduced_or_no_fetal_movements: 'no',
+      __breaking_water: 'no',
+      __easily_tired: 'no',
+      __face_hand_swelling: 'no',
+      __breathlessness: 'no',
+      __uses_llin: 'no',
+      __takes_iron_folate_daily: 'no',
+      __received_deworming_meds: '',
+      __tested_for_hiv_in_past_3_months: 'no',
+      __received_tetanus_toxoid_this_pregnancy: '',
+      meta: {
+        __patient_uuid: 'patient_id',
+        __patient_id: 'patient_id',
+        __household_uuid: 'patient_parent_id',
+        __source: 'action',
+        __source_id: ''
+      }
+    });
+  });
+
+  it('pregnancy with no pregnancy follow up date', async () => {
+    // Load the pregnancy form and fill in
+    const result = await harness.fillForm(...pregnancyRegistrationScenarios.safeNoFollowUp);
+
+    // Verify that the form successfully got submitted
+    expect(result.errors).to.be.empty;
+
+    // Verify some attributes on the resulting report
+    expect(result.report.fields.data).to.deep.include({
+      __lmp_method: 'method_lmp',
+      __no_lmp_registration_reason: '',
+      __lmp_date: '1999-08-01',
+      __lmp_approx_weeks: '',
+      __lmp_approx_months: '',
+      __edd: '2000-05-07',
+      __num_previous_anc_hf_visits: '0',
+      __previous_anc_hf_visit_dates: '',
+      __next_anc_hf_visit_date_known: 'no',
+      __next_anc_hf_visit_date: '',
+      __has_risk_factors: 'no',
+      __first_pregnancy: 'no',
+      __previous_miscarriage: 'no',
+      __previous_difficulties: 'no',
+      __more_than_4_children: 'no',
+      __last_baby_born_less_than_1_year_ago: 'no',
+      __heart_condition: 'no',
+      __asthma: 'no',
+      __high_blood_pressure: 'no',
+      __diabetes: 'no',
+      __additional_high_risk_condition_to_report: 'no',
+      __additional_high_risk_condition: '',
+      __has_danger_sign: 'no',
+      __vaginal_bleeding: 'no',
+      __fits: 'no',
+      __severe_abdominal_pain: 'no',
+      __severe_headache: 'no',
+      __very_pale: 'no',
+      __fever: 'no',
+      __reduced_or_no_fetal_movements: 'no',
+      __breaking_water: 'no',
+      __easily_tired: 'no',
+      __face_hand_swelling: 'no',
+      __breathlessness: 'no',
+      __uses_llin: 'no',
+      __takes_iron_folate_daily: 'no',
+      __received_deworming_meds: 'no',
+      __tested_for_hiv_in_past_3_months: 'no',
+      __received_tetanus_toxoid_this_pregnancy: '',
+      meta: {
+        __patient_uuid: 'patient_id',
+        __patient_id: 'patient_id',
+        __household_uuid: 'patient_parent_id',
+        __source: 'action',
+        __source_id: ''
+      }
+    });
+  });
+
+  it('pregnancy with current weeks pregnant', async () => {
+    // Load the pregnancy form and fill in
+    const result = await harness.fillForm(...pregnancyRegistrationScenarios.safe12WeeksApprox);
+
+    // Verify that the form successfully got submitted
+    expect(result.errors).to.be.empty;
+
+    // Verify analytics fields on the resulting report
+    expect(result.report.fields.data).to.deep.include({
+      __lmp_method: 'method_approx',
+      __no_lmp_registration_reason: '',
+      __lmp_date: '1999-10-09',
+      __lmp_approx_weeks: '12',
+      __lmp_approx_months: '',
+      __edd: '2000-07-15',
+      __num_previous_anc_hf_visits: '0',
+      __previous_anc_hf_visit_dates: '',
+      __next_anc_hf_visit_date_known: 'no',
+      __next_anc_hf_visit_date: '',
+      __has_risk_factors: 'no',
+      __first_pregnancy: 'no',
+      __previous_miscarriage: 'no',
+      __previous_difficulties: 'no',
+      __more_than_4_children: 'no',
+      __last_baby_born_less_than_1_year_ago: 'no',
+      __heart_condition: 'no',
+      __asthma: 'no',
+      __high_blood_pressure: 'no',
+      __diabetes: 'no',
+      __additional_high_risk_condition_to_report: 'no',
+      __additional_high_risk_condition: '',
+      __has_danger_sign: 'no',
+      __vaginal_bleeding: 'no',
+      __fits: 'no',
+      __severe_abdominal_pain: 'no',
+      __severe_headache: 'no',
+      __very_pale: 'no',
+      __fever: 'no',
+      __reduced_or_no_fetal_movements: 'no',
+      __breaking_water: 'no',
+      __easily_tired: 'no',
+      __face_hand_swelling: 'no',
+      __breathlessness: 'no',
+      __uses_llin: 'no',
+      __takes_iron_folate_daily: 'no',
+      __received_deworming_meds: '',
+      __tested_for_hiv_in_past_3_months: 'no',
+      __received_tetanus_toxoid_this_pregnancy: '',
+      meta: {
+        __patient_uuid: 'patient_id',
+        __patient_id: 'patient_id',
+        __household_uuid: 'patient_parent_id',
+        __source: 'action',
+        __source_id: ''
+      }
+    });
+  });
+
+  
+  it('pregnancy with current months pregnant', async () => {
+    // Load the pregnancy form and fill in
+    const result = await harness.fillForm(...pregnancyRegistrationScenarios.safe3MonthsApprox);
+
+    // Verify that the form successfully got submitted
+    expect(result.errors).to.be.empty;
+
+    // Verify analytics fields on the resulting report
+    expect(result.report.fields.data).to.deep.include({
+      __lmp_method: 'method_approx',
+      __no_lmp_registration_reason: '',
+      __lmp_date: '1999-10-01',
+      __lmp_approx_weeks: '',
+      __lmp_approx_months: '3',
+      __edd: '2000-07-07',
+      __num_previous_anc_hf_visits: '0',
+      __previous_anc_hf_visit_dates: '',
+      __next_anc_hf_visit_date_known: 'no',
+      __next_anc_hf_visit_date: '',
+      __has_risk_factors: 'no',
+      __first_pregnancy: 'no',
+      __previous_miscarriage: 'no',
+      __previous_difficulties: 'no',
+      __more_than_4_children: 'no',
+      __last_baby_born_less_than_1_year_ago: 'no',
+      __heart_condition: 'no',
+      __asthma: 'no',
+      __high_blood_pressure: 'no',
+      __diabetes: 'no',
+      __additional_high_risk_condition_to_report: 'no',
+      __additional_high_risk_condition: '',
+      __has_danger_sign: 'no',
+      __vaginal_bleeding: 'no',
+      __fits: 'no',
+      __severe_abdominal_pain: 'no',
+      __severe_headache: 'no',
+      __very_pale: 'no',
+      __fever: 'no',
+      __reduced_or_no_fetal_movements: 'no',
+      __breaking_water: 'no',
+      __easily_tired: 'no',
+      __face_hand_swelling: 'no',
+      __breathlessness: 'no',
+      __uses_llin: 'no',
+      __takes_iron_folate_daily: 'no',
+      __received_deworming_meds: 'no',
+      __tested_for_hiv_in_past_3_months: 'no',
+      __received_tetanus_toxoid_this_pregnancy: '',
+      meta: {
+        __patient_uuid: 'patient_id',
+        __patient_id: 'patient_id',
+        __household_uuid: 'patient_parent_id',
+        __source: 'action',
+        __source_id: ''
+      }
+    });
+  });
+
+  
+  it('pregnancy with edd', async () => {
+    // Load the pregnancy form and fill in
+    const result = await harness.fillForm(...pregnancyRegistrationScenarios.safeWithEddMethod);
+
+    // Verify that the form successfully got submitted
+    expect(result.errors).to.be.empty;
+
+    // Verify analytics fields on the resulting report
+    expect(result.report.fields.data).to.deep.include({
+      __lmp_method: 'method_edd',
+      __no_lmp_registration_reason: '',
+      __lmp_date: '1999-08-01',
+      __lmp_approx_weeks: '',
+      __lmp_approx_months: '',
+      __edd: '2000-05-07',
+      __num_previous_anc_hf_visits: '0',
+      __previous_anc_hf_visit_dates: '',
+      __next_anc_hf_visit_date_known: 'no',
+      __next_anc_hf_visit_date: '',
+      __has_risk_factors: 'no',
+      __first_pregnancy: 'no',
+      __previous_miscarriage: 'no',
+      __previous_difficulties: 'no',
+      __more_than_4_children: 'no',
+      __last_baby_born_less_than_1_year_ago: 'no',
+      __heart_condition: 'no',
+      __asthma: 'no',
+      __high_blood_pressure: 'no',
+      __diabetes: 'no',
+      __additional_high_risk_condition_to_report: 'no',
+      __additional_high_risk_condition: '',
+      __has_danger_sign: 'no',
+      __vaginal_bleeding: 'no',
+      __fits: 'no',
+      __severe_abdominal_pain: 'no',
+      __severe_headache: 'no',
+      __very_pale: 'no',
+      __fever: 'no',
+      __reduced_or_no_fetal_movements: 'no',
+      __breaking_water: 'no',
+      __easily_tired: 'no',
+      __face_hand_swelling: 'no',
+      __breathlessness: 'no',
+      __uses_llin: 'no',
+      __takes_iron_folate_daily: 'no',
+      __received_deworming_meds: 'no',
+      __tested_for_hiv_in_past_3_months: 'no',
+      __received_tetanus_toxoid_this_pregnancy: '',
+      meta: {
+        __patient_uuid: 'patient_id',
+        __patient_id: 'patient_id',
+        __household_uuid: 'patient_parent_id',
+        __source: 'action',
+        __source_id: ''
+      }
+    });
+  });
+
+});
