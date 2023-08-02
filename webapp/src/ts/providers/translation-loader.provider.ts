3a735e85f2bb8daf043800ffd4bf840fb47c46dd
Joshua Kuestersteffen <jkuester@kuester7.com>
2023-08-02 08:46:43 -0500

    chore: change up type on translation-loader.provider.ts

--- webapp/src/ts/providers/translation-loader.provider.ts
+++ webapp/src/ts/providers/translation-loader.provider.ts
@@ -14,7 +14,7 @@ export class TranslationLoaderProvider implements TranslateLoader {
 
   getTranslation(locale) {
     if (!locale) {
-      return undefined as unknown as Observable<any>;
+      return undefined as any;
     }
 
     if (this.loadingPromises[locale]) {
