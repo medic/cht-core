7d89c288afe286d1e9e7bea1cb2937ef7c6efc6c
Diana Barsan <barsan@medic.org>
2023-08-08 08:11:49 +0300

    run all specs when passing no suite.
    Signed-off-by: Diana Barsan <barsan@medic.org>

--- tests/e2e/default/wdio.conf.js
+++ tests/e2e/default/wdio.conf.js
@@ -2,6 +2,9 @@ const wdioBaseConfig = require('../wdio.conf');
 const { suites } = require('./suites');
 
 // Override specific properties from wdio base config
-const defaultConfig = Object.assign(wdioBaseConfig.config, { suites });
+const defaultConfig = Object.assign(wdioBaseConfig.config, {
+  suites,
+  specs: ['**/*.wdio-spec.js'],
+});
 
 exports.config = defaultConfig;
