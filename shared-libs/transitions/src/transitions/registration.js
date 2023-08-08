1bf6514ae984fb6c2e48d12fe59287bdb6bfbfa4
PrjShrestha <prajwol.shrestha18@gmail.com>
2023-08-08 18:52:08 +0545

    added null checks on getpatientphonefield function

--- shared-libs/transitions/src/transitions/registration.js
+++ shared-libs/transitions/src/transitions/registration.js
@@ -45,13 +45,14 @@ const getPatientPhoneField = (settings, currentForm) => {
   // Get the current form for which we are running transition form
   const formDef = settings.forms[currentForm];
 
-  // Get the phone field i.e.the field with type phone_number in the form
-  const phoneField = (Object.keys(formDef.fields).filter(key => formDef.fields[key].type === 'phone_number'));
-
-  // Return the phone field
-  if (phoneField && phoneField[0]) {
-    return phoneField[0];
-  }
+  if (formDef && formDef.fields) { 
+    // Get the phone field i.e.the field with type phone_number in the form
+    const phoneField = (Object.keys(formDef.fields).filter(key => formDef.fields[key].type === 'phone_number'));
+    // Return the phone field
+    if (phoneField && phoneField[0]) {
+      return phoneField[0];
+    }
+  }  
 };
 
 
