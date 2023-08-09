6d30414f16b58c0bf12ac230c347481a0db9ff0d
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2023-08-09 13:44:00 +1200

    convert a bunch more grunt exec to npm run

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,8 +12,6 @@ const DEV = !BUILD_NUMBER;
 const buildUtils = require('./scripts/build');
 const buildVersions = require('./scripts/build/versions');
 
-const ESLINT_COMMAND = './node_modules/.bin/eslint --color --cache';
-
 module.exports = function(grunt) {
   'use strict';
 
@@ -24,30 +22,9 @@ module.exports = function(grunt) {
     exec: {
       'compile-ddocs-primary': 'node ./scripts/build/ddoc-compile.js primary',
       'compile-ddocs-staging': 'node ./scripts/build/ddoc-compile.js staging',
-      'uglify-api':
-        'node ./node_modules/uglify-js/bin/uglifyjs api/build/static/login/script.js -o api/build/static/login/script.js && ' +
-        'node ./node_modules/uglify-js/bin/uglifyjs api/build/static/login/lib-bowser.js -o api/build/static/login/lib-bowser.js',
-      'uglify-admin':
-        'node ./node_modules/uglify-js/bin/uglifyjs api/build/static/admin/js/main.js -o api/build/static/admin/js/main.js && ' +
-        'node ./node_modules/uglify-js/bin/uglifyjs api/build/static/admin/js/templates.js -o api/build/static/admin/js/templates.js',
+
       'push-ddoc-to-staging': 'node ./scripts/build/push-ddoc-to-staging.js',
       'clean-build-dir': 'rm -rf build && mkdir build',
-      'jsdoc-admin': './node_modules/jsdoc/jsdoc.js -d jsdocs/admin -c node_modules/angular-jsdoc/common/conf.json -t node_modules/angular-jsdoc/angular-template admin/src/js/**/*.js',
-      'jsdoc-sentinel': './node_modules/jsdoc/jsdoc.js -d jsdocs/sentinel sentinel/src/**/*.js',
-      'jsdoc-api': './node_modules/jsdoc/jsdoc.js -d jsdocs/api -R api/README.md api/src/**/*.js',
-      'jsdoc-shared-libs': './node_modules/jsdoc/jsdoc.js -d jsdocs/shared-libs shared-libs/**/src/**/*.js',
-      'less': './node_modules/less/bin/lessc admin/src/css/main.less api/build/static/admin/css/main.css',
-      'browserify-admin': 'mkdir -p api/build/static/admin/js/ && ' +
-        'node ./node_modules/browserify/bin/cmd.js ' +
-        '--debug ' +
-        '-t browserify-ngannotate ' +
-        '-r "./admin/node_modules/angular-translate/dist/angular-translate-interpolation-messageformat/angular-translate-interpolation-messageformat:angular-translate-interpolation-messageformat" ' +
-        '-r "./admin/node_modules/google-libphonenumber:google-libphonenumber" ' +
-        '-r "./admin/node_modules/gsm:gsm" ' +
-        '-r "./admin/node_modules/object-path:object-path" ' +
-        '-r "./admin/node_modules/bikram-sambat:bikram-sambat" ' +
-        '-r "./admin/node_modules/lodash/core:lodash/core" ' +
-        'admin/src/js/main.js > api/build/static/admin/js/main.js',
       'compile-admin-templates':
         'mkdir -p api/build/static/admin/js/ && ' +
         'node ./scripts/build/build-angularjs-template-cache.js',
@@ -60,19 +37,12 @@ module.exports = function(grunt) {
       'karma-admin': 'node ./scripts/ci/run-karma.js',
       'copy-ddocs': 'mkdir -p build/ddocs && cp -r ddocs/* build/ddocs/',
       'copy-api-ddocs': 'mkdir -p api/build/ddocs && cp build/ddocs/*.json api/build/ddocs/',
-      'copy-webapp-static':
-        'cp -r webapp/src/audio api/build/static/webapp/ && ' +
-        'cp -r webapp/src/fonts api/build/static/webapp/ && ' +
-        'cp -r webapp/src/img api/build/static/webapp/',
+      'copy-webapp-static': 'cp -r webapp/src/audio webapp/src/fonts webapp/src/img api/build/static/webapp/',
       'copy-api-resources': 'cp -r api/src/public/* api/build/static/',
       'copy-api-bowser': 'cp api/node_modules/bowser/bundled.js api/src/public/login/lib-bowser.js',
-      'copy-admin-static':
-        'cp admin/src/templates/index.html api/build/static/admin/ && ' +
-        'mkdir -p api/build/static/admin/fonts/ && ' +
-        'cp admin/node_modules/font-awesome/fonts/* api/build/static/admin/fonts/ && ' +
-        'cp webapp/src/fonts/* api/build/static/admin/fonts/',
-      'enketo-css': 'node node_modules/sass/sass.js webapp/src/css/enketo/enketo.scss api/build/static/webapp/enketo.less --no-source-map',
-      'eslint-sw': `${ESLINT_COMMAND} -c ./.eslintrc build/service-worker.js`,
+      'copy-admin-static': 'cp -r admin/src/templates/index.html admin/node_modules/font-awesome/fonts webapp/src/fonts api/build/static/admin',
+
+
       'build-service-images': {
         cmd: () => buildVersions.SERVICES
           .map(service =>
@@ -109,15 +79,6 @@ module.exports = function(grunt) {
           .map(service => `docker push ${buildVersions.getImageTag(service)}`)
           .join(' && '),
       },
-      'api-dev': {
-        cmd:
-          'TZ=UTC ./node_modules/.bin/nodemon --inspect=0.0.0.0:9229 --ignore "api/build/static" --ignore "api/build/public" --watch api --watch "shared-libs/**/src/**" api/server.js -- --allow-cors',
-      },
-      'sentinel-dev': {
-        cmd:
-          'TZ=UTC ./node_modules/.bin/nodemon --inspect=0.0.0.0:9228 --watch sentinel --watch "shared-libs/**/src/**" sentinel/server.js',
-      },
-
       'npm-ci-modules': 'node scripts/build/cli npmCiModules',
       'check-version': `node scripts/ci/check-versions.js`,
       'test-config-standard': {
@@ -170,6 +131,18 @@ module.exports = function(grunt) {
       'mocha-unit-api': 'npm run mocha-unit-api',
       'mocha-unit-sentinel': 'npm run mocha-unit-sentinel',
       'mocha-integration-api': 'npm run mocha-integration-api',
+      'jsdoc-admin': 'npm run jsdoc-admin',
+      'jsdoc-sentinel': 'npm run jsdoc-sentinel',
+      'jsdoc-api': 'npm run jsdoc-api',
+      'jsdoc-shared-libs': 'npm run jsdoc-shared-libs',
+      'dev-api': 'npm run dev-api',
+      'dev-sentinel': 'npm run dev-sentinel',
+      'enketo-css': 'npm run css-enketo',
+      'eslint-sw': 'npm run lint-service-worker',
+      'less': 'npm run css-admin',
+      'browserify-admin': 'npm run browserify-admin',
+      'uglify-api': 'npm run uglify-api',
+      'uglify-admin': 'npm run uglify-admin',
     }
   });
 
@@ -322,11 +295,11 @@ module.exports = function(grunt) {
 
   grunt.registerTask('dev-api', 'Run api and watch for file changes', [
     'exec:copy-api-bowser',
-    'exec:api-dev',
+    'exec:dev-api',
   ]);
 
   grunt.registerTask('dev-sentinel', 'Run sentinel and watch for file changes', [
-    'exec:sentinel-dev',
+    'exec:dev-sentinel',
   ]);
 
   grunt.registerTask('create-staging-doc', buildUtils.createStagingDoc);
