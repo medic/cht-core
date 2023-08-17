449bca5d89d2a40b67e8d369bcec025c65d55f6b
Diana Barsan <barsan@medic.org>
2023-08-17 22:54:45 +0300

    install everything?

--- tests/scalability/run_suite.sh
+++ tests/scalability/run_suite.sh
@@ -22,6 +22,8 @@ echo installing node
 apt-get install nodejs npm -y
 
 cd cht-core
+npm ci
+cd webapp && npm ci && cd ../
 cd tests/scalability
 echo "Changing config to match url arg"
 node -p "const fs = require('fs');var path = './config.json';var config = JSON.stringify({...require(path), url: '$MEDIC_URL'}, null, 2);fs.writeFileSync(path,config,{encoding:'utf8',flag:'w'});"
