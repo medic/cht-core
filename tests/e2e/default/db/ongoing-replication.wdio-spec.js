a6d09b5409237b56da50b25a49d176bcb9de6908
ngaruko <ngarukob@yahoo.com>
2023-08-22 09:12:22 +0800

    increase timeout for ongoing replication

--- tests/e2e/default/db/ongoing-replication.wdio-spec.js
+++ tests/e2e/default/db/ongoing-replication.wdio-spec.js
@@ -21,7 +21,8 @@ const saveData = async (hierarchy) => {
   await utils.saveDocs(hierarchy.reports);
 };
 
-describe('ongoing replication', () => {
+describe('ongoing replication', function() {
+  this.timeout(4 * 60 * 1000); //sometimes test takes a little longer than original 2 minutes timeout
   before(async () => {
     await sentinelUtils.skipToSeq();
 
@@ -190,8 +191,8 @@ describe('ongoing replication', () => {
     const docId = docs.persons[0]._id;
     //create conflict
     await browser.throttle('offline');
-    await chtDbUtils.updateDoc(docId, { local_update: 1 });
-    await chtDbUtils.updateDoc(docId, { local_update: 2 });
+    await chtDbUtils.updateDoc(docId, { local_update : 1 });
+    await chtDbUtils.updateDoc(docId, { local_update : 2 });
     let serverDoc = await utils.getDoc(docId);
     serverDoc.remote_update = 1;
     await utils.saveDoc(serverDoc);
@@ -205,7 +206,7 @@ describe('ongoing replication', () => {
     expect(localDoc.local_update).to.equal(2);
 
     await browser.throttle('offline');
-    await chtDbUtils.updateDoc(docId, { local_update: 3 });
+    await chtDbUtils.updateDoc(docId, { local_update : 3 });
     serverDoc = await utils.getDoc(docId);
     serverDoc.remote_update = 2;
     await utils.saveDoc(serverDoc);
