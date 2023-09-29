2d9ae57b533968d856d60b3c23bfdff50e55bb27
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2023-09-29 09:28:41 -0400

    Test only one spec

--- tests/e2e/default/wdio.chromedriver.conf.js
+++ tests/e2e/default/wdio.chromedriver.conf.js
@@ -3,16 +3,15 @@ const { suites } = require('./suites');
 
 const defaultMinimumBrowserConfig = Object.assign(wdioBaseConfig.config, {
   //suites,
-  specs: ['/Users/marialorenarodriguezviruel/medic-workspace/cht-core/tests/e2e/default/navigation/navigation.wdio-spec.js'],
+  specs: ['tests/e2e/default/login/login-logout.wdio-spec.js'],
   capabilities: [{
     maxInstances: 1,
     browserName: 'chrome',
     browserVersion: '90.0.4430.72',
     acceptInsecureCerts: true,
     'goog:chromeOptions': {
-      args: [/*'headless',*/ 'disable-gpu', 'deny-permission-prompts', 'ignore-certificate-errors', 'no-sandbox'],
-      //binary: '/usr/bin/google-chrome-stable'
-      binary: '/Applications/Google Chrome 90.app/Contents/MacOS/Google Chrome'
+      args: ['headless', 'disable-gpu', 'deny-permission-prompts', 'ignore-certificate-errors', 'no-sandbox'],
+      binary: '/usr/bin/google-chrome-stable'
     }
   }],
   services: ['chromedriver'],
