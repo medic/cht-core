27a384a71a86d37a53b9255f396aca90f4d6e4a1
tatilepizs <tatiana@medic.org>
2023-10-03 15:33:39 -0600

    pregnancy visit test

--- tests/e2e/cht-form/pregnancy-visit.wdio-spec.js
+++ tests/e2e/cht-form/pregnancy-visit.wdio-spec.js
@@ -1,5 +1,7 @@
 const mockConfig = require('./mock-config');
 const {getFormTitle} = require('@page-objects/default/enketo/generic-form.wdio.page');
+const genericForm = require('@page-objects/default/enketo/generic-form.wdio.page');
+const pregnancyVisitForm = require('@page-objects/default/enketo/pregnancy-visit.wdio.page');
 
 describe('cht-form web component - Pregnancy Visit Form', () => {
 
@@ -7,10 +9,59 @@ describe('cht-form web component - Pregnancy Visit Form', () => {
     const url = await mockConfig.startMockApp('pregnancy_home_visit');
     await browser.url(url);
 
+    let countDangerSigns = 0;
     const title  = await getFormTitle();
     expect(title).to.eq('Pregnancy home visit');
 
-    //In progress
+    await genericForm.nextPage();
+    await pregnancyVisitForm.selectVisitOption();
+    await pregnancyVisitForm.confirmGestationalAge();
+    await genericForm.nextPage();
+    countDangerSigns += await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.VAGINAL_BLEEDING);
+    countDangerSigns += await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.FITS);
+    countDangerSigns += await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.ABDOMINAL_PAIN);
+    countDangerSigns += await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.HEADACHE);
+    countDangerSigns += await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.VERY_PALE);
+    countDangerSigns += await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.FEVER);
+    countDangerSigns += await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.REDUCE_FETAL_MOV);
+    countDangerSigns += await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.BREAKING_OF_WATER);
+    countDangerSigns += await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.EASILY_TIRED);
+    countDangerSigns += await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.SWELLING_HANDS);
+    countDangerSigns += await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.BREATHLESSNESS);
+    await genericForm.nextPage();
+    await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.LLIN);
+    await genericForm.nextPage();
+    await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.IRON_FOLATE);
+    await genericForm.nextPage();
+    await pregnancyVisitForm.selectYesNoOption(pregnancyVisitForm.HIV_TESTED);
+    await genericForm.nextPage();
 
+    const countSummaryDangerSigns = await pregnancyVisitForm.countSummaryDangerSigns();
+    expect(countSummaryDangerSigns).to.equal(countDangerSigns);
+
+    await genericForm.submitForm();
+
+    const data = await $('#submittedData').getText();
+    const jsonObj = JSON.parse(data)[0].fields;
+
+    expect(jsonObj.pregnancy_summary.visit_option).to.equal('yes');
+    expect(jsonObj.pregnancy_summary.g_age_correct).to.equal('yes');
+
+    expect(jsonObj.danger_signs.vaginal_bleeding).to.equal('yes');
+    expect(jsonObj.danger_signs.fits).to.equal('yes');
+    expect(jsonObj.danger_signs.severe_abdominal_pain).to.equal('yes');
+    expect(jsonObj.danger_signs.severe_headache).to.equal('yes');
+    expect(jsonObj.danger_signs.very_pale).to.equal('yes');
+    expect(jsonObj.danger_signs.fever).to.equal('yes');
+    expect(jsonObj.danger_signs.reduced_or_no_fetal_movements).to.equal('yes');
+    expect(jsonObj.danger_signs.breaking_water).to.equal('yes');
+    expect(jsonObj.danger_signs.easily_tired).to.equal('yes');
+    expect(jsonObj.danger_signs.face_hand_swelling).to.equal('yes');
+    expect(jsonObj.danger_signs.breathlessness).to.equal('yes');
+    expect(jsonObj.danger_signs.r_danger_sign_present).to.equal('yes');
+
+    expect(jsonObj.safe_pregnancy_practices.malaria.llin_use).to.equal('yes');
+    expect(jsonObj.safe_pregnancy_practices.iron_folate.iron_folate_daily).to.equal('yes');
+    expect(jsonObj.safe_pregnancy_practices.hiv_status.hiv_tested).to.equal('yes');
   });
 });
--- tests/page-objects/default/enketo/pregnancy-visit.wdio.page.js
+++ tests/page-objects/default/enketo/pregnancy-visit.wdio.page.js
@@ -14,6 +14,28 @@ const formDocument = {
     }
   }
 };
+
+const FORM = 'form[data-form-id="pregnancy_home_visit"]';
+const VAGINAL_BLEEDING = 'input[name="/pregnancy_home_visit/danger_signs/vaginal_bleeding"]';
+const FITS = 'input[name="/pregnancy_home_visit/danger_signs/fits"]';
+const ABDOMINAL_PAIN = 'input[name="/pregnancy_home_visit/danger_signs/severe_abdominal_pain"]';
+const HEADACHE = 'input[name="/pregnancy_home_visit/danger_signs/severe_headache"]';
+const VERY_PALE = 'input[name="/pregnancy_home_visit/danger_signs/very_pale"]';
+const FEVER = 'input[name="/pregnancy_home_visit/danger_signs/fever"]';
+const REDUCE_FETAL_MOV = 'input[name="/pregnancy_home_visit/danger_signs/reduced_or_no_fetal_movements"]';
+const BREAKING_OF_WATER = 'input[name="/pregnancy_home_visit/danger_signs/breaking_water"]';
+const EASILY_TIRED = 'input[name="/pregnancy_home_visit/danger_signs/easily_tired"]';
+const SWELLING_HANDS = 'input[name="/pregnancy_home_visit/danger_signs/face_hand_swelling"]';
+const BREATHLESSNESS = 'input[name="/pregnancy_home_visit/danger_signs/breathlessness"]';
+const LLIN = 'input[name="/pregnancy_home_visit/safe_pregnancy_practices/malaria/llin_use"]';
+const IRON_FOLATE = 'input[name="/pregnancy_home_visit/safe_pregnancy_practices/iron_folate/iron_folate_daily"]';
+const HIV_TESTED = 'input[name="/pregnancy_home_visit/safe_pregnancy_practices/hiv_status/hiv_tested"]';
+
+const visitOption = (value) => $(FORM +
+  ` input[name="/pregnancy_home_visit/pregnancy_summary/visit_option"][value="${value}"]`);
+const gestationalAgeCorrect = (value) => $(FORM +
+  ` input[name="/pregnancy_home_visit/pregnancy_summary/g_age_correct"][value="${value}"]`);
+
 const dangerSignLabel = () =>
   $('label.question.readonly.or-branch.non-select.or-appearance-h1.or-appearance-red > span.question-label');
 const dangerSignSummary = () =>
@@ -43,7 +65,47 @@ const uploadPregnancyVisitForm = async () => {
   await utils.saveDoc(formDocument);
 };
 
+const selectVisitOption = async (value = 'yes') => {
+  const option = await visitOption(value);
+  await option.waitForClickable();
+  await option.click();
+};
+
+const confirmGestationalAge = async (value = 'yes') => {
+  const gestationalAge = await gestationalAgeCorrect(value);
+  await gestationalAge.waitForClickable();
+  await gestationalAge.click();
+};
+
+const selectYesNoOption = async (selector, value = 'yes') => {
+  const element = await $(`${FORM} ${selector}[value="${value}"]`);
+  await element.waitForDisplayed();
+  await element.click();
+  return value === 'yes';
+};
+
+const countSummaryDangerSigns = async () => {
+  return await $$(
+    'section[name="/pregnancy_home_visit/summary"] ' +
+    ':not(label.disabled) span[data-itext-id*="/pregnancy_home_visit/summary/r_danger_sign_"]'
+  ).length;
+};
+
 module.exports = {
+  VAGINAL_BLEEDING,
+  FITS,
+  ABDOMINAL_PAIN,
+  HEADACHE,
+  VERY_PALE,
+  FEVER,
+  REDUCE_FETAL_MOV,
+  BREAKING_OF_WATER,
+  EASILY_TIRED,
+  SWELLING_HANDS,
+  BREATHLESSNESS,
+  LLIN,
+  IRON_FOLATE,
+  HIV_TESTED,
   selectPatient,
   selectDangerSign,
   selectAllDangerSigns,
@@ -51,5 +113,9 @@ module.exports = {
   dangerSignLabel,
   dangerSignSummary,
   followUpMessage,
-  uploadPregnancyVisitForm
+  uploadPregnancyVisitForm,
+  selectVisitOption,
+  confirmGestationalAge,
+  selectYesNoOption,
+  countSummaryDangerSigns,
 };
