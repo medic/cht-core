fa0dcbf71a8bd9bfb575293942a8048d4407a244
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2023-10-10 13:35:10 -0300

    Fix eslint

--- tests/e2e/default/wdio.chromedriver.conf.js
+++ tests/e2e/default/wdio.chromedriver.conf.js
@@ -10,7 +10,8 @@ const defaultMinimumBrowserConfig = Object.assign(wdioBaseConfig.config, {
     browserVersion: '90.0.4430.72',
     acceptInsecureCerts: true,
     'goog:chromeOptions': {
-      args: ['headless', 'disable-gpu', 'deny-permission-prompts', 'ignore-certificate-errors', 'no-sandbox', 'window-size=1200,900'],
+      args: ['headless', 'disable-gpu', 'deny-permission-prompts', 'ignore-certificate-errors', 'no-sandbox',
+        'window-size=1200,900'],
       binary: '/usr/bin/google-chrome-stable'
     }
   }],
