7fae8a76ae367c15ec2e27c1826dc1dc2dfa7679
tatilepizs <tatiana@medic.org>
2023-10-13 10:39:22 -0600

    add individual test for "Death mother and baby" in the delivery form test

--- tests/e2e/cht-form/default/delivery.wdio-spec.js
+++ tests/e2e/cht-form/default/delivery.wdio-spec.js
@@ -4,8 +4,9 @@ const genericForm = require('@page-objects/default/enketo/generic-form.wdio.page
 const deliveryForm = require('@page-objects/default/enketo/delivery.wdio.page');
 
 describe('cht-form web component - Delivery Form', () => {
+  const DATE = moment().format('YYYY-MM-DD');
 
-  it('should submit a delivery', async () => {
+  it('should submit a delivery - Alive mother and baby', async () => {
     const url = await mockConfig.startMockApp('default', 'delivery');
     await browser.url(url);
 
@@ -16,7 +17,6 @@ describe('cht-form web component - Delivery Form', () => {
     });
 
     const BABY_NAME = 'Benja';
-    const BABY_DOB = moment().format('YYYY-MM-DD');
     const BABY_SEX = 'male';
     const title  = await genericForm.getFormTitle();
     expect(title).to.equal('Delivery');
@@ -35,7 +35,7 @@ describe('cht-form web component - Delivery Form', () => {
     await deliveryForm.selectDeliveryOutcomeBabiesAlive('1');
     await deliveryForm.selectDeliveryOutcomeDeliveryPlace('health_facility');
     await deliveryForm.selectDeliveryOutcomeDeliveryMode('vaginal');
-    await deliveryForm.setDeliveryOutcomeDateOfDelivery(BABY_DOB);
+    await deliveryForm.setDeliveryOutcomeDateOfDelivery(DATE);
 
     await genericForm.nextPage();
 
@@ -63,7 +63,7 @@ describe('cht-form web component - Delivery Form', () => {
 
     const summaryInfo = await deliveryForm.getSummaryInfo();
     expect(summaryInfo.womanCondition).to.equal('Alive and well');
-    expect(summaryInfo.deliveryDate).to.equal(BABY_DOB);
+    expect(summaryInfo.deliveryDate).to.equal(DATE);
     expect(summaryInfo.deliveryPlace).to.equal('Health facility');
     expect(summaryInfo.deliveredBabies).to.equal('1');
     expect(summaryInfo.deceasedBabies).to.equal('0');
@@ -83,7 +83,7 @@ describe('cht-form web component - Delivery Form', () => {
     expect(jsonObjMother.pnc_danger_sign_check.r_pnc_danger_sign_present).to.equal('no');
     expect(jsonObjMother.delivery_outcome.babies_delivered).to.equal('1');
     expect(jsonObjMother.delivery_outcome.babies_alive).to.equal('1');
-    expect(jsonObjMother.delivery_outcome.delivery_date).to.equal(BABY_DOB);
+    expect(jsonObjMother.delivery_outcome.delivery_date).to.equal(DATE);
     expect(jsonObjMother.delivery_outcome.delivery_place).to.equal('health_facility');
     expect(jsonObjMother.delivery_outcome.delivery_mode).to.equal('vaginal');
     expect(jsonObjMother.babys_condition.baby_repeat[0].baby_details.baby_condition).to.equal('alive_well');
@@ -93,7 +93,44 @@ describe('cht-form web component - Delivery Form', () => {
 
     expect(jsonObjBaby.name).to.equal(BABY_NAME);
     expect(jsonObjBaby.sex).to.equal(BABY_SEX);
-    expect(jsonObjBaby.date_of_birth).to.equal(BABY_DOB);
+    expect(jsonObjBaby.date_of_birth).to.equal(DATE);
     expect(jsonObjBaby.t_danger_signs_referral_follow_up).to.equal('no');
   });
+
+  it('should submit a delivery - Death mother and baby', async () => {
+    const url = await mockConfig.startMockApp('default', 'delivery');
+    await browser.url(url);
+
+    await browser.execute(() => {
+      const myForm = document.getElementById('myform');
+      myForm.contactSummary = { pregnancy_uuid: 'test UUID' };
+      myForm.content = { contact: { _id: '12345' } };
+      myForm.user = { phone: '+50689999999' };
+    });
+
+    const title  = await genericForm.getFormTitle();
+    expect(title).to.equal('Delivery');
+
+    await deliveryForm.selectDeliveryConditionWomanOutcome('deceased');
+    await genericForm.nextPage();
+    await deliveryForm.fillWomanDeathInformation({
+      date: DATE,
+      place: 'health_facility',
+      notes: 'Test notes - Mother\'s death'
+    });
+    await genericForm.nextPage();
+    await genericForm.nextPage();
+
+    await genericForm.submitForm();
+
+    const data = await $('#submittedData').getText();
+    const jsonObj = JSON.parse(data)[1];
+
+    expect(jsonObj.form).to.equal('death_report');
+    expect(jsonObj.from).to.equal('+50689999999');
+    expect(jsonObj.fields.death_details.date_of_death).to.equal(DATE);
+    expect(jsonObj.fields.death_details.place_of_death).to.equal('health_facility');
+    expect(jsonObj.fields.death_details.death_information).to.equal('Test notes - Mother\'s death');
+  });
+
 });
--- tests/page-objects/default/enketo/delivery.wdio.page.js
+++ tests/page-objects/default/enketo/delivery.wdio.page.js
@@ -1,3 +1,5 @@
+const moment = require('moment/moment');
+
 const deliveryConditionWomanOutcomeField = (value) =>
   $(`input[type="radio"][name="/delivery/condition/woman_outcome"][value="${value}"]`);
 const deliveryPostnatalDangerFeverField = (value) =>
@@ -85,6 +87,13 @@ const sumDeliveredBabies = () => $(SUMMARY_SECTION +
 const sumDeceasedBabies = () => $(SUMMARY_SECTION +
   ' span[data-value=" /delivery/delivery_outcome/babies_deceased_num "]');
 const sumPncVisits = () => $(`${SUMMARY_SECTION} span[data-itext-id="/delivery/summary/r_pnc_visit_none:label"]`);
+
+const womanDeathDate = () => $('section[name="/delivery/death_info_woman"] .widget.date .input-small');
+const womanDeathPlace = (value) =>
+  $(`input[data-name="/delivery/death_info_woman/woman_death_place"][value="${value}"]`);
+const womanDeliveredBabies = (value) =>
+  $(`input[data-name="/delivery/death_info_woman/woman_death_birth"][value="${value}"]`);
+const womanDeathNote = () => $('input[name="/delivery/death_info_woman/woman_death_add_notes"]');
 
 const selectDeliveryConditionWomanOutcome = async (value) => {
   const womanOutcome = await deliveryConditionWomanOutcomeField(value);
@@ -273,6 +282,18 @@ const getSummaryInfo = async () => {
   };
 };
 
+const fillWomanDeathInformation = async ({
+  date: dateValue = moment().format('YYYY-MM-DD'),
+  place: placeValue = 'health_facility',
+  deliveredBabies: deliveredBabiesValue = 'no',
+  notes: notesValue = 'Test notes - Mother\'s death '
+} = {}) => {
+  await womanDeathPlace(placeValue).click();
+  await womanDeliveredBabies(deliveredBabiesValue).click();
+  await womanDeathNote().setValue(notesValue);
+  await womanDeathDate().setValue(dateValue);
+};
+
 module.exports = {
   selectDeliveryConditionWomanOutcome,
   selectDeliveryPostnatalDangerSignsFever,
@@ -304,4 +325,5 @@ module.exports = {
   selectDeliveryBabyBlueSkin,
   selectDeliveryPncVisits,
   getSummaryInfo,
+  fillWomanDeathInformation,
 };
