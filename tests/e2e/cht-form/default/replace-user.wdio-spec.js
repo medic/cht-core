c61ce87c6968a6b7ec560e78431f2160237a7fc3
tatilepizs <tatiana@medic.org>
2023-10-13 15:37:41 -0600

    add replace-user test

---
+++ tests/e2e/cht-form/default/replace-user.wdio-spec.js
@@ -0,0 +1,49 @@
+const mockConfig = require('../mock-config');
+const genericForm = require('@page-objects/default/enketo/generic-form.wdio.page');
+const replaceUserForm = require('@page-objects/default/enketo/replace-user.wdio.page');
+
+describe('cht-form web component - Replace User Form', () => {
+  it('should submit the replace user form', async () => {
+    const url = await mockConfig.startMockApp('default', 'replace_user');
+    await browser.url(url);
+
+    await browser.execute(() => {
+      const myForm = document.getElementById('myform');
+      myForm.content = { contact: { _id: '12345', role: 'chw' } };
+      myForm.user = { phone: '+50689999999' };
+    });
+
+    const title  = await genericForm.getFormTitle();
+    expect(title).to.equal('Replace User');
+
+    await replaceUserForm.selectAdminCode('1234');
+    await genericForm.nextPage();
+    await replaceUserForm.selectContactFullName('Replacement User');
+    await replaceUserForm.selectContactSex(replaceUserForm.SEX.female);
+    await replaceUserForm.selectContactDobUnknown();
+    await replaceUserForm.selectContactAgeYears(22);
+    await genericForm.nextPage();
+
+    const { phone, contactName } = await replaceUserForm.getWarningMsgDetails();
+    expect(phone).to.equal('+50689999999');
+    expect(contactName).to.equal('Replacement User');
+
+    await genericForm.submitForm();
+
+    const data = await $('#submittedData').getText();
+    const jsonObjContact = JSON.parse(data)[0].fields;
+    const jsonObjNewContact = JSON.parse(data)[1];
+
+    expect(jsonObjContact.patient_id).to.equal('12345');
+    expect(jsonObjContact.user_phone).to.equal('+50689999999');
+    expect(jsonObjContact.contact_role).to.equal('chw');
+    expect(jsonObjContact.new_contact_name).to.equal('Replacement User');
+    expect(jsonObjContact.new_contact_phone).to.equal('+50689999999');
+
+    expect(jsonObjNewContact.name).to.equal('Replacement User');
+    expect(jsonObjNewContact.sex).to.equal('female');
+    expect(jsonObjNewContact.phone).to.equal('+50689999999');
+    expect(jsonObjNewContact.role).to.equal('chw');
+  });
+
+});
