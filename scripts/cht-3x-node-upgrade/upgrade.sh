2e2e5160c10ea0ebd8d64db73eaefc8ceb240045
mrjones-plip <mrjones-plip@plip.com>
2023-10-20 13:32:26 -0700

    Update CHT 3.x node upgrade script to support both node 8 and 12

--- scripts/cht-3x-node-upgrade/upgrade.sh
+++ scripts/cht-3x-node-upgrade/upgrade.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
 #####################################################
-# Upgrade node from 8 -> 12                         #
+# Upgrade node from 8 to latest 8 or 12             #
+# Defaults to version 12, but you can               #
+#     pass "8" or "12" as arguments                 #
 #                                                   #
 # Only for use on CHT 3.x instances that have       #
 # TLS issues on outbound push                       #
 #                                                   #
 # see https://github.com/medic/cht-core/issues/7957 #
+# see https://github.com/medic/config-muso/issues/1016 #
 #                                                   #
 #####################################################
 
-echo "Start...Node version installed:"
-/srv/software/medic-core/v2.1.1/x64/bin/node -v
+set -e
 
+NODE_VERSION="${1:-12}"
+
+if( "$NODE_VERSION" = "12" );then
+  NODE_VERSION="12.22.12"
+else
+  NODE_VERSION="8.17.0"
+fi
+
+echo "Start to install latest Node $NODE_VERSION..."
+
+echo "Current Node version installed before upgrade:"
+/srv/software/medic-core/v2.1.1/x64/bin/node -v
 
 echo ""
 echo "Installing zip..."
@@ -34,12 +48,12 @@ echo ""
 echo "Downloading node and installing..."
 echo ""
 
-curl -so /srv/node.12.22.12.zip https://raw.githubusercontent.com/medic/cht-core/master/scripts/cht-3x-node-upgrade/node.12.22.12.zip
+curl -so /srv/node.$NODE_VERSION.zip https://raw.githubusercontent.com/medic/cht-core/master/scripts/cht-3x-node-upgrade/node.$NODE_VERSION.zip
 
-unzip /srv/node.12.22.12.zip
+unzip /srv/node.$NODE_VERSION.zip
 chmod +x node
 mv /srv/node /srv/software/medic-core/v2.1.1/x64/bin/node
-rm /srv/node.12.22.12.zip
+rm /srv/node.$NODE_VERSION.zip
 
 
 echo ""
