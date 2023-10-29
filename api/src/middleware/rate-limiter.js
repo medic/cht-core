c618e6a0bb2204cd05940aa286374991e580e807
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2023-10-30 10:27:41 +1300

    separate requests from login failures

--- api/src/middleware/rate-limiter.js
+++ api/src/middleware/rate-limiter.js
@@ -1,20 +1,38 @@
 const { RateLimiterMemory } = require('rate-limiter-flexible');
 
-const rateLimiter = new RateLimiterMemory({
-  keyPrefix: 'middleware',
+const failedLoginLimit = new RateLimiterMemory({
+  keyPrefix: 'failed-login',
   points: 10, // 10 requests
-  duration: 10, // per 1 second by IP
+  duration: 10, // per 10 seconds
+});
+
+const requestLimit = new RateLimiterMemory({
+  keyPrefix: 'request',
+  points: 10, // 10 requests
+  duration: 1, // per 1 second
 });
 
 const rateLimiterMiddleware = (req, res, next) => {
-  // TODO: also consume given username
-  rateLimiter.consume(req.ip)
-    .then(() => next())
-    .catch(() => res.status(429).send('Too Many Requests'));
+
+  requestLimit.consume(req.ip)
+    .then(() => failedLoginLimit.get(req.ip))
+    .then(limit => {
+      if (limit && limit.remainingPoints <= 0) {
+        throw new Error('Too many failed login attempts');
+      }
+      next();
+    })
+    .catch(() => {
+      return res.status(429).send('Too Many Requests');
+    });
 
   res.on('finish', () => {
-    if (res.statusCode !== 401 && res.statusCode !== 429 && !res.unauthorized) {
-      rateLimiter.reward(req.ip);
+    if (res.statusCode === 401 || res.statusCode === 429 || res.unauthorized) {
+      // TODO: also consume given username
+      failedLoginLimit.consume(req.ip)
+        .catch(() => {
+          // ignore - cannot set headers as they're already set
+        });
     }
   });
 
