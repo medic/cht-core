444823270e7e1387ebf1e4c24cafa3a54f7f598c
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2023-10-31 17:33:13 -0300

    Use browserVersion to choose the method to get text from a span element

--- tests/page-objects/standard/enketo/delivery.wdio.page.js
+++ tests/page-objects/standard/enketo/delivery.wdio.page.js
@@ -1,6 +1,7 @@
-const OUTCOME = {liveBirth: 'healthy', stillBirth: 'still_birth', miscarriage: 'miscarriage'};
-const LOCATION = {facility: 'f', homeAttendant: 's', homeNoAttendant: 'ns'};
-
+const OUTCOME = { liveBirth: 'healthy', stillBirth: 'still_birth', miscarriage: 'miscarriage' };
+const LOCATION = { facility: 'f', homeAttendant: 's', homeNoAttendant: 'ns' };
+const MINIMUM_BROWSER_VERSION = '90';
+const ACTIVE_SPAN = '.option-label.active';
 const FORM = 'form[data-form-id="delivery"]';
 const pregnancyOutcome = (value) => $(`${FORM} ` +
   `input[name="/delivery/group_delivery_summary/g_pregnancy_outcome"][value="${value}"`);
@@ -9,10 +10,10 @@ const pregnancyOutcomeLabel = (value) => $(`${FORM} ` +
 const deliveryLocation = (value) => $(`${FORM} ` +
   `input[name="/delivery/group_delivery_summary/g_delivery_code"][value="${value}"`);
 const deliveryLocationLabel = (value) => $(`${FORM} ` +
-  `span[data-itext-id="/delivery/group_delivery_summary/g_delivery_code/${value}:label"]`);
+  `span[data-itext-id="/delivery/group_delivery_summary/g_delivery_code/${value}:label"]` + ACTIVE_SPAN);
 const deliveryDate = () => $(`${FORM} div.widget.date input`);
 const smsNote = () => $(`${FORM} textarea[name="/delivery/group_note/g_chw_sms"]`);
-const outcomeSummary = () => $(`${FORM} ` + 
+const outcomeSummary = () => $(`${FORM} ` +
   `span[data-value=" /delivery/group_delivery_summary/display_delivery_outcome "]`);
 const locationSummary = () => $(`${FORM} span[data-value=" /delivery/group_summary/r_delivery_location "]`);
 const followUpSMS = () => $(`${FORM} span[data-value=" /delivery/chw_sms "]`);
@@ -28,10 +29,12 @@ const selectDeliveryLocation = async (value = LOCATION.facility) => {
   const location = await deliveryLocation(value);
   await location.waitForDisplayed();
   await location.click();
-  return await deliveryLocationLabel(value).getText();
+  const locationLabel = driver.capabilities.browserVersion.split('.').shift() === MINIMUM_BROWSER_VERSION ? await (await deliveryLocationLabel(value)).getAttribute("innerHTML")
+    : await (await deliveryLocationLabel(value)).getText();
+  return locationLabel;
 };
 
-const setDeliveryDate =  async (value) => {
+const setDeliveryDate = async (value) => {
   const date = await deliveryDate();
   await date.waitForDisplayed();
   await date.setValue(value);
