725e80a73c8b63a456e95d28d0b325ac77f1634e
latin-panda <66472237+latin-panda@users.noreply.github.com>
2023-11-06 09:16:08 +0700

    search by barcode

--- webapp/src/ts/components/search-bar/search-bar.component.ts
+++ webapp/src/ts/components/search-bar/search-bar.component.ts
@@ -7,14 +7,21 @@ import {
   AfterViewInit,
   Output,
   ViewChild,
+  Inject,
 } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { combineLatest, Subscription } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
 
 import { Selectors } from '@mm-selectors/index';
 import { FreetextFilterComponent } from '@mm-components/filters/freetext-filter/freetext-filter.component';
 import { ResponsiveService } from '@mm-services/responsive.service';
 import { SearchFiltersService } from '@mm-services/search-filters.service';
+import { AuthService } from '@mm-services/auth.service';
+import { SessionService } from '@mm-services/session.service';
+import { GlobalActions } from '@mm-actions/global';
+
+export const CAN_USE_BARCODE_SCANNER = 'can_use_barcode_scanner';
 
 @Component({
   selector: 'mm-search-bar',
@@ -30,6 +37,10 @@ export class SearchBarComponent implements AfterContentInit, AfterViewInit, OnDe
   @Output() toggleFilter: EventEmitter<any> = new EventEmitter();
   @Output() search: EventEmitter<any> = new EventEmitter();
 
+  private canUseBarcodeScanner = false;
+  private globalAction: GlobalActions;
+  private windowRef;
+  private barcodeDetector;
   private filters;
   subscription: Subscription = new Subscription();
   activeFilters: number = 0;
@@ -42,14 +53,26 @@ export class SearchBarComponent implements AfterContentInit, AfterViewInit, OnDe
     private store: Store,
     private responsiveService: ResponsiveService,
     private searchFiltersService: SearchFiltersService,
-  ) { }
+    private authService: AuthService,
+    private sessionService: SessionService,
+    @Inject(DOCUMENT) private document:Document,
+  ) {
+    this.windowRef = this.document.defaultView;
+    this.globalAction = new GlobalActions(store);
+  }
 
   ngAfterContentInit() {
     this.subscribeToStore();
   }
 
-  ngAfterViewInit() {
+  async ngAfterViewInit() {
+    await this.checkPermissions();
     this.searchFiltersService.init(this.freetextFilter);
+    await this.initBarcodeScanner();
+  }
+
+  private async checkPermissions() {
+    this.canUseBarcodeScanner = !this.sessionService.isAdmin() && await this.authService.has(CAN_USE_BARCODE_SCANNER);
   }
 
   private subscribeToStore() {
@@ -67,12 +90,37 @@ export class SearchBarComponent implements AfterContentInit, AfterViewInit, OnDe
     this.subscription.add(subscription);
   }
 
+  private async initBarcodeScanner() {
+    if (!this.isBarcodeScannerAvailable()) {
+      return;
+    }
+
+    const barcodeTypes = await this.windowRef.BarcodeDetector.getSupportedFormats();
+    console.info(`Supported barcode formats: ${barcodeTypes.join(', ')}`);
+    this.barcodeDetector = new this.windowRef.BarcodeDetector({ formats: barcodeTypes });
+
+    const imageHolder = this.windowRef.document.createElement('img');
+    imageHolder.addEventListener('load', () => this.scanBarcode(imageHolder));
+
+    const input = this.windowRef.document.getElementById('barcode-scanner-input');
+    input.addEventListener('change', () => {
+      if (!input.files) {
+        return;
+      }
+      const reader = new FileReader();
+      reader.addEventListener('load', event => imageHolder.src = event?.target?.result);
+      reader.readAsDataURL(input.files[0]);
+      input.value = '';
+    });
+  }
+
   clear() {
     if (this.disabled) {
       return;
     }
     this.freetextFilter.clear(true);
     this.toggleMobileSearch(false);
+    this.initBarcodeScanner();
   }
 
   toggleMobileSearch(forcedValue?) {
@@ -98,6 +146,32 @@ export class SearchBarComponent implements AfterContentInit, AfterViewInit, OnDe
 
   showClearIcon() {
     return this.openSearch || !!this.filters?.search;
+  }
+
+  isBarcodeScannerAvailable() {
+    if (!this.canUseBarcodeScanner) {
+      return false;
+    }
+
+    if (!('BarcodeDetector' in window)) {
+      const message = 'Barcode scanner is not supported by this browser.';
+      this.globalAction.setSnackbarContent(message);
+      console.warn(message);
+      return false;
+    }
+
+    return true;
+  }
+
+  private async scanBarcode(imageHolder) {
+    try {
+      const barcodes = await this.barcodeDetector.detect(imageHolder);
+      barcodes.length && this.searchFiltersService.freetextSearch(barcodes[0].rawValue);
+    } catch (error) {
+      const message = 'Cannot read barcode';
+      this.globalAction.setSnackbarContent(message);
+      console.error(message, error);
+    }
   }
 
   ngOnDestroy() {
