5b71f470228af298d583867aab09856634a1d4d2
Joshua Kuestersteffen <jkuester@kuester7.com>
2023-11-09 10:06:13 -0600

    Update pages objects to avoid multi-line implicit returns

--- tests/page-objects/default/enketo/pregnancy-visit.wdio.page.js
+++ tests/page-objects/default/enketo/pregnancy-visit.wdio.page.js
@@ -20,15 +20,17 @@ const LLIN = 'input[name="/pregnancy_home_visit/safe_pregnancy_practices/malaria
 const IRON_FOLATE = 'input[name="/pregnancy_home_visit/safe_pregnancy_practices/iron_folate/iron_folate_daily"]';
 const HIV_TESTED = 'input[name="/pregnancy_home_visit/safe_pregnancy_practices/hiv_status/hiv_tested"]';
 
-const visitOption = (value) =>
-  $(`${FORM} input[name="/pregnancy_home_visit/pregnancy_summary/visit_option"][value="${value}"]`);
-const gestationalAgeCorrect = (value) =>
-  $(`${FORM} input[name="/pregnancy_home_visit/pregnancy_summary/g_age_correct"][value="${value}"]`);
+const visitOption = (value) => {
+  return $(`${FORM} input[name="/pregnancy_home_visit/pregnancy_summary/visit_option"][value="${value}"]`);
+};
+const gestationalAgeCorrect = (value) => {
+  return $(`${FORM} input[name="/pregnancy_home_visit/pregnancy_summary/g_age_correct"][value="${value}"]`);
+};
 
-const dangerSignLabel = () =>
-  $('label.question.readonly.or-branch.non-select.or-appearance-h1.or-appearance-red > span.question-label');
-const dangerSignSummary = () =>
-  $$('label.question.readonly.or-branch.non-select.or-appearance-li');
+const dangerSignLabel = () => {
+  return $('label.question.readonly.or-branch.non-select.or-appearance-h1.or-appearance-red > span.question-label');
+};
+const dangerSignSummary = () => $$('label.question.readonly.or-branch.non-select.or-appearance-li');
 const followUpMessage = () => $('[data-value=" /pregnancy_visit/chw_sms "]');
 
 const selectPatient = async (patientName) => {
