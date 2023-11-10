1cb67ba852b11f86612a6966a6671cb75b7191f5
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2023-11-10 13:53:28 -0300

    Move goToReports to afterEach hook

--- tests/e2e/default/navigation/hamburger-menu.wdio-spec.js
+++ tests/e2e/default/navigation/hamburger-menu.wdio-spec.js
@@ -2,29 +2,25 @@ const commonPage = require('@page-objects/default/common/common.wdio.page');
 const loginPage = require('@page-objects/default/login/login.wdio.page');
 
 describe('Hamburger Menu tests', async () => {
-  /*before(async () => {
+  before(async () => {
     await loginPage.cookieLogin();
-  });*/
+  });
 
-  /*beforeEach(async () => {
+  afterEach(async () => {
     await commonPage.goToReports();
-  });*/
+  });
 
   it('should open About', async () => {
-    await loginPage.cookieLogin();
-    await commonPage.goToReports();
     await commonPage.openHamburgerMenu();
     await commonPage.openAboutMenu();
   });
 
   it('should open User settings', async () => {
-    await commonPage.goToReports();
     await commonPage.openHamburgerMenu();
     await commonPage.openUserSettingsAndFetchProperties();
   });
 
   it('should open Report bug', async () => {
-    await commonPage.goToReports();
     await commonPage.openHamburgerMenu();
     const actualProperties = await commonPage.openReportBugAndFetchProperties();
     expect(actualProperties.header).to.equal('Report bug');
@@ -32,7 +28,6 @@ describe('Hamburger Menu tests', async () => {
   });
 
   it('should open Configuration app', async () => {
-    await commonPage.goToReports();
     await commonPage.openHamburgerMenu();
     await commonPage.openAppManagement();
   });
