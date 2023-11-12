7140b79fe69e91c73dc015c0622380c52f73cf9e
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2023-11-12 13:39:42 -0300

    Add close reload modal after go to Abour. As per screenshot the reload modal is blocking the version

--- tests/e2e/upgrade/upgrade.wdio-spec.js
+++ tests/e2e/upgrade/upgrade.wdio-spec.js
@@ -113,6 +113,7 @@ describe('Performing an upgrade', () => {
     await commonPage.closeReloadModal(true);
 
     await commonPage.goToAboutPage();
+    await commonPage.closeReloadModal(true);
     expect(await aboutPage.getVersion()).to.include(TAG ? TAG : `${BRANCH} (`);
     await commonPage.logout();
   });
