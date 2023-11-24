13c3ac37ed6518b3760204f028ff73836d87a7f0
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2023-11-24 17:11:44 -0300

    Trying to activate search just changing input value

---
+++ tests/e2e/default-mobile/contacts/barcode-search-contacts.wdio-spec.js
@@ -0,0 +1,33 @@
+const searchPage = require('@page-objects/default-mobile/search/search.wdio.page.js');
+const loginPage = require('@page-objects/default/login/login.wdio.page');
+const utils = require('@utils');
+const contactPage = require('@page-objects/default/contacts/contacts.wdio.page');
+const commonPage = require('@page-objects/default/common/common.wdio.page');
+const placeFactory = require('@factories/cht/contacts/place');
+const personFactory = require('@factories/cht/contacts/person');
+const userFactory = require('@factories/cht/users/users');
+const path = require('path');
+
+const places = placeFactory.generateHierarchy();
+const healthCenter = places.get('health_center');
+const offlineUser = userFactory.build({ place: healthCenter._id, roles: ['chw'] });
+const person = personFactory.build({ parent: { _id: healthCenter._id, parent: healthCenter.parent } });
+const barcodeImagePath = path.join(__dirname, '/images/123456.jpg');
+
+describe('Test Contact Search with Barcode Scanner', async () => {
+  before(async () => {
+    person._id = '123456';
+    await utils.saveDocs([...places.values(), person]);
+    await utils.createUsers([offlineUser]);
+    const canUseBarcodeScannerPermission = ['can_use_barcode_scanner'];
+    await utils.updatePermissions(offlineUser.roles, canUseBarcodeScannerPermission, [], false);
+    await loginPage.login(offlineUser);
+    await commonPage.waitForPageLoaded();
+    await commonPage.goToPeople();
+  });
+
+  it('Search should display result', async () => {
+    await searchPage.performBarcodeSearch(barcodeImagePath);
+
+  });
+});
--- tests/page-objects/default-mobile/search/search.wdio.page.js
+++ tests/page-objects/default-mobile/search/search.wdio.page.js
@@ -1,6 +1,8 @@
 const searchPageDefault = require('@page-objects/default/search/search.wdio.page');
 
 const openSearchBox = () => $('.mm-search-bar-container .search-bar-left-icon .fa-search');
+const barcodeSearchBox = () => $('.fa-qrcode');
+const barcodeSearchInput = () => $('.barcode-scanner-input');
 
 const performSearch = async (term) => {
   await (await openSearchBox()).waitForClickable();
@@ -8,7 +10,27 @@ const performSearch = async (term) => {
   await searchPageDefault.performSearch(term);
 };
 
+const openBarcodeSearchBox = async () => {
+  await (await barcodeSearchBox()).waitForClickable();
+  await (await barcodeSearchBox()).click();
+};
+
+const performBarcodeSearch = async (barcodeImagePath) => {
+  //
+  const remoteFilePath = await browser.uploadFile(barcodeImagePath);
+  await (await barcodeSearchInput()).addValue(remoteFilePath);
+  //$('.barcode-scanner-input').submit();
+  //await (await barcodeSearchInput()).click();
+  //await (await searchBox()).clearValue();
+  //await browser.keys(ENTER);
+  // After search is triggered there can be a slight delay before the AJAX call
+  // is made and the search spinner shows up hence we just need to wait for a bit before moving forward
+  await browser.pause(1000);
+};
+
+
 module.exports = {
   searchPageDefault,
   performSearch,
+  performBarcodeSearch,
 };
