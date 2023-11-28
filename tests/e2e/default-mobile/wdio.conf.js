0aa9383d732f5cf822bb71d9c04a91f920b7db59
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2023-11-28 18:26:43 -0300

    Use emulateDevice instead of setWindowSize

--- tests/e2e/default-mobile/wdio.conf.js
+++ tests/e2e/default-mobile/wdio.conf.js
@@ -7,16 +7,26 @@ chai.use(require('chai-exclude'));
 exports.config = Object.assign(wdioBaseConfig.config, {
   suites: {
     all: [
-      './**/*.wdio-spec.js',
+      '/Users/marialorenarodriguezviruel/medic-workspace/cht-core/tests/e2e/default-mobile/contacts/barcode-search-contacts.wdio-spec.js',
       [
         '../default/login/login-logout.wdio-spec.js',
         '../default/navigation/navigation.wdio-spec.js',
         '../default/navigation/hamburger-menu.wdio-spec.js',
       ],
-    ]},
+    ]
+  },
   beforeSuite: async () => {
     // We tried the browser.emulateDevice('...') function but it's not stable enough,
     // it looses the mobile view and switches back to desktop
-    await browser.setWindowSize(450, 700);
+    //await browser.setWindowSize(450, 700);
+    await browser.emulateDevice({
+      viewport: {
+        width: 600,
+        height: 960,
+        isMobile: true,
+        hasTouch: true,
+      },
+      userAgent: 'Chrome/latest'
+    });
   },
 });
