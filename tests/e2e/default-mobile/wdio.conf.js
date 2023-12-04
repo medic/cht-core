01eb63cdae1709c2ce112937525b5a56cc364a51
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2023-12-04 10:04:00 -0300

    Use chromeOptions to enable mobile emulation

--- tests/e2e/default-mobile/wdio.conf.js
+++ tests/e2e/default-mobile/wdio.conf.js
@@ -2,7 +2,7 @@ const wdioBaseConfig = require('../wdio.conf');
 
 const chai = require('chai');
 chai.use(require('chai-exclude'));
-
+const DEBUG = process.env.DEBUG;
 const ANDROID_VERSION = '13';
 const MOBILE_CHROME_VERSION = '118.0.5993.112';
 
@@ -18,20 +18,23 @@ exports.config = Object.assign(wdioBaseConfig.config, {
       ],
     ]
   },
-  beforeSuite: async () => {
-    // We tried the browser.emulateDevice('...') function but it's not stable enough,
-    // it looses the mobile view and switches back to desktop.
-    // Adding to the comment above, it loses the mobile view when a test fails.
-    // It may be better to use beforeHook instead of beforeSuite so it can set the capability before each test.
-    await browser.emulateDevice({
-      viewport: {
-        width: 600,
-        height: 960,
-        isMobile: true,
-        hasTouch: true,
+  capabilities: [{
+    maxInstances: 1,
+    browserName: 'chrome',
+    acceptInsecureCerts: true,
+    'goog:chromeOptions': {
+      args: DEBUG ? ['disable-gpu', 'deny-permission-prompts', 'ignore-certificate-errors'] :
+        ['headless', 'disable-gpu', 'deny-permission-prompts', 'ignore-certificate-errors'],
+      mobileEmulation: {
+        userAgent: `Mozilla/5.0 (Linux; Android ${ANDROID_VERSION}; IN2010) AppleWebKit/537.36 (KHTML, like Gecko) ` +
+          `Chrome/${MOBILE_CHROME_VERSION} Mobile Safari/537.36`,
+        deviceMetrics: {
+          "mobile": true,
+          "touch": true,
+          "width": 600,
+          "height": 960
+        },
       },
-      userAgent: `Mozilla/5.0 (Linux; Android ${ANDROID_VERSION}; IN2010) AppleWebKit/537.36 (KHTML, like Gecko) ` +
-        `Chrome/${MOBILE_CHROME_VERSION} Mobile Safari/537.36`
-    });
-  },
+    }
+  }]
 });
