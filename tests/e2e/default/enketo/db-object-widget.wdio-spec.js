4af3b055f35b3242d34a448633ec263ad9f41404
latin-panda <66472237+latin-panda@users.noreply.github.com>
2023-12-18 14:24:09 +0700

    e2e no flaky

--- tests/e2e/default/enketo/db-object-widget.wdio-spec.js
+++ tests/e2e/default/enketo/db-object-widget.wdio-spec.js
@@ -1,14 +1,13 @@
-const moment = require('moment');
 const fs = require('fs');
 
 const utils = require('@utils');
-const commonPage = require('@page-objects/default/common/common.wdio.page');
-const loginPage = require('@page-objects/default/login/login.wdio.page');
 const userFactory = require('@factories/cht/users/users');
 const placeFactory = require('@factories/cht/contacts/place');
 const personFactory = require('@factories/cht/contacts/person');
-const contactPage = require('@page-objects/default/contacts/contacts.wdio.page');
+const commonPage = require('@page-objects/default/common/common.wdio.page');
+const loginPage = require('@page-objects/default/login/login.wdio.page');
 const genericForm = require('@page-objects/default/enketo/generic-form.wdio.page');
+const reportsPage = require('@page-objects/default/reports/reports.wdio.page');
 
 describe('DB Object Widget', () => {
   const formId = 'db-object-widget';
@@ -38,8 +37,8 @@ describe('DB Object Widget', () => {
   });
 
   const offlineUser = userFactory.build({ place: districtHospital._id, roles: [ 'chw' ] });
-  const personArea1 = personFactory.build({ parent: {_id: area1._id, parent: area1.parent} });
-  const personArea2 = personFactory.build({name: 'Patricio', parent: {_id: area2._id, parent: area2.parent} });
+  const personArea1 = personFactory.build({ parent: { _id: area1._id, parent: area1.parent } });
+  const personArea2 = personFactory.build({ name: 'Patricio', parent: { _id: area2._id, parent: area2.parent } });
 
   before(async () => {
     await utils.saveDocs([ ...places.values(), area2, personArea1, personArea2, formDocument ]);
@@ -50,9 +49,27 @@ describe('DB Object Widget', () => {
   it('should display only the contacts from the parent contact', async () => {
     await commonPage.goToPeople(area1._id);
     await commonPage.openFastActionReport(formId);
-    await browser.debug();
 
-    expect('hola').to.equal('Health facility');
+    const sameParent = await genericForm.getDBObjectWidgetValues('/db_object_form/people/person_test_same_parent');
+    expect(sameParent.length).to.equal(1);
+    expect(sameParent[0].name).to.equal('Mary Smith');
+    await sameParent[0].click();
+
+    const allContacts = await genericForm.getDBObjectWidgetValues('/db_object_form/people/person_test_all');
+    expect(allContacts.length).to.equal(3);
+    expect(allContacts[0].name).to.equal('Mary Smith');
+    expect(allContacts[1].name).to.equal('OfflineUser');
+    expect(allContacts[2].name).to.equal('Patricio');
+    await allContacts[1].click();
+
+    await genericForm.submitForm();
+    await commonPage.waitForPageLoaded();
+    await commonPage.goToReports();
+
+    const firstReport = await reportsPage.getListReportInfo(await reportsPage.firstReport());
+
+    expect(firstReport.heading).to.equal('OfflineUser');
+    expect(firstReport.form).to.equal('Form db-object-widget');
   });
 
 });
