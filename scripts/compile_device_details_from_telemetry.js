ea99e15d833a7af3066eec73ee544072a98059bb
m5r <mokht@rmi.al>
2023-12-29 08:49:18 +0100

    create view to index telemetry entries with device infos

--- scripts/compile_device_details_from_telemetry.js
+++ scripts/compile_device_details_from_telemetry.js
@@ -1,12 +1,30 @@
 const db = require('../api/src/db');
 
+const sortObjectKeys = (obj, compareFn = (a, b) => a.localeCompare(b)) => Object.keys(obj).sort(compareFn).reduce((result, key) => {
+  result[key] = obj[key];
+  return result;
+}, {});
+
 (async () => {
   console.time('query allDocs');
-  // TODO: make a view? an index? call .allDocs() and filter here?
-  const telemetry = (await db.medicUsersMeta.find({
-    selector: { type: 'telemetry' },
-    limit: 50000,
-  })).docs;
+  const telemetry = await db.medicUsersMeta.query('users-meta/telemetry', { include_docs: true });
+
+  // let telemetry = await db.medicUsersMeta.allDocs({ include_docs: true });
+  // telemetry = telemetry.rows.filter(e => e.doc.type === 'telemetry').map(e => e.doc);
+  // console.log("telemetry", telemetry.length);
+
+  // await db.medicUsersMeta.createIndex({
+  //   index: {
+  //     fields: ['type'],
+  //   },
+  // });
+
+  // const telemetry = await db.medicUsersMeta.find({
+  //   limit: 50000,
+  //   selector: { type: 'telemetry' },
+  // });
+  // console.log("telemetry", telemetry.docs.length);
+
   console.timeEnd('query allDocs');
 
   // used to see which user is still using an outdated browser/app
@@ -15,11 +33,11 @@ const db = require('../api/src/db');
   // used to get a bird's eye view of the deployment, how many devices have outdated software
   const versionsCounts = {
     android: {},
-    apk: {},
+    app: {},
     webview: {},
   };
   console.time('compute device stuff');
-  telemetry.forEach(entry => {
+  telemetry.rows.forEach(({ doc: entry }) => {
     if (!entry.device && !entry.metadata) {
       return;
     }
@@ -55,12 +73,23 @@ const db = require('../api/src/db');
       userByDeviceId[entry.metadata.user].push(deviceMetas);
     } else {
       if (device.date < date) {
-        console.log(`found a more recent entry for user ${entry.metadata.user} & device ${deviceId} - ${device.date} < ${date}`);
+        // console.log(`found a more recent entry for user ${entry.metadata.user} & device ${deviceId} - ${device.date} < ${date}`);
         // if we got a more recent entry, replace the old one we had
         userByDeviceId[entry.metadata.user].splice(deviceIndex, 1, deviceMetas);
       }
     }
   });
-
   console.timeEnd('compute device stuff');
+
+  versionsCounts.android = sortObjectKeys(versionsCounts.android);
+  versionsCounts.app = sortObjectKeys(versionsCounts.app);
+  versionsCounts.webview = sortObjectKeys(versionsCounts.webview, (a, b) => {
+    if (a === undefined) {
+      return -1;
+    }
+    if (b === undefined) {
+      return 1;
+    }
+    return parseInt(a) - parseInt(b);
+  });
 })();
