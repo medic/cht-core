9502b214dd1387121987fb10f104364fc1871616
Ben Kiarie <kiarie@medic.org>
2024-01-05 14:31:40 +0300

    Fix: resolve feedback

--- tests/e2e/default-mobile/browser-compatibility/browser-compatibility.wdio-spec.js
+++ tests/e2e/default-mobile/browser-compatibility/browser-compatibility.wdio-spec.js
@@ -4,7 +4,12 @@ const loginPage = require('@page-objects/default/login/login.wdio.page');
 const constants = require('@constants');
 
 describe('Browser Compatibility Modal', () => {
-  const outdatedChrome = 'Mozilla/5.0 (Linux; Android 13; IN2010) AppleWebKit/537.36 (KHTML, like Gecko) ' +
+  const newChromeVersion =
+    'Mozilla/5.0 (Linux; Android 13; IN2010) AppleWebKit/537.36 (KHTML, like Gecko) ' +
+    'Chrome/118.0.5993.112 Mobile Safari/537.36';
+
+  const outdatedChromeVersion =
+    'Mozilla/5.0 (Linux; Android 13; IN2010) AppleWebKit/537.36 (KHTML, like Gecko) ' +
     'Chrome/74.0.5993.112 Mobile Safari/537.36';
 
   beforeEach(async () => {
@@ -20,12 +25,7 @@ describe('Browser Compatibility Modal', () => {
     await browser.url('/');
   });
 
-  it('should not display the browser compatibility modal for updated Chrome versions', async () => {
-    await commonPage.goToBase();
-    await modalPage.checkModalHasClosed();
-  });
-
-  it('should display the browser compatibility modal for outdated Chrome versions', async () => {
+  it('should not display the browser compatibility modal for updated Chrome version', async () => {
     await browser.emulateDevice({
       viewport: {
         width: 600,
@@ -33,7 +33,22 @@ describe('Browser Compatibility Modal', () => {
         isMobile: true,
         hasTouch: true,
       },
-      userAgent: outdatedChrome,
+      userAgent: newChromeVersion,
+    });
+
+    await commonPage.goToBase();
+    await modalPage.checkModalHasClosed();
+  });
+
+  it('should display the browser compatibility modal for outdated Chrome version', async () => {
+    await browser.emulateDevice({
+      viewport: {
+        width: 600,
+        height: 960,
+        isMobile: true,
+        hasTouch: true,
+      },
+      userAgent: outdatedChromeVersion,
     });
 
     await commonPage.goToBase();
