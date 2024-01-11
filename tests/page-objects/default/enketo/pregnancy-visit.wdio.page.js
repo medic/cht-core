1e7770541d1928676a8ae4daae44950413721c01
tatilepizs <tatiana@medic.org>
2024-01-11 11:05:30 -0600

    update pregnancy-visit.wdio.page.js

--- tests/page-objects/default/enketo/pregnancy-visit.wdio.page.js
+++ tests/page-objects/default/enketo/pregnancy-visit.wdio.page.js
@@ -1,20 +1,20 @@
 const fs = require('fs');
 const utils = require('@utils');
-const xmlForm = fs.readFileSync(`${__dirname}/../../../../config/standard/forms/app/pregnancy_visit.xml`, 'utf8');
-const formDocument = {
-  _id: 'form:pregnancy-visit',
-  internalId: 'pregnancy-visit',
-  title: 'Pregnancy Visit',
-  type: 'form',
-  _attachments: {
-    xml: {
-      content_type: 'application/octet-stream',
-      data: Buffer.from(xmlForm).toString('base64')
-    }
-  }
-};
 
 const uploadPregnancyVisitForm = async () => {
+  const xmlForm = fs.readFileSync(`${__dirname}/../../../../config/standard/forms/app/pregnancy_visit.xml`, 'utf8');
+  const formDocument = {
+    _id: 'form:pregnancy-visit',
+    internalId: 'pregnancy-visit',
+    title: 'Pregnancy Visit',
+    type: 'form',
+    _attachments: {
+      xml: {
+        content_type: 'application/octet-stream',
+        data: Buffer.from(xmlForm).toString('base64')
+      }
+    }
+  };
   await utils.saveDoc(formDocument);
 };
 
