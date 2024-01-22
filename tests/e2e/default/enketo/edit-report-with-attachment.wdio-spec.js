93178a9decb6d12635ca04e9bc41866e546fdc29
tatilepizs <tatiana@medic.org>
2024-01-22 16:09:49 -0600

    organize test edi-report-with-attachment

--- tests/e2e/default/enketo/edit-report-with-attachment.wdio-spec.js
+++ tests/e2e/default/enketo/edit-report-with-attachment.wdio-spec.js
@@ -8,8 +8,10 @@ const fs = require('fs');
 const commonElements = require('@page-objects/default/common/common.wdio.page');
 const oneTextForm = fs.readFileSync(`${__dirname}/forms/one-text-form.xml`, 'utf8');
 
-const instanceID = uuid();
-const reportModelXml = `
+describe('Edit report with attachment', () => {
+
+  const instanceID = uuid();
+  const reportModelXml = `
 <one_text_form xmlns:jr="http://openrosa.org/javarosa" id="required_note">
   <intro>initial text</intro>
   <meta>
@@ -18,48 +20,46 @@ const reportModelXml = `
   </meta>
 </one_text_form>`;
 
-const { userContactDoc, docs } = userData;
-const formDoc = {
-  _id: 'form:one_text_form',
-  internalId: 'one_text_form',
-  title: 'One text form',
-  type: 'form',
-  _attachments: {
-    xml: {
-      content_type: 'application/octet-stream',
-      data: Buffer.from(oneTextForm).toString('base64'),
+  const { userContactDoc, docs } = userData;
+  const formDoc = {
+    _id: 'form:one_text_form',
+    internalId: 'one_text_form',
+    title: 'One text form',
+    type: 'form',
+    _attachments: {
+      xml: {
+        content_type: 'application/octet-stream',
+        data: Buffer.from(oneTextForm).toString('base64'),
+      }
+    },
+    context: {
+      expression: 'summary.alive',
     }
-  },
-  context: {
-    expression: 'summary.alive',
-  }
-};
-const reportDoc ={
-  _id: uuid(),
-  form: formDoc.internalId,
-  type: 'data_record',
-  reported_date: Date.now(),
-  content_type: 'xml',
-  contact: userContactDoc,
-  hidden_fields: ['meta'],
-  fields: {
-    // to prove that when xml attachment exists, it is used to populate edit form instead of fields
-    intro: 'not same text as in xml attachment',
-    meta: {
-      instanceID: `uuid:${instanceID}`,
-      deprecatedID: ''
+  };
+  const reportDoc ={
+    _id: uuid(),
+    form: formDoc.internalId,
+    type: 'data_record',
+    reported_date: Date.now(),
+    content_type: 'xml',
+    contact: userContactDoc,
+    hidden_fields: ['meta'],
+    fields: {
+      // to prove that when xml attachment exists, it is used to populate edit form instead of fields
+      intro: 'not same text as in xml attachment',
+      meta: {
+        instanceID: `uuid:${instanceID}`,
+        deprecatedID: ''
+      }
+    },
+    _attachments: {
+      content: {
+        content_type: 'application/octet-stream',
+        data: Buffer.from(reportModelXml).toString('base64'),
+      }
     }
-  },
-  _attachments: {
-    content: {
-      content_type: 'application/octet-stream',
-      data: Buffer.from(reportModelXml).toString('base64'),
-    }
-  }
-};
+  };
 
-
-describe('Edit report with attachment', () => {
   before(async () => {
     await utils.seedTestData(userContactDoc, [...docs, formDoc]);
     await loginPage.cookieLogin();
