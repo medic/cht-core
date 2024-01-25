e525ff21466e6bdcd64dd5446eeb9765cf1bf84c
m5r <mokht@rmi.al>
2024-01-25 19:04:04 +0100

    push branch with more memory-efficient query to compare perfs

--- api/src/services/export/user-devices-mapper.js
+++ api/src/services/export/user-devices-mapper.js
@@ -1,32 +1,40 @@
 const db = require('../../db');
 
 module.exports = async () => {
-  const { rows } = await db.medicUsersMeta.allDocs({ include_docs: true });
-  const telemetry = rows.filter(e => e.doc.type === 'telemetry').map(e => e.doc);
   const userByDeviceId = {};
-  telemetry.forEach((entry) => {
-    if (!entry.device && !entry.metadata) {
-      return;
-    }
+  const limit = 1000;
+  let skip = 0;
+  let response = await db.medicUsersMeta.allDocs({ include_docs: true, limit, skip });
+  while (response.rows.length === limit) {
+    response.rows
+      .filter(e => e.doc.type === 'telemetry').map(e => e.doc)
+      .forEach((entry) => {
+        if (!entry.device && !entry.metadata) {
+          return;
+        }
 
-    const user = entry.metadata.user;
-    const date = `${entry.metadata.year}-${entry.metadata.month}-${entry.metadata.day}`;
-    const deviceMetas = {
-      user,
-      deviceId: entry.metadata.deviceId,
-      date,
-      webview: /Chrome\/(\d+\.\d+)/.exec(entry.device.userAgent)?.[1],
-      apk: entry.device.deviceInfo.app?.version,
-      android: entry.device.deviceInfo.software?.androidVersion,
-      cht: entry.metadata.versions.app,
-      settings: entry.metadata.versions.settings,
-    };
-    if (
-      !userByDeviceId[user] ||
-      userByDeviceId[user].date < date
-    ) {
-      userByDeviceId[user] = deviceMetas;
-    }
-  });
+        const user = entry.metadata.user;
+        const date = `${entry.metadata.year}-${entry.metadata.month}-${entry.metadata.day}`;
+        const deviceMetas = {
+          user,
+          deviceId: entry.metadata.deviceId,
+          date,
+          webview: /Chrome\/(\d+\.\d+)/.exec(entry.device.userAgent)?.[1],
+          apk: entry.device.deviceInfo.app?.version,
+          android: entry.device.deviceInfo.software?.androidVersion,
+          cht: entry.metadata.versions.app,
+          settings: entry.metadata.versions.settings,
+        };
+        if (
+          !userByDeviceId[user] ||
+          userByDeviceId[user].date < date
+        ) {
+          userByDeviceId[user] = deviceMetas;
+        }
+      });
+
+    skip += limit;
+    response = await db.medicUsersMeta.allDocs({ include_docs: true, limit, skip });
+  }
   return Object.values(userByDeviceId);
 };
