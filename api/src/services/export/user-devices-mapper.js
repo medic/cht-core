5e087a8e59bb3859dbb5226466ba8f0c3001eea1
m5r <mokht@rmi.al>
2024-01-25 19:03:29 +0100

    push branch with raw query to compare perfs with couch view

--- api/src/services/export/user-devices-mapper.js
+++ api/src/services/export/user-devices-mapper.js
@@ -1,22 +1,32 @@
 const db = require('../../db');
 
 module.exports = async () => {
-  const { rows } = await db.medicUsersMeta.query('users-meta/device_by_user', { group: true });
-  return rows.map(doc => {
-    const user = doc.key;
-    const date = doc.value.date;
-    const deviceId = doc.value.device.deviceId;
-    const webview = /Chrome\/(\d+\.\d+)/.exec(doc.value.device.userAgent)?.[1];
-    const { apk, android, cht, settings } = doc.value.device.versions;
-    return {
+  const { rows } = await db.medicUsersMeta.allDocs({ include_docs: true });
+  const telemetry = rows.filter(e => e.doc.type === 'telemetry').map(e => e.doc);
+  const userByDeviceId = {};
+  telemetry.forEach((entry) => {
+    if (!entry.device && !entry.metadata) {
+      return;
+    }
+
+    const user = entry.metadata.user;
+    const date = `${entry.metadata.year}-${entry.metadata.month}-${entry.metadata.day}`;
+    const deviceMetas = {
       user,
-      deviceId,
+      deviceId: entry.metadata.deviceId,
       date,
-      webview,
-      apk,
-      android,
-      cht,
-      settings,
+      webview: /Chrome\/(\d+\.\d+)/.exec(entry.device.userAgent)?.[1],
+      apk: entry.device.deviceInfo.app?.version,
+      android: entry.device.deviceInfo.software?.androidVersion,
+      cht: entry.metadata.versions.app,
+      settings: entry.metadata.versions.settings,
     };
+    if (
+      !userByDeviceId[user] ||
+      userByDeviceId[user].date < date
+    ) {
+      userByDeviceId[user] = deviceMetas;
+    }
   });
+  return Object.values(userByDeviceId);
 };
