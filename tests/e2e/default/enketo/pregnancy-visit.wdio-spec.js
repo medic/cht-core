7185aa3bc53e5840687736890814ead16cef10c1
tatilepizs <tatiana@medic.org>
2024-01-25 15:57:07 -0600

    Re-write the pregnancy-visit e2e test to use the form from the default config - This will also cover the ticket #8603

--- tests/e2e/default/enketo/pregnancy-visit.wdio-spec.js
+++ tests/e2e/default/enketo/pregnancy-visit.wdio-spec.js
@@ -1,76 +1,83 @@
+const moment = require('moment');
 const utils = require('@utils');
-const userData = require('@page-objects/default/users/user.data');
 const loginPage = require('@page-objects/default/login/login.wdio.page');
 const commonPage = require('@page-objects/default/common/common.wdio.page');
 const reportsPage = require('@page-objects/default/reports/reports.wdio.page');
-const genericForm = require('@page-objects/default/enketo/generic-form.wdio.page');
 const pregnancyVisitForm = require('@page-objects/default/enketo/pregnancy-visit.wdio.page');
-const commonEnketoPage = require('@page-objects/default/enketo/common-enketo.wdio.page');
+const placeFactory = require('@factories/cht/contacts/place');
+const userFactory = require('@factories/cht/users/users');
+const personFactory = require('@factories/cht/contacts/person');
+const pregnancyForm = require('@page-objects/default/enketo/pregnancy.wdio.page');
+const analyticsPage = require('@page-objects/default/analytics/analytics.wdio.page');
 
-// skipping for now per: https://github.com/medic/cht-core/issues/8603
-// and https://github.com/medic/cht-core/pull/8762#pullrequestreview-1784800918. Be sure to uncomment next line:
-describe.skip('Pregnancy Visit', () => {
-  before(async () => {
-    await pregnancyVisitForm.uploadPregnancyVisitForm();
-    await utils.seedTestData(userData.userContactDoc, userData.docs);
-    await loginPage.cookieLogin();
-    await commonPage.goToReports();
+describe('Pregnancy Visit', () => {
+  const places = placeFactory.generateHierarchy();
+  const healthCenter = places.get('health_center');
+  const offlineUser = userFactory.build({ place: healthCenter._id, roles: ['chw'] });
+  const pregnantWoman = personFactory.build({
+    date_of_birth: moment().subtract(25, 'years').format('YYYY-MM-DD'),
+    parent: { _id: healthCenter._id, parent: healthCenter.parent }
   });
 
-  it('Submit and validate Pregnancy Visit form and keeps the report minified', async () => {
-    const name = userData.userContactDoc.name;
-    const dangerSignsQuestion = `Confirm with ${name} if ${name} has any of the following danger signs.`;
+  before(async () => {
+    await utils.saveDocs([...places.values(), pregnantWoman]);
+    await utils.createUsers([offlineUser]);
+    await loginPage.login(offlineUser);
+    await commonPage.goToPeople(pregnantWoman._id);
+    await commonPage.openFastActionReport('pregnancy');
+    await pregnancyForm.submitDefaultPregnancy();
+  });
 
-    await commonPage.openFastActionReport('pregnancy-visit', false);
-    await genericForm.selectContact(name);
-    await genericForm.nextPage();
+  it('should submit a pregnancy visit and validate that the report was created successfully.', async () => {
+    const { TARGET_MET_COLOR, TARGET_UNMET_COLOR } = analyticsPage;
 
-    await commonEnketoPage.selectCheckBox(dangerSignsQuestion, 'Pain, pressure or cramping in abdomen');
-    await commonEnketoPage.selectCheckBox(dangerSignsQuestion,
-      'Bleeding or fluid leaking from vagina or vaginal discharge with bad odour');
-    await commonEnketoPage.selectCheckBox(dangerSignsQuestion, 'Severe nausea or vomiting');
-    await commonEnketoPage.selectCheckBox(dangerSignsQuestion, 'Fever of 38 degrees or higher');
-    await commonEnketoPage.selectCheckBox(dangerSignsQuestion, 'Severe headache or new, blurry vision problems');
-    await commonEnketoPage.selectCheckBox(dangerSignsQuestion,
-      'Sudden weight gain or severe swelling of feet, ankles, face, or hands');
-    await commonEnketoPage.selectCheckBox(dangerSignsQuestion, 'Less movement and kicking from the baby');
-    await commonEnketoPage.selectCheckBox(dangerSignsQuestion, 'Blood in the urine or painful, burning urination');
-    await commonEnketoPage.selectCheckBox(dangerSignsQuestion, 'Diarrhea that doesn\'t go away');
-    await genericForm.nextPage();
-    await commonEnketoPage.setTextareaValue('You can add a personal note to the SMS here:',
-      'Test notes - Pregnancy visit');
-    await genericForm.nextPage();
+    await commonPage.openFastActionReport('pregnancy_home_visit');
+    await pregnancyVisitForm.submitDefaultPregnancyVisit();
 
-    const summaryTexts = [
-      name,
-      'Pain or cramping in abdomen',
-      'Bleeding or fluid leaking from vagina or vaginal discharge with bad odour',
-      'Severe nausea or vomiting',
-      'Fever of 38 degrees or higher',
-      'Severe headache or new, blurry vision problems',
-      'Sudden weight gain or severe swelling of feet, ankles, face, or hands',
-      'Less movement and kicking from the baby (after week 20 of pregnancy)',
-      'Blood in the urine or painful, burning urination',
-      'Diarrhea that doesn\'t go away',
-      'Test notes - Pregnancy visit',
-    ];
+    // Verify created report
+    await commonPage.goToReports();
+    const { heading, form } = await reportsPage.getListReportInfo(await reportsPage.firstReport());
+    expect(heading).to.equal(pregnantWoman.name);
+    expect(form).to.equal('Pregnancy home visit');
 
-    await commonEnketoPage.validateSummaryReport(summaryTexts);
+    // Verify that the target tile was updated for pregnancies with 1+ visits
+    await commonPage.goToAnalytics();
+    await analyticsPage.goToTargets();
 
-    await genericForm.submitForm();
+    expect(await analyticsPage.getTargets()).to.have.deep.members([
+      {title: 'Deaths', goal: '0', count: '0', countNumberColor: TARGET_MET_COLOR},
+      {title: 'New pregnancies', goal: '20', count: '1', countNumberColor: TARGET_UNMET_COLOR},
+      {title: 'Live births', count: '0', countNumberColor: TARGET_MET_COLOR},
+      {title: 'Active pregnancies', count: '1', countNumberColor: TARGET_MET_COLOR},
+      {title: 'Active pregnancies with 1+ routine facility visits', count: '1', countNumberColor: TARGET_MET_COLOR},
+      {title: 'In-facility deliveries', percent: '0%', percentCount: '(0 of 0)'},
+      {title: 'Active pregnancies with 4+ routine facility visits', count: '0', countNumberColor: TARGET_MET_COLOR},
+      {title: 'Active pregnancies with 8+ routine contacts', count: '0', countNumberColor: TARGET_MET_COLOR}
+    ]);
 
-    //report summary
-    const firstReport = await reportsPage.getListReportInfo(await reportsPage.firstReport());
-    expect(firstReport.heading).to.equal(name);
-    expect(firstReport.form).to.equal('Pregnancy Visit');
-    expect(firstReport.lineage).to.equal(userData.docs[0].name);
+    // Adding 3 more pregnancy home visits to verify the target tile updated for pregnancies with 4+ visits
+    await commonPage.goToPeople(pregnantWoman._id);
+    await commonPage.openFastActionReport('pregnancy_home_visit');
+    await pregnancyVisitForm.submitDefaultPregnancyVisit();
+    await commonPage.openFastActionReport('pregnancy_home_visit');
+    await pregnancyVisitForm.submitDefaultPregnancyVisit();
+    await commonPage.openFastActionReport('pregnancy_home_visit');
+    await pregnancyVisitForm.submitDefaultPregnancyVisit();
 
-    //report details
-    const openReportInfo = await reportsPage.getOpenReportInfo();
-    expect(openReportInfo.senderName).to.equal(`Submitted by ${name}`);
-    expect(openReportInfo.senderPhone).to.equal(userData.userContactDoc.phone);
-    expect(openReportInfo.lineage).to.equal(userData.docs[0].name);
-    expect(await (await reportsPage.selectedCaseId()).getText()).to.match(/^\d{5}$/);
+    await commonPage.goToAnalytics();
+    await analyticsPage.goToTargets();
+
+    expect(await analyticsPage.getTargets()).to.have.deep.members([
+      {title: 'Deaths', goal: '0', count: '0', countNumberColor: TARGET_MET_COLOR},
+      {title: 'New pregnancies', goal: '20', count: '1', countNumberColor: TARGET_UNMET_COLOR},
+      {title: 'Live births', count: '0', countNumberColor: TARGET_MET_COLOR},
+      {title: 'Active pregnancies', count: '1', countNumberColor: TARGET_MET_COLOR},
+      {title: 'Active pregnancies with 1+ routine facility visits', count: '1', countNumberColor: TARGET_MET_COLOR},
+      {title: 'In-facility deliveries', percent: '0%', percentCount: '(0 of 0)'},
+      {title: 'Active pregnancies with 4+ routine facility visits', count: '1', countNumberColor: TARGET_MET_COLOR},
+      {title: 'Active pregnancies with 8+ routine contacts', count: '1', countNumberColor: TARGET_MET_COLOR}
+    ]);
+
   });
 });
 
