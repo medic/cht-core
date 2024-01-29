6db80705454d0a52e1efda0c6f1211b213edf6e0
tatilepizs <tatiana@medic.org>
2024-01-29 15:42:02 -0600

    Running only the pregnancy-visit test - try 3

--- tests/e2e/default/suites.js
+++ tests/e2e/default/suites.js
@@ -1,33 +1,33 @@
 const SUITES = {
-  /*core: [
-    './admin/!**!/!*.wdio-spec.js',
-    './login/!**!/!*.wdio-spec.js',
-    './translations/!**!/!*.wdio-spec.js',
-    './more-options-menu/!**!/!*.wdio-spec.js',
-    './users/!**!/!*.wdio-spec.js',
-    './about/!**!/!*.wdio-spec.js',
-    './navigation/!**!/!*.wdio-spec.js',
-    './privacy-policy/!**!/!*.wdio-spec.js',
+  core: [
+    './admin/**/*.wdio-spec.js',
+    './login/**/*.wdio-spec.js',
+    './translations/**/*.wdio-spec.js',
+    './more-options-menu/**/*.wdio-spec.js',
+    './users/**/*.wdio-spec.js',
+    './about/**/*.wdio-spec.js',
+    './navigation/**/*.wdio-spec.js',
+    './privacy-policy/**/*.wdio-spec.js',
   ],
   workflows: [
-    './analytics/!**!/!*.wdio-spec.js',
-    './contacts/!**!/!*.wdio-spec.js',
-    './reports/!**!/!*.wdio-spec.js',
-    './targets/!**!/!*.wdio-spec.js',
-    './tasks/!**!/!*.wdio-spec.js',
-    './sms/!**!/!*.wdio-spec.js',
+    './analytics/**/*.wdio-spec.js',
+    './contacts/**/*.wdio-spec.js',
+    './reports/**/*.wdio-spec.js',
+    './targets/**/*.wdio-spec.js',
+    './tasks/**/*.wdio-spec.js',
+    './sms/**/*.wdio-spec.js',
   ],
   data: [
-    './db/!**!/!*.wdio-spec.js',
-    './purge/!**!/!*.wdio-spec.js',
-    './telemetry/!**!/!*.wdio-spec.js'
+    './db/**/*.wdio-spec.js',
+    './purge/**/*.wdio-spec.js',
+    './telemetry/**/*.wdio-spec.js'
   ],
   lowLevel: [
-    './pwa/!**!/!*.wdio-spec.js',
-    './service-worker/!**!/!*.wdio-spec.js',
-    './transitions/!**!/!*.wdio-spec.js',
-    './logging/!**!/!*.wdio-spec.js'
-  ],*/
+    './pwa/**/*.wdio-spec.js',
+    './service-worker/**/*.wdio-spec.js',
+    './transitions/**/*.wdio-spec.js',
+    './logging/**/*.wdio-spec.js'
+  ],
   enketo: [
     './enketo/**/pregnancy-visit.wdio-spec.js',
   ]
