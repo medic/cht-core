a31d89ccdc0deac5329a7ed995a1420612eacd05
latin-panda <66472237+latin-panda@users.noreply.github.com>
2024-02-12 19:45:49 +0700

    debugging e2e

--- tests/e2e/default/wdio.conf.js
+++ tests/e2e/default/wdio.conf.js
@@ -1,5 +1,4 @@
 const wdioBaseConfig = require('../wdio.conf');
-const { suites } = require('./suites');
 
 // Override specific properties from wdio base config
 const defaultConfig = Object.assign(wdioBaseConfig.config, {
