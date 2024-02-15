e528b1bbb23b36e2f219937a9435e5d364b355f7
mrjones-plip <mrjones-plip@plip.com>
2024-02-15 15:40:01 -0800

    add are you sure prompt to existing script, defend against admin and medic

--- scripts/bulk-password-change.js
+++ scripts/bulk-password-change.js
@@ -1,6 +1,7 @@
 const rpn = require('request-promise-native');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED=0;
 const minimist = require('minimist');
+const readline = require('readline');
 
 const argv = minimist(process.argv.slice(2), {
   alias: {
@@ -43,25 +44,60 @@ const options = {
   }   
 };
 
+// thanks https://stackoverflow.com/a/50890409 !
+const areYouSure = async () => {
+  const NOTE = ` ****** WARNING ******\
+
+Continuing will log ALL users out of the CHT until you provide them with the password. 
+
+They will ALL have the same password "Secret_1"
+
+Do you want to continue? [y/N]
+`;
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise(resolve => rl.question(NOTE, ans => {
+    rl.close();
+    resolve(ans);
+  }));
+};
+
+const changeUserPass = async (user, options) => {
+  const admins = ['admin', 'medic'];
+  const adminError = new Error('403 - Cannot change password for "medic" or "admin" users.');
+  const postOptions = {...options};
+  postOptions.body = {
+    'password': 'Secret_1'
+  };
+  postOptions.uri = `${options.uri}/${user}`;
+  try {
+    if (admins.includes(user)) {
+      throw adminError;
+    }
+    await rpn.post(postOptions);
+    console.log('SUCCESS', user);
+  } catch (e) {
+    console.log('ERROR', user, e.message);
+  }
+};
+
 const execute = async () => {
+  const input = await areYouSure();
+  if (input.toLowerCase() !== 'y') {
+    process.exit(0);
+  }
   let users = [];
   try {
     users = await rpn.get(options);
   } catch (e) {
     console.log('An error while getting the list of users - ', e.message);
   }
-  
+
   users.forEach(async (user) => {
-    const postOptions = Object.assign({}, options);
-    postOptions.body = {
-      'password': 'Secret_1'
-    };
-    postOptions.uri = `${options.uri}/${user.username}`;
-    try {
-      await rpn.post(postOptions);
-    } catch (e) {
-      console.log('An error while updating the password - ', e.message);
-    }
+    await changeUserPass(user.username, options);
   });
 };
 
