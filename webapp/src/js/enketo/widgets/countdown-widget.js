8cc620ea84eba289e165f553bf7127232b009725
Joshua Kuestersteffen <jkuester@kuester7.com>
2024-03-07 12:57:05 -0600

    Refactor timer animation code to make it more maintainable and testable

--- webapp/src/js/enketo/widgets/countdown-widget.js
+++ webapp/src/js/enketo/widgets/countdown-widget.js
@@ -16,6 +16,7 @@
 
 'use strict';
 const Widget = require( 'enketo-core/src/js/widget' ).default;
+const TimerAnimation = require( '../lib/timer-animation' );
 const $ = require( 'jquery' );
 require( 'enketo-core/src/js/plugins' );
 
@@ -53,115 +54,11 @@ class Timerwidget extends Widget {
     const canvas = $('<canvas width="%s" height="%s">'.replace(/%s/g, DIM));
     $wrapper.append(canvas);
     const duration = parseInt($wrapper.attr('data-cht-duration')) || deprecated.getDefaultValue($el) || DEFAULT_TIME;
-    new TimerAnimation(canvas[0], DIM, DIM, duration, $el);
+
+    // Marks the hidden OK radio button as checked
+    const setTimerCompleted = () => $el.prop('checked', true).trigger('change');
+    TimerAnimation.animate(canvas[0], duration, setTimerCompleted);
   }
 }
 
 module.exports = Timerwidget;
-
-const TimerAnimation = function(canvas, canvasW, canvasH, duration, $el) {
-  const pi = Math.PI;
-  const LIM = duration * 500; // Half of the time the animation should take in milliseconds
-  const ctx = canvas.getContext('2d');
-
-  const centre = { x: canvasW/2, y: canvasH/2 };
-  const radius = Math.min(canvasW, canvasH) / 2;
-  const activeBgColor = '#0000ff';
-  const inactiveBgColor = '#cccccc';
-  const arcColor = '#cccccc';
-  let running;
-
-  const audio = (function() {
-    const androidSoundSupport = window.medicmobile_android &&
-      typeof window.medicmobile_android.playAlert === 'function';
-    if (androidSoundSupport) {
-      return { play: () => window.medicmobile_android.playAlert() };
-    }
-    const cached = new Audio('/audio/alert.mp3');
-    return { play: () => cached.play() };
-  }());
-  
-  const setTimerCompleted = () => {
-    $el.prop('checked', true).trigger('change'); // Mark the hidden OK radio button as checked
-  };
-
-  //> UTILS
-  const drawCircle = (ctx, c) => {
-    drawArc(ctx, c, 360);
-  };
-
-  const drawArc = (ctx, c, arc) => {
-    const arcRadians = pi*arc/180;
-    ctx.beginPath();
-    ctx.arc(c.x, c.y, c.r, -pi/2, arcRadians-(pi/2), false);
-    ctx.lineTo(c.x, c.y);
-    ctx.fillStyle = c.color;
-    ctx.fill();
-  };
-
-  //> ANIMATION
-  const drawAnimation = (offset) => {
-    drawBackgroundCircle(activeBgColor);
-    drawTimerArc(offset);
-  };
-  const drawBackgroundCircle = (color) => {
-    drawCircle(ctx, {
-      x: centre.x, y: centre.y, r: radius, color: color });
-  };
-  const drawTimerArc = (offset) => {
-    drawArc(
-      ctx,
-      { x: centre.x, y: centre.y, r: radius, color: arcColor },
-      offset * 180 / LIM
-    );
-  };
-
-  const startTimer = () => {
-    running = true;
-    setTimeout(function() {
-      animate(Date.now());
-    }, 0);
-  };
-
-  const animate = (start) => {
-    const offset = Date.now() - start;
-
-    if (!running) {
-      return;
-    }
-
-    if (offset < LIM*2) {
-      drawAnimation(offset);
-      requestAnimationFrame(function() {
-        animate(start);
-      });
-    } else {
-      drawBackgroundCircle(inactiveBgColor);
-      running = false;
-      setTimerCompleted();
-      if ($(canvas).closest('body').length > 0) {
-        // only beep if the canvas is still attached to the DOM
-        audio.play();
-      }
-    }
-  };
-
-  // init
-  (function() {
-    const resetTimer = () => {
-      running = false;
-      drawBackgroundCircle(inactiveBgColor);
-    };
-
-    // set up initial state
-    resetTimer();
-
-    canvas.addEventListener('click', function() {
-      if (running) {
-        resetTimer();
-      } else {
-        startTimer();
-      }
-    });
-  }());
-};
