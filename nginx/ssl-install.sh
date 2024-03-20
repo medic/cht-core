bc5150b15176051c208bd845707983ea637f99b1
Sugat Bajracharya <sugat@medic.org>
2024-03-20 21:21:23 +0545

    fix(#308): Add assetlinks.json to nginx.conf

--- nginx/ssl-install.sh
+++ nginx/ssl-install.sh
@@ -132,6 +132,23 @@ EOF
 
 prepare_android_assetlink(){
   mkdir -p /etc/nginx/private/android
+
+cat > /etc/nginx/private/android/assetlinks.json << EOF
+[
+  {
+    "relation": [
+      "delegate_permission/common.handle_all_urls"
+    ],
+    "target": {
+      "namespace": "android_app",
+      "package_name": "org.medicmobile.webapp.mobile.new_brand",
+      "sha256_cert_fingerprints": [
+        "25:DC:55:48:8E:05:A8:6C:21:AC:F8:81:FF:75:CC:36:9D:10:E3:BA:5D:1C:2A:6C:89:BD:B7:2B:63:15:77:00"
+      ]
+    }
+  }
+]
+EOF
 }
 
 main (){
