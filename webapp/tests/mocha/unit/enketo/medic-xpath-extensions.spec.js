695fbeafe55fb605a6c7274162d9116706d07dc0
Anro Swart <anro.swart@westerncape.gov.za>
2024-03-28 13:06:47 +0200

    chore: update date diff function

--- webapp/tests/mocha/unit/enketo/medic-xpath-extensions.spec.js
+++ webapp/tests/mocha/unit/enketo/medic-xpath-extensions.spec.js
@@ -6,33 +6,25 @@ const medicXpathExtensions = require('../../../../src/js/enketo/medic-xpath-exte
 medicXpathExtensions.init(null, toBik_text, moment);
 const func = medicXpathExtensions.func;
 
-const getTimezoneOffset = Date.prototype.getTimezoneOffset;
+describe('medic-xpath-extensions', function () {
+  const wrapDate = (date) => { 
+    return { t: 'str', v: date };
+  };
 
-describe('medic-xpath-extensions', function() {
-
-  beforeEach(() => {
-    // make the result consistent regardless of which timezone the test is run in
-    Date.prototype.getTimezoneOffset = () => -240;
-  });
-
-  afterEach(() => {
-    Date.prototype.getTimezoneOffset = getTimezoneOffset;
-  });
-
-  describe('getTimezoneOffsetAsTime()', function() {
-    it('returns the time zone offset in hours when given a time zone difference in minutes', function() {
+  describe('getTimezoneOffsetAsTime()', function () {
+    it('returns the time zone offset in hours when given a time zone difference in minutes', function () {
       Date.prototype.getTimezoneOffset = () => -60;
       assert.equal(medicXpathExtensions.getTimezoneOffsetAsTime(new Date()), '+01:00');
     });
 
-    it('returns a negative time zone offset when given a positive time zone difference', function() {
+    it('returns a negative time zone offset when given a positive time zone difference', function () {
       Date.prototype.getTimezoneOffset = () => 60;
       assert.equal(medicXpathExtensions.getTimezoneOffsetAsTime(new Date()), '-01:00');
     });
   });
 
-  describe('toISOLocalString()', function() {
-    it('returns the ISO local string consistent with the time zone offset', function() {
+  describe('toISOLocalString()', function () {
+    it('returns the ISO local string consistent with the time zone offset', function () {
       const date = new Date('August 19, 1975 23:15:30 GMT+07:00');
       Date.prototype.getTimezoneOffset = () => -60;
       assert.equal(medicXpathExtensions.toISOLocalString(date), '1975-08-19T17:15:30.000+01:00');
@@ -41,39 +33,77 @@ describe('medic-xpath-extensions', function() {
     });
   });
 
-  describe('#difference-in-months', function() {
+  describe('#difference-in-years', function () {
+    const daysRemaining = func['cht:difference-in-years'];
 
+    it('Should correctly calculate the difference in years', () => {
+      const startDate = moment('2020-01-01');
+      const endDate = moment('2024-01-01');
+      const result = daysRemaining(wrapDate(startDate), wrapDate(endDate));
+      assert.equal(result.v, 4); // Expected difference in years between the start and end dates
+    });
+
+    it('Should return 0 if start and end dates are the same', () => {
+      const startDate = moment('2020-01-01');
+      const endDate = moment('2020-01-01');
+      const result = daysRemaining(wrapDate(startDate), wrapDate(endDate));
+      assert.equal(result.v, 0);
+    });
+
+    it('Should handle leap years correctly', () => {
+      const startDate = moment('2020-02-29'); // Leap year
+      const endDate = moment('2024-02-29'); // Leap year
+      const result = daysRemaining(wrapDate(startDate), wrapDate(endDate));
+      assert.equal(result.v, 4); // Expected difference in years should be 4 despite leap years
+    });
+
+    it('Should handle invalid dates', () => {
+      const startDate = moment('2020-01-01');
+      const endDate = moment('Invalid Date');
+      const result = daysRemaining(wrapDate(startDate), wrapDate(endDate));
+      assert.equal(result.v, ''); // Expected result for an invalid date
+    });
+
+    it('Should handle start date after end date', () => {
+      const startDate = moment('2024-01-01');
+      const endDate = moment('2020-01-01');
+      const result = daysRemaining(wrapDate(startDate), wrapDate(endDate));
+      assert.equal(result.v, -4); // Expected negative difference as start date is after end date
+    });
+  });
+
+  describe('#difference-in-months', function () {
     [
-      [ '2015-10-01', '2015-10-01', 0, ],
-      [ '2015-09-01', '2015-10-01', 1, ],
-      [ '2015-09-02', '2015-10-01', 0, ],
-      [ '2015-10-01', '2015-11-01', 1, ],
-      [ '2015-10-02', '2015-11-01', 0, ],
-      [ '2014-10-01', '2015-10-01', 12, ],
-      [ '2014-10-02', '2015-10-01', 11, ],
-      [ '2015-10-01T11:11:11.111', '2014-10-01T11:11:11.111', -12, ],
-      [ '2014-10-01T00:00:00.000', '2015-10-01T00:00:00.000', 12, ],
-      [ 'August 19, 1975 00:00:00 GMT', 'August 18, 1976 23:15:30 GMT+07:00', 11 ],
-      [ 'Sun Sep 25 2005 1:00:00 GMT+0100', 'Sun Oct 24 2005 22:00:00 GMT+2300', 0],
-      [ 'Sun Sep 25 2005 1:00:00 GMT+0100', 'Sun Oct 25 2005 22:00:00 GMT+2200', 1]
-    ].forEach(function(example) {
+      ['2015-10-01', '2015-10-01', 0,],
+      ['2015-09-01', '2015-10-01', 1,],
+      ['2015-09-02', '2015-10-01', 0,],
+      ['2015-10-01', '2015-11-01', 1,],
+      ['2015-10-02', '2015-11-01', 0,],
+      ['2014-10-01', '2015-10-01', 12,],
+      ['2014-10-02', '2015-10-01', 11,],
+      ['2015-10-01T11:11:11.111', '2014-10-01T11:11:11.111', -12,],
+      ['2014-10-01T00:00:00.000', '2015-10-01T00:00:00.000', 12,],
+      ['August 19, 1975 00:00:00 GMT', 'August 18, 1976 23:15:30 GMT+07:00', 11],
+      ['Sun Sep 25 2005 1:00:00 GMT+0100', 'Sun Oct 25 2005 22:00:00 GMT+2300', 0],
+      ['Sun Sep 25 2005 1:00:00 GMT+0100', 'Sun Oct 25 2005 22:00:00 GMT+2200', 1]
+    ].forEach(function (example) {
       const d1 = { t: 'str', v: example[0] };
       const d2 = { t: 'str', v: example[1] };
       const expectedDifference = example[2];
 
-      it(`should report difference between ${d1.v} and ${d2.v} as ${expectedDifference}`, function() {
-        assert.equal(func['difference-in-months'](d1, d2).v, expectedDifference);
+      it(`should report difference between ${d1.v} and ${d2.v} as ${expectedDifference}`, function () {
+        assert.equal(func['cht:difference-in-months'](d1, d2).v, expectedDifference);
       });
     });
 
-    it('should report difference between date objects', function() {
+    it('should report difference between date objects', function () {
       const d1 = { t: 'date', v: new Date('2015-09-22') };
       const d2 = { t: 'date', v: new Date('2014-09-22') };
 
       assert.equal(func['difference-in-months'](d1, d2).v, -12);
     });
 
-    it('should report difference between day counts', function() {
+    it('should report difference between day counts', function () {
       const num1 = { t: 'num', v: 10 };
       const num2 = { t: 'num', v: 50 };
       const str1 = { t: 'str', v: '10' };
@@ -83,14 +113,14 @@ describe('medic-xpath-extensions', function() {
       assert.equal(func['difference-in-months'](str1, num2).v, 1);
     });
 
-    it('should report difference between element arrays', function() {
+    it('should report difference between element arrays', function () {
       const d1 = { t: 'arr', v: [{ textContent: '2015-09-22' }] };
       const d2 = { t: 'arr', v: [{ textContent: '2014-09-22' }] };
 
       assert.equal(func['difference-in-months'](d1, d2).v, -12);
     });
 
-    it('should return an empty string when the difference cannot be calculated', function() {
+    it('should return an empty string when the difference cannot be calculated', function () {
       const invalidStr = { t: 'str', v: 'nonsense' };
       const bool = { t: 'bool', v: true };
       const nonsense = { t: 'nonsense', v: '2015-09-22' };
@@ -102,14 +132,63 @@ describe('medic-xpath-extensions', function() {
     });
   });
 
-  describe('#to-bikram-sambat()', () => {
-
+  describe('#difference-in-weeks', function () {
+    const weeksRemaining = func['cht:difference-in-weeks'];
     [
-      [ { t: 'str', v: '2015-9-2' }, '१६ भदौ २०७२'],
-      [ { t: 'str', v: '1999-12-12' }, '२६ मंसिर २०५६'],
-      [ { t: 'date', v: new Date('2015-10-01T00:00:00.000') }, '१४ असोज २०७२'],
-      [ { t: 'num', v: 11111 }, '२१ जेठ २०५७'],
-      [ { t: 'arr', v: [{ textContent: '2014-09-22' }] }, '६ असोज २०७१'],
+      ['2023-09-10', '2023-09-22', 1],
+      ['2023-01-05', '2023-10-11', 39],
+      ['2023-10-05', '2023-11-01', 3],
+      ['2023-02-05', '2023-05-29', 16],
+      ['2023-06-10', '2023-06-16', 0]
+    ].forEach(([startDate, endDate, expectedDiffInWeeks]) => {
+      it('should return the difference in weeks', function () {
+        const weeks_remaining = weeksRemaining(wrapDate(startDate), wrapDate(endDate));
+        assert.deepStrictEqual(weeks_remaining.v, expectedDiffInWeeks);
+      });
+    });
+  });
+
+  describe('#difference-in-days', function () {
+    const diffInDays = func['cht:difference-in-days'];
+
+    it('should handle cases where end date is before start date and return negative number', function () {
+      const result = diffInDays(wrapDate('2023-08-01'), wrapDate('2023-01-01'));
+      assert.equal(result.v, -212);
+    });
+
+    it('should return an empty string when at least one input is not a valid date', function () {
+      const result = diffInDays(wrapDate('2023-01-01'), wrapDate('invalid-date'));
+      assert.equal(result.v, '');
+    });
+
+    it('should return the difference when valid inputs are provided', function () {
+      const result = diffInDays(wrapDate('2023-01-01'), wrapDate('2023-08-01'));
+      assert.deepStrictEqual(result.v, 212);
+    });
+  });
+
+  describe('#days-remaining-in-week', function () {
+    const daysRemaining = func['cht:days-remaining-in-week'];
+    [
+      ['2023-09-21', '2023-09-22', 1],
+      ['2023-01-05', '2023-10-11', 6],
+      ['2023-02-05', '2023-11-01', 3],
+      ['2023-02-05', '2023-10-29', 0]
+    ].forEach(([startDate, endDate, expectedDiffInDays]) => {
+      it('should return the days remaining in the week', function () {
+        const days_remaining = daysRemaining(wrapDate(startDate), wrapDate(endDate));
+        assert.deepStrictEqual(days_remaining.v, expectedDiffInDays);
+      });
+    });
+  });
+
+  describe('#to-bikram-sambat()', () => {
+    [
+      [{ t: 'str', v: '2015-9-2' }, '१६ भदौ २०७२'],
+      [{ t: 'str', v: '1999-12-12' }, '२६ मंसिर २०५६'],
+      [{ t: 'date', v: new Date('2015-10-01T00:00:00.000') }, '१४ असोज २०७२'],
+      [{ t: 'num', v: 11111 }, '२१ जेठ २०५७'],
+      [{ t: 'arr', v: [{ textContent: '2014-09-22' }] }, '६ असोज २०७१'],
     ].forEach(([date, expected]) => {
       it(`should format the ${date.t} [${JSON.stringify(date.v)}] according to the Bikram Sambat calendar`, () => {
         assert.equal(func['to-bikram-sambat'](date).v, expected);
@@ -136,7 +215,6 @@ describe('medic-xpath-extensions', function() {
   });
 
   describe('#add-date()', () => {
-
     const display = value => value ? value.v : '';
 
     [
@@ -271,7 +349,7 @@ describe('medic-xpath-extensions', function() {
       { t: 'arr', v: [{ textContent: 'nonsense' }] },
       { t: 'arr', v: ['2014-09-22'] },
     ].forEach(date => {
-      it(`should return "Invalid Date" when given ${date.t} [${date.v}] as the date parameter`, () => {
+      it(`should return 'Invalid Date' when given ${date.t} [${date.v}] as the date parameter`, () => {
         const year = { t: 'num', v: 1 };
         assert.equal(func['add-date'](date, year).v.toString(), 'Invalid Date');
       });
