36dd4d1a528ec4355527123396d1df289d14d13f
Ben Kiarie <kiarie@medic.org>
2024-04-12 12:18:07 +0300

    fix: ignore sonar for now

--- shared-libs/search/src/search.js
+++ shared-libs/search/src/search.js
@@ -52,11 +52,13 @@ module.exports = function(Promise, DB) {
     const firstMapKeys = Object.keys(responsesMapById[0]);
     let intersectionSet = new Set(firstMapKeys);
 
+    // NOSONAR_BEGIN
     responsesMapById.slice(1).forEach(map => {
       intersectionSet = new Set(
         [...intersectionSet].filter(id => Object.hasOwnProperty.call(map, id))
       );
     });
+    // NOSONAR_END
 
     // we use the last query response values for sorting by default, but we allow other responses to "pitch in"
     // used for displaying muted contacts to the bottom when sorting by last visited date
