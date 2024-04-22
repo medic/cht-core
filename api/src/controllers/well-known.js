c522d63c7a7bb898c57e491b2b1557b30f1a8e41
m5r <mokht@rmi.al>
2024-04-22 15:10:23 +0200

    rewrite try/catch with promise, makes for a simpler function

--- api/src/controllers/well-known.js
+++ api/src/controllers/well-known.js
@@ -3,15 +3,14 @@ const serverUtils = require('../server-utils');
 
 module.exports = {
   assetlinks: async (req, res) => {
-    try {
-      const settings = await settingsService.get();
-      if (settings.assetlinks) {
-        return res.json(settings.assetlinks);
-      }
+    return settingsService.get()
+      .then((settings) => {
+        if (!settings.assetlinks) {
+          throw { code: 404, error: 'not_found', reason: 'No assetlinks.json configured' };
+        }
 
-      serverUtils.error({ code: 404, error: 'not_found', reason: 'No assetlinks.json configured' }, req, res);
-    } catch (error) {
-      serverUtils.error(error, req, res);
-    }
+        return res.json(settings.assetlinks);
+      })
+      .catch(error => serverUtils.error(error, req, res));
   },
 };
