0338b023e10d93ba5490900968862461df954859
Gareth Bowen <gareth@medic.org>
2024-04-22 09:26:30 +0200

    chore(#8982): upgrade to angular 17

---
+++ webapp/src/ts/zone-flags.ts
@@ -0,0 +1,3 @@
+// needed to make database-connection-monitor.service.ts work. without this unhandled
+// rejections are swallowed by zonejs
+(window as any).__zone_symbol__DISABLE_WRAPPING_UNCAUGHT_PROMISE_REJECTION = false;
