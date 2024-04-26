c8985c111d2d3ab1e5724fa187dd70467d16d7c7
Diana Barsan <35681649+dianabarsan@users.noreply.github.com>
2024-04-26 23:36:01 +0700

    feat(#8909): add CI jobs that run integration tests over a CHT launched in a K3D cluster  (#8978)

--- tests/integration/couchdb/couch_chttpd.spec.js
+++ tests/integration/couchdb/couch_chttpd.spec.js
@@ -38,18 +38,17 @@ const expectCorrectMetadata = (metadata) => {
   expect(metadata._id).to.equal(constants.DB_NAME);
   // 12 shards, evenly distributed across 3 nodes
   expect(Object.keys(metadata.by_range).length).to.equal(12);
-  expect(metadata.by_node).to.have.keys([
-    'couchdb@couchdb-1.local', 'couchdb@couchdb-2.local', 'couchdb@couchdb-3.local'
-  ]);
-  expect(metadata.by_node['couchdb@couchdb-1.local'].length).to.equal(4);
-  expect(metadata.by_node['couchdb@couchdb-2.local'].length).to.equal(4);
-  expect(metadata.by_node['couchdb@couchdb-3.local'].length).to.equal(4);
+  const nodeName = (nbr) => `couchdb@couchdb-${nbr}${utils.isK3D() ? `.${utils.PROJECT_NAME}.svc.cluster` : ''}.local`;
+  expect(metadata.by_node).to.have.keys([nodeName(1), nodeName(2), nodeName(3)]);
+  expect(metadata.by_node[nodeName(1)].length).to.equal(4);
+  expect(metadata.by_node[nodeName(2)].length).to.equal(4);
+  expect(metadata.by_node[nodeName(3)].length).to.equal(4);
 };
 
 describe('accessing couch clustering endpoint', () => {
   it('should block unauthenticated access through the host network', async () => {
     await expect(
-      utils.request({ uri: `https://localhost/_node/_local/_dbs/${constants.DB_NAME}`, noAuth: true })
+      utils.request({ uri: `https://${constants.API_HOST}/_node/_local/_dbs/${constants.DB_NAME}`, noAuth: true })
     ).to.be.rejectedWith(Error, 'unauthorized');
   });
 
@@ -58,14 +57,14 @@ describe('accessing couch clustering endpoint', () => {
     expectCorrectMetadata(metadata);
   });
 
-  it('should block unauthenticated access through docker network', async () => {
+  it('should block unauthenticated access through docker network @docker', async () => {
     await startContainer();
     const logs = await getLogs();
     expect(logs.length).to.equal(1);
     expect(logs[0]).to.deep.equal({ error: 'unauthorized', reason: 'Authentication required.' });
   });
 
-  it('should allow authenticated access through docker network', async () => {
+  it('should allow authenticated access through docker network @docker', async () => {
     await startContainer(true);
     const logs = await getLogs();
     expect(logs.length).to.equal(1);
