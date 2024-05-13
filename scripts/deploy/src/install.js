29c1b7f0c00102ed4da235e51708c2c799e4459e
Henok Getachew Alemayehu <henygetachew@gmail.com>
2024-05-13 14:07:38 +0300

    Replace throw with Promise.reject in async func

--- scripts/deploy/src/install.js
+++ scripts/deploy/src/install.js
@@ -54,7 +54,7 @@ async function get_image_tag(chtversion) {
     const response = await fetch(`https://staging.dev.medicmobile.org/_couch/builds_4/medic:medic:${chtversion}`);
     const data = await response.json();
     const tag = data.tags.find(tag => tag.container_name === 'cht-api');
-    if (!tag) throw new Error('cht image tag not found');
+    if (!tag) return Promise.reject(new UserRuntimeError('cht image tag not found'));
     return tag.image.split(':').pop();
 }
 
