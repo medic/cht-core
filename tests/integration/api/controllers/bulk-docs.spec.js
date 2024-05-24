73f391c8d3d024070ba1b05869a4eef07bfe46c0
Diana Barsan <barsan@medic.org>
2024-05-24 06:43:23 +0300

    update bulk docs integration test
    Signed-off-by: Diana Barsan <barsan@medic.org>

--- tests/integration/api/controllers/bulk-docs.spec.js
+++ tests/integration/api/controllers/bulk-docs.spec.js
@@ -84,6 +84,7 @@ describe('bulk-docs handler', () => {
   before(async () => {
     await utils.saveDoc(parentPlace);
     await sUtils.waitForSentinel();
+    await utils.updatePermissions(['district_admin'], ['can_have_multiple_places'], [], true);
     await utils.createUsers(users);
   });
 
