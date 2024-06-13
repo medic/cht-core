b15a2520d784d317c18ed856e16d5252ff1d3a5b
Diana Barsan <barsan@medic.org>
2024-06-13 23:34:32 +0300

    remove debug logs and run all tests

--- tests/integration/couchdb/couch_chttpd.spec.js
+++ tests/integration/couchdb/couch_chttpd.spec.js
@@ -21,8 +21,7 @@ const startContainer = async (useAuthentication) => {
   if (useAuthentication) {
     env.COUCH_AUTH = `${constants.USERNAME}:${constants.PASSWORD}`;
   }
-  const t = await runDockerCommand('docker-compose', ['up', '--build', '--force-recreate'], env);
-  console.log(t);
+  await runDockerCommand('docker-compose', ['up', '--build', '--force-recreate'], env);
 };
 
 const stopContainer = async () => {
@@ -33,7 +32,6 @@ const getLogs = async () => {
   const containerName = (await runDockerCommand('docker-compose', ['ps', '-q', '-a']))[0];
   const logs = await runDockerCommand('docker', ['logs', containerName]);
   try {
-    console.log(logs);
     return logs?.filter(log => log).map(log => JSON.parse(log));
   } catch (err) {
     console.log(logs);
