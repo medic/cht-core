9ec22cbbd497498c80b77333b31f9e4b058fc459
tatilepizs <tatiana@medic.org>
2024-06-27 13:52:22 -0600

    Remove the installation of the BRANCH version after the installation of 'master'

--- tests/e2e/upgrade/upgrade.wdio-spec.js
+++ tests/e2e/upgrade/upgrade.wdio-spec.js
@@ -49,27 +49,6 @@ const deleteUpgradeLogs = async () => {
   await utils.logsDb.bulkDocs(logs);
 };
 
-const upgradeVersion = async (branchVersion) => {
-  await upgradePage.goToUpgradePage();
-  await upgradePage.expandPreReleasesAccordion();
-
-  const installButton = await upgradePage.getInstallButton(branchVersion, TAG);
-  await installButton.click();
-
-  const confirm = await upgradePage.upgradeModalConfirm();
-  await confirm.click();
-
-  await (await upgradePage.cancelUpgradeButton()).waitForDisplayed();
-  await (await upgradePage.deploymentInProgress()).waitForDisplayed();
-  await (await upgradePage.deploymentInProgress()).waitForDisplayed({ reverse: true, timeout: 100000 });
-
-  if (testFrontend) {
-    // https://github.com/medic/cht-core/issues/9186
-    // this is an unfortunate incompatibility between current API and admin app in the old version
-    await (await upgradePage.deploymentComplete()).waitForDisplayed();
-  }
-};
-
 describe('Performing an upgrade', () => {
   before(async () => {
     await utils.saveDocs([...docs.places, ...docs.clinics, ...docs.persons, ...docs.reports]);
@@ -108,7 +87,24 @@ describe('Performing an upgrade', () => {
   });
 
   it('should upgrade to current branch', async () => {
-    await upgradeVersion(BRANCH);
+    await upgradePage.goToUpgradePage();
+    await upgradePage.expandPreReleasesAccordion();
+
+    const installButton = await upgradePage.getInstallButton(BRANCH, TAG);
+    await installButton.click();
+
+    const confirm = await upgradePage.upgradeModalConfirm();
+    await confirm.click();
+
+    await (await upgradePage.cancelUpgradeButton()).waitForDisplayed();
+    await (await upgradePage.deploymentInProgress()).waitForDisplayed();
+    await (await upgradePage.deploymentInProgress()).waitForDisplayed({ reverse: true, timeout: 100000 });
+
+    if (testFrontend) {
+      // https://github.com/medic/cht-core/issues/9186
+      // this is an unfortunate incompatibility between current API and admin app in the old version
+      await (await upgradePage.deploymentComplete()).waitForDisplayed();
+    }
 
     const currentVersion = await upgradePage.getCurrentVersion();
     expect(version.getVersion(true)).to.include(currentVersion);
@@ -146,6 +142,7 @@ describe('Performing an upgrade', () => {
     await browser.refresh();
     await commonPage.waitForPageLoaded();
     await commonPage.goToAboutPage();
+    await (await aboutPage.aboutCard()).waitForDisplayed();
     const expected = TAG || `${utils.escapeBranchName(BRANCH)} (`;
     expect(await aboutPage.getVersion()).to.include(expected);
     await commonPage.logout();
@@ -159,16 +156,27 @@ describe('Performing an upgrade', () => {
       createUser: false
     });
 
-    await upgradeVersion('master');
+    await upgradePage.goToUpgradePage();
+    await upgradePage.expandPreReleasesAccordion();
+
+    await (upgradePage.getInstallButton('master', TAG)).click();
+    await (upgradePage.upgradeModalConfirm()).click();
+
+    await (await upgradePage.cancelUpgradeButton()).waitForDisplayed();
+    await (await upgradePage.deploymentInProgress()).waitForDisplayed();
+    await (await upgradePage.deploymentInProgress()).waitForDisplayed({ reverse: true, timeout: 100000 });
+
+    if (testFrontend) {
+      // https://github.com/medic/cht-core/issues/9186
+      // this is an unfortunate incompatibility between current API and admin app in the old version
+      await (await upgradePage.deploymentComplete()).waitForDisplayed();
+    }
 
     expect(await upgradePage.getBuild()).to.include('alpha');
     await commonPage.goToAboutPage();
     await commonPage.waitForPageLoaded();
     await (await aboutPage.aboutCard()).waitForDisplayed();
     expect(await aboutPage.getVersion()).to.include('master');
-
-    // installing again the BRANCH version for further validations
-    await upgradeVersion(BRANCH);
 
     await commonPage.logout();
   });
