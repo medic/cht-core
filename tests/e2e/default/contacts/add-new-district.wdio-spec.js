4690c50602712ced491a4b1a396a31bf6410b89f
tatilepizs <tatiana@medic.org>
2024-06-28 14:38:13 -0600

    clean and organize add-new-district.wdio-spec.js e2e test

--- tests/e2e/default/contacts/add-new-district.wdio-spec.js
+++ tests/e2e/default/contacts/add-new-district.wdio-spec.js
@@ -6,11 +6,12 @@ const commonPage = require('@page-objects/default/common/common.wdio.page');
 const contactPage = require('@page-objects/default/contacts/contacts.wdio.page');
 const loginPage = require('@page-objects/default/login/login.wdio.page');
 
-
 describe('Add new district tests : ', () => {
+
   before(async () => await loginPage.cookieLogin());
+
   afterEach(async () => {
-    sentinelUtils.waitForSentinel();
+    await sentinelUtils.waitForSentinel();
     await utils.revertDb([/^form:/], true);
   });
 
@@ -79,7 +80,7 @@ describe('Add new district tests : ', () => {
     expect(updatedDistrict.contact_type).to.equal('not a district_hospital'); // editing didn't overwrite
 
     // expect to have a single children section
-    expect(await (await contactPage.childrenCards()).length).to.equal(1);
+    expect((await contactPage.childrenCards()).length).to.equal(1);
     // expect to list two children
     expect(await contactPage.getAllRHSPeopleNames()).to.deep.equal(['Tudor', 'Ginny']);
   });
