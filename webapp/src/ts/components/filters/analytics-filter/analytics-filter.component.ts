50c0765514c1ab684c35f39a706139f7e1fb09ac
Ben Kiarie <kiarie@medic.org>
2024-07-04 19:03:01 +0300

    chore: add filter button check and update color

--- webapp/src/ts/components/filters/analytics-filter/analytics-filter.component.ts
+++ webapp/src/ts/components/filters/analytics-filter/analytics-filter.component.ts
@@ -5,24 +5,38 @@ import {
   EventEmitter,
   Input,
   OnDestroy,
+  OnInit,
   Output } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Subscription, filter } from 'rxjs';
+import { AuthService } from '@mm-services/auth.service';
+import { UserSettingsService } from '@mm-services/user-settings.service';
+import { SessionService } from '@mm-services/session.service';
 
+const AGGREGATE_TARGET_URL = '/analytics/target-aggregates';
 @Component({
   selector: 'mm-analytics-filters',
   templateUrl: './analytics-filter.component.html'
 })
-export class AnalyticsFilterComponent implements AfterContentInit, AfterContentChecked, OnDestroy {
+export class AnalyticsFilterComponent implements AfterContentInit, AfterContentChecked, OnInit, OnDestroy {
   @Input() analyticsModules: any[] = [];
-  @Input() showFilterButton: boolean = false;
   @Output() toggleFilter: EventEmitter<any> = new EventEmitter();
   activeModule;
   subscriptions: Subscription = new Subscription();
+  showFilterButton;
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService,
+    private userSettingsService: UserSettingsService,
+    private sessionService: SessionService
   ) { }
+
+  ngOnInit() {
+    this.canDisplayFilterButton();
+    this.subscribeToRouteChanges();
+  }
 
   ngAfterContentInit() {
     const subscription = this.route.url.subscribe(() => this.setActiveModule());
@@ -43,5 +57,52 @@ export class AnalyticsFilterComponent implements AfterContentInit, AfterContentC
     this.activeModule = this.analyticsModules?.find(module => {
       return module.id === this.route.snapshot?.firstChild?.data?.moduleId;
     });
+  }
+
+  private subscribeToRouteChanges() {
+    const routeSubscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.canDisplayFilterButton();
+    });
+    this.subscriptions.add(routeSubscription);
+  }
+
+  private hasMultipleFacilities() {
+    return this.userSettingsService
+      .get()
+      .then((userSettings: any) => {
+        const facility = userSettings.facility_id;
+        return Array.isArray(facility) && facility.length > 1;
+      });
+  }
+
+  private checkPermissions() {
+    return Promise
+      .all([
+        this.authService.has('!can_view_old_filter_and_search'),
+        this.authService.has('!can_view_old_action_bar')
+      ])
+      .then(([cannotViewOldFilter, cannotViewOldActionBar]) => {
+        return cannotViewOldFilter && cannotViewOldActionBar;
+      })
+      .catch(error => {
+        console.error('Error checking permissions:', error);
+        return false;
+      });
+  }
+
+  private isTargetAggregates() {
+    return this.router.url.startsWith(AGGREGATE_TARGET_URL);
+  }
+
+  private async canDisplayFilterButton() {
+    const isAdmin = this.sessionService.isAdmin();
+    const [hasMultipleFacilities, checkPermissions] = await Promise.all([
+      this.hasMultipleFacilities(),
+      this.checkPermissions(),
+    ]);
+
+    this.showFilterButton = hasMultipleFacilities && !isAdmin && checkPermissions && this.isTargetAggregates();
   }
 }
--- webapp/src/ts/modules/analytics/analytics.component.ts
+++ webapp/src/ts/modules/analytics/analytics.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription, filter } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 import { GlobalActions } from '@mm-actions/global';
 import { Selectors } from '@mm-selectors/index';
 import { SidebarFilterService } from '@mm-services/sidebar-filter.service';
-import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   templateUrl: './analytics.component.html'
@@ -15,20 +14,16 @@ export class AnalyticsComponent implements OnInit, OnDestroy {
   private globalActions;
   subscriptions: Subscription = new Subscription();
   analyticsModules = [];
-  showFilterButton = false;
 
   constructor(
     private store: Store,
-    private router: Router,
-    private sidebarFilterService: SidebarFilterService
+    private sidebarFilterService: SidebarFilterService,
   ) {
     this.globalActions = new GlobalActions(store);
   }
 
   ngOnInit() {
-    this.checkCurrentRoute();
     this.subscribeToStore();
-    this.subscribeToRouteChanges();
     this.globalActions.unsetSelected();
   }
 
@@ -41,24 +36,6 @@ export class AnalyticsComponent implements OnInit, OnDestroy {
       .select(Selectors.getAnalyticsModules)
       .subscribe(analyticsModules => this.analyticsModules = analyticsModules);
     this.subscriptions.add(subscription);
-  }
-
-  private subscribeToRouteChanges() {
-    const routeSubscription = this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event: NavigationEnd) => {
-      this.canDisplayFilterButton(event.urlAfterRedirects);
-    });
-    this.subscriptions.add(routeSubscription);
-  }
-
-  private canDisplayFilterButton(currentRoute) {
-    this.showFilterButton = currentRoute.includes('target-aggregates');
-  }
-
-  private checkCurrentRoute() {
-    const currentUrl = this.router.url;
-    this.canDisplayFilterButton(currentUrl);
   }
 
   onToggleFilter() {
