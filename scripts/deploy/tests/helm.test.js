3ddf32f593a24f49d19dd98919672ea53605e707
Henok Getachew Alemayehu <henygetachew@gmail.com>
2024-07-05 20:38:05 +0300

    Better tests

--- scripts/deploy/tests/helm.test.js
+++ scripts/deploy/tests/helm.test.js
@@ -6,6 +6,7 @@ import UserRuntimeError from '../src/error.js';
 
 const MEDIC_REPO_NAME = 'medic';
 const MEDIC_REPO_URL = 'https://docs.communityhealthtoolkit.org/helm-charts';
+const CHT_CHART_NAME = `${MEDIC_REPO_NAME}/cht-chart-4x`;
 
 describe('helmInstallOrUpdate function', () => {
   const fakeValuesFile = 'path_to_fake_values.yaml';
@@ -31,68 +32,79 @@ describe('helmInstallOrUpdate function', () => {
   it('should upgrade an existing release', () => {
     // Mock the response to simulate existing release
     child_process.execSync.withArgs(`helm list -n ${fakeNamespace}`).returns(Buffer.from('test-project'));
-    child_process.execSync.returns(Buffer.from('Upgrade successful'));
+    child_process.execSync.withArgs('helm repo list -o json')
+      .returns(Buffer.from(JSON.stringify([{ name: MEDIC_REPO_NAME, url: MEDIC_REPO_URL }])));
+    child_process.execSync.withArgs(`helm repo update ${MEDIC_REPO_NAME}`).returns(Buffer.from('Update successful'));
+    const upgradeCommand = `helm upgrade --install test-project ${CHT_CHART_NAME} --version 5.0.0 ` +
+      `--namespace ${fakeNamespace} --values ${fakeValuesFile} --set cht_image_tag=${fakeImageTag}`;
+    child_process.execSync.withArgs(upgradeCommand, { stdio: 'inherit' }).returns(Buffer.from('Upgrade successful'));
 
     helmInstallOrUpdate(fakeValuesFile, fakeNamespace, fakeValues, fakeImageTag);
 
-    const expectedCommand = `helm upgrade` +
-        ` --install test-project medic/cht-chart-4x` +
-        ` --version 5.0.0 --namespace test-namespace` +
-        ` --values ${fakeValuesFile} --set cht_image_tag=${fakeImageTag}`;
-    expect(child_process.execSync.calledWith(expectedCommand)).to.be.true;
+    expect(child_process.execSync.callCount).to.equal(5);
   });
 
   it('should install a new release when no release exists', () => {
     // Mock the response to simulate no existing release
     child_process.execSync.withArgs(`helm list -n ${fakeNamespace}`).returns(Buffer.from(''));
-    child_process.execSync.returns(Buffer.from('Install successful'));
+    child_process.execSync.withArgs('helm repo list -o json')
+      .returns(Buffer.from(JSON.stringify([{ name: MEDIC_REPO_NAME, url: MEDIC_REPO_URL }])));
+    child_process.execSync.withArgs(`helm repo update ${MEDIC_REPO_NAME}`).returns(Buffer.from('Update successful'));
+    child_process.execSync.withArgs(`kubectl get namespace ${fakeNamespace}`).throws(new Error('Namespace not found'));
+    const installCommand = `helm install test-project ${CHT_CHART_NAME} --version 5.0.0 --namespace ${fakeNamespace} ` +
+      `--create-namespace --values ${fakeValuesFile} --set cht_image_tag=${fakeImageTag}`;
+    child_process.execSync.withArgs(installCommand, { stdio: 'inherit' }).returns(Buffer.from('Install successful'));
 
     helmInstallOrUpdate(fakeValuesFile, fakeNamespace, fakeValues, fakeImageTag);
 
-    const expectedCommand = `helm install test-project medic/cht-chart-4x` + 
-        ` --version 5.0.0 --namespace test-namespace --create-namespace` +
-        ` --values ${fakeValuesFile} --set cht_image_tag=${fakeImageTag}`;
-    expect(child_process.execSync.calledWith(expectedCommand)).to.be.true;
+    expect(child_process.execSync.callCount).to.equal(5);
   });
 
   it('should exit when error thrown', () => {
-    // Force an error to be thrown
-    child_process.execSync.throws(new Error('fake error'));
+    // Mock the response to simulate error
+    child_process.execSync.withArgs('helm repo list -o json')
+      .returns(Buffer.from(JSON.stringify([{ name: MEDIC_REPO_NAME, url: MEDIC_REPO_URL }])));
+    child_process.execSync.withArgs(`helm repo update ${MEDIC_REPO_NAME}`).returns(Buffer.from('Update successful'));
+    child_process.execSync.withArgs(`helm list -n ${fakeNamespace}`).throws(new Error('fake error'));
 
     helmInstallOrUpdate(fakeValuesFile, fakeNamespace, fakeValues, fakeImageTag);
 
     expect(process.exit.calledWith(1)).to.be.true;
+    expect(child_process.execSync.callCount).to.equal(4);
   });
 
   it('should handle helm repo not found and add it', () => {
     // Mock the response to simulate helm repo not found
     const fakeRepoList = JSON.stringify([]);
     child_process.execSync.withArgs('helm repo list -o json').returns(Buffer.from(fakeRepoList));
+    child_process.execSync.withArgs(`helm repo add ${MEDIC_REPO_NAME} ${MEDIC_REPO_URL}`, { stdio: 'inherit' })
+      .returns(Buffer.from('Add successful'));
 
     ensureMedicHelmRepo();
 
-    expect(child_process.execSync.calledWith(`helm repo add ${MEDIC_REPO_NAME} ${MEDIC_REPO_URL}`)).to.be.true;
+    expect(child_process.execSync.callCount).to.equal(2);
   });
 
   it('should handle existing helm repo and update it', () => {
     // Mock the response to simulate existing helm repo
     const fakeRepoList = JSON.stringify([{ name: MEDIC_REPO_NAME, url: MEDIC_REPO_URL }]);
     child_process.execSync.withArgs('helm repo list -o json').returns(Buffer.from(fakeRepoList));
+    child_process.execSync.withArgs(`helm repo update ${MEDIC_REPO_NAME}`, { stdio: 'inherit' })
+      .returns(Buffer.from('Update successful'));
 
     ensureMedicHelmRepo();
 
-    expect(child_process.execSync.calledWith(`helm repo update ${MEDIC_REPO_NAME}`)).to.be.true;
+    expect(child_process.execSync.callCount).to.equal(2);
   });
 
   it('should exit if helm repo url does not match', () => {
     // Mock the response to simulate helm repo url mismatch
     const fakeRepoList = JSON.stringify([{ name: MEDIC_REPO_NAME, url: 'https://wrong.url' }]);
     child_process.execSync.withArgs('helm repo list -o json').returns(Buffer.from(fakeRepoList));
-    child_process.execSync.throws(
-      new UserRuntimeError(`Medic repo found but url not matching '${MEDIC_REPO_URL}', see: helm repo list`)
-    );
+
     ensureMedicHelmRepo();
 
     expect(process.exit.calledWith(1)).to.be.true;
+    expect(child_process.execSync.callCount).to.equal(1);
   });
 });
