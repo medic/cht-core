1044d6b77c27a8b3d8aa53ef0bdb8f882f923508
Ben Kiarie <kiarie@medic.org>
2024-07-05 15:42:11 +0300

    chore: display radio buttons

--- webapp/src/ts/modules/analytics/analytics-target-aggregates-sidebar-filter.component.ts
+++ webapp/src/ts/modules/analytics/analytics-target-aggregates-sidebar-filter.component.ts
@@ -5,9 +5,11 @@ import { Store } from '@ngrx/store';
 import { GlobalActions } from '@mm-actions/global';
 import { Selectors } from '@mm-selectors/index';
 import { GetDataRecordsService } from '@mm-services/get-data-records.service';
+import { SettingsService } from '@mm-services/settings.service';
 import { TelemetryService } from '@mm-services/telemetry.service';
 import { UserSettingsService } from '@mm-services/user-settings.service';
 
+const CONTACT_TYPE_ID = 'district_hospital';
 @Component({
   selector: 'mm-analytics-target-aggregates-sidebar-filter',
   templateUrl: './analytics-target-aggregates-sidebar-filter.component.html'
@@ -19,10 +21,13 @@ export class AnalyticsTargetAggregatesSidebarFilterComponent implements OnInit, 
   private globalActions;
   subscriptions: Subscription = new Subscription();
   isOpen = false;
+  userFacilities;
+  facilityLabel;
 
   constructor(
     private store: Store,
     private getDataRecordsService: GetDataRecordsService,
+    private settingsService: SettingsService,
     private telemetryService: TelemetryService,
     private userSettingsService: UserSettingsService,
   ) {
@@ -32,6 +37,7 @@ export class AnalyticsTargetAggregatesSidebarFilterComponent implements OnInit, 
   ngOnInit() {
     this.subscribeToStore();
     this.getUserFacility();
+    this.getFacilityLabel();
   }
 
   ngOnDestroy() {
@@ -67,7 +73,24 @@ export class AnalyticsTargetAggregatesSidebarFilterComponent implements OnInit, 
     return this.getDataRecordsService
       .get(facilityId)
       .then(places => {
-        console.log('places', places);
+        this.userFacilities = places;
       });
+  }
+
+  private getFacilityLabel() {
+    this.settingsService
+      .get()
+      .then((settings: any) => {
+        const placeLabel = settings.contact_types.find(type => type.id === CONTACT_TYPE_ID);
+        this.facilityLabel = placeLabel.name_key;
+      })
+      .catch(error => {
+        console.error('Error fetching settings', error);
+        this.facilityLabel = 'Facility';
+      });
+  }
+
+  fetchAggregateTargets(facilityId) {
+    console.log('Selected facility ID:', facilityId);
   }
 }
