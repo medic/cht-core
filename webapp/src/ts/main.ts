8b0d5394be2ff4605b9b86397b5b04eb022a1de0
latin-panda <66472237+latin-panda@users.noreply.github.com>
2024-07-08 19:54:56 +0700

    does it work with e2e?

--- webapp/src/ts/main.ts
+++ webapp/src/ts/main.ts
@@ -11,7 +11,7 @@ window.$ = window.jQuery = require('jquery');
 import { enableProdMode } from '@angular/core';
 import '@angular/compiler';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import pouchdbDebug from 'pouchdb-debug';
+// import pouchdbDebug from 'pouchdb-debug';
 import * as $ from 'jquery';
 
 import { AppModule } from './app.module';
@@ -44,7 +44,7 @@ Object.defineProperties($, {
   htmlPrefilter: { value: (html) => html.replace(rxhtmlTag, '<$1></$2>') }
 });
 
-window.PouchDB.plugin(pouchdbDebug);
+// window.PouchDB.plugin(pouchdbDebug);
 bootstrapper(POUCHDB_OPTIONS)
   .then(() => {
     window.startupTimes.bootstrapped = performance.now();
