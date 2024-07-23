105d5db67dde85e1d0c503d52b1781f8c2c4b78d
Sugat Bajracharya <30311933+sugat009@users.noreply.github.com>
2024-07-23 10:37:05 +0545

    Update shared-libs/cht-datasource/src/qualifier.ts
    Co-authored-by: Joshua Kuestersteffen <jkuester@kuester7.com>

--- shared-libs/cht-datasource/src/qualifier.ts
+++ shared-libs/cht-datasource/src/qualifier.ts
@@ -29,7 +29,7 @@ export const isUuidQualifier = (identifier: unknown): identifier is UuidQualifie
 };
 
 /**
- * A qualifier that identifies an entity based on type
+A qualifier that identifies contacts based on type.
  */
 export type ContactTypeQualifier = Readonly<{ contactType: string }>;
 
