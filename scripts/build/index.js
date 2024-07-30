5605cc37eedbbd61c8c9d45044a585f1b580c199
Gareth Bowen <gareth@medic.org>
2024-07-30 17:48:26 +0200

    copy node modules direct into service directory

--- scripts/build/index.js
+++ scripts/build/index.js
@@ -241,9 +241,9 @@ const buildServiceImages = async () => {
   await exec('cp', ['-r', './node_modules', './package.json', './package-lock.json', workingDir]);
   console.log('packing modules');
   await exec('npm', ['pack'], { cwd: workingDir });
-  console.log('extracting modules');
-  await exec('tar', ['-Pxvzf', `${workingDir}/medic-4.10.0.tgz`, '-C', './build']); // TODO grab the correct version
   for (const service of versions.SERVICES) {
+    console.log('extracting modules');
+    await exec('tar', ['-Pxvzf', `${workingDir}/medic-4.10.0.tgz`, '-C', './' + service]); // TODO grab the correct version
     const tag = versions.getImageTag(service);
     if (INTERNAL_CONTRIBUTOR) {
       await buildMultiPlatformServiceImage(service, tag);
