99862909c660198c9b5c50fe8e73b4be7e2bc704
Joshua Kuestersteffen <jkuester@kuester7.com>
2024-08-06 18:08:28 -0500

    Add tests for existing tel.validate logic

--- webapp/tests/karma/js/enketo/widgets/phone-widget.spec.ts
+++ webapp/tests/karma/js/enketo/widgets/phone-widget.spec.ts
@@ -1,14 +1,27 @@
 import jQuery from 'jquery';
 import sinon from 'sinon';
 import { expect } from 'chai';
+import { FormModel } from 'enketo-core';
+import phoneNumber from '@medic/phone-number';
+const PhoneWidget = require('../../../../../src/js/enketo/widgets/phone-widget');
 
 describe('Enketo: Phone Widget', () => {
   const $ = jQuery;
   const inputName = '/some/input/name';
-  let phoneWidget;
+  const SETTINGS = { hello: 'world' };
+  const DENORMALIZED_NUMBER = '+1 (650) 222-3333';
+  const NORMALIZED_NUMBER = '+16502223333';
+
+  let phoneNumberValidate;
+  let phoneNumberNormalize;
   let settingsService;
+  let dbQuery;
+  let dbService;
+  let originalCHTCore;
+
   const inputSelector = (name) => $('input[name="' + name + '"]');
   const proxySelector = (name) => inputSelector(name).prev();
+
   const buildHtml = (type = 'tel') => {
     const html = 
       '<div id="phone-widget-test"><label class="question non-select"> \
@@ -20,16 +33,31 @@ describe('Enketo: Phone Widget', () => {
       </label></div>';
     document.body.insertAdjacentHTML('afterbegin', html);
   };
+
+  const buildContactFormHtml = (id) => {
+    const formHtml = `<div id="contact-form" data-editing="${id}"></div>`;
+    document.body.insertAdjacentHTML('afterbegin', formHtml);
+  };
+
+  before(() => originalCHTCore = window.CHTCore);
+  after(() => window.CHTCore = originalCHTCore);
   
   beforeEach(() => {
-    settingsService = { get: sinon.stub().resolves({}) };
-    // Fetch phone widget.
-    phoneWidget = require('../../../../../src/js/enketo/widgets/phone-widget');
+    phoneNumberValidate = sinon.stub(phoneNumber, 'validate');
+    phoneNumberNormalize = sinon.stub(phoneNumber, 'normalize').returns(NORMALIZED_NUMBER);
+    settingsService = { get: sinon.stub().resolves(SETTINGS) };
+    dbQuery = sinon.stub().resolves({ rows: [] });
+    dbService = { get: sinon.stub().returns({ query: dbQuery }) };
+    window.CHTCore = {
+      Settings: settingsService,
+      DB: dbService
+    };
   });
 
   afterEach(() => {
     sinon.restore();
     $('#phone-widget-test').remove();
+    $('#contact-form').remove();
   });
 
   it('should be placed in DOM when widget is added', () => {
@@ -37,73 +65,144 @@ describe('Enketo: Phone Widget', () => {
     const input = inputSelector(inputName);
     const proxyInput = proxySelector(inputName);
 
-    new phoneWidget($(phoneWidget.selector)[0], {}, settingsService);
+    new PhoneWidget($(PhoneWidget.selector)[0], {}, settingsService);
 
     // Check a proxy input field is added, and the real one is hidden.
     expect($('input').length).to.equal(2);
     expect(input.is(':visible')).to.equal(false);
     expect(proxyInput.length).to.equal(1);
     expect(proxyInput.is(':visible')).to.equal(true);
+    expect(settingsService.get.calledOnceWithExactly()).to.be.true;
   });
 
-  it('should format input when input value change', () => {
+  it('should format input when input value change', async () => {
     buildHtml();
-    new phoneWidget($(phoneWidget.selector)[0], {}, settingsService);
+    await new PhoneWidget($(PhoneWidget.selector)[0], {}, settingsService);
     const input = inputSelector(inputName);
     const proxyInput = proxySelector(inputName);
 
     proxyInput
-      .val('+1 (650) 222-3333')
+      .val(DENORMALIZED_NUMBER)
       .change();
 
     expect(proxyInput.length).to.equal(1);
     expect(input.length).to.equal(1);
-    expect(input.val()).to.equal('+16502223333');
+    expect(input.val()).to.equal(NORMALIZED_NUMBER);
+    expect(settingsService.get.calledOnceWithExactly()).to.be.true;
+    expect(phoneNumberNormalize.args).to.deep.equal([
+      [{ }, DENORMALIZED_NUMBER],
+      [SETTINGS, DENORMALIZED_NUMBER]
+    ]);
   });
 
   it('should still format if no settings are found', () => {
     buildHtml();
-    new phoneWidget($(phoneWidget.selector)[0], {}, settingsService);
+    // Not awaiting the settings to be returned
+    new PhoneWidget($(PhoneWidget.selector)[0], {}, settingsService);
     const input = inputSelector(inputName);
     const proxyInput = proxySelector(inputName);
 
     proxyInput
-      .val('+1 (650) 222-3333')
+      .val(DENORMALIZED_NUMBER)
       .change();
 
-    expect(input.val()).to.equal('+16502223333');
+    expect(input.val()).to.equal(NORMALIZED_NUMBER);
+    expect(settingsService.get.calledOnceWithExactly()).to.be.true;
+    expect(phoneNumberNormalize.calledOnceWithExactly({ }, DENORMALIZED_NUMBER)).to.be.true;
   });
 
   it('should not format invalid input', () => {
+    phoneNumberNormalize.returns(false);
     buildHtml();
-    new phoneWidget($(phoneWidget.selector)[0], {}, settingsService);
+    new PhoneWidget($(PhoneWidget.selector)[0], {}, settingsService);
     const input = inputSelector(inputName);
     const proxyInput = proxySelector(inputName);
-    const invalid = '+1 (650) 222-33333333';
 
     proxyInput
-      .val(invalid)
+      .val(DENORMALIZED_NUMBER)
       .change();
 
-    expect(input.val()).to.equal(invalid);
+    expect(input.val()).to.equal(DENORMALIZED_NUMBER);
+    expect(settingsService.get.calledOnceWithExactly()).to.be.true;
+    expect(phoneNumberNormalize.calledOnceWithExactly({ }, DENORMALIZED_NUMBER)).to.be.true;
   });
 
   it('should keep formatted input when value is valid', () => {
     buildHtml();
-    new phoneWidget($(phoneWidget.selector)[0], {}, settingsService);
+    new PhoneWidget($(PhoneWidget.selector)[0], {}, settingsService);
     const input = inputSelector(inputName);
     const proxyInput = proxySelector(inputName);
-    const valid = '+16502223333';
 
     proxyInput
-      .val(valid)
+      .val(NORMALIZED_NUMBER)
       .change();
 
-    expect(input.val()).to.equal(valid);
+    expect(input.val()).to.equal(NORMALIZED_NUMBER);
+    expect(settingsService.get.calledOnceWithExactly()).to.be.true;
+    expect(phoneNumberNormalize.calledOnceWithExactly({ }, NORMALIZED_NUMBER)).to.be.true;
   });
 
   it('should not modify non-phone fields', () => {
     buildHtml('other');
-    expect($(phoneWidget.selector).length).to.equal(0);
+    expect($(PhoneWidget.selector).length).to.equal(0);
+  });
+
+  describe('types.tel validate', () => {
+    it('returns true for a valid phone number', async () => {
+      phoneNumberValidate.returns(true);
+
+      const result = await FormModel.prototype.types.tel.validate(DENORMALIZED_NUMBER);
+
+      expect(result).to.be.true;
+      expect(settingsService.get.calledOnceWithExactly()).to.be.true;
+      expect(phoneNumberValidate.calledOnceWithExactly(SETTINGS, DENORMALIZED_NUMBER)).to.be.true;
+      expect(phoneNumberNormalize.calledOnceWithExactly(SETTINGS, DENORMALIZED_NUMBER)).to.be.true;
+      expect(dbService.get.calledOnceWithExactly()).to.be.true;
+      expect(dbQuery.calledOnceWithExactly('medic-client/contacts_by_phone', { key: NORMALIZED_NUMBER })).to.be.true;
+    });
+
+    it('throws error for invalid phone number', async () => {
+      phoneNumberValidate.returns(false);
+
+      await expect(FormModel.prototype.types.tel.validate(DENORMALIZED_NUMBER)).to.be
+        .rejectedWith(`invalid phone number: "${DENORMALIZED_NUMBER}"`);
+
+      expect(settingsService.get.calledOnceWithExactly()).to.be.true;
+      expect(phoneNumberValidate.calledOnceWithExactly(SETTINGS, DENORMALIZED_NUMBER)).to.be.true;
+      expect(phoneNumberNormalize.notCalled).to.be.true;
+      expect(dbService.get.notCalled).to.be.true;
+      expect(dbQuery.notCalled).to.be.true;
+    });
+
+    it('throws error for duplicate phone number', async () => {
+      buildContactFormHtml('my-contact-id');
+      phoneNumberValidate.returns(true);
+      dbQuery.resolves({ rows: [{ id: 'some-id' }] });
+
+      await expect(FormModel.prototype.types.tel.validate(DENORMALIZED_NUMBER)).to.be
+        .rejectedWith(`phone number not unique: "${DENORMALIZED_NUMBER}"`);
+
+      expect(settingsService.get.calledOnceWithExactly()).to.be.true;
+      expect(phoneNumberValidate.calledOnceWithExactly(SETTINGS, DENORMALIZED_NUMBER)).to.be.true;
+      expect(phoneNumberNormalize.calledOnceWithExactly(SETTINGS, DENORMALIZED_NUMBER)).to.be.true;
+      expect(dbService.get.calledOnceWithExactly()).to.be.true;
+      expect(dbQuery.calledOnceWithExactly('medic-client/contacts_by_phone', { key: NORMALIZED_NUMBER })).to.be.true;
+    });
+
+    it('returns true for a duplicate phone number when it is for the contact being edited', async () => {
+      const contactId = 'my-contact-id';
+      buildContactFormHtml(contactId);
+      phoneNumberValidate.returns(true);
+      dbQuery.resolves({ rows: [{ id: contactId }] });
+
+      const result = await FormModel.prototype.types.tel.validate(DENORMALIZED_NUMBER);
+
+      expect(result).to.be.true;
+      expect(settingsService.get.calledOnceWithExactly()).to.be.true;
+      expect(phoneNumberValidate.calledOnceWithExactly(SETTINGS, DENORMALIZED_NUMBER)).to.be.true;
+      expect(phoneNumberNormalize.calledOnceWithExactly(SETTINGS, DENORMALIZED_NUMBER)).to.be.true;
+      expect(dbService.get.calledOnceWithExactly()).to.be.true;
+      expect(dbQuery.calledOnceWithExactly('medic-client/contacts_by_phone', { key: NORMALIZED_NUMBER })).to.be.true;
+    });
   });
 });
