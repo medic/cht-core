15b6e3941930060c3650b3cdbbeb829d56596237
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2024-08-07 13:28:56 +0545

    Cleanup after refactoring

--- shared-libs/cht-datasource/src/person.ts
+++ shared-libs/cht-datasource/src/person.ts
@@ -27,12 +27,6 @@ export namespace v1 {
     readonly parent?: Place.v1.PlaceWithLineage | NormalizedParent,
   }
 
-  interface getPageParams {
-    personType: ContactTypeQualifier;
-    limit?: number;
-    skip?: number;
-  }
-
   const assertPersonQualifier: (qualifier: unknown) => asserts qualifier is UuidQualifier = (qualifier: unknown) => {
     if (!isUuidQualifier(qualifier)) {
       throw new Error(`Invalid identifier [${JSON.stringify(qualifier)}].`);
