fbdb8c8c4b8a096fc06214b17feeaea8045ce8bb
Sugat Bajracharya <30311933+sugat009@users.noreply.github.com>
2024-08-14 09:20:19 +0545

    Update shared-libs/cht-datasource/src/libs/core.ts
    Co-authored-by: Joshua Kuestersteffen <jkuester@kuester7.com>

--- shared-libs/cht-datasource/src/libs/core.ts
+++ shared-libs/cht-datasource/src/libs/core.ts
@@ -115,7 +115,7 @@ export abstract class AbstractDataContext implements DataContext {
 
 /**
  * Represents a page of results. The `data` array contains the results for this page. The `cursor` field contains a
- * key that can be used to fetch the next page of results. If no `cursor` value is returned, there are no additional
+ * token that can be used to fetch the next page of results. If no `cursor` value is returned, there are no additional
  * results available. (Note that no assumptions should be made about the _contents_ of the cursor string.)
  * @typeParam T the type of the data in the page
  */
