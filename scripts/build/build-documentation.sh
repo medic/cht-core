1a59345f36907682ae670b3b54561b115335ca47
Gareth Bowen <gareth@medic.org>
2024-08-14 07:19:35 +0200

    chore(#9320): rationalise more dependencies and update dependabot

--- scripts/build/build-documentation.sh
+++ scripts/build/build-documentation.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "build-documentation: building jsdocs"
-jsdoc -d jsdocs/admin -c node_modules/angular-jsdoc/common/conf.json -t node_modules/angular-jsdoc/angular-template admin/src/js/**/*.js
+jsdoc -d jsdocs/admin -c admin/node_modules/angular-jsdoc/common/conf.json -t admin/node_modules/angular-jsdoc/angular-template admin/src/js/**/*.js
 jsdoc -d jsdocs/sentinel sentinel/src/**/*.js
 jsdoc -d jsdocs/api -R api/README.md api/src/**/*.js
 jsdoc -d jsdocs/shared-libs shared-libs/**/src/**/*.js
