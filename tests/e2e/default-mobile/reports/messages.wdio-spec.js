595a0f1145863db0e6bba13bb227f85c8a2343dc
Aniekan Eshiet <aeshiet@Aniekans-Medic-MacBook-Pro.local>
2024-08-16 10:43:29 +0100

    add update settings

--- tests/e2e/default-mobile/reports/messages.wdio-spec.js
+++ tests/e2e/default-mobile/reports/messages.wdio-spec.js
@@ -8,7 +8,7 @@ const commonElements = require('@page-objects/default/common/common.wdio.page');
 const reportsPage = require('@page-objects/default/reports/reports.wdio.page');
 const sentinelUtils = require('@utils/sentinel');
 const pregnancyFactory = require('@factories/cht/reports/pregnancy');
-
+const helperFunctions = require('../utils/helper-functions');
 
 describe('Reports tab', () => {
   const places = placeFactory.generateHierarchy();
@@ -16,56 +16,47 @@ describe('Reports tab', () => {
   const healthCenter = places.get('health_center');
   const clinic = places.get('clinic');
 
-  const offlineUserContact = personFactory.build({
-    name: 'OfflineUser',
-    phone: '+25474425741',
+  const onlineUserContact = personFactory.build({
+    name: 'OnlineUser',
+    phone: '+25475525749',
     parent: { _id: healthCenter._id, parent: healthCenter.parent }
   });
 
-  const offlineUser = userFactory.build({ place: healthCenter._id, roles: ['chw'], contact: offlineUserContact });
+  const onlineUser = userFactory.build({ place: healthCenter._id, roles: ['program_officer'], contact: onlineUserContact });
 
   const person = personFactory.build({
     _id: 'patient1',
-    phone: '+14152223344',
+    phone: '+25475525741',
     name: 'patient1',
     parent: { _id: clinic._id, parent: clinic.parent }
   });
 
-  const report = pregnancyFactory.build({ contact: offlineUserContact, fields: { patient_id: person._id, case_id: 'case-12' } });
+  const report = pregnancyFactory.build({ contact: onlineUserContact, fields: { patient_id: person._id, case_id: 'case-12' } });
 
   before(async () => {
-    //await utils.updateSettings(appSettings.WITH_SMS_FORMS, true);
     await utils.saveDocs([...places.values(), person]);
     await utils.saveDocs([report]);
-    await utils.createUsers([offlineUser]);
-    await loginPage.login(offlineUser);
+    await utils.createUsers([onlineUser]);
+    await loginPage.login(onlineUser);
   });
 
   after(async () => await utils.revertSettings(true));
 
-  afterEach(async () => await utils.deleteAllDocs([/^form:/].concat(...places.values()).concat(person._id)));
-
   it('should see option to send message', async () => {
-    await sentinelUtils.waitForSentinel([report._id]);
+    await helperFunctions.updateSettings(onlineUser);
 
     await commonElements.goToReports();
     const firstReport = await reportsPage.firstReport();
     await reportsPage.openSelectedReport(firstReport);
     // await commonElements.waitForPageLoaded();
 
-    await browser.pause(900000)
+    await browser.debug();
+    // await browser.pause(900000)
 
     // const fabLabels = await commonElements.getFastActionItemsLabels();
     // const fabLabels = await commonElements.multipleActions();
     await commonElements.clickFastActionFAB({ waitForList: false });
 
     expect(fabLabels).to.include.members(['Edit facility', 'Send message', 'Fake']);
-
-    // await browser.pause(480000)
-
-    // await verifyListReportContent({ formName: 'REF_REF' });
-    // await verifyOpenReportContent({ formName: 'REF_REF', subject: person.name });
-
-    // await browser.pause(480000)
   });
 });
