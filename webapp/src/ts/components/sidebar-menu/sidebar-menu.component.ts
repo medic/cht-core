e43268adaf6ebfb482ba950fe58963d9295630dc
latin-panda <66472237+latin-panda@users.noreply.github.com>
2024-08-22 17:22:09 +0700

    translations and layout

---
+++ webapp/src/ts/components/sidebar-menu/sidebar-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mm-sidebar-menu',
+  templateUrl: './sidebar-menu.component.html',
+})
+export class SidebarMenuComponent {
+  close() {
+
+  }
+}
