5976bd872a774acff7ae74167f997a5daa107677
Aniekan Eshiet <aeshiet@Aniekans-Medic-MacBook-Pro.local>
2024-08-22 11:44:49 +0100

    chore: update old nav test path 2

--- tests/e2e/default/suites.js
+++ tests/e2e/default/suites.js
@@ -10,29 +10,32 @@ const SUITES = {
     './old-navigation/**/*.wdio-spec.js',
     // './privacy-policy/**/*.wdio-spec.js',
   ],
-  // workflows: [
+  workflows: [
+    './old-navigation/**/*.wdio-spec.js'
   //   './analytics/**/*.wdio-spec.js',
   //   './contacts/**/*.wdio-spec.js',
   //   './reports/**/*.wdio-spec.js',
   //   './targets/**/*.wdio-spec.js',
   //   './tasks/**/*.wdio-spec.js',
   //   './sms/**/*.wdio-spec.js',
-  // ],
-  // data: [
+  ],
+  data: [
+    './old-navigation/**/*.wdio-spec.js'
   //   './db/**/*.wdio-spec.js',
   //   './purge/**/*.wdio-spec.js',
   //   './telemetry/**/*.wdio-spec.js'
-  // ],
-  // lowLevel: [
+  ],
+  lowLevel: [
+    './old-navigation/**/*.wdio-spec.js'
   //   './pwa/**/*.wdio-spec.js',
   //   './service-worker/**/*.wdio-spec.js',
   //   './transitions/**/*.wdio-spec.js',
   //   './logging/**/*.wdio-spec.js'
-  // ],
-  // enketo: [
+  ],
+  enketo: [
+    './old-navigation/**/*.wdio-spec.js'
   //   './enketo/**/*.wdio-spec.js',
-  // ]
+  ]
 };
 
 exports.suites = SUITES;
-
--- tests/e2e/default/wdio.conf.js
+++ tests/e2e/default/wdio.conf.js
@@ -4,7 +4,7 @@ const { suites } = require('./suites');
 // Override specific properties from wdio base config
 const defaultConfig = Object.assign(wdioBaseConfig.config, {
   suites,
-  specs: ['**/*/old-navigation.wdio-spec.js'],
+  specs: ['**/old-navigation.wdio-spec.js'],
 });
 
 exports.config = defaultConfig;
