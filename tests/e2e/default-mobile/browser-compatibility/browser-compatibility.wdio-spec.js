8e26d0a1dd9bb1e37e31bfe7566368e34eb42c8e
tatilepizs <tatiana@medic.org>
2024-08-23 14:09:47 -0600

    update browser-compatibility.wdio-spec.js test

--- tests/e2e/default-mobile/browser-compatibility/browser-compatibility.wdio-spec.js
+++ tests/e2e/default-mobile/browser-compatibility/browser-compatibility.wdio-spec.js
@@ -1,26 +1,23 @@
 const commonPage = require('@page-objects/default/common/common.wdio.page');
 const modalPage = require('@page-objects/default/common/modal.wdio.page');
 const loginPage = require('@page-objects/default/login/login.wdio.page');
-const constants = require('@constants');
-const ANDROID_VERSION = '13';
-const SUPPORTED_CHROME_VERSION = '118.0.5993.112';
-const OUTDATED_CHROME_VERSION = '74.0.5993.112';
 
 describe('Browser Compatibility Modal', () => {
-  const newChromeVersion =
-    `Mozilla/5.0 (Linux; Android ${ANDROID_VERSION}; IN2010) AppleWebKit/537.36 (KHTML, like Gecko) ` +
-    `Chrome/${SUPPORTED_CHROME_VERSION} Mobile Safari/537.36`;
+  const ANDROID_VERSION = '13';
+  const SUPPORTED_CHROME_VERSION = '118.0.5993.112';
+  const OUTDATED_CHROME_VERSION = '74.0.5993.112';
 
-  const outdatedChromeVersion =
-    `Mozilla/5.0 (Linux; Android ${ANDROID_VERSION}; IN2010) AppleWebKit/537.36 (KHTML, like Gecko) ` +
-    `Chrome/${OUTDATED_CHROME_VERSION} Mobile Safari/537.36`;
+  const EMULATE_DEVICE_SETTINGS = {
+    viewport: {
+      width: 600,
+      height: 960,
+      isMobile: true,
+      hasTouch: true,
+    }
+  };
 
   beforeEach(async () => {
-    await loginPage.login({
-      username: constants.USERNAME,
-      password: constants.PASSWORD,
-      createUser: true,
-    });
+    await loginPage.cookieLogin();
   });
 
   afterEach(async () => {
@@ -29,30 +26,20 @@ describe('Browser Compatibility Modal', () => {
   });
 
   it('should not display the browser compatibility modal for updated Chrome version', async () => {
-    await browser.emulateDevice({
-      viewport: {
-        width: 600,
-        height: 960,
-        isMobile: true,
-        hasTouch: true,
-      },
-      userAgent: newChromeVersion,
-    });
+    EMULATE_DEVICE_SETTINGS.userAgent = `Mozilla/5.0 (Linux; Android ${ANDROID_VERSION}; `+
+      `IN2010) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${SUPPORTED_CHROME_VERSION} Mobile Safari/537.36`;
+
+    await browser.emulateDevice(EMULATE_DEVICE_SETTINGS);
 
     await commonPage.goToBase();
     await modalPage.checkModalHasClosed();
   });
 
   it('should display the browser compatibility modal for outdated Chrome version', async () => {
-    await browser.emulateDevice({
-      viewport: {
-        width: 600,
-        height: 960,
-        isMobile: true,
-        hasTouch: true,
-      },
-      userAgent: outdatedChromeVersion,
-    });
+    EMULATE_DEVICE_SETTINGS.userAgent = `Mozilla/5.0 (Linux; Android ${ANDROID_VERSION}; ` +
+      `IN2010) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${OUTDATED_CHROME_VERSION} Mobile Safari/537.36`;
+
+    await browser.emulateDevice(EMULATE_DEVICE_SETTINGS);
 
     await commonPage.goToBase();
     const modal = await modalPage.getModalDetails();
