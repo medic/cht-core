ddcf9f4ffaa416fcd207ca6d8bbf43cfe06d82b0
latin-panda <66472237+latin-panda@users.noreply.github.com>
2024-08-26 20:26:27 +0700

    Menu options

--- webapp/src/ts/components/sidebar-menu/sidebar-menu.component.ts
+++ webapp/src/ts/components/sidebar-menu/sidebar-menu.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 
 import { Selectors } from '@mm-selectors/index';
 import { GlobalActions } from '@mm-actions/global';
+import { LocationService } from '@mm-services/location.service';
 
 @Component({
   selector: 'mm-sidebar-menu',
@@ -14,12 +15,31 @@ export class SidebarMenuComponent implements OnInit, OnDestroy {
   @ViewChild('sidebar') sidebar!: MatSidenav;
   private subscriptions: Subscription = new Subscription();
   private globalActions: GlobalActions;
+  mainModules: MenuOptions[] = [];
+  adminAppPath: string = '';
 
-  constructor(private store: Store) {
+  constructor(
+    private store: Store,
+    private locationService: LocationService,
+  ) {
     this.globalActions = new GlobalActions(store);
   }
 
   ngOnInit() {
+    this.adminAppPath = this.locationService.adminPath;
+    this.setMainModules();
+    this.subscribeToStore();
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
+  close() {
+    return this.globalActions.closeSidebarMenu();
+  }
+
+  private subscribeToStore() {
     const subscribe = this.store
       .select(Selectors.getSidebarMenu)
       .subscribe(sidebarMenu => {
@@ -31,15 +51,49 @@ export class SidebarMenuComponent implements OnInit, OnDestroy {
     this.subscriptions.add(subscribe);
   }
 
-  ngOnDestroy() {
-    this.subscriptions.unsubscribe();
-  }
-
-  close() {
-    return this.globalActions.closeSidebarMenu();
+  private setMainModules() {
+    this.mainModules = [
+      {
+        routerLink: 'messages',
+        icon: 'fa-envelope',
+        translationKey: 'Messages',
+        hasPermissions: 'can_view_messages,!can_view_messages_tab'
+      },
+      {
+        routerLink: 'tasks',
+        icon: 'fa-flag',
+        translationKey: 'Tasks',
+        hasPermissions: 'can_view_tasks,!can_view_tasks_tab'
+      },
+      {
+        routerLink: 'reports',
+        icon: 'fa-list-alt',
+        translationKey: 'Reports',
+        hasPermissions: 'can_view_reports,!can_view_reports_tab'
+      },
+      {
+        routerLink: 'contacts',
+        icon: 'fa-user',
+        translationKey: 'Contacts',
+        hasPermissions: 'can_view_contacts,!can_view_contacts_tab'
+      },
+      {
+        routerLink: 'analytics',
+        icon: 'fa-bar-chart-o',
+        translationKey: 'Analytics',
+        hasPermissions: 'can_view_analytics,!can_view_analytics_tab',
+      },
+    ];
   }
 }
 
 export interface SidebarMenu {
   isOpen: boolean;
 }
+
+export type MenuOptions = {
+  icon: string;
+  translationKey: string;
+  routerLink: string | undefined;
+  hasPermissions: string | undefined;
+};
