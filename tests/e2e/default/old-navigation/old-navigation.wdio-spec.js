c6d46ddeeaa563d49a316fcad21629d6ab44e4e7
tatilepizs <tatiana@medic.org>
2024-09-10 13:16:31 -0600

    fix old-navigation.wdio-spec.js

--- tests/e2e/default/old-navigation/old-navigation.wdio-spec.js
+++ tests/e2e/default/old-navigation/old-navigation.wdio-spec.js
@@ -78,7 +78,7 @@ describe('Old Navigation', () => {
 
   it('should navigate to the Reports section and open the first report listed', async () => {
     await oldNavigationPage.goToReports();
-    await reportsPage.openSelectedReport(await reportsPage.firstReport());
+    await reportsPage.openSelectedReport(await reportsPage.leftPanelSelectorsfirstReport());
     await waitForPageLoaded();
     const openReportInfo = await reportsPage.getOpenReportInfo();
     expect(openReportInfo.patientName).to.equal(person.name);
