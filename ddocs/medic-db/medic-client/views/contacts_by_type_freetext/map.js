b4215d541ec433e12a3c66cfa41e7ca37e3a0cc8
m5r <mokht@rmi.al>
2024-09-12 14:47:25 +0200

    simplify both contacts freetext views and include other properties

--- ddocs/medic-db/medic-client/views/contacts_by_type_freetext/map.js
+++ ddocs/medic-db/medic-client/views/contacts_by_type_freetext/map.js
@@ -1,36 +1,27 @@
 function(doc) {
-  var include = [ 'name', 'external_id', 'notes' ];
-
-  var usedKeys = [];
-  var emitMaybe = function(type, key, value) {
-    if (usedKeys.indexOf(key) === -1 && // Not already used
-        key.length > 2 // Not too short
-    ) {
-      usedKeys.push(key);
-      emit([ type, key ], value);
-    }
+  var emitCaseId = function(value, order) {
+    emit(['case_id:' + value], order);
   };
 
   var emitField = function(type, key, value, order) {
     if (!key || !value) {
       return;
     }
-    key = key.toLowerCase();
-    if (include.indexOf(key) === -1 || /_date$/.test(key)) {
-      return;
-    }
+
     if (typeof value === 'string') {
       value = value.toLowerCase();
       value.split(/\s+/).forEach(function(word) {
-        emitMaybe(type, word, order);
+        if (word.length <= 2) {
+          return;
+        }
+
+        emit([type, word], order);
       });
-    }
-    if (typeof value === 'number' || typeof value === 'string') {
-      emitMaybe(type, key + ':' + value, order);
     }
   };
 
-  var types = [ 'district_hospital', 'health_center', 'clinic', 'person' ];
+  var include = ['name', 'case_id', 'place_id', 'patient_id', 'external_id', 'house_number', 'notes'];
+  var types = ['district_hospital', 'health_center', 'clinic', 'person'];
   var idx;
   var type;
   if (doc.type === 'contact') {
@@ -43,12 +34,15 @@ function(doc) {
     type = doc.type;
     idx = types.indexOf(type);
   }
-  if (idx !== -1) {
+
+  var isContactOrPlace = idx !== -1;
+  if (isContactOrPlace) {
     var dead = !!doc.date_of_death;
     var muted = !!doc.muted;
     var order = dead + ' ' + muted + ' ' + idx + ' ' + (doc.name && doc.name.toLowerCase());
-    Object.keys(doc).forEach(function(key) {
+    include.forEach(function(key) {
       emitField(type, key, doc[key], order);
     });
+    emitCaseId(doc.case_id, order);
   }
 }
