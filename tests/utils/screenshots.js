903d7b067469ee6e3b4416471f86e8b5ebc2deed
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@mbp-de-maria.lan>
2024-09-17 16:43:44 -0400

    implement feedback for screenshots utility

--- tests/utils/screenshots.js
+++ tests/utils/screenshots.js
@@ -1,64 +1,66 @@
 const sharp = require('sharp');
 
-const MOBILE_WIDTH = 320;
-const MOBILE_HEIGHT = 570;
-const DESKTOP_WIDTH = 1000;
-const DESKTOP_HEIGHT = 820;
+const MOBILE_WINDOW_WIDTH = 768;
+const MOBILE_VIEWPORT_WIDTH = 320;
+const MOBILE_VIEWPORT_HEIGHT = 570;
+const DESKTOP_WINDOW_WIDTH = 1000;
+const DESKTOP_WINDOW_HEIGHT = 820;
+const HIGH_DENSITY_DISPLAY_2X = 2;
 
 const isMobile = async () => {
   const { width } = await browser.getWindowSize();
-  return width < 768;
+  return width < MOBILE_WINDOW_WIDTH;
 };
 
 const resizeWindowForScreenshots = async () => {
   if (await isMobile()) {
     await browser.emulateDevice({
       viewport: {
-        width: MOBILE_WIDTH,
-        height: MOBILE_HEIGHT,
+        width: MOBILE_VIEWPORT_WIDTH,
+        height: MOBILE_VIEWPORT_HEIGHT,
         isMobile: true,
         hasTouch: true,
       },
-      userAgent: 'my custom user agent'
+      userAgent: 'Mozilla/5.0 (Linux; Android 11; Pixel 4) ' +
+      'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+      'Chrome/92.0.4515.159 Mobile Safari/537.36'
     });
   } else {
-    await browser.setWindowSize(DESKTOP_WIDTH, DESKTOP_HEIGHT);
+    await browser.setWindowSize(DESKTOP_WINDOW_WIDTH, DESKTOP_WINDOW_HEIGHT);
   }
 };
 
 const generateScreenshot = async (scenario, step) => {
-  const isCI = process.env.CI === 'true';
-  if (!isCI) {
-    const device = await isMobile() ? 'mobile' : 'desktop';
+  const device = await isMobile() ? 'mobile' : 'desktop';
 
-    const filename = `./documentation/${scenario}_${step}_${device}.png`;
+  const filename = `./documentation/${scenario}_${step}_${device}.png`;
 
-    const fullScreenshotBuffer = Buffer.from(await browser.takeScreenshot(), 'base64');
-    let extractWidth;
-    let extractHeight;
-    let screenshotSharp = sharp(fullScreenshotBuffer);
+  const fullScreenshotBuffer = Buffer.from(await browser.takeScreenshot(), 'base64');
+  let extractWidth;
+  let extractHeight;
+  let screenshotSharp = sharp(fullScreenshotBuffer);
 
-    const metadata = await screenshotSharp.metadata();
+  const metadata = await screenshotSharp.metadata();
 
-    if (await isMobile()) {
-      extractWidth = Math.min(MOBILE_WIDTH, metadata.width);
-      extractHeight = Math.min(MOBILE_HEIGHT, metadata.height);
-      screenshotSharp = screenshotSharp.extract({
-        width: extractWidth,
-        height: extractHeight,
-        left: 0,
-        top: 0
-      });
-    } else {
-      extractWidth = Math.min(DESKTOP_WIDTH, metadata.width);
-      extractHeight = Math.min(DESKTOP_HEIGHT, metadata.height);
-    }
-
-    // Resize the image to 2x for high-density displays
-    screenshotSharp = screenshotSharp.resize(extractWidth * 2, extractHeight * 2);
-
-    await screenshotSharp.toFile(filename);
+  if (await isMobile()) {
+    extractWidth = Math.min(MOBILE_VIEWPORT_WIDTH, metadata.width);
+    extractHeight = Math.min(MOBILE_VIEWPORT_HEIGHT, metadata.height);
+    screenshotSharp = screenshotSharp.extract({
+      width: extractWidth,
+      height: extractHeight,
+      left: 0,
+      top: 0
+    });
+  } else {
+    extractWidth = Math.min(DESKTOP_WINDOW_WIDTH, metadata.width);
+    extractHeight = Math.min(DESKTOP_WINDOW_HEIGHT, metadata.height);
   }
+
+  screenshotSharp = screenshotSharp
+    .resize(extractWidth * HIGH_DENSITY_DISPLAY_2X,
+      extractHeight * HIGH_DENSITY_DISPLAY_2X);
+
+  await screenshotSharp.toFile(filename);
 };
 
 module.exports = {
