2d3660c574ba6d85851b1c4072ecbf60de273a3a
Diana Barsan <barsan@medic.org>
2024-09-24 15:27:06 +0300

    fix linting

--- webapp/src/ts/services/form.service.ts
+++ webapp/src/ts/services/form.service.ts
@@ -8,7 +8,6 @@ import * as medicXpathExtensions from '../../js/enketo/medic-xpath-extensions';
 import { DbService } from '@mm-services/db.service';
 import { FileReaderService } from '@mm-services/file-reader.service';
 import { LineageModelGeneratorService } from '@mm-services/lineage-model-generator.service';
-import { SearchService } from '@mm-services/search.service';
 import { SubmitFormBySmsService } from '@mm-services/submit-form-by-sms.service';
 import { UserContactService } from '@mm-services/user-contact.service';
 import { XmlFormsService } from '@mm-services/xml-forms.service';
