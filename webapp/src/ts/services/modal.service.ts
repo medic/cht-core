587fa3a0f86c3753c962393139066d3a4893d831
latin-panda <66472237+latin-panda@users.noreply.github.com>
2024-10-02 23:18:37 +0700

    fixes training card scroll

--- webapp/src/ts/services/modal.service.ts
+++ webapp/src/ts/services/modal.service.ts
@@ -37,7 +37,7 @@ export class ModalService {
     return this.matDialog.open(component, {
       autoFocus: false,
       disableClose: true,
-      minWidth: isMobile ? '90vw' : '400px', // Give maximum space to date picker's calendar when in mobile.
+      minWidth: isMobile ? '90vw' : '340px', // Give maximum space to date picker's calendar when in mobile.
       width: '600px',
       maxWidth: '90vw',
       minHeight: '100px',
