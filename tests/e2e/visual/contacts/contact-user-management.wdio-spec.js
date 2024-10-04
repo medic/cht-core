429b14b2372e5a8584c5daca0496aaef4ead8042
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@mbp-de-maria.lan>
2024-10-03 20:03:49 -0400

    add condition cards and care guides

--- tests/e2e/visual/contacts/contact-user-management.wdio-spec.js
+++ tests/e2e/visual/contacts/contact-user-management.wdio-spec.js
@@ -10,27 +10,27 @@ const path = require('path');
 const { resizeWindowForScreenshots, generateScreenshot } = require('@utils/screenshots');
 
 describe('Contact List Page', () => {
-  const modifyRolePermissionsAndContactTypes = async (roleValue, addPermissions, removePermissions = []) => {
+  const modifyRolePermissions = async (roleValue, addPermissions, removePermissions = []) => {
     const roles = [roleValue];
     const settings = await utils.getSettings();
-    /*let contactTypes = settings.contact_types;
-
-    contactTypes = contactTypes.map(contactType => {
-      if (contactType.id === 'person') {
-        return { ...contactType, count_visits: true };
-      } else if (contactType.id === 'clinic') {
-        const { count_visits, ...rest } = contactType;
-        return rest;
-      }
-      return contactType;
-    });*/
-
     const permissions = await utils.getUpdatedPermissions(roles, addPermissions, removePermissions);
 
     await utils.updateSettings(
       { roles: settings.roles, permissions: permissions},
       { revert: true, ignoreReload: true, refresh: true, sync: true }
     );
+  };
+
+  const updateContactSummarySettings = async () => {
+    await chtConfUtils.initializeConfigDir();
+    const contactSummaryFile = path.join(__dirname, 'contact-summary.templated.js');
+    const contactSummaryExtrasFile = path.join(__dirname, 'contact-summary-extras.js');
+    const { contactSummary } = await chtConfUtils.compileNoolsConfig({ contactSummary: contactSummaryFile, contactSummaryExtras: contactSummaryExtrasFile });
+    await utils.updateSettings(
+      { contact_summary: contactSummary},
+      { revert: true, ignoreReload: true, refresh: true, sync: true }
+    );
+    await commonPage.waitForPageLoaded();
   };
 
   const docs = dataFactory.createHierarchy({
@@ -46,16 +46,6 @@ describe('Contact List Page', () => {
     await utils.saveDocs([...docs.places, ...docs.clinics, ...docs.persons, ...docs.reports]);
     await utils.createUsers([docs.user]);
     await utils.addTranslations('en', {'contact.last.visit.unknown': 'Last Visited'});
-    /*await loginPage.login(docs.user);
-    await chtConfUtils.initializeConfigDir();
-    const contactSummaryFile = path.join(__dirname, 'contact-template-config.js');
-    const { contactSummary } = await chtConfUtils.compileNoolsConfig({ contactSummary: contactSummaryFile });
-    await utils.updateSettings(
-      { contact_summary: contactSummary},
-      { revert: true, ignoreReload: true, refresh: true, sync: true }
-    );
-    await commonPage.waitForPageLoaded();
-    await commonPage.logout();*/
   });
 
   after(async () => {
@@ -69,6 +59,8 @@ describe('Contact List Page', () => {
 
   afterEach(async () => {
     await commonPage.goToBase();
+    await utils.revertSettings();
+    await commonPage.goToBase();
     await commonPage.logout();
   });
 
@@ -76,6 +68,7 @@ describe('Contact List Page', () => {
     it('should show contacts list, search, profiles (person and family), '+
       'contact summary, condition cards and care guides', async () => {
       await loginPage.login(docs.user);
+      updateContactSummarySettings();
       await commonPage.goToPeople();
       expect(await commonPage.isPeopleListPresent()).to.be.true;
       await generateScreenshot('people', 'list');
@@ -83,6 +76,7 @@ describe('Contact List Page', () => {
       await generateScreenshot('people', 'search');
       await searchPage.clearSearch();
       await contactPage.selectLHSRowByText('Beatrice Bass Family');
+      await searchPage.clearSearch();
       await generateScreenshot('people', 'profile-family');
       await commonPage.goToBase();
       await commonPage.goToPeople();
@@ -91,6 +85,9 @@ describe('Contact List Page', () => {
     });
 
     it('should show profiles (area and branch)', async () => {
+      await loginPage.login(docs.user);
+      updateContactSummarySettings();
+      await commonPage.logout();
       await loginPage.cookieLogin();
       await commonPage.goToPeople();
       expect(await commonPage.isPeopleListPresent()).to.be.true;
@@ -102,14 +99,44 @@ describe('Contact List Page', () => {
       await generateScreenshot('people', 'profile-branch');
     });
 
-    it.only('should show UHC sort', async () => {
+    it('should show UHC sort', async () => {
       await loginPage.login(docs.user);
-      await modifyRolePermissionsAndContactTypes('chw', ['can_view_uhc_stats', 'can_view_last_visited_date'], []);
+      await modifyRolePermissions('chw', ['can_view_uhc_stats', 'can_view_last_visited_date'], []);
       await commonPage.waitForPageLoaded();
       await commonPage.goToPeople();
       await sortPage.selectSortOrder('By date last visited');
       await sortPage.openSortMenu();
       await generateScreenshot('people', 'sort');
+    });
+
+    it('should show condition cards', async () => {
+      await loginPage.login(docs.user);
+      updateContactSummarySettings();
+      await commonPage.goToPeople();
+      expect(await commonPage.isPeopleListPresent()).to.be.true;
+      await contactPage.selectLHSRowByText('Beatrice Bass');
+      await (await contactPage.pregnancyCardSelectors.pregnancyCard()).scrollIntoView();
+      await generateScreenshot('people', 'condition-card-active-pregnancy');
+      await commonPage.goToBase();
+      await commonPage.goToPeople();
+      //await contactPage.selectLHSRowByText('Hawa Bass');
+      //await (await contactPage.pregnancyCardSelectors.pregnancyCard()).scrollIntoView();
+      //await generateScreenshot('people', 'condition-card-inmunization');
+    });
+
+    it.only('should show cares guides', async () => {
+      await loginPage.login(docs.user);
+      await chtConfUtils.initializeConfigDir();
+      const formsPath = path.join(__dirname, 'forms');
+      await chtConfUtils.compileAndUploadAppForms(formsPath);
+      await modifyRolePermissions('chw', [],['can_view_call_action', 'can_view_message_action']);
+      await commonPage.waitForPageLoaded();
+      await commonPage.goToPeople();
+      expect(await commonPage.isPeopleListPresent()).to.be.true;
+      await contactPage.selectLHSRowByText('Dana Dearborn');
+      await commonPage.clickFastActionFAB({ waitForList: false });
+      await generateScreenshot('people', 'care_guides');
+      await commonPage.goToBase();
     });
   });
 });
