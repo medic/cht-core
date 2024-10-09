6ae4ef6339e4017d21de37f9ffefd73fb21a3f71
Diana Barsan <barsan@medic.org>
2024-10-09 08:11:12 +0300

    log feedback docs
    Signed-off-by: Diana Barsan <barsan@medic.org>

--- tests/e2e/default/transitions/create-user-for-contacts.replace-user.wdio-spec.js
+++ tests/e2e/default/transitions/create-user-for-contacts.replace-user.wdio-spec.js
@@ -284,6 +284,10 @@ describe('Create user for contacts', () => {
         const basicReport3 = await utils.getDoc(basicReportId3);
         // New reports written by the old user are not re-parented
         expect(basicReport3.contact._id).to.equal(originalContactId);
+
+        const feedbackDocs = await chtDbUtils.getFeedbackDocs();
+        console.log(JSON.stringify(feedbackDocs, null, 2));
+        await chtDbUtils.clearFeedbackDocs();
       });
 
       it('creates a new user when the replace_user form is submitted while online', async () => {
@@ -332,6 +336,10 @@ describe('Create user for contacts', () => {
         await commonPage.waitForPageLoaded();
         const [cookie] = await browser.getCookies('userCtx');
         expect(cookie.value).to.include(newUserSettings.name);
+
+        const feedbackDocs = await chtDbUtils.getFeedbackDocs();
+        console.log(JSON.stringify(feedbackDocs, null, 2));
+        await chtDbUtils.clearFeedbackDocs();
       });
 
       it('does not assign new person as primary contact of parent place ' +
@@ -384,6 +392,10 @@ describe('Create user for contacts', () => {
         await commonPage.waitForPageLoaded();
         const [cookie] = await browser.getCookies('userCtx');
         expect(cookie.value).to.include(newUserSettings.name);
+
+        const feedbackDocs = await chtDbUtils.getFeedbackDocs();
+        console.log(JSON.stringify(feedbackDocs, null, 2));
+        await chtDbUtils.clearFeedbackDocs();
       });
 
       it('creates new user from latest replace_user form data ' +
@@ -481,6 +493,10 @@ describe('Create user for contacts', () => {
         // Basic form reports were successfully synced to the server
         const basicReportsFromRemote = await utils.getDocs([basicReportId0, basicReportId1]);
         basicReportsFromRemote.forEach((report, index) => expect(report).to.deep.equal(basicReports[index]));
+
+        const feedbackDocs = await chtDbUtils.getFeedbackDocs();
+        console.log(JSON.stringify(feedbackDocs, null, 2));
+        await chtDbUtils.clearFeedbackDocs();
       });
 
       it('creates new user when replace_user form is submitted ' +
@@ -548,6 +564,10 @@ describe('Create user for contacts', () => {
         await loginPage.login(otherUser);
         await commonPage.waitForPageLoaded();
         await commonPage.goToPeople(originalContactId);
+
+        const feedbackDocs = await chtDbUtils.getFeedbackDocs();
+        console.log(JSON.stringify(feedbackDocs, null, 2));
+        await chtDbUtils.clearFeedbackDocs();
       });
 
       it('creates new user for the first version of a contact ' +
@@ -668,6 +688,10 @@ describe('Create user for contacts', () => {
         // be deleted. This first call will delete the current "winning" version and then the subsequent call in
         // afterEach will handle deleting the other version.
         await utils.revertDb([/^form:/], true);
+
+        const feedbackDocs = await chtDbUtils.getFeedbackDocs();
+        console.log(JSON.stringify(feedbackDocs, null, 2));
+        await chtDbUtils.clearFeedbackDocs();
       });
 
       it('does not create a new user or re-parent reports when the transition is disabled', async () => {
@@ -703,6 +727,10 @@ describe('Create user for contacts', () => {
         // Original contact not updated
         const updatedOriginalContact = await utils.getDoc(DEFAULT_USER_CONTACT_DOC._id);
         expect(updatedOriginalContact.user_for_contact).to.be.undefined;
+
+        const feedbackDocs = await chtDbUtils.getFeedbackDocs();
+        console.log(JSON.stringify(feedbackDocs, null, 2));
+        await chtDbUtils.clearFeedbackDocs();
       });
 
       it('does not create any new user nor does it reparent new reports when the transition fails', async () => {
@@ -781,6 +809,10 @@ describe('Create user for contacts', () => {
         const basicReportId3 = await submitBasicForm();
         const subsequentBasicReports = await chtDbUtils.getDocs([basicReportId2, basicReportId3]);
         subsequentBasicReports.forEach((report) => expect(report.contact._id).to.equal(originalContactId));
+
+        const feedbackDocs = await chtDbUtils.getFeedbackDocs();
+        console.log(JSON.stringify(feedbackDocs, null, 2));
+        await chtDbUtils.clearFeedbackDocs();
       });
     });
 
