e1894e492c1b451922a1936834c6f025269e9e3f
Rafa <raphael@medic.org>
2024-10-14 21:15:15 +0100

    lint

--- tests/e2e/default/wdio.conf.js
+++ tests/e2e/default/wdio.conf.js
@@ -4,7 +4,7 @@ const { suites } = require('./suites');
 // Override specific properties from wdio base config
 const defaultConfig = Object.assign(wdioBaseConfig.config, {
   suites,
-  specs: ['**/tasks-b.wdio-spec.js'],
+  specs: ['**/*.wdio-spec.js'],
 });
 
 exports.config = defaultConfig;
