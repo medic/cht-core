653c77c4255748fb39d35d5af1145fa2446f6d02
m5r <mokht@rmi.al>
2024-10-14 19:10:16 +0200

    nouveau-backed fulltext search index

---
+++ ddocs/medic-db/medic-nouveau/nouveau/contacts_by_freetext/index.js
@@ -0,0 +1,47 @@
+function(doc) {
+  const skip = [ '_id', '_rev', 'type', 'refid', 'geolocation' ];
+  let toIndex = '';
+
+  const types = [ 'district_hospital', 'health_center', 'clinic', 'person' ];
+  let idx;
+  if (doc.type === 'contact') {
+    idx = types.indexOf(doc.contact_type);
+    if (idx === -1) {
+      idx = doc.contact_type;
+    }
+  } else {
+    idx = types.indexOf(doc.type);
+  }
+
+  const isContactDoc = idx !== -1;
+  if (isContactDoc) {
+    Object.keys(doc).forEach(function(key) {
+      const value = doc[key];
+      if (!key || !value) {
+        return;
+      }
+
+      key = key.toLowerCase();
+      if (skip.indexOf(key) !== -1 || /_date$/.test(key)) {
+        return;
+      }
+
+      if (typeof value === 'string') {
+        toIndex += ' ' + value;
+      }
+
+      /*if (typeof value === 'number') {
+        index('double', key, value, { store: true });
+      }*/
+
+      /*if (typeof value === 'string') {
+        index('text', key, value, { store: true });
+      }*/
+    });
+
+    toIndex = toIndex.trim();
+    if (toIndex) {
+      index('text', 'default', toIndex, { store: true });
+    }
+  }
+}
