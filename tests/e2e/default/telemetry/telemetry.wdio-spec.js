bf001aac0eee2da1359f8afa152582f48f8d5e43
m5r <mokht@rmi.al>
2024-10-23 12:15:57 +0200

    e2e test for contact search telemetry

--- tests/e2e/default/telemetry/telemetry.wdio-spec.js
+++ tests/e2e/default/telemetry/telemetry.wdio-spec.js
@@ -6,41 +6,36 @@ const placeFactory = require('@factories/cht/contacts/place');
 const personFactory = require('@factories/cht/contacts/person');
 const { faker: Faker } = require('@faker-js/faker');
 const userFactory = require('@factories/cht/users/users');
+const searchPage = require('@page-objects/default/search/search.wdio.page');
+const contactPage = require('@page-objects/default/contacts/contacts.wdio.page');
 const { BRANCH, TAG } = process.env;
-
-const setupUser = () => {
-  const places = placeFactory.generateHierarchy();
-  const districtHospital = places.get('district_hospital');
-  const contact = personFactory.build({
-    name: Faker.name.firstName(),
-    parent: { _id: districtHospital._id },
-  });
-  const user = userFactory.build({
-    username: Faker.internet.userName().toLowerCase().replace(/[^0-9a-zA-Z_]/g, ''),
-    password: 'Secret_1',
-    place: districtHospital._id,
-    contact: contact._id,
-    known: true,
-  });
-
-  return {
-    docs: [
-      ...places.values(),
-      contact,
-    ],
-    user,
-  };
-};
 
 describe('Telemetry', () => {
   const DATE_FORMAT = 'YYYY-MM-DD';
   const TELEMETRY_PREFIX = 'telemetry';
-  let user;
-  let docs;
+
+  const places = placeFactory.generateHierarchy();
+  const clinic = places.get('clinic');
+  const patient = personFactory.build({
+    phone: '+12068881234',
+    parent: { _id: clinic._id, parent: clinic.parent },
+  });
+
+  const healthCenter = places.get('health_center');
+  const contact = personFactory.build({
+    name: Faker.person.firstName(),
+    parent: { _id: healthCenter._id },
+  });
+  const user = userFactory.build({
+    username: Faker.internet.userName().toLowerCase().replace(/[^0-9a-zA-Z_]/g, ''),
+    password: 'Secret_1',
+    place: healthCenter._id,
+    contact: contact._id,
+    known: true,
+  });
 
   before(async () => {
-    ({ docs, user } = setupUser());
-    await utils.saveDocs(docs);
+    await utils.saveDocs([...places.values(), contact, patient]);
     await utils.createUsers([user]);
     await loginPage.login(user);
     await commonPage.waitForPageLoaded();
@@ -81,5 +76,37 @@ describe('Telemetry', () => {
 
     const version = TAG || utils.escapeBranchName(BRANCH) || clientDdoc.build_info.base_version;
     expect(clientDdoc.build_info.version).to.include(version);
+  });
+
+  describe('search matches telemetry', () => {
+    const getTelemetryEntryByKey = async (key) => {
+      const todayDBName = `${TELEMETRY_PREFIX}-${moment().format(DATE_FORMAT)}-${user.username}`;
+      const todayTelemetryDocs = await browser.execute(async (dbName) => {
+        // eslint-disable-next-line no-undef
+        const docs = await window.PouchDB(dbName).allDocs({ include_docs: true });
+        return docs.rows.filter(row => row.doc.key.startsWith('search_match'));
+      }, todayDBName);
+      return todayTelemetryDocs.filter(row => row.doc.key === key);
+    };
+
+    it('should record telemetry for contact searches', async () => {
+      await commonPage.goToPeople();
+
+      const [firstName, lastName] = patient.name.split(' ');
+      const phone = patient.phone;
+      const patient_id = patient.patient_id;
+      const searchTerms = [firstName, lastName, phone, patient_id, `patient_id:${patient_id}`];
+
+      for (const searchTerm of searchTerms) {
+        await searchPage.performSearch(searchTerm);
+        await contactPage.selectLHSRowByText(patient.name, false);
+        await searchPage.clearSearch();
+      }
+
+      expect(await getTelemetryEntryByKey('search_match:contacts_by_freetext:name')).to.have.lengthOf(2);
+      expect(await getTelemetryEntryByKey('search_match:contacts_by_freetext:phone')).to.have.lengthOf(1);
+      expect(await getTelemetryEntryByKey('search_match:contacts_by_freetext:patient_id')).to.have.lengthOf(1);
+      expect(await getTelemetryEntryByKey('search_match:contacts_by_freetext:patient_id:$value')).to.have.lengthOf(1);
+    });
   });
 });
