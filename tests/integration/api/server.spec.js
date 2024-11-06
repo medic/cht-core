88d4030b1f3a7353ca07ffbb2c2e4d7fb45f735e
Diana Barsan <barsan@medic.org>
2024-11-06 20:51:55 +0200

    update regex, it was matching too many things.
    Signed-off-by: Diana Barsan <barsan@medic.org>

--- tests/integration/api/server.spec.js
+++ tests/integration/api/server.spec.js
@@ -235,12 +235,13 @@ describe('server', () => {
   });
 
   describe('Request ID propagated to audit layer', () => {
-    const ID_REGEX = /\b[0-9a-f]{12}\b/;
+    const ID_REGEX = /[,\s]([0-9a-f]{12})[,\s]/;
 
     const getReqId = (logLine) => {
       if (ID_REGEX.test(logLine)) {
         const match = logLine.match(ID_REGEX);
-        return match?.[0];
+        console.warn(match);
+        return match?.[1];
       }
     };
 
@@ -367,6 +368,7 @@ describe('server', () => {
         const reqID = getReqId(apiLogs[0]);
 
         const haproxyRequests = haproxyLogs.filter(entry => getReqId(entry) === reqID);
+        console.warn(haproxyRequests);
         expect(haproxyRequests.length).to.equal(12);
         expect(haproxyRequests[0]).to.include('_session');
         expect(haproxyRequests[5]).to.include('/medic-test/_design/medic/_view/contacts_by_depth');
