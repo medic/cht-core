52ac11b349bbaf86adfcf3e1d3308dd5cc810636
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2024-11-07 18:41:09 +0545

    add missing files

---
+++ api/src/controllers/contact.js
@@ -0,0 +1,33 @@
+const auth = require('../auth');
+const { Contact, Qualifier} = require('@medic/cht-datasource');
+const ctx = require('../services/data-context');
+const serverUtils = require('../server-utils');
+
+const getContact = ({ with_lineage }) => ctx.bind(
+  with_lineage === 'true'
+    ? Contact.v1.getWithLineage
+    : Contact.v1.get
+);
+
+const checkUserPermissions = async (req) => {
+  const userCtx = await auth.getUserCtx(req);
+  if (!auth.isOnlineOnly(userCtx) || !auth.hasAllPermissions(userCtx, 'can_view_contacts')) {
+    return Promise.reject({ code: 403, message: 'Insufficient privileges' });
+  }
+};
+
+module.exports = {
+  v1: {
+    get: serverUtils.doOrError(async (req, res) => {
+      await checkUserPermissions(req);
+      const { uuid } = req.params;
+      const contact = await getContact(req.query)(Qualifier.byUuid(uuid));
+
+      if (!contact) {
+        return serverUtils.error({ status: 404, message: 'Contact not found' }, req, res);
+      }
+
+      return res.json(contact);
+    })
+  }
+};
