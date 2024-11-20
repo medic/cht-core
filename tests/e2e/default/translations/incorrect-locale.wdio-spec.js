883118ca8f1d4dc4a27f114f25bd8a8b16db9a9b
Diana Barsan <barsan@medic.org>
2024-11-20 13:22:43 +0200

    proper cleanup for incorrect locale test
    Signed-off-by: Diana Barsan <barsan@medic.org>

--- tests/e2e/default/translations/incorrect-locale.wdio-spec.js
+++ tests/e2e/default/translations/incorrect-locale.wdio-spec.js
@@ -4,7 +4,6 @@ const userSettingsElements = require('@page-objects/default/users/user-settings.
 const contactElements = require('@page-objects/default/contacts/contacts.wdio.page');
 const loginPage = require('@page-objects/default/login/login.wdio.page');
 const placeFactory = require('@factories/cht/contacts/place');
-const sentinelUtils = require('@utils/sentinel');
 const commonPage = require('@page-objects/default/common/common.wdio.page');
 
 describe('Testing Incorrect locale', () => {
@@ -31,14 +30,15 @@ describe('Testing Incorrect locale', () => {
     await utils.enableLanguage(LANGUAGE_CODE);
   };
 
-  before(async () => {
+  beforeEach(async () => {
     await loginPage.cookieLogin();
     await utils.saveDoc(place);
-    await sentinelUtils.waitForSentinel();
   });
 
   afterEach(async () => {
+    await utils.deleteDocs([ place._id, `messages-${LANGUAGE_CODE}` ]);
     await utils.revertSettings(true);
+    await commonElements.reloadSession();
   });
   
   it('should work with incorrect locale', async () => {
@@ -61,9 +61,5 @@ describe('Testing Incorrect locale', () => {
 
     const tasksFilter = await contactElements.getReportTaskFiltersText();
     expect(tasksFilter.sort()).to.deep.equal(['1 saptamana', '2 saptamani', 'View all'].sort());
-
-    await utils.revertSettings(true);
-    await utils.deleteDocs([ place._id, `messages-${LANGUAGE_CODE}` ]);
-    await browser.setCookies({ name: 'locale', value: 'en' });
   });
 });
