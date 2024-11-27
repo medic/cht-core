d507f20b976f67701f195a42e1c9618eec28634f
m5r <mokht@rmi.al>
2024-11-27 11:43:44 +0100

    ok contacts_by_type_freetext

--- ddocs/medic-db/medic-nouveau/nouveau/contacts_by_type_freetext/index.js
+++ ddocs/medic-db/medic-nouveau/nouveau/contacts_by_type_freetext/index.js
@@ -21,13 +21,24 @@ function (doc) {
       if (!key || !value) {
         return;
       }
-      key = key.toLowerCase();
+
+      key = key.toLowerCase().trim();
       if (skip.indexOf(key) !== -1 || /_date$/.test(key)) {
         return;
       }
 
+      const fieldNameRegex = /^\$?[a-zA-Z][a-zA-Z0-9_]*$/g
+      if (!fieldNameRegex.test(key)) {
+        log(`key "${key}" doesn't pass regex - "${doc.id}"`);
+      }
+
       if (typeof value === 'string') {
         toIndex += ' ' + value;
+        index('text', key, value, { store: true });
+      }
+
+      if (typeof value === 'number') {
+        index('double', key, value, { store: true });
       }
     });
   }
