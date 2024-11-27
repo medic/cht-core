723842fb57f5773f93ef15d3603e1433f6e11524
Diana Barsan <barsan@medic.org>
2024-11-27 08:12:02 +0200

    add custom not found error

--- api/src/services/privacy-policy.js
+++ api/src/services/privacy-policy.js
@@ -2,6 +2,7 @@ const sanitizeHtml = require('sanitize-html');
 const db = require('../db');
 const logger = require('@medic/logger');
 const config = require('../config');
+const { NotFoundError } = require('../errors');
 
 const PRIVACY_POLICY_DOC_ID = 'privacy-policies';
 
@@ -22,9 +23,7 @@ const getDoc = (options=({})) => {
     .then(doc => {
       const policies = doc.privacy_policies;
       if (!policies || !Object.keys(policies).length) { // invalid doc
-        const err = new Error(`Invalid ${PRIVACY_POLICY_DOC_ID} doc: missing required "privacy_policies" property`);
-        err.status = 404;
-        throw err;
+        throw new NotFoundError(`Invalid ${PRIVACY_POLICY_DOC_ID} doc: missing required "privacy_policies" property`);
       }
       return doc;
     })
