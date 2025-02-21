3e9366880495907cd54bf73e29c568875dd44cdc
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-02-21 15:17:31 -0600

    Remove webapp unit tests for views that do not exist

--- webapp/tests/mocha/unit/views/reports_by_freetext.spec.js
+++ webapp/tests/mocha/unit/views/reports_by_freetext.spec.js
@@ -1,4 +1,4 @@
-const { loadView, buildViewMapFn } = require('./utils');
+const { buildViewMapFn } = require('./utils');
 const medicOfflineFreetext = require('../../../../src/js/bootstrapper/offline-ddocs/medic-offline-freetext');
 const { expect } = require('chai');
 
@@ -11,174 +11,169 @@ const createReport = (data = {}) => {
   };
 };
 
+const mapFn = buildViewMapFn(medicOfflineFreetext.views.reports_by_freetext.map);
+
 describe('reports_by_freetext', () => {
+  afterEach(() => mapFn.reset());
+
   [
-    ['online view', loadView('medic-db', 'medic-client', 'reports_by_freetext')],
-    ['offline view', buildViewMapFn(medicOfflineFreetext.views.reports_by_freetext.map)],
-  ].forEach(([name, mapFn]) => {
-    describe(name, () => {
-      afterEach(() => mapFn.reset());
+    undefined,
+    'invalid',
+    'contact',
+    'person',
+  ].forEach(type => it(`emits nothing when type is invalid [${type}]`, () => {
+    const doc = createReport({ type });
+    const emitted = mapFn(doc, true);
+    expect(emitted).to.be.empty;
+  }));
 
-      [
-        undefined,
-        'invalid',
-        'contact',
-        'person',
-      ].forEach(type => it(`emits nothing when type is invalid [${type}]`, () => {
-        const doc = createReport({ type });
-        const emitted = mapFn(doc, true);
-        expect(emitted).to.be.empty;
-      }));
+  [
+    undefined,
+    null,
+    '',
+  ].forEach(form => it(`emits nothing when form is not valued [${form}]`, () => {
+    const doc = createReport({ form });
+    const emitted = mapFn(doc, true);
+    expect(emitted).to.be.empty;
+  }));
 
-      [
-        undefined,
-        null,
-        '',
-      ].forEach(form => it(`emits nothing when form is not valued [${form}]`, () => {
-        const doc = createReport({ form });
-        const emitted = mapFn(doc, true);
-        expect(emitted).to.be.empty;
-      }));
+  [
+    null, undefined, { hello: 'world' }, {}, true
+  ].forEach(hello => it(
+    `emits nothing for a field when value is not a string or number [${JSON.stringify(hello)}]`,
+    () => {
+      const doc = createReport({ hello });
 
-      [
-        null, undefined, { hello: 'world' }, {}, true
-      ].forEach(hello => it(
-        `emits nothing for a field when value is not a string or number [${JSON.stringify(hello)}]`,
-        () => {
-          const doc = createReport({ hello });
+      const emitted = mapFn(doc, true);
 
-          const emitted = mapFn(doc, true);
+      expect(emitted).to.deep.equal([
+        { key: ['test'], value: doc.reported_date },
+        { key: ['form:test'], value: doc.reported_date },
+      ]);
+    }
+  ));
 
-          expect(emitted).to.deep.equal([
-            { key: ['test'], value: doc.reported_date },
-            { key: ['form:test'], value: doc.reported_date },
-          ]);
-        }
-      ));
+  it('emits only key:value for field when value is number', () => {
+    const doc = createReport({ hello: 1234 });
 
-      it('emits only key:value for field when value is number', () => {
-        const doc = createReport({ hello: 1234 });
+    const emitted = mapFn(doc, true);
 
-        const emitted = mapFn(doc, true);
+    expect(emitted).to.deep.equal([
+      { key: ['test'], value: doc.reported_date },
+      { key: ['form:test'], value: doc.reported_date },
+      { key: ['hello:1234'], value: doc.reported_date }
+    ]);
+  });
 
-        expect(emitted).to.deep.equal([
-          { key: ['test'], value: doc.reported_date },
-          { key: ['form:test'], value: doc.reported_date },
-          { key: ['hello:1234'], value: doc.reported_date }
-        ]);
-      });
+  [
+    't', 'to'
+  ].forEach(hello => it(`emits nothing but key:value when value is too short [${hello}]`, () => {
+    const doc = createReport({ hello });
 
-      [
-        't', 'to'
-      ].forEach(hello => it(`emits nothing but key:value when value is too short [${hello}]`, () => {
-        const doc = createReport({ hello });
+    const emitted = mapFn(doc, true);
 
-        const emitted = mapFn(doc, true);
+    expect(emitted).to.deep.equal([
+      { key: ['test'], value: doc.reported_date },
+      { key: ['form:test'], value: doc.reported_date },
+      { key: [`hello:${hello}`], value: doc.reported_date }
+    ]);
+  }));
 
-        expect(emitted).to.deep.equal([
-          { key: ['test'], value: doc.reported_date },
-          { key: ['form:test'], value: doc.reported_date },
-          { key: [`hello:${hello}`], value: doc.reported_date }
-        ]);
-      }));
+  it('emits nothing for field when value is empty', () => {
+    const doc = createReport({ hello: '' });
 
-      it('emits nothing for field when value is empty', () => {
-        const doc = createReport({ hello: '' });
+    const emitted = mapFn(doc, true);
 
-        const emitted = mapFn(doc, true);
+    expect(emitted).to.deep.equal([
+      { key: ['test'], value: doc.reported_date },
+      { key: ['form:test'], value: doc.reported_date },
+    ]);
+  });
 
-        expect(emitted).to.deep.equal([
-          { key: ['test'], value: doc.reported_date },
-          { key: ['form:test'], value: doc.reported_date },
-        ]);
-      });
+  [
+    '_id', '_rev', 'refid', 'content', 'Refid',
+  ].forEach(key => it(`emits nothing for a skipped field: [${key}]`, () => {
+    const doc = createReport({ [key]: 'world' });
 
-      [
-        '_id', '_rev', 'refid', 'content', 'Refid',
-      ].forEach(key => it(`emits nothing for a skipped field: [${key}]`, () => {
-        const doc = createReport({ [key]: 'world' });
+    const emitted = mapFn(doc, true);
 
-        const emitted = mapFn(doc, true);
+    expect(emitted).to.deep.equal([
+      { key: ['test'], value: doc.reported_date },
+      { key: ['form:test'], value: doc.reported_date },
+    ]);
+  }));
 
-        expect(emitted).to.deep.equal([
-          { key: ['test'], value: doc.reported_date },
-          { key: ['form:test'], value: doc.reported_date },
-        ]);
-      }));
+  it('emits nothing for fields that end with "_date"', () => {
+    const doc = createReport({ birth_date: 'world' });
 
-      it('emits nothing for fields that end with "_date"', () => {
-        const doc = createReport({ birth_date: 'world' });
+    const emitted = mapFn(doc, true);
 
-        const emitted = mapFn(doc, true);
+    expect(emitted).to.deep.equal([
+      { key: ['test'], value: doc.reported_date },
+      { key: ['form:test'], value: doc.reported_date },
+    ]);
+  });
 
-        expect(emitted).to.deep.equal([
-          { key: ['test'], value: doc.reported_date },
-          { key: ['form:test'], value: doc.reported_date },
-        ]);
-      });
-
-      it('emits value only once', () => {
-        const doc = createReport({
-          hello: 'world world',
-          hello1: 'world',
-          hello3: 'world',
-        });
-
-        const emitted = mapFn(doc, true);
-
-        expect(emitted).to.deep.equal([
-          { key: ['test'], value: doc.reported_date },
-          { key: ['form:test'], value: doc.reported_date },
-          { key: ['world'], value: doc.reported_date },
-          { key: ['hello:world world'], value: doc.reported_date },
-          { key: ['hello1:world'], value: doc.reported_date },
-          { key: ['hello3:world'], value: doc.reported_date }
-        ]);
-      });
-
-      it('normalizes keys and values to lowercase', () => {
-        const doc = createReport({ HeLlo: 'WoRlD', NBR: 1234 });
-
-        const emitted = mapFn(doc, true);
-
-        expect(emitted).to.deep.equal([
-          { key: ['test'], value: doc.reported_date },
-          { key: ['form:test'], value: doc.reported_date },
-          { key: ['world'], value: doc.reported_date },
-          { key: ['hello:world'], value: doc.reported_date },
-          { key: ['nbr:1234'], value: doc.reported_date },
-        ]);
-      });
-
-      it('emits each word in a string', () => {
-        const doc = createReport({ hello: `the quick\nBrown\tfox` });
-
-        const emitted = mapFn(doc, true);
-
-        expect(emitted).to.deep.equal([
-          { key: ['test'], value: doc.reported_date },
-          { key: ['form:test'], value: doc.reported_date },
-          { key: ['the'], value: doc.reported_date },
-          { key: ['quick'], value: doc.reported_date },
-          { key: ['brown'], value: doc.reported_date },
-          { key: ['fox'], value: doc.reported_date },
-          { key: ['hello:the quick\nbrown\tfox'], value: doc.reported_date },
-        ]);
-      });
-
-      it('emits non-ascii values', () => {
-        const doc = createReport({ name: 'बुद्ध Élève' });
-
-        const emitted = mapFn(doc, true);
-
-        expect(emitted).to.deep.equal([
-          { key: ['test'], value: doc.reported_date },
-          { key: ['form:test'], value: doc.reported_date },
-          { key: ['बुद्ध'], value: doc.reported_date },
-          { key: ['élève'], value: doc.reported_date },
-          { key: ['name:बुद्ध élève'], value: doc.reported_date }
-        ]);
-      });
+  it('emits value only once', () => {
+    const doc = createReport({
+      hello: 'world world',
+      hello1: 'world',
+      hello3: 'world',
     });
+
+    const emitted = mapFn(doc, true);
+
+    expect(emitted).to.deep.equal([
+      { key: ['test'], value: doc.reported_date },
+      { key: ['form:test'], value: doc.reported_date },
+      { key: ['world'], value: doc.reported_date },
+      { key: ['hello:world world'], value: doc.reported_date },
+      { key: ['hello1:world'], value: doc.reported_date },
+      { key: ['hello3:world'], value: doc.reported_date }
+    ]);
+  });
+
+  it('normalizes keys and values to lowercase', () => {
+    const doc = createReport({ HeLlo: 'WoRlD', NBR: 1234 });
+
+    const emitted = mapFn(doc, true);
+
+    expect(emitted).to.deep.equal([
+      { key: ['test'], value: doc.reported_date },
+      { key: ['form:test'], value: doc.reported_date },
+      { key: ['world'], value: doc.reported_date },
+      { key: ['hello:world'], value: doc.reported_date },
+      { key: ['nbr:1234'], value: doc.reported_date },
+    ]);
+  });
+
+  it('emits each word in a string', () => {
+    const doc = createReport({ hello: `the quick\nBrown\tfox` });
+
+    const emitted = mapFn(doc, true);
+
+    expect(emitted).to.deep.equal([
+      { key: ['test'], value: doc.reported_date },
+      { key: ['form:test'], value: doc.reported_date },
+      { key: ['the'], value: doc.reported_date },
+      { key: ['quick'], value: doc.reported_date },
+      { key: ['brown'], value: doc.reported_date },
+      { key: ['fox'], value: doc.reported_date },
+      { key: ['hello:the quick\nbrown\tfox'], value: doc.reported_date },
+    ]);
+  });
+
+  it('emits non-ascii values', () => {
+    const doc = createReport({ name: 'बुद्ध Élève' });
+
+    const emitted = mapFn(doc, true);
+
+    expect(emitted).to.deep.equal([
+      { key: ['test'], value: doc.reported_date },
+      { key: ['form:test'], value: doc.reported_date },
+      { key: ['बुद्ध'], value: doc.reported_date },
+      { key: ['élève'], value: doc.reported_date },
+      { key: ['name:बुद्ध élève'], value: doc.reported_date }
+    ]);
   });
 });
