0ed9b2b68979aadeb243e14abfb2b34500c03529
Robert <rmayore@livinggoods.org>
2025-02-25 12:57:53 +0300

    feat(#9760): added sso-login.js unit tests
    Signed-off-by: Robert <rmayore@livinggoods.org>

--- shared-libs/user-management/src/sso-login.js
+++ shared-libs/user-management/src/sso-login.js
@@ -5,18 +5,17 @@ const SETTINGS_DOC_ID = 'settings';
 
 const getSettingsDoc = async() => await db.medic.get(SETTINGS_DOC_ID);
 
-const hasBothOidcAndTokenOrPasswordLogin = data => data.oidc_provider && (data.password || data.token_login === true);
+const hasBothOidcAndTokenOrPasswordLogin = data => data.oidc_provider && (!!data.password || data.token_login === true);
 
-const isSsoLoginEnabled = (settings) =>  settings && settings.oidc_provider && settings.oidc_provider.client_id;
+const isSsoLoginEnabled = settings =>  !!settings && !!settings.oidc_provider && !!settings.oidc_provider.client_id;
 
-const isOidcClientIdValid = (settings, clientId) => settings.oidc_provider
+const isOidcClientIdValid = (settings, clientId) => !!settings.oidc_provider
  && clientId === settings.oidc_provider.client_id;
 
 const validateSsoLogin = async(data) => {
   if (hasBothOidcAndTokenOrPasswordLogin(data)){
     return {
-      msg: 'Either OIDC Login only or Token/Password Login is allowed',
-      key: 'configuration.oidc.client.required' //TODO: Ask about these keys
+      msg: 'Either OIDC Login only or Token/Password Login is allowed'
     }; 
   }
     
@@ -24,21 +23,18 @@ const validateSsoLogin = async(data) => {
 
   if (!settings){
     return {
-      msg: 'Settings Doc Not Found',
-      key: 'configuration.oidc.client.required' //TODO: Ask about these keys
+      msg: 'Settings Doc Not Found'
     }; 
   }
   if (!isSsoLoginEnabled(settings)){
     return {
-      msg: 'OIDC Login is not enabled',
-      key: 'configuration.oidc.client.required' //TODO: Ask about these keys
+      msg: 'OIDC Login is not enabled'
     }; 
 
   }
   if (isSsoLoginEnabled(settings) && !isOidcClientIdValid(settings, data.oidc_provider)){
     return {
-      msg: 'Invalid OIDC Client Id',
-      key: 'configuration.oidc.client.required' //TODO: Ask about these keys
+      msg: 'Invalid OIDC Client Id'
     }; 
   }
   data.password = passwords.generate(); //TODO: Both create & update user are updating the passowrd
@@ -46,5 +42,9 @@ const validateSsoLogin = async(data) => {
 
 
 module.exports = {
-  validateSsoLogin
+  validateSsoLogin,
+  hasBothOidcAndTokenOrPasswordLogin,
+  isSsoLoginEnabled,
+  isOidcClientIdValid,
+  getSettingsDoc
 };
