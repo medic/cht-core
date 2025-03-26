e73d2e3036c34f2936f4b92d387300650262926c
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-03-26 16:14:33 -0500

    fix: update cht-form to support extension-libs (#9840)
    Co-authored-by: Diana Barsan <35681649+dianabarsan@users.noreply.github.com>

--- webapp/web-components/cht-form/src/app.component.ts
+++ webapp/web-components/cht-form/src/app.component.ts
@@ -1,12 +1,16 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
 import { EnketoFormContext, EnketoService } from '@mm-services/enketo.service';
 import * as medicXpathExtensions from '../../../src/js/enketo/medic-xpath-extensions';
 import moment from 'moment';
 import { toBik_text } from 'bikram-sambat';
 import { TranslateService } from '@mm-services/translate.service';
 import { ContactSaveService } from '@mm-services/contact-save.service';
-import { NgIf } from '@angular/common';
+import { NgIf, DOCUMENT } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
+import { CHTDatasourceService } from '@mm-services/cht-datasource.service';
+import { CHTDatasourceService as CHTDatasourceServiceStub } from './stubs/cht-datasource.service';
+
+const DEFAULT_FORM_ID = 'cht-form-id';
 
 @Component({
   selector: 'cht-form',
@@ -15,12 +19,7 @@ import { TranslatePipe } from '@ngx-translate/core';
   imports: [NgIf, TranslatePipe],
 })
 export class AppComponent {
-  private readonly DEFAULT_FORM_ID = 'cht-form-id';
   private readonly DEFAULT_USER = { contact_id: 'default_user', language: 'en' } as const;
-  private readonly DEFAULT_STATUS = {
-    saving: false as boolean,
-    error: null as string | null,
-  } as const;
 
   private readonly HARDCODED_TYPES = [
     'district_hospital',
@@ -29,82 +28,72 @@ export class AppComponent {
     'person'
   ];
 
-  private _formId = this.DEFAULT_FORM_ID;
+  private readonly chtDataSourceService: CHTDatasourceServiceStub;
+
+  private formContext = new ChtFormEnketoFormContext();
+
   private _formXml?: string;
   private _formModel?: string;
   private _formHtml?: string;
-  private _contactSummary?: Record<string, any>;
-  private _contactType?: string;
-  private _content: Record<string, any> | null = null;
   private _user: typeof this.DEFAULT_USER & Record<string, any> = this.DEFAULT_USER;
 
   private currentRender?: Promise<void>;
   private reRenderForm = false;
-
-  @Input() editing = false;
-  @Input() status = { ...this.DEFAULT_STATUS };
 
   @Output() onRender: EventEmitter<void> = new EventEmitter();
   @Output() onCancel: EventEmitter<void> = new EventEmitter();
   @Output() onSubmit: EventEmitter<Object[]> = new EventEmitter();
 
   constructor(
+    chtDatasourceService: CHTDatasourceService,
     private readonly contactSaveService: ContactSaveService,
     private readonly enketoService: EnketoService,
     private readonly translateService: TranslateService,
+    @Inject(DOCUMENT) private readonly document: Document,
   ) {
+    this.chtDataSourceService = chtDatasourceService as unknown as CHTDatasourceServiceStub;
     const zscoreUtil = {};
-    const api = {};
-    medicXpathExtensions.init(zscoreUtil, toBik_text, moment, api);
+    medicXpathExtensions.init(zscoreUtil, toBik_text, moment, this.chtDataSourceService.getSync());
   }
 
   @Input() set formId(value: string) {
     if (!value?.trim().length) {
       throw new Error('The Form Id must be populated.');
     }
-    this._formId = value;
+    this.formContext.formId = value;
     this.queueRenderForm();
   }
 
-  @Input() set formHtml(value: string) {
-    if (!value?.trim().length) {
-      throw new Error('The Form HTML must be populated.');
-    }
+  @Input() set formHtml(value: string | undefined) {
     this._formHtml = value;
     this.queueRenderForm();
   }
 
-  @Input() set formModel(value: string) {
-    if (!value?.trim().length) {
-      throw new Error('The Form Model must be populated.');
-    }
+  @Input() set formModel(value: string | undefined) {
     this._formModel = value;
     this.queueRenderForm();
   }
 
-  @Input() set formXml(value: string) {
-    if (!value?.trim().length) {
-      throw new Error('The Form XML must be populated.');
-    }
+  @Input() set formXml(value: string | undefined) {
     this._formXml = value;
     this.queueRenderForm();
   }
 
   @Input() set contactSummary(value: Record<string, any> | undefined) {
-    this._contactSummary = value ? { context: value } : undefined;
+    this.formContext.contactSummary = value ? { context: value } : undefined;
     this.queueRenderForm();
   }
 
   @Input() set contactType(value: string | undefined) {
-    this._contactType = value;
+    this.formContext.contactType = value;
     this.queueRenderForm();
   }
 
-  @Input() set content(value: Record<string, any> | null) {
-    this._content = value;
-    if (this._content?.contact && !this._content.source) {
-      this._content.source = 'contact';
+  @Input() set content(value: Record<string, any> | undefined) {
+    if (value?.contact && !value.source) {
+      value.source = 'contact';
     }
+    this.formContext.content = value;
     this.queueRenderForm();
   }
 
@@ -116,8 +105,30 @@ export class AppComponent {
     this.queueRenderForm();
   }
 
+  @Input() set extensionLibs(value: Record<string, any> | undefined) {
+    if (!value) {
+      this.chtDataSourceService.clearExtensionLibs();
+      this.queueRenderForm();
+      return;
+    }
+
+    Object
+      .keys(value)
+      .forEach(key => this.chtDataSourceService.addExtensionLib(key, value[key]));
+    this.queueRenderForm();
+  }
+
+  @Input() get editing() {
+    return this.formContext.editing;
+  }
+
+
+  @Input() get status() {
+    return this.formContext.status;
+  }
+
   get formId(): string {
-    return this._formId;
+    return this.formContext.formId;
   }
 
   cancelForm(): void {
@@ -126,25 +137,26 @@ export class AppComponent {
   }
 
   async submitForm(): Promise<void> {
-    this.status.saving = true;
+    this.formContext.status.saving = true;
 
     try {
       const submittedDocs = await this.getDocsFromForm();
       this.onSubmit.emit(submittedDocs);
     } catch (e) {
       console.error('Error submitting form data: ', e);
-      this.status.error = await this.translateService.get('error.report.save');
+      this.formContext.status.error = await this.translateService.get('error.report.save');
     } finally {
-      this.status.saving = false;
+      this.formContext.status.saving = false;
     }
   }
 
   private async getDocsFromForm() {
     const currentForm = this.enketoService.getCurrentForm();
-    if (this._contactType) {
-      const typeFields = this.HARDCODED_TYPES.includes(this._contactType)
-        ? { type: this._contactType }
-        : { type: 'contact', contact_type: this._contactType };
+    const { contactType } = this.formContext;
+    if (contactType) {
+      const typeFields = this.HARDCODED_TYPES.includes(contactType)
+        ? { type: contactType }
+        : { type: 'contact', contact_type: contactType };
       const { preparedDocs } = await this.contactSaveService.save(currentForm, null, typeFields, null);
       return preparedDocs;
     }
@@ -153,7 +165,12 @@ export class AppComponent {
       xml: this._formXml,
       doc: {}
     };
-    return this.enketoService.completeNewReport(this._formId, currentForm, formDoc, this._content?.contact);
+    return this.enketoService.completeNewReport(
+      this.formContext.formId,
+      currentForm,
+      formDoc,
+      this.formContext.content?.contact
+    );
   }
 
   private queueRenderForm() {
@@ -179,24 +196,15 @@ export class AppComponent {
       return;
     }
 
-    const selector = `#${this._formId}`;
     // Can have a race condition where the formId is set here but the Angular attribute data binding has not updated
     // the DOM yet (the formId is used as the id of the .enketo element)
-    if (!$(selector).length) {
-      return this.waitForSelector(selector)
+    if (!$(this.formContext.selector).length) {
+      return this.waitForSelector(this.formContext.selector)
         .then(() => this.renderForm());
     }
 
-    const editedListener = () => this.editing = true;
-    const valuechangeListener = () => this.status.error = null;
-    const formDoc = { _id: this._formId };
-    const type = this._contactType ? 'contact': 'report';
-    const formContext = new EnketoFormContext(selector, type, formDoc, this._content);
-    formContext.editedListener = editedListener;
-    formContext.valuechangeListener = valuechangeListener;
-    formContext.contactSummary = this._contactSummary;
     const formDetails = this.getFormDetails();
-    await this.enketoService.renderForm(formContext, formDetails, this._user);
+    await this.enketoService.renderForm(this.formContext, formDetails, this._user);
     this.onRender.emit();
   }
 
@@ -204,6 +212,7 @@ export class AppComponent {
     const currentForm = this.enketoService.getCurrentForm();
     if (currentForm) {
       this.enketoService.unload(currentForm);
+      $(`${this.formContext.selector} .container.pages`).empty();
     }
   }
 
@@ -236,15 +245,65 @@ export class AppComponent {
 
   private tearDownForm() {
     this.unloadForm();
-    this._formXml = undefined;
-    this._formHtml = undefined;
-    this._formModel = undefined;
-    this._contactSummary = undefined;
-    this._contactType = undefined;
-    this._content = null;
-    this._formId = this.DEFAULT_FORM_ID;
-    this._user = this.DEFAULT_USER;
-    this.editing = false;
-    this.status = { ...this.DEFAULT_STATUS };
+    this.formContext = new ChtFormEnketoFormContext();
+    this.currentRender = undefined;
+    this.reRenderForm = false;
+
+    // The web component framework does some kind of "lazy setting" where it will not call a setter with the same value
+    // twice. So, we cannot just reset the internal state of the "inputs" here. We need to call "through the front door"
+    //  so the context knows that the values have changed instead of just directly resetting the state of this class.
+    const myForm = this.document
+      .getElementById(this.formContext.formId)
+      ?.closest('cht-form');
+    const component = myForm || this;
+    // @ts-expect-error it does exist
+    component.formXml = undefined;
+    // @ts-expect-error it does exist
+    component.formHtml = undefined;
+    // @ts-expect-error it does exist
+    component.formModel = undefined;
+    // @ts-expect-error it does exist
+    component.contactSummary = undefined;
+    // @ts-expect-error it does exist
+    component.contactType = undefined;
+    // @ts-expect-error it does exist
+    component.content = undefined;
+    // @ts-expect-error it does exist
+    component.formId = DEFAULT_FORM_ID;
+    // @ts-expect-error it does exist
+    component.user = this.DEFAULT_USER;
+    // @ts-expect-error it does exist
+    component.extensionLibs = undefined;
+  }
+}
+
+class ChtFormEnketoFormContext implements EnketoFormContext {
+  readonly editedListener = () => this.editing = true;
+  readonly valuechangeListener = () => this.status.error = null;
+  contactSummary?: Record<string, any>;
+  readonly status = {
+    saving: false,
+    error: null as string | null
+  };
+
+  formId = DEFAULT_FORM_ID;
+  contactType?: string;
+  content?: Record<string, any>;
+  editing = false;
+
+  get formDoc() {
+    return { _id: this.formId };
+  }
+
+  get instanceData() {
+    return this.content;
+  }
+
+  get selector() {
+    return `#${this.formId}`;
+  }
+
+  get type() {
+    return this.contactType ? 'contact': 'report';
   }
 }
---
+++ webapp/web-components/cht-form/tests/karma/stubs/db.service.spec.ts
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import { TestBed } from '@angular/core/testing';
+import { DbService } from '../../../src/stubs/db.service';
+
+describe('DB Service', () => {
+  let service: DbService;
+
+  beforeEach(() => service = TestBed.inject(DbService));
+
+  ['get', 'info'].forEach(fnName => {
+    it(fnName, async () => {
+      const result = await service.get()[fnName]();
+
+      expect(result).to.be.undefined;
+    });
+  });
+
+  describe('query', () => {
+    it('returns empty rows for medic-client/contacts_by_phone', async () => {
+      const selector = 'medic-client/contacts_by_phone';
+
+      const result = await service
+        .get()
+        .query(selector);
+
+      expect(result).to.deep.equal({ rows: [] });
+    });
+
+    it('throws error for other queries', async () => {
+      const selector = 'anythingelse';
+      const options = { hello: 'world' };
+
+      const db = service.get();
+
+      await expect(db.query(selector, options)).to.be.rejectedWith(
+        `Unsupported selector: DbService.get.query(${selector}, ${JSON.stringify(options)})`
+      );
+    });
+  });
+
+  it('getAttachment', async () => {
+    const result = await service
+      .get()
+      .getAttachment();
+
+    expect(result).to.be.instanceof(Blob);
+  });
+});
