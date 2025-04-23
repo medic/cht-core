a90c05f488fb1a8a5f5ee8d579a730ed3d05f07a
Maria Lorena Rodriguez Viruel <marialorenarodriguezviruel@MacBook-Pro-de-Maria.local>
2025-04-23 09:24:50 -0300

    move the wait

--- tests/e2e/default/enketo/pregnancy-visit.wdio-spec.js
+++ tests/e2e/default/enketo/pregnancy-visit.wdio-spec.js
@@ -63,7 +63,6 @@ describe('Pregnancy Visit', () => {
     expect(heading).to.equal(pregnantWoman.name);
     expect(form).to.equal('Pregnancy home visit');
 
-    await browser.pause(500);
     // Verify that the target tile was updated for pregnancies with 1+ visits
     await commonPage.goToAnalytics();
     await analyticsPage.goToTargets();
@@ -88,6 +87,8 @@ describe('Pregnancy Visit', () => {
     await pregnancyVisitForm.submitDefaultPregnancyVisit();
     await commonPage.openFastActionReport('pregnancy_home_visit');
     await pregnancyVisitForm.submitDefaultPregnancyVisit();
+
+    await browser.pause(500);
 
     await commonPage.goToAnalytics();
     await analyticsPage.goToTargets();
