fe633d2d02c722693e6405719539c13d9c883658
Shashi sah <86319846+shashi-sah2003@users.noreply.github.com>
2025-05-01 10:22:21 +0000

    fix: lazy loading to avoid circular dependency

--- shared-libs/couch-request/src/couch-request.js
+++ shared-libs/couch-request/src/couch-request.js
@@ -1,4 +1,3 @@
-const environment = require('@medic/environment');
 const path = require('path');
 const os = require('os');
 let asyncLocalStorage;
@@ -27,6 +26,8 @@ const addServername = isTrue(process.env.ADD_SERVERNAME_TO_HTTP_AGENT);
 //
 
 const getUserAgent = async () => {
+  // lazy load environment to avoid circular dependency
+  const environment = require('@medic/environment');
   const chtVersion = await environment.getVersion();
   const platform = os.platform();
   const arch = os.arch();
@@ -154,6 +155,8 @@ const setRequestOptions = async (options) => {
   const sendJson = getSendJson(options);
   setRequestContentType(options, sendJson);
   if (addServername) {
+    // lazy load environment to avoid circular dependency
+    const environment = require('@medic/environment');
     options.servername = environment.host;
   }
 };
