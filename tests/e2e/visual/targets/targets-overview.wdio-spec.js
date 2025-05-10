2421bbc1efcc6f4d23c6ebcbf4808575f6f7a66d
Dhruv Pathak <dhruvpathak482@gmail.com>
2025-05-10 12:09:45 +0000

    Resolved the suggest changes and liniting issues

--- tests/e2e/visual/targets/targets-overview.wdio-spec.js
+++ tests/e2e/visual/targets/targets-overview.wdio-spec.js
@@ -7,45 +7,71 @@ const userFactory = require('@factories/cht/users/users');
 const placeFactory = require('@factories/cht/contacts/place');
 const personFactory = require('@factories/cht/contacts/person');
 const deliveryFactory = require('@factories/cht/reports/delivery');
-const deliveryHomeFactory = require('@factories/cht/reports/deliveryHome');
 const pregnancyFactory = require('@factories/cht/reports/pregnancy');
 const { generateScreenshot } = require('@utils/screenshots');
 
-describe('...', () => {
+describe('Targets overview', () => {
   // Generate test data (people, pregnancies, deliveries) using functions 
   const places = placeFactory.generateHierarchy();
   const healthCenter = places.get('health_center');
   const offlineUser = userFactory.build({ username: 'offline-user-nav', roles: [ 'chw' ], place: healthCenter._id });
   const numberOfPatients = 17; // Define  patients 
-  const patients = Array.from({ length: numberOfPatients }, () => 
-  personFactory.build({ 
+  const patients = Array.from({ length: numberOfPatients }, () => personFactory.build({ 
     parent: { _id: healthCenter._id, parent: healthCenter.parent } 
-  })
-);
+  }));
   const reports = [
-    ...patients.map(patient => 
-  pregnancyFactory.build({
-    fields: { patient_id: patient._id, patient_uuid: patient._id, patient_name: patient.name }
-    })),  
+    ...patients.slice(0, 10).map(patient => pregnancyFactory.build({
+      fields: { patient_id: patient._id, patient_uuid: patient._id, patient_name: patient.name }
+    })), 
+    ...patients.slice(10, 15).map(patient => pregnancyFactory.build({
+      fields: { 
+        patient_id: patient._id, 
+        patient_uuid: patient._id, 
+        patient_name: patient.name, 
+        anc_visits_hf: {
+          anc_visits_hf_past: {
+            visited_hf_count: '0',
+          }}}
+    })),
+    ...patients.slice(15, 17).map(patient => pregnancyFactory.build({
+      fields: { 
+        patient_id: patient._id, 
+        patient_uuid: patient._id, 
+        patient_name: patient.name, 
+        anc_visits_hf: {
+          anc_visits_hf_past: {
+            visited_hf_count: '5',
+          }}}
+    })),
+    pregnancyVisitFactory.build({
+      fields: { patient_id: patients[15]._id, patient_uuid: patients[15]._id, patient_name: patients[15].name},
+    }),
+    pregnancyVisitFactory.build({
+      fields: { patient_id: patients[15]._id, patient_uuid: patients[15]._id, patient_name: patients[15].name},
+    }),
     deliveryFactory.build({
       fields: { patient_id: patients[1]._id, patient_uuid: patients[1]._id, patient_name: patients[1].name },
     }),
-        deliveryFactory.build({
+    deliveryFactory.build({
       fields: { patient_id: patients[2]._id, patient_uuid: patients[2]._id, patient_name: patients[2].name },
     }),
-        deliveryHomeFactory.build({
-      fields: { patient_id: patients[3]._id, patient_uuid: patients[3]._id, patient_name: patients[3].name },
+    deliveryFactory.build({
+      fields: {  patient_id: patients[3]._id, patient_uuid: patients[3]._id, patient_name: patients[3].name,
+        delivery_outcome: {
+          delivery_place: 'home'
+        },
+        data: {
+          __delivery_place: 'home'
+        }
+      },
     }),
   ];
 
-  let patientDocs;
-  let reportDocs;
-
   before(async () => {
     await utils.saveDocs([ ...places.values() ]);
-    reportDocs = await utils.saveDocs(reports);
-    patientDocs = await utils.saveDocs( patients );
-    await utils.createUsers([ offlineUser ]);
+    await utils.saveDocs(reports);
+    await utils.saveDocs(patients);
+    await utils.createUsers([offlineUser]);
     await loginPage.login(offlineUser);
   });
 
