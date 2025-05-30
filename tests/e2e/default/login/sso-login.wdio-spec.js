328f356ec972fb3bf942c7c3c74c0c5e6bc95efd
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-05-30 09:32:40 -0500

    Minor cleanup

--- tests/e2e/default/login/sso-login.wdio-spec.js
+++ tests/e2e/default/login/sso-login.wdio-spec.js
@@ -1,7 +1,7 @@
 const loginPage = require('@page-objects/default/login/login.wdio.page');
 const commonPage = require('@page-objects/default/common/common.wdio.page');
 const utils = require('@utils');
-const { startOidcServer, stopOidcServer, getDiscoveryUrl } = require('@utils/mock-oidc-provider');
+const { startOidcServer, stopOidcServer, getDiscoveryUrl, EMAIL } = require('@utils/mock-oidc-provider');
 const constants = require('@constants');
 
 describe('SSO Login', () => {
@@ -9,11 +9,7 @@ describe('SSO Login', () => {
     username: 'testusername',
     roles: ['program_officer'],
     phone: '+40766565656',
-    oidc_username: 'test@email.com'
-  };
-
-  const createUser = (user) => {
-    return utils.request({ path: '/api/v1/users', method: 'POST', body: user });
+    oidc_username: EMAIL
   };
 
   const setupSsoLogin = async () => {
@@ -42,7 +38,7 @@ describe('SSO Login', () => {
   });
 
   it('should log in the sso user', async () => {
-    await createUser(user);
+    await utils.createUsers([user]);
     await loginPage.ssoLogin();
     await commonPage.tabsSelector.messagesTab().waitForDisplayed();
   });
