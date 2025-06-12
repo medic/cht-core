7774a27314bd9397fd6ef1d73f70cf5257fbea88
Najuna Brian <160729266+najuna-brian@users.noreply.github.com>
2025-06-12 17:01:51 +0300

    Update ddocs/users-meta-db/users-meta/views/device_by_user/map.js
    Co-authored-by: Diana Barsan <35681649+dianabarsan@users.noreply.github.com>

--- ddocs/users-meta-db/users-meta/views/device_by_user/map.js
+++ ddocs/users-meta-db/users-meta/views/device_by_user/map.js
@@ -35,7 +35,24 @@ function(doc) {
           total: doc.device &&
             doc.device.deviceInfo &&
             doc.device.deviceInfo.storage &&
-            doc.device.deviceInfo.storage.total
+var deviceInfo = doc.device && doc.device.deviceInfo;
+ emit([doc.metadata.user, doc.metadata.deviceId], {
+      date: doc.metadata.year + '-' + pad(doc.metadata.month) + '-' + pad(doc.metadata.day),
+      id: doc._id,
+      device: {
+        userAgent: doc.device && doc.device.userAgent,
+        versions: {
+          apk: deviceInfo && deviceInfo.app && deviceInfo.app.version,
+          android: deviceInfo && deviceInfo.software && deviceInfo.software.androidVersion,
+          cht: doc.metadata.versions && doc.metadata.versions.app,
+          settings: doc.metadata.versions && doc.metadata.versions.settings,
+        },
+        storage: {
+          free: deviceInfo && deviceInfo.storage && deviceInfo.storage.free,
+          total: deviceInfo && deviceInfo.storage && deviceInfo.storage.total
+        }
+      },
+    });
         }
       },
     });
