2884f2e5bc7eeb397881dc3d705d9dafea9e6d53
Diana Barsan <barsan@medic.org>
2025-06-27 15:35:29 +0300

    updates apache jmeter link

--- tests/scalability/run_suite.sh
+++ tests/scalability/run_suite.sh
@@ -5,8 +5,8 @@ set -e
 shutdown -P +60
 
 mkdir -p /cht
-chmod 777 /cht;
-cd cht
+chmod 777 /cht
+cd /cht
 
 echo Cloning cht-core to /cht-core
 git clone --single-branch --branch "$TAG" https://github.com/medic/cht-core.git;
@@ -19,19 +19,19 @@ echo installing JAVA
 apt-get install default-jre -y
 
 echo installing node
-apt-get install nodejs npm -y
+curl -sL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh # install node 22
+apt-get install nodejs@ npm -y
 
-cd cht-core
-npm install patch-package
-cd webapp && npm ci && cd ../
-cd tests/scalability
+cd cht-core/tests/scalability
 echo "Changing config to match url arg"
 node -p "const fs = require('fs');var path = './config.json';var config = JSON.stringify({...require(path), url: '$MEDIC_URL'}, null, 2);fs.writeFileSync(path,config,{encoding:'utf8',flag:'w'});"
 echo "npm install for jmeter suite"
 npm ci
+npm install eurodigit
 
 echo "jmeter install"
-wget https://dlcdn.apache.org/jmeter/binaries/apache-jmeter-5.6.tgz -O ./apache-jmeter.tgz
+wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.6.3.tgz -O ./apache-jmeter.tgz
 mkdir -p ./jmeter
 tar -xf apache-jmeter.tgz -C ./jmeter --strip-components=1
 
@@ -46,11 +46,11 @@ tmp_dir=$(mktemp -d -t -p ./ report-XXXXXXXXXX)
 ./jmeter/bin/jmeter -n  -t sync.jmx -Jworking_dir="$tmp_dir" -Jnode_binary="$(which node)" -Jnumber_of_threads=10 -l "$tmp_dir"/cli_run.jtl -e -o "$tmp_dir"
 mv ./jmeter.log "$tmp_dir"/jmeter.log
 
-echo "Installing AWS CLI"
-apt-get install unzip -y
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
-echo "Uploading logs and screenshots to ${S3_PATH}..."
-/usr/local/bin/aws s3 cp "$tmp_dir" "$S3_PATH" --recursive
+#echo "Installing AWS CLI"
+#apt-get install unzip -y
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#./aws/install
+#echo "Uploading logs and screenshots to ${S3_PATH}..."
+#/usr/local/bin/aws s3 cp "$tmp_dir" "$S3_PATH" --recursive
 echo "FINISHED! "
