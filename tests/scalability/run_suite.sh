119015aeae673511c387f735957db15d4a76d89f
Diana Barsan <barsan@medic.org>
2025-06-28 09:35:25 +0300

    update scalability test script with npm setup, AWS CLI installation, and log upload to S3

--- tests/scalability/run_suite.sh
+++ tests/scalability/run_suite.sh
@@ -9,7 +9,7 @@ chmod 777 /cht
 cd cht
 
 echo Cloning cht-core to /cht-core
-git clone --single-branch --branch $TAG https://github.com/medic/cht-core.git
+git clone --single-branch --branch "$TAG" https://github.com/medic/cht-core.git
 
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
@@ -23,12 +23,14 @@ curl -sL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh # install node 22
 apt-get install nodejs
 
-cd cht-core/tests/scalability
+cd cht-core
+npm install patch-package
+cd webapp && npm ci && cd ../
+cd tests/scalability
 echo "Changing config to match url arg"
 node -p "const fs = require('fs');var path = './config.json';var config = JSON.stringify({...require(path), url: '$MEDIC_URL'}, null, 2);fs.writeFileSync(path,config,{encoding:'utf8',flag:'w'});"
 echo "npm install for jmeter suite"
 npm ci
-npm install eurodigit
 
 echo "jmeter install"
 wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.6.3.tgz -O ./apache-jmeter.tgz
@@ -46,28 +48,22 @@ tmp_dir=$(mktemp -d -t -p ./ report-XXXXXXXXXX)
 ./jmeter/bin/jmeter -n  -t sync.jmx -Jworking_dir="$tmp_dir" -Jnode_binary="$(which node)" -Jnumber_of_threads=10 -l "$tmp_dir"/cli_run.jtl -e -o "$tmp_dir"
 mv ./jmeter.log "$tmp_dir"/jmeter.log
 
-cd /cht
-remote_repo="https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com/medic/scalability-results.git"
-git clone remote_repo
-mkdir -p results
-cp -r $tmp_dir results/$DATA_PATH
-git config http.sslVerify false
-git add -A
-git commit -m "scalability results for $TAG"
-git push "${remote_repo}" HEAD:"master"
-
 echo "Installing AWS CLI"
 apt-get install unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
 echo "Uploading logs and screenshots to ${S3_PATH}..."
-/usr/local/bin/aws s3 cp $tmp_dir "$S3_PATH" --recursive
-#echo "Installing AWS CLI"
-#apt-get install unzip -y
-#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-#unzip awscliv2.zip
-#./aws/install
-#echo "Uploading logs and screenshots to ${S3_PATH}..."
-#/usr/local/bin/aws s3 cp "$tmp_dir" "$S3_PATH" --recursive
+/usr/local/bin/aws s3 cp "$tmp_dir" "$S3_PATH" --recursive
+
+cd /cht
+remote_repo="https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com/medic/scalability-results.git"
+git clone remote_repo
+mkdir -p results
+cp -r "$tmp_dir" results/"$DATA_PATH"
+git config http.sslVerify false
+git add -A
+git commit -m "scalability results for $TAG"
+git push "${remote_repo}" HEAD:"master"
+
 echo "FINISHED! "
