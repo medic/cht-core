94b69ef85a14ce828adfe545422e16d262bfdd84
Diana Barsan <barsan@medic.org>
2025-07-07 14:53:23 +0300

    fix npm install

--- tests/scalability/couchdb-benchmark/utils.js
+++ tests/scalability/couchdb-benchmark/utils.js
@@ -1,5 +1,6 @@
 const fs = require('fs').promises;
 const path = require('path');
+const request = require('@medic/couch-request');
 
 const FILE_NAME = path.join(__dirname, '..', 'benchmark_results.md');
 
@@ -29,9 +30,31 @@ const printResults = async (endpoint, results) => {
   await fs.appendFile(FILE_NAME, formatted, 'utf8');
 };
 
+const getServerInfo = async () => {
+  const serverURL = new URL(module.exports.db);
+  serverURL.pathname = '';
+
+  return await request.get({ url: serverURL.toString() });
+};
+
+const writeDbInfo = async () => {
+  const dbInfo = await request.get({ url: module.exports.db });
+  const serverInfo = await getServerInfo();
+
+  let formatted = '# CouchDb Performance\n';
+
+  formatted += `## Database Info\n`;
+  formatted += `CouchDb version: ${serverInfo.version}\n\n`;
+  formatted += `Database doc count: ${dbInfo.doc_count}\n\n`;
+  formatted += '\n\n';
+
+  await fs.appendFile(FILE_NAME, formatted, 'utf8');
+};
+
 module.exports = {
   printResults,
   cleanFile,
+  writeDbInfo,
   db: process.env.COUCH_URL || 'http://localhost:5984/medic',
 };
 
