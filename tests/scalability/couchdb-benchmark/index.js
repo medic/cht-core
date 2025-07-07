011ba2f1418d0a949eab2b279f07c2511652ef94
Diana Barsan <barsan@medic.org>
2025-07-07 14:53:23 +0300

    fix npm install

--- tests/scalability/couchdb-benchmark/index.js
+++ tests/scalability/couchdb-benchmark/index.js
@@ -1,10 +1,12 @@
-const { printResults, cleanFile } = require('./utils');
+const { printResults, cleanFile, writeDbInfo } = require('./utils');
 
 const testChanges = require('./changes');
 const testAllDocs = require('./all_docs');
 
 (async () => {
   await cleanFile();
+  await writeDbInfo();
+
   await printResults('_changes', await testChanges());
   await printResults('_all_docs', await testAllDocs());
 
