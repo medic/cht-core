ecfd40e1e6fa50166296fdcc6686e458376a7d19
Apoorva Pendse <apoorvavpendse@gmail.com>
2025-07-15 10:15:32 +0530

    remove getUpdatedFields
    We no longer need this as we directly
    `PUT` the updatePayload after validation.
    
    Signed-off-by: Apoorva Pendse <apoorvavpendse@gmail.com>

--- shared-libs/cht-datasource/src/local/libs/core.ts
+++ shared-libs/cht-datasource/src/local/libs/core.ts
@@ -18,27 +18,6 @@ export const normalizeFreetext = (
   return freetext.trim().toLowerCase();
 };
 
-/** @internal*/
-export const getUpdatedFields = (
-  originalDoc:Record<string, unknown>,
-  updatedDoc : Record<string, unknown>,
-  ignoreUpdateFields : Set<string>
-):Record<string, unknown> => {
-  const updatedFields:Record<string, unknown> = {};
-  for (const key of Object.keys(originalDoc)) {
-    if (ignoreUpdateFields.has(key)) {
-      continue;
-    }
-    if (updatedDoc[key] ===undefined) {
-      // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-      delete originalDoc[key];
-      continue;
-    }
-    updatedFields[key] = updatedDoc[key];
-  }
-  return updatedFields;
-};
-
 interface HasParentOrContact {
   contact?: string;
   parent?: string;
