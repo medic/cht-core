47d6d2222d70396220f6398e1cc022c72f65a252
Shashi Sah <sah.shashi2003@gmail.com>
2025-07-20 10:04:21 +0000

    update person getWithLineage to use fetchHydratedDoc and fixed unit test cases
    Signed-off-by: Shashi Sah <sah.shashi2003@gmail.com>

--- shared-libs/cht-datasource/src/local/person.ts
+++ shared-libs/cht-datasource/src/local/person.ts
@@ -8,7 +8,7 @@ import { LocalDataContext, SettingsService } from './libs/data-context';
 import logger from '@medic/logger';
 import { InvalidArgumentError } from '../libs/error';
 import { validateCursor } from './libs/core';
-import lineage from '@medic/lineage';
+import { fetchHydratedDoc } from './libs/lineage';
 
 /** @internal */
 export namespace v1 {
@@ -41,20 +41,13 @@ export namespace v1 {
 
   /** @internal */
   export const getWithLineage = ({ medicDb, settings }: LocalDataContext) => {
-    const { fetchHydratedDoc } = lineage(Promise, medicDb) as { 
-                                      fetchHydratedDoc: (
-                                        uuid: string, 
-                                        options?: { throwWhenMissingLineage?: boolean }, 
-                                        callback?: (err: Error | null, result?: Doc) => void
-                                      ) => Promise<Doc> 
-                                    };
-      
+    const fetchHydratedMedicDoc = fetchHydratedDoc(medicDb);
     return async (identifier: UuidQualifier): Promise<Nullable<Person.v1.PersonWithLineage>> => {
-      const person = await fetchHydratedDoc(identifier.uuid);
+      const person = await fetchHydratedMedicDoc(identifier.uuid);
       if (!isPerson(settings)(person, identifier.uuid)) {
         return null;
       }
-  
+
       return person;
     };
   };
