343a853ed7efaeb99a582fe52061f0a662fdd00c
Najuna Brian <najunabrian@gmail.com>
2025-07-26 03:50:27 +0300

    fix(#9869): conditionally create storage object in CouchDB view
    Fixes integration test failures by only creating storage object when
    storageFree or storageTotal values are defined in the view emission.

--- ddocs/users-meta-db/users-meta/views/device_by_user/map.js
+++ ddocs/users-meta-db/users-meta/views/device_by_user/map.js
@@ -15,22 +15,29 @@ function(doc) {
 
     var deviceInfo = doc.device && doc.device.deviceInfo;
 
+    var deviceObject = {
+      userAgent: doc.device && doc.device.userAgent,
+      versions: {
+        apk: deviceInfo && deviceInfo.app && deviceInfo.app.version,
+        android: deviceInfo && deviceInfo.software && deviceInfo.software.androidVersion,
+        cht: doc.metadata.versions && doc.metadata.versions.app,
+        settings: doc.metadata.versions && doc.metadata.versions.settings,
+      }
+    };
+
+    var storageFree = deviceInfo && deviceInfo.storage && deviceInfo.storage.free;
+    var storageTotal = deviceInfo && deviceInfo.storage && deviceInfo.storage.total;
+    if (storageFree !== undefined || storageTotal !== undefined) {
+      deviceObject.storage = {
+        free: storageFree,
+        total: storageTotal
+      };
+    }
+
     emit([doc.metadata.user, doc.metadata.deviceId], {
       date: doc.metadata.year + '-' + pad(doc.metadata.month) + '-' + pad(doc.metadata.day),
       id: doc._id,
-      device: {
-        userAgent: doc.device && doc.device.userAgent,
-        versions: {
-          apk: deviceInfo && deviceInfo.app && deviceInfo.app.version,
-          android: deviceInfo && deviceInfo.software && deviceInfo.software.androidVersion,
-          cht: doc.metadata.versions && doc.metadata.versions.app,
-          settings: doc.metadata.versions && doc.metadata.versions.settings,
-        },
-        storage: { 
-          free: deviceInfo && deviceInfo.storage && deviceInfo.storage.free,
-          total: deviceInfo && deviceInfo.storage && deviceInfo.storage.total
-        }
-      }
+      device: deviceObject
     });
   }
 }
