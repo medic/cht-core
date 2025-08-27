9dcd227143dfab40a6bf9834d1d181c88f8e2045
Diana Barsan <35681649+dianabarsan@users.noreply.github.com>
2025-08-27 13:02:36 +0300

    feat(#9954): no k8s upgrade in admin app (#10045)
    Adds API endpoint which queries the upgrade service api, and responds with a truthy value when it detects that the upgrade service is the docker upgrade service. Admin app calls the endpoint and hides the upgrade button.
    
    #9954

--- api/src/services/setup/utils.js
+++ api/src/services/setup/utils.js
@@ -350,6 +350,16 @@ const makeUpgradeRequest = async (payload) => {
   return response;
 };
 
+const isDockerUpgradeServiceRunning = async () => {
+  try {
+    const response = await request.get({ url: UPGRADE_SERVICE_URL });
+    return !!response.ok;
+  } catch (err) {
+    logger.error('Error occurred when connecting to upgrade service: %o', err);
+    return false;
+  }
+};
+
 const upgradeResponseSuccess = (payload, response) => {
   if (!response) {
     return false;
@@ -380,4 +390,5 @@ module.exports = {
   getStagingDoc,
   getUpgradeServicePayload,
   makeUpgradeRequest,
+  isDockerUpgradeServiceRunning,
 };
