588541234d436b3fddc1129c0f477daed2c31cce
Maria Lorena Rodriguez <lorerod@gmail.com>
2025-09-17 16:04:49 -0300

    resolve linting errors run script

--- tests/scalability/ongoing/run.sh
+++ tests/scalability/ongoing/run.sh
@@ -12,25 +12,25 @@ if [ $RUN_LOCAL = false ]
 then
   sudo shutdown -P +60
   echo Cloning cht-core to /cht-core
-  git clone --single-branch --branch $TAG_NAME https://github.com/medic/cht-core.git;
+  git clone --single-branch --branch "$TAG_NAME" https://github.com/medic/cht-core.git;
   cd cht-core/tests/scalability/ongoing
 fi
 
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
-if [ -z $BASE_URL ]
+if [ -z "$BASE_URL" ]
 then
     echo "Please pass the base URL via BASE_URL environment variable (e.g., https://192.168.86.250:10449)"
     exit 1
 fi
 
-if [ -z $ADMIN_USER ]
+if [ -z "$ADMIN_USER" ]
 then
     echo "Please pass the admin username via ADMIN_USER environment variable (e.g., medic)"
     exit 1
 fi
 
-if [ -z $ADMIN_PASSWORD ]
+if [ -z "$ADMIN_PASSWORD" ]
 then
     echo "Please pass the admin password via ADMIN_PASSWORD environment variable (e.g., password)"
     exit 1
@@ -39,35 +39,35 @@ fi
 ADMIN_INSTANCE_URL="https://${ADMIN_USER}:${ADMIN_PASSWORD}@${BASE_URL#https://}"
 BASE_INSTANCE_URL="$BASE_URL"
 
-if [ -z $DATA_DIR ]
+if [ -z "$DATA_DIR" ]
 then
     echo "Please pass the path to the desired data directory via DATA_DIR environment variable"
     exit 1
 fi
 
-mkdir -p $DATA_DIR
-mkdir -p $DATA_DIR/dbs
+mkdir -p "$DATA_DIR"
+mkdir -p "$DATA_DIR"/dbs
 
 echo "npm install for jmeter suite"
 npm ci
 
-node --inspect=0.0.0.0:9930 ./generate-data.js $ADMIN_INSTANCE_URL $DATA_DIR
+node --inspect=0.0.0.0:9930 ./generate-data.js "$ADMIN_INSTANCE_URL" "$DATA_DIR"
 
 echo "jmeter install"
-wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.6.3.tgz -O $DATA_DIR/apache-jmeter.tgz
-mkdir $DATA_DIR/jmeter && tar -xf $DATA_DIR/apache-jmeter.tgz -C $DATA_DIR/jmeter --strip-components=1
+wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.6.3.tgz -O "$DATA_DIR"/apache-jmeter.tgz
+mkdir "$DATA_DIR"/jmeter && tar -xf "$DATA_DIR"/apache-jmeter.tgz -C "$DATA_DIR"/jmeter --strip-components=1
 echo "Installing Plugins" &&
-wget  https://repo1.maven.org/maven2/kg/apc/jmeter-plugins-manager/1.4/jmeter-plugins-manager-1.4.jar -O $DATA_DIR/jmeter/lib/ext/jmeter-plugins-manager-1.4.jar
-wget 'http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.2/cmdrunner-2.2.jar' -O $DATA_DIR/jmeter/lib/cmdrunner-2.2.jar
-java -cp $DATA_DIR/jmeter/lib/ext/jmeter-plugins-manager-1.4.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
-$DATA_DIR/jmeter/bin/PluginsManagerCMD.sh install jpgc-mergeresults
+wget  https://repo1.maven.org/maven2/kg/apc/jmeter-plugins-manager/1.4/jmeter-plugins-manager-1.4.jar -O "$DATA_DIR"/jmeter/lib/ext/jmeter-plugins-manager-1.4.jar
+wget 'http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.2/cmdrunner-2.2.jar' -O "$DATA_DIR"/jmeter/lib/cmdrunner-2.2.jar
+java -cp "$DATA_DIR"/jmeter/lib/ext/jmeter-plugins-manager-1.4.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
+"$DATA_DIR"/jmeter/bin/PluginsManagerCMD.sh install jpgc-mergeresults
 echo "jmeter do it!"
-echo $(which node)
+which node
 
 OPTIMAL_THREADS=$(node calculate-threads.js 2>/dev/null | grep -E '^[0-9]+$' | head -1)
 echo "Calculated optimal threads from config.js: $OPTIMAL_THREADS"
 
-$DATA_DIR/jmeter/bin/jmeter -n  -t ./sync.jmx -Jworking_dir=./ -Jnode_binary=$(which node) -Jdata_dir=$DATA_DIR -Jinstance_url=$BASE_INSTANCE_URL -Jskip=1 -Jnumber_of_threads=$OPTIMAL_THREADS -l ./report/cli_run.jtl -e -o ./report
+"$DATA_DIR"/jmeter/bin/jmeter -n  -t ./sync.jmx -Jworking_dir=./ -Jnode_binary="$(which node)" -Jdata_dir="$DATA_DIR" -Jinstance_url="$BASE_INSTANCE_URL" -Jskip=1 -Jnumber_of_threads="$OPTIMAL_THREADS" -l ./report/cli_run.jtl -e -o ./report
 mv ./jmeter.log ./report/jmeter.log
 echo "FINISHED! "
 
