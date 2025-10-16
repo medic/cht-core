b0f5e3974e2002efe9c9bd2c328494ddd7bbc8ec
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-10-16 15:04:32 -0500

    Add new qualifiers

--- shared-libs/cht-datasource/src/qualifier.ts
+++ shared-libs/cht-datasource/src/qualifier.ts
@@ -1,4 +1,4 @@
-import { isString, hasField, isRecord, Nullable } from './libs/core';
+import { hasField, isRecord, isString, Nullable } from './libs/core';
 import { InvalidArgumentError } from './libs/error';
 
 /**
@@ -10,7 +10,7 @@ export type UuidQualifier = Readonly<{ uuid: string }>;
  * Builds a qualifier that identifies an entity by its UUID.
  * @param uuid the UUID of the entity
  * @returns the qualifier
- * @throws Error if the UUID is invalid
+ * @throws InvalidArgumentError if the UUID is invalid
  */
 export const byUuid = (uuid: string): UuidQualifier => {
   if (!isString(uuid) || uuid.length === 0) {
@@ -38,7 +38,7 @@ export type ContactTypeQualifier = Readonly<{ contactType: string }>;
  * Build the TypeQualifier that categorizes an entity by its type
  * @param contactType the type of the entity
  * @returns the type
- * @throws Error if the type is invalid
+ * @throws InvalidArgumentError if the type is invalid
  */
 export const byContactType = (contactType: string): ContactTypeQualifier => {
   if (!isString(contactType) || contactType.length === 0) {
@@ -66,7 +66,7 @@ export type FreetextQualifier = Readonly<{ freetext: string }>;
  * Builds a qualifier for finding entities by the given freetext string.
  * @param freetext the text to search with
  * @returns the qualifier
- * @throws Error if the search string is not invalid
+ * @throws InvalidArgumentError if the search string is not valid
  *
  * See {@link isFreetextQualifier} for validity of a search string.
  */
@@ -115,6 +115,110 @@ export const isKeyedFreetextQualifier = (qualifier: FreetextQualifier): boolean 
   }
 
   return false;
+};
+
+// /**
+//  * Identifies a specific reporting period (e.g. a calendar month) with the format YYYY-MM.
+//  */
+// export type ReportingPeriod = string & { __brand: 'ReportingPeriod' };
+
+/**
+ * A qualifier that identifies entities based on a reporting period (e.g. a calendar month). The reporting period
+ * should be represented with the format YYYY-MM (e.g. "2025-07").
+ */
+export interface ReportingPeriodQualifier {
+  readonly reportingPeriod: string;
+}
+
+const REPORTING_PERIOD_PATTERN = /^\d{4}-(0[1-9]|1[0-2])$/;
+
+/**
+ * Returns `true` if the given qualifier is a {@link ReportingPeriodQualifier} otherwise `false`.
+ * @param qualifier the qualifier to check
+ * @returns `true` if the given qualifier is a {@link ReportingPeriodQualifier}, otherwise `false`.
+ */
+export const isReportingPeriodQualifier = (qualifier: unknown): qualifier is ReportingPeriodQualifier => {
+  return isRecord(qualifier) &&
+    hasField(qualifier, { name: 'reportingPeriod', type: 'string' }) &&
+    REPORTING_PERIOD_PATTERN.test(qualifier.reportingPeriod);
+};
+
+/**
+ * Builds a qualifier for finding entities by reporting period.
+ * @param reportingPeriod the reporting period to search with
+ * @returns the qualifier
+ * @throws InvalidArgumentError if the reporting period is not valid
+ */
+export const byReportingPeriod = (reportingPeriod: string): ReportingPeriodQualifier => {
+  const qualifier = { reportingPeriod };
+  if (!isReportingPeriodQualifier(qualifier)) {
+    throw new InvalidArgumentError(`Invalid reporting period [${reportingPeriod}].`);
+  }
+  return qualifier;
+};
+
+/**
+ * A qualifier that identifies entities based on a username (without the "org.couchdb.user:" prefix).
+ */
+export interface UsernameQualifier {
+  readonly username: string
+}
+
+/**
+ * Returns `true` if the given qualifier is a {@link UsernameQualifier} otherwise `false`.
+ * @param qualifier the qualifier to check
+ * @returns `true` if the given qualifier is a {@link UsernameQualifier}, otherwise `false`.
+ */
+export const isUsernameQualifier = (qualifier: unknown): qualifier is UsernameQualifier => {
+  return isRecord(qualifier) &&
+    hasField(qualifier, { name: 'username', type: 'string' }) &&
+    qualifier.username.length > 0;
+};
+
+/**
+ * Builds a qualifier for finding entities by username.
+ * @param username the username to search with
+ * @returns the qualifier
+ * @throws InvalidArgumentError if the username is not valid
+ */
+export const byUsername = (username: string): UsernameQualifier => {
+  const qualifier = { username };
+  if (!isUsernameQualifier(qualifier)) {
+    throw new InvalidArgumentError(`Invalid username [${username}].`);
+  }
+  return qualifier;
+};
+
+/**
+ * A qualifier that identifies entities based their association with the identified contact.
+ */
+export interface ContactUuidQualifier {
+  readonly contactUuid: string
+}
+
+/**
+ * Returns `true` if the given qualifier is a {@link ContactUuidQualifier} otherwise `false`.
+ * @param qualifier the qualifier to check
+ * @returns `true` if the given qualifier is a {@link ContactUuidQualifier}, otherwise `false`.
+ */
+export const isContactUuidQualifier = (qualifier: unknown): qualifier is ContactUuidQualifier => {
+  return isRecord(qualifier) &&
+    hasField(qualifier, { name: 'contactUuid', type: 'string' }) &&
+    qualifier.contactUuid.length > 0;
+};
+
+/**
+ * Builds a qualifier for finding entities by contact UUID.
+ * @param contactUuid the contact UUID to search with
+ * @returns the qualifier
+ * @throws InvalidArgumentError if the contact UUID is not valid
+ */
+export const byContactUuid = (contactUuid: string): ContactUuidQualifier => {
+  const qualifier = { contactUuid };
+  if (!isContactUuidQualifier(qualifier)) {
+    throw new InvalidArgumentError(`Invalid contact UUID [${contactUuid}].`);
+  }
+  return qualifier;
 };
 
 /**
