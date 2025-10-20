8cadca033b3a2a66cdf23b79ca1af48ae3bb675b
Tom Wier <thomas@medic.org>
2025-10-20 13:13:22 +0300

    chore(#10322): docs_by_type emist only 'unusual' types

--- ddocs/medic-db/medic-client/views/doc_by_type/map.js
+++ ddocs/medic-db/medic-client/views/doc_by_type/map.js
@@ -1,3 +1,14 @@
 function(doc) {
-  emit([ doc.type ]);
+  // if querying for contacts, data_records, or tasks, use a view
+  // specific to that type
+  if (doc.type !== 'contact' &&
+      doc.type !== 'clinic' &&
+      doc.type !== 'health_center' &&
+      doc.type !== 'district_hospital' &&
+      doc.type !== 'person' &&
+      doc.type !== 'data_record' &&
+      doc.type !== 'task' &&
+      doc.type !== 'target') {
+    emit([ doc.type ]);
+  }
 }
