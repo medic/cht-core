a6b5bf59ccdac8f4cb92a8aed5eeb508d8849159
Clifton McIntosh <cliff.mcintosh@gmail.com>
2025-11-07 14:40:35 -0600

    chore(#9621): define online user constant

--- shared-libs/constants/src/index.js
+++ shared-libs/constants/src/index.js
@@ -1,6 +1,11 @@
 // Shared constants for CHT-Core
 // This library provides a single source of truth for magic strings and constants used throughout the application
 
+// User Roles
+const USER_ROLES = {
+  ONLINE: 'mm-online',
+};
+
 module.exports = {
-  // Constants will be added here
+  USER_ROLES,
 };
