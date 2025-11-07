c7baf4340b96e0b302494ccf2d41fb4d51f92ec3
Clifton McIntosh <cliff.mcintosh@gmail.com>
2025-11-07 13:38:02 -0600

    chore(#9621): add shared constants

---
+++ shared-libs/constants/src/index.js
@@ -0,0 +1,6 @@
+// Shared constants for CHT-Core
+// This library provides a single source of truth for magic strings and constants used throughout the application
+
+module.exports = {
+  // Constants will be added here
+};
