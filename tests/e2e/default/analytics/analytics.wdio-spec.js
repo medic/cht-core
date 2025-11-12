fef3308dce7e285d19aa9fd3c552e1b0aff25c11
Bernard K <kagondubernard81@gmail.com>
2025-11-12 20:47:39 +0300

    feat(#10332): add targets filter selection telemetry (#10371)
    Co-authored-by: Joshua Kuestersteffen <jkuester@kuester7.com>
    Co-authored-by: paulpascal <paulalogno@medic.org>

--- tests/e2e/default/analytics/analytics.wdio-spec.js
+++ tests/e2e/default/analytics/analytics.wdio-spec.js
@@ -11,6 +11,7 @@ const placeFactory = require('@factories/cht/contacts/place');
 const personFactory = require('@factories/cht/contacts/person');
 const chtConfUtils = require('@utils/cht-conf');
 const chtDbUtils = require('@utils/cht-db');
+const { getTelemetry, destroyTelemetryDb } = require('@utils/telemetry');
 const { TARGET_MET_COLOR, TARGET_UNMET_COLOR } = analyticsPage;
 
 describe('Targets', () => {
@@ -49,6 +50,7 @@ describe('Targets', () => {
 
   afterEach(async () => {
     await utils.revertSettings(true);
+    await destroyTelemetryDb(chw.username);
   });
 
   it('should display targets from default config', async () => {
@@ -103,6 +105,8 @@ describe('Targets', () => {
 
     const filterLabel = await analyticsPage.filterOptionLabel();
     expect(await filterLabel.getText()).to.equal('This month');
+    const telemetry = await getTelemetry('sidebar_filter:analytics:targets:open', chw.username);
+    expect(telemetry.length).to.equal(1);
   });
 
   it('should show error message for bad config', async () => {
