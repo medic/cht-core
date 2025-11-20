3e6b194fd824dce9be3846bcd0047855fe4296f9
JenniAjoy <52172589+JenniAjoy@users.noreply.github.com>
2025-11-20 12:09:12 -0600

    6784-safariFix method fix

--- api/src/public/login/script.js
+++ api/src/public/login/script.js
@@ -168,10 +168,11 @@ const checkSession = function() {
 };
 
 const isUsingSupportedBrowser = () => {
-  if (!window.bowser || !window.bowser.getParser) {
+  if (!globalThis.bowser || !globalThis.bowser.getParser) {
     return false;
   }
-  const parser = window.bowser.getParser(window.navigator.userAgent);
+
+  const parser = globalThis.bowser.getParser(globalThis.navigator.userAgent);
   return parser.satisfies({
     chrome: '>=90', // Chrome 90 was released on April 14, 2021; for desktop and Android.
     firefox: '>=98', // Firefox 98 was released on March 8, 2022; for desktop and Android.
