79eff6df150ce220d3c9505697fa145ba3f576b2
Clifton McIntosh <cliff.mcintosh@gmail.com>
2025-11-21 15:38:40 -0600

    alphabetize constants in shared-libs/constants/src/index.js

--- shared-libs/constants/src/index.js
+++ shared-libs/constants/src/index.js
@@ -1,14 +1,19 @@
 // Shared constants for CHT-Core
 // This library provides a single source of truth for magic strings and constants used throughout the application
 
-// User Roles
-const USER_ROLES = {
-  ONLINE: 'mm-online',
-};
-
 // Document IDs
 const DOC_IDS = {
   SERVICE_WORKER_META: 'service-worker-meta',
+};
+
+// Document Types
+const DOC_TYPES = {
+  TRANSLATIONS: 'translations',
+};
+
+// HTTP Headers
+const HTTP_HEADERS = {
+  REQUEST_ID: 'X-Request-Id',
 };
 
 // Sentinel Metadata
@@ -17,20 +22,15 @@ const SENTINEL_METADATA = {
   BACKGROUND_SEQ: '_local/background-seq',
 };
 
-// HTTP Headers
-const HTTP_HEADERS = {
-  REQUEST_ID: 'X-Request-Id',
-};
-
-// Document Types
-const DOC_TYPES = {
-  TRANSLATIONS: 'translations',
+// User Roles
+const USER_ROLES = {
+  ONLINE: 'mm-online',
 };
 
 module.exports = {
-  USER_ROLES,
   DOC_IDS,
-  SENTINEL_METADATA,
-  HTTP_HEADERS,
   DOC_TYPES,
+  HTTP_HEADERS,
+  SENTINEL_METADATA,
+  USER_ROLES,
 };
