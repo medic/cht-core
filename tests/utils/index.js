7dc5db2ca7e213a9c12381f655377da20b868c01
Diana Barsan <barsan@medic.org>
2026-02-13 18:10:31 +0200

    refactor k3d registry setup: replace inline config creation with `fs.writeFile`

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -1251,11 +1251,13 @@ const createCluster = async (dataDir) => {
   const match = port.trim().match(/:(\d+)$/);
   K3D_REGISTRY_PORT = match[1];
 
+  // Create registries config file for k3d
   const registriesConfig = `/tmp/k3d-registries.yaml`;
   const configContent = `mirrors:
   "${K3D_REPO()}":
     endpoint:
-      - http://${K3D_REPO()}`;
+      - http://${K3D_REPO()}
+`;
   await fs.writeFile(registriesConfig, configContent);
 
   await runCommand(
