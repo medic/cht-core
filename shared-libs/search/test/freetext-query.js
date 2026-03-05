38b67101af85ef136dbe2e57a1f5a262322ddcc6
Diana Barsan <barsan@medic.org>
2026-03-05 17:20:00 +0200

    tiny up freetext-query.js tests

--- shared-libs/search/test/freetext-query.js
+++ shared-libs/search/test/freetext-query.js
@@ -2,11 +2,10 @@ const chai = require('chai');
 const sinon = require('sinon');
 const chtDatasource = require('@medic/cht-datasource');
 const logger = require('@medic/logger');
+const queryFreetext = require('../src/freetext-query').queryFreetext;
 
 describe('freetext-query', () => {
   'use strict';
-
-  let queryFreetext;
 
   beforeEach(() => {
     sinon.stub(logger, 'error');
@@ -14,14 +13,7 @@ describe('freetext-query', () => {
 
   afterEach(() => {
     sinon.restore();
-    delete require.cache[require.resolve('../src/freetext-query')];
   });
-
-  const loadModule = (getDatasourceImpl) => {
-    sinon.stub(chtDatasource, 'getDatasource').callsFake(getDatasourceImpl);
-    delete require.cache[require.resolve('../src/freetext-query')];
-    queryFreetext = require('../src/freetext-query').queryFreetext;
-  };
 
   const createAsyncGenerator = (values) => {
     return (async function* () {
@@ -33,7 +25,7 @@ describe('freetext-query', () => {
 
   it('should query reports freetext and return rows', async () => {
     const getUuidsByFreetext = sinon.stub().returns(createAsyncGenerator(['id1', 'id2']));
-    loadModule(() => ({ v1: { report: { getUuidsByFreetext } } }));
+    sinon.stub(chtDatasource, 'getDatasource').returns({ v1: { report: { getUuidsByFreetext } } });
 
     const request = { params: { key: 'search_term' } };
     const result = await queryFreetext({}, request, 'reports');
@@ -45,7 +37,10 @@ describe('freetext-query', () => {
   it('should query contacts freetext without type', async () => {
     const getUuidsByFreetext = sinon.stub().returns(createAsyncGenerator(['c1', 'c2']));
     const getUuidsByTypeFreetext = sinon.stub();
-    loadModule(() => ({ v1: { contact: { getUuidsByFreetext, getUuidsByTypeFreetext } } }));
+
+    sinon.stub(chtDatasource, 'getDatasource').returns(
+      { v1: { contact: { getUuidsByFreetext, getUuidsByTypeFreetext } } }
+    );
 
     const request = { params: { key: 'term' } };
     const result = await queryFreetext({}, request, 'contacts');
@@ -58,7 +53,9 @@ describe('freetext-query', () => {
   it('should query contacts freetext with type', async () => {
     const getUuidsByFreetext = sinon.stub();
     const getUuidsByTypeFreetext = sinon.stub().returns(createAsyncGenerator(['c3']));
-    loadModule(() => ({ v1: { contact: { getUuidsByFreetext, getUuidsByTypeFreetext } } }));
+    sinon.stub(chtDatasource, 'getDatasource').returns(
+      { v1: { contact: { getUuidsByFreetext, getUuidsByTypeFreetext } } }
+    );
 
     const request = { params: { key: 'term', type: 'person' } };
     const result = await queryFreetext({}, request, 'contacts');
@@ -69,7 +66,7 @@ describe('freetext-query', () => {
   });
 
   it('should return empty array for unknown type', async () => {
-    loadModule(() => ({ v1: {} }));
+    sinon.stub(chtDatasource, 'getDatasource').returns({ v1: {} });
 
     const request = { params: { key: 'term' } };
     const result = await queryFreetext({}, request, 'unknown');
@@ -79,9 +76,7 @@ describe('freetext-query', () => {
   });
 
   it('should return empty array and log error on exception', async () => {
-    loadModule(() => {
-      throw new Error('datasource error');
-    });
+    sinon.stub(chtDatasource, 'getDatasource').throws(new Error('datasource error'));
 
     const request = { params: { key: 'term' } };
     const result = await queryFreetext({}, request, 'reports');
@@ -93,7 +88,7 @@ describe('freetext-query', () => {
 
   it('should return empty array for empty generator', async () => {
     const getUuidsByFreetext = sinon.stub().returns(createAsyncGenerator([]));
-    loadModule(() => ({ v1: { report: { getUuidsByFreetext } } }));
+    sinon.stub(chtDatasource, 'getDatasource').returns({ v1: { report: { getUuidsByFreetext } } });
 
     const request = { params: { key: 'nothing' } };
     const result = await queryFreetext({}, request, 'reports');
