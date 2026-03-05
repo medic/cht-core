15295cfbab642916d57ea53ab29542e596512a31
Lidiya <duddekuntalidiya@gmail.com>
2026-03-05 23:09:34 +0530

    Update map.js

--- ddocs/medic-db/medic/views/tasks_in_terminal_state/map.js
+++ ddocs/medic-db/medic/views/tasks_in_terminal_state/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-  if (doc.type === 'task') {
+  if (doc.type === DOC_TYPES.TASK) {
     var isTerminalState = ['Cancelled', 'Completed', 'Failed'].indexOf(doc.state) >= 0;
     if (isTerminalState && doc.emission) {
       emit(doc.emission.endDate);
