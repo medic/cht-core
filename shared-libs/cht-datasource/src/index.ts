f382785bef793bd415b17fb2d693220089491276
Sugat Bajracharya <30311933+sugat009@users.noreply.github.com>
2026-03-10 17:45:42 +0545

    feat(#9835): add cht datasource apis for creation and update of contacts and reports (#10083)
    Signed-off-by: Apoorva Pendse <apoorvavpendse@gmail.com>
    Signed-off-by: apoorvapendse <apoorvavpendse@gmail.com>
    Co-authored-by: Apoorva Pendse <apoorvavpendse@gmail.com>
    Co-authored-by: Joshua Kuestersteffen <jkuester@kuester7.com>

--- shared-libs/cht-datasource/src/index.ts
+++ shared-libs/cht-datasource/src/index.ts
@@ -36,17 +36,19 @@ import * as Qualifier from './qualifier';
 import { and, byContactId, byContactIds, byReportingPeriod } from './qualifier';
 import * as Report from './report';
 import * as Target from './target';
+import * as Input from './input';
 import { DEFAULT_DOCS_PAGE_LIMIT, DEFAULT_IDS_PAGE_LIMIT, } from './libs/constants';
 
 export { Nullable, NonEmptyArray } from './libs/core';
 export { DataContext } from './libs/data-context';
 export { getLocalDataContext } from './local';
 export { getRemoteDataContext } from './remote';
-export { InvalidArgumentError } from './libs/error';
+export { InvalidArgumentError, ResourceNotFoundError } from './libs/error';
 export * as Contact from './contact';
 export * as Person from './person';
 export * as Place from './place';
 export * as Qualifier from './qualifier';
+export * as Input from './input';
 export * as Report from './report';
 export * as Target from './target';
 
@@ -209,7 +211,7 @@ export const getDatasource = (ctx: DataContext) => {
          * returned. Subsequent pages can be retrieved by providing the cursor returned with the previous page.
          * @param limit the maximum number of place to return. Default is 100.
          * @returns a page of places for the provided specifications
-         * @throws InvalidArgumentError if no type is provided or if the type is not for a place
+         * @throws InvalidArgumentError if no type is provided or if the type is not a supported place contact type
          * @throws InvalidArgumentError if the provided limit is `<= 0`
          * @throws InvalidArgumentError if the provided cursor is not a valid page token or `null`
          * @see {@link getByType} which provides the same data, but without having to manually account for paging
@@ -226,9 +228,42 @@ export const getDatasource = (ctx: DataContext) => {
          * Returns a generator for fetching all places with the given type.
          * @param placeType the type of place to return
          * @returns a generator for fetching all places with the given type
-         * @throws InvalidArgumentError if no type if provided or if the type is not for a place
+         * @throws InvalidArgumentError if no type if provided or if the type is not a supported place contact type
          */
-        getByType: (placeType: string) => ctx.bind(Place.v1.getAll)(Qualifier.byContactType(placeType))
+        getByType: (placeType: string) => ctx.bind(Place.v1.getAll)(Qualifier.byContactType(placeType)),
+
+        /**
+         * Creates a new place record.
+         * @param input input fields for creating a place
+         * @returns the created place record
+         * @throws InvalidArgumentError if `type` is not provided or is not a supported place contact type
+         * @throws InvalidArgumentError if `name` is not provided
+         * @throws InvalidArgumentError if `parent` is not provided for types requiring a parent or is not the
+         * identifier of a valid contact. The parent contact's type must be one of the supported parent contact
+         * types for the new place.
+         * @throws InvalidArgumentError if the provided `reported_date` is not in a valid format. Valid formats are
+         * 'YYYY-MM-DDTHH:mm:ssZ', 'YYYY-MM-DDTHH:mm:ss.SSSZ', or <unix epoch>.
+         * @throws InvalidArgumentError if the provided `contact` is not the identifier of a valid person contact
+         */
+        create: (input: Input.v1.PlaceInput) => ctx.bind(Place.v1.create)(input),
+
+        /**
+         * Updates an existing place to have the provided data.
+         * @param updated the updated place data. The complete data for the place must be provided. Existing fields not
+         * included in the updated data will be removed from the place. If the provided parent/contact lineage is
+         * hydrated (e.g. for a {@link PlaceWithLineage}), the lineage will be properly dehydrated before being stored.
+         * @returns the updated place with the new `_rev` value
+         * @throws InvalidArgumentError if `_id` is not provided
+         * @throws ResourceNotFoundError if `_id does not identify an existing place contact
+         * @throws InvalidArgumentError if `_rev` is not provided or does not match the place's current `_rev` value
+         * @throws InvalidArgumentError if `name` is not provided
+         * @throws InvalidArgumentError if the provided `contact` is not the identifier of a valid person contact
+         * @throws InvalidArgumentError if any of the following read-only properties are changed: `reported_date`,
+         * `parent`, `type`, `contact_type`
+         */
+        update: <T extends Place.v1.Place | Place.v1.PlaceWithLineage>(
+          updated: Input.v1.UpdatePlaceInput<T>
+        ) => ctx.bind(Place.v1.update)(updated)
       },
       person: {
         /**
@@ -274,6 +309,36 @@ export const getDatasource = (ctx: DataContext) => {
          * @throws InvalidArgumentError if no type is provided or if the type is not for a person
          */
         getByType: (personType: string) => ctx.bind(Person.v1.getAll)(Qualifier.byContactType(personType)),
+
+        /**
+         * Creates a new person record.
+         * @param input input fields for creating a person
+         * @returns the created person record
+         * @throws InvalidArgumentError if `type` is not provided or is not a supported person contact type
+         * @throws InvalidArgumentError if `name` is not provided
+         * @throws InvalidArgumentError if `parent` is not provided or is not the identifier of a valid contact. The
+         * parent contact's type must be one of the supported parent contact types for the new person.
+         * @throws InvalidArgumentError if the provided `reported_date` is not in a valid format. Valid formats are
+         * 'YYYY-MM-DDTHH:mm:ssZ', 'YYYY-MM-DDTHH:mm:ss.SSSZ', or <unix epoch>.
+         */
+        create: (input: Input.v1.PersonInput) => ctx.bind(Person.v1.create)(input),
+
+        /**
+         * Updates an existing person to have the provided data.
+         * @param updated the updated person data. The complete data for the person must be provided. Existing fields
+         * not included in the updated data will be removed from the person. If the provided parent lineage is
+         * hydrated (e.g. for a {@link PersonWithLineage}), the lineage will be properly dehydrated before being stored.
+         * @returns the updated person with the new `_rev` value
+         * @throws InvalidArgumentError if `_id` is not provided
+         * @throws ResourceNotFoundError if `_id does not identify an existing person contact
+         * @throws InvalidArgumentError if `_rev` is not provided or does not match the person's current `_rev` value
+         * @throws InvalidArgumentError if `name` is not provided
+         * @throws InvalidArgumentError if any of the following read-only properties are changed: `reported_date`,
+         * `parent`, `type`, `contact_type`
+         */
+        update: <T extends Person.v1.Person | Person.v1.PersonWithLineage>(
+          updated: T
+        ) => ctx.bind(Person.v1.update)(updated)
       },
       report: {
         /**
@@ -320,6 +385,36 @@ export const getDatasource = (ctx: DataContext) => {
         getUuidsByFreetext: (
           qualifier: string,
         ) => ctx.bind(Report.v1.getUuids)(Qualifier.byFreetext(qualifier)),
+
+        /**
+         * Creates a new report record.
+         * @param input input fields for creating a report
+         * @returns the created report record
+         * @throws InvalidArgumentError if `form` is not provided or is not a supported form id
+         * @throws InvalidArgumentError if `contact` is not provided or is not the identifier of a valid contact
+         * @throws InvalidArgumentError if the provided `reported_date` is not in a valid format. Valid formats are
+         * 'YYYY-MM-DDTHH:mm:ssZ', 'YYYY-MM-DDTHH:mm:ss.SSSZ', or <unix epoch>.
+         */
+        create: (input: Input.v1.ReportInput) => ctx.bind(Report.v1.create)(input),
+
+        /**
+         * Updates an existing report to have the provided data.
+         * @param updated the updated report data. The complete data for the report must be provided. Existing fields
+         * not included in the updated data will be removed from the report. If the provided parent/patient/place
+         * lineage is hydrated (e.g. for a {@link ReportWithLineage}), the lineage will be properly dehydrated before
+         * being stored.
+         * @returns the updated report with the new `_rev` value
+         * @throws InvalidArgumentError if `_id` is not provided
+         * @throws ResourceNotFoundError if `_id does not identify an existing report
+         * @throws InvalidArgumentError if `_rev` is not provided or does not match the report's current `_rev` value
+         * @throws InvalidArgumentError if `form` is not provided or is not a supported form id
+         * @throws InvalidArgumentError if `contact` is not provided or is not a valid contact
+         * @throws InvalidArgumentError if any of the following read-only properties are changed: `reported_date`,
+         * `type`
+         */
+        update: <T extends Report.v1.Report | Report.v1.ReportWithLineage>(
+          updated: T
+        ) => ctx.bind(Report.v1.update)(updated)
       },
       target: {
         /**
@@ -384,7 +479,7 @@ export const getDatasource = (ctx: DataContext) => {
          * @throws InvalidArgumentError if no contact identifiers are provided
          */
         getByReportingPeriodContactIds: (
-          reportingPeriod: string, 
+          reportingPeriod: string,
           contactIds: string | [string, ...string[]]
         ) => ctx.bind(Target.v1.getAll)(and(
           byReportingPeriod(reportingPeriod),
