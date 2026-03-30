092738330a8d7a724655ddd6f5adcf3f4ecbc77b
Diana Barsan <barsan@medic.org>
2026-03-30 14:55:22 +0300

    wait for page load before trying to sync.
    Signed-off-by: Diana Barsan <barsan@medic.org>

--- tests/e2e/default/db/db-sync.wdio-spec.js
+++ tests/e2e/default/db/db-sync.wdio-spec.js
@@ -211,7 +211,7 @@ describe('db-sync', () => {
       const { rev } = await createMetaDoc(localDoc);
       localDoc._rev = rev;
 
-      await browser.refresh(); // meta databases sync every 30 minutes
+      await commonElements.refresh(); // meta databases sync every 30 minutes
       await commonElements.sync();
 
       const [remoteDoc] = await utils.getMetaDocs(restrictedUserName, [localDoc._id]);
@@ -226,7 +226,7 @@ describe('db-sync', () => {
       const readReport = { _id: `read:report:${report2}` };
       await utils.saveMetaDocs(restrictedUserName, [readReport]);
 
-      await browser.refresh(); // meta databases sync every 30 minutes
+      await commonElements.refresh(); // meta databases sync every 30 minutes
       await commonElements.sync();
 
       // if the test fails, it helps to see which reports are read or not in the failpic
