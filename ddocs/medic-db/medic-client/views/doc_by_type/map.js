90291807fc1017fabbf8b5285462768c6cf026ee
Tom Wier <thomas@medic.org>
2026-04-07 14:54:27 +0300

    feat(#10638): not indexing common documnets in doc_by_type

--- ddocs/medic-db/medic-client/views/doc_by_type/map.js
+++ ddocs/medic-db/medic-client/views/doc_by_type/map.js
@@ -1,3 +1,9 @@
 function(doc) {
-  emit([ doc.type ]);
+  var excluded = [
+    'task', 'data_record', 'contact', 'target', 'telemetry',
+    'district_hospital', 'health_center', 'clinic', 'person'
+  ];
+  if (excluded.indexOf(doc.type) === -1) {
+    emit([ doc.type ]);
+  }
 }
