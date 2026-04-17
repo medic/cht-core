6020734cfbf58fabb103d43d665ccd3817775af3
Aravind <gmarav005@gmail.com>
2026-04-17 21:32:53 +0530

    fix(test): remove local DOC_TYPES declaration and use global

--- admin/tests/unit/controllers/multiple-user.spec.js
+++ admin/tests/unit/controllers/multiple-user.spec.js
@@ -1,4 +1,4 @@
-const { DOC_TYPES } = require('@medic/constants');
+const { DOC_TYPES: LOCAL_DOC_TYPES } = require('@medic/constants');
 
 describe('MultipleUserCtrl controller', () => {
   'use strict';
@@ -83,7 +83,7 @@ describe('MultipleUserCtrl controller', () => {
               },
               role: 'chw',
               geolocalized: 'false',
-              type: DOC_TYPES.CONTACT,
+              type: LOCAL_DOC_TYPES.CONTACT,
               contact_type: 'c62_chw',
               name: 'test user'
             },
