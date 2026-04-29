abe2a2dd222e6e62c03a3c6cbc64d159cab6ee43
Hendrixx-RE <athukuttan2004@gmail.com>
2026-04-29 23:35:04 +0530

    reverted the doc_by_type change as adviced

--- ddocs/medic-db/medic-client/views/doc_by_type/map.js
+++ ddocs/medic-db/medic-client/views/doc_by_type/map.js
@@ -1,6 +1,3 @@
 function(doc) {
-  var INDEXED_TYPES = ['user-settings', 'translations', 'form', 'meta'];
-  if (INDEXED_TYPES.indexOf(doc.type) !== -1) {
-    emit([ doc.type ]);
-  }
+  emit([ doc.type ]);
 }
