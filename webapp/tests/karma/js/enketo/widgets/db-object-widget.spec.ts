c73cf308689c6769226868f8720ec5e084e3261f
Abhishek <abhishekup082@gmail.com>
2026-05-04 01:23:25 +0530

    fix: load contact data in db-object-widget when field becomes relevant

--- webapp/tests/karma/js/enketo/widgets/db-object-widget.spec.ts
+++ webapp/tests/karma/js/enketo/widgets/db-object-widget.spec.ts
@@ -61,4 +61,77 @@ describe('Enketo: DB Object Widget', () => {
 
   });
 
+  describe('enable', () => {
+    let select2SearchInit;
+    let widget;
+    let originalCHTCore;
+
+    const makeSelectInput = (doc?) => ({
+      select2: sinon.stub().returns(doc ? [{ id: 'some-id', doc }] : []),
+    });
+
+    beforeEach(() => {
+      select2SearchInit = sinon.stub().resolves();
+      originalCHTCore = (window as any).CHTCore;
+      (window as any).CHTCore = { Select2Search: { init: select2SearchInit } };
+
+      widget = {
+        _contactTypes: ['person'],
+        _allowNew: false,
+        _filterByParent: false,
+        enable: dbObjectWidget.prototype.enable,
+      };
+    });
+
+    afterEach(() => {
+      (window as any).CHTCore = originalCHTCore;
+    });
+
+    it('should call Select2Search.init() with the pre-populated value when field becomes relevant', () => {
+      const contactId = 'contact-uuid-123';
+      widget._$textInput = { val: sinon.stub().returns(contactId) };
+      widget._$selectInput = makeSelectInput();
+
+      widget.enable();
+
+      expect(select2SearchInit.callCount).to.equal(1);
+      expect(select2SearchInit.args[0][0]).to.equal(widget._$selectInput);
+      expect(select2SearchInit.args[0][1]).to.deep.equal(['person']);
+      expect(select2SearchInit.args[0][2]).to.deep.include({
+        allowNew: false,
+        filterByParent: false,
+        initialValue: contactId,
+      });
+    });
+
+    it('should NOT call Select2Search.init() when the contact doc is already loaded', () => {
+      const contactId = 'contact-uuid-123';
+      widget._$textInput = { val: sinon.stub().returns(contactId) };
+      widget._$selectInput = makeSelectInput({ _id: contactId, name: 'Test Contact' });
+
+      widget.enable();
+
+      expect(select2SearchInit.callCount).to.equal(0);
+    });
+
+    it('should NOT call Select2Search.init() when text input is empty', () => {
+      widget._$textInput = { val: sinon.stub().returns('') };
+      widget._$selectInput = makeSelectInput();
+
+      widget.enable();
+
+      expect(select2SearchInit.callCount).to.equal(0);
+    });
+
+    it('should NOT call Select2Search.init() when _$textInput is not set', () => {
+      widget._$textInput = undefined;
+      widget._$selectInput = makeSelectInput();
+
+      widget.enable();
+
+      expect(select2SearchInit.callCount).to.equal(0);
+    });
+
+  });
+
 });
