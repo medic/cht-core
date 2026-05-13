0b59462f23803c686838199495786a849d2fdf9c
megha1807 <meghakumarig5@gmail.com>
2026-05-13 21:04:35 +0530

    fix: use single quotes in test attachmentBlob type

--- webapp/tests/karma/ts/modules/reports/reports-add.component.spec.ts
+++ webapp/tests/karma/ts/modules/reports/reports-add.component.spec.ts
@@ -544,7 +544,7 @@ describe('Reports Add Component', () => {
         route.snapshot.params = { reportId: 'my_report' };
         const doc = { _id: 'report-id', form: 'my-form' };
         lineageModelGeneratorService.report.resolves({ doc });
-        const attachmentBlob = { attachment: "blob", type: "audio/mpeg" };
+        const attachmentBlob = { attachment: 'blob', type: 'audio/mpeg' };
         dbService.getAttachment.resolves(attachmentBlob);
         const base64 = 'base64';
         fileReaderService.base64.resolves(base64);
@@ -575,7 +575,7 @@ describe('Reports Add Component', () => {
         route.snapshot.params = { reportId: 'my_report' };
         const doc = { _id: 'report-id', form: 'my-form' };
         lineageModelGeneratorService.report.resolves({ doc });
-        const attachmentBlob = { attachment: "blob", type: "video/mp4" };
+        const attachmentBlob = { attachment: 'blob', type: 'video/mp4' };
         dbService.getAttachment.resolves(attachmentBlob);
         const base64 = 'base64';
         fileReaderService.base64.resolves(base64);
