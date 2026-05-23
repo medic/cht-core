5a15dcf2b85a57811ac236bf17b2a2038da344e3
mrjones-plip <mrjones-plip@plip.com>
2026-05-23 14:40:17 -0700

    WIP - add cert, add node exporter

--- tests/scalability/prepare-ec2.sh
+++ tests/scalability/prepare-ec2.sh
@@ -18,5 +18,30 @@ CHT_COMPOSE_PATH=/cht/compose
 COUCHDB_PASSWORD=medicScalability
 EOF
 docker compose up -d
-sleep 60
-/root/cht-core/scripts/add-local-ip-certs-to-docker.sh  cht-nginx-1
+curl -sO https://raw.githubusercontent.com/medic/cht-core/refs/heads/master/scripts/add-local-ip-certs-to-docker.sh
+
+max=120
+count=0
+while [[ count -le max  ]]; do
+  nginx=$(docker ps --format "table {{.Names}}" | grep cht-nginx-1)
+  if [[ "$nginx" == "cht-nginx-1" ]]; then
+    bash ./add-local-ip-certs-to-docker.sh cht-nginx-1
+    count=max
+  fi
+  ((count=count+1))
+done
+
+arch=$(dpkg --print-architecture)
+
+node_exp_url='https://github.com/prometheus/node_exporter/releases/download/v1.11.1/node_exporter-1.11.1.linux-'
+if [[ "$arch" == "amd64" ]]; then
+  node_exp_url="${node_exp_url}amd64.tar.gz"
+  echo "found amd64"
+elif [[ "$arch" e==q "arm64" ]]; then
+  node_exp_url="${node_exp_url}arm64.tar.gz"
+  echo "found arm64"
+else
+  echo "${node_exp_url} not found, exiting"
+  exit 1
+fi
+curl -Os ${node_exp_url}
