1596e1338d65de1667544451b4c847713bd2e35a
mrjones-plip <mrjones-plip@plip.com>
2026-05-23 13:38:52 -0700

    test(#medic-infra-1394) ec2 test boostrap for scalability

--- tests/scalability/prepare-ec2.sh
+++ tests/scalability/prepare-ec2.sh
@@ -2,35 +2,17 @@
 set -e
 
 shutdown -P +60
-mkdir -p /cht
-chmod 777 /cht;
-
-# Install Docker CE
-apt-get update
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release \
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-mkdir -p /cht/upgrade-service
-mkdir -p /cht/compose
-
+mkdir -p /cht/upgrade-service  /cht/compose
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh ./get-docker.sh
+BUILD="https://staging.dev.medicmobile.org/_couch/builds_4/medic:medic:5.1.0"
 curl -s https://raw.githubusercontent.com/medic/cht-upgrade-service/main/docker-compose.yml \
   -o /cht/upgrade-service/docker-compose.yml
 curl -s "$BUILD"/docker-compose/cht-core.yml -o /cht/compose/cht-core.yml
 curl -s "$BUILD"/docker-compose/cht-couchdb.yml -o /cht/compose/cht-couchdb.yml
 
 cd /cht/upgrade-service/
-cat > ./.env << EOF
+cat > /cht/upgrade-service//.env << EOF
 DOCKER_CONFIG_PATH=/home/.docker
 CHT_COMPOSE_PATH=/cht/compose
 COUCHDB_PASSWORD=medicScalability
