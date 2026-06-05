22a7bf97946824adc751bf746c346aee3744a9a8
Diana Barsan <barsan@medic.org>
2026-06-05 12:11:57 +0300

    Add delay in archiving test to ensure proper document processing.

--- tests/integration/sentinel/schedules/archive.spec.js
+++ tests/integration/sentinel/schedules/archive.spec.js
@@ -197,6 +197,8 @@ describe('sentinel processes archive jobs', () => {
     expect(jobs).to.have.lengthOf(1);
     expect(jobs[0].count).to.equal(COUNT);
 
+    await utils.delayPromise(10000); // wait for all the docs to be processed by api
+
     await runArchiving(true);
 
     const archived = await liveRows(archiveDb, { keys: ids });
