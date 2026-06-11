2ecfe4b677206b52ed595b61768c6db786c2718d
Diana Barsan <barsan@medic.org>
2026-06-11 15:17:02 +0300

    test: reduce default `SEED_REPORTS` from 1000 to 500 in timeout probe
    Signed-off-by: Diana Barsan <barsan@medic.org>

--- tests/integration/couchdb/views/nouveau-timeout-probe.spec.js
+++ tests/integration/couchdb/views/nouveau-timeout-probe.spec.js
@@ -28,7 +28,7 @@ const { DOC_TYPES } = require('@medic/constants');
 
 const TOTAL = Number(process.env.NOUVEAU_PROBE_REQUESTS) || 2000;
 const CONCURRENCY = Number(process.env.NOUVEAU_PROBE_CONCURRENCY) || 10;
-const SEED_REPORTS = Number(process.env.NOUVEAU_PROBE_REPORTS) || 1000;
+const SEED_REPORTS = Number(process.env.NOUVEAU_PROBE_REPORTS) || 500;
 
 // anything within a couple seconds of couch's 30s request_timeout is a (near-)miss worth flagging
 const SLOW_MS = 28 * 1000;
