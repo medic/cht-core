991a4c1467d00fcb939947d78e61d52f7e7bbad9
Diana Barsan <barsan@medic.org>
2026-06-20 07:32:43 +0300

    fix(tests): improve error logging for unmatched messages in transitions utility

--- tests/integration/transitions/utils.js
+++ tests/integration/transitions/utils.js
@@ -23,7 +23,7 @@ const getApiSmsChanges = (messages) => {
         }
         const idx = expectedMessages.findIndex(message => message === change.doc.sms_message.message);
         if (idx === -1) {
-          return;
+          console.error('unexpected message', JSON.stringify(doc, null, 2));
         }
         changes.push(change);
         expectedMessages.splice(idx, 1);
