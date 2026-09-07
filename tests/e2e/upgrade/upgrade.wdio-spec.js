c430042e89041f0a360e78f6c2403d7e6db84ab1
Diana Barsan <barsan@medic.org>
2026-09-07 15:42:28 +0300

    update e2e upgrade tests: add clearUpgradeLogs, extend deployment timeout
    Signed-off-by: Diana Barsan <barsan@medic.org>

--- tests/e2e/upgrade/upgrade.wdio-spec.js
+++ tests/e2e/upgrade/upgrade.wdio-spec.js
@@ -44,6 +44,12 @@ describe('Performing an upgrade', () => {
     return logs.rows.map(row => row.doc);
   };
 
+  const clearUpgradeLogs = async () => {
+    const logs = await getUpgradeLogs();
+    logs.forEach(log => log._deleted = true);
+    await utils.logsDb.bulkDocs(logs);
+  };
+
   const deleteUpgradeLogs = async () => {
     const logs = await getUpgradeLogs();
     logs.forEach(log => log._deleted = true);
@@ -51,6 +57,7 @@ describe('Performing an upgrade', () => {
   };
 
   before(async () => {
+    await clearUpgradeLogs();
     await utils.saveDocs([...docs.places, ...docs.clinics, ...docs.persons, ...docs.reports]);
     await utils.createUsers([docs.user]);
 
@@ -69,7 +76,7 @@ describe('Performing an upgrade', () => {
 
   after(async () => {
     await utils.deleteUsers([docs.user]);
-    await utils.revertDb([/^form:/], true);
+    await utils.revertDb();
   });
 
   it('should have an upgrade_log after installing the app, without logs upgrade is aborted', async () => {
